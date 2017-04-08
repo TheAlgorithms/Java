b0027cd585746ff82bee16b18259a195e22a8cfc
Ashish Agarwal <ashishagarwal2225@gmail.com>
2017-04-08 18:26:06 +0530

    Counting words of a string

---
+++ countwords.java
@@ -0,0 +1,23 @@
+class CountTheWords
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the string");
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        String s=sc.nextLine();
+ 
+        int count = 1;
+ 
+        for (int i = 0; i < s.length()-1; i++)
+        {
+            if((s.charAt(i) == ' ') && (s.charAt(i+1) != ' '))
+            {
+                count++;
+            }
+        }
+ 
+        System.out.println("Number of words in a string = "+count);
+    }
+}
