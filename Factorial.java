9c25ae7ad79413cb7991841d0c2cd7f90bd90926
MatheusPK <matheuskulick@hotmail.com>
2017-07-03 19:41:13 -0300

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,50 +1,24 @@
-package factorial;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class Fatorial {
 
-/**
- * This program will print out the factorial of any non-negative
- * number that you input into it.
- * 
- * @author Marcus
- *
- */
-public class Factorial{
+	public static long factorial(long n){
+          return n > 0 ? n*factorial(n-1) : 1;
+	}
 
-	/**
-	 * The main method
-	 * 
-	 * @param args Command line arguments
-	 */
-	public static void main(String[] args){
-	  Scanner input = new Scanner(System.in);
-          System.out.print("Enter a non-negative integer: ");
-          
-       //If user does not enter an Integer, we want program to fail gracefully, letting the user know why it terminated   
-          try{
-            int number = input.nextInt();
-            
-       //We keep prompting the user until they enter a positive number 
-            while(number < 0){
-              System.out.println("Your input must be non-negative. Please enter a positive number: ");
-              number = input.nextInt();
-            }
-       //Display the result
- 	   System.out.println("The factorial of " + number + " will yield: " + factorial(number));  
-           
-          }catch(Exception e){
-            System.out.println("Error: You did not enter an integer. Program has terminated.");
-          }
-          input.close();
-        }
-        
-	/**
-	 * Recursive Factorial Method
-	 * 
-	 * @param n The number to factorial
-	 * @return The factorial of the number
-	 */
-	public static long factorial(int n){
-          if(n == 0 || n == 1) return 1;
-          return n * factorial(n - 1);
+	public static long input() {
+		long number = 0;
+		try { 
+			number = Long.parseLong(JOptionPane.showInputDialog("pick a non negative number"));
+			if (number < 0) throw new Exception();
+			return number;
+		} catch(Exception e) {
+			JOptionPane.showMessageDialog(null, "Error: " + e);
+			return input();				
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "The number fatorial is " + factorial(input()));
 	}
 }
