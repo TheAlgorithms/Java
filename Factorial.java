4eeeb82f6248b498e3a697a74254c0e9e3555e8d
ASVS-Kartheek <sukruthkartheek@gmail.com>
2017-07-08 12:06:33 +0530

    Updated Factorial using Big Integer Class

--- Factorial.java
+++ Factorial.java
@@ -44,8 +44,12 @@ public class Factorial{
 	 * @return The factorial of the number
 	 */
 	public static BigInteger factorial(int n){
-          if(n == 0 || n == 1) return 1;
-          return n * factorial(n - 1);
+          BigInteger f = new BigInteger("1");
+          
+          for(int i = 2; i<=n; ++i) {
+              f = f.multiply(BigInteger.valueOf(i));
+          }
+          return f;
 	}
 
 }
