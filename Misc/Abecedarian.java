e416be63bb7d684d4bb9d6e443addbf33c3d4b69
Oskar Enmalm <oskarenmalm@gmail.com>
2017-09-29 00:49:28 +0200

    Create Abecedarian.java

---
+++ Misc/Abecedarian.java
@@ -0,0 +1,21 @@
+//Oskar Enmalm 29/9/17
+//An Abecadrian is a word where each letter is in alphabetical order
+    
+class Abecedarian{
+
+    public static boolean isAbecedarian(String s){
+        int index = s.length() - 1;
+
+        for(int i =0; i <index; i++){
+
+            if(s.charAt(i)<=s.charAt(i + 1)){} //Need to check if each letter for the whole word is less than the one before it
+
+            else{
+                System.out.println("That is not abecedarian");
+                return false;
+            }
+
+        }
+        System.out.println("Wow, that number is abecedarian");
+        return true;
+    }
