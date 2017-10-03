79e2eb2826c8819f80bb006f05edbe927cdd69bd
Balance-Breaker <ankit97dhankhar@gmail.com>
2017-10-03 12:38:23 +0530

    Removed unrequired Package

--- Misc/TowerOfHanoiUsingRecursion.java
+++ Misc/TowerOfHanoiUsingRecursion.java
@@ -1,8 +1,6 @@
-package com.manmeet;
-
 import java.util.Scanner;
 
-public class TowerOfHanoi
+class TowerOfHanoi
 {
     public static void shift(int n, String startPole, String intermediatePole, String endPole)
     {
@@ -10,6 +8,8 @@ public class TowerOfHanoi
         {
             return;
         }
+      
+      
         // Shift function is called in recursion for swapping the n-1 disc from the startPole to the intermediatePole
         shift(n - 1, startPole, endPole, intermediatePole);
         System.out.println("\nMove \"" + n + "\" from " + startPole + " --> " + endPole); // Result Printing
