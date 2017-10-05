cc4fda5d0c0f14388e47c066d87e0cdf8e79f54a
Deepak <deepakkumar180997@gmail.com>
2017-10-05 19:32:03 +0530

    updated
    proper spacing

--- Dynamic Programming/EggDropping.java
+++ Dynamic Programming/EggDropping.java
@@ -13,18 +13,18 @@ public class EggDropping
 		for (int i = 1; i <= n; i++)
 		{
 		    eggFloor[i][0] = 0;   // Zero trial for zero floor.
-			  eggFloor[i][1] = 1;   // One trial for one floor 
+		    eggFloor[i][1] = 1;   // One trial for one floor 
 		}
 		
-	  // j trials for only 1 egg
+	        // j trials for only 1 egg
 		
-    for (int j = 1; j <= m; j++)
+                for (int j = 1; j <= m; j++)
 			 eggFloor[1][j] = j;
 		
 		// Using bottom-up approach in DP
 		
-    for (int i = 2; i <= n; i++)
-		{
+   		 for (int i = 2; i <= n; i++)
+		  {
 			for (int j = 2; j <= m; j++)
 			{
 				eggFloor[i][j] = Integer.MAX_VALUE;
@@ -33,22 +33,21 @@ public class EggDropping
 					result = 1 + Math.max(eggFloor[i-1][x-1], eggFloor[i][j-x]);
           
 					//choose min of all values for particular x
-          if (result < eggFloor[i][j])
-						eggFloor[i][j] = result;
+        				 if (result < eggFloor[i][j])
+					     eggFloor[i][j] = result;
 				}
 			}
 		}
+		  
 		return eggFloor[n][m];
-
 	}
 		
 	//testing program
 	public static void main(String args[])
 	{
-		int n = 2, m = 4;
-    //result outputs min no. of trials in worst case for n eggs and m floors
-    
-    int result = minTrials(n, m);
-		System.out.println(result); 
+	      int n = 2, m = 4;
+              //result outputs min no. of trials in worst case for n eggs and m floors
+              int result = minTrials(n, m);
+	      System.out.println(result); 
 	}
 }
