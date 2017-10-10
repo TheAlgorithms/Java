d93bd890f5b1c8ad9bc9bb8bb7ff24c9927b7cb1
disc0rd <cdaniel.lf@gmail.com>
2017-10-10 07:57:31 -0500

    remove empty lines

--- Sorts/ShakerSort.java
+++ Sorts/ShakerSort.java
@@ -4,12 +4,12 @@
 *
 * Cocktail shaker sort,[1] also known as bidirectional bubble sort,[2] cocktail sort, shaker sort (which can also
 * refer to a variant of selection sort), ripple sort, shuffle sort,[3] or shuttle sort, is a variation of bubble sort
- * that is both a stable sorting algorithm and a comparison sort. The algorithm differs from a bubble sort in that
- * it sorts in both directions on each pass through the list. This sorting algorithm is only marginally more
- * difficult to implement than a bubble sort, and solves the problem of turtles in bubble sorts. It provides
- * only marginal performance improvements, and does not improve asymptotic performance; like the bubble sort,
-  * it is not of practical interest (insertion sort is preferred for simple sorts), though it finds some use
-  * in education.
+* that is both a stable sorting algorithm and a comparison sort. The algorithm differs from a bubble sort in that
+* it sorts in both directions on each pass through the list. This sorting algorithm is only marginally more
+* difficult to implement than a bubble sort, and solves the problem of turtles in bubble sorts. It provides
+* only marginal performance improvements, and does not improve asymptotic performance; like the bubble sort,
+* it is not of practical interest (insertion sort is preferred for simple sorts), though it finds some use
+* in education.
 *
 * */
 public class ShakerSort {
@@ -22,7 +22,8 @@ public class ShakerSort {
             System.out.println(i);
         }
     }
-
+    
+    
     public static void shakerSort(int[] arr_) {
         for (int i = 0; i < arr_.length/2; i++) {
             boolean change = false;
@@ -45,8 +46,4 @@ public class ShakerSort {
             if(!change) break;
         }
     }
-
-
-
-
 }
