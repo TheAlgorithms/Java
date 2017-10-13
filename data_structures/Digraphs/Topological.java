a571b934d49328cbc56cd4b26326e3e4077ecf07
Kevin <finchkevinm74@gmail.com>
2017-10-12 20:21:09 -0400

    Add topological sort

---
+++ data_structures/Digraphs/Topological.java
@@ -0,0 +1,131 @@
+**
+ *  The {@code Topological} class represents a data type for 
+ *  determining a topological order of a directed acyclic graph (DAG).
+ *  Recall, a digraph has a topological order if and only if it is a DAG.
+ *  The <em>hasOrder</em> operation determines whether the digraph has
+ *  a topological order, and if so, the <em>order</em> operation
+ *  returns one.
+ *  <p>
+ *  This implementation uses depth-first search.
+ *  The constructor takes time proportional to <em>V</em> + <em>E</em>
+ *  (in the worst case),
+ *  where <em>V</em> is the number of vertices and <em>E</em> is the number of edges.
+ *  Afterwards, the <em>hasOrder</em> and <em>rank</em> operations takes constant time;
+ *  the <em>order</em> operation takes time proportional to <em>V</em>.
+ *  <p>
+ *  See {@link DirectedCycle}, {@link DirectedCycleX}, and
+ *  {@link EdgeWeightedDirectedCycle} to compute a
+ *  directed cycle if the digraph is not a DAG.
+ *  See {@link TopologicalX} for a nonrecursive queue-based algorithm
+ *  to compute a topological order of a DAG.
+ *  <p>
+ *  For additional documentation,
+ *  see <a href="http://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class Topological {
+    private Iterable<Integer> order;  // topological order
+    private int[] rank;               // rank[v] = position of vertex v in topological order
+
+    /**
+     * Determines whether the digraph {@code G} has a topological order and, if so,
+     * finds such a topological order.
+     * @param G the digraph
+     */
+    public Topological(Digraph G) {
+        DirectedCycle finder = new DirectedCycle(G);
+        if (!finder.hasCycle()) {
+            DepthFirstOrder dfs = new DepthFirstOrder(G);
+            order = dfs.reversePost();
+            rank = new int[G.V()];
+            int i = 0;
+            for (int v : order)
+                rank[v] = i++;
+        }
+    }
+
+    /**
+     * Determines whether the edge-weighted digraph {@code G} has a topological
+     * order and, if so, finds such an order.
+     * @param G the edge-weighted digraph
+     */
+    public Topological(EdgeWeightedDigraph G) {
+        EdgeWeightedDirectedCycle finder = new EdgeWeightedDirectedCycle(G);
+        if (!finder.hasCycle()) {
+            DepthFirstOrder dfs = new DepthFirstOrder(G);
+            order = dfs.reversePost();
+        }
+    }
+
+    /**
+     * Returns a topological order if the digraph has a topologial order,
+     * and {@code null} otherwise.
+     * @return a topological order of the vertices (as an interable) if the
+     *    digraph has a topological order (or equivalently, if the digraph is a DAG),
+     *    and {@code null} otherwise
+     */
+    public Iterable<Integer> order() {
+        return order;
+    }
+
+    /**
+     * Does the digraph have a topological order?
+     * @return {@code true} if the digraph has a topological order (or equivalently,
+     *    if the digraph is a DAG), and {@code false} otherwise
+     */
+    public boolean hasOrder() {
+        return order != null;
+    }
+
+    /**
+     * Does the digraph have a topological order?
+     * @return {@code true} if the digraph has a topological order (or equivalently,
+     *    if the digraph is a DAG), and {@code false} otherwise
+     * @deprecated Replaced by {@link #hasOrder()}.
+     */
+    @Deprecated
+    public boolean isDAG() {
+        return hasOrder();
+    }
+
+    /**
+     * The the rank of vertex {@code v} in the topological order;
+     * -1 if the digraph is not a DAG
+     *
+     * @param v the vertex
+     * @return the position of vertex {@code v} in a topological order
+     *    of the digraph; -1 if the digraph is not a DAG
+     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+     */
+    public int rank(int v) {
+        validateVertex(v);
+        if (hasOrder()) return rank[v];
+        else            return -1;
+    }
+
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+    private void validateVertex(int v) {
+        int V = rank.length;
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    /**
+     * Unit tests the {@code Topological} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        String filename  = args[0];
+        String delimiter = args[1];
+        SymbolDigraph sg = new SymbolDigraph(filename, delimiter);
+        Topological topological = new Topological(sg.digraph());
+        for (int v : topological.order()) {
+            StdOut.println(sg.nameOf(v));
+        }
+    }
+
+}
