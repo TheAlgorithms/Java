262dbf9b8529ad28b80c943b55775e950f1d75dd
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2017-10-15 15:47:03 +0530

    Update DecimalToOctal.java

--- Conversions/DecimalToOctal.java
+++ Conversions/DecimalToOctal.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 
 /**
- * This class converts Decimal numbers to Octal Numbers
+ * This class converts a Decimal number to a Binary number
  * 
- * @author Unknown
+ * @author Spandamn (https://github.com/Spandamn)
  *
  */
 class Decimal_Octal
@@ -13,21 +13,21 @@ class Decimal_Octal
 	 * 
 	 * @param args Command line Arguments
 	 */
-  public static void main(String[] args)
-  {
-    Scanner sc=new Scanner(System.in);
-    int n,k,d,s=0,c=0;
-    System.out.print("Decimal number: ");
-    n=sc.nextInt();
-    k=n;
-    while(k!=0)
-    {
-      d=k%8;
-      s+=d*(int)Math.pow(10,c++);
-      k/=8;
-    }
-   
-    System.out.println("Octal equivalent:"+s);
-    sc.close();
-  }
+  	public static void main (String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n, k;
+		String s = "";
+		System.out.print("Decimal number: ");
+		n = sc.nextInt();
+		k = n;
+		while(k != 0)
+		{
+			s = (k % 8) + s;
+			k /= 8;
+		} // converting decimal to octal
+		int oct = Integer.parseInt(s); // Converts the String to an Integer
+		System.out.println("Octal equivalent: " + oct);
+		sc.close();
+	}
 }
