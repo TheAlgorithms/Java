87b1f77e565cc18411c5017927c4a0d220afab9e
Varun Upadhyay <varun.u28@gmail.com>
2017-10-24 10:37:46 -0700

    Update and rename ft.java to FloydTriangle.java

--- Others/FloydTriangle.java
+++ Others/FloydTriangle.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-class FloydTriangle {
+public class FloydTriangle {
     public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
           System.out.println("Enter the number of rows which you want in your Floyd Triangle: ");
