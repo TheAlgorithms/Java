bab15231fb57fb9f57783360b57cd9c51cce2ba8
Nathan Fei <nathanfei123@gmail.com>
2017-10-26 16:56:47 -0400

    Update OctalToHexaDecimal.java

--- Conversions/OctalToHexaDecimal.java
+++ Conversions/OctalToHexaDecimal.java
@@ -14,29 +14,15 @@ public class OctalToHexaDecimal {
 	 * @param args Command line arguments
 	 */
 	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int octal = sc.nextInt();
-		System.out.println("Binary equivalent: " + octToHex(octal));
-		sc.close();
-	}
-	
-	/**
-	 * This method converts an octal number
-	 * to a hexadecimal number.
-	 * 
-	 * @param octal The octal number
-	 * @return The hexadecimal number
-	 */
-	public static int octToHex(int octal) {
 		Scanner scan;
-	  System.out.println("Octal to HexaDecimal");
+		System.out.println("Octal to HexaDecimal");
 		scan = new Scanner(System.in);
 		// Entering the needed number
 		System.out.println("\nEnter the number : ");
 		int num = Integer.parseInt(scan.nextLine(), 8);
-    scan.close();
-    // Converting the number to binary
-    String out = toHexString(num);
-    System.out.println("Hexadecimal equivalent : " + out);
+		scan.close();
+		// Converting the number to hexadecimal
+		String out = toHexString(num);
+		System.out.println("Hexadecimal equivalent : " + out);
 	}
 }
