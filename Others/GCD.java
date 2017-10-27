205698d90fd254d8f2ab3c33706dd41dc2288fd8
teerapat1739 <teerapat12345678@gmail.com>
2017-10-28 03:39:09 +0700

    Update GCD.java

--- Others/GCD.java
+++ Others/GCD.java
@@ -1,20 +1,22 @@
 //Oskar Enmalm 3/10/17
 //This is Euclid's algorithm which is used to find the greatest common denominator
+//Overide function name gcd
 
 public class GCD{
 
-        public static int gcd(int a, int b) {
-
-        int r = a % b;
-        while (r != 0) {
-            b = r;
-            r = b % r;
+      public static int gcd(int num1, int num2) {
+	
+        int gcdValue = num1 % num2;
+        while (gcdValue != 0) {
+            num2 = gcdValue;
+            gcdValue = num2 % gcdValue;
         }
-        return b;
+        return num2;
     }
 	public static int gcd(int[] number) {
 		  int result = number[0];
 		  for(int i = 1; i < number.length; i++) 
+			  //call gcd function (input two value)
 		          result = gcd(result, number[i]);
 		  
 		  return result;
@@ -22,6 +24,7 @@ public class GCD{
 
 	public static void main(String[] args) {
 		int[] myIntArray = {4,16,32};
+		//call gcd function (input array)
                 System.out.println(gcd(myIntArray));
     }
 }
