1d1583c24d9e2791c20b68f058c2ef28a92c8e45
Miguel Ángel Campos <miammiel@hotmail.com>
2017-10-31 19:01:06 -0600

    Update Palindrome.java

--- Others/Palindrome.java
+++ Others/Palindrome.java
@@ -1,16 +1,21 @@
-class Palindrome {
-	
-	public String reverseString(String x){ //*helper method
-		String output = "";
-		for(int i=x.length()-1; i>=0; i--){
-			output += x.charAt(i); //addition of chars create String
-		}
-		return output;
-	}
-	
-	
-	public Boolean isPalindrome(String x){ //*palindrome method, returns true if palindrome
-		return (x.equalsIgnoreCase(reverseString(x)));
-	}
-  
-  }
+/**
+Class implementing the logic for checking if a string x given is Palindrome or not
+A Palindrome String is that if read backwards is the same as if read right to left.
+ */
+class Palindrome { 
+
+	public Boolean isPalindrome(String num) {
+        char d[] = num.toCharArray();
+        int i = 0;
+        int j = d.length - 1;
+        while (i < j) {
+            if (d[i] != d[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+}
