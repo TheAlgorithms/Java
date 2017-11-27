a7928db72b43d8ca331a069ba7342f90826084bf
unknown <cnddhks846@naver.com>
2017-11-27 17:25:14 +0900

    Refactoring OctalToDecimal.java

--- Conversions/OctalToDecimal.java
+++ Conversions/OctalToDecimal.java
@@ -7,41 +7,39 @@ import java.util.Scanner;
  *
  */
 public class OctalToDecimal {
-	
+
 	/**
 	 * Main method
 	 * 
-	 * @param args Command line arguments
+	 * @param args
+	 *            Command line arguments
 	 */
 	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int o = sc.nextInt();
-		System.out.println("Decimal equivalent: " + convertOctalToDecimal(o));
-		sc.close();
+		convertOctalToDecimal();
 	}
-	
+
 	/**
-	 * This method converts an octal number to
-	 * a decimal number.
+	 * This method converts an octal number to a decimal number.
 	 * 
-	 * @param o The octal number
+	 * @param o
+	 *            The octal number
 	 * @return The decimal number
 	 */
-	public static int convertOctalToDecimal(int o) {
+	public static void convertOctalToDecimal() {
 		System.out.print("Octal Input: ");
-		// Read the input from the console which we are expecting as an octal number:
+		// Read the input from the console which we are expecting as an octal
+		// number:
 		Scanner s = new Scanner(System.in);
 		String inputHex = s.nextLine();
-		try{
+		try {
 			// Actual conversion of Octal to Decimal:
 			Integer outputDecimal = Integer.parseInt(inputHex, 8);
 			System.out.println("Decimal Equivalent : " + outputDecimal);
-		}
-		catch(NumberFormatException ne){
-			// Printing a warning message if the input is not a valid octal number:
+		} catch (NumberFormatException ne) {
+			// Printing a warning message if the input is not a valid octal
+			// number:
 			System.out.println("Invalid Input, Expecting octal number 0-7");
-		}
-		finally{
+		} finally {
 			s.close();
 		}
 	}
