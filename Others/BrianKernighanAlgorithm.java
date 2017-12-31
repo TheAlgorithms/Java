1567b58a46c2a7f7d92af4e8cb0c712d78569a08
Varun Upadhyay <varun.u28@gmail.com>
2017-12-31 06:47:51 -0800

    Update BrianKernighanAlgorithm.java

--- Others/BrianKernighanAlgorithm.java
+++ Others/BrianKernighanAlgorithm.java
@@ -8,14 +8,11 @@ import java.util.Scanner;
  * 
  * algorithm to count the number of set bits in a given number
  * 
- * Subtraction of 1 from a number toggles all the bits (from
- * right to left) till the rightmost set bit(including the
+ * Subtraction of 1 from a number toggles all the bits (from right to left) till the rightmost set bit(including the
  * rightmost set bit). 
- * So if we subtract a number by 1 and do bitwise & with
- * itself i.e. (n & (n-1)), we unset the rightmost set bit. 
+ * So if we subtract a number by 1 and do bitwise & with itself i.e. (n & (n-1)), we unset the rightmost set bit. 
  * 
- * If we do n & (n-1) in a loop and count the no of times loop
- * executes we get the set bit count.
+ * If we do n & (n-1) in a loop and count the no of times loop executes we get the set bit count.
  * 
  * 
  * Time Complexity: O(logn)
