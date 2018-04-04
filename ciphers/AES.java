c3c362fff381c264c9260694851953d527653abd
khalil2535 <khalil2535@localhost>
2018-04-04 07:02:39 +0300

    (Switch) instead of (if)

--- ciphers/AES.java
+++ ciphers/AES.java
@@ -569,26 +569,32 @@ public class AES {
             System.out.println("Enter (e) letter for encrpyt or (d) letter for decrypt :");
             char choice = input.nextLine().charAt(0);
             String in;
-            if (choice == 'E' || choice == 'e') {
+            switch(choice)
+            {
+            case 'E':
+            case 'e':
                 System.out.println("Choose a plaintext block (128-Bit Integer in base 16):");
                 in = input.nextLine();
                 BigInteger plaintext = new BigInteger(in, 16);
                 System.out.println("Choose a Key (128-Bit Integer in base 16):");
                 in = input.nextLine();
-                BigInteger key = new BigInteger(in, 16);
-
-                System.out.println("The encrypted message is: \n" + encrypt(plaintext, key).toString(16));
-            }
-            if (choice == 'D' || choice == 'd') {
-                System.out.println("Enter your ciphertext block (128-Bit Integer in base 16):");
+                BigInteger encryptionKey = new BigInteger(in, 16);
+                System.out.println("The encrypted message is: \n" + encrypt(plaintext, encryptionKey).toString(16));
+                break;
+            case 'D':
+            case 'd':
+            	System.out.println("Enter your ciphertext block (128-Bit Integer in base 16):");
                 in = input.nextLine();
                 BigInteger ciphertext = new BigInteger(in, 16);
                 System.out.println("Choose a Key (128-Bit Integer in base 16):");
                 in = input.nextLine();
-                BigInteger key = new BigInteger(in, 16);
-                System.out.println("The deciphered message is:\n" + decrypt(ciphertext, key).toString(16));
+                BigInteger decryptionKey = new BigInteger(in, 16);
+                System.out.println("The deciphered message is:\n" + decrypt(ciphertext, decryptionKey).toString(16));
+                break;
+                default:
+                	System.out.println("** End **");
             }
         }
-        System.out.println("** End **");
+        
     }
 }
