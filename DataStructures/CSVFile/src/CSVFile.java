8972bf70555682d9fc88f43f4df1a0bb604b4ace
khalil2535 <khalil2535@localhost>
2018-04-05 08:10:25 +0300

    Update CSVFile.java

--- DataStructures/CSVFile/src/CSVFile.java
+++ DataStructures/CSVFile/src/CSVFile.java
@@ -71,7 +71,6 @@ package DataStructures.CSVFile.src;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
