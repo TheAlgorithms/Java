7d178c51b44ac95920e04eba9163cc91265c6b61
nik <nikitap4.92@gmail.com>
2018-04-12 16:37:47 +0300

    Refactored a few searching algorithms Added documentation to these algorithms

--- Searches/src/search/IterativeBinarySearch.java
+++ Searches/src/search/IterativeBinarySearch.java
@@ -1,14 +1,28 @@
+package search;
 
 import java.util.Arrays;
 import java.util.Random;
 
 /**
- * 
+ * Binary search is one of the most popular algorithms
+ * This class represents iterative version  {@link BinarySearch}
+ * Iterative binary search is likely to have lower constant factors because it doesn't involve the overhead of manipulating the call stack.
+ * But in java the recursive version can be optimized by the compiler to this version.
+ *
+ * Worst-case performance	O(log n)
+ * Best-case performance	O(1)
+ * Average performance	O(log n)
+ * Worst-case space complexity	O(1)
+ *
  * @author Gabriele La Greca : https://github.com/thegabriele97
- * 
+ * @author Podshivalov Nikita (https://github.com/nikitap492)
+ *
+ * @see SearchAlgorithm
+ * @see BinarySearch
+ *
  */
 
-public final class IterativeBinarySearch {
+public final class IterativeBinarySearch implements SearchAlgorithm {
 
     /**
      * This method implements an iterative version of binary search algorithm
@@ -18,13 +32,14 @@ public final class IterativeBinarySearch {
      * 
      * @return the index of key in the array or -1 if not found
      */
-    public static <T extends Comparable<T>> int binarySearch(T[] array, T key) {
+    @Override
+    public  <T extends Comparable<T>> int find(T[] array, T key) {
         int l, r, k, cmp;
 
         l = 0;
-        r = array.length - 1;
+        r = array.length;
 
-        while (l <= r) {
+        while (l < r) {
             k = (l + r) / 2;
             cmp = key.compareTo(array[k]);
 
@@ -32,7 +47,7 @@ public final class IterativeBinarySearch {
                 return k;
             } else if (cmp < 0) {
                 r = --k;
-            } else if (cmp > 0) {
+            } else {
                 l = ++k;
             }
         }
@@ -53,7 +68,7 @@ public final class IterativeBinarySearch {
         //Arrays.sort(array); //if needed
         Integer key = base + rand.nextInt(array.length * 2); //can generate keys that aren't in array
 
-        System.out.println(binarySearch(array, key));
+        System.out.println(new IterativeBinarySearch().find(array, key));
         System.out.println(Arrays.binarySearch(array, key));
     }
 }
