cf778675df4aaa2e5a964a2922a8431ccc913edf
nik <nikitap4.92@gmail.com>
2018-04-12 10:14:22 +0300

    Comb sort was implemented

--- Sorts/src/sort/SortAlgorithm.java
+++ Sorts/src/sort/SortAlgorithm.java
@@ -11,8 +11,18 @@ import java.util.List;
  **/
 public interface SortAlgorithm {
 
+    /**
+     * Main method arrays sorting algorithms
+     * @param unsorted - an array should be sorted
+     * @return a sorted array
+     */
     <T extends Comparable<T>> T[] sort(T[] unsorted);
 
+    /**
+     * Auxiliary method for algorithms what wanted to work with lists from JCF
+     * @param unsorted - a list should be sorted
+     * @return a sorted list
+     */
     @SuppressWarnings("unchecked")
     default <T extends Comparable<T>> List<T> sort(List<T> unsorted){
         return Arrays.asList(sort(unsorted.toArray((T[]) new Comparable[unsorted.size()])));
