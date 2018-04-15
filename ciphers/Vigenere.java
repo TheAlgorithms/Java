e53ec5303476c304aa87388880155181bfa44026
Fractum <24612656+straiffix@users.noreply.github.com>
2018-04-15 10:45:31 +0200

    Update Vigenere.java
    - Remove continue;
    - Change condition with Character.isLetter() and Character.isUpperCase();

--- ciphers/Vigenere.java
+++ ciphers/Vigenere.java
@@ -8,49 +8,53 @@ package ciphers;
 
 public class Vigenere  {
 
-    public  static String encrypt(final String message, final String key)
+     public static String encrypt(final String message, final String key)
     {
 
         String result = "";
 
-        for (int i = 0, j = 0; i < message.length(); i++)
-        {
+        for (int i = 0, j = 0; i < message.length(); i++) {
             char c = message.charAt(i);
-            if (c >= 'A' && c <= 'Z') {
-                result += (char) ((c + key.toUpperCase().charAt(j) - 2 * 'A') % 26 + 'A');
-                j = ++j % key.length();
-            } else if (c >= 'a' && c <= 'z') {
-                result += (char) ((c + key.toLowerCase().charAt(j) - 2 * 'a') % 26 + 'a');
-                j = ++j % key.length();
+            if (Character.isLetter(c)){
+                if(Character.isUpperCase(c)) {
+                    result += (char) ((c + key.toUpperCase().charAt(j) - 2 * 'A') % 26 + 'A');
+
+                } else {
+                    result += (char) ((c + key.toLowerCase().charAt(j) - 2 * 'a') % 26 + 'a');
+
+                }
             } else {
                 result+=c;
-                continue;
             }
+            j = ++j % key.length();
         }
         return result;
     }
 
-    public static String decrypt( final String message, final String key)
+     public static String decrypt( final String message, final String key)
     {
         String result ="";
 
         for(int i = 0, j = 0; i < message.length(); i++){
 
             char c = message.charAt(i);
-            if((c >= 'A' && c <= 'Z')){
-                result += ((char)('Z'-(25-(c-key.toUpperCase().charAt(j)))%26));
-            }
-            else if(c >= 'a' && c <= 'z'){
-                result += ((char)('z'-(25-(c-key.toLowerCase().charAt(j)))%26));
-            }
-            else{
+            if (Character.isLetter(c)){
+                if(Character.isUpperCase(c)) {
+                    result += ((char)('Z'-(25-(c-key.toUpperCase().charAt(j)))%26));
+
+                } else {
+                    result += ((char)('z'-(25-(c-key.toLowerCase().charAt(j)))%26));
+
+                }
+            } else {
                 result+=c;
-                continue;
             }
+
             j = ++j % key.length();
+
         }
         return result;
-    }
+     }
     public static void main (String [] args){
         String text="Hello World!";
         String key="itsakey";
