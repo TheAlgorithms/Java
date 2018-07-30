69279375d40bcfc315071ee7b97a926ca105ad43
Keshav <bansal_k@worksap.co.jp>
2018-07-30 11:16:40 +0800

    Add method for minimum number of coins required for given amount

--- Dynamic Programming/CoinChange.java
+++ Dynamic Programming/CoinChange.java
@@ -10,9 +10,11 @@ public class CoinChange {
     public static void main(String[] args) {
 
         int amount = 12;
-        int[] coins = {1, 2, 5};
+        int[] coins = {2, 4, 5};
 
         System.out.println("Number of combinations of getting change for " + amount + " is: " + change(coins, amount));
+        System.out.println("Minimum number of coins required for amount :" + amount + " is: " + minimumCoins(coins, amount));
+
     }
 
     /**
@@ -29,7 +31,7 @@ public class CoinChange {
 
         for (int coin : coins) {
             for (int i=coin; i<amount+1; i++) {
-                    combinations[i] += combinations[i-coin];
+                combinations[i] += combinations[i-coin];
             }
             // Uncomment the below line to see the state of combinations for each coin
             // printAmount(combinations);
@@ -37,14 +39,41 @@ public class CoinChange {
 
         return combinations[amount];
     }
+    /**
+     * This method finds the minimum number of coins needed for a given amount.
+     *
+     * @param coins The list of coins
+     * @param amount The amount for which we need to find the minimum number of coins.
+     * Finds the the minimum number of coins that make a given value.
+     **/
+    public static int minimumCoins(int[] coins, int amount) {
+        //minimumCoins[i] will store the minimum coins needed for amount i
+        int[] minimumCoins = new int[amount+1];
+
+        minimumCoins[0] = 0;
+
+        for(int i=1;i<=amount;i++){
+            minimumCoins[i]=Integer.MAX_VALUE;
+        }
+        for(int i=1;i<=amount;i++){
+            for (int coin :coins){
+                if(coin <=i){
+                    int sub_res = minimumCoins[i-coin];
+                    if (sub_res != Integer.MAX_VALUE && sub_res + 1 < minimumCoins[i])
+                        minimumCoins[i] = sub_res + 1;
+                }
+            }
+        }
+        // Uncomment the below line to see the state of combinations for each coin
+        //printAmount(minimumCoins);
+        return minimumCoins[amount];
+    }
 
     // A basic print method which prints all the contents of the array
     public static void printAmount(int[] arr) {
-
         for (int i=0; i<arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
-
 }
