ec3d6a784759c807344e51d6bae806f89b240088
lq <liuqian920320@163.com>
2018-08-28 22:42:03 +0800

    add Test for InsertionSort

---
+++ src/main/com/java/sorts/InsertionSort.java
@@ -0,0 +1,29 @@
+package src.main.com.java.sorts;
+
+import static src.main.com.java.sorts.SortUtils.less;
+
+public class InsertionSort {
+
+  /**
+   * This method implements the Generic Insertion Sort
+   * Sorts the array in increasing order
+   *
+   * @param array The array to be sorted
+   **/
+  public <T extends Comparable<T>> T[] sort(T[] array) {
+    for (int j = 1; j < array.length; j++) {
+
+      // Picking up the key(Card)
+      T key = array[j];
+      int i = j - 1;
+
+      while (i >= 0 && less(key, array[i])) {
+        array[i + 1] = array[i];
+        i--;
+      }
+      // Placing the key (Card) at its correct position in the sorted subarray
+      array[i + 1] = key;
+    }
+    return array;
+  }
+}
