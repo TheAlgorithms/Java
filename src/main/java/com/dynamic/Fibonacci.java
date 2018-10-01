3155d2794122913f03fc3e82d2b9c5e105662770
Felix <mail@craxy.de>
2018-10-01 16:22:49 +0200

    Made the algorithm dynamic

--- src/main/java/com/dynamic/Fibonacci.java
+++ src/main/java/com/dynamic/Fibonacci.java
@@ -1,10 +1,14 @@
 package src.main.java.com.dynamic;
 
+import java.util.HashMap;
+
 /**
  * The Fibonacci sequence is a sequence of numbers in which each number after the first two is the sum of the two preceding ones.
  * 1, 1, 2, 3, 5, 8, 13, ...
  */
 public class Fibonacci {
+
+    private static HashMap<Integer, Integer> fibonacciCache = new HashMap<>();
 
     /**
      * A recursive method that calculates and returns the nth fibonacci number.
@@ -16,7 +20,14 @@ public class Fibonacci {
             return n;
         }
 
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        if(fibonacciCache.containsKey(n)) {
+            return fibonacciCache.get(n);
+        }
+
+        int calculatedFibonacci = fibonacci(n - 1) + fibonacci(n - 2);
+        fibonacciCache.put(n, calculatedFibonacci);
+
+        return calculatedFibonacci;
     }
 
 }
