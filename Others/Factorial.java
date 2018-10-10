b65416dd305dbe9a6c9ffabe2ded80eda6b8cf2a
D.Sunil <sunilnitdgp5@gmail.com>
2018-10-10 23:42:51 +0530

    Updated Factorial.java for large input
    The code will work if the number is too large.

--- Others/Factorial.java
+++ Others/Factorial.java
@@ -1,49 +1,50 @@
-import java.util.Scanner;
-
-/**
- * This program will print out the factorial of any non-negative
- * number that you input into it.
- * 
- * @author Marcus
- *
- */
-public class Factorial{
-
-	/**
-	 * The main method
-	 * 
-	 * @param args Command line arguments
-	 */
-	public static void main(String[] args){
-	  Scanner input = new Scanner(System.in);
-          System.out.print("Enter a non-negative integer: ");
-          
-       //If user does not enter an Integer, we want program to fail gracefully, letting the user know why it terminated   
-          try{
-            int number = input.nextInt();
-            
-       //We keep prompting the user until they enter a positive number 
-            while(number < 0){
-              System.out.println("Your input must be non-negative. Please enter a positive number: ");
-              number = input.nextInt();
-            }
-       //Display the result
- 	   System.out.println("The factorial of " + number + " will yield: " + factorial(number));  
-           
-          }catch(Exception e){
-            System.out.println("Error: You did not enter an integer. Program has terminated.");
-          }
-          input.close();
-        }
-        
-	/**
-	 * Recursive Factorial Method
-	 * 
-	 * @param n The number to factorial
-	 * @return The factorial of the number
-	 */
-	public static long factorial(int n){
-          if(n == 0 || n == 1) return 1;
-          return n * factorial(n - 1);
-	}
-}
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Sunil { 
+      
+    // This function finds factorial of  
+    // large numbers and prints them 
+    static void factorial(int n) 
+    { 
+        int res[] = new int[500]; 
+  
+        // Initialize result 
+        res[0] = 1; 
+        int res_size = 1; 
+        for (int x = 2; x <= n; x++) 
+            res_size = multiply(x, res, res_size); 
+  
+        System.out.println("Factorial of given number is "); 
+        for (int i = res_size - 1; i >= 0; i--) 
+            System.out.print(res[i]); 
+    } 
+    static int multiply(int x, int res[], int res_size) 
+    { 
+        int carry = 0; // Initialize carry 
+  
+        // One by one multiply n with individual  
+        // digits of res[] 
+        for (int i = 0; i < res_size; i++) 
+        { 
+            int prod = res[i] * x + carry; 
+            res[i] = prod % 10;  
+                               
+            carry = prod/10;
+  
+        // Put carry in res and increase result size 
+        while (carry!=0) 
+        { 
+            res[res_size] = carry % 10; 
+            carry = carry / 10; 
+            res_size++; 
+        } 
+        return res_size; 
+    } 
+  
+    // Driver program 
+    public static void main(String args[]) 
+    { 
+        factorial(100); 
+    } 
+} 
