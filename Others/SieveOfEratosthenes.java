54bc0af24ce647007dad0239f101b3b6fe7cb456
D.Sunil <sunilnitdgp5@gmail.com>
2018-10-10 23:08:51 +0530

    Updated some packages and used a predefined method
    I have just used some packages which were not included previously and used a predefined method i.e., Arrays.fill() which is an alternative for for loop. And marked all the multiples of a number in the array as 0.

--- Others/SieveOfEratosthenes.java
+++ Others/SieveOfEratosthenes.java
@@ -1,8 +1,6 @@
-/**
- *
- * @author Varun Upadhyay (https://github.com/varunu28)
- *
- */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 public class SieveOfEratosthenes {
 
     /**
@@ -14,30 +12,22 @@ public class SieveOfEratosthenes {
 
     public static void findPrimesTillN(int n) {
         int[] arr = new int[n+1];
-
-        for (int i=0;i<=n;i++) {
-            arr[i] = 1;
-        }
-
-        arr[0] = arr[1] = 0;
-
-        for (int i=2;i<=Math.sqrt(n);i++) {
-            if (arr[i] == 1) {
-                for (int j=2;i*j <= n;j++) {
-                    arr[i*j] = 0;
+        Arrays.fill(arr,1);
+        for(int i=2;i*i<=n;i++){
+            if(arr[i]==1){
+                for(int j=2*i;j<=n;j+=i){
+                    arr[j]=0;
                 }
             }
         }
-
-        for (int i=0;i<n+1;i++) {
-            if (arr[i] == 1) {
+        for (int i=2;i<=n;i++) {
+            if (arr[i]==1) {
                 System.out.print(i + " ");
             }
         }
 
         System.out.println();
     }
-
     // Driver Program
     public static void main(String[] args) {
         int n = 100;
