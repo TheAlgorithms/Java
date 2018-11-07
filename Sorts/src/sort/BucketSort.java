419b3434542beba5c78270e9dab541d860ef989b
Sameer <sameer1771999@gmail.com>
2018-11-07 18:49:38 +0530

    Updated bucket sort program

--- Sorts/src/sort/BucketSort.java
+++ Sorts/src/sort/BucketSort.java
@@ -1,41 +1,55 @@
-// This is a java program to sort the numbers using the Bucket Sort Technique. The algorithm allocates the number of memory locations equal to maximum number and initializes all to zero. 
-// Then each location is incremented as the numbers appears. The time complexity of the algorithm is O(n).
-
 import java.util.*;
- 
-public class BucketSort{
- 
-   public static void sort(int[] a, int maxVal) {
-      int [] bucket=new int[maxVal+1];
- 
-      for (int i=0; i<bucket.length; i++) {
-         bucket[i]=0;
-      }
- 
-      for (int i=0; i<a.length; i++) {
-         bucket[a[i]]++;
-      }
- 
-      int outPos=0;
-      for (int i=0; i<bucket.length; i++) {
-         for (int j=0; j<bucket[i]; j++) {
-            a[outPos++]=i;
-         }
-      }
-   }
- 
- 
-   public static void main(String[] args) {
-      int maxVal=5;
-      int [] data= {5,3,0,2,4,1,0,5,2,3,1,4}; 
- 
-      System.out.println("Before: " + Arrays.toString(data));
-      sort(data,maxVal);
-      System.out.println("After:  " + Arrays.toString(data));
-   }
+
+class BucketSort {
+	public static int[] bucketSort(int[] array, int bucketCount) {
+
+	if (bucketCount <= 0) throw new IllegalArgumentException("Invalid bucket count");
+		if (array.length <= 1) return array; //trivially sorted
+	int high = array[0];
+		int low = array[0];
+		for (int i = 1; i < array.length; i++) { //find the range of input elements
+	if (array[i] > high) high = array[i];
+	if (array[i] < low) low = array[i];
+	}
+	double interval = ((double)(high - low + 1))/bucketCount; //range of one bucket
+	ArrayList<Integer> buckets[] = new ArrayList[bucketCount];
+	for (int i = 0; i < bucketCount; i++) { //initialize buckets
+	buckets[i] = new ArrayList();
+	}
+	for(int i = 0; i < array.length; i++) 
+	{ //partition the input array
+		buckets[(int)((array[i] - low)/interval)].add(array[i]);
+	}	
+		int pointer = 0;
+		for (int i = 0; i < buckets.length; i++) 
+		{
+		Collections.sort(buckets[i]); //mergeSort
+		for (int j = 0; j < buckets[i].size(); j++) 
+		{ //merge the buckets
+	    array[pointer] = buckets[i].get(j);
+		pointer++;
+		}
+	
+	   }
+	return array;
+	
+	}
+
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		int[] a=new int[t];
+		for(int i=0;i<t;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		bucketSort(a,t);
+		for(int i=0;i<t;i++)
+		{
+			System.out.println(a[i]);
+		}
+
+	}
 }
-
-
-// Output:
-// Before: [5, 3, 0, 2, 4, 1, 0, 5, 2, 3, 1, 4]
-// After:  [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
