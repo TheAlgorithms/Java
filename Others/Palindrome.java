2d383bf48e7f058ec6d20b668e7a89edb3d51d43
Libin Yang <szuyanglb@outlook.com>
2018-12-24 10:01:45 +0800

    Update Palindrome.java

--- Others/Palindrome.java
+++ Others/Palindrome.java
@@ -1,28 +1,23 @@
 class Palindrome {
-	
-	private String reverseString(String x){ //*helper method	
-		String output = "";
-		for(int i=x.length()-1; i>=0; i--){
-			output += x.charAt(i); //addition of chars create String
-		}
-		return output;
-	}
-	
-	
-	public Boolean FirstWay(String x){ //*palindrome method, returns true if palindrome
-		if(x == null || x.length() <= 1) 
-            		return true;
-        	return (x.equalsIgnoreCase(reverseString(x)));
-	}
-  	
-  	public boolean SecondWay(String x)
-  	{
-  		if (x.length() == 0 || x.length() == 1)
-  			return true;
 
-  		if (x.charAt(0) != x.charAt(x.length() - 1))
-  			return false;
+    private String reverseString(String x) { // *helper method
+        StringBuilder output = new StringBuilder(x);
+        return output.reverse().toString();
+    }
 
-  		return SecondWay(x.substring(1 , x.length() - 1));
-  	}
-  }
+    public boolean FirstWay(String x) { // *palindrome method, returns true if palindrome
+        if (x == null || x.length() <= 1)
+            return true;
+        return x.equalsIgnoreCase(reverseString(x));
+    }
+
+    public boolean SecondWay(String x) {
+        if (x.length() == 0 || x.length() == 1)
+            return true;
+
+        if (x.charAt(0) != x.charAt(x.length() - 1))
+            return false;
+
+        return SecondWay(x.substring(1, x.length() - 1));
+    }
+}
