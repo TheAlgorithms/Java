6312a1ee872d04c0d0a68e4dc1ee85b2743706fd
Adan Rodriguez <adanrodriguezthis33@gmail.com>
2019-01-29 19:09:17 -0300

    binary number in 64 bits

--- src/main/java/com/conversions/BinaryToHexadecimal.java
+++ src/main/java/com/conversions/BinaryToHexadecimal.java
@@ -1,5 +1,6 @@
 package src.main.java.com.conversions;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 
 public class BinaryToHexadecimal {
@@ -12,7 +13,7 @@ public class BinaryToHexadecimal {
      * @return The hexadecimal number
      */
 
-    public String binToHex(long binary) {
+    public String binToHex(BigInteger binary) {
         //hm to store hexadecimal codes for binary numbers within the range: 0000 to 1111 i.e. for decimal numbers 0 to 15
         HashMap<Integer,String> hmHexadecimal = new HashMap<>();
 
@@ -25,14 +26,15 @@ public class BinaryToHexadecimal {
         for(i=10 ; i<16 ; i++)
             hmHexadecimal.put(i,String.valueOf((char)('A'+i-10)));
 
-        long currentbit;
-        while(binary != 0) {
+        int currentbit;
+        BigInteger tenValue = new BigInteger("10");
+        while(binary.compareTo(BigInteger.ZERO) != 0) {
             int code4 = 0;	//to store decimal equivalent of number formed by 4 decimal digits
             for(i=0 ; i<4 ; i++)
             {
-                currentbit = binary % 10;
-                binary = binary / 10;
-                code4 += currentbit * Math.pow(2, i);
+                currentbit = binary.mod(tenValue).intValueExact();
+                binary = binary.divide(tenValue);
+                code4 += currentbit*Math.pow(2, i);
             }
             hex= hmHexadecimal.get(code4) + hex;
         }
