df085d0e1dee6952f45512511a4e3a97a2368f29
Adan Rodriguez <adanrodriguezthis33@gmail.com>
2019-02-03 17:39:11 -0300

    convert decimal to octal value

---
+++ src/test/java/com/conversions/DecimalToOctalTest.java
@@ -0,0 +1,16 @@
+package src.test.java.com.conversions;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DecimalToOctalTest {
+    @Test
+    public void testDecimalToOctalTest() {
+        DecimalToOctal decimalToOctal = new DecimalToOctal();
+        Assert.assertEquals("Incorrect Conversion", "41", decimalToOctal.decimalToOctal("33"));
+        Assert.assertEquals("Incorrect Conversion", "5512", decimalToOctal.decimalToOctal("2890"));
+        Assert.assertEquals("Incorrect Conversion", "12525252525252525252525241", decimalToOctal.decimalToOctal("50371909150609548946081"));
+        Assert.assertEquals("Incorrect Conversion", "13", decimalToOctal.decimalToOctal("11"));
+        Assert.assertEquals("Incorrect Conversion", "46703754", decimalToOctal.decimalToOctal("10192876"));
+    }
+}
