3b72294f25e21c9574605aa6b376f1e4bfc23f77
nbdgit <nilanjan.das81@gmail.com>
2019-04-14 16:56:52 +0530

    Commit BinaryToGrayTest

---
+++ src/test/java/com/conversions/BinaryToGrayTest.java
@@ -0,0 +1,19 @@
+package src.test.java.com.conversions;
+
+import src.main.java.com.conversions.BinaryToGray;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BinaryToGrayTest
+{
+	
+	
+	@Test
+	public void testBinaryToGray()
+	{
+		BinaryToGray btog=new BinaryToGray();
+		assertEquals("1101",btog.binaryToGray("1001"));
+		assertEquals("11010011101",btog.binaryToGray("10011101001"));
+	}
+	
+}
