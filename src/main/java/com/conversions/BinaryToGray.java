230f04f6141565f242c6832ff752d93134a0ac52
Havan Agrawal <havanagrawal@gmail.com>
2019-05-03 00:31:43 +0530

    Apply suggestions from code review
    Co-Authored-By: nbdgit <48550673+nbdgit@users.noreply.github.com>

--- src/main/java/com/conversions/BinaryToGray.java
+++ src/main/java/com/conversions/BinaryToGray.java
@@ -3,28 +3,26 @@ package src.main.java.com.conversions;
 public class BinaryToGray
 {
 	/*	This method convert the binary number into gray code
-		@param binaryno need to convert binary number into gray code
+		@param binarycode need to convert binary number into gray code
 		@return graycode return as string
 	*/
 
 	public String binaryToGray(String binarycode)
 	{
 				
-		String graycode=Character.toString(binarycode.charAt(0));
-	
+		String graycode = Character.toString(binarycode.charAt(0));
 		
-		for(int i=0;i<binarycode.length()-1; i++)
+		for(int i = 0; i < binarycode.length() - 1; i++)
 		{
 			
-			if(binarycode.charAt(i)==binarycode.charAt(i+1))
-				graycode=graycode+"0";
+			if (binarycode.charAt(i) == binarycode.charAt(i+1))
+				graycode = graycode + "0";
 			else
-				graycode=graycode+"1";
+				graycode = graycode + "1";
 			
 		}
 	
 		return graycode;
-		
 	}
 
 }
--- src/test/java/com/conversions/BinaryToGrayTest.java
+++ src/test/java/com/conversions/BinaryToGrayTest.java
@@ -7,12 +7,11 @@ import static org.junit.Assert.assertEquals;
 public class BinaryToGrayTest
 {
 	
-	
 	@Test
 	public void testBinaryToGray()
 	{
-		BinaryToGray btog=new BinaryToGray();
-		assertEquals("1101",btog.binaryToGray("1001"));
+		BinaryToGray btog = new BinaryToGray();
+		assertEquals("1101", btog.binaryToGray("1001"));
 		assertEquals("11010011101",btog.binaryToGray("10011101001"));
 	}
 	
