c526f53ffd5bdaf1a213eb3ce7cb791fc5e08a7b
Abir Bhushan <abirchandrabhushan@gmail.com>
2019-05-22 11:26:03 +0530

    Update DepthFirstSearchTest.java
    Imported BinaryTree class and removed deprecated assertEquals()

--- src/test/java/com/search/DepthFirstSearchTest.java
+++ src/test/java/com/search/DepthFirstSearchTest.java
@@ -2,34 +2,37 @@ package src.test.java.com.search;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 import src.main.java.com.search.DepthFirstSearch;
+import src.main.java.com.search.BinaryTree;
 
 public class DepthFirstSearchTest {
-    @Test
-    public void testDepthFirstSearch() {
 
-        BinaryTree<Integer> tree1 = new BinaryTree<>();
+	@Test
+	public void test() {
+		BinaryTree<Integer> tree1 = new BinaryTree<>();
         tree1.add(1,1);
         tree1.add(2,2);
         tree1.add(3,3);
         tree1.add(4,4);
-        Assert.assertEquals("Incorrect index", 3,    DepthFirstSearch.find(3, tree1));
-        Assert.assertEquals("Incorrect index", 1,    DepthFirstSearch.find(1, tree1));
-        Assert.assertEquals("Incorrect index", null, DepthFirstSearch.find(0, tree1));
-        Assert.assertEquals("Incorrect index", null, DepthFirstSearch.find(8, tree1));
-        Assert.assertEquals("Incorrect index", null, DepthFirstSearch.find(-2, tree1));
-
+        Assert.assertEquals((Integer) 3, DepthFirstSearch.find(3, tree1));
+        Assert.assertEquals((Integer) 1, DepthFirstSearch.find(1, tree1));
+        Assert.assertEquals(null       , DepthFirstSearch.find(0, tree1));
+        Assert.assertEquals(null       , DepthFirstSearch.find(8, tree1));
+        Assert.assertEquals(null       , DepthFirstSearch.find(-2, tree1));
+        
         BinaryTree<String> tree2 = new BinaryTree<>();
         tree2.add("1","A");
         tree2.add("2","B");
         tree2.add("3","C");
         tree2.add("4","D");
         
-        Assert.assertEquals("Incorrect index", "C",  DepthFirstSearch.find(tree2,"3"));
-        Assert.assertEquals("Incorrect index", "B",  DepthFirstSearch.find(tree2,"2"));
-        Assert.assertEquals("Incorrect index", null, DepthFirstSearch.find(tree2,"F"));
+        Assert.assertEquals("C" , DepthFirstSearch.find("3", tree2));
+        Assert.assertEquals("B" , DepthFirstSearch.find("2", tree2));
+        Assert.assertEquals(null, DepthFirstSearch.find("F", tree2));
 
         BinaryTree<String> tree3 = new BinaryTree<>();
-        Assert.assertEquals("Incorrect index", null, DepthFirstSearch.find(tree3, ""));
-    }
+        Assert.assertEquals(null, DepthFirstSearch.find("", tree3));
+        
+	}
 }
