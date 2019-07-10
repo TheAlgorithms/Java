dc5ae2d03bdf974100ab0551045c4526ac6da7b5
Priyansh-Kedia <52661249+Priyansh-Kedia@users.noreply.github.com>
2019-07-10 10:17:08 +0530

    Update BinaryToDecimal.java
    Changed bin_num and bin_copy to binNum and binCopy respectively.

--- Conversions/BinaryToDecimal.java
+++ Conversions/BinaryToDecimal.java
@@ -15,14 +15,14 @@ class BinaryToDecimal {
      */
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int bin_num, bin_copy, d, s = 0, power = 0;
+        int binNum, binCopy, d, s = 0, power = 0;
         System.out.print("Binary number: ");
-        bin_num = sc.nextInt();
-        bin_copy = bin_num;
-        while (bin_copy != 0) {
-            d = bin_copy % 10;
+        binNum = sc.nextInt();
+        binCopy = binNum;
+        while (binCopy != 0) {
+            d = binCopy % 10;
             s += d * (int) Math.pow(2, power++);
-            bin_copy /= 10;
+            binCopy /= 10;
         }
         System.out.println("Decimal equivalent:" + s);
         sc.close();
