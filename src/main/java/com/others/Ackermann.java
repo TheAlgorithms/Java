3e1fbad6938695def860c4c19ad39bde6e517f75
5n0w_wh1t3 <26778292+5n0wwh1t3@users.noreply.github.com>
2019-07-17 13:27:09 -0700

    Ackermann updated

--- src/main/java/com/others/Ackermann.java
+++ src/main/java/com/others/Ackermann.java
@@ -11,7 +11,7 @@ public class Ackermann {
    * Defined only for NONNEGATIVE integers !!!
    * 
    * Time complexity is super-exponential. O(n(^))
-   * 
+   * Any input m higher tahn (3,3) will result in StackOverflow
    * @param m
    * @param n
    * @return
@@ -20,14 +20,15 @@ public class Ackermann {
    */
   public long Ack(long m, long n) {
 
-    if (m == 0) return n + 1;
-    
-    if (n == 0) return Ack(m - 1, 1);
-    
+    if (m == 0)
+      return n + 1;
+
+    if (n == 0)
+      return Ack(m - 1, 1);
+
     return Ack(m - 1, Ack(m, n - 1));
-    }
-  
   }
 
+}
 
 
