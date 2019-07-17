dc3778cfbc9ad9fe4af878ffd9b3a5611e4e03e1
5n0w_wh1t3 <26778292+5n0wwh1t3@users.noreply.github.com>
2019-07-17 13:23:56 -0700

    Ackermann added.

---
+++ src/main/java/com/others/Ackermann.java
@@ -0,0 +1,33 @@
+package src.main.java.com.others;
+
+
+public class Ackermann {
+
+
+  /**
+   * Ackermann function - simplest and earliest-discovered examples of a total computable function
+   * that is not primitive recursive.
+   * 
+   * Defined only for NONNEGATIVE integers !!!
+   * 
+   * Time complexity is super-exponential. O(n(^))
+   * 
+   * @param m
+   * @param n
+   * @return
+   *
+   *
+   */
+  public long Ack(long m, long n) {
+
+    if (m == 0) return n + 1;
+    
+    if (n == 0) return Ack(m - 1, 1);
+    
+    return Ack(m - 1, Ack(m, n - 1));
+    }
+  
+  }
+
+
+
