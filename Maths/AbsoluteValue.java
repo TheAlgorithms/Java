b882515eb005c0856dd5d665e2fc3cd164b9f37b
Yang Libin <contact@yanglibin.info>
2019-08-15 09:12:33 +0800

    Update AbsoluteValue.java

--- Maths/AbsoluteValue.java
+++ Maths/AbsoluteValue.java
@@ -4,24 +4,21 @@ package Maths;
  * @author PatOnTheBack
  */
 
- public class AbsoluteValue {
+public class AbsoluteValue {
 
     public static void main(String[] args) {
-
         int value = -34;
-
-        System.out.println("The absolute value of " + value + " is " + abs_val(value));
-
+        System.out.println("The absolute value of " + value + " is " + absVal(value));
     }
 
-    public static int abs_val(int value) {
-      // If value is less than zero, make value positive.
-      if (value < 0) {
-        return -value;
-      }
-
-      return value;
-
+    /**
+     * If value is less than zero, make value positive.
+     *
+     * @param value a number
+     * @return the absolute value of a number
+     */
+    public static int absVal(int value) {
+        return value > 0 ? value : -value;
     }
 
- }
+}
