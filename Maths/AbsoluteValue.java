d5adb530bfcb68815cc049fac69828edc663a0d4
Human_Bot <54164361+Balls-and-Dick-69@users.noreply.github.com>
2019-08-15 23:19:00 -0500

    What is this useless
    Made useful

--- Maths/AbsoluteValue.java
+++ Maths/AbsoluteValue.java
@@ -1,14 +1,18 @@
 package Maths;
+import java.util.Scanner;
 
 /**
  * @author PatOnTheBack
  */
 
-public class AbsoluteValue {
+public class Algorithms {
 
     public static void main(String[] args) {
-        int value = -34;
-        System.out.println("The absolute value of " + value + " is " + absVal(value));
+        //int value = -34; looks like it's useless
+        Scanner f = new Scanner(System.in);
+        System.out.println("Enter some value:  ");
+        double value = f.nextDouble(); // greater capacity w 64 bit IEEE and it wil still work fine
+        System.out.println("The absolute value of " + value + " is " + absVal(value)); // is this really an algorithm
     }
 
     /**
