c65470d86f65a525acf37636d58df6a029a1a28f
misaka-mikoto-39 <52187071+misaka-mikoto-39@users.noreply.github.com>
2019-09-19 22:23:33 +0900

    Add test

--- DynamicProgramming/CoinChangeTest.java
+++ DynamicProgramming/CoinChangeTest.java
@@ -10,67 +10,67 @@ class CoinChangeTest {
 	@Test
 	void testMinimumCoins() {
         int[] coins = {0};
-        Assert.assertEquals("invalid input!", main.minimumCoins(coins, -1));
-        Assert.assertEquals("invalid input!", main.minimumCoins(coins, 0));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, 1));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, 100));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE - 1));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE + 1));
+        Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, -1));
+        Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, 0));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, 100));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE - 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE + 1));
 		
 		coins[0] = 1;
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, -1));
-		Assert.assertEquals(0, main.minimumCoins(coins, 0));
-		Assert.assertEquals(1, main.minimumCoins(coins, 1));
-		Assert.assertEquals(100, main.minimumCoins(coins, 100));
-		Assert.assertEquals(Integer.MAX_VALUE - 1, main.minimumCoins(coins, Integer.MAX_VALUE - 1));
-		Assert.assertEquals(Integer.MAX_VALUE, main.minimumCoins(coins, Integer.MAX_VALUE));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE + 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, -1));
+		Assert.assertEquals(0, CoinChange.minimumCoins(coins, 0));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, 1));
+		Assert.assertEquals(100, CoinChange.minimumCoins(coins, 100));
+		Assert.assertEquals(Integer.MAX_VALUE - 1, CoinChange.minimumCoins(coins, Integer.MAX_VALUE - 1));
+		Assert.assertEquals(Integer.MAX_VALUE, CoinChange.minimumCoins(coins, Integer.MAX_VALUE));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE + 1));
 		
 		coins[0] = 2;
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, -1));
-		Assert.assertEquals(0, main.minimumCoins(coins, 0));
-		Assert.assertEquals(1, main.minimumCoins(coins, 1));
-		Assert.assertEquals(50, main.minimumCoins(coins, 100));
-		Assert.assertEquals((Integer.MAX_VALUE - 1)/2, main.minimumCoins(coins, Integer.MAX_VALUE - 1));
-		Assert.assertEquals((Integer.MAX_VALUE)/2, main.minimumCoins(coins, Integer.MAX_VALUE));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE + 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, -1));
+		Assert.assertEquals(0, CoinChange.minimumCoins(coins, 0));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, 1));
+		Assert.assertEquals(50, CoinChange.minimumCoins(coins, 100));
+		Assert.assertEquals((Integer.MAX_VALUE - 1)/2, CoinChange.minimumCoins(coins, Integer.MAX_VALUE - 1));
+		Assert.assertEquals((Integer.MAX_VALUE)/2, CoinChange.minimumCoins(coins, Integer.MAX_VALUE));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE + 1));
 		
 		coins[0] = 100;
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, -1));
-		Assert.assertEquals(0, main.minimumCoins(coins, 0));
-		Assert.assertEquals(1, main.minimumCoins(coins, 1));
-		Assert.assertEquals(1, main.minimumCoins(coins, 100));
-		Assert.assertEquals(21474837, main.minimumCoins(coins, Integer.MAX_VALUE - 1));
-		Assert.assertEquals(21474837, main.minimumCoins(coins, Integer.MAX_VALUE));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE + 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, -1));
+		Assert.assertEquals(0, CoinChange.minimumCoins(coins, 0));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, 1));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, 100));
+		Assert.assertEquals(21474837, CoinChange.minimumCoins(coins, Integer.MAX_VALUE - 1));
+		Assert.assertEquals(21474837, CoinChange.minimumCoins(coins, Integer.MAX_VALUE));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE + 1));
 		
 		coins[0] = Integer.MAX_VALUE - 1;
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, -1));
-		Assert.assertEquals(0, main.minimumCoins(coins, 0));
-		Assert.assertEquals(1, main.minimumCoins(coins, 1));
-		Assert.assertEquals(1, main.minimumCoins(coins, 100));
-		Assert.assertEquals(1, main.minimumCoins(coins, Integer.MAX_VALUE - 1));
-		Assert.assertEquals(2, main.minimumCoins(coins, Integer.MAX_VALUE));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE + 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, -1));
+		Assert.assertEquals(0, CoinChange.minimumCoins(coins, 0));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, 1));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, 100));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, Integer.MAX_VALUE - 1));
+		Assert.assertEquals(2, CoinChange.minimumCoins(coins, Integer.MAX_VALUE));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE + 1));
 		
 		coins[0] = Integer.MAX_VALUE;
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, -1));
-		Assert.assertEquals(0, main.minimumCoins(coins, 0));
-		Assert.assertEquals(1, main.minimumCoins(coins, 1));
-		Assert.assertEquals(1, main.minimumCoins(coins, 100));
-		Assert.assertEquals(1, main.minimumCoins(coins, Integer.MAX_VALUE - 1));
-		Assert.assertEquals(1, main.minimumCoins(coins, Integer.MAX_VALUE));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE + 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, -1));
+		Assert.assertEquals(0, CoinChange.minimumCoins(coins, 0));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, 1));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, 100));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, Integer.MAX_VALUE - 1));
+		Assert.assertEquals(1, CoinChange.minimumCoins(coins, Integer.MAX_VALUE));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE + 1));
 		
 		coins[0] = Integer.MAX_VALUE + 1;
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, -1));
-        Assert.assertEquals("invalid input!", main.minimumCoins(coins, 0));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, 1));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, 100));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE - 1));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE));
-		Assert.assertEquals("invalid input!", main.minimumCoins(coins, Integer.MAX_VALUE + 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, -1));
+        Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, 0));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, 100));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE - 1));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE));
+		Assert.assertEquals("invalid input!", CoinChange.minimumCoins(coins, Integer.MAX_VALUE + 1));
 		
 	}
 
