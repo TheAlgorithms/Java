000f7381b7fc6ea1f48b5132b85693a60cb521cd
Duc Dung <vudung042@gmail.com>
2019-09-27 16:05:47 +0700

    Update testGCD.java

--- Others/testGCD.java
+++ Others/testGCD.java
@@ -7,69 +7,45 @@ public class TestGCD {
 
     @Test
     public void testGCDWithBoundaryValue() {
-        int x = 6464500;
-        int y = 1546340;
+        int x = 0;
+        int y = 150;
         int result = GCD.gcd(x, y);
-        Assert.assertEquals(20, result);
+        Assert.assertEquals(150, result);
 
-        x = 35;
-        y = 41;
+        x = 15;
+        y = 0;
         result = GCD.gcd(x,y);
-        Assert.assertEquals(1, result);
-
-        //Test with Min Value
-        x = Integer.MIN_VALUE;
-        y = Integer.MIN_VALUE + 1;
-        result = GCD.gcd(x,y);
-        Assert.assertEquals(1, result);
-
-        x = Integer.MIN_VALUE + 1;
-        y = 500;
-        result = GCD.gcd(x,y);
-        Assert.assertEquals(1, result);
-
-        x = 198;
-        y = Integer.MIN_VALUE;
-        result = GCD.gcd(x,y);
-        Assert.assertEquals(2, result);
-
-        //Test with Max
-        x = Integer.MAX_VALUE;
-        y = Integer.MAX_VALUE - 1;
-        result = GCD.gcd(x,y);
-        Assert.assertEquals(1, result);
-
-        x = Integer.MAX_VALUE;
-        y = 1973;
-        result = GCD.gcd(x,y);
-        Assert.assertEquals(1, result);
-
-        x = 402;
-        y = Integer.MAX_VALUE - 1;
-        result = GCD.gcd(x,y);
-        Assert.assertEquals(6, result);
-
-        //Test with special value
-
-        x = Integer.MAX_VALUE;
-        y = Integer.MIN_VALUE;
-        result = GCD.gcd(x,y);
-        Assert.assertEquals(1, result);
-
-        x = 1;
-        y = Integer.MIN_VALUE;
-        result = GCD.gcd(x,y);
-        Assert.assertEquals(1,result);
+        Assert.assertEquals(15, result);
 
         x = 0;
-        y = Integer.MAX_VALUE;
+        y = 0;
         result = GCD.gcd(x,y);
-        Assert.assertEquals(2147483647,result);
+        Assert.assertEquals(0, result);
 
-        x = 1;
-        y = Integer.MAX_VALUE;
+        x = 50;
+        y = 60;
         result = GCD.gcd(x,y);
-        Assert.assertEquals(1,result);
+        Assert.assertEquals(1, result);
+
+        x = 40;
+        y = 0;
+        result = GCD.gcd(x,y);
+        Assert.assertEquals(40, result);
+
+        x = 50;
+        y = 40;
+        result = GCD.gcd(x,y);
+        Assert.assertEquals(1, result);
+
+        x = 30;
+        y = 0;
+        result = GCD.gcd(x,y);
+        Assert.assertEquals(30, result);
+
+        x = 30;
+        y = 40;
+        result = GCD.gcd(x,y);
+        Assert.assertEquals(1, result);
     }
 
 }
