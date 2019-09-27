d4e1f2c7561c4489f32f2277ec651253784b9f30
hoanphi2201 <phihoan10@gmail.com>
2019-09-27 17:11:21 +0700

    Fix expect testcase

--- src/test/java/com/conversions/RomanToIntegerTest.java
+++ src/test/java/com/conversions/RomanToIntegerTest.java
@@ -32,32 +32,83 @@ public class RomanToIntegerTest {
         });
     }
     @Test
-    public void singleDigit() {
+    public void singleDigitI() {
         Assertions.assertEquals(1,  RomanToInteger.romanToInt("I"));
+    }
+    @Test
+    public void singleDigitV() {
         Assertions.assertEquals(5,  RomanToInteger.romanToInt("V"));
+    }
+    @Test
+    public void singleDigitX() {
         Assertions.assertEquals(10,  RomanToInteger.romanToInt("X"));
+    }
+    @Test
+    public void singleDigitL() {
         Assertions.assertEquals(50,  RomanToInteger.romanToInt("L"));
+    }
+    @Test
+    public void singleDigitC() {
         Assertions.assertEquals(100,  RomanToInteger.romanToInt("C"));
+    }
+    @Test
+    public void singleDigitD() {
         Assertions.assertEquals(500,  RomanToInteger.romanToInt("D"));
+    }
+    @Test
+    public void singleDigitM() {
         Assertions.assertEquals(1000,  RomanToInteger.romanToInt("M"));
     }
     @Test
-    public void repetition() {
+    public void repetitionII() {
         Assertions.assertEquals(2, RomanToInteger.romanToInt("II"));
+    }
+    @Test
+    public void repetition() {
         Assertions.assertEquals(20, RomanToInteger.romanToInt("XX"));
     }
     @Test
-    public void manyLettersInOrder() {
+    public void manyLettersInOrderIV() {
         Assertions.assertEquals(4, RomanToInteger.romanToInt("IV"));
-        Assertions.assertEquals(4, RomanToInteger.romanToInt("IX"));
-        Assertions.assertEquals(4, RomanToInteger.romanToInt("XI"));
+    }
+    @Test
+    public void manyLettersInOrderIX() {
+        Assertions.assertEquals(9, RomanToInteger.romanToInt("IX"));
+    }
+    @Test
+    public void manyLettersInOrderXI() {
+        Assertions.assertEquals(11, RomanToInteger.romanToInt("XI"));
+    }
+    @Test
+    public void manyLettersInOrderXIV() {
         Assertions.assertEquals(14, RomanToInteger.romanToInt("XIV"));
+    }
+    @Test
+    public void manyLettersInOrderXXXIX() {
         Assertions.assertEquals(39, RomanToInteger.romanToInt("XXXIX"));
+    }
+    @Test
+    public void manyLettersInOrderCCCXLIV() {
         Assertions.assertEquals(344, RomanToInteger.romanToInt("CCCXLIV"));
+    }
+    @Test
+    public void manyLettersInOrderCCCXCIX() {
         Assertions.assertEquals(399, RomanToInteger.romanToInt("CCCXCIX"));
+    }
+    @Test
+    public void manyLettersInOrderCD() {
         Assertions.assertEquals(400, RomanToInteger.romanToInt("CD"));
+    }
+    @Test
+    public void manyLettersInOrderCM() {
         Assertions.assertEquals(900, RomanToInteger.romanToInt("CM"));
+    }
+    @Test
+    public void manyLettersInOrderMMMCMXCII() {
         Assertions.assertEquals(3992, RomanToInteger.romanToInt("MMMCMXCII"));
+    }
+    @Test
+    public void manyLettersInOrderMMMCMXCIX() {
         Assertions.assertEquals(3999, RomanToInteger.romanToInt("MMMCMXCIX"));
     }
 }
