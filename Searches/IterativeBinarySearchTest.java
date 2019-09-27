9eefe1ab3423b29777066992e90a79b6d6b9979b
DaoDucKhiem <38343622+DaoDucKhiem@users.noreply.github.com>
2019-09-28 00:47:13 +0700

    delete old package IterativeBinarySearchTest and create new class used data flow testing based on def-use standard

--- Searches/IterativeBinarySearchTest.java
+++ Searches/IterativeBinarySearchTest.java
@@ -1,5 +1,4 @@
-package IterativeBinarySearchTest;
-
+import Searches.IterativeBinarySearch;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
@@ -7,20 +6,21 @@ import org.junit.BeforeClass;
 public class IterativeBinarySearchTest {
     private static IterativeBinarySearch search;
 
-
     @BeforeClass
 
     public static void prepareForalltest() {
         search = new IterativeBinarySearch();
     }
 
+    //array null return -1.
     @Test
     public void testSearch2() {
-        Integer [] arr = {0, 1, 2, 3, 4, 5};
+        Integer [] arr = {};
         int index = search.find(arr, 1);
-        assertTrue(index==1);
+        assertTrue(index==-1);
     }
 
+    //array != null return index of key
     @Test
     public void testSearch3() {
         Integer [] arr = {0, 1, 2, 3, 4, 5};
@@ -28,18 +28,11 @@ public class IterativeBinarySearchTest {
         assertTrue(index==2);
     }
 
+    //arry not null but can't find key, return -1
     @Test
     public void testSearch4() {
         Integer [] arr = {3, 5, 2, 6, 8};
         int index = search.find(arr, 4);
         assertTrue(index==-1);
     }
-
-    @Test
-    public void testSearch5() {
-        Integer [] arr = {0, 3, 5, 7, 9};
-        int index = search.find(arr, 1);
-        assertTrue(index==-1);
-    }
-
 }
