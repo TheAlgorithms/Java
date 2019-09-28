58fc950cd71ee265805c7b42c94409a700c3b947
SleepingDog158 <37662121+SleepingDog158@users.noreply.github.com>
2019-09-28 11:57:29 +0700

    Update GCDtest.java

--- GCDtest.java
+++ GCDtest.java
@@ -9,43 +9,42 @@ public class GCDTest{
 	@Test
 	public void test1(){
 		int a = 0;
-    int b = 56;
-	int answer1 = GCD.gcd(a,b);
-    assertEquals(answer1, 56);
+    		int b = 56;
+		int answer1 = GCD.gcd(a,b);
+    		assertEquals(answer1, 56);
 	}
 
 	@Test
 	public void test2(){
 		int a = 30;
-    int b = 0;
-int answer2 = GCD.gcd(a,b);
-    assertEquals(answer2, 30);
+    		int b = 0;
+		int answer2 = GCD.gcd(a,b);
+    		assertEquals(answer2, 30);
 	}
 
 	@Test
 	public void test3(){
 		int a = 8;
-    int b = 4;
-int answer3 = GCD.gcd(a,b);
-    assertEquals(answer3,4);
+    		int b = 4;
+		int answer3 = GCD.gcd(a,b);
+    		assertEquals(answer3,4);
 	}
 
 	@Test
 	public void test4(){
 		int a = 3;
-    int b = 13;
-int answer4 = GCD.gcd(a,b);
-    assertEquals(answer4, 1);
+    		int b = 13;
+		int answer4 = GCD.gcd(a,b);
+    		assertEquals(answer4, 1);
 	}
 
-}
 
-@Test
-public void test5(){
+	@Test
+	public void test5(){
 		int a = 65;
-    int b = 1;
-int answer5 = GCD.gcd(a,b);
-    assertEquals(answer5, 1);
+    		int b = 1;
+		int answer5 = GCD.gcd(a,b);
+    		assertEquals(answer5, 1);
 	}
 
 }
