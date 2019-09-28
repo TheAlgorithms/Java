67d210b8651dacbd812e126b8e99f794236741e0
phamconganh <conganhphamnd@gmail.com>
2019-09-28 23:55:15 +0700

    add test data

--- DynamicProgramming/CoinChangeTestData.java
+++ DynamicProgramming/CoinChangeTestData.java
@@ -1,17 +1,26 @@
 package DynamicProgramming;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class CoinChangeTestData {
     /**
-     * Số tiền ngoài biên (âm hoặc vô cực)
+     * All-Du path with var coint 1-2-3-5-6-7-8-11
      */
     @Test
-    public void wrongAmount() {
+    public void testCoint() {
+        int amount = 1;
+        int[] coins = { 1, 4, 5 };
+        assertEquals(CoinChange.minimumCoins(coins, amount), 1);
+    }
+
+    /**
+     * All-Du path with var amouont 1-2-3-5-11
+     */
+    @Test
+    public void testAmount() {
         int amount = -1;
         int[] coins = { 2, 4, 5 };
         try {
@@ -23,37 +32,10 @@ public class CoinChangeTestData {
     }
 
     /**
-     * Có loại tiền ngoài biên (âm hoặc cô cực)
+     * All-Du path with var minimumCoins 1-2-3-4-5-6-7-8-9-10-11
      */
     @Test
-    public void wrongCoin() {
-        int amount = 1;
-        int[] coins = { -1, 4, 5 };
-        try {
-            CoinChange.minimumCoins(coins, amount);
-            assertTrue(false);
-        } catch (Exception e) {
-            assertTrue(true);
-        }
-    }
-
-    /**
-     * Không có tổng loại tiền nào có thể tạo được số tiền
-     * (Số tiền nhỏ hơn các loại tiền hoặc không tạo được tổng)
-     */
-    @Test
-    public void noSolution() {
-        int amount = 7;
-        int[] coins = { 3, 5, 6 };
-        int num = CoinChange.minimumCoins(coins, amount);
-        assertNotEquals(0, num);
-    }
-
-    /**
-     * Tìm thấy solution
-     */
-    @Test
-    public void haveolution() {
+    public void testMinimumCoins() {
         int amount = 12;
         int[] coins = { 2, 4, 5 };
         int num = CoinChange.minimumCoins(coins, amount);
