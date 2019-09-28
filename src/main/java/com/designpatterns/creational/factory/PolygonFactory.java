98982f8f251578d31c4ac947fdfeaf7d900b62cf
Renjith <grenjithr@gmail.com>
2019-09-27 22:35:00 -0700

    Converting to a gradle project

--- src/main/java/com/designpatterns/creational/factory/PolygonFactory.java
+++ src/main/java/com/designpatterns/creational/factory/PolygonFactory.java
@@ -3,8 +3,8 @@ package com.designpatterns.creational.factory;
 /**
  * In class-based programming, the factory method pattern is a creational pattern that uses factory methods to deal
  * with the problem of creating objects without having to specify the exact class of the object that will be created.
- * This is done by creating objects by calling a factory method—either specified in an interface and implemented by
- * child classes, or implemented in a base class and optionally overridden by derived classes—rather than by calling
+ * This is done by creating objects by calling a factory method-either specified in an interface and implemented by
+ * child classes, or implemented in a base class and optionally overridden by derived classes-rather than by calling
  * a constructor.
  *
  * @see <a href="https://en.wikipedia.org/wiki/Factory_method_pattern">Factory Pattern</a>
--- src/main/java/com/designpatterns/structural/adapter/MovableAdapter.java
+++ src/main/java/com/designpatterns/structural/adapter/MovableAdapter.java
@@ -2,11 +2,11 @@ package com.designpatterns.structural.adapter;
 
 /**
  * An Adapter pattern acts as a connector between two incompatible interfaces that otherwise cannot be connected
- * directly. An Adapter wraps an existing class with a new interface so that it becomes compatible with the client’s
+ * directly. An Adapter wraps an existing class with a new interface so that it becomes compatible with the client's
  * interface.
  * <br>
  * The main motive behind using this pattern is to convert an existing interface into another interface that the client
- * expects. It’s usually implemented once the application is designed.
+ * expects. It's usually implemented once the application is designed.
  *
  * @see <a href="https://en.wikipedia.org/wiki/Adapter_pattern">Adapter Pattern</a>
  */
