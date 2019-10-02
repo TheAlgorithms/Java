50c6aa3addc0b4518538ffa161219a465bc5682e
debarghya472 <54908977+debarghya472@users.noreply.github.com>
2019-10-02 15:07:50 +0530

    corrected prime check

--- Maths/PrimeCheck.java
+++ Maths/PrimeCheck.java
@@ -27,7 +27,7 @@ public class PrimeCheck {
         if (n < 2 || n % 2 == 0) {
             return false;
         }
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+        for (int i = 3; i <=n/2; i ++) {
             if (n % i == 0) {
                 return false;
             }
