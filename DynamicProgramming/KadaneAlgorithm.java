f395a71b96e65fdd250ad6eca1eaeac203e8dbe6
Ayush Varshney <34279863+blast314@users.noreply.github.com>
2019-10-03 18:34:23 -0400

    Add info of indicies of the largest subarray sum
    Added the information in the output so that it is clear where the largest contiguous sum is coming from.

--- DynamicProgramming/KadaneAlgorithm.java
+++ DynamicProgramming/KadaneAlgorithm.java
@@ -12,44 +12,49 @@ import java.util.Scanner;
 
 public class KadaneAlgorithm {
 
-    /**
-     * This method implements Kadane's Algorithm
-     *
-     * @param arr The input array
-     * @return The maximum contiguous subarray sum of the array
-     */
-    static int largestContiguousSum(int arr[]) {
-        int i, len = arr.length, cursum = 0, maxsum = Integer.MIN_VALUE;
-        if (len == 0)    //empty array
-            return 0;
-        for (i = 0; i < len; i++) {
-            cursum += arr[i];
-            if (cursum > maxsum) {
-                maxsum = cursum;
-            }
-            if (cursum <= 0) {
-                cursum = 0;
-            }
-        }
-        return maxsum;
-    }
+	/**
+	 * This method implements Kadane's Algorithm
+	 *
+	 * @param arr
+	 *            The input array
+	 * @return The maximum contiguous subarray sum of the array
+	 */
+	static int[] largestContiguousSum(int arr[]) {
+		int i, len = arr.length, start = 0, s = 0, end = 0, cursum = 0, maxsum = Integer.MIN_VALUE;
+		if (len == 0) // empty array
+			return new int[] { 0, 0, 0 };
+		for (i = 0; i < len; i++) {
+			cursum += arr[i];
+			if (cursum > maxsum) {
+				maxsum = cursum;
+				start = s;
+				end = i;
+			}
+			if (cursum < 0) {
+				cursum = 0;
+				s = i + 1;
+			}
+		}
+		return new int[] { maxsum, start, end };
+	}
 
-    /**
-     * Main method
-     *
-     * @param args Command line arguments
-     */
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n, arr[], i;
-        n = sc.nextInt();
-        arr = new int[n];
-        for (i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int maxContSum = largestContiguousSum(arr);
-        System.out.println(maxContSum);
-        sc.close();
-    }
+	/**
+	 * Main method
+	 *
+	 * @param args
+	 *            Command line arguments
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n, arr[], i;
+		n = sc.nextInt();
+		arr = new int[n];
+		for (i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int[] maxContSum = largestContiguousSum(arr);
+		System.out.println("Maximum contiguous sum is " + maxContSum[0] + " from the subarray [" + maxContSum[1] + ", " + maxContSum[2] + "]");
+		sc.close();
+	}
 
 }
