539828a013cc386bc4941b8853c25ade4043089b
YATIN <yatin88617@gmail.com>
2019-10-04 18:09:27 +0530

    simplified program

--- Maths/PrimeCheck.java
+++ Maths/PrimeCheck.java
@@ -21,17 +21,13 @@ public class PrimeCheck {
      * @return {@code true} if {@code n} is prime
      */
     public static boolean isPrime(int n) {
-        if (n == 2) {
-            return true;
-        }
-        if (n < 2 || n % 2 == 0) {
-            return false;
-        }
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        
+	for(int i=2;i<=Math.sqrt(n);i++){
+             
+	  if(n%i==0)
+	    return false;
+
+	}
+	return true;
     }
 }
