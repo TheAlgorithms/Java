744657905fc25ecb9128f6bf8e63366db7fda030
shellhub <shellhub.me@gmail.com>
2019-10-05 19:28:55 +0800

    optimization

--- Maths/PrimeCheck.java
+++ Maths/PrimeCheck.java
@@ -27,7 +27,7 @@ public class PrimeCheck {
         if (n < 2 || n % 2 == 0) {
             return false;
         }
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+        for (int i = 3, limit = (int) Math.sqrt(n); i <= limit; i += 2) {
             if (n % i == 0) {
                 return false;
             }
