c3841f4187388718be67ea58dddc4dd1c71cc7df
Justin Giffard <justingiffard@gmail.com>
2019-10-07 14:23:30 +0200

    Added permutations to maths algorithms

---
+++ Maths/Permutations.java
@@ -0,0 +1,26 @@
+package Maths;
+
+public class Permutations {
+    public static void main(String[] args) {
+        int n = ;
+        int k = 3
+        System.out.println(n + "P" + k  + " = " + permutations(5,3));
+    }
+
+    /**
+     * Calculate the number of permuatations possible given number of elements and length of permutation.
+     * Can be thought of as drawing balls from a bag without putting them back and puttin them in a row so the final output will have
+     * no repeats and order will matter
+     * 
+     * eg. Given 3 options all permutations of lenth 2 (3P2)
+     * 12, 13, 21, 23, 31, 32
+     * therefore will output 6
+     * 
+     *  @param n number of elements
+     *  @param k length of permutation
+     *  @return the number of permutations from {@code n} options of length {@code k}
+     */
+    public static long permutations(int n, int k) {
+        return factorial(n)/factorial(n-k)
+    }
+}
