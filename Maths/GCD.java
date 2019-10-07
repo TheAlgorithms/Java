0d706c601ab8498d4222afc46b084021fe6f83de
Justin Giffard <justingiffard@gmail.com>
2019-10-07 15:39:12 +0200

    GCD

---
+++ Maths/GCD.java
@@ -0,0 +1,18 @@
+package Maths;
+
+public class GCD {
+    public static void main(String[] args) {
+        int i = 98;
+        int j = 56;
+
+        System.out.println("The GCM of "+i +" and " +j +" is " + gcd(i, j));
+    }
+
+    /**
+     *  Calculate the greatest common denomator of {@code i} and {@code j}
+     */
+    public static int gcd(int i, int j) {
+        if (i == 0) return j;
+        return gcd(j % i, i);
+    }
+}
