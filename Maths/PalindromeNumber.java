58eb2ec581fe03afdbf27d480cbe6a4bcc2e364f
shellhub <shellhub.me@gmail.com>
2019-10-11 16:17:38 +0800

    PalindromeNumber

---
+++ Maths/PalindromeNumber.java
@@ -0,0 +1,30 @@
+package Maths;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+
+        assert isPalindrome(12321);
+        assert !isPalindrome(1234);
+        assert isPalindrome(1);
+    }
+
+    /**
+     * Check if {@code n} is palindrome number or not
+     *
+     * @param number the number
+     * @return {@code true} if {@code n} is palindrome number, otherwise {@code false}
+     */
+    public static boolean isPalindrome(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException(number + "");
+        }
+        int numberCopy = number;
+        int reverseNumber = 0;
+        while (numberCopy != 0) {
+            int remainder = numberCopy % 10;
+            reverseNumber = reverseNumber * 10 + remainder;
+            numberCopy /= 10;
+        }
+        return number == reverseNumber;
+    }
+}
