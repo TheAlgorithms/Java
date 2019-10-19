2fd983cefc5fd33d2d5772d498708038d9437ef2
MatanDavidi <davidi.matan@gmail.com>
2019-10-19 18:16:00 +0200

    Replace empty if statement by changing if condition

--- Others/Abecedarian.java
+++ Others/Abecedarian.java
@@ -11,14 +11,11 @@ public class Abecedarian {
         int index = s.length() - 1;
 
         for (int i = 0; i < index; i++) {
-
-            if (s.charAt(i) <= s.charAt(i + 1)) {
-            } //Need to check if each letter for the whole word is less than the one before it
-
-            else {
+            if (s.charAt(i) > s.charAt(i + 1)) {
                 return false;
             }
         }
+        
         return true;
     }
 }
