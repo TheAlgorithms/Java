c7a0b644166bf7c55504888629f6fd972292010e
vineet1020 <56506554+vineet1020@users.noreply.github.com>
2019-10-25 00:30:39 +0530

    Factorial.java
    Made the code suitable for higher valued inputs using BigInteger

--- Maths/Factorial.java
+++ Maths/Factorial.java
@@ -1,25 +1,42 @@
-package Maths;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-public class Factorial {
-    public static void main(String[] args) {
-        int n = 5;
-        System.out.println(n + "! = " + factorial(n));
+public class Solution {
+    static void LongFactorials(int n) {
+        if(n>=20)
+        {
+        int arr[]=new int[51561];
+        
+        BigInteger temp=new BigInteger("1");
+        for(int i=1;i<=n;i++)
+        temp=temp.multiply(BigInteger.valueOf(i));
+        System.out.println(temp);
+        }
+        else if(n<20 && n>=0) 
+        {
+          int sum=1;
+            for(int i=0;i<n;i++)
+            {
+                sum*=i;
+            }
+            System.out.println(sum);
+            
+        }
     }
 
-    /**
-     * Calculate factorial
-     *
-     * @param n the number
-     * @return the factorial of {@code n}
-     */
-    public static long factorial(int n) {
-        if (n < 0) {
-            throw new ArithmeticException("n < 0");
-        }
-        long fac = 1;
-        for (int i = 1; i <= n; ++i) {
-            fac *= i;
-        }
-        return fac;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        extraLongFactorials(n);
+
+        scanner.close();
     }
 }
