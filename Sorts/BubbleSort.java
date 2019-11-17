30abaf7d0a8cc13c0035a472af8e33e0f9f3a7b6
Abubakar548 <bsef17m548@pucit.edu.pk>
2019-11-17 18:33:12 +0500

    Bubble Sort bugg #1164 fixed

--- Sorts/BubbleSort.java
+++ Sorts/BubbleSort.java
@@ -12,19 +12,16 @@ class BubbleSort implements SortAlgorithm {
     /**
      * This method implements the Generic Bubble Sort
      *
-     * @param array The array to be sorted
-     *              Sorts the array in increasing order
+     * @param array The array to be sorted Sorts the array in increasing order
      **/
 
     @Override
     public <T extends Comparable<T>> T[] sort(T array[]) {
         for (int i = 0, size = array.length; i < size - 1; ++i) {
-            boolean swapped = false;
+
             for (int j = 0; j < size - 1 - i; ++j) {
-                swapped = less(array[j], array[j + 1]) && swap(array, j, j + 1);
-            }
-            if (!swapped) {
-                break;
+                if (less(array[j], array[j + 1]))
+                    swap(array, j, j + 1);
             }
         }
         return array;
@@ -34,7 +31,7 @@ class BubbleSort implements SortAlgorithm {
     public static void main(String[] args) {
 
         // Integer Input
-        Integer[] integers = {4, 23, 6, 78, 1, 54, 231, 9, 12};
+        Integer[] integers = { 4, 23, 6, 78, 1, 54, 231, 9, 12 };
         BubbleSort bubbleSort = new BubbleSort();
         bubbleSort.sort(integers);
 
@@ -42,8 +39,8 @@ class BubbleSort implements SortAlgorithm {
         print(integers);
 
         // String Input
-        String[] strings = {"c", "a", "e", "b", "d"};
-        //Output => e, d, c, b, a
+        String[] strings = { "c", "a", "e", "b", "d" };
+        // Output => e, d, c, b, a
         print(bubbleSort.sort(strings));
 
     }
