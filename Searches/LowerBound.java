3e89e0381874cfe728d1c3b7cc2e8d3113ca6d0c
Shamim-Nik <56692841+Shamim-Nik@users.noreply.github.com>
2020-03-13 05:17:21 +0600

    Create LowerBound.java

---
+++ Searches/LowerBound.java
@@ -0,0 +1,20 @@
+public class  LowerBound {
+
+    public static void LowerBound(String[] args) {
+    }
+
+    private static int lowerBound(int[] array, int length, int value) {
+        int low = 0;
+        int high = length;
+        while (low < high) {
+            final int mid = (low + high) / 2;
+            //checks if the value is less than middle element of the array
+            if (value <= array[mid]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
