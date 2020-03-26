ffb4ce1bff6e6ede4405cdd013d8ccd2fc9255de
iheb1196 <55294784+iheb1196@users.noreply.github.com>
2020-03-26 21:04:02 +0100

    DecimalToHexadecimal

--- src/main/java/com/conversions/DecimalToHexadecimal.java
+++ src/main/java/com/conversions/DecimalToHexadecimal.java
@@ -5,6 +5,11 @@ import java.math.BigInteger;
 public class DecimalToHexadecimal {
     private static final char[] hexChars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
     private static final BigInteger valueHex = BigInteger.valueOf(16);
+    private static final int sizeOfIntInHalfBytes = 8;
+	private static final int numberOfBitsInAHalfByte = 4;
+	private static final int halfByte = 0x0F;
+	private static final char[] hexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+			'F' };
 
     /**
      * This method converts and decimal number to a Hexadecimal number
@@ -24,4 +29,25 @@ public class DecimalToHexadecimal {
         }
         return hex;
     }
+    public static String decToHex(int dec) {
+		StringBuilder hexBuilder = new StringBuilder(sizeOfIntInHalfBytes);
+		hexBuilder.setLength(sizeOfIntInHalfBytes);
+		for (int i = sizeOfIntInHalfBytes - 1; i >= 0; --i) {
+			int j = dec & halfByte;
+			hexBuilder.setCharAt(i, hexDigits[j]);
+			dec >>= numberOfBitsInAHalfByte;
+		}
+		return hexBuilder.toString().toLowerCase();
+	}
+
+	// Test above function.
+	public static void main(String[] args) {
+		System.out.println("Test...");
+		int dec = 70654444;
+		String libraryDecToHex = Integer.toHexString(dec);
+		String decToHex = decToHex(dec);
+		System.out.println("Result from the library : " + libraryDecToHex);
+		System.out.println("Result decToHex method : " + decToHex);
+	}
 }
+
