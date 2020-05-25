fd24ffc5e35555f7ae0e5eef5f45f00fc7fc4a30
MohamedBechir <jamoussi.bechir@yahoo.com>
2020-05-25 15:03:41 +0100

    Fix requested changes

--- src/main/java/com/conversions/HexadecimalToBinary.java
+++ src/main/java/com/conversions/HexadecimalToBinary.java
@@ -11,34 +11,92 @@ public class HexadecimalToBinary {
 	
 	public String hexToBin (String hexStr) {
 		
-		String binaryString = "", hexaNumbers = "0123456789ABCDEF";
-		int indexOfHex, decimalNumber = 0, k = 1;
+		String binaryString = "", hexaNumbers = "0123456789ABCDEF", 
+				DecimalStr ="", binaryStringBefore ="" , binaryStringAfter = "";
+		int indexOfHex, decimalNumber = 0, k = 1, n =1, z=1, decimalNumberBefore = 0
+				, decimalNumberAfter = 0;
 		char letter;
-		int binaryArray[] = new int [50];
+		int binaryArray[] = new int [60];
+		int binaryArrayBefore[] = new int [60];
+		int binaryArrayAfter[] = new int [60];
 		
 		hexStr = hexStr.toUpperCase();
-		
+		int pointPosition = hexStr.indexOf(".");
 		/**
 		 * Transform the hexadecimal number to decimal number
 		 */
+		if ( pointPosition == -1) {
 		for ( int i = 0 ;  i < hexStr.length(); i++) {
-			letter = hexStr.charAt(i);
-			indexOfHex = hexaNumbers.indexOf(letter);
-			decimalNumber = 16 * decimalNumber + indexOfHex;
+				letter = hexStr.charAt(i);
+				indexOfHex = hexaNumbers.indexOf(letter);
+				decimalNumber = 16 * decimalNumber + indexOfHex;
+			}
+		}
+		else {
+			for ( int i = 0 ;  i < pointPosition ; i++) {
+				letter = hexStr.charAt(i);
+				indexOfHex = hexaNumbers.indexOf(letter);
+				decimalNumberBefore = 16 * decimalNumberBefore + indexOfHex;
+			}
+			String decimalNumberBeforeStr = String.valueOf(decimalNumberBefore);
+
+			for ( int i = pointPosition+1 ;  i < hexStr.length() ; i++) {
+				letter = hexStr.charAt(i);
+				indexOfHex = hexaNumbers.indexOf(letter);
+				decimalNumberAfter = 16 * decimalNumberAfter + indexOfHex;
+			}
+
+			String decimalNumberAfterStr = String.valueOf(decimalNumberAfter);
+			
+			DecimalStr = decimalNumberBeforeStr + '.' + decimalNumberAfterStr;
+			}
+
+		
+		
+		int pointPositionDec = DecimalStr.indexOf(".");
+		/**
+		 * Check whether the result contains a floating point or not
+		 */
+		if (pointPositionDec == -1) {
+			while (decimalNumber != 0) {
+				binaryArray[k++] = decimalNumber % 2;
+				decimalNumber = decimalNumber / 2;
+			}	
+
+		}else {
+			/**
+			 * If it contains floating points we need to divide it into two parts before the point and after it
+			 */
+				while (decimalNumberBefore != 0) {
+					binaryArrayBefore[z++] = decimalNumberBefore % 2;
+					decimalNumberBefore = decimalNumberBefore / 2;
+				}	
+				while (decimalNumberAfter != 0) {
+					binaryArrayAfter[n++] = decimalNumberAfter % 2;
+					decimalNumberAfter = decimalNumberAfter / 2;
+				}	
+
 		}
 
-		/**
-		 * Transform decimal number to binary and put it in an array
-		 */
-		while (decimalNumber != 0) {
-			binaryArray[k++] = decimalNumber % 2;
-			decimalNumber = decimalNumber / 2;
-		}
-		/**
-		 * Put the binary in a string
-		 */
-		for ( int j = k-1 ; j>0 ; j--) {
-			binaryString = binaryString + binaryArray[j];
+		if(pointPositionDec == -1) {
+			for ( int j = k-1 ; j>0 ; j--) {
+				binaryString = binaryString + binaryArray[j];
+			}	
+		}else {
+			for ( int j = z-1 ; j>0 ; j--) {
+				binaryStringBefore = binaryStringBefore + binaryArrayBefore[j];
+			}	
+
+			for ( int j = n-1 ; j>0 ; j--) {
+				binaryStringAfter = binaryStringAfter + binaryArrayAfter[j];
+			}
+			/**
+			 * Remove the zeroes in the end of the hexadecimal
+			 */
+			binaryStringAfter = binaryStringAfter.replaceAll("0*$", "").replaceAll("\\.$", "");
+			
+			
+			binaryString = binaryStringBefore + "." + binaryStringAfter;
 		}
 
 		return binaryString;
--- src/test/java/com/conversions/HexadecimalToBinaryTest.java
+++ src/test/java/com/conversions/HexadecimalToBinaryTest.java
@@ -1,7 +1,5 @@
 package com.conversions;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,14 +7,24 @@ class HexadecimalToBinaryTest {
 
 	@Test
 	void test() {
+		//HexadecimaltTesting
 		HexadecimalToBinary hexadecimalToBinary = new HexadecimalToBinary();
-        Assertions.assertEquals("10101011", hexadecimalToBinary.hexToBin("AB"), "Incorrect Conversion");
+        Assertions.assertEquals("1011110011101111", hexadecimalToBinary.hexToBin("BCEF"), "Incorrect Conversion");
         Assertions.assertEquals("10101101010101111001101", hexadecimalToBinary.hexToBin("56ABCD"), "Incorrect Conversion");
         Assertions.assertEquals("10011101111011010001001", hexadecimalToBinary.hexToBin("4ef689"), "Incorrect Conversion");
         Assertions.assertEquals("10011101111", hexadecimalToBinary.hexToBin("4EF"), "Incorrect Conversion");
         Assertions.assertEquals("101010111100110111101111", hexadecimalToBinary.hexToBin("ABCDEF"), "Incorrect Conversion");
         //It returns -1 if you enter a wrong hexaDecimal
         Assertions.assertEquals("-1", hexadecimalToBinary.hexToBin("K"), "Incorrect Conversion");
+        
+        
+        //Hexadecimal with floating point testing 
+        Assertions.assertEquals("101010111100.101111", hexadecimalToBinary.hexToBin("ABC.BC"), "Incorrect Conversion");
+        Assertions.assertEquals("10101101010.101111001101", hexadecimalToBinary.hexToBin("56A.BCD"), "Incorrect Conversion");
+        Assertions.assertEquals("1001110.1111011010001001", hexadecimalToBinary.hexToBin("4e.f689"), "Incorrect Conversion");
+        Assertions.assertEquals("1001110.1111", hexadecimalToBinary.hexToBin("4E.F"), "Incorrect Conversion");
+        Assertions.assertEquals("10101011110011011110.1111", hexadecimalToBinary.hexToBin("ABCDE.F"), "Incorrect Conversion");
+  
 	}
 
 }
