b9ea01c4eb17dbfcaceec409cc07cc069d541d2c
sharmaVipin101 <vipinsharma.jnv@live.com>
2020-05-28 13:37:11 +0530

    Corrected ShellSort.java

--- Sorts/ShellSort.java
+++ Sorts/ShellSort.java
@@ -1,42 +1,44 @@
-package Sorts;
+import java.util.*;
 
-import static Sorts.SortUtils.*;
-
-public class ShellSort implements SortAlgorithm {
-
-    /**
-     * This method implements Generic Shell Sort.
-     *
-     * @param array the array to be sorted
-     */
-    @Override
-    public <T extends Comparable<T>> T[] sort(T[] array) {
-        int length = array.length;
-        int gap = 1;
-
-        /* Calculate gap for optimization purpose */
-        while (gap < length / 3) {
-            gap = 3 * gap + 1;
-        }
-
-        for (; gap > 0; gap /= 3) {
-            for (int i = gap; i < length; i++) {
-                int j;
-		T temp = array[i];
-                for (j = i; j >= gap && less(temp, array[j - gap]); j -= gap) {
-                    array[j] = array[j - gap];
-                }
-                array[j] = temp;
-            }
-        }
-        return array;
-    }
+class ShellSort 
+{ 
+  
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i] + " "); 
+        System.out.println(); 
+    } 
+  
+      static  int sort(int arr[]) 
+        { 
+            int n = arr.length; 
+      
+          
+            for (int gap = n/2; gap > 0; gap /= 2) 
+            { 
+                
+                for (int i = gap; i < n; i += 1) 
+                { 
+                   
+                    int temp = arr[i]; 
+                    int j; 
+                    for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
+                        arr[j] = arr[j - gap]; 
+      
+                    arr[j] = temp; 
+                } 
+            } 
+            return 0; 
+        } 
 
     /* Driver Code */
     public static void main(String[] args) {
-        Integer[] toSort = {4, 23, 6, 78, 1, 54, 231, 9, 12};
+        int ar[] = {4, 23, 6, 78, 1, 54, 231, 9, 12};
 
-        ShellSort sort = new ShellSort();
-        print(sort.sort(toSort));
+        sort(ar);
+        printArray(ar);
+        
     }
 }
