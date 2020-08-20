34869cb5cb5d29344cd1faef73cf53928ca833be
shellhub <shellhub.me@gmail.com>
2020-08-21 00:00:48 +0800

    optimization

--- Maths/AliquotSum.java
+++ Maths/AliquotSum.java
@@ -25,7 +25,7 @@ public class AliquotSum {
      */
     public static int aliquotSum(int number) {
         int sum = 0;
-        for (int i = 1; i < number; ++i) {
+        for (int i = 1, limit = number / 2; i <= limit; ++i) {
             if (number % i == 0) {
                 sum += i;
             }
