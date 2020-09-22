093cc37806ac4ed5e4f8b1d51fda2029e986cdb8
shellhub <shellhub.me@gmail.com>
2020-09-22 11:06:57 +0800

    count singly linked list using recursion

---
+++ DataStructures/Lists/CountSinglyLinkedListRecursion.java
@@ -0,0 +1,26 @@
+package DataStructures.Lists;
+
+public class CountSinglyLinkedListRecursion extends SinglyLinkedList {
+    public static void main(String[] args) {
+        CountSinglyLinkedListRecursion list = new CountSinglyLinkedListRecursion();
+        for (int i = 1; i <= 5; ++i) {
+            list.insert(i);
+        }
+        assert list.count() == 5;
+    }
+
+    /**
+     * Calculate the count of the list manually using recursion.
+     *
+     * @param head head of the list.
+     * @return count of the list.
+     */
+    private int countRecursion(Node head) {
+        return head == null ? 0 : 1 + countRecursion(head.next);
+    }
+
+    @Override
+    public int count() {
+        return countRecursion(getHead());
+    }
+}
