d5bec9fe0febf48668c00d5d821d8e74c2453131
Sarthak Jain <71564274+Sarthak-15220@users.noreply.github.com>
2020-10-01 22:11:19 +0530

    Update Pow.java
    Directly use power function in java to calculate a power b.

--- Maths/Pow.java
+++ Maths/Pow.java
@@ -18,10 +18,6 @@ public class Pow {
      * @return the value {@code a}<sup>{@code b}</sup>.
      */
     public static long pow(int a, int b) {
-        long result = 1;
-        for (int i = 1; i <= b; i++) {
-            result *= a;
-        }
-        return result;
+        return Math.pow(a,b);
     }
 }
