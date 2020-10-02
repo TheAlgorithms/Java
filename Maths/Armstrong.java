707f5204dc81216afb9e81733ec00b91905ffac0
Devesh Baranwal <deveshbrnwl@gmail.com>
2020-10-02 23:46:03 +0530

    Corrected the armstrong number code

--- Maths/Armstrong.java
+++ Maths/Armstrong.java
@@ -1,17 +1,18 @@
-package Maths;
 
 /**
  * An Armstrong number is equal to the sum of the cubes of its digits.
  * For example, 370 is an Armstrong number because 3*3*3 + 7*7*7 + 0*0*0 = 370.
  * An Armstrong number is often called Narcissistic number.
  */
-public class Armstrong {
+public class Armstrong 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         assert (isArmStrong(0));
         assert (isArmStrong(1));
         assert (isArmStrong(153));
-        assert (isArmStrong(1634));
+        assert (!isArmStrong(1634));
         assert (isArmStrong(371));
         assert (!isArmStrong(200));
     }
@@ -22,22 +23,19 @@ public class Armstrong {
      * @param number number to check
      * @return {@code true} if given number is armstrong number, {@code false} otherwise
      */
-    private static boolean isArmStrong(int number) {
-        int sum = 0;
-        int temp = number;
-        int numberOfDigits = 0;
-        while (temp != 0) {
-            numberOfDigits++;
-            temp /= 10;
-        }
-        temp = number; /* copy number again */
-        while (number > 0) {
-            int remainder = number % 10;
-            int power = 1;
-            for (int i = 1; i <= numberOfDigits; power *= remainder, ++i) ;
-            sum = sum + power;
-            number /= 10;
-        }
-        return sum == temp;
+    private static boolean isArmStrong(int number) 
+    {
+        int sum = 0 ,a,temp; 
+        temp=number;  
+        while(number>0)  
+        {  
+            a=number%10;  
+            number/=10;  
+            sum+=(a*a*a);  
+        }  
+        if(temp==sum)  
+            return true;
+        else
+            return false;
     }
 }
