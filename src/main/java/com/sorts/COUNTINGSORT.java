66f78db417bc7c52ef31f9a25a142482bb97875c
Shruti Rawal <54466640+ShrutiRawal@users.noreply.github.com>
2020-10-03 19:49:17 +0530

    fix:updated for typo

--- src/main/java/com/sorts/COUNTINGSORT.java
+++ src/main/java/com/sorts/COUNTINGSORT.java
@@ -1,20 +1,19 @@
- import java.util.*;
+import java.util.*;
 class CountingSort { 
-	void countSort(char arr[]) 
-	{ 
+	void countSort(char arr[]){ 
 		int n = arr.length;  
 		char temp[] = new char[n];  
-		int count[] = new int[256]; 
-		Arrays.fill(count,0);
+		int freq[] = new int[256]; 
+		Arrays.fill(freq,0);
 		for(int i=0;i<n;i++){ 
-	            count[arr[i]]++; 
+	            freq[arr[i]]++; 
                 }
 		for(int i=1;i<256;i++){
-		     count[i] += count[i-1];
+		     freq[i] += freq[i-1];
 		} 
 		for(int i=n-1;i>=0;i--) { 
-		    temp[count[arr[i]]-1] = arr[i]; 
-		    count[arr[i]]++; 
+		    temp[freq[arr[i]]-1] = arr[i]; 
+		    freq[arr[i]]++; 
 		}  
 		for(int i=0;i<n;i++){
 		    arr[i] = temp[i];
