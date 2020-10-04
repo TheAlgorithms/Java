c0c26908fd114c38c097945f67f4d312d38425bf
Abhishek Nallana <60889353+abhinallana@users.noreply.github.com>
2020-10-04 11:45:34 +0530

    Different Method for Recursion
    using if and else statements finding recursion. By user input

--- Maths/FactorialRecursion.java
+++ Maths/FactorialRecursion.java
@@ -1,26 +1,23 @@
 package Maths;
 
 public class FactorialRecursion {
+    
+    //recursion method for Factorial
+	public static int factorial(int n) {
+		if(n<1) 
+				return 1;
+		else
+				return n*factorial(n-1);
+	}
 
-    /* Driver Code */
-    public static void main(String[] args) {
-        assert factorial(0) == 1;
-        assert factorial(1) == 1;
-        assert factorial(2) == 2;
-        assert factorial(3) == 6;
-        assert factorial(5) == 120;
-    }
-
-    /**
-     * Recursive FactorialRecursion Method
-     *
-     * @param n The number to factorial
-     * @return The factorial of the number
-     */
-    public static long factorial(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("number is negative");
-        }
-        return n == 0 || n == 1 ? 1 : n * factorial(n - 1);
+   /*Driver Code*/
+	public static void main(String[] args) {
+		//user input
+		Scanner sc = new Scanner(System.in);
+		//calling factorial
+		int input = factorial(sc.nextInt());
+		
+		//output
+		System.out.println("Factorial : " + input);
     }
 }
