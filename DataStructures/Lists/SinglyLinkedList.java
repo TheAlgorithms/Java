ade9c2c71da555548f562295c6d7c800d40056d0
Dnyaneshwar Ware <62518496+Dnyaneshwar-dev@users.noreply.github.com>
2020-10-05 19:24:43 +0530

    SinglyLinkedList.c
    Singly Linked List implementation in C Language

--- DataStructures/Lists/SinglyLinkedList.java
+++ DataStructures/Lists/SinglyLinkedList.java
@@ -1,333 +1,167 @@
-package DataStructures.Lists;
+#include <stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+    int data;
+    struct node* next;
+}NODE;
+NODE* st=NULL;
+//Function to create new node
+NODE* createNode(int d)
+{
+    NODE *a;
+    a=(NODE*)malloc(sizeof(NODE));
+    a->data=d;
+    a->next=NULL;
+    return a;
+}
 
-/**
- * This class implements a SinglyLinked List. This is done
- * using SinglyLinkedList class and a LinkForLinkedList Class.
- * <p>
- * A linked list is similar to an array, it hold values.
- * However, links in a linked list do not have indexes. With
- * a linked list you do not need to predetermine it's size as
- * it grows and shrinks as it is edited. This is an example of
- * a singly linked list. Elements can only be added/removed
- * at the head/front of the list.
- */
-public class SinglyLinkedList {
-    /**
-     * Head refer to the front of the list
-     */
-    private Node head;
+//Function to retrive last node address
+NODE *getLastNode()
+{
+    NODE *a=st;
+    while(a->next!=NULL)
+        a=a->next;
+    return a;
+}
 
-    /**
-     * Size of SinglyLinkedList
-     */
-    private int size;
-
-    /**
-     * Init SinglyLinkedList
-     */
-    public SinglyLinkedList() {
-        head = null;
-        size = 0;
+//Function to display Linked List
+void display()
+{
+    NODE *a=st;
+    if(st==NULL)
+    {
+        printf("Empty List\n");
+        return;
     }
-
-    /**
-     * Init SinglyLinkedList with specified head node and size
-     *
-     * @param head the head node of list
-     * @param size the size of list
-     */
-    public SinglyLinkedList(Node head, int size) {
-        this.head = head;
-        this.size = size;
-    }
-
-    /**
-     * Inserts an element at the head of the list
-     *
-     * @param x element to be added
-     */
-    public void insertHead(int x) {
-        insertNth(x, 0);
-    }
-
-    /**
-     * Insert an element at the tail of the list
-     *
-     * @param data element to be added
-     */
-    public void insert(int data) {
-        insertNth(data, size);
-    }
-
-    /**
-     * Inserts a new node at a specified position of the list
-     *
-     * @param data     data to be stored in a new node
-     * @param position position at which a new node is to be inserted
-     */
-    public void insertNth(int data, int position) {
-        checkBounds(position, 0, size);
-        Node newNode = new Node(data);
-        if (head == null) { /* the list is empty */
-            head = newNode;
-            size++;
-            return;
-        } else if (position == 0) { /* insert at the head of the list */
-            newNode.next = head;
-            head = newNode;
-            size++;
-            return;
-        }
-        Node cur = head;
-        for (int i = 0; i < position - 1; ++i) {
-            cur = cur.next;
-        }
-        newNode.next = cur.next;
-        cur.next = newNode;
-        size++;
-    }
-
-
-    /**
-     * Deletes a node at the head
-     */
-    public void deleteHead() {
-        deleteNth(0);
-    }
-
-    /**
-     * Deletes an element at the tail
-     */
-    public void delete() {
-        deleteNth(size - 1);
-    }
-
-    /**
-     * Deletes an element at Nth position
-     */
-    public void deleteNth(int position) {
-        checkBounds(position, 0, size - 1);
-        if (position == 0) {
-            Node destroy = head;
-            head = head.next;
-            destroy = null; /* clear to let GC do its work */
-            size--;
-            return;
-        }
-        Node cur = head;
-        for (int i = 0; i < position - 1; ++i) {
-            cur = cur.next;
-        }
-
-        Node destroy = cur.next;
-        cur.next = cur.next.next;
-        destroy = null; // clear to let GC do its work
-
-        size--;
-    }
-
-    /**
-     * @param position to check position
-     * @param low      low index
-     * @param high     high index
-     * @throws IndexOutOfBoundsException if {@code position} not in range {@code low} to {@code high}
-     */
-    public void checkBounds(int position, int low, int high) {
-        if (position > high || position < low) {
-            throw new IndexOutOfBoundsException(position + "");
-        }
-    }
-
-    /**
-     * Clear all nodes in the list
-     */
-    public void clear() {
-        Node cur = head;
-        while (cur != null) {
-            Node prev = cur;
-            cur = cur.next;
-            prev = null; // clear to let GC do its work
-        }
-        head = null;
-        size = 0;
-    }
-
-    /**
-     * Checks if the list is empty
-     *
-     * @return {@code true} if list is empty, otherwise {@code false}.
-     */
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    /**
-     * Returns the size of the linked list.
-     *
-     * @return the size of the list.
-     */
-    public int size() {
-        return size;
-    }
-
-    /**
-     * Get head of the list.
-     *
-     * @return head of the list.
-     */
-    public Node getHead() {
-        return head;
-    }
-
-    /**
-     * Calculate the count of the list manually
-     *
-     * @return count of the list
-     */
-    public int count() {
-        int count = 0;
-        Node cur = head;
-        while (cur != null) {
-            cur = cur.next;
-            count++;
-        }
-        return count;
-    }
-
-    /**
-     * Test if the value key is present in the list.
-     *
-     * @param key the value to be searched.
-     * @return {@code true} if key is present in the list, otherwise {@code false}.
-     */
-    public boolean search(int key) {
-        Node cur = head;
-        while (cur != null) {
-            if (cur.value == key) {
-                return true;
-            }
-            cur = cur.next;
-        }
-        return false;
-    }
-
-    /**
-     * Return element at special index.
-     *
-     * @param index given index of element
-     * @return element at special index.
-     */
-    public int getNth(int index) {
-        checkBounds(index, 0, size - 1);
-        Node cur = head;
-        for (int i = 0; i < index; ++i) {
-            cur = cur.next;
-        }
-        return cur.value;
-    }
-
-
-    @Override
-    public String toString() {
-        if (size == 0) {
-            return "";
-        }
-        StringBuilder builder = new StringBuilder();
-        Node cur = head;
-        while (cur != null) {
-            builder.append(cur.value).append("->");
-            cur = cur.next;
-        }
-        return builder.replace(builder.length() - 2, builder.length(), "").toString();
-    }
-
-
-    /**
-     * Driver Code
-     */
-    public static void main(String[] arg) {
-        SinglyLinkedList list = new SinglyLinkedList();
-        assert list.isEmpty();
-        assert list.size() == 0
-                && list.count() == 0;
-        assert list.toString().equals("");
-
-        /* Test insert function */
-        list.insertHead(5);
-        list.insertHead(7);
-        list.insertHead(10);
-        list.insert(3);
-        list.insertNth(1, 4);
-        assert list.toString().equals("10->7->5->3->1");
-
-        /* Test search function */
-        assert list.search(10)
-                && list.search(5)
-                && list.search(1)
-                && !list.search(100);
-
-        /* Test get function */
-        assert list.getNth(0) == 10
-                && list.getNth(2) == 5
-                && list.getNth(4) == 1;
-
-        /* Test delete function */
-        list.deleteHead();
-        list.deleteNth(1);
-        list.delete();
-        assert list.toString().equals("7->3");
-
-        assert list.size == 2
-                && list.size() == list.count();
-
-        list.clear();
-        assert list.isEmpty();
-
-        try {
-            list.delete();
-            assert false; /* this should not happen */
-        } catch (Exception e) {
-            assert true; /* this should happen */
-        }
+    printf("List: \n");
+    while(a!=NULL)
+    {
+        printf("%5d",a->data);
+        a=a->next;
     }
 }
 
-/**
- * This class is the nodes of the SinglyLinked List.
- * They consist of a value and a pointer to the node
- * after them.
- */
-class Node {
-    /**
-     * The value of the node
-     */
-    int value;
-
-    /**
-     * Point to the next node
-     */
-    Node next;
-
-    Node() {
-
+//Function to Count Nodes from list
+int count()
+{
+    int c=0;
+    NODE *a=st;
+    while(a!=NULL)
+    {
+        c++;
+        a=a->next;
     }
+    return c;
+}
 
-    /**
-     * Constructor
-     *
-     * @param value Value to be put in the node
-     */
-    Node(int value) {
-        this(value, null);
+//Function to compute data sum of list
+int sum()
+{
+    NODE *a=st;
+    int tot=0;
+    while(a!=NULL)
+    {
+        tot+=a->data;
+        a=a->next;
     }
+    return tot;
+}
 
-    /**
-     * Constructor
-     * @param value Value to be put in the node
-     * @param next Reference to the next node
-     */
-    Node(int value, Node next) {
-        this.value = value;
-        this.next = next;
+//Function to add new node at end
+void addEnd(int d)
+{
+    NODE *a,*b;
+    a=createNode(d);
+    if(st==NULL){
+        st=a;
+        return;
     }
+    b=getLastNode();
+    b->next=a;
+}
+
+//Function to addd new node at begin
+void addBeg(int d)
+{
+    NODE *a;
+    a=createNode(d);
+    a->next=st;
+    st=a;
+}
+
+//Function to  find maximum data from list
+
+int getMax()
+{
+    //Considering list is not empty
+    NODE *a=st;
+    int mx=a->data;
+    a=a->next;
+    while(a!=NULL)
+    {
+        if(a->data>mx)
+            mx=a->data;
+        a=a->next;
+    }
+    return mx;
+}
+
+//Function to get Minimum in list
+int getMin()
+{
+    //Considering list is not empty
+    NODE *a=st;
+    int mn=a->data;
+    a=a->next;
+    while(a!=NULL)
+    {
+        if(a->data<mn)
+            mn=a->data;
+        a=a->next;
+    }
+    return mn;
+}
+
+int main()
+{
+    int opt,d,res;
+    while(1)
+    {
+        printf("\nSingly Linked List\n");
+        printf("Menu:\n1.Add at End\n2.Add at Begin\n3.Display\n4.Count Nodes\n5.Sum\n6.Get Minimum\n7.Get Maximum\n8.Exit\nOption : ");
+        scanf("%d",&opt);
+        if(opt>7)
+            break;
+        switch (opt) {
+            case 1:
+                printf("\nEnter Data at End : ");
+                scanf("%d",&d);
+                addEnd(d);
+                break;
+            case 2:
+                printf("\nEnter Data at Begin: ");
+                scanf("%d",&d);
+                addBeg(d);
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                res=count();
+                printf("\nNodes : %d",res);
+                break;
+            case 5:
+                res=sum();
+                printf("\nSum : %d",res);
+                break;
+            case 6:
+                printf("\nMimimum Data value : %d",getMin());
+                break;
+            case 7:
+                printf("\nMaximum Data Value : %d",getMax());
+
+        }
+    }
+    return 0;
 }
