93171a6675db178ee8a9fcedf2ef796044fdff2b
Priya Shrinithi <47693475+PriyaShrinithi@users.noreply.github.com>
2020-10-06 14:39:06 +0530

    Update Dfs.java

--- DataStructures/Graphs/Dfs.java
+++ DataStructures/Graphs/Dfs.java
@@ -1,33 +1,31 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 
-class Dfs
-{
+class Dfs {
+    LinkedList<Integer>[] adjacencyList;
+    boolean[] visited;
     private int vertices;
-    LinkedList<Integer> adjacencyList[];
     private List<Integer> visitedNodes = new ArrayList<Integer>();
-    boolean visited[];
 
-    Dfs(int v)
-    {
+    Dfs(int v) {
         //create adjacencyList object
         adjacencyList = new LinkedList[v]; //length of the nodes;
         vertices = v; //to reuse v
-        for(int i=0;i<v;i++)
-        {
+        for (int i = 0; i < v; i++) {
             adjacencyList[i] = new LinkedList<Integer>(); //create a node for each vertex
         }
         visited = new boolean[vertices];
     }
-    void addEdge(int source, int target)
-    {
+
+    void addEdge(int source, int target) {
         adjacencyList[source].add(target);//find the source node and add a link
         //System.out.println(adjacencyList[source]); // shows all by=ut first node
     }
 
-    void dfs(int source)
-    {
+    void dfsTraversal(int source) {
         visited[source] = true;
         visitedNodes.add(source);
 
@@ -35,19 +33,17 @@ class Dfs
 
         Iterator<Integer> node = adjacencyList[source].listIterator();
 
-        while(node.hasNext())
-        {
+        while (node.hasNext()) {
             source = node.next();
-            if(!visited[source])
-                dfs(source);
+            if (!visited[source])
+                dfsTraversal(source);
         }
 
         //visit unrelated nodes
 
-        for(int i = 0;i<vertices;i++)
-        {
-            if(!visited[i])
-                dfs(i);
+        for (int i = 0; i < vertices; i++) {
+            if (!visited[i])
+                dfsTraversal(i);
         }
     }
 
