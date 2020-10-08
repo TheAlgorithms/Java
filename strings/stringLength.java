da453480ec465c21d0ada0ca44088f7828e245fb
jainath875 <54453129+jainath875@users.noreply.github.com>
2020-10-08 09:00:33 +0530

    Create stringLength.java
    string Length in java

---
+++ strings/stringLength.java
@@ -0,0 +1,7 @@
+public class LengthExample{  
+public static void main(String args[]){  
+String s1="javatpoint";  
+String s2="python";  
+System.out.println("string length is: "+s1.length());//10 is the length of javatpoint string  
+System.out.println("string length is: "+s2.length());//6 is the length of python string  
+}}  
