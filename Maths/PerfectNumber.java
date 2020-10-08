de993a541ae4fd159f1a29ef8cc93d30df7a44c7
akxt1022 <ashishtantiya@gmail.com>
2020-10-08 16:31:44 +0530

    Fix perfect number logic

--- Maths/PerfectNumber.java
+++ Maths/PerfectNumber.java
@@ -23,7 +23,7 @@ public class PerfectNumber {
      */
     public static boolean isPerfectNumber(int number) {
         int sum = 0;  /* sum of its positive divisors */
-        for (int i = 1; i < number; ++i) {
+        for (int i = 1; i <= number/2; ++i) {
             if (number % i == 0) {
                 sum += i;
             }
