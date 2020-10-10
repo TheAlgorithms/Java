51bcbef9a6f383c46ecf36436618d47d4ef48517
Shruti Sharma <56972234+sj056@users.noreply.github.com>
2020-10-10 09:59:54 +0530

    Updated Anagram.java

--- strings/Anagram.java
+++ strings/Anagram.java
@@ -1,34 +1,40 @@
 import java.util.Scanner;
+
 public class Anagram {
 
 	public static void main(String[] args) {
+		
 		Scanner scan=new Scanner(System.in);
+		//Taking the input from the user
 		String a=scan.nextLine();
 		String b=scan.nextLine();
-		 boolean ana=true;
-	      int[] u=new int[256];
-	      int[] v=new int[256];
-	      a=a.toLowerCase();
-	      b=b.toLowerCase();
+		
+		boolean ana=true;
 	
-	      for(char c : a.toCharArray())
-	      {
+	        int[] u=new int[256];
+	        int[] v=new int[256];
+		
+	        a=a.toLowerCase();
+	        b=b.toLowerCase();
+	
+	        for(char c : a.toCharArray())
+	        {
 	          int i=(int)c;
 	          u[i]++;
-	      }
-	       for(char c : b.toCharArray())
-	      {
+	        }
+	        for(char c : b.toCharArray())
+	        {
 	          int i=(int)c;
 	          v[i]++;
-	      }
-	      for(int j=0;j<256;j++)
-	      {
-	       if(u[j]!=v[j])
-	       {
+	        }
+	        for(int j=0;j<256;j++)
+	        {
+	         if(u[j]!=v[j])
+	         {
 	           ana=false;
 	           break;
-	       }
-	      }
-	      System.out.println(ana);
+	         }
+		}
+	        System.out.println(ana);
 	}
-}
+ }
