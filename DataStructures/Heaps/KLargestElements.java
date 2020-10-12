4af90c14f555ad79be3e92611002f768b0c211e3
Stuti Jain <41702974+JainStuti25@users.noreply.github.com>
2020-10-12 22:58:23 +0530

    Create KLargestElements.java

---
+++ DataStructures/Heaps/KLargestElements.java
@@ -0,0 +1,35 @@
+//Hactober Fest 2020
+
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class KLargestElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i =0;i< arr.length;i++){
+            if (i<k){
+                pq.add(arr[i]);
+            }
+            else {
+                if (arr[i]> pq.peek()){
+                    pq.remove();
+                    pq.add(arr[i]);
+                }
+            }
+        }
+
+        while (pq.size()>0){
+            System.out.println(pq.remove());
+        }
+
+    }
+
+}
