a9293ebe71dc1286ca50b652112583f6c1e2eb8e
Nightwatch-Fox11 <56548742+Nightwatch-Fox11@users.noreply.github.com>
2020-10-13 14:12:42 +0800

    Update MD5.java

--- ciphers/MD5.java
+++ ciphers/MD5.java
@@ -11,7 +11,6 @@ public class MD5Utils {
             md.update(str.getBytes());
             // digest() returns the MD5 hash value, which is an 8-bit string, this is because the MD5 hash value is a 16 bit hex value, it is actually an 8-bit character
             // The function BigInteger converts an 8-bit string into a 16 bit hex value, which is represented by a string, and gets a hash value in the form of a string
-            //一个byte是八位二进制，也就是2位十六进制字符（2的8次方等于16的2次方）
             return new BigInteger(1, md.digest()).toString(16);
         } catch (Exception e) {
             e.printStackTrace();
