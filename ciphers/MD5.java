03070b3a382f7756b9f5a33592659585f7ed0a29
Nightwatch-Fox11 <56548742+Nightwatch-Fox11@users.noreply.github.com>
2020-10-13 14:14:14 +0800

    Update MD5.java
    A simple implementation of MD5.

--- ciphers/MD5.java
+++ ciphers/MD5.java
@@ -9,7 +9,7 @@ public class MD5Utils {
             MessageDigest md = MessageDigest.getInstance("MD5");
             // Calculating MD5 functions
             md.update(str.getBytes());
-            // digest() returns the MD5 hash value, which is an 8-bit string, this is because the MD5 hash value is a 16 bit hex value, it is actually an 8-bit character
+            // digest() returns the MD5 hash value, which is an 8-bit string, this is because the MD5 hash value is a 16 bit hex value, it's actually an 8-bit character
             // The function BigInteger converts an 8-bit string into a 16 bit hex value, which is represented by a string, and gets a hash value in the form of a string
             return new BigInteger(1, md.digest()).toString(16);
         } catch (Exception e) {
