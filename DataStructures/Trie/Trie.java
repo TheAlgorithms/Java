79497df25f2f4a6a94a3967f0cf09e04fd667878
Priya Shrinithi <47693475+PriyaShrinithi@users.noreply.github.com>
2020-10-16 17:46:46 +0530

    Add files via upload

--- DataStructures/Trie/Trie.java
+++ DataStructures/Trie/Trie.java
@@ -1,1 +1,57 @@
+package com.company;
+
+class Trie
+{
+    private TrieNode root;
+    class TrieNode
+    {
+        boolean EndOfWord;
+        TrieNode [] child = new TrieNode[26];
+    }
+    public Trie()
+    {
+        root = new TrieNode();
+    }
+
+    public void insert(String word)
+    {
+        TrieNode crawl = root;
+        for(int i = 0;i<word.length();i++)
+        {
+            int i_i = word.charAt(i)-'a'; //returns difference of ascii
+            if(crawl.child[i_i]==null)
+                crawl.child[i_i] = new TrieNode();
+            crawl = crawl.child[i_i];
+        }
+        crawl.EndOfWord = true;
+    }
+
+    public boolean search(String word)
+    {
+        TrieNode crawl = root;
+        for(int i = 0;i<word.length();i++)
+        {
+            int i_i = word.charAt(i)-'a'; //returns difference of ascii
+            if(crawl.child[i_i]==null)
+                return false;
+            crawl = crawl.child[i_i];
+        }
+        return crawl.EndOfWord;
+    }
+
+
+    public boolean startsWith(String prefix)
+    {
+        TrieNode crawl = root;
+        for(int i = 0;i<prefix.length();i++)
+        {
+            int i_i = prefix.charAt(i)-'a'; //returns difference of ascii
+            if(crawl.child[i_i]==null)
+                return false;
+            crawl = crawl.child[i_i];
+        }
+        return true;
+    }
+}
+
 
