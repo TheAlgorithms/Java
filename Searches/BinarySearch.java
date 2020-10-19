d3a6b9bf013db1ba4b45fe4f53d19750ab9d3566
Pratham-123 <54859695+Pratham-123@users.noreply.github.com>
2020-10-19 13:48:22 +0530

    New Commit

--- Searches/BinarySearch.java
+++ Searches/BinarySearch.java
@@ -7,54 +7,20 @@ import java.util.stream.IntStream;
 
 import static java.lang.String.format;
 
-/**
- *
- *
- *
- * Binary search is one of the most popular algorithms
- * The algorithm finds the position of a target value within a sorted array
- *
- * Worst-case performance	O(log n)
- * Best-case performance	O(1)
- * Average performance	O(log n)
- * Worst-case space complexity	O(1)
- *
- *
- * @author Varun Upadhyay (https://github.com/varunu28)
- * @author Podshivalov Nikita (https://github.com/nikitap492)
- *
- * @see SearchAlgorithm
- * @see IterativeBinarySearch
- *
- */
+
 
 class BinarySearch implements SearchAlgorithm {
 
-    /**
-     *
-     * @param array is an array where the element should be found
-     * @param key is an element which should be found
-     * @param <T> is any comparable type
-     * @return index of the element
-     */
+   
     @Override
     public  <T extends Comparable<T>> int find(T[] array, T key) {
         return search(array, key, 0, array.length);
     }
 
-    /**
-     * This method implements the Generic Binary Search
-     *
-     * @param array The array to make the binary search
-     * @param key The number you are looking for
-     * @param left The lower bound
-     * @param right The  upper bound
-     * @return the location of the key
-     **/
     private <T extends Comparable<T>> int search(T array[], T key, int left, int right){
         if (right < left) return -1; // this means that the key not found
 
-        // find median
+       
         int median = (left + right) >>> 1;
         int comp = key.compareTo(array[median]);
 
@@ -67,9 +33,9 @@ class BinarySearch implements SearchAlgorithm {
         }
     }
 
-    // Driver Program
+   
     public static void main(String[] args) {
-        // Just generate data
+       
         Random r = ThreadLocalRandom.current();
 
         int size = 100;
