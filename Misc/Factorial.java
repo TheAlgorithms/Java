f6125e549bcf213d54a1d49b7d72fa292c0b96d6
Supuna Kaumada <malliyawadusupuna@gmail.com>
2020-10-20 14:03:47 +0530

    Added Factorial calculation algorithm
    Added Factorial calculation algorithm

---
+++ Misc/Factorial.java
@@ -0,0 +1,15 @@
+class Factorial {
+
+    static int factorial( int n ) {
+        if (n != 0)  // termination condition
+            return n * factorial(n-1); // recursive call
+        else
+            return 1;
+    }
+
+    public static void main(String[] args) {
+        int number = 4, result;
+        result = factorial(number);
+        System.out.println(number + " factorial = " + result);
+    }
+}
