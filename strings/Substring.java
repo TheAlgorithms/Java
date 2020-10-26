8c8a6a66f6f0cfc6afa7fcdf0e632085a5ba3498
pardaz-banu <45086957+pardaz-banu@users.noreply.github.com>
2020-10-26 08:35:09 +0530

    Update Substring.java

--- strings/Substring.java
+++ strings/Substring.java
@@ -4,11 +4,12 @@ import java.math.*;
 import java.text.*;
 public class Substring
 {
-public static void main(String[] args){
-Scanner sc = new Scanner(System.in);
-String st = sc.next();
-int start = sc.nextInt();
-int end = sc.nextInt();
-System.out.println(st.substring(start, end));
-}
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    String st = sc.next();
+    int start = sc.nextInt();
+    int end = sc.nextInt();
+    System.out.println(st.substring(start, end));
+  }
 }
