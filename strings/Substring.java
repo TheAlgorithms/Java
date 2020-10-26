fc760f55168a9c8b9afcb12a15936a37e4c01541
pardaz-banu <45086957+pardaz-banu@users.noreply.github.com>
2020-10-26 08:31:41 +0530

    Add files via upload

---
+++ strings/Substring.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.text.*;
+public class Substring
+{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+String st = sc.next();
+int start = sc.nextInt();
+int end = sc.nextInt();
+System.out.println(st.substring(start, end));
+}
+}
