6c60265d8128211ec293e3d5ea785225d7600a1a
iamhitman21 <mkdecent21@gmail.com>
2020-10-27 21:01:15 +0530

    Impoved code

---
+++ DataStructures/Stacks/NextGreaterElement.java
@@ -0,0 +1,42 @@
+package DataStructures.Stacks;
+
+import java.util.Stack;
+
+public class NextGreaterElement{ 
+
+	public static void printNGE(int arr[], int n) { 
+		int i = 0; 
+		Stack<Integer> s = new Stack(); 
+		int element, next; 
+
+		s.push(arr[0]); 
+		for (i = 1; i < n; i++) { 
+			next = arr[i]; 
+			if (s.isEmpty() == false) { 
+				element = s.pop(); 
+				while (element < next) { 
+					System.out.println(element + " --> " + next); 
+					if (s.isEmpty() == true) 
+						break; 
+					element = s.pop(); 
+				} 
+				if (element > next) {
+					s.push(element); 
+                }    
+			} 
+			s.push(next); 
+		} 
+
+		while (s.isEmpty() == false) { 
+			element = s.pop(); 
+			next = -1; 
+			System.out.println(element + " -- " + next); 
+		} 
+	} 
+
+	public static void main(String[] args) { 
+		int arr[] = { 11, 13, 21, 3 }; 
+		int n = arr.length; 
+		printNGE(arr, n); 
+	} 
+} 
