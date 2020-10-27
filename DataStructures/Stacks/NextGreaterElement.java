17e566412b9f7b49bd338e25d91a8e0acde9a922
iamhitman21 <mkdecent21@gmail.com>
2020-10-27 22:37:00 +0530

    improved code format

--- DataStructures/Stacks/NextGreaterElement.java
+++ DataStructures/Stacks/NextGreaterElement.java
@@ -3,12 +3,10 @@ package DataStructures.Stacks;
 import java.util.Stack;
 
 public class NextGreaterElement{ 
-
 	public static void printNGE(int arr[], int n) { 
 		int i = 0; 
 		Stack<Integer> s = new Stack(); 
 		int element, next; 
-
 		s.push(arr[0]); 
 		for (i = 1; i < n; i++) { 
 			next = arr[i]; 
@@ -26,7 +24,6 @@ public class NextGreaterElement{
 			} 
 			s.push(next); 
 		} 
-
 		while (s.isEmpty() == false) { 
 			element = s.pop(); 
 			next = -1; 
@@ -39,4 +36,5 @@ public class NextGreaterElement{
 		int n = arr.length; 
 		printNGE(arr, n); 
 	} 
-} 
+}
+ 
