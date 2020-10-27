625c15934b17c230b157810d7dc8ff58fc9a2088
Cole Polyak <colepolyak@gmail.com>
2020-10-27 10:05:11 -0600

    Tweaked Syntax

--- Others/FisherYatesShuffle.java
+++ Others/FisherYatesShuffle.java
@@ -10,10 +10,12 @@ import java.util.Random;
  */
 class FisherYates {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         System.out.println("Array before shuffling");
-        for(Integer i : arr) System.out.print(i + ", ");
+        for (Integer i : arr) {
+            System.out.print(i + ", ");
+        }
         System.out.println("");
 
         // Begin Fisher-Yates
@@ -22,7 +24,7 @@ class FisherYates {
 
         Random rng = new Random();
 
-        while(switchIndex != 0) {
+        while (switchIndex != 0) {
             roll = rng.nextInt(switchIndex);
             int temp = arr[roll];
             arr[roll] = arr[switchIndex];
@@ -33,6 +35,9 @@ class FisherYates {
         // End Fisher-Yates
 
         System.out.println("Array after shuffling");
-        for(Integer i : arr) System.out.print(i + ", ");
+        for (Integer i : arr) {
+            System.out.print(i + ", ");
+        }
     }
 }
+
