d0b43db438ea4343ab958c6da6c578b7313ea6be
icoder211 <58630967+icoder211@users.noreply.github.com>
2020-11-07 18:58:01 +0530

    Using manual loop to fill DistanceMatrix with 0s

--- DataStructures/Graphs/FloydWarshall.java
+++ DataStructures/Graphs/FloydWarshall.java
@@ -14,7 +14,11 @@ public class FloydWarshall {
             [numberofvertices
                 + 1]; // stores the value of distance from all the possible path form the source
     // vertex to destination vertex
-    Arrays.fill(DistanceMatrix, 0);
+    for(int i = 0;i < numberofvertices + 1; i++) {
+      for(int j = 0;j < numberofvertices + 1; j++) {
+        DistanceMatrix[i][j] = 0;
+      }
+    }
     this.numberofvertices = numberofvertices;
   }
 
