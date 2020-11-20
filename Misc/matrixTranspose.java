9cdaa2aced94a7d7225b448a901cc936938e4375
Rajat Jain <${GITHUB_ACTOR}@users.noreply.github.com>
2020-11-20 20:10:55 +0530

    Formatted with Google Java Formatter

--- Misc/matrixTranspose.java
+++ Misc/matrixTranspose.java
@@ -1,21 +1,23 @@
 package Misc;
 import java.util.*;
 public class matrixTranspose {
-
-	
 /** @author RajatJain */
-// It takes input from user and transpoase the matrix
+// It takes input from user and transpose the matrix
 	public static void main(String[] args) {
+		//Declartion of array
 		int a[][]=new int[3][3];
+        
 		Scanner sc=new Scanner(System.in);
+		// Declartion of variables 
 		int i,j;
 		System.out.println("Take input for forming a matrix");
 		for(i=0;i<3;i++) {
 			for(j=0;j<3;j++) 
 			{
+// Take input from user
 				a[i][j]=sc.nextInt();
-			}
-		}
+			} 
+	         	}
 		System.out.println("Matrix before transpose:");
 		for(i=0;i<3;i++) {
 			for(j=0;j<3;j++) 
