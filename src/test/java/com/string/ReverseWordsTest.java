f70f03df32199adf233e5528fca5061e06ac7182
Nishant <nishant.jai@outlook.com>
2020-12-07 17:25:27 +0530

    Formatted ReverseWords.java by creating static functions

--- src/test/java/com/string/ReverseWordsTest.java
+++ src/test/java/com/string/ReverseWordsTest.java
@@ -7,9 +7,8 @@ class ReverseWordsTest {
 
     @Test
     void testReverseWords() {
-        ReverseWords reverseWords = new ReverseWords();
-        Assertions.assertEquals("siht si ym rac", reverseWords.returnReverseWords("this is my car"), "Words reversed correctly");
-        Assertions.assertEquals("CBA 321", reverseWords.returnReverseWords("ABC 123"), "Words reversed correctly");
+        Assertions.assertEquals("siht si ym rac", ReverseWords.returnReverseWords("this is my car"), "Words reversed correctly");
+        Assertions.assertEquals("CBA 321", ReverseWords.returnReverseWords("ABC 123"), "Words reversed correctly");
 
     }
 }
