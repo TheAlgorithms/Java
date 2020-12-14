add0a05f9314b26ffd87eba3533beaae822a3b82
kshivam953 <kshivam953@gmail.com>
2020-12-14 14:28:57 +0530

    Optimize way of Anagrams

--- strings/CheckAnagrams.java
+++ strings/CheckAnagrams.java
@@ -1,32 +1,42 @@
 package strings;
 
-import java.util.Arrays;
 
-/**
- * Two strings are anagrams if they are made of the same letters arranged differently (ignoring the
- * case).
- */
-public class CheckAnagrams {
-  public static void main(String[] args) {
-    assert isAnagrams("Silent", "Listen");
-    assert isAnagrams("This is a string", "Is this a string");
-    assert !isAnagrams("There", "Their");
-  }
 
-  /**
-   * Check if two strings are anagrams or not
-   *
-   * @param s1 the first string
-   * @param s2 the second string
-   * @return {@code true} if two string are anagrams, otherwise {@code false}
-   */
-  public static boolean isAnagrams(String s1, String s2) {
-    s1 = s1.toLowerCase();
-    s2 = s2.toLowerCase();
-    char[] values1 = s1.toCharArray();
-    char[] values2 = s2.toCharArray();
-    Arrays.sort(values1);
-    Arrays.sort(values2);
-    return new String(values1).equals(new String(values2));
-  }
+public class Anagram {
+
+	public static void main(String[] args) {
+		String a = "aac321@#";
+		String b = "aac123#@";
+		
+		boolean isAnagram = true;
+		
+		int al[] = new int[256];
+		int bl[] = new int[256];
+		
+		for(char c: a.toCharArray()){
+			int index = (int) c;
+			al[index]++;
+		}
+		
+		for( char c: b.toCharArray()){
+			int index = (int) c;
+			bl[index]++;
+		}
+		for(int i = 0;i<256;i++) {
+			if(al[i] != bl[i]) {
+				isAnagram = false;
+				break;
+			}
+		}
+		if(isAnagram) {
+			System.out.println("Anagram");
+		}
+		else {
+			System.out.println("Not Anagram");
+		}
+		
+		
+		
+	}
+
 }
