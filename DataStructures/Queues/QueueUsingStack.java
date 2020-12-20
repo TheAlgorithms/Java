7eedfcbda49e4c39e4e63fd24ce8d1da264b870f
munikatoch <munikatoch@gmail.com>
2020-12-20 22:06:26 +0530

    Added references and formatted code

--- DataStructures/Queues/QueueUsingStack.java
+++ DataStructures/Queues/QueueUsingStack.java
@@ -1,39 +1,42 @@
 package DataStructures.Queues;
+
 import java.util.*;
 
 public class QueueUsingStack {
     private Stack<Integer> stack1;
     private Stack<Integer> stack2;
     private int front;
+
     public QueueUsingStack() {
         stack1 = new Stack<Integer>();
         stack2 = new Stack<Integer>();
     }
-    
+
     public void push(int x) {
-        if(stack1.isEmpty()) {
+        if (stack1.isEmpty()) {
             front = x;
         }
         stack1.push(x);
     }
-    
+
     public int pop() {
-        if(stack2.isEmpty()) {
-            while(!stack1.isEmpty()) {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
                 stack2.push(stack1.pop());
             }
         }
         return stack2.pop();
     }
-    
+
     public int peek() {
-        if(!stack2.isEmpty()) {
+        if (!stack2.isEmpty()) {
             return stack2.peek();
         }
         return front;
     }
-    
+
     public boolean empty() {
         return stack1.isEmpty() && stack2.isEmpty();
     }
 }
+// https://leetcode.com/problems/implement-queue-using-stacks/solution/
