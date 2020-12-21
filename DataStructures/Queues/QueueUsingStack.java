d3ab92a7bdef38fa7180034dcf220b7cd469e6c2
munikatoch <munikatoch@gmail.com>
2020-12-21 08:37:49 +0530

    Did some code formatting

--- DataStructures/Queues/QueueUsingStack.java
+++ DataStructures/Queues/QueueUsingStack.java
@@ -1,42 +1,42 @@
 package DataStructures.Queues;
 
-import java.util.*;
+import java.util.Stack;
 
 public class QueueUsingStack {
-    private Stack<Integer> stack1;
-    private Stack<Integer> stack2;
-    private int front;
+  private Stack<Integer> stack1;
+  private Stack<Integer> stack2;
+  private int front;
 
-    public QueueUsingStack() {
-        stack1 = new Stack<Integer>();
-        stack2 = new Stack<Integer>();
-    }
+  public QueueUsingStack() {
+    stack1 = new Stack<Integer>();
+    stack2 = new Stack<Integer>();
+  }
 
-    public void push(int x) {
-        if (stack1.isEmpty()) {
-            front = x;
-        }
-        stack1.push(x);
+  public void push(int x) {
+    if (stack1.isEmpty()) {
+      front = x;
     }
+    stack1.push(x);
+  }
 
-    public int pop() {
-        if (stack2.isEmpty()) {
-            while (!stack1.isEmpty()) {
-                stack2.push(stack1.pop());
-            }
-        }
-        return stack2.pop();
+  public int pop() {
+    if (stack2.isEmpty()) {
+      while (!stack1.isEmpty()) {
+        stack2.push(stack1.pop());
+      }
     }
+    return stack2.pop();
+  }
 
-    public int peek() {
-        if (!stack2.isEmpty()) {
-            return stack2.peek();
-        }
-        return front;
+  public int peek() {
+    if (!stack2.isEmpty()) {
+      return stack2.peek();
     }
+    return front;
+  }
 
-    public boolean empty() {
-        return stack1.isEmpty() && stack2.isEmpty();
-    }
+  public boolean empty() {
+    return stack1.isEmpty() && stack2.isEmpty();
+  }
 }
 // https://leetcode.com/problems/implement-queue-using-stacks/solution/
--- DataStructures/Stacks/StackUsingQueue.java
+++ DataStructures/Stacks/StackUsingQueue.java
@@ -1,32 +1,35 @@
 package DataStructures.Stacks;
 
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class StackUsingQueue {
-    private Queue<Integer> queue;
 
-    public StackUsingQueue() {
-        queue = new LinkedList<Integer>();
-    }
+  private Queue<Integer> queue;
 
-    public void push(int x) {
-        queue.add(x);
-        int size = queue.size();
-        while (size-- > 1) {
-            queue.add(queue.remove());
-        }
-    }
+  public StackUsingQueue() {
+    queue = new LinkedList<Integer>();
+  }
 
-    public int pop() {
-        return queue.remove();
+  public void push(int x) {
+    queue.add(x);
+    int size = queue.size();
+    while (size > 1) {
+      queue.add(queue.remove());
+      size--;
     }
+  }
 
-    public int top() {
-        return queue.peek();
-    }
+  public int pop() {
+    return queue.remove();
+  }
 
-    public boolean empty() {
-        return queue.isEmpty();
-    }
+  public int top() {
+    return queue.peek();
+  }
+
+  public boolean empty() {
+    return queue.isEmpty();
+  }
 }
 // https://leetcode.com/problems/implement-stack-using-queues/solution/
