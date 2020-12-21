3b9115c86da8d4797a8fe3f136b33ce839211118
munikatoch <munikatoch@gmail.com>
2020-12-21 18:02:44 +0000

    Used 4 space indendation

--- Others/StackUsingQueue.java
+++ Others/StackUsingQueue.java
@@ -22,91 +22,91 @@ import java.util.Queue;
  */
 public class StackUsingQueue {
 
-  // Queue that will keep track of elements inserted and removed
-  private Queue<Integer> queue;
+    // Queue that will keep track of elements inserted and removed
+    private Queue<Integer> queue;
 
-  // Init Queue
-  public StackUsingQueue() {
-    queue = new LinkedList<>();
-  }
-
-  /**
-   *
-   * Inserts an item onto the top of the stack.
-   *
-   * @param item - The item to be added onto top of the stack
-   */
-  public void push(int item) {
-    queue.add(item);
-    int size = queue.size();
-    while (size > 1) {
-      queue.add(queue.remove());
-      size--;
+    // Init Queue
+    public StackUsingQueue() {
+        queue = new LinkedList<>();
     }
-  }
 
-  /**
-   * Removes the item at the top of the stack and returns that item as the value
-   * of this function.
-   *
-   * @return The item at the top of the stack
-   * @throws EmptyStackException - If stack is empty
-   */
-  public int pop() {
-    if (!queue.isEmpty()) {
-      return queue.remove();
+    /**
+     *
+     * Inserts an item onto the top of the stack.
+     *
+     * @param item - The item to be added onto top of the stack
+     */
+    public void push(int item) {
+        queue.add(item);
+        int size = queue.size();
+        while (size > 1) {
+            queue.add(queue.remove());
+            size--;
+        }
     }
-    throw new EmptyStackException();
-  }
 
-  /**
-   * Looks at the item at the top of the stack without removing it from the stack.
-   *
-   * @return The item at the top of the stack
-   * @throws EmptyStackException - If stack is empty
-   */
-  public int peek() {
-    if (!queue.isEmpty()) {
-      return queue.peek();
+    /**
+     * Removes the item at the top of the stack and returns that item as the value
+     * of this function.
+     *
+     * @return The item at the top of the stack
+     * @throws EmptyStackException - If stack is empty
+     */
+    public int pop() {
+        if (!queue.isEmpty()) {
+            return queue.remove();
+        }
+        throw new EmptyStackException();
     }
-    throw new EmptyStackException();
-  }
 
-  /**
-   * Tests if this stack is empty.
-   *
-   * @return true if and only if this stack contains no items; false otherwise
-   */
-  public boolean empty() {
-    return queue.isEmpty();
-  }
+    /**
+     * Looks at the item at the top of the stack without removing it from the stack.
+     *
+     * @return The item at the top of the stack
+     * @throws EmptyStackException - If stack is empty
+     */
+    public int peek() {
+        if (!queue.isEmpty()) {
+            return queue.peek();
+        }
+        throw new EmptyStackException();
+    }
 
-  /**
-   * Returns the number of items in the stack.
-   *
-   * @return The number of items in the stack
-   */
-  public int size() {
-    return queue.size();
-  }
+    /**
+     * Tests if this stack is empty.
+     *
+     * @return true if and only if this stack contains no items; false otherwise
+     */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
 
-  /**
-   * Main method.
-   *
-   * @param args Command line arguments
-   */
-  public static void main(String[] args) {
-    int one = 1;
-    int two = 2;
-    int three = 3;
-    StackUsingQueue myStack = new StackUsingQueue();
-    myStack.push(one);
-    myStack.push(two);
-    myStack.push(three);
-    assert myStack.peek() == three;
-    assert myStack.pop() == three;
-    assert myStack.pop() == two;
-    assert !myStack.empty();
-    assert myStack.size() == 1;
-  }
+    /**
+     * Returns the number of items in the stack.
+     *
+     * @return The number of items in the stack
+     */
+    public int size() {
+        return queue.size();
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        StackUsingQueue myStack = new StackUsingQueue();
+        myStack.push(one);
+        myStack.push(two);
+        myStack.push(three);
+        assert myStack.peek() == three;
+        assert myStack.pop() == three;
+        assert myStack.pop() == two;
+        assert !myStack.empty();
+        assert myStack.size() == 1;
+    }
 }
