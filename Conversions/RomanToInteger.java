8e120afe40076260fe8207ba09d7b99c0927c6d6
AstroAnu10 <kulkarni10aniket@gmail.com>
2020-12-31 10:13:24 +0530

    Updated Roman to Integer conversion. Issue with invalid input.

--- Conversions/RomanToInteger.java
+++ Conversions/RomanToInteger.java
@@ -4,63 +4,89 @@ import java.util.*;
 
 public class RomanToInteger {
 
-  private static Map<Character, Integer> map =
-      new HashMap<Character, Integer>() {
-        /** */
-        private static final long serialVersionUID = 87605733047260530L;
+	private static Map<Character, Integer> map = new HashMap<Character, Integer>() {
+		/** */
+		private static final long serialVersionUID = 87605733047260530L;
 
-        {
-          put('I', 1);
-          put('V', 5);
-          put('X', 10);
-          put('L', 50);
-          put('C', 100);
-          put('D', 500);
-          put('M', 1000);
-        }
-      };
-  // Roman Number = Roman Numerals
+		{
+			put('I', 1);
+			put('V', 5);
+			put('X', 10);
+			put('L', 50);
+			put('C', 100);
+			put('D', 500);
+			put('M', 1000);
+		}
+	};
+	private static Map<String, Integer> mapException = new HashMap<String, Integer>() {
+		/** */
+		private static final long serialVersionUID = 87605733047260530L;
 
-  /**
-   * This function convert Roman number into Integer
-   *
-   * @param A Roman number string
-   * @return integer
-   */
-  public static int romanToInt(String A) {
+		{
+			put("IV", 4);
+			put("IX", 9);
+			put("XL", 40);
+			put("XC", 90);
+			put("CD", 400);
+			put("CM", 500);
+		}
+	};
+	// Roman Number = Roman Numerals
 
-    A = A.toUpperCase();
-    char prev = ' ';
+	/**
+	 * This function convert Roman number into Integer
+	 *
+	 * @param A Roman number string
+	 * @return integer
+	 */
+	public static int romanToInt(String A) {
 
-    int sum = 0;
+		A = A.toUpperCase();
 
-    int newPrev = 0;
-    for (int i = A.length() - 1; i >= 0; i--) {
-      char c = A.charAt(i);
+		for (int i = 1; i < A.length(); i++) {
+			if (map.get(A.charAt(i)) > map.get(A.charAt(i - 1))) {
+				if(mapException.containsKey(new String(new char[] { A.charAt(i-1), A.charAt(i) }))) {
+					i++;
+				} else {
+					return -1;
+				}
+			}
+		}
 
-      if (prev != ' ') {
-        // checking current Number greater then previous or not
-        newPrev = map.get(prev) > newPrev ? map.get(prev) : newPrev;
-      }
+		char prev = ' ';
 
-      int currentNum = map.get(c);
+		int sum = 0;
 
-      // if current number greater then prev max previous then add
-      if (currentNum >= newPrev) {
-        sum += currentNum;
-      } else {
-        // subtract upcoming number until upcoming number not greater then prev max
-        sum -= currentNum;
-      }
+		int newPrev = 0;
+		for (int i = A.length() - 1; i >= 0; i--) {
+			char c = A.charAt(i);
 
-      prev = c;
-    }
+			if (prev != ' ') {
+				// checking current Number greater then previous or not
+				newPrev = map.get(prev) > newPrev ? map.get(prev) : newPrev;
+			}
 
-    return sum;
-  }
+			int currentNum = map.get(c);
 
-  public static void main(String[] args) {
-    int sum = romanToInt("MDCCCIV");
-    System.out.println(sum);
+			// if current number greater then prev max previous then add
+			if (currentNum >= newPrev) {
+				sum += currentNum;
+			} else {
+				// subtract upcoming number until upcoming number not greater then prev max
+				sum -= currentNum;
+			}
+
+			prev = c;
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+    int sum = romanToInt("LXXXVIII");
+    if(sum==-1)
+    	System.out.println("Invalid Input");
+    else
+    	System.out.println(sum);
   }
 }
