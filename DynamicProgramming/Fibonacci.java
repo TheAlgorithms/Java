c1f7b28b8fb3151e75730be1f6be0658f7017f2f
Aybars Acar <aybars.dev@gmail.com>
2021-01-02 20:30:04 +1100

    Added 2 new methods
    1. Used a bottom up approach with memoization storing the values in a primitive integer array
    2. Implemented using a matrix multiplication to achieve logarithmic time complexity

--- DynamicProgramming/Fibonacci.java
+++ DynamicProgramming/Fibonacci.java
@@ -88,4 +88,68 @@ public class Fibonacci {
     }
     return res;
   }
+  
+  /**
+   * iterative implementation with memoization
+   * bottom up approach
+   * array is used to store the values
+   *
+   * @param n
+   * @return
+   */
+  public int fibBottomUpUsingArray(int n)
+  {
+    int[] result = new int[n + 1];
+    result[0] = 0;
+    result[1] = 1;
+
+    for (int i = 2; i < result.length; i++)
+    {
+      result[i] = result[i - 1] + result[i - 2];
+    }
+
+    return result[n];
+  }
+  
+  /**
+   * Time Complexity: O(log(n))
+   *
+   * @param n
+   * @return
+   */
+  public int fibMatrix(int n)
+  {
+    int[][] F = new int[][]{{1, 1}, {1, 0}};
+
+    if (n == 0) return 0;
+
+    power(F, n - 1);
+
+    return F[0][0];
+  }
+
+  private void power(int[][] F, int n)
+  {
+    if (n == 0 || n == 1) return;
+
+    int[][] M = new int[][]{{1, 1}, {1, 0}};
+
+    power(F, n / 2);
+    multiply(F, F);
+
+    if (n % 2 != 0) multiply(F, M);
+  }
+
+  private void multiply(int[][] F, int[][] M)
+  {
+    int x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
+    int y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
+    int z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
+    int w = F[1][0] * M[0][1] + F[1][1] * M[1][1];
+
+    F[0][0] = x;
+    F[0][1] = y;
+    F[1][0] = z;
+    F[1][1] = w;
+  }
 }
