4ff1b3053aee80fdf7ad8e8f21884788c0df0115
JackZeled0n <jackiezeledon@gmail.com>
2021-01-03 03:31:46 -0600

    Fix ReverseWords class and test

--- src/test/java/com/string/ReverseWordsTest.java
+++ src/test/java/com/string/ReverseWordsTest.java
@@ -8,8 +8,8 @@ class ReveresWordsTest {
     @Test
     void testReverseWords() {
         ReverseWords reverseWords = new ReverseWords();
-        Assertions.assertEquals(true, reverseWords.returnReverseWords("this is my car"), "siht si ym rac");
-        Assertions.assertEquals(true, reverseWords.returnReverseWords("ABC 123"), "CBA 321");
+        Assertions.assertEquals(true, reverseWords.returnReverseWords("this is my car").equals("siht si ym rac"), "Correct");
+        Assertions.assertEquals(true, reverseWords.returnReverseWords("ABC 123").equals("CBA 321"), "Correct");
 
     }
 }
