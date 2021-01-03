a98c28bff46462c14a20eb1e040eecf89ef39846
Sayar Bhattacharyya <sayarbhattacharyya03@gmail.com>
2021-01-03 11:50:54 +0530

    Added a validity check to RomanToInteger.

--- Conversions/RomanToInteger.java
+++ Conversions/RomanToInteger.java
@@ -30,6 +30,11 @@ public class RomanToInteger {
   public static int romanToInt(String A) {
 
     A = A.toUpperCase();
+    boolean valid = A.matches("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+    if(!valid){
+      
+      return -1;
+    }
     char prev = ' ';
 
     int sum = 0;
@@ -60,7 +65,7 @@ public class RomanToInteger {
   }
 
   public static void main(String[] args) {
-    int sum = romanToInt("MDCCCIV");
-    System.out.println(sum);
+    int sum = romanToInt("IC");
+    System.out.println((sum>=0)?sum:"Invalid Input");
   }
 }
