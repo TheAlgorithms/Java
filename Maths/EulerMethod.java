60b402e4fe629ce4eeccd8e77ef3ed28803c76d1
algobytewise <algobytewise@gmail.com>
2021-03-12 10:14:19 +0530

    Reformatted documentation & implemented tests

--- Maths/EulerMethod.java
+++ Maths/EulerMethod.java
@@ -1,14 +1,19 @@
-/*
-In mathematics and computational science, the Euler method (also called forward Euler method) is a first-order numerical procedure for solving ordinary differential equations (ODEs) with a given initial value. It is the most basic explicit method for numerical integration of ordinary differential equations. The method proceeds in a series of steps. At each step the y-value is calculated by evaluating the differential equation at the previous step, multiplying the result with the step-size and adding it to the last y-value: y_n+1 = y_n + stepSize * f(x_n, y_n).
-(description adapted from https://en.wikipedia.org/wiki/Euler_method )
-(see also: https://www.geeksforgeeks.org/euler-method-solving-differential-equation/ )
-*/
-
 import java.util.ArrayList;
 import java.util.function.BiFunction;
 
+/**
+ * In mathematics and computational science, the Euler method (also called forward Euler method) is
+ * a first-order numerical procedure for solving ordinary differential equations (ODEs) with a given
+ * initial value. It is the most basic explicit method for numerical integration of ordinary
+ * differential equations. The method proceeds in a series of steps. At each step the y-value is
+ * calculated by evaluating the differential equation at the previous step, multiplying the result
+ * with the step-size and adding it to the last y-value: y_n+1 = y_n + stepSize * f(x_n, y_n).
+ * (description adapted from https://en.wikipedia.org/wiki/Euler_method ) (see also:
+ * https://www.geeksforgeeks.org/euler-method-solving-differential-equation/ )
+ */
 public class EulerMethod {
 
+  /** Illustrates how the algorithm is used in 3 examples and prints the results to the console. */
   public static void main(String[] args) {
     System.out.println("example 1:");
     BiFunction<Double, Double, Double> exampleEquation1 = (x, y) -> x;
@@ -34,24 +39,51 @@ public class EulerMethod {
         point -> System.out.println(String.format("x: %1$f; y: %2$f", point[0], point[1])));
   }
 
+  /**
+   * calculates the next y-value based on the current value of x, y and the stepSize the console.
+   *
+   * @param xCurrent Current x-value.
+   * @param stepSize Step-size on the x-axis.
+   * @param yCurrent Current y-value.
+   * @param differentialEquation The differential equation to be solved.
+   * @return The next y-value.
+   */
   public static double eulerStep(
       double xCurrent,
       double stepSize,
       double yCurrent,
       BiFunction<Double, Double, Double> differentialEquation) {
-    // calculates the next y-value based on the current value of x, y and the stepSize
+    if (stepSize <= 0) {
+      throw new IllegalArgumentException("stepSize should be greater than zero");
+    }
     double yNext = yCurrent + stepSize * differentialEquation.apply(xCurrent, yCurrent);
     return yNext;
   }
 
+  /**
+   * Loops through all the steps until xEnd is reached, adds a point for each step and then returns
+   * all the points
+   *
+   * @param xStart First x-value.
+   * @param xEnd Last x-value.
+   * @param stepSize Step-size on the x-axis.
+   * @param yStart First y-value.
+   * @param differentialEquation The differential equation to be solved.
+   * @return The points constituting the solution of the differential equation.
+   */
   public static ArrayList<double[]> eulerFull(
       double xStart,
       double xEnd,
       double stepSize,
       double yStart,
       BiFunction<Double, Double, Double> differentialEquation) {
-    // loops through all the steps until xEnd is reached, adds a point for each step and then
-    // returns all the points
+    if (xStart >= xEnd) {
+      throw new IllegalArgumentException("xEnd should be greater than xStart");
+    }
+    if (stepSize <= 0) {
+      throw new IllegalArgumentException("stepSize should be greater than zero");
+    }
+
     ArrayList<double[]> points = new ArrayList<double[]>();
     double[] firstPoint = {xStart, yStart};
     points.add(firstPoint);
