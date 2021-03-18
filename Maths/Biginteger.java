b91c76610f3e668a0b59df904e89c4afaf75ab8f
vivek286 <solankivivek01@gmail.com>
2021-03-18 22:37:18 +0530

    Issue #1682 Factorial of large number

---
+++ Maths/Biginteger.java
@@ -0,0 +1,26 @@
+package Maths;
+// Java program to find large
+// factorials using BigInteger
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Biginteger {
+    static BigInteger factorial(int N)
+    {
+        // Initialize result
+        BigInteger f
+                = new BigInteger("1"); // Or BigInteger.ONE
+
+        // Multiply f with 2, 3, ...N
+        for (int i = 2; i <= N; i++)
+            f = f.multiply(BigInteger.valueOf(i));
+
+        return f;
+    }
+
+    // Driver method
+    public static void main(String args[]) throws Exception
+    {
+        int N = 50;
+        System.out.println(factorial(N));
+    }
+}
