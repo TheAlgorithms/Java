d43f451a4245c9e8a86676ee5be8f53d67925fce
li2niu <chuanyi@protonmail.com>
2021-04-09 23:41:33 +0800

    improve knapsack code:the default value of an int array element is 0

--- DynamicProgramming/Knapsack.java
+++ DynamicProgramming/Knapsack.java
@@ -9,12 +9,11 @@ public class Knapsack {
     int rv[][] = new int[n + 1][W + 1]; // rv means return value
 
     // Build table rv[][] in bottom up manner
-    for (i = 0; i <= n; i++) {
+    for (i = 0; i < n; i++) {
       for (w = 0; w <= W; w++) {
-        if (i == 0 || w == 0) rv[i][w] = 0;
-        else if (wt[i - 1] <= w)
-          rv[i][w] = Math.max(val[i - 1] + rv[i - 1][w - wt[i - 1]], rv[i - 1][w]);
-        else rv[i][w] = rv[i - 1][w];
+        if (wt[i] <= w)
+          rv[i+1][w] = Math.max(val[i] + rv[i][w - wt[i]], rv[i][w]);
+        else rv[i+1][w] = rv[i][w];
       }
     }
 
