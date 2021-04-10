eadf3b042e12bc4b8bb03cd0fb0964e8263df643
LoopGlitch26 <grandson.baba.2012@gmail.com>
2021-04-10 16:58:22 +0530

    Google Java Format code

--- ProjectEuler/Problem05.java
+++ ProjectEuler/Problem05.java
@@ -4,6 +4,7 @@ Problem 5
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 Answer:232792560
 */
+package ProjectEuler;
 
 public class Problem05 
 {
