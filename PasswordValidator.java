d981b0f803fa4be1b415967261ad7c03cd1620d3
anishsaraogi <asaraogi98@gmail.com>
2021-05-22 13:28:03 +0530

    Create PasswordValidator.java

---
+++ PasswordValidator.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class PasswordValidator
+{
+  public static void main(String args[])
+  {
+    PasswordValidator pv=new PasswordValidtor();
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Input Password: ");
+    String P=sc.nextLine();
+    boolean v=pv.isValidPassword(P);
+    if(v)
+    {
+      System.out.println("\n\nPassword should be more than 5 but less than 20 characters in length.\nPassword should contain at least one number.\nPassword should contain at least one special character.\nPassword should not contain any spaces.\nInvalid Password.\n");
+    }
+    else
+    {
+      System.out.println("\n\nValid Password.\n");
+    }
+    System.out.println("Password is: "+P);
+  }
+  public static boolean isValidPassword(String password)
+  {
+     boolean isValid = true;
+     if (password.length() > 20 || password.length() < 5)
+     {
+       isValid = false;
+     }
+     String numbers = "(.*[0-9].*)";
+     if (!password.matches(numbers))
+     {
+       isValid = false;
+     }
+     String specialChars = "(.*[@,#,$,%].*$)";
+     if (!password.matches(specialChars))
+     {
+       isValid = false;
+     }
+     if (password.matches(" "))
+     {
+       isValid = false;
+     }
+     return isValid;
+  }
+}
