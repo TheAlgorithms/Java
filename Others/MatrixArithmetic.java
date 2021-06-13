f3af0fc2a868604c7607825e4a0437586b9d82ed
ddiaaz <76887398+ddiaaz@users.noreply.github.com>
2021-06-12 20:00:28 -0700

    Update MatrixArithmetic.java

--- Others/MatrixArithmetic.java
+++ Others/MatrixArithmetic.java
@@ -3,83 +3,103 @@
  * 
  * @author Daniel Diaz
  * 
- * Given two matrices, matrix A and matrix B, MatrixArithmetic calculates and prints
+ * Given two matrices, matrix A and matrix B, MatrixArithmetic calculates and tests
  * the sum, difference, and product (dot product) of the two matrices.
  *
  */
 
-public class MatrixArithmetic {
+public class MatrixArithmitic {
 
 	public static void main(String[] args) {
 		System.out.println("Arithmetic Matrix Operations\n");
 		
 		int[][] matrixA = {{1,1,0,1,2}, {2,1,1,1,1}, {1,2,3,1,4}, {3,1,1,2,3}};
 		int[][] matrixB = {{2,3,1,5,5}, {2,1,1,2,1}, {2,2,1,3,4}, {1,2,6,2,4}};
-		
+						
+		Matrix newMatrix = new Matrix(4,5);
+		newMatrix.addMatrix(matrixA);
 		System.out.println("Matrix A:\n");
-		MatrixArithmetic.printMatrix(matrixA);
+		System.out.println(newMatrix);
 		System.out.println("Matrix B:\n");
-		MatrixArithmetic.printMatrix(matrixB);
+		newMatrix.addMatrix(matrixB);
+		System.out.println(newMatrix);
+		newMatrix.addMatrix(matrixA);
+		
+		System.out.println("\nA + B:\n");
+		newMatrix.addMatrices(matrixB);
+		System.out.println("\nA - B:\n");
+		newMatrix.subtractMatrices(matrixB);
+		System.out.println("\nA * B:\n");
+		newMatrix.multiplyMatrices(matrixB);
+	}
+}
 
-		int[][] sum = MatrixArithmetic.addMatrices(matrixA, matrixB);
-		int[][] sub = MatrixArithmetic.subtractMatrices(matrixA, matrixB);
-		int[][] product = MatrixArithmetic.multiplyMatrices(matrixA, matrixB);
-		
-		System.out.println("A + B:\n");
-		MatrixArithmetic.printMatrix(sum);
-		System.out.println("A - B:\n");
-		MatrixArithmetic.printMatrix(sub);
-		System.out.println("A * B:\n");
-		MatrixArithmetic.printMatrix(product);
-		
+class Matrix{
+	private static int rows, columns;
+	private static int[][] matrix;
+	
+	Matrix(int rows, int columns){
+		Matrix.rows = rows;
+		Matrix.columns = columns;
+		matrix = new int[rows][columns];
 	}
 	
-	public static int[][] addMatrices(int[][] matrix1, int[][] matrix2){
-		int[][] resultMatrix = new int[3][4];
+	public void addMatrix(int[][] matrix) {
+		Matrix.matrix = matrix;
+	}
+	
+	public static int getRows() {
+		return rows;
+	}
+	
+	public static int getColumns(){
+		return columns;
+	}
+	
+	public static void addMatrices(int[][] matrix){
+		int[][] resultMatrix = new int[rows][columns];
 		
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 4; j++) {
-				resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < columns; j++) {
+				System.out.print((Matrix.matrix[i][j] + matrix[i][j]) + " ");
 			}
-		}
-		
-		return resultMatrix;
+			System.out.println();
+		}		
 	}
 	
-	public static int[][] subtractMatrices(int[][] matrix1, int[][] matrix2){
-		int[][] resultMatrix = new int[3][4];
-		
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 4; j++) {
-				resultMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
+	public static void subtractMatrices(int[][] matrix){
+		int[][] resultMatrix = new int[rows][columns];
+		for(int i = 0; i < rows; i++) {
+			for(int j = 0; j < columns; j++) {
+				System.out.print((Matrix.matrix[i][j] + (-1)*matrix[i][j]) + " ");
 			}
-		}
-		
-		return resultMatrix;
+			System.out.println();
+		}		
 	}
 	
-	public static int[][] multiplyMatrices(int[][] matrix1, int[][] matrix2){
-		int[][] resultMatrix = new int[3][4];
+	public static void multiplyMatrices(int[][] matrix){
+		int[][] resultMatrix = new int[rows][columns];
 		int sum = 0;
-		for(int row = 0; row < 3; row++) {
-		  for(int i = 0; i < 4; i++) {
-			for(int j = 0; j < 4; j++) {
-				sum += matrix1[i][j]*matrix2[j][i];
+		for(int row = 0; row < rows; row++) {
+		  for(int i = 0; i < columns; i++) {
+			for(int j = 0; j < columns-1; j++) {
+				sum += Matrix.matrix[row][j]*matrix[j][i];
 			}
-			resultMatrix[row][i] = sum;
+			System.out.print(sum + " ");
 			sum = 0;
 		  }
+		  System.out.println();
 		}
-		return resultMatrix;
 	}
 	
-	public static void printMatrix(int[][] matrix) {
-		for(int i = 0; i < 3; i++) {
-	    	for(int j = 0; j < 4; j++) {
-	    		System.out.print(matrix[i][j] + " ");
+	public String toString() {
+		String output = "";
+		for(int i = 0; i < rows; i++) {
+	    	for(int j = 0; j < columns; j++) {
+	    		output += matrix[i][j] + " ";
 	    	}
-	    	System.out.println();
+	    	output += "\n";
+		}
+		return output;
 	}
-		System.out.println();
-  }
 }
