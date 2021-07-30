b58e33c43c2e455a362b4cc4176e9e761b6159a8
Satyam Mittal <satyam101905@gmail.com>
2021-07-29 22:56:36 -0700

    Fixes: #{2269}

--- strings/CheckVowel.java
+++ strings/CheckVowel.java
@@ -15,10 +15,21 @@ public class CheckVowel {
     public static int numberOfVowelsInString(String s){
         int count=0;
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='a' ||s.charAt(i)=='e' ||s.charAt(i)=='i' ||s.charAt(i)=='o' ||s.charAt(i)=='u')
+            if(isVowelChar(s.charAt(i)))
                 count++;
         }
         return count;
+    }
+
+    /**
+     * this method is used to check whether a character is vowel or not
+     *
+     * @param ch
+     * @return
+     */
+    private static boolean isVowelChar(char ch){
+        return ((ch=='a' ||ch=='e' ||ch=='i' ||ch=='o' ||ch=='u')||
+                (ch=='A' ||ch=='E' ||ch=='I' ||ch=='O' ||ch=='U'));
     }
 
     /** Driver Code */
