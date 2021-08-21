de475d9096aaf95cbc103808897273e05161ce86
vennelachowdary11 <89140626+vennelachowdary11@users.noreply.github.com>
2021-08-21 23:18:38 +0530

    balanced brackets

--- DataStructures/Stacks/BalancedBrackets.java
+++ DataStructures/Stacks/BalancedBrackets.java
@@ -1,78 +1,50 @@
-package DataStructures.Stacks;
+package com.java2novice.algos;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
 import java.util.Stack;
-
-/**
- * The nested brackets problem is a problem that determines if a sequence of brackets are properly
- * nested. A sequence of brackets s is considered properly nested if any of the following conditions
- * are true: - s is empty - s has the form (U) or [U] or {U} where U is a properly nested string - s
- * has the form VW where V and W are properly nested strings For example, the string "()()[()]" is
- * properly nested but "[(()]" is not. The function called is_balanced takes as input a string S
- * which is a sequence of brackets and returns true if S is nested and false otherwise.
- *
- * @author akshay sharma
- * @author <a href="https://github.com/khalil2535">khalil2535<a>
- * @author shellhub
- */
-class BalancedBrackets {
-
-  /**
-   * Check if {@code leftBracket} and {@code rightBracket} is paired or not
-   *
-   * @param leftBracket left bracket
-   * @param rightBracket right bracket
-   * @return {@code true} if {@code leftBracket} and {@code rightBracket} is paired, otherwise
-   *     {@code false}
-   */
-  public static boolean isPaired(char leftBracket, char rightBracket) {
-    char[][] pairedBrackets = {
-      {'(', ')'},
-      {'[', ']'},
-      {'{', '}'},
-      {'<', '>'}
-    };
-    for (char[] pairedBracket : pairedBrackets) {
-      if (pairedBracket[0] == leftBracket && pairedBracket[1] == rightBracket) {
-        return true;
-      }
+ 
+public class BalancedBrackets {
+ 
+    static HashMap<Character, Character> bMap;
+ 
+    static {
+        bMap = new HashMap<>();
+        bMap.put('(', ')');
+        bMap.put('[', ']');
+        bMap.put('{', '}');
     }
-    return false;
-  }
-
-  /**
-   * Check if {@code brackets} is balanced
-   *
-   * @param brackets the brackets
-   * @return {@code true} if {@code brackets} is balanced, otherwise {@code false}
-   */
-  public static boolean isBalanced(String brackets) {
-    if (brackets == null) {
-      throw new IllegalArgumentException("brackets is null");
+ 
+    static String isBalanced(String str) {
+ 
+        if ((str.length() % 2) != 0) {
+            return "NO";
+        }
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i < str.length(); i++) {
+            Character ch = str.charAt(i);
+            if(bMap.containsKey(ch)) {
+                stack.push(ch);
+            } else if(stack.isEmpty() || ch != bMap.get(stack.pop())) {
+                return "NO";
+            }
+        }
+ 
+        return stack.isEmpty()? "YES": "NO";
     }
-    Stack<Character> bracketsStack = new Stack<>();
-    for (char bracket : brackets.toCharArray()) {
-      switch (bracket) {
-        case '(':
-        case '[':
-        case '{':
-          bracketsStack.push(bracket);
-          break;
-        case ')':
-        case ']':
-        case '}':
-          if (bracketsStack.isEmpty() || !isPaired(bracketsStack.pop(), bracket)) {
-            return false;
-          }
-          break;
-        default: /* other character is invalid */
-          return false;
-      }
+ 
+    private static final Scanner scanner = new Scanner(System.in);
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for (int tItr = 0; tItr < t; tItr++) {
+            String s = scanner.nextLine();
+            String result = isBalanced(s);
+            System.out.println(result);
+        }
+        scanner.close();
     }
-    return bracketsStack.isEmpty();
-  }
-
-  public static void main(String[] args) {
-    assert isBalanced("[()]{}{[()()]()}");
-    assert !isBalanced("[(])");
-  }
 }
