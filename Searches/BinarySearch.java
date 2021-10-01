42165d58c6e3ee0e159cdd830206df4859ec0265
Ayush Yadav <49021668+201851032-iiitv@users.noreply.github.com>
2021-10-01 22:19:06 +0530

    Update BinarySearch.java

--- Searches/BinarySearch.java
+++ Searches/BinarySearch.java
@@ -1,91 +1,62 @@
-package Searches;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java .util.Arrays;
 
-import static java.lang.String.format;
+public class BinarySearch {
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.IntStream;
+    public static void main(String[] args) {
+int arr[]=new int[1024];
+int mid=(arr.length-1)/2;
+        BinarySearch ob=new BinarySearch();
+        int i=0;
+        try {
+            FileInputStream fstream=new FileInputStream("File 1.txt");
+            DataInputStream in=new DataInputStream(fstream);
+            BufferedReader br=new BufferedReader(new InputStreamReader(in));
+            String s;
+            while((s=br.readLine())!=null)
+            {
+             arr[i]=Integer.parseInt(s);
+                i++;
+            }
+            Arrays.sort(arr);
+            Scanner sc=new Scanner(System.in);
+            int value=arr[mid];
+            
+            long start=System.nanoTime();
+            
+            int p=ob.BinarySearch(arr,value);
+            
+            long end=System.nanoTime();
+            System.out.println((end-start));    
+            
+            System.out.println("number found at : "+p);
+        }
+        catch(Exception e) {
+            System.out.println("error:"+e.getMessage());
+        }
 
-/**
- * Binary search is one of the most popular algorithms The algorithm finds the position of a target
- * value within a sorted array
- *
- * <p>Worst-case performance O(log n) Best-case performance O(1) Average performance O(log n)
- * Worst-case space complexity O(1)
- *
- * @author Varun Upadhyay (https://github.com/varunu28)
- * @author Podshivalov Nikita (https://github.com/nikitap492)
- * @see SearchAlgorithm
- * @see IterativeBinarySearch
- */
-class BinarySearch implements SearchAlgorithm {
+}
 
-  /**
-   * @param array is an array where the element should be found
-   * @param key is an element which should be found
-   * @param <T> is any comparable type
-   * @return index of the element
-   */
-  @Override
-  public <T extends Comparable<T>> int find(T[] array, T key) {
-    return search(array, key, 0, array.length);
-  }
-
-  /**
-   * This method implements the Generic Binary Search
-   *
-   * @param array The array to make the binary search
-   * @param key The number you are looking for
-   * @param left The lower bound
-   * @param right The upper bound
-   * @return the location of the key
-   */
-  private <T extends Comparable<T>> int search(T array[], T key, int left, int right) {
-    if (right < left) return -1; // this means that the key not found
-
-    // find median
-    int median = (left + right) >>> 1;
-    int comp = key.compareTo(array[median]);
-
-    if (comp == 0) {
-      return median;
-    } else if (comp < 0) {
-      return search(array, key, left, median - 1);
-    } else {
-      return search(array, key, median + 1, right);
+int BinarySearch(int[] arr, int key) {
+            int n=arr.length;
+            int low=0;
+            int high=n-1;               
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if(key==arr[mid])
+            return mid;
+        else if(key<arr[mid])
+            high=mid-1;
+        else if(key>arr[mid])
+            low=mid+1;
+            
     }
-  }
-
-  // Driver Program
-  public static void main(String[] args) {
-    // Just generate data
-    Random r = ThreadLocalRandom.current();
-
-    int size = 100;
-    int maxElement = 100000;
-
-    Integer[] integers =
-        IntStream.generate(() -> r.nextInt(maxElement))
-            .limit(size)
-            .sorted()
-            .boxed()
-            .toArray(Integer[]::new);
-
-    // The element that should be found
-    int shouldBeFound = integers[r.nextInt(size - 1)];
-
-    BinarySearch search = new BinarySearch();
-    int atIndex = search.find(integers, shouldBeFound);
-
-    System.out.println(
-        format(
-            "Should be found: %d. Found %d at index %d. An array length %d",
-            shouldBeFound, integers[atIndex], atIndex, size));
-
-    int toCheck = Arrays.binarySearch(integers, shouldBeFound);
-    System.out.println(
-        format(
-            "Found by system method at an index: %d. Is equal: %b", toCheck, toCheck == atIndex));
-  }
+            
+        return -1;
+    }
 }
