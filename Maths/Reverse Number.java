90adb14fa7bd36cee2e46e312d1ce1c825772db6
Ananya Gupta <anannya.gta@gmail.com>
2021-10-01 14:29:59 +0530

    Create Reverse Number.java

---
+++ Maths/Reverse Number.java
@@ -0,0 +1,14 @@
+public class ReverseNumber  
+{  
+public static void main(String[] args)   
+{  
+int number = 987654, reverse = 0;  
+while(number != 0)   
+{  
+int remainder = number % 10;  
+reverse = reverse * 10 + remainder;  
+number = number/10;  
+}  
+System.out.println("The reverse of the given number is: " + reverse);  
+}  
+}  
