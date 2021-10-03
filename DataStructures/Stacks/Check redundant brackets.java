1f56c1db4c792007ee9cfe4e660afdc09f4a78df
Jay <jay0256.cse19@chitkara.edu.in>
2021-10-03 12:52:21 +0530

    Add files via upload

---
+++ DataStructures/Stacks/Check redundant brackets.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+
+	public static boolean checkRedundantBrackets(String e) {
+		//Your code goes here
+        Stack<Character> s = new Stack<>();
+        boolean ans=false;
+        for(int i=0;i<e.length();i++){
+            if(e.charAt(i)=='+'||e.charAt(i)=='-'||e.charAt(i)=='*'||e.charAt(i)=='/'||e.charAt(i)=='('){
+                s.push(e.charAt(i));
+            }else if(e.charAt(i)==')'){
+                if(s.peek()=='(')
+                    ans=true;
+                while(s.peek()=='+'||s.peek()=='-'||s.peek()=='*'||s.peek()=='/'){
+                    s.pop();
+                }
+                s.pop();
+            }
+        }
+        return ans;
+	}
+}
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String expression = s.nextLine();
+    	System.out.println(Solution.checkRedundantBrackets(expression));
+	}
+}
