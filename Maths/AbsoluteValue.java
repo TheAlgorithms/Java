d034aa983f42b23bba1b5cb3c9d3924edc66837b
vaibhavss08 <81666621+vaibhavss08@users.noreply.github.com>
2021-10-03 20:37:10 +0530

    Update AbsoluteValue.java

--- Maths/AbsoluteValue.java
+++ Maths/AbsoluteValue.java
@@ -21,6 +21,6 @@ public class AbsoluteValue {
    * @return the absolute value of a number
    */
   public static int absVal(int value) {
-    return value < 0 ? -value : value;
+    return Math.abs(value);
   }
 }
