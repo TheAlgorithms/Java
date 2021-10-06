dc3a835342a94df8d5d16beb620bd7618dc80f50
19UCS165 Amritesh Anand <amriteshanand19@gmail.com>
2021-10-06 15:11:31 +0000

    some fixes according to convention

--- DynamicProgramming/CatalanNumber.java
+++ DynamicProgramming/CatalanNumber.java
@@ -21,37 +21,38 @@ public class CatalanNumber {
    *          n should be less than equal to 50 as 50th Catalan number is 6,533,841,209,031,609,592
    *          for n > 50, BigInteger class should be used instead long   
    * 
-   * @return catalan[n] 
+   * @return catalanArray[n] the nth Catalan number
    */
-    static long catalanDP(int n){
+    static long findNthCatalan(int n){
 
-        // Table to store the results of subproblems
-        long catalan[] = new long[n + 1];
+        // Array to store the results of subproblems i.e Catalan numbers from [1...n-1]
+        long catalanArray[] = new long[n + 1];
 
         // Initialising C₀ = 1 and C₁ = 1 
-        catalan[0] = 1;
-        catalan[1] = 1;
+        catalanArray[0] = 1;
+        catalanArray[1] = 1;
 
         /**
          * The Catalan numbers satisfy the recurrence relation
-         * C₀=1 and Cn = Σ Ci*Cn-1-i,  i = 0 to n-1 , n > 0 
-         */
+         * C₀=1 and Cn = Σ (Ci * Cn-1-i),  i = 0 to n-1 , n > 0 
+         */ 
         for(int i = 2; i <= n; i++){
-            catalan[i] = 0;
+            catalanArray[i] = 0;
             for(int j = 0; j < i; j++){
-                catalan[i] += catalan[j] * catalan[i - j - 1];
+                catalanArray[i] += catalanArray[j] * catalanArray[i - j - 1];
             }
         }
 
-        return catalan[n];
+        return catalanArray[n];
     }
 
+    // Main method
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the number n to find nth Catalan number (n <= 50)");
         int n = sc.nextInt();
-        System.out.println(n + "th Catalan number is " + catalanDP(n));
+        System.out.println(n + "th Catalan number is " + findNthCatalan(n));
         
         sc.close();
     }
