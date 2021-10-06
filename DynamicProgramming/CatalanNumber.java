514e5db415e65776a34933a9f87f0745fc80336b
19UCS165 Amritesh Anand <amriteshanand19@gmail.com>
2021-10-06 14:43:58 +0000

    Implemented program to find nth Catalan number

---
+++ DynamicProgramming/CatalanNumber.java
@@ -0,0 +1,59 @@
+package DynamicProgramming;
+
+/**
+ * This file contains an implementation of finding the nth CATALAN NUMBER using dynamic programming
+ * Wikipedia: https://en.wikipedia.org/wiki/Catalan_number
+ * 
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(n)
+ * 
+ * @author AMRITESH ANAND (https://github.com/amritesh19) 
+ */
+
+import java.util.Scanner;
+
+public class CatalanNumber {
+
+  /**
+   * This method finds the nth Catalan number
+   *
+   * @param n input n which determines the nth Catalan number
+   *          n should be less than equal to 50 as 50th Catalan number is 6,533,841,209,031,609,592
+   *          for n > 50, BigInteger class should be used instead long   
+   * 
+   * @return catalan[n] 
+   */
+    static long catalanDP(int n){
+
+        // Table to store the results of subproblems
+        long catalan[] = new long[n + 1];
+
+        // Initialising C₀ = 1 and C₁ = 1 
+        catalan[0] = 1;
+        catalan[1] = 1;
+
+        /**
+         * The Catalan numbers satisfy the recurrence relation
+         * C₀=1 and Cn = Σ Ci*Cn-1-i,  i = 0 to n-1 , n > 0 
+         */
+        for(int i = 2; i <= n; i++){
+            catalan[i] = 0;
+            for(int j = 0; j < i; j++){
+                catalan[i] += catalan[j] * catalan[i - j - 1];
+            }
+        }
+
+        return catalan[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number n to find nth Catalan number (n <= 50)");
+        int n = sc.nextInt();
+        System.out.println(n + "th Catalan number is " + catalanDP(n));
+        
+        sc.close();
+    }
+}
+
