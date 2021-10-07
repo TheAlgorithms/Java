818ba2761baa7f9267eb062c2ead363cdb3fd506
Kush Sharma <kushsharma1001@gmail.com>
2021-10-08 00:05:03 +0530

    Update GenericRoot.java

--- Maths/GenericRoot.java
+++ Maths/GenericRoot.java
@@ -1,8 +1,5 @@
 package test;
 
-import java.io.*;
-import java.util.*;
-
 /*
  * Algorithm explanation: https://technotip.com/6774/c-program-to-find-generic-root-of-a-number/#:~:text=Generic%20Root%3A%20of%20a%20number,get%20a%20single%2Ddigit%20output.&text=For%20Example%3A%20If%20user%20input,%2B%204%20%2B%205%20%3D%2015.
  */
