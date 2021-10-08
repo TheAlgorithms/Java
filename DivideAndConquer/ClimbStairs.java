2b25d1a39ab42a7c716349566a81cee9b45895b6
Kush Sharma <kushsharma1001@gmail.com>
2021-10-08 22:15:19 +0530

    Added Solution for climb N stairs problem
    Added a program using divide and conquer to count the number of ways we can climb N stairs when only a given set of steps can be taken each time.

---
+++ DivideAndConquer/ClimbStairs.java
@@ -0,0 +1,25 @@
+// Algorithm Explanation: https://www.geeksforgeeks.org/count-ways-reach-nth-stair/
+class GenericRoot {
+
+	private static int count;
+
+	public static void main(String args[]) {
+		int no = 5; // Nth stair to climb
+		int steps[] = new int[] { 1, 4 }; // number of steps allowed each time.
+		getPathCountToClimbNStairs(no, steps, "");
+		System.out.println(count);
+	}
+
+	public static void getPathCountToClimbNStairs(int n, int allowedStepsAtATime[], String ans) {
+		if (n == 0) {
+			count++;
+			return;
+		}
+
+		for (int item : allowedStepsAtATime) {
+			if (n - item >= 0) {
+				getPathCountToClimbNStairs(n - item, allowedStepsAtATime, ans + item);
+			}
+		}
+	}
+}
