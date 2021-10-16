20a2d4f57c8251a4c7c0c6b2c32921fb461dc1e9
Abhishek Kumar Nayak <62214881+Abhisheknayak040@users.noreply.github.com>
2021-10-16 17:05:15 +0530

    Create OrderAgnosticBS.java

---
+++ Searches/OrderAgnosticBS.java
@@ -0,0 +1,42 @@
+public class OrderAgnosticBS{
+    public static void main(String[] args) {
+//        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr = {99, 80, 75, 22, 11, 10, 5, 2, -3};
+        int target = 22;
+        int ans = orderAgnosticBS(arr, target);
+        System.out.println(ans);
+    }
+
+    static int orderAgnosticBS(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        // find whether the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
