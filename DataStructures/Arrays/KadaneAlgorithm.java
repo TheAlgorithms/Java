959a39cf11e0a3e2fe38d835def04ce3c3c66ac9
Saurabh Dhage <45616010+SaurabhDhage@users.noreply.github.com>
2021-10-17 11:02:50 +0530

    Update KadaneAlgorithm.java

--- DataStructures/Arrays/KadaneAlgorithm.java
+++ DataStructures/Arrays/KadaneAlgorithm.java
@@ -1,1 +1,20 @@
-
+//max sum subarray problem
+public class KadaneAlgorithm {
+    public static void main(String[] args){
+        int[] arr ={-1,-2,-3,-4};
+        int max=0,curr=0,neg=Integer.MIN_VALUE;
+        for (int value : arr) {
+            curr += value;
+            if (curr > max)
+                max = curr;
+            else if (curr < 0)
+                curr = 0;
+            if(neg<value)
+                neg=value;
+        }
+        if(neg>0)
+            System.out.println(max);
+        else
+            System.out.println(neg);
+    }
+}
