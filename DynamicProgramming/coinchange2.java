97faeee97742a3b4070978764ca964b171b84e98
Om prakash <omprakash72810@gmail.com>
2021-10-18 18:21:47 +0530

    Update coinchange2.java

--- DynamicProgramming/coinchange2.java
+++ DynamicProgramming/coinchange2.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.security.KeyStore.Entry;
-import java.util.ArrayList;
+package DynamicProgramming;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -11,12 +8,9 @@ import java.util.Scanner;
 
 public class coinChange2{
     public static void main(String[] args) throws Exception { 
-        Scanner scanner = new Scanner(System.in);
-        int amount=scanner.nextInt();
-        int n=scanner.nextInt();
-        int coins[]=new int[n];
-        for(int i=0; i<n; i++) coins[i]=scanner.nextInt();
-     
+       int amount = 12;
+    int[] coins = {2, 4, 5};
+
       System.out.print(change(amount, coins));
       
     }
@@ -48,3 +42,9 @@ public class coinChange2{
     }
 
 }
+
+
+
+
+
+
