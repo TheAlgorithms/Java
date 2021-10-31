dcdee5e11e2c308cbbe7ddb2333aabe6c3827a5a
Harman Kahlon <lapy113@gmail.com>
2021-10-31 16:46:20 +0530

    Bottom view for tree added

---
+++ DataStructures/Trees/PrintBottomView.java
@@ -0,0 +1,156 @@
+//Initial Template for Java
+package DataStructures.Trees;
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data; //data of the node
+    int hd; //horizontal distance of the node
+    Node left, right; //left and right references
+ 
+    // Constructor of tree node
+    public Node(int key)
+    {
+        data = key;
+        hd = Integer.MAX_VALUE;
+        left = right = null;
+    }
+}
+
+
+
+class PrintBottomView {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	        Solution ob = new Solution();
+			    ArrayList <Integer> res = ob.bottomView(root);
+			    for (Integer num : res) System.out.print (num + " ");
+		     	System.out.println();
+	        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to return a list containing the bottom view of the given tree.
+    
+    public ArrayList <Integer> bottomView(Node root)
+    {   class pair{
+            Node root;
+            int axis;
+            
+            pair(Node b,int a){
+                this.root =b;
+                this.axis =a;
+            }
+        
+        }
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        LinkedList<pair> q = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        q.offer(new pair(root,0));
+        
+        while(!q.isEmpty()){
+            pair t = q.poll();
+            
+            map.put(t.axis,t.root.data);
+            if(t.root.left!=null){
+                q.offer(new pair(t.root.left,t.axis-1));
+            }
+            if(t.root.right!=null){
+                q.offer(new pair(t.root.right,t.axis+1));
+            }
+            
+        }
+        for(int i : map.values()){
+            list.add(i);
+        }
+        return list;
+    }
+}
