b0ccec9d617d921e52d77e4ed88c1ac6a1c1bf0f
Du Yuanchao <duyuanchao.me@gmail.com>
2021-11-03 09:07:59 +0800

    Update bubble sort (#2806)

--- Sorts/BubbleSort.java
+++ Sorts/BubbleSort.java
@@ -18,9 +18,9 @@ class BubbleSort implements SortAlgorithm {
    */
   @Override
   public <T extends Comparable<T>> T[] sort(T[] array) {
-    for (int i = 0, size = array.length; i < size - 1; ++i) {
+    for (int i = 1, size = array.length; i < size; ++i) {
       boolean swapped = false;
-      for (int j = 0; j < size - 1 - i; ++j) {
+      for (int j = 0; j < size - i; ++j) {
         if (greater(array[j], array[j + 1])) {
           swap(array, j, j + 1);
           swapped = true;
