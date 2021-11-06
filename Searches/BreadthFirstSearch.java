9049124991f59921a529bbf30913d5933fb686b6
xzero <andrejmezler@yahoo.de>
2021-11-06 14:39:02 +0100

    Add importing of Node class from DepthFirstSearch

--- Searches/BreadthFirstSearch.java
+++ Searches/BreadthFirstSearch.java
@@ -1,34 +1,17 @@
 package Searches;
 
-import java.util.*;
+import Searches.DepthFirstSearch.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author:  caos321
  * @date:    31 October 2021 (Sunday)
  */
 public class BreadthFirstSearch {
-    static class Node {
-        private final String name;
-        private final List<Node> subNodes;
-
-        public Node(final String name) {
-            this.name = name;
-            this.subNodes = new ArrayList<>();
-        }
-
-        public Node(final String name, final List<Node> subNodes) {
-            this.name = name;
-            this.subNodes = subNodes;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public List<Node> getSubNodes() {
-            return subNodes;
-        }
-    }
 
     public static Optional<Node> search(final Node node, final String name) {
         if (node.getName().equals(name)) {
