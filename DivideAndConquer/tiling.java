9806d61caaf5745a68acc60632127abe8055623f
Kartikay Kapil <77982430+kartikay23@users.noreply.github.com>
2021-11-15 20:24:31 +0530

    Update tiling.java
    Added problem statement and the link to it.

--- DivideAndConquer/tiling.java
+++ DivideAndConquer/tiling.java
@@ -1,3 +1,6 @@
+/* Problem Statement- Given a n by n board where n is of form 2k where k >= 1 (Basically n is a power of 2 with minimum value as 2).
+The board has one missing cell (of size 1 x 1). Fill the board using L shaped tiles. A L shaped tile is a 2 x 2 square with one cell of size 1×1 missing.
+link- [Geeksforgeeks Tiling problem](https://www.geeksforgeeks.org/tiling-problem-using-divide-and-conquer-algorithm/) */
 // Java program to place tiles
 public class tiling
 {
