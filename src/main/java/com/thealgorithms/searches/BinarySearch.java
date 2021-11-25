3b002aba6f334e8491c0ccfe6f3fe8ee5c158c0e
Prashant <prashantyad54321@gmail.com>
2021-11-25 11:36:16 +0530

    remove comment

--- src/main/java/com/thealgorithms/searches/BinarySearch.java
+++ src/main/java/com/thealgorithms/searches/BinarySearch.java
@@ -23,12 +23,7 @@ import com.thealgorithms.devutils.searches.SearchAlgorithm;
  */
 class BinarySearch implements SearchAlgorithm {
 
-    /**
-     * @param array is an array where the element should be found
-     * @param key is an element which should be found
-     * @param <T> is any comparable type
-     * @return index of the element
-     */
+    
     @Override
     public <T extends Comparable<T>> int find(T[] array, T key) {
         return search(array, key, 0, array.length);
