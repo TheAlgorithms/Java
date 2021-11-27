b870de4db474b032b25638eac0fe09283718f063
Yang Libin <szuyanglb@outlook.com>
2021-11-27 18:55:32 +0800

    Fix formatting in Juggler Sequence (#2846)

--- src/main/java/com/thealgorithms/maths/JugglerSequence.java
+++ src/main/java/com/thealgorithms/maths/JugglerSequence.java
@@ -1,5 +1,8 @@
 package com.thealgorithms.maths;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Java program for printing juggler sequence 
  * Wikipedia: https://en.wikipedia.org/wiki/Juggler_sequence
@@ -9,56 +12,40 @@ package com.thealgorithms.maths;
  * */
 
 public class JugglerSequence {
+    /**
+     * This method prints juggler sequence starting with the number in the parameter
+     *
+     * @param inputNumber Number from which juggler sequence is to be started
+     */
+    public static void jugglerSequence(int inputNumber) {
+        // Copy method argument to a local variable
+        int n = inputNumber;
+        List<String> seq = new ArrayList<>();
+        seq.add(n + "");
+        // Looping till n reaches 1
+        while (n != 1) {
+            int temp;
+            // if previous term is even then
+            // next term in the sequence is square root of previous term
+            // if previous term is odd then
+            // next term is floor value of 3 time the square root of previous term
 
+            // Check if previous term is even or odd
+            if (n % 2 == 0) {
+                temp = (int) Math.floor(Math.sqrt(n));
+            } else {
+                temp = (int) Math.floor(Math.sqrt(n) * Math.sqrt(n) * Math.sqrt(n));
+            }
+            n = temp;
+            seq.add(n + "");
+        }
+        String res = String.join(",", seq);
+        System.out.println(res);
+    }
 
-	/**
-	 * This method prints juggler sequence starting with the number in the parameter
-	 *
-	 * @param inputNumber Number from which juggler sequence is to be started
-	 * */
-	static void jugglerSequence(int inputNumber) {
-		//Copy method argument to a local variable
-		int n = inputNumber;
-
-		//Printing first number
-		System.out.print(n+",");
-
-		//Looping till n reaches 1
-		while(n != 1) {
-			int temp=0;
-
-			//if previous term is even then 
-			// next term in the sequence is square root of previous term
-			//if previous term is odd then
-			// next term is floor value of 3 time the square root of previous term
-
-			//Check if previous term is even or odd
-			if(n%2 == 0) {
-				temp = (int) Math.floor(Math.sqrt(n));
-			}
-			else {
-				temp = (int) Math.floor(Math.sqrt(n)*Math.sqrt(n)*Math.sqrt(n));
-			}
-
-			//Printing next term
-			if(temp != 1) {
-				System.out.print(temp+",");
-			}
-			else{
-				System.out.print(temp);
-			}
-
-			n = temp;
-
-		}
-
-	}
-
-	//Driver code
-	public static void main(String[] args) {
-		jugglerSequence(3);
-
-		//Output: 3,5,11,36,6,2,1
-	}
-
+    // Driver code
+    public static void main(String[] args) {
+        jugglerSequence(3);
+        // Output: 3,5,11,36,6,2,1
+    }
 }
