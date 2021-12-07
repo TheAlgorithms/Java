4036173ec32474ed3cf2dcbfb0d038265e4aa947
s-devran <78323181+s-devran@users.noreply.github.com>
2021-12-07 16:21:13 +0300

    Update SwitchingLetters.java
    Added comments.

--- src/main/java/com/thealgorithms/strings/SwitchingLetters.java
+++ src/main/java/com/thealgorithms/strings/SwitchingLetters.java
@@ -1,10 +1,11 @@
+package com.thealgorithms.strings;
+
 import java.util.Scanner;
 
-public class SwitchingLetters
-{
+public class SwitchingLetters {
 	
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
+		
 		Scanner keyboard = new Scanner(System.in);
 
 		System.out.println("Please enter two numbers:");
@@ -21,8 +22,9 @@ public class SwitchingLetters
 		System.out.println("The new sentence:\n" + switchLetters(index1, index2, sentence));
 	}
 
+	/* switchLetters method switches letters according to given indexes. */
+
 	public static String switchLetters(int index1, int index2, String sentence) {
 		return sentence.substring(0,index1) + sentence.charAt(index2) + sentence.substring(index1 + 1, index2) + sentence.charAt(index1) + sentence.substring(index2 + 1, sentence.length());
-
 	}
 }
