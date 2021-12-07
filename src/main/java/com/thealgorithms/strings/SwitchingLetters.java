ce170a0b62f811874a23fca57fab1fbbbe1ad23f
s-devran <78323181+s-devran@users.noreply.github.com>
2021-12-07 16:18:28 +0300

    Add files via upload
    Here is my contribution.

---
+++ src/main/java/com/thealgorithms/strings/SwitchingLetters.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SwitchingLetters
+{
+	
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Please enter two numbers:");
+
+		int index1 = keyboard.nextInt() - 1;
+		int index2 = keyboard.nextInt() - 1;
+
+		keyboard.nextLine();
+
+		System.out.println("Please enter a sentence:");
+
+		String sentence = keyboard.nextLine();
+		
+		System.out.println("The new sentence:\n" + switchLetters(index1, index2, sentence));
+	}
+
+	public static String switchLetters(int index1, int index2, String sentence) {
+		return sentence.substring(0,index1) + sentence.charAt(index2) + sentence.substring(index1 + 1, index2) + sentence.charAt(index1) + sentence.substring(index2 + 1, sentence.length());
+
+	}
+}
