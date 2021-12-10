e07b5f07cf440a99cfb3c7a7aeb7faaae347b53f
sangin.lee <sangin.lee.life@gmail.com>
2021-12-10 13:34:50 +0900

    Separated junit test class of ArrayLeftRotation as ArrayLeftRotationTest.java

--- src/main/java/com/thealgorithms/others/ArrayLeftRotation.java
+++ src/main/java/com/thealgorithms/others/ArrayLeftRotation.java
@@ -1,8 +1,5 @@
 package com.thealgorithms.others;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 /*
  * A left rotation operation on an array
  * shifts each of the array's elements
@@ -13,45 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrayLeftRotation {
 
-	@Test
-	public void testForOneElement() {
-		int[] arr = {3};
-		int[] result = rotateLeft(arr, 3);
-		assertArrayEquals(arr, result);
-	}
-	
-	@Test
-	public void testForZeroStep() {
-		int[] arr = {3, 1, 5, 8, 6};
-		int[] result = rotateLeft(arr, 0);
-		assertArrayEquals(arr, result);
-	}
-	
-	@Test
-	public void testForEqualSizeStep() {
-		int[] arr = {3, 1, 5, 8, 6};
-		int[] result = rotateLeft(arr, 5);
-		assertArrayEquals(arr, result);
-	}
-	
-	@Test
-	public void testForLowerSizeStep() {
-		int[] arr = {3, 1, 5, 8, 6};
-		int n = 2;
-		int[] expected = {5, 8, 6, 3, 1};
-		int[] result = rotateLeft(arr, n);
-		assertArrayEquals(expected, result);
-	}
-	
-	@Test
-	public void testForHigherSizeStep() {
-		int[] arr = {3, 1, 5, 8, 6};
-		int n = 7;
-		int[] expected = {5, 8, 6, 3, 1};
-		int[] result = rotateLeft(arr, n);
-		assertArrayEquals(expected, result);
-	}
-	
 	/*
 	 * Returns the result of left rotation of given array arr and integer n
 	 * 
