a7ce3ceae2a806fbf46c257f38da3c35bbb85c87
s-devran <78323181+s-devran@users.noreply.github.com>
2021-12-14 21:41:33 +0300

    Update KaprekarNumbers.java

--- src/main/java/com/thealgorithms/maths/KaprekarNumbers.java
+++ src/main/java/com/thealgorithms/maths/KaprekarNumbers.java
@@ -2,36 +2,11 @@ package com.thealgorithms.maths;
 
 public class KaprekarNumbers {
 	
-	public static void main(String[] args) {
 
-		/* This program demonstrates if a given number is Kaprekar Number or not.
-		Kaprekar Number: A Kaprekar number is an n-digit number which its square can be split into two parts where the right part has n
-		digits and sum of these parts is equal to the original number. */
+	/* This program demonstrates if a given number is Kaprekar Number or not.
+	Kaprekar Number: A Kaprekar number is an n-digit number which its square can be split into two parts where the right part has n
+	digits and sum of these parts is equal to the original number. */
 
-		// Kaprekar Numbers
-		System.out.println(isKaprekarNumber(1));
-		System.out.println(isKaprekarNumber(9));
-		System.out.println(isKaprekarNumber(45));
-		System.out.println(isKaprekarNumber(55));
-		System.out.println(isKaprekarNumber(99));
-		System.out.println(isKaprekarNumber(297));
-		System.out.println(isKaprekarNumber(703));
-		System.out.println(isKaprekarNumber(999));
-		System.out.println(isKaprekarNumber(2223));
-		System.out.println(isKaprekarNumber(2728));
-		System.out.println(isKaprekarNumber(4879));
-		System.out.println(isKaprekarNumber(5292));
-		System.out.println(isKaprekarNumber(857143));
-
-		// None Kaprekar Numbers
-		System.out.println(isKaprekarNumber(3));
-		System.out.println(isKaprekarNumber(11));
-		System.out.println(isKaprekarNumber(13));
-		System.out.println(isKaprekarNumber(26));
-		System.out.println(isKaprekarNumber(56));
-		System.out.println(isKaprekarNumber(98));
-
-	}
 
 	// Checks whether a given number is Kaprekar Number or not
 
