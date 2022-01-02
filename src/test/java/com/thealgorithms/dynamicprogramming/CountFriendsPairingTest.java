edbdb0025d0d63320f596bc0afcb6a0b3b298775
Deeeeksha <deeksha.tripathi@knoldus.com>
2022-01-02 18:06:34 +0530

    CountFriendsPairing algorithm added with test ccccases

---
+++ src/test/java/com/thealgorithms/dynamicprogramming/CountFriendsPairingTest.java
@@ -0,0 +1,18 @@
+package com.thealgorithms.others;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CountFriendsPairingTest { 
+CountFriendsPairings obj=new CountFriendsPairings(); 
+
+@Test 
+public void test_CountFriendsPairings() { 
+assertEquals(10,obj.countFriendsPairings(4)); 
+}
+@Test 
+public void test_CountFriendsPairings() { 
+assertEquals(,obj.countFriendsPairings(3)); 
+} 
+} 
