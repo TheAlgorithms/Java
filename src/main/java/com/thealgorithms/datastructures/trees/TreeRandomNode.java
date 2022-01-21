69bc948faf4415acca5894582d500b10fda12bfb
Andrii Siriak <siryaka@gmail.com>
2022-01-21 21:18:52 +0200

    Update TreeRandomNode.java

--- src/main/java/com/thealgorithms/datastructures/trees/TreeRandomNode.java
+++ src/main/java/com/thealgorithms/datastructures/trees/TreeRandomNode.java
@@ -69,22 +69,6 @@ public class TreeRandomNode {
         System.out.println("Random Node : " + random);
 
     }
-
-
-    public static void main(String[] args) {
-        TreeRandomNode tree = new TreeRandomNode();
-
-
-        tree.root = new Node(1);
-        tree.root.left = new Node(12);
-        tree.root.right = new Node(9);
-        tree.root.left.left = new Node(5);
-        tree.root.left.right = new Node(6);
-
-        tree.getrandom(tree.root);
-
-
-    }
 }
 
 
