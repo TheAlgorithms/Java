cf6c020fffb4e22ade586ceb1541b86279c52dd7
alexlemo20 <alexlemo@csd.auth.gr>
2022-02-10 09:22:58 +0200

    Removed main, printing methods, added nextfit and testing methods for CPU algorithms.

--- src/main/java/com/thealgorithms/others/CPUalgorithms.java
+++ src/main/java/com/thealgorithms/others/CPUalgorithms.java
@@ -6,13 +6,6 @@ package com.thealgorithms.others;
 import java.util.ArrayList;
 
 public abstract class CPUalgorithms {
-    public static void main (String [] args){
-        CPUalgorithms algorithm = new FirstFitCPU();
-        int [] sizeOfBlocks = {15, 40, 10, 20};
-        int [] sizesOfProcesses = {10, 40, 25, 30};
-        ArrayList <Integer> memoryAllocation = algorithm.fitProcess(sizeOfBlocks, sizesOfProcesses);
-        algorithm.printMemoryAllocation(memoryAllocation);
-    }
 
     /**
      * Method to allocate memory to blocks according to CPU algorithms.
@@ -29,13 +22,6 @@ public abstract class CPUalgorithms {
      */
     public abstract ArrayList<Integer> fitProcess(int[] sizeOfBlocks, int[] sizeOfProcesses);
 
-    /**
-     * Method to print the memory allocated.
-     *
-     * @param memAllocation: an ArrayList of Integer representing the memory
-     * allocation done by the worstFit method.
-     */
-    public abstract void printMemoryAllocation(ArrayList<Integer> memAllocation);
 }
 /**
  * @author Dekas Dimitrios
@@ -119,25 +105,6 @@ class BestFitCPU extends CPUalgorithms{
         return memAlloc;
     }
 
-    /**
-     * Method to print the memory allocated.
-     *
-     * @param memAllocation: an ArrayList of Integer representing the memory
-     * allocation done by the bestFit method.
-     */
-    public void printMemoryAllocation(ArrayList<Integer> memAllocation) {
-        System.out.println("Process No.\tBlock No.");
-        System.out.println("===========\t=========");
-        for (int i = 0; i < memAllocation.size(); i++) {
-            System.out.print(" " + i + "\t\t\t");
-            if (memAllocation.get(i) != NO_ALLOCATION) {
-                System.out.print(memAllocation.get(i));
-            } else {
-                System.out.print("Not Allocated");
-            }
-            System.out.println();
-        }
-    }
 }
 
 /**
@@ -205,25 +172,6 @@ class WorstFitCPU extends CPUalgorithms{
         return memAlloc;
     }
 
-    /**
-     * Method to print the memory allocated.
-     *
-     * @param memAllocation: an ArrayList of Integer representing the memory
-     * allocation done by the worstFit method.
-     */
-    public void printMemoryAllocation(ArrayList<Integer> memAllocation) {
-        System.out.println("Process No.\tBlock No.");
-        System.out.println("===========\t=========");
-        for (int i = 0; i < memAllocation.size(); i++) {
-            System.out.print(" " + i + "\t\t\t");
-            if (memAllocation.get(i) != NO_ALLOCATION) {
-                System.out.print(memAllocation.get(i));
-            } else {
-                System.out.print("Not Allocated");
-            }
-            System.out.println();
-        }
-    }
 }
 
 /**
@@ -283,25 +231,71 @@ class FirstFitCPU extends CPUalgorithms{
         return memAlloc;
     }
 
-    /**
-     * Method to print the memory allocated.
-     *
-     * @param memAllocation: an ArrayList of Integer representing the memory
-     * allocation done by the firstFit method.
-     */
-    public void printMemoryAllocation(ArrayList<Integer> memAllocation) {
-        System.out.println("Process No.\tBlock No.");
-        System.out.println("===========\t=========");
-        for (int i = 0; i < memAllocation.size(); i++) {
-            System.out.print(" " + i + "\t\t\t");
-            if (memAllocation.get(i) != NO_ALLOCATION) {
-                System.out.print(memAllocation.get(i));
-            } else {
-                System.out.print("Not Allocated");
-            }
-            System.out.println();
-        }
-    }
 }
 
+/**
+ * @author Alexandros Lemonaris
+ */
+class NextFit extends CPUalgorithms{
+
+    private static final int NO_ALLOCATION
+            = -255; // if a process has been allocated in position -255,
+    // it means that it has not been actually allocated.
+    private int counter = 0; // variable that keeps the position of the last registration into the memory
+    /**
+     * Method to find the index of the memory block that is going to fit the
+     * given process based on the next fit algorithm. In the case of next fit,
+     * if the search is interrupted in between, the new search is carried out from the last location.
+     *
+     * @param blocks: the array with the available memory blocks.
+     * @param process: the size of the process.
+     * @return the index of the block that fits, or -255 if no such block
+     * exists.
+     */
+    private int findNextFit(int[] blockSizes, int processSize) {
+
+        for (int i = 0; i < blockSizes.length; i++) {
+            if (counter + i >= blockSizes.length){
+                counter = -i; // starts from the start of the array
+            }
+            if (blockSizes[i + counter] >= processSize) {
+                counter += i;
+                return counter;
+            }
+        }
+        // If there is not a block that can fit the process, return -255 as the result
+        counter += blockSizes.length; // counter keeps its last value
+        return NO_ALLOCATION;
+    }
+
+    /**
+     * Method to allocate memory to blocks according to the first fit algorithm.
+     * It should return an ArrayList of Integers, where the index is the process
+     * ID (zero-indexed) and the value is the block number (also zero-indexed).
+     *
+     * @param sizeOfBlocks: an int array that contains the sizes of the memory
+     * blocks available.
+     * @param sizeOfProcesses: an int array that contains the sizes of the
+     * processes we need memory blocks for.
+     * @return the ArrayList filled with Integers repressenting the memory
+     * allocation that took place.
+     */
+    public ArrayList<Integer> fitProcess(int[] sizeOfBlocks, int[] sizeOfProcesses) {
+        // The array list responsible for saving the memory allocations done by the first-fit algorithm
+        ArrayList<Integer> memAlloc = new ArrayList<>();
+        // Do this for every process
+        for (int processSize : sizeOfProcesses) {
+            int chosenBlockIdx
+                    = findNextFit(
+                    sizeOfBlocks, processSize); // Find the index of the memory block going to be used
+            memAlloc.add(chosenBlockIdx); // Store the chosen block index in the memAlloc array list
+            if (chosenBlockIdx
+                    != NO_ALLOCATION) { // Only if a block was chosen to store the process in it,
+                sizeOfBlocks[chosenBlockIdx] -= processSize; // resize the block based on the process size
+            }
+        }
+        return memAlloc;
+    }
+
+}
 
