e47c70940394878bfbf64818fed7ab18e30a7e76
Vivek <vivektheintel@gmail.com>
2022-03-16 00:53:33 +0530

    Refactor

--- src/test/java/com/thealgorithms/maths/ArmstrongTest.java
+++ src/test/java/com/thealgorithms/maths/ArmstrongTest.java
@@ -13,11 +13,12 @@ class ArmstrongTest {
     @Test
     void testisArmStrong() {
         Armstrong armstrong = new Armstrong();
-        assertThat(armstrong.isArmStrong(153)).isTrue();
         assertThat(armstrong.isArmStrong(0)).isTrue();
         assertThat(armstrong.isArmStrong(1)).isTrue();
-        assertThat(armstrong.isArmStrong(1634)).isFalse(); //this proves wrong v1.
+        assertThat(armstrong.isArmStrong(153)).isTrue();
         assertThat(armstrong.isArmStrong(371)).isTrue();
+
+        assertThat(armstrong.isArmStrong(1634)).isFalse();
         assertThat(armstrong.isArmStrong(200)).isFalse();
     }
 }
