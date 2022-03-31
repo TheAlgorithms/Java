63f486c6bae9ef2fb82a593e8ed09e79cc9ef842
Aldo Telese <63289653+aldotele@users.noreply.github.com>
2022-03-31 19:54:48 +0200

    Add tests for CharacterSame (#2989)

---
+++ src/test/java/com/thealgorithms/strings/CharacterSameTest.java
@@ -0,0 +1,28 @@
+package com.thealgorithms.strings;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CharacterSameTest {
+    @Test
+    public void isAllCharactersSame() {
+        String input1 = "aaa";
+        String input2 = "abc";
+        String input3 = "1  1  1  1";
+        String input4 = "111";
+        String input5 = "";
+        String input6 = "           ";
+        String input7 = ".       ";
+
+        assertTrue(CharactersSame.isAllCharactersSame(input1));
+        assertFalse(CharactersSame.isAllCharactersSame(input2));
+        assertFalse(CharactersSame.isAllCharactersSame(input3));
+        assertTrue(CharactersSame.isAllCharactersSame(input4));
+        assertTrue(CharactersSame.isAllCharactersSame(input5));
+        assertTrue(CharactersSame.isAllCharactersSame(input6));
+        assertFalse(CharactersSame.isAllCharactersSame(input7));
+
+    }
+
+}
