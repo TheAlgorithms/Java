d69880e46769687d23e51c880ec97b122cbcd751
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-04-01 00:17:07 +0530

    Fixes(#2889)

--- src/test/java/com/thealgorithms/others/CountFriendsPairingTest.java
+++ src/test/java/com/thealgorithms/others/CountFriendsPairingTest.java
@@ -8,55 +8,55 @@ public class CountFriendsPairingTest {
 	void testForOneElement() 
 	{
         int a[] = {1,2,2};
-		assertTrue(CountFriendsPairing.CountFriendsPairing(3,a));
+		assertTrue(CountFriendsPairing.countFriendsPairing(3,a));
 	}
 
 	@Test
 	void testForTwoElements() 
 	{
         int a[] = {1,2,2,3};
-		assertTrue(CountFriendsPairing.CountFriendsPairing(4,a));
+		assertTrue(CountFriendsPairing.countFriendsPairing(4,a));
 	}
 
 	@Test
 	void testForThreeElements() 
 	{
         int a[] = {1,2,2,3,3};
-		assertTrue(CountFriendsPairing.CountFriendsPairing(5,a));
+		assertTrue(CountFriendsPairing.countFriendsPairing(5,a));
 	}
 
 	@Test
 	void testForFourElements() 
 	{
         int a[] = {1,2,2,3,3,4};
-		assertTrue(CountFriendsPairing.CountFriendsPairing(6,a));
+		assertTrue(CountFriendsPairing.countFriendsPairing(6,a));
 	}
 
 	@Test
 	void testForFiveElements() 
 	{
         int a[] = {1,2,2,3,3,4,4};
-		assertTrue(CountFriendsPairing.CountFriendsPairing(7,a));
+		assertTrue(CountFriendsPairing.countFriendsPairing(7,a));
 	}
 
 	@Test
 	void testForSixElements() 
 	{
         int a[] = {1,2,2,3,3,4,4,4};
-		assertTrue(CountFriendsPairing.CountFriendsPairing(8,a));
+		assertTrue(CountFriendsPairing.countFriendsPairing(8,a));
 	}
 
 	@Test
 	void testForSevenElements() 
 	{
         int a[] = {1,2,2,3,3,4,4,4,5};
-		assertTrue(CountFriendsPairing.CountFriendsPairing(9,a));
+		assertTrue(CountFriendsPairing.countFriendsPairing(9,a));
     }
 
 	@Test
 	void testForEightElements() 
 	{
         int a[] = {1,2,2,3,3,4,4,4,5,5};
-		assertTrue(CountFriendsPairing.CountFriendsPairing(10,a));
+		assertTrue(CountFriendsPairing.countFriendsPairing(10,a));
 	}
 }
