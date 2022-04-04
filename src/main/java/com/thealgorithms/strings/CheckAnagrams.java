79f9eab098896715a87107f84a55f791a0d87de8
acbin <44314231+acbin@users.noreply.github.com>
2022-04-04 16:39:26 +0800

    Update CheckAnagrams.java

--- src/main/java/com/thealgorithms/strings/CheckAnagrams.java
+++ src/main/java/com/thealgorithms/strings/CheckAnagrams.java
@@ -1,53 +1,42 @@
 package com.thealgorithms.strings;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * Two strings are anagrams if they are made of the same letters arranged
- * differently (ignoring the case).
+ * Vowel Count is a system whereby character strings are placed in order based
+ * on the position of the characters in the conventional ordering of an
+ * alphabet. Wikipedia: https://en.wikipedia.org/wiki/Alphabetical_order
  */
-public class CheckAnagrams {
+public class CheckVowels {
+    private static final Set<Character> VOWELS = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
-    public static void main(String[] args) {
-        assert isAnagrams("Silent", "Listen");
-        assert isAnagrams("This is a string", "Is this a string");
-        assert !isAnagrams("There", "Their");
+    /**
+     * Check if a string is has vowels or not
+     *
+     * @param input a string
+     * @return {@code true} if given string has vowels, otherwise {@code false}
+     */
+    public static boolean hasVowels(String input) {
+        return countVowels(input) > 0;
     }
 
     /**
-     * Check if two strings are anagrams or not
+     * count the number of vowels
      *
-     * @param s1 the first string
-     * @param s2 the second string
-     * @return {@code true} if two string are anagrams, otherwise {@code false}
+     * @param input a string prints the count of vowels
      */
-    public static boolean isAnagrams(String s1, String s2) {
-        int l1 = s1.length();
-        int l2 = s2.length();
-        s1 = s1.toLowerCase();
-        s2 = s2.toLowerCase();
-        Map<Character, Integer> charAppearances = new HashMap<>();
-
-        for (int i = 0; i < l1; i++) {
-            char c = s1.charAt(i);
-            int numOfAppearances = charAppearances.getOrDefault(c, 0);
-            charAppearances.put(c, numOfAppearances + 1);
+    public static int countVowels(String input) {
+        if (input == null) {
+            return 0;
         }
-
-        for (int i = 0; i < l2; i++) {
-            char c = s2.charAt(i);
-            if (!charAppearances.containsKey(c)) {
-                return false;
-            }
-            charAppearances.put(c, charAppearances.get(c) - 1);
-        }
-
-        for (int cnt : charAppearances.values()) {
-            if (cnt != 0) {
-                return false;
+        int cnt = 0;
+        for (char c : input.toLowerCase().toCharArray()) {
+            if (VOWELS.contains(c)) {
+                ++cnt;
             }
         }
-        return true;
+        return cnt;
     }
 }
