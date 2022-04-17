1864d5f739743b0558499a2943f0544bc4fb83bb
RaghavTaneja <97575679+RaghavTaneja@users.noreply.github.com>
2022-04-17 12:06:54 -0500

    Area of a triangle using side lengths

---
+++ src/main/java/com/thealgorithms/maths/HeronsFormula.java
@@ -0,0 +1,27 @@
+package com.thealgorithms.maths;
+
+/**
+ * Find the area of a triangle using only side lengths
+ */
+ 
+public class HeronsFormula {
+
+	public static void main(String[] args)
+	{
+		assert Herons(3,4,5) == 6.0;
+		assert Herons(24,30,18) == 216.0;
+		assert Herons(1,1,1) == 0.4330127018922193;
+		assert Herons(4,5,8) == 8.181534085976786;
+	}
+  
+	public static double Herons(int s1, int s2, int s3)
+	{
+		double a = s1;
+		double b = s2;
+		double c = s3;
+		double s = (a + b + c)/2.0;
+		double area = 0;
+		area = Math.sqrt((s)*(s-a)*(s-b)*(s-c));
+		return area;
+	}
+}
