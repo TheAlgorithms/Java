4ce6b8b3d4822af724657b0fb5e1e3f4f24cbf9a
RaghavTaneja <97575679+RaghavTaneja@users.noreply.github.com>
2022-04-19 20:12:07 -0500

    Fixed errors

--- src/main/java/com/thealgorithms/maths/HeronsFormula.java
+++ src/main/java/com/thealgorithms/maths/HeronsFormula.java
@@ -1,7 +1,6 @@
 package com.thealgorithms.maths;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 /**
