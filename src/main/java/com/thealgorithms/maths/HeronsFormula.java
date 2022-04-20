e7d612762d91bcd12e7d6713f4b4713490fd9729
RaghavTaneja <97575679+RaghavTaneja@users.noreply.github.com>
2022-04-19 20:29:01 -0500

    Changed testing method to match similar algorithms in repository

--- src/main/java/com/thealgorithms/maths/HeronsFormula.java
+++ src/main/java/com/thealgorithms/maths/HeronsFormula.java
@@ -1,14 +1,19 @@
 package com.thealgorithms.maths;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-
 /**
- * 
  * Find the area of a triangle using only side lengths
- *
  */
+
 public class HeronsFormula {
+
+	public static void main(String[] args)
+	{
+		assert Double.compare(Herons(3,4,5), 6.0) == 0;
+		assert Double.compare(Herons(24,30,18), 216.0) == 0;;
+		assert Double.compare(Herons(1,1,1), 0.4330127018922193) == 0;
+		assert Double.compare(Herons(4,5,8), 8.181534085976786) == 0;
+	}
+
 	public static double Herons(int s1, int s2, int s3)
 	{
 		double a = s1;
@@ -18,14 +23,5 @@ public class HeronsFormula {
 		double area = 0;
 		area = Math.sqrt((s)*(s-a)*(s-b)*(s-c));
 		return area;
-	}
-	@Test
-	void testAssertions()
-	{
-		assertAll("triangles",
-		        () -> assertEquals(Herons(3,4,5), 6.0),
-		        () -> assertEquals(Herons(24,30,18), 216.0),
-		        () -> assertEquals(Herons(1,1,1), 0.4330127018922193),
-		        () -> assertEquals(Herons(4,5,8), 8.181534085976786));
 	}
 }
