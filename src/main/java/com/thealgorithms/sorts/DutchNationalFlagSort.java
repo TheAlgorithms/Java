4caed76c86b103af2df87571c1c676c0ce5d7501
Antonia Strack <antonia.strack@mni.thm.de>
2022-04-23 23:19:00 +0200

    Remove main and System.out, add tests

--- src/main/java/com/thealgorithms/sorts/DutchNationalFlagSort.java
+++ src/main/java/com/thealgorithms/sorts/DutchNationalFlagSort.java
@@ -15,9 +15,7 @@ public class DutchNationalFlagSort implements SortAlgorithm {
 
     @Override
     public <T extends Comparable<T>> T[] sort(T[] unsorted) {
-        System.out.println(unsorted[(int) Math.floor((unsorted.length)/2.0) -1]);
-
-        return dutch_national_flag_sort(unsorted, unsorted[(int) Math.floor((unsorted.length)/2.0) -1]);
+        return dutch_national_flag_sort(unsorted, unsorted[(int) Math.ceil((unsorted.length)/2.0) -1]);
     }
 
     public <T extends Comparable<T>> T[] sort(T[] unsorted, T intendedMiddle) {
@@ -30,12 +28,10 @@ public class DutchNationalFlagSort implements SortAlgorithm {
         int k = arr.length - 1;
 
         while( j <= k){
-            // arr[j] is more than middle
             if ( 0 > arr[j].compareTo(intendedMiddle)){
                 SortUtils.swap(arr, i, j);
                 j++;
                 i++;
-                // arr[j] is more than middle
             } else if (0 < arr[j].compareTo(intendedMiddle)){
                 SortUtils.swap(arr, j, k);
                 k--;
@@ -44,37 +40,5 @@ public class DutchNationalFlagSort implements SortAlgorithm {
             }
         }
        return arr;
-    }
-
-    public static void main(String[] args){
-        DutchNationalFlagSort dnfs = new DutchNationalFlagSort();
-
-        System.out.println("First: Integer[] without giving a intended middle value.\n " +
-                "Function will use the value positioned in the middle as intended middle value.");
-        Integer[] integerArray0 = {40, 4, 8, 10, 15, 9, 11};
-        // Print integerArray unsorted
-        SortUtils.print(integerArray0);
-
-        System.out.println("Sencond: Integer[] with giving a intended middle value.\n " +
-                "Inended middle value is 16.");
-        Integer[] integerArray1 = {16, 22, 8, 1, 16, 9, 2};
-        // Print integerArray unsorted
-        SortUtils.print(integerArray1);
-
-        dnfs.sort(integerArray1, 16);
-        // Print integerArray sorted
-        SortUtils.print(integerArray1);
-
-        System.out.println("Second: String[] with giving a intended middle value.\n " +
-                "Intended middle value is \"F\".");
-        String[] stringArray = {"D", "A", "F", "B", "E", "F", "C", "G"};
-        // Print integerArray unsorted
-        SortUtils.print(stringArray);
-
-        dnfs.sort(stringArray, "F");
-        // Print integerArray sorted
-        SortUtils.print(stringArray);
-
-
     }
 }
