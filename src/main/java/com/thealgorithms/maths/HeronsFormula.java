b277cd658d3358327402484d25a39e07478fd3e9
RaghavTaneja <97575679+RaghavTaneja@users.noreply.github.com>
2022-04-25 19:32:45 -0500

    Update HeronsFormula.java

--- src/main/java/com/thealgorithms/maths/HeronsFormula.java
+++ src/main/java/com/thealgorithms/maths/HeronsFormula.java
@@ -5,15 +5,7 @@ package com.thealgorithms.maths;
  */
 
 public class HeronsFormula {
-
-	public static void main(String[] args)
-	{
-		assert Double.compare(Herons(3,4,5), 6.0) == 0;
-		assert Double.compare(Herons(24,30,18), 216.0) == 0;;
-		assert Double.compare(Herons(1,1,1), 0.4330127018922193) == 0;
-		assert Double.compare(Herons(4,5,8), 8.181534085976786) == 0;
-	}
-
+	
 	public static double Herons(int s1, int s2, int s3)
 	{
 		double a = s1;
