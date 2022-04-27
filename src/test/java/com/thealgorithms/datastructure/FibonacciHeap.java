bee059dc6a2e02d117a66c7e5f08ac9819516e27
Omar <omar.taiseerr@gmail.com>
2022-04-27 14:49:26 +0300

    adding new data structure
    adding fibonacci heap data structure

---
+++ src/test/java/com/thealgorithms/datastructure/FibonacciHeap.java
@@ -0,0 +1,27 @@
+package com.thealgorithms.datastructures;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PermutationTest {
+
+    @Test
+    void testHeap()
+    {
+        FibonacciHeap fibonacciHeap = new FibonacciHeap();
+        fibonacciHeap.insert(5);
+        fibonacciHeap.insert(3);
+        fibonacciHeap.insert(1);
+        fibonacciHeap.insert(18);
+        fibonacciHeap.insert(33);
+
+
+        assertEquals(fibonacciHeap.findMin().getKey(), 1);
+        fibonacciHeap.deleteMin();
+        assertEquals(fibonacciHeap.findMin().getKey(), 3);
+    }
+}
