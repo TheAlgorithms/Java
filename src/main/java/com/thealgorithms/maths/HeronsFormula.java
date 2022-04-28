41be089f6a918aece04e6c951359c699e755206e
RaghavTaneja <97575679+RaghavTaneja@users.noreply.github.com>
2022-04-28 02:40:13 -0500

    Add Tests for Heron's Formula (#3035)

---
+++ src/main/java/com/thealgorithms/maths/HeronsFormula.java
@@ -0,0 +1,19 @@
+package com.thealgorithms.maths;
+
+/**
+ * Find the area of a triangle using only side lengths
+ */
+
+public class HeronsFormula {
+	
+	public static double Herons(int s1, int s2, int s3)
+	{
+		double a = s1;
+		double b = s2;
+		double c = s3;
+		double s = (a + b + c)/2.0;
+		double area = 0;
+		area = Math.sqrt((s)*(s-a)*(s-b)*(s-c));
+		return area;
+	}
+}
