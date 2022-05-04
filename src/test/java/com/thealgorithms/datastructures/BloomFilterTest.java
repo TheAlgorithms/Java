f272d8a949d47a1b23474fb418a505c335f139cc
Omar <59146036+omarmahamid@users.noreply.github.com>
2022-05-04 09:24:19 +0300

    Add Bloom Filter (#3042)

---
+++ src/test/java/com/thealgorithms/datastructures/BloomFilterTest.java
@@ -0,0 +1,28 @@
+package com.thealgorithms.datastructures.bloomfilter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class BloomFilterTest {
+
+    @Test
+    public void test1(){
+        BloomFilter<Integer> bloomFilter = new BloomFilter<>(3,10);
+        bloomFilter.insert(3);
+        bloomFilter.insert(17);
+
+        Assertions.assertTrue(bloomFilter.contains(3));
+        Assertions.assertTrue(bloomFilter.contains(17));
+    }
+
+    @Test
+    public void test2(){
+        BloomFilter<String> bloomFilter = new BloomFilter<>(4,20);
+        bloomFilter.insert("omar");
+        bloomFilter.insert("mahamid");
+
+        Assertions.assertTrue(bloomFilter.contains("omar"));
+        Assertions.assertTrue(bloomFilter.contains("mahamid"));
+    }
+}
