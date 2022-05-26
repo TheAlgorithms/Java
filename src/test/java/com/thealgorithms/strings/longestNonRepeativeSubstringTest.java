921c4b447e6bd00a77eaff42813c9f3ced3d9b75
SanOtaku09 <sanotaku098@gmail.com>
2022-05-26 14:45:50 +0530

    test for longest non repeative subtring

---
+++ src/test/java/com/thealgorithms/strings/longestNonRepeativeSubstringTest.java
@@ -0,0 +1,14 @@
+package com.thealgorithms.strings;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class longestNonRepeativeSubstringTest {
+    @Test
+    public void palindrome() {
+        String input1 = "HelloWorld";
+        String input2 = "javaIsAProgrammingLanguage";
+        Assertions.assertEquals( longestNonRepeativeSubstring.lengthOfLongestSubstring( input1 ) , 5 ) ;
+        Assertions.assertEquals( longestNonRepeativeSubstring.lengthOfLongestSubstring( input2 ) , 9 ) ;
+    }
+}
