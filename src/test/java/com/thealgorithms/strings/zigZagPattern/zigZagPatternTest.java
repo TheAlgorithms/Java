c85dd0d319a0dccbe1a0b00b0bf4adc9cee1add6
SanOtaku09 <sanotaku098@gmail.com>
2022-05-26 14:56:02 +0530

    test for zigZagPattern

---
+++ src/test/java/com/thealgorithms/strings/zigZagPattern/zigZagPatternTest.java
@@ -0,0 +1,14 @@
+package com.thealgorithms.strings.zigZagPattern;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class zigZagPatternTest {
+    @Test
+    public void palindrome() {
+        String input1 = "HelloWorldFromJava";
+        String input2 = "javaIsAProgrammingLanguage";
+        Assertions.assertEquals( zigZagPattern.encode( input1 , 4 ) , "HooeWrrmalolFJvlda" ) ;
+        Assertions.assertEquals( zigZagPattern.encode( input2 , 4 ) , "jAaLgasPrmgaaevIrgmnnuaoig" ) ;
+    }
+}
