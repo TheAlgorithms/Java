6719a331c4f2c2fbc977d7bf8e3a2975bbbc1048
phamhuythiep12 <phamhuythiep14@gmail.com>
2022-06-01 22:42:50 +0700

    Test

---
+++ src/test/java/com/thealgorithms/maths/1.GaussianTest.java
@@ -0,0 +1,28 @@
+package com.thealgorithms.maths;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GaussianTest {
+
+    @Test
+    void passTest1()
+    {
+        ArrayList<Double> list = new ArrayList<Double>();
+        ArrayList<Double> gaussian = new ArrayList<Double>();
+        ArrayList<Double> answer = new ArrayList<Double>();
+        answer.add(0.0);
+        answer.add(1.0);
+
+        int matrixSize = 2;
+        list.add(1.1);
+        list.add(1.2);
+        list.add(1.3);
+        list.add(2.4);
+        list.add(1.0);
+        list.add(1.0);
+        gaussian=gaussian(matrixSize,list);
+
+        assertEquals(answer,gaussian);
+    }
+}
