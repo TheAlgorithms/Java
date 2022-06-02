0e7cf24ccd1b181877f6ab19fad34c27a45da3c6
TaiNguyen <tt.tai.nguyen@tomosia.com>
2022-06-02 09:58:51 +0700

    Add tests for findMin

--- src/test/java/com/thealgorithms/maths/FindMinTest.java
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -7,6 +7,6 @@ public class FindMinTest {
     
     @Test
     public void testFindMinValue(){
-        assertEquals(10, FindMin.findMin(new int[] {1,2,3,4,5,6,7,8,9,10}));
+        assertEquals(1, FindMin.findMin(new int[] {1,2,3,4,5,6,7,8,9,10}));
     }
 }
