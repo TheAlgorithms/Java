19ceb3ab99a5f2f1a697744ed5bbb69601640650
Hien Nguyen <85053616+hnq-jan8@users.noreply.github.com>
2022-06-02 11:11:29 +0700

    add Junit Test for SelectionSort

---
+++ src/test/java/com/thealgorithms/sorts/SelectionSortTest.java
@@ -0,0 +1,24 @@
+package com.thealgorithms.sorts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SelectionSortTest {
+
+    @Test
+    void IntegerArrTest() {
+        Integer[] arr = {4, 23, 6, 78, 1, 54, 231, 9, 12};
+        SelectionSort selectionSort = new SelectionSort();
+
+        assertArrayEquals(new Integer[]{1, 4, 6, 9, 12, 23, 54, 78, 231}, selectionSort.sort(arr));
+    }
+
+    @Test
+    void StringArrTest() {
+        String[] arr = {"c", "a", "e", "b", "d"};
+        SelectionSort selectionSort = new SelectionSort();
+
+        assertArrayEquals(new String[]{"a", "b", "c", "d", "e"}, selectionSort.sort(arr));
+    }
+}
