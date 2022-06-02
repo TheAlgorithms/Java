5127bd9e3cb5ffda03884f8f10a6ea5f4898d12e
Phạm Huy Thiệp <85355862+phamhuythiep12@users.noreply.github.com>
2022-06-02 13:42:28 +0700

    Create 1.GaussianTest.java

--- src/test/java/com/thealgorithms/maths/1.GaussianTest.java
+++ src/test/java/com/thealgorithms/maths/1.GaussianTest.java
@@ -15,11 +15,6 @@ public class GaussianTest {
         answer.add(1.0);
 
         int matrixSize = 2;
-        list.add(1.1);
-        list.add(1.2);
-        list.add(1.3);
-        list.add(2.4);
-        list.add(1.0);
         list.add(1.0);
         gaussian=gaussian(matrixSize,list);
 
@@ -27,24 +22,3 @@ public class GaussianTest {
     }
 }
 
-    @Test
-    void passTest2()
-    {
-        ArrayList<Double> list = new ArrayList<Double>();
-        ArrayList<Double> gaussian = new ArrayList<Double>();
-        ArrayList<Double> answer = new ArrayList<Double>();
-        answer.add(0.0);
-        answer.add(2.0);
-
-        int matrixSize = 3;
-        list.add(0.1);
-        list.add(1.2);
-        list.add(1.3);
-        list.add(2.9);
-        list.add(2.3);
-        list.add(3.0);
-        gaussian=gaussian(matrixSize,list);
-
-        assertEquals(answer,gaussian);
-    }
-}
