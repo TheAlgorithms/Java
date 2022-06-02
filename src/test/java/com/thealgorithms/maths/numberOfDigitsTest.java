edf51cd47c44b707d266e604b8b0bc29f9b9881f
tle <thongld.bi11-257@st.usth.edu.vn>
2022-06-02 12:48:09 +0700

    Update numberOfDigitsTest.java

---
+++ src/test/java/com/thealgorithms/maths/numberOfDigitsTest.java
@@ -0,0 +1,26 @@
+package com.thealgorithms.maths;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class numberOfDigitsTest {
+  @Test
+  public void testNumberOfDigits() {
+    assertEquals(1, NumberOfDigits.numberOfDigits(987654321));
+  }
+
+  @Test
+  public void testNumberOfDigitsFast() {
+    assertEquals(3, NumberOfDigits. numberOfDigitsFast(98765432));
+  }
+
+  @Test
+  public void testNumberOfDigitsFaster() {
+    assertEquals(4, NumberOfDigits. numberOfDigitsFaster(7765321));
+  }
+
+  @Test
+  public void testNumberOfDigitsFaster() {
+    assertEquals(0, NumberOfDigits. numberOfDigitsRecursion(88654321));
+  }
+}
