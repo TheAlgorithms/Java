6ef66de59213f145e387ba3ab0bb0e11977f2bc4
TaiNguyen <tt.tai.nguyen@tomosia.com>
2022-06-02 09:39:56 +0700

    Add tests for findMin

---
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -0,0 +1,12 @@
+package com.thealgorithms.maths;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FindMinTest {
+    
+    @Test
+    public void testFindMinValue(){
+        assertEquals(10, FindMin.findMin(new int[] {1,2,3,4,5,6,7,8,9,10}));
+    }
+}
