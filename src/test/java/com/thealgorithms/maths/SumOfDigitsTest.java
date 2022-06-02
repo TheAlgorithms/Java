8280a94d4e96150cd5bb1a76d7b4dd7d88f93a5f
hnq-jan8 <nqhien.tm02@outlook.com>
2022-06-02 09:34:05 +0700

    update Junit Test for SumOfDigits

--- src/test/java/com/thealgorithms/maths/SumOfDigitsTest.java
+++ src/test/java/com/thealgorithms/maths/SumOfDigitsTest.java
@@ -7,13 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class SumOfDigitsTest {
 
     @Test
-    void sumOfDigits() {
-//        var sumOfDigits = new SumOfDigits();
+    void testZero() {
         assertEquals(0, SumOfDigits.sumOfDigits(0));
         assertEquals(0, SumOfDigits.sumOfDigitsRecursion(0));
         assertEquals(0, SumOfDigits.sumOfDigitsFast(0));
+}
+
+    @Test
+    void testPositive() {
         assertEquals(15, SumOfDigits.sumOfDigits(12345));
         assertEquals(15, SumOfDigits.sumOfDigitsRecursion(12345));
         assertEquals(15, SumOfDigits.sumOfDigitsFast(12345));
+    }
+
+    @Test
+    void testNegative() {
+        assertEquals(6, SumOfDigits.sumOfDigits(-123));
+        assertEquals(6, SumOfDigits.sumOfDigitsRecursion(-123));
+        assertEquals(6, SumOfDigits.sumOfDigitsFast(-123));
     }
 }
