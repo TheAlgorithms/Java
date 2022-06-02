dbf71f9d1749ba93194e9d392ffcbe4f7bc50906
Nguyễn Tuấn Khải <nkhai462@gmail.com>
2022-06-02 13:36:19 +0700

    Create factorialtest.java

---
+++ factorialtest.java
@@ -0,0 +1,14 @@
+public package com.thealgorithms.maths;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FactorialTest {
+
+    @Test
+    public void testFactorial() {
+        assertEquals(120, Factorial.factorial(5));
+    }
+} factorialtest {
+    
+}
