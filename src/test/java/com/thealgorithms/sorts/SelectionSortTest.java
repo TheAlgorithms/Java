39a0f478d24467636e7b3b02ab6e48e0b3bb1dc2
Hien Nguyen <85053616+hnq-jan8@users.noreply.github.com>
2022-06-03 17:58:41 +0700

    update SelectionSortTest.java

--- src/test/java/com/thealgorithms/sorts/SelectionSortTest.java
+++ src/test/java/com/thealgorithms/sorts/SelectionSortTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SelectionSortTest {
+public class SelectionSortTest {
 
     @Test
     void IntegerArrTest() {
