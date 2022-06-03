21014be8d431c3bf632523006af51b7491f2707e
Nguyễn Tuấn Khải <nkhai462@gmail.com>
2022-06-03 16:46:04 +0700

    Create fibonancy.java

---
+++ fibonancy.java
@@ -0,0 +1,25 @@
+public class fibonacci {
+    
+}
+
+   
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FibonacciTest {
+    private Fibonacci fibonacci;
+
+    @Before
+    public void setUp() {
+        fibonacci = new Fibonacci();
+    }
+
+    @Test
+    public void calculate_0() {
+        int testIndex = 0;
+        int expectedResult = 1;
+
+        assertEquals(expectedResult, fibonacci.calculate(testIndex));
+    }
+}
