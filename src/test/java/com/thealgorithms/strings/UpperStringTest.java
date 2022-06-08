9a3b4e17063d4cb779ab76ddf3c4be10f55d1743
thangle1206 <106686277+thangle1206@users.noreply.github.com>
2022-06-08 10:43:44 +0700

    Update partition for UpperStringTest.java

--- src/test/java/com/thealgorithms/strings/UpperStringTest.java
+++ src/test/java/com/thealgorithms/strings/UpperStringTest.java
@@ -7,19 +7,16 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UpperStringTest{
     @Test
     public void toUpperCase1() {
+		//Partition for having space 
         String str1 = "sOfTware TEstinG";
         assertEquals("SOFTWARE TESTING", Upper.toUpperCase(str1));
     }
 
     @Test
     public void toUpperCase2() {
-        String str2 = "hEllO WoRLD";
-        assertEquals("HELLO WORLD", Upper.toUpperCase(str2));
+    	//Partition for not having space
+        String str2 = "tHAnG";
+        assertEquals("THANG", Upper.toUpperCase(str2));
     }
 
-    @Test
-    public void toUpperCase3() {
-        String str3 = "i aM Thang";
-        assertEquals("I AM THANG", Upper.toUpperCase(str3));
-    }
 }
