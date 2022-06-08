c46c8f93bef865f4b1334ec396dd4ea28780ffe5
ppmy02 <batmanvszero@gmail.com>
2022-06-08 17:45:09 +0700

    Update Input Domains for sumOfDigitsTest

--- src/test/java/com/thealgorithms/maths/sumOfDigitsTest.java
+++ src/test/java/com/thealgorithms/maths/sumOfDigitsTest.java
@@ -3,15 +3,34 @@ package com.thealgorithms.maths;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class sumOfDigitsTest {
-    @Test
-    void test1(){
-        Assertions.assertEquals(6, SumOfDigits.sumOfDigitsRecursion(123));
-    }
 
+public class sumOfDigitsTest {
+    /**
+    * Test case for float input
+    */
     @Test
-    void test2(){
+    public void testSumWithFloat(){
+        Assertions.assertEquals(9, SumOfDigits.sumOfDigitsRecursion(2.34));
+    }
+    /**
+     * Test case with input equal 0
+     */
+    @Test
+    public void testSumWith0(){
+        Assertions.assertEquals(0, SumOfDigits.sumOfDigitsRecursion(0));
+    }
+    /**
+     * Test case for valid input
+     */
+    @Test
+    public void testSumWithPositiveInput(){
         Assertions.assertEquals(15, SumOfDigits.sumOfDigitsRecursion(456));
     }
-
+    /**
+     * Test case with Negative number
+     */
+    @Test
+    public void testSumWithNegativeInput(){
+        Assertions.assertEquals(6, SumOfDigits.sumOfDigitsRecursion(-123));
+    }
 }
