32200b5c914f4ffe48b0b02d63da6e1fc2a86591
Nguyễn Tuấn Khải <nkhai462@gmail.com>
2022-06-08 10:43:56 +0700

    Update findMin.java

--- package/findMin.java
+++ package/findMin.java
@@ -1,24 +1,10 @@
-public class findMin {
-    package findMin {
-        import java.util.*;
-        import java.lang.*;
-        import java.io.*;
-        class findMin {
-            public static void main(String args[]) {
-                Scanner sc = new Scanner(System.in);
-                int n = sc.nextInt();
-                int a[] = new int[n];
-                for (int i = 0; i < n; i++) {
-                    a[i] = sc.nextInt();
-                }
-                int min = a[0];
-                for (int i = 1; i < n; i++) {
-                    if (a[i] < min) {
-                        min = a[i];
-                    }
-                }
-                System.out.println(min);
-            }
-        }
+package com.thealgorithms.maths;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class findMinTest {
+    private findMin findMin;
+    public void setUp() {
+        findMin = new findMin();
     }
 }
