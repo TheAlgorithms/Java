d997c2a665dfae69be2fbb6ae18c2cc115363288
HoangTuanBI11-280 <101243562+HoangTuanBI11-280@users.noreply.github.com>
2022-06-09 00:09:39 +0700

    Update FibonacciTest.java

--- src/test/java/com/thealgorithms/maths/FibonacciTest.java
+++ src/test/java/com/thealgorithms/maths/FibonacciTest.java
@@ -1,53 +1,16 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Before;
-import org.junit.Test;
+package com.thealgorithms.maths;
 
-public class FibonacciTest {
-    private Fibonacci fibonacci;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @Before
-    public void setUp() {
-        fibonacci = new Fibonacci();
-    }
-    
-    public class Fibonacci {
-        public int calculate(int index) {
-            return 1;
-        }
-    }    
-    
+
+public class FibonacciNumTest {
+
     @Test
-    public void calculate_0() {
-        int testIndex = 0;
-        int expectedResult = 1;
-
-        assertEquals(expectedResult, fibonacci.calculate(testIndex));
+    public void TestisisPerfectSquare() {
+        assertEquals(true, FibonacciNumber.isPerfectSquare(1));
+        assertEquals(false, FibonacciNumber.isPerfectSquare(2));
+        assertEquals(false, FibonacciNumber.isPerfectSquare(21));
     }
     
-    @Test
-    public void calculate_4() {
-        int testIndex = 4;
-        int expectedResult = 5;
-
-        assertEquals(expectedResult, fibonacci.calculate(testIndex));
-    }
-    
-    @Test
-    public void calculate_5() {
-        int testIndex = 5;
-        int expectedResult = 8;
-
-        assertEquals(expectedResult, fibonacci.calculate(testIndex));
-    }
-    
-    public class Fibonacci1 {
-        public int calculate(int index) {
-            if (index == 0 || index == 1) {
-                return 1;
-            }
-
-            return calculate(index - 1) + calculate(index - 2);
-        }
-    }
-    
-}    
+}
