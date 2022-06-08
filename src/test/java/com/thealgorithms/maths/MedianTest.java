24015d193b2291b54b83a3e377f458d610c6a03d
Lê Anh Vũ <100741247+Ophiuchuss@users.noreply.github.com>
2022-06-08 10:44:54 +0700

    Update MedianTest.java

--- src/test/java/com/thealgorithms/maths/MedianTest.java
+++ src/test/java/com/thealgorithms/maths/MedianTest.java
@@ -6,11 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MedianTest {
 
     @Test
+    //valid partition
     public void testEvenElements() {
         assertEquals(5.5, Median.median(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
     }
 
     @Test
+    //valid partition
     public void testOddElements(){
         assertEquals(5, Median.median(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}));
     }
