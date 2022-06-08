f07f43e743636a67d3c2fd3867c1ef04137cd3e9
Ph1eu <hieupm.bi11-090@st.usth.edu.vn>
2022-06-08 10:12:14 +0700

    Update PythagoreanTripleTest

--- src/test/java/com/thealgorithms/maths/PythagoreanTripleTest.java
+++ src/test/java/com/thealgorithms/maths/PythagoreanTripleTest.java
@@ -5,14 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PythagoreanTripleTest {
     @Test
-    public void Testpythagoreantriple(){
-        
+    public void TestTrueValuepythagoreantriple(){
+        //valid partition
         assertEquals(true, PythagoreanTriple.isPythagTriple(3,4,5));
         assertEquals(true, PythagoreanTriple.isPythagTriple(6,8,10));
         assertEquals(true, PythagoreanTriple.isPythagTriple(9,12,15));
         assertEquals(true, PythagoreanTriple.isPythagTriple(12,16,20));
         assertEquals(true, PythagoreanTriple.isPythagTriple(15,20,25));
         assertEquals(true, PythagoreanTriple.isPythagTriple(18,24,30));        
+        
+    }   
+    @Test
+    public void TestFalseValuepythagoreantriple(){
+        //invalid partition
         assertEquals(false, PythagoreanTriple.isPythagTriple(5,20,30));
         assertEquals(false, PythagoreanTriple.isPythagTriple(6,8,100));
         assertEquals(false, PythagoreanTriple.isPythagTriple(-2,-2,2));   
