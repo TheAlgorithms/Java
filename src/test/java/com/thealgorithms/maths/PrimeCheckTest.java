6dc7f32684b805926f204d621c388e5827a7ad4a
DuyLe0912 <letrankhuongduy0912@gmail.com>
2022-06-08 09:50:48 +0700

    Update PrimeCheckTest.java

--- src/test/java/com/thealgorithms/maths/PrimeCheckTest.java
+++ src/test/java/com/thealgorithms/maths/PrimeCheckTest.java
@@ -1,41 +1,21 @@
 package com.thealgorithms.maths;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-public class PrimeCheckTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class PrimeCheckTest{
     @Test
-    void test1() {
-        Assertions.assertTrue(PrimeCheck.isPrime(2));
+    public void Test1() {
+        //Valid Partition
+    	assertEquals(true,PrimeCheck.isPrime(3));
     }
-
     @Test
-    void test2() {
-        Assertions.assertFalse(PrimeCheck.isPrime(-1));
+    public void Test2() {
+        //Invalid Partition
+    	assertEquals(false,PrimeCheck.isPrime(4));
     }
-
     @Test
-    void test3() {
-        Assertions.assertFalse(PrimeCheck.isPrime(4));
-    }
-
-    @Test
-    void test4() {
-        Assertions.assertTrue(PrimeCheck.isPrime(5));
-    }
-
-    @Test
-    void test5() {
-        Assertions.assertFalse(PrimeCheck.isPrime(15));
-    }
-
-    @Test
-    void test6() {
-        Assertions.assertTrue(PrimeCheck.isPrime(11));
-    }
-
-    @Test
-    void test7() {
-        Assertions.assertFalse(PrimeCheck.isPrime(49));
+    public void Test3() {
+        //Invalid Partition
+    	assertEquals(false,PrimeCheck.isPrime(-13));
     }
 }
