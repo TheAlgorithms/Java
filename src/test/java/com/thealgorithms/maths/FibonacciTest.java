fd7e1d48bcf2208549c9a7f6a2a68dad45bf2724
HoangTuanBI11-280 <101243562+HoangTuanBI11-280@users.noreply.github.com>
2022-06-08 13:25:28 +0700

    Update FibonacciTest.java

--- src/test/java/com/thealgorithms/maths/FibonacciTest.java
+++ src/test/java/com/thealgorithms/maths/FibonacciTest.java
@@ -9,7 +9,13 @@ public class FibonacciTest {
     public void setUp() {
         fibonacci = new Fibonacci();
     }
-
+    
+    public class Fibonacci {
+        public int calculate(int index) {
+            return 1;
+        }
+    }    
+    
     @Test
     public void calculate_0() {
         int testIndex = 0;
@@ -17,4 +23,31 @@ public class FibonacciTest {
 
         assertEquals(expectedResult, fibonacci.calculate(testIndex));
     }
-}
+    
+    @Test
+    public void calculate_4() {
+        int testIndex = 4;
+        int expectedResult = 5;
+
+        assertEquals(expectedResult, fibonacci.calculate(testIndex));
+    }
+    
+    @Test
+    public void calculate_5() {
+        int testIndex = 5;
+        int expectedResult = 8;
+
+        assertEquals(expectedResult, fibonacci.calculate(testIndex));
+    }
+    
+    public class Fibonacci1 {
+        public int calculate(int index) {
+            if (index == 0 || index == 1) {
+                return 1;
+            }
+
+            return calculate(index - 1) + calculate(index - 2);
+        }
+    }
+    
+}    
