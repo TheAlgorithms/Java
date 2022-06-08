30ec0443338881d460e04a2fcca42a496e71e6b4
Huyen165 <90266891+Huyen165@users.noreply.github.com>
2022-06-08 09:34:09 +0700

    Update FactorialTest.java

--- src/test/java/com/thealgorithms/maths/FactorialTest.java
+++ src/test/java/com/thealgorithms/maths/FactorialTest.java
@@ -3,11 +3,8 @@ package com.thealgorithms.maths;
 import javax.annotation.processing.Generated;
 
 public class FactorialTest {
-
-	
-	
 	@Test
-	public void testFactorial() throws Exception {
+	public void testFactorial1() throws Exception {
 		int n = 0;
 		long result;
 
@@ -15,12 +12,20 @@ public class FactorialTest {
 		n = 0;
 		result = Factorial.factorial(n);
 		Assert.assertEquals(null, result);
-
+	}
+	@Test
+	public void testFactorial2() throws Exception {
+		int n = 0;
+		long result;
 		// test 2
 		n = -1;
 		result = Factorial.factorial(n);
 		Assert.assertEquals(null, result);
-
+	}
+	@Test
+	public void testFactorial3() throws Exception {
+		int n = 0;
+		long result;
 		// test 3
 		n = 1;
 		result = Factorial.factorial(n);
