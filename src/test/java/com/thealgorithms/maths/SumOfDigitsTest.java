926775e079893e666bcbf79d0491127e427ea7d1
Hien Nguyen <85053616+hnq-jan8@users.noreply.github.com>
2022-06-09 14:42:30 +0700

    Update SumOfDigitsTest.java

--- src/test/java/com/thealgorithms/maths/SumOfDigitsTest.java
+++ src/test/java/com/thealgorithms/maths/SumOfDigitsTest.java
@@ -5,25 +5,27 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SumOfDigitsTest {
+    
+    SumOfDigits SoD = new SumOfDigits();
 
     @Test
     void testZero() {
-        assertEquals(0, SumOfDigits.sumOfDigits(0));
-        assertEquals(0, SumOfDigits.sumOfDigitsRecursion(0));
-        assertEquals(0, SumOfDigits.sumOfDigitsFast(0));
+        assertEquals(0, SoD.sumOfDigits(0));
+        assertEquals(0, SoD.sumOfDigitsRecursion(0));
+        assertEquals(0, SoD.sumOfDigitsFast(0));
 }
 
     @Test
     void testPositive() {
-        assertEquals(15, SumOfDigits.sumOfDigits(12345));
-        assertEquals(15, SumOfDigits.sumOfDigitsRecursion(12345));
-        assertEquals(15, SumOfDigits.sumOfDigitsFast(12345));
+        assertEquals(15, SoD.sumOfDigits(12345));
+        assertEquals(15, SoD.sumOfDigitsRecursion(12345));
+        assertEquals(15, SoD.sumOfDigitsFast(12345));
     }
 
     @Test
     void testNegative() {
-        assertEquals(6, SumOfDigits.sumOfDigits(-123));
-        assertEquals(6, SumOfDigits.sumOfDigitsRecursion(-123));
-        assertEquals(6, SumOfDigits.sumOfDigitsFast(-123));
+        assertEquals(6, SoD.sumOfDigits(-123));
+        assertEquals(6, SoD.sumOfDigitsRecursion(-123));
+        assertEquals(6, SoD.sumOfDigitsFast(-123));
     }
 }
