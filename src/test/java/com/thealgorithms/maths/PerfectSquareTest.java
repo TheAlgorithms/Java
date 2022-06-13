36ea89e55af905d3232be735d71e25b53f3a5302
BI11-061 Hoàng Anh Đức <89365021+Anhduc2k2@users.noreply.github.com>
2022-06-13 13:48:40 +0700

    Update PerfectSquareTest.java

--- src/test/java/com/thealgorithms/maths/PerfectSquareTest.java
+++ src/test/java/com/thealgorithms/maths/PerfectSquareTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PerfectSquareTest{
 	
 	@Test
-	//Valid Partition
 	public void TestPerfectSquare(){
+		//Valid Partition
 		int number = 9;
 		
 		boolean result = PerfectSquare.isPerfectSquare(number);
@@ -17,12 +17,21 @@ public class PerfectSquareTest{
 	}
 	
 	@Test
-	//Invalid Partition
 	public void TestPerfectSquare(){
+		//Invalid Partition 1
 		int number = 3;
 		
 		boolean result = PerfectSquare.isPerfectSquare(number);
 		
 		assertFalse(result)
+	}
+	@Test
+	public void TestPerfectSquareifisNegativeNumber(){
+		//Invalid Partition 2
+		int number = -10;
+
+		boolean result = PerfectSquare.isPerfectSquare(number);
+
+		assertFalse(result);
 	}
 }
