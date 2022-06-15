ea42139c2edf8acfc97a9ef43e79fa8e9b3f4018
TaiNguyen <tt.tai.nguyen@tomosia.com>
2022-06-15 14:19:03 +0700

    Partition Test for FindMin

--- src/test/java/com/thealgorithms/maths/FindMinTest.java
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -1,17 +1,26 @@
 package com.thealgorithms.maths;
 
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindMinTest {
-  @Test
-  public void test1(){
-    assertEquals(1, FindMin.findMin(new int[] {1, 3, 5, 7, 9}));
+  @Test public void validPartition() {
+      //Valid Partition
+  	Assertions.assertSame(1, FindMin.findMin(new int[] {1, 3, 5, 7, 9}));
   }
   
-  @Test
-  public void test2(){
-    assertEquals(0, FindMin.findMin(new int[] {0, 192, 384, 576}));
+  @Test public void invalidPartition1() {
+      //Invalid Partition
+  	Assertions.assertNotSame(3, FindMin.findMin(new int[] {1, 3, 5, 7, 9}));
+  }
+  
+  @Test public void invalidPartition2() {
+      //Invalid Partition
+  	Assertions.assertNotSame(-2, FindMin.findMin(new int[] {1, 3, 5, 7, 9}));
+  }
+  
+  @Test public void invalidPartition3() {
+      //Invalid Partition
+  	Assertions.assertNotSame(null, FindMin.findMin(new int[] {1, 3, 5, 7, 9}));
   }
 }
