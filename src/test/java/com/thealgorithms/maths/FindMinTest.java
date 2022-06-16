ab5694e7b472e37fb98f8786e9582695f79894fc
nguyenviettrung-bi11276 <101244039+nguyenviettrung-bi11276@users.noreply.github.com>
2022-06-16 13:15:59 +0700

    Update FindMinTest.java

--- src/test/java/com/thealgorithms/maths/FindMinTest.java
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -1,17 +1,27 @@
 package com.thealgorithms.maths;
 
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FindMinTest {
   @Test
-  public void test1(){
-    assertEquals(1, FindMin.findMin(new int[] {1, 3, 5, 7, 9}));
+  public void test_from_max2min(){
+    assertEquals(1, FindMin.findMin(new int[] {9, 7, 5, 3, 1}));
   }
   
   @Test
-  public void test2(){
+  public void test_from_min2max(){
     assertEquals(0, FindMin.findMin(new int[] {0, 192, 384, 576}));
   }
+	
+  @Test
+  public void test_random(){
+    assertEquals(0, FindMin.findMin(new int[] {0, 5, 2, 1}));
+  }
+
+  @Test
+  public void test_id(){
+    assertEquals(0, FindMin.findMin(new int[] {0, 0, 0, 0}));
+  }
+
 }
