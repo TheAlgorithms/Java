7c343a92fc21921561a6b6f4cdcd4af5ed5daeb5
Akshay Dubey <akshay.dubey055@gmail.com>
2022-06-17 13:01:46 +0530

    refactor: Improve flow and rename solve method
    - Rename solve method to isPathFound method which now returns true if path is found false otherwise, earlier the return type of this method was void
    - Improve flow by removing throw statement by using boolean return type
    - Replace all tab spaces with whitespaces

--- src/main/java/com/thealgorithms/datastructures/graphs/HamiltonianCycle.java
+++ src/main/java/com/thealgorithms/datastructures/graphs/HamiltonianCycle.java
@@ -6,100 +6,91 @@ package com.thealgorithms.datastructures.graphs;
  */
 public class HamiltonianCycle {
 
-	private int V, pathCount;
-	private int[] path;
-	private int[][] graph;
+    private int V, pathCount;
+    private int[] path;
+    private int[][] graph;
 
-	/**
-	 * Find hamiltonian cycle for given graph G(V,E)
-	 *
-	 * @param graph Adjacency matrix of a graph G(V, E) 
-	 * for which hamiltonian path is to be found
-	 * @return Array containing hamiltonian cycle else returns 1D array with value -1.
-	 */
-	public int[] findHamiltonianCycle(int[][] graph){
-		this.V = graph.length;
-		this.path = new int[this.V];
+    /**
+     * Find hamiltonian cycle for given graph G(V,E)
+     * @param graph Adjacency matrix of a graph G(V, E) 
+     * for which hamiltonian path is to be found
+     * @return Array containing hamiltonian cycle else returns 1D array with value -1.
+     */
+    public int[] findHamiltonianCycle(int[][] graph){
+        this.V = graph.length;
+        this.path = new int[this.V];
 
-		//Initialize path array with -1 value
-		for(int i=0 ; i<this.path.length ; i++) {
-			this.path[i] = -1;
-		}
+        //Initialize path array with -1 value
+        for(int i=0 ; i<this.path.length ; i++) {
+            this.path[i] = -1;
+        }
 
-		this.graph = graph;        
+        this.graph = graph;        
 
-		try{            
-			this.path[0] = 0;
-			this.pathCount = 1;            
+        this.path[0] = 0;
+        this.pathCount = 1;            
+        if(!isPathFound(0)) {
+            for(int i=0 ; i<this.path.length ; i++) {
+                this.path[i] = -1;
+            }
+        }
 
-			solve(0);
+        return path;
+    }
 
-			for(int i=0 ; i<this.path.length ; i++) {
-				this.path[i] = -1;
-			}
-		}
-		catch (Exception e){
+    /** function to find paths recursively
+     * Find paths recursively from given vertex
+     * @param vertex Vertex from which path is to be found
+     * @returns true if path is found false otherwise
+     */
+    public boolean isPathFound(int vertex) {
+        if (this.graph[vertex][0] == 1 && this.pathCount == this.V) {
+            return true;
+        }
 
-		}
-		return path;
-	}
+        /** all vertices selected but last vertex not linked to 0 **/
+        if (this.pathCount == this.V) {
+            return false;
+        }
 
-	/** function to find paths recursively **/
-	/**
-	 * Find paths recursively from given vertex
-	 *
-	 * @param vertex Vertex from which path is to be found
-	 */
-	public void solve(int vertex) throws Exception{
-		if (this.graph[vertex][0] == 1 && this.pathCount == this.V) {
-			throw new Exception("Solution found");
-		}
+        for (int v = 0; v < this.V; v++){
+            /** if connected **/
+            if (this.graph[vertex][v] == 1 ){
+                /** add to path **/            
+                this.path[this.pathCount++] = v;    
 
-		/** all vertices selected but last vertex not linked to 0 **/
-		if (this.pathCount == this.V) {
-			return;
-		}
+                /** remove connection **/            
+                this.graph[vertex][v] = 0;
+                this.graph[v][vertex] = 0;
 
-		for (int v = 0; v < this.V; v++){
-			/** if connected **/
-			if (this.graph[vertex][v] == 1 ){
-				/** add to path **/            
-				this.path[this.pathCount++] = v;    
+                /** if vertex not already selected  solve recursively **/
+                if (!isPresent(v)) {
+                    return isPathFound(v);
+                }
 
-				/** remove connection **/            
-				this.graph[vertex][v] = 0;
-				this.graph[v][vertex] = 0;
+                /** restore connection **/
+                this.graph[vertex][v] = 1;
+                this.graph[v][vertex] = 1;
 
-				/** if vertex not already selected  solve recursively **/
-				if (!isPresent(v)) {
-					solve(v);
-				}
+                /** remove path **/
+                this.path[--this.pathCount] = -1;
+            }
+        }
+        return false;
+    }    
 
-				/** restore connection **/
-				this.graph[vertex][v] = 1;
-				this.graph[v][vertex] = 1;
+    /** function to check if path is already selected
+     * Check if path is already selected
+     * @param vertex Starting vertex 
+     */
+    public boolean isPresent(int vertex){
 
-				/** remove path **/
-				this.path[--this.pathCount] = -1;                    
+        for (int i = 0; i < pathCount - 1; i++) {
+            if (path[i] == vertex) {
+                return true;
+            }
+        }
 
-			}
-		}
-	}    
-
-	/** function to check if path is already selected **/
-	/**
-	 * Check if path is already selected
-	 *
-	 * @param vertex Starting vertex 
-	 */
-	public boolean isPresent(int vertex){
-
-		for (int i = 0; i < pathCount - 1; i++) {
-			if (path[i] == vertex) {
-				return true;
-			}
-		}
-
-		return false;
-	}
+        return false;
+    }
 }
