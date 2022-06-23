34f7b18db85838d823e440f82218c7984db4b063
Sedat Aybars Nazlica <saybarsnazlica@gmail.com>
2022-06-23 13:45:58 +0900

    Initialize string length outside of the for loop

--- src/main/java/com/thealgorithms/strings/HammingDistance.java
+++ src/main/java/com/thealgorithms/strings/HammingDistance.java
@@ -18,10 +18,11 @@ public class HammingDistance {
         if (s1.length() != s2.length()) {
             throw new Exception("String lengths must be equal");
         }
-
+        
+        int stringLength = s1.length();
         int counter = 0;
         
-        for (int i = 0, length = s1.length(); i < length; i++) {
+        for (int i = 0; i < stringLength; i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
                 counter++;
             }
