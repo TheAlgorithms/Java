b87db9273e4c0a43a9310324281e1fed213e3d73
Nguyễn Tuấn Khải <nkhai462@gmail.com>
2022-06-23 07:28:12 +0700

    commit

--- src/main/java/com/thealgorithms/maths/StandardScore.java
+++
@@ -1,9 +0,0 @@
-package com.thealgorithms.maths;
-
-public class StandardScore {
-	public static double zScore(double num, double mean, double stdDev)
-	{
-		double z = (num - mean)/stdDev;
-		return z;
-	}
-}
