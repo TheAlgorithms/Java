6c4f0ae4a6d0d70b9ba700ac583f1efcb6b8b2ba
Ankush263 <ankushbanik263@gmail.com>
2022-06-28 12:21:11 +0530

    Add test case to BinaryPow Algorithm

---
+++ src/test/java/com/thealgorithms/maths/BinaryPowTest.java
@@ -0,0 +1,16 @@
+package com.thealgorithms.maths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BinaryPowTest {
+
+    @Test
+    void testBinaryPow() {
+        assertEquals(8, BinaryPow.binPow(2, 3));
+        assertEquals(32768, BinaryPow.binPow(2, 15));
+        assertEquals(19683, BinaryPow.binPow(3, 9));
+        assertEquals(1, BinaryPow.binPow(3324, 0));
+    }
+}
