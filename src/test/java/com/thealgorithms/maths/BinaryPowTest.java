7f031b1b7cf6cfc6c8bdd9c1eeb1bbb715172776
Ankush263 <86042508+Ankush263@users.noreply.github.com>
2022-06-30 16:51:53 +0530

    Update BinaryPowTest

--- src/test/java/com/thealgorithms/maths/BinaryPowTest.java
+++ src/test/java/com/thealgorithms/maths/BinaryPowTest.java
@@ -1,7 +1,6 @@
 package com.thealgorithms.maths;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,9 +14,4 @@ public class BinaryPowTest {
         assertEquals(19683, BinaryPow.binPow(3, 9));
     }
 
-    @Test
-    void testBinaryPowException() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> BinaryPow.ExceptionBinPow(32, -4));
-        assertEquals("Power cannot be less then 0", exception.getMessage());
-    }
 }
