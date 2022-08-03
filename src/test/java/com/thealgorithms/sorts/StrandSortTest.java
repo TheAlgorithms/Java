f26c44d8b8212cff2afff4a99d63bf81c1017852
Marcus <104601262+MarcusCody@users.noreply.github.com>
2022-08-04 01:44:37 +0800

    Create JUnit tests for Strand Sort Algorithm

---
+++ src/test/java/com/thealgorithms/sorts/StrandSortTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.assertArrayEquals;
+
+package com.thealgorithms.sorts;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import org.junit.jupiter.api.Test;
+
+public class StrandSortTest {
+	@Test
+    // valid test case
+    public void StrandSortNonDuplicateTest() {
+		StrandSort strand = new StrandSort();
+		int[] expectedArray = {1,2,3,4,5};
+		LinkedList<Integer> actualList = strand.strandSort(new LinkedList<Integer>(Arrays.asList(3,1,2,4,5)));
+		int[] actualArray = new int[actualList.size()];
+		for(int i=0; i<actualList.size(); i++) {
+			actualArray[i] = actualList.get(i);
+		}
+		assertArrayEquals(expectedArray, actualArray);
+
+	}
+	@Test
+    // valid test case
+	public void StrandSortDuplicateTest() {
+		StrandSort strand = new StrandSort();
+		int[] expectedArray = {2, 2, 2, 5, 7};
+		LinkedList<Integer> actualList = strand.strandSort(new LinkedList<Integer>(Arrays.asList(7, 2, 2, 2, 5)));
+		int[] actualArray = new int[actualList.size()];
+		for(int i=0; i<actualList.size(); i++) {
+			actualArray[i] = actualList.get(i);
+		}
+		assertArrayEquals(expectedArray, actualArray);
+
+	}
+
+}
