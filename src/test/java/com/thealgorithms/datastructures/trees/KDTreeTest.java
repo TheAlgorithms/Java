189d35b58a16edaa5e971718e18087a28f1e7b0c
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-08-08 10:38:17 +0430

    Add tests for findMin of KDTree

---
+++ src/test/java/com/thealgorithms/datastructures/trees/KDTreeTest.java
@@ -0,0 +1,33 @@
+package com.thealgorithms.datastructures.trees;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KDTreeTest {
+
+    @Test
+    void findMin() {
+        int[][] coordinates = {
+                {30, 40},
+                {5, 25},
+                {70, 70},
+                {10, 12},
+                {50, 30},
+                {35, 45}
+        };
+        KDTree.Point[] points = Arrays.stream(coordinates)
+                .map(KDTree.Point::new)
+                .toArray(KDTree.Point[]::new);
+
+        KDTree kdTree = new KDTree(points);
+
+
+        assertEquals(5, kdTree.findMin(0).getPoint().getCoordinate(0));
+        assertEquals(12, kdTree.findMin(1).getPoint().getCoordinate(1));
+    }
+
+}
