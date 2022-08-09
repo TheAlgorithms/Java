f0ad0d1f8b79643ecc59002a3b76d9c492809e02
LauKinHoong <kinhoong_lau92@hotmail.com>
2022-08-10 00:12:26 +0800

    Update LeastCommonMultiple.java

--- src/main/java/com/thealgorithms/maths/LeastCommonMultiple.java
+++ src/main/java/com/thealgorithms/maths/LeastCommonMultiple.java
@@ -7,5 +7,8 @@ package com.thealgorithms.maths;
 public class LeastCommonMultiple {
     public static void main(String args[]){
         
+        int num1, num2, num3, high;
+        int cmv = 0;
+    }
     }
 }
