e87e097f06753137752cde689ac3602bd5f57d1b
HManiac74 <63391783+HManiac74@users.noreply.github.com>
2022-08-17 07:30:15 +0200

    Cleanup unused imports (#3217)

--- src/main/java/com/thealgorithms/backtracking/Permutation.java
+++ src/main/java/com/thealgorithms/backtracking/Permutation.java
@@ -1,6 +1,5 @@
 package com.thealgorithms.backtracking;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
--- src/test/java/com/thealgorithms/sorts/DutchNationalFlagSortTest.java
+++ src/test/java/com/thealgorithms/sorts/DutchNationalFlagSortTest.java
@@ -1,8 +1,6 @@
 package com.thealgorithms.sorts;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DutchNationalFlagSortTest {
