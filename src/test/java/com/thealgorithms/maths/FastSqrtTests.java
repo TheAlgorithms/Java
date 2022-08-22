9b144cc084dc42f7b8aae46b19cb777370779548
siddhant2002 <siddhantswarupmallick@gmail.com>
2022-08-22 12:56:34 +0530

    Fixes #3199

--- src/test/java/com/thealgorithms/maths/FastSqrtTests.java
+++ src/test/java/com/thealgorithms/maths/FastSqrtTests.java
@@ -9,47 +9,47 @@ public class FastSqrtTests {
 		assertTrue(FastSqrt.inverseSqrt(1332,0.027388954535126686));
         // calls for the 2nd inverse method
     }
-	// @Test
-	// void testForOneElement() 
-	// {
-	// 	assertTrue(FastSqrt.inverseSqrt(1332f,0.027388955));
-    //     // calls for the 1st inverse method
-	// }
+	@Test
+	void testForsecond() 
+	{
+		assertTrue(FastSqrt.inverseSqrt(1332f,0.027388955));
+        // calls for the 1st inverse method
+	}
 
-	// @Test
-	// void testForThreeElements() 
-	// {
-	// 	assertTrue(FastSqrt.inverseSqrt(1,0.9983071684837341));
-	// }
+	@Test
+	void testForThird() 
+	{
+		assertTrue(FastSqrt.inverseSqrt(1,0.9983071684837341));
+	}
     
-	// @Test
-	// void testForFourElements() 
-	// {
-	// 	assertTrue(FastSqrt.inverseSqrt(1f,0.99830717));
-	// }
+	@Test
+	void testForFourth() 
+	{
+		assertTrue(FastSqrt.inverseSqrt(1f,0.99830717));
+	}
 
-	// @Test
-	// void testForFiveElements() 
-	// {
-	// 	assertTrue(FastSqrt.inverseSqrt(4522,0.014867560006678104));
-	// }
+	@Test
+	void testForFifth() 
+	{
+		assertTrue(FastSqrt.inverseSqrt(4522,0.014867560006678104));
+	}
     
-	// @Test
-	// void testForSixElements() 
-	// {
-	// 	assertTrue(FastSqrt.inverseSqrt(4522f,0.01486756));
-	// }
+	@Test
+	void testForSixth() 
+	{
+		assertTrue(FastSqrt.inverseSqrt(4522f,0.01486756));
+	}
 
-	// @Test
-	// void testForSevenElements() 
-	// {
-	// 	assertTrue(FastSqrt.inverseSqrt(21,0.218117818236351));
-    // }
+	@Test
+	void testForSeventh() 
+	{
+		assertTrue(FastSqrt.inverseSqrt(21,0.218117818236351));
+    }
 
-	// @Test
-	// void testForEightElements() 
-	// {
-	// 	assertTrue(FastSqrt.inverseSqrt(21f,0.21811782));
-	// }
+	@Test
+	void testForEighth() 
+	{
+		assertTrue(FastSqrt.inverseSqrt(21f,0.21811782));
+	}
 }
 
