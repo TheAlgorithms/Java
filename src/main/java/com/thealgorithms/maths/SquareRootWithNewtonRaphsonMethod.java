fde6af389acdb066bb7835da1acef4280b3919a7
Shashwat Gupta <shashwatg79802@gmail.com>
2022-08-26 11:45:27 +0530

    Documentation changes

--- src/main/java/com/thealgorithms/maths/SquareRootWithNewtonRaphsonMethod.java
+++ src/main/java/com/thealgorithms/maths/SquareRootWithNewtonRaphsonMethod.java
@@ -23,7 +23,7 @@ public class SquareRootWithNewtonRaphsonMethod {
         double x = n;                         //initially taking a guess that x = n.
         double root = 0.5 * (x + n/x);        //applying Newton-Raphson Method.
 
-        while (Math.abs(root - x) > 0.0000001) {      //root - x = error and error < 0.01, 0.01 is the precision value taken over here.
+        while (Math.abs(root - x) > 0.0000001) {      //root - x = error and error < 0.0000001, 0.0000001 is the precision value taken over here.
 
             x = root;                         //decreasing the value of x to root, i.e. decreasing the guess.
             root = 0.5 * (x + n/x);
