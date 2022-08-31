325729aebb07849869ed33cae7fa8ac53c4b616a
Akshay Dubey <akshay.dubey055@gmail.com>
2022-08-31 15:00:53 +0530

    Add unit tests for mobius function

---
+++ src/test/java/com/thealgorithms/maths/MobiusFunctionTest.java
@@ -0,0 +1,57 @@
+package com.thealgorithms.maths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MobiusFunctionTest {
+
+	@Test
+	void testMobiusForZero() {
+		//given
+		int number = 0;
+		int expectedValue = 1;
+
+		//when
+		int actualValue = MobiusFunction.mobius(number);
+
+		//then
+		assertEquals(expectedValue, actualValue);
+	}
+
+	@Test
+	void testMobiusForNegativeNumber() {
+		//given
+		int number = -1;
+		int expectedValue = 1;
+
+		//when
+		int actualValue = MobiusFunction.mobius(number);
+
+		//then
+		assertEquals(expectedValue, actualValue);
+	}
+	
+	@Test
+	void testMobiusFunction(){
+		int[] expectedResultArray = {
+				1, 1, -1, -1, 0, -1, 1, -1, 0, 0, 1, -1, 0, -1, 1, 1, 0, -1, 0, -1, 0, 1, 1, -1, 0, 
+				0, 1, 0, 0, -1, -1, -1, 0, 1, 1, 1, 0, -1, 1, 1, 0, -1, -1, -1, 0, 0, 1, -1, 0, 0, 
+				0, 1, 0, -1, 0, 1, 0, 1, 1, -1, 0, -1, 1, 0, 0, 1, -1, -1, 0, 1, -1, -1, 0, -1, 1, 
+				0, 0, 1, -1, -1, 0, 0, 1, -1, 0, 1, 1, 1, 0, -1, 0, 1, 0, 1, 1, 1, 0, -1, 0, 0, 0
+		};
+
+		for(int i = 0; i <= 100; i++) {
+
+			//given
+			int expectedValue = expectedResultArray[i];
+
+			//when
+			int actualValue = MobiusFunction.mobius(i);
+
+			//then
+			assertEquals(expectedValue,actualValue);
+		}
+	}
+
+}
