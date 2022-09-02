3425aeebb8c709f506aacbeb547b04c87002ec87
poorvadiwan <diwanpoorva07@gmail.com>
2022-09-02 22:25:47 +0530

    This program calculates perimeter

---
+++ src/main/java/com/thealgorithms/maths/Perimeter.java
@@ -0,0 +1,44 @@
+package com.thealgorithms.maths;
+
+public class Perimeter {
+    // Perimeter of different 2D geometrical shapes
+
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     *Calculate the Perimeter of polygon.
+
+     * @parameter length of side.
+     * @parameter number of sides.
+     * @return Perimeter of given polygon
+     */
+    public static float perimeter_polygon( int n, float side){
+        float perimeter = n*side;
+        return perimeter;
+    }
+
+    /**
+     *Calculate the Perimeter of rectangle.
+
+     * @parameter length and breadth.
+     * @return Perimeter of given rectangle
+     */
+    public static float perimeter_rectangle( float len, float brea){
+        float perimeter = 2*(len + brea);
+        return perimeter;
+    }
+
+    /**
+     *Calculate the circumference of circle.
+
+     * @parameter radius of circle.
+     * @return circumference of given circle.
+     */
+    public static double circumference( float r){
+        double circumference = 2*Math.PI*r;
+        return circumference;
+    }
+
+}
