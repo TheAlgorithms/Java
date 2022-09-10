a8d5deee51e35ac17c0becccc89c7403d76cdedc
Mann <68820639+Mann-tech13@users.noreply.github.com>
2022-09-11 01:41:21 +0530

    Update Isomorphic.java

--- src/main/java/com/thealgorithms/strings/Isomorphic.java
+++ src/main/java/com/thealgorithms/strings/Isomorphic.java
@@ -17,46 +17,28 @@ public class Isomorphic {
     // character of first string as KEY and another as VALUE
     // now check occurence by keeping the track with SET data structure
     // -------------------------------------------------------------
-    Map<Character, Character> map = new HashMap<Character, Character>();
-    Set<Character> set = new HashSet<Character>();
+    Map<Character, Character> mapCharactersFrequency = new HashMap<Character, Character>();
+    Set<Character> trackUinqueCharacter = new HashSet<Character>();
     
     for(int i=0; i<s.length(); i++){
-        if(map.containsKey(s.charAt(i))){
-            if(t.charAt(i) != map.get(s.charAt(i))){
+        if(mapCharactersFrequency.containsKey(s.charAt(i))){
+            if(t.charAt(i) != mapCharactersFreq.get(s.charAt(i))){
                 return false;
             }
         }
         else{
-            if(set.contains(t.charAt(i))){
+            if(trackUinqueCharacter.contains(t.charAt(i))){
                 return false;
             }
             
-            map.put(s.charAt(i), t.charAt(i));
+            mapCharactersFrequency.put(s.charAt(i), t.charAt(i));
         }
-        set.add(t.charAt(i));
+        trackUinqueCharacter.add(t.charAt(i));
     }
     
     // -------------------------------------------------------------
 
     
     return true;
-}
-
-  public static void main(String[] args) {
-
-    String str1 = new String();
-    String str2 = new String();
-
-    // Taking input of strings from user
-    // ---------------------------------------
-    Scanner sc = new Scanner(System.in);
-    str1 = sc.nextLine();
-    str2 = sc.nextLine();
-    // ---------------------------------------
-
-    // Checking is two strings are isomorphic
-    boolean ans = checkStrings(str1, str2);
-
-    System.out.println(ans);
   }
 }
