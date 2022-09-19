971c1bb579cf6340c1562cbd44c97216cbbfc522
Akshay Dubey <akshay.dubey055@gmail.com>
2022-09-19 11:54:54 +0530

    fix: Fix pFactors method

--- src/main/java/com/thealgorithms/maths/PrimeFactorization.java
+++ src/main/java/com/thealgorithms/maths/PrimeFactorization.java
@@ -1,23 +1,39 @@
 package com.thealgorithms.maths;
 
+/*
+ * Authors: 
+ * (1) Aitor Fidalgo Sánchez (https://github.com/aitorfi) 
+ * (2) Akshay Dubey (https://github.com/itsAkshayDubey)
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactorization {
 
-    public static void pfactors(int n) {
+	static List<Integer> primeFactors = new ArrayList<>();
 
-        while (n % 2 == 0) {
-            System.out.print(2 + " ");
-            n /= 2;
-        }
+	public static List<Integer> pfactors(int n) {
 
-        for (int i = 3; i <= Math.sqrt(n); i += 2) {
-            while (n % i == 0) {
-                System.out.print(i + " ");
-                n /= i;
-            }
-        }
+		if (n == 0) {
+			return primeFactors;
+		}
 
-        if (n > 2) {
-            System.out.print(n);
-        }
-    }
+		while (n % 2 == 0) {
+			primeFactors.add(2);
+			n /= 2;
+		}
+
+		for (int i = 3; i <= Math.sqrt(n); i += 2) {
+			while (n % i == 0) {
+				primeFactors.add(i);
+				n /= i;
+			}
+		}
+
+		if (n > 2) {
+			primeFactors.add(n);
+		}
+		return primeFactors;
+	}
 }
