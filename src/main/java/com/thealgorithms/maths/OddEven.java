66fc8c82ef580122e692e7e6e94f071caf71cbbc
Aniket Bhattacharjee <appsoftlimited@gmail.com>
2022-09-21 20:10:34 +0530

    Create OddEven.java

---
+++ src/main/java/com/thealgorithms/maths/OddEven.java
@@ -0,0 +1,17 @@
+public class OddEven
+{
+public static void main(int n) // Let's suppose n to be 3
+{
+if(n%2!=0) // 3%2!=0 (True)
+System.out.println(n+"is Odd.");
+else // if n is even 
+System.out.println(n+" is Even.")
+}
+}
+
+/**
+To check if a number is even, you can use-
+if(n%2==0)
+*/
+
+
