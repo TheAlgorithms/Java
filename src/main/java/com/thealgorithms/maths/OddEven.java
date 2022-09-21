1d64d4bcf6916715678f69a1177f9c0808c1990e
Aniket Bhattacharjee <appsoftlimited@gmail.com>
2022-09-21 20:22:44 +0530

    Fixed typo

--- src/main/java/com/thealgorithms/maths/OddEven.java
+++ src/main/java/com/thealgorithms/maths/OddEven.java
@@ -5,7 +5,7 @@ public static void main(int n) // Let's suppose n to be 3
 if(n%2!=0) // 3%2!=0 (True)
 System.out.println(n+"is Odd.");
 else // if n is even 
-System.out.println(n+" is Even.")
+System.out.println(n+" is Even.");
 }
 }
 
