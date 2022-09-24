e89912dfe81acebfbda81c9402cd3d366072e1e8
Amarildo Aliaj <82412439+amarildoaliaj@users.noreply.github.com>
2022-09-24 22:11:23 +0200

    refactor: Reduced unnecessarily long test

--- src/test/java/com/thealgorithms/maths/CollatzConjectureTest.java
+++ src/test/java/com/thealgorithms/maths/CollatzConjectureTest.java
@@ -28,12 +28,8 @@ class CollatzConjectureTest {
 
     @Test
     void collatzConjecture() {
-        final List<Integer> expected = List.of(
-                850, 425, 1276, 638, 319, 958, 479, 1438, 719, 2158, 1079, 3238, 1619, 4858, 2429,
-                7288, 3644, 1822, 911, 2734, 1367, 4102, 2051, 6154, 3077, 9232, 4616, 2308, 1154,
-                577, 1732, 866, 433, 1300, 650, 325, 976, 488, 244, 122, 61, 184, 92, 46, 23, 70,
-                35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1);
-        assertIterableEquals(expected, cConjecture.collatzConjecture(850));
+        final List<Integer> expected = List.of(35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1);
+        assertIterableEquals(expected, cConjecture.collatzConjecture(35));
     }
 
     @Test
