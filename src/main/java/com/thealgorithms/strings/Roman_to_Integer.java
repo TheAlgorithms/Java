53bdf65a99e8a089b808bf7f748439badd623193
Timur Jumaev <43972229+skinox@users.noreply.github.com>
2022-09-30 16:05:48 -0400

    Create Roman_to_Integer.java
    Here is the algorithm how to convert Roman to Integer

---
+++ src/main/java/com/thealgorithms/strings/Roman_to_Integer.java
@@ -0,0 +1,31 @@
+class Roman_to_Integer {
+    public int romanToInt(String s) {
+        int result = 0;
+        char[] arr1 = s.toCharArray();
+        for(char ch: arr1){
+            switch (ch) {
+                case 'I' -> result += 1;
+                case 'V' -> result += 5;
+                case 'X' -> result += 10;
+                case 'L' -> result += 50;
+                case 'C' -> result += 100;
+                case 'D' -> result += 500;
+                case 'M' -> result += 1000;
+                default -> {
+                }
+            }
+        }
+        
+        for(int i=0; i<arr1.length-1;i++){
+            if(arr1[i]==('I')&&(arr1[i+1]==('V')||arr1[i+1]==('X'))){
+                result-=2;
+            }else if(arr1[i]==('X')&&(arr1[i+1]==('L')||arr1[i+1]==('C'))){
+                result-=20;
+            }else if(arr1[i]==('C')&&(arr1[i+1]==('D')||arr1[i+1]==('M'))){
+                result-=200;
+            }
+        }
+        
+         return result;
+    }
+}
