47867bc77c2e4989267e3876d21332f5b5009279
Ricardo Ramos <ricardo.ramos.moura@hotmail.com>
2022-10-01 12:44:11 -0300

    algorithm to verify that the characteristic is letter or number

---
+++ src/main/java/com/thealgorithms/others/CheckIsNumberOrLetter.java
@@ -0,0 +1,27 @@
+/**
+ * checks if a character is a number or a letter
+ */
+package com.thealgorithms.others;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CheckIsNumberOrLetter {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a text containing letters and numbers: ");
+
+        String inputString = bufferedReader.readLine();
+
+        for(int i=0; i < inputString.length(); i++) {
+            Boolean check = Character.isDigit(inputString.charAt(i));
+            if(check) {
+                System.out.println(inputString.charAt(i) + " is a number!");
+            } else {
+                System.out.println(inputString.charAt(i) + " is a letter!");
+            }
+        }
+    }
+}
