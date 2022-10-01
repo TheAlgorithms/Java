633704dc38871e61d9a9fad935701fd9b6b665d6
Mike <mikeshappy13@gmail.com>
2022-10-01 11:46:12 -0400

    Added Euler's Totient Function and test cases

--- src/main/java/com/thealgorithms/maths/TotientFunction.java
+++ src/main/java/com/thealgorithms/maths/TotientFunction.java
@@ -3,6 +3,7 @@ package com.thealgorithms.maths;
 // Euler's Totient Function takes in 
 // some value n and returns the number of integers in {1,...,n-1} 
 // that are relatively prime to n. 
+// https://en.wikipedia.org/wiki/Euler%27s_totient_function
 
 public class TotientFunction
 {
