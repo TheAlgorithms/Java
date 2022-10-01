92792f1d3060f0af2bebe0574c1f85890ef643b2
ff124012 <44043869+ff124012@users.noreply.github.com>
2022-10-02 00:43:46 +0900

    Add test for Lower

--- src/test/java/com/thealgorithms/strings/LowerTest.java
+++ src/test/java/com/thealgorithms/strings/LowerTest.java
@@ -10,6 +10,7 @@ public class LowerTest {
         String input1 = "hello world";
         String input2 = "HelLO WoRld";
         String input3 = "HELLO WORLD";
+        
         assertEquals("hello world", Lower.toLowerCase(input1));
         assertEquals("hello world", Lower.toLowerCase(input2));
         assertEquals("hello world", Lower.toLowerCase(input3));
