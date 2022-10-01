13d4573aa9b59f6b06949b88c3d6977f4852c8ab
Mike <mikeshappy13@gmail.com>
2022-10-01 11:41:25 -0400

    Added Euler's Totient Function and tests

---
+++ src/main/java/com/thealgorithms/maths/TotientFunction.java
@@ -0,0 +1,32 @@
+package com.thealgorithms.maths;
+
+// Euler's Totient Function takes in 
+// some value n and returns the number of integers in {1,...,n-1} 
+// that are relatively prime to n. 
+
+public class TotientFunction
+{
+    //Recursive GCD function
+    public static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+
+    //Simple iterative Euler Totient Function
+    public static int phi(int n)
+    {
+        //Initialize result
+        int totient = 1;
+        //Loop over the set of n-1 numbers
+        for (int i = 2; i < n; ++i)
+        {
+            //i is relatively prime to n if gcd(i, n) == 1 so we increment totient
+            if (gcd(i, n) == 1)
+                totient++;
+        }
+        //return the resulting totient
+        return totient;
+    }
+}
