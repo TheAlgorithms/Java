33a5a0a08af026f660a3c88e91d0a974ed8b038f
Ricardo Ramos <ricardo.ramos.moura@hotmail.com>
2022-10-01 10:45:55 -0300

    checks if a character is a number or a letter

--- src/main/java/com/thealgorithms/strings/CheckIsNumberOrLetter.java
+++ src/main/java/com/thealgorithms/strings/CheckIsNumberOrLetter.java
@@ -7,7 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class NumberOrCharacterIsDigitTest {
+public class CheckIsNumberOrLetter {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
