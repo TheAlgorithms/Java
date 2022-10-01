7d012658e311d6c8f699509685b4f128face8832
Rohit Singh <rohitkumarsingh875.hitit2020@gmail.com>
2022-10-01 17:01:06 +0530

    added a main function to the program SquareRootWithNewtonRaphsonMethod.java

--- src/main/java/com/thealgorithms/maths/SquareRootWithNewtonRaphsonMethod.java
+++ src/main/java/com/thealgorithms/maths/SquareRootWithNewtonRaphsonMethod.java
@@ -18,6 +18,8 @@ import java.util.Scanner;
 
 public class SquareRootWithNewtonRaphsonMethod {
 
+
+    //main func
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.println("enter the number");
@@ -31,7 +33,7 @@ public class SquareRootWithNewtonRaphsonMethod {
         double x = n;                         //initially taking a guess that x = n.
         double root = 0.5 * (x + n/x);        //applying Newton-Raphson Method.
 
-        while (Math.abs(root - x) > 0.0000001) {      //root - x = error and error < 0.0000001, 0.0000001 is the precision value taken over here.
+        while (Math.abs(root - x) > 0.000001) {      //root - x = error and error < 0.0000001, 0.0000001 is the precision value taken over here.
 
             x = root;                         //decreasing the value of x to root, i.e. decreasing the guess.
             root = 0.5 * (x + n/x);
