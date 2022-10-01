6771ada20bf92834a45c9f046924494386bf35d9
Pradeep Kurapati <kurapatipradeep0@gmail.com>
2022-10-01 13:53:29 +0530

    Added Array Datastructure and insertion operation

---
+++ src/test/java/com/thealgorithms/datastructures/array/ArrayInsertion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArrayInsertion {
+    public static void main(String[] args) {
+        System.out.print("Enter size of the Array: ");
+        Scanner input = new Scanner(System.in);
+        int n= input.nextInt();
+        System.out.println("Enter "+n+" elements: ");
+        int[] insert=new int[n];
+        for (int i = 0; i < n; i++)
+            insert[i]=input.nextInt();
+        System.out.print("Your array is: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(insert[i]+" ");
+        }
+    }
+}
