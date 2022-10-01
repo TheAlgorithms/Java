394429d5b26463b863bb5b41c7b383db7d4be983
Rohit Singh <rohitkumarsingh875.hitit2020@gmail.com>
2022-10-01 16:41:07 +0530

    added a main function

--- src/main/java/com/thealgorithms/maths/SquareRootWithNewtonRaphsonMethod.java
+++ src/main/java/com/thealgorithms/maths/SquareRootWithNewtonRaphsonMethod.java
@@ -18,6 +18,14 @@ import java.util.Scanner;
 
 public class SquareRootWithNewtonRaphsonMethod {
 
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the number");
+        int a = sc.nextInt();
+        System.out.println(squareRoot(a));
+        sc.close();
+    }
+
     public static double squareRoot (int n) {
 
         double x = n;                         //initially taking a guess that x = n.
