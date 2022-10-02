8ae1bcf7764a736604bdea23a2bcbfb6d529871c
Gaurav Gautam <gautamgaurav945@gmail.com>
2022-10-02 10:55:18 +0530

    bit manipulation module added with some theory and conversion

---
+++ src/main/java/com/thealgorithms/Bit_Manipulation/Decimal_TO_Binary__USING_INBUILT_METHOD.java
@@ -0,0 +1,16 @@
+package com.thealgorithms.Bit_Manipulation;
+// The Integer.toBinaryString() method converts decimal to binary string. The signature of toBinaryString() method is given below:
+
+
+
+
+public class Decimal_TO_Binary__USING_INBUILT_METHOD {
+    public static void main(String args[]){  
+        System.out.println(Integer.toBinaryString(10));  
+        System.out.println(Integer.toBinaryString(21));  
+        System.out.println(Integer.toBinaryString(31));  
+        } 
+}
+
+
+//Read more at https://www.javatpoint.com/java-decimal-to-binary
