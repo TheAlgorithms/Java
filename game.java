6104c4391ef7775d26adf1427a3fd95aea46eea6
Sai-Tejaswi-kallepalli <84557695+Sai-Tejaswi-kallepalli@users.noreply.github.com>
2022-10-03 23:14:09 +0530

    Add files via upload

---
+++ game.java
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <conio.h>
+
+char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+int checkwin();
+void board();
+
+int main()
+{
+    int player = 1, i, choice;
+
+    char mark;
+    do
+    {
+        board();
+        player = (player % 2) ? 1 : 2;
+
+        printf("Player %d, enter a number:  ", player);
+        scanf("%d", &choice);
+
+        mark = (player == 1) ? 'X' : 'O';
+
+        if (choice == 1 && square[1] == '1')
+            square[1] = mark;
+            
+        else if (choice == 2 && square[2] == '2')
+            square[2] = mark;
+            
+        else if (choice == 3 && square[3] == '3')
+            square[3] = mark;
+            
+        else if (choice == 4 && square[4] == '4')
+            square[4] = mark;
+            
+        else if (choice == 5 && square[5] == '5')
+            square[5] = mark;
+            
+        else if (choice == 6 && square[6] == '6')
+            square[6] = mark;
+            
+        else if (choice == 7 && square[7] == '7')
+            square[7] = mark;
+            
+        else if (choice == 8 && square[8] == '8')
+            square[8] = mark;
+            
+        else if (choice == 9 && square[9] == '9')
+            square[9] = mark;
+            
+        else
+        {
+            printf("Invalid move ");
+
+            player--;
+            getch();
+        }
+        i = checkwin();
+
+        player++;
+    }while (i ==  - 1);
+    
+    board();
+    
+    if (i == 1)
+        printf("==>\aPlayer %d win ", --player);
+    else
+        printf("==>\aGame draw");
+
+    getch();
+
+    return 0;
+}
+
+/*********************************************
+
+FUNCTION TO RETURN GAME STATUS
+1 FOR GAME IS OVER WITH RESULT
+-1 FOR GAME IS IN PROGRESS
+O GAME IS OVER AND NO RESULT
+ **********************************************/
+
+int checkwin()
+{
+    if (square[1] == square[2] && square[2] == square[3])
+        return 1;
+        
+    else if (square[4] == square[5] && square[5] == square[6])
+        return 1;
+        
+    else if (square[7] == square[8] && square[8] == square[9])
+        return 1;
+        
+    else if (square[1] == square[4] && square[4] == square[7])
+        return 1;
+        
+    else if (square[2] == square[5] && square[5] == square[8])
+        return 1;
+        
+    else if (square[3] == square[6] && square[6] == square[9])
+        return 1;
+        
+    else if (square[1] == square[5] && square[5] == square[9])
+        return 1;
+        
+    else if (square[3] == square[5] && square[5] == square[7])
+        return 1;
+        
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
+        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] 
+        != '7' && square[8] != '8' && square[9] != '9')
+
+        return 0;
+    else
+        return  - 1;
+}
+
+
+/*******************************************************************
+FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
+ ********************************************************************/
+
+
+void board()
+{
+    system("cls");
+    printf("\n\n\tTic Tac Toe\n\n");
+
+    printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+
+
+    printf("     |     |     \n");
+    printf("  %c  |  %c  |  %c \n", square[1], square[2], square[3]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[4], square[5], square[6]);
+
+    printf("_____|_____|_____\n");
+    printf("     |     |     \n");
+
+    printf("  %c  |  %c  |  %c \n", square[7], square[8], square[9]);
+
+    printf("     |     |     \n\n");
+}
