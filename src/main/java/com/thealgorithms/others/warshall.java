3a4823c879ead3dfa564825fe24841304d39e9e4
Sriram Bharadwaj <92371562+SriramBharadwaj1@users.noreply.github.com>
2022-10-03 15:46:01 +0530

    warshasll algorithm

---
+++ src/main/java/com/thealgorithms/others/warshall.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class warshall
+{
+	static void update(double[][] paths, int n, int medial)
+	{
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				double d=paths[i][medial]+paths[medial][j];
+				if(d<paths[i][j])
+				{
+					paths[i][j]=d;
+				}
+			}
+		}
+	}
+
+	static void func(double[][] paths,int n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			update(paths,n,i);
+			System.out.println("after iteration "+i);
+			for(double[] j:paths)
+		{
+			System.out.println(Arrays.toString(j));
+		}
+		}
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		Scanner x=new Scanner(System.in);
+		System.out.println("P. Sriram Bhardwaj");
+		System.out.println("Regd.no - 122010308005");
+		System.out.println(" ");
+		System.out.println("All pairs shortest path");
+		System.out.println(" ");
+		 System.out.println("enter the number of elements");
+		int n=x.nextInt();
+		
+		double[][] paths=new double[n][n];
+		double inf=Double.POSITIVE_INFINITY;
+		for(int i=0;i<n;i++)
+		{	
+			for(int j=0;j<n;j++)
+			{	System.out.println("enter element "+i+" "+j);
+				double d=x.nextDouble();
+				if(d==0)
+				{
+					if(i==j)
+					{
+						paths[i][j]=d;
+					}
+					
+					else
+					{
+						paths[i][j]=inf;
+					}
+				}
+				else
+				{
+					paths[i][j]=d;
+				}
+			}
+		}
+		
+		func(paths,n);
+		System.out.println("the shortest paths are");
+		for(double[] i:paths)
+		{
+			System.out.println(Arrays.toString(i));
+		}
+	}
+}
+
+			
+		
+		
