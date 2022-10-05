b5a6af210085031b9e6c5b7c24b691d5d07eb214
dobariyapurva2003 <107313472+dobariyapurva2003@users.noreply.github.com>
2022-10-05 19:02:59 +0530

    WithoutDuplication

---
+++ src/main/java/com/thealgorithms/maths/WithoutDuplication.java
@@ -0,0 +1,26 @@
+//Here this code generate 4 digit numbers which are combinations of
+//given digits without repeatation or duplication.
+
+import java.util.*;
+class WithoutDuplication {
+    public static void main(String[] args)
+    {
+        int[] a = new int[4];
+        Scanner input = new Scanner(System.in);
+        for(int i=0;i<4;i++){
+            System.out.println("Enter "+(i+1)+" number : ");
+            a[i] = input.nextInt();
+        }
+        for(int w = 0;w<4;w++){
+            for(int x = 0;x<4;x++){
+                for(int y = 0;y<4;y++){
+                    for(int z = 0;z<4;z++){
+                        if(w!=x && w!=y && w!=z && x!=y && y!=z && z!=x) {
+                            System.out.println(a[w] + "" + a[x] + "" + a[y] + "" + a[z]);
+                        }
+                        }
+                }
+            }
+        }
+    }
+}
