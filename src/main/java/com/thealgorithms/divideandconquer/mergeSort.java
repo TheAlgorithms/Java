8c3a4779ff3d72e7a03f865e5b5a82b4994a9a02
Ayush Raj <rajayush0751@gmail.com>
2022-10-06 19:58:54 +0530

    added merge sort in divide and conquer folder

---
+++ src/main/java/com/thealgorithms/divideandconquer/mergeSort.java
@@ -0,0 +1,81 @@
+package com.thealgorithms.divideandconquer;
+
+// Merge sort is similar to the quick sort algorithm as it uses the divide and conquer approach to sort the elements.
+// The average case time complexity of merge sort is O(n*logn).
+// The space complexity of merge sort is O(n)
+
+class Merge {
+
+    void merge(int a[], int beg, int mid, int end) {
+        int i, j, k;
+        int n1 = mid - beg + 1;
+        int n2 = end - mid;
+
+        int LeftArray[] = new int[n1];
+        int RightArray[] = new int[n2];
+
+        for (i = 0; i < n1; i++)
+            LeftArray[i] = a[beg + i];
+        for (j = 0; j < n2; j++)
+            RightArray[j] = a[mid + 1 + j];
+
+        i = 0; 
+        j = 0;
+        k = beg; 
+
+        while (i < n1 && j < n2) {
+            if (LeftArray[i] <= RightArray[j]) {
+                a[k] = LeftArray[i];
+                i++;
+            } else {
+                a[k] = RightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            a[k] = LeftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            a[k] = RightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    void mergeSort(int a[], int beg, int end) {
+        if (beg < end) {
+            int mid = (beg + end) / 2;
+            mergeSort(a, beg, mid);
+            mergeSort(a, mid + 1, end);
+            merge(a, beg, mid, end);
+        }
+    }
+
+    /* Function to print the array */
+    void printArray(int a[], int n) {
+        int i;
+        for (i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
+    }
+
+}
+
+
+ public class mergeSort {
+
+    public static void main(String[] args) {
+        int a[] = { 11, 30, 24, 7, 31, 16, 39, 41 };
+        int n = a.length;
+        Merge m1 = new Merge();
+        System.out.println("\nBefore sorting array elements are - ");
+        m1.printArray(a, n);
+        m1.mergeSort(a, 0, n - 1);
+        System.out.println("\nAfter sorting array elements are - ");
+        m1.printArray(a, n);
+        System.out.println("");
+    }
+}
