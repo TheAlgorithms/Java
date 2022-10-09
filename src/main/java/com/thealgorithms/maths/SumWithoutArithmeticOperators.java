52278f6283ff72e5cab260fe8b383d77d58ac9fc
samratpodder <iamsamrat16@outlook.com>
2022-10-10 01:50:44 +0530

    Added reference link to algorithm

--- src/main/java/com/thealgorithms/maths/SumWithoutArithmeticOperators.java
+++ src/main/java/com/thealgorithms/maths/SumWithoutArithmeticOperators.java
@@ -5,6 +5,7 @@ public class SumWithoutArithmeticOperators {
     /**
         * Calculate the sum of two numbers a and b without using any arithmetic operators (+, -, *, /).
         * All the integers associated are unsigned 32-bit integers
+        *https://stackoverflow.com/questions/365522/what-is-the-best-way-to-add-two-numbers-without-using-the-operator
         *@param a - It is the first number 
         *@param b - It is the second number
         *@return returns an integer which is the sum of the first and second number
