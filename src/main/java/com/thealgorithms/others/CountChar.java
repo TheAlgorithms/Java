cb332334d349d8cf575bce0ce65b7080d0f636c7
Ricardo Ramos <ricardo.ramos.moura@hotmail.com>
2022-10-10 09:18:09 -0300

    Removed main method

--- src/main/java/com/thealgorithms/others/CountChar.java
+++ src/main/java/com/thealgorithms/others/CountChar.java
@@ -4,14 +4,6 @@ import java.util.Scanner;
 
 public class CountChar {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your text: ");
-        String str = input.nextLine();
-        input.close();
-        System.out.println("There are " + CountCharacters(str) + " characters.");
-    }
-
     /**
      * Count non space character in string
      *
