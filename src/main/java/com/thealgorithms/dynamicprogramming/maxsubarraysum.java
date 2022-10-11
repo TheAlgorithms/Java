7ffe3640665c0a65119bd9001334b03e207f625a
Suryansh <suryanshchaudhary28@gmail.com>
2022-10-12 01:28:10 +0530

    Added max subarray sum using DP in O(N)

---
+++ src/main/java/com/thealgorithms/dynamicprogramming/maxsubarraysum.java
@@ -0,0 +1,20 @@
+public class Kadane {
+	public static void main(String[] args)
+	{
+		int[] a = { -2, -3, 4, -1, -2, 1, 5, -3 };
+		System.out.println("Maximum contiguous sum is " + maxSubArraySum(a));
+	}
+	public static int maxSubArraySum(int a[])
+	{
+		int size = a.length;
+		int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+		for (int i = 0; i < size; i++) {
+			max_ending_here = max_ending_here + a[i];
+			if (max_so_far < max_ending_here)
+				max_so_far = max_ending_here;
+			if (max_ending_here < 0)
+				max_ending_here = 0;
+		}
+		return max_so_far;
+	}
+}
