d21fcf57b1a6fc113c2357bd64d6b50d1f4276c1
thenuka99 <janithherath2@gmail.com>
2022-10-11 14:39:00 +0530

    java-Factorial

---
+++ src/main/java/com/thealgorithms/others/Factorial.java
@@ -0,0 +1,25 @@
+package com.thealgorithms.others;
+
+import java.util.Scanner;
+
+/**
+ * You can read more about Factorial
+ *
+ *
+ * See https://en.wikipedia.org/wiki/Factorial
+ */
+public class Factorial {
+
+    public static void main(String[] args) {
+        // Get input from the user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("inupt the number:");
+        int num = sc.nextInt();
+        long factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+
+    public static long multiplyNumbers(int num) {
+        return num >= 1 ? (long) num * multiplyNumbers(num - 1) : 1L;
+    }
+}
