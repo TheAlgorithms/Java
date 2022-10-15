505107d6063676806d1c83ae54975b39c273d40d
Darshan Balaji Rathod <90858956+drashan14@users.noreply.github.com>
2022-10-15 10:40:37 +0530

    Update all changes.

--- Maxsumarray.java
+++ Maxsumarray.java
@@ -1,23 +1,23 @@
 //Using Kadane's Algorithm.
-import java.util.*;//Import all the libraries.
+import java.util.*;//Import all Classes.
 
 public class kadenes
 {
     public static void main(String[] args) {
         Scanner s1=new Scanner(System.in);
-        int n=s1.nextInt();//Input for size of the array.
+        int n=s1.nextInt();
         int arr[]=new int[n];
         
         for (int i=0;i<n;i++)
         {
-             arr[i]=s1.nextInt();//Store the element one by one according to the size of array.
+             arr[i]=s1.nextInt();
         }
-        int sum=0;//Initialize sum and maximum variable .
+        int sum=0;//Initialize variable.
         int maxsum=0; 
         for (int j=0;j<arr.length;j++)
         {
                    sum=sum+arr[j];
-                   maxsum=Math.max(maxsum,sum);//Using math function get the maximum value.
+                   maxsum=Math.max(maxsum,sum);//Using math function.
                    if(sum<0){
                     sum=0;
                    }
