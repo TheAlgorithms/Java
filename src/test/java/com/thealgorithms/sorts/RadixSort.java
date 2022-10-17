d244399a363ecad1b2eceee55f59a5066da304fe
pareshapraj <72244369+pareshapraj@users.noreply.github.com>
2022-10-17 22:10:37 +0530

    Added Radix Sort in Java Programming

---
+++ src/test/java/com/thealgorithms/sorts/RadixSort.java
@@ -0,0 +1,66 @@
+// Radix Sort in Java Programming
+
+import java.util.Arrays;
+
+class RadixSort {
+
+  // Using counting sort to sort the elements in the basis of significant places
+  void countingSort(int array[], int size, int place) {
+    int[] output = new int[size + 1];
+    int max = array[0];
+    for (int i = 1; i < size; i++) {
+      if (array[i] > max)
+        max = array[i];
+    }
+    int[] count = new int[max + 1];
+
+    for (int i = 0; i < max; ++i)
+      count[i] = 0;
+
+    // Calculate count of elements
+    for (int i = 0; i < size; i++)
+      count[(array[i] / place) % 10]++;
+
+    // Calculate cumulative count
+    for (int i = 1; i < 10; i++)
+      count[i] += count[i - 1];
+
+    // Place the elements in sorted order
+    for (int i = size - 1; i >= 0; i--) {
+      output[count[(array[i] / place) % 10] - 1] = array[i];
+      count[(array[i] / place) % 10]--;
+    }
+
+    for (int i = 0; i < size; i++)
+      array[i] = output[i];
+  }
+
+  // Function to get the largest element from an array
+  int getMax(int array[], int n) {
+    int max = array[0];
+    for (int i = 1; i < n; i++)
+      if (array[i] > max)
+        max = array[i];
+    return max;
+  }
+
+  // Main function to implement radix sort
+  void radixSort(int array[], int size) {
+    // Get maximum element
+    int max = getMax(array, size);
+
+    // Apply counting sort to sort elements based on place value.
+    for (int place = 1; max / place > 0; place *= 10)
+      countingSort(array, size, place);
+  }
+
+  // Driver code
+  public static void main(String args[]) {
+    int[] data = { 121, 432, 564, 23, 1, 45, 788 };
+    int size = data.length;
+    RadixSort rs = new RadixSort();
+    rs.radixSort(data, size);
+    System.out.println("Sorted Array in Ascending Order: ");
+    System.out.println(Arrays.toString(data));
+  }
+}
