ac25327d07df06dfa1a8bc5204740c8d40ccd4d5
Deepu Gupta <84670355+deepug12@users.noreply.github.com>
2022-10-21 00:58:45 +0530

    Added code for Sort a stack using another stack

---
+++ SortTheStack.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+class Solution
+{
+	public static Stack<Integer> sortstack(Stack<Integer>
+											input)
+	{
+		Stack<Integer> tmpStack = new Stack<Integer>();
+		while(!input.isEmpty())
+		{
+			int tmp = input.pop();
+			while(!tmpStack.isEmpty() && tmpStack.peek()
+												> tmp)
+			{
+			input.push(tmpStack.pop());
+			}
+			tmpStack.push(tmp);
+		}
+		return tmpStack;
+	}
+	
+	public static void main(String args[])
+	{
+		Stack<Integer> st = new Stack<Integer>();
+		st.add(34);
+		st.add(3);
+		st.add(31);
+		st.add(98);
+	
+		Stack<Integer> tmpStack = Solution(st);
+	
+		while (!tmpStack.empty())
+		{
+			System.out.print(tmpStack.pop()+" ");
+		}
+	}
+}
