44c55dee57cb046830620dff2179bce93e4e5b5f
Pradyumn Singh Rahar <rvrahar@gmail.com>
2022-10-20 15:38:08 +0530

    Create FactorsTest.java
    Added tests for Factors

---
+++ src/test/java/com/thealgorithms/maths/FactorsTest.java
@@ -0,0 +1,42 @@
+package com.thealgorithms.maths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FactorsTest {
+
+    @Test
+    public void test() {
+        ArrayList<Long> out = new ArrayList<Long>();
+        out.add(toLong(1));
+        assertEquals(new ArrayList<>(out, Factors.factors(1));
+    }
+
+    @Test
+    public void test() {
+        ArrayList<Long> out = new ArrayList<Long>();
+        out.add(toLong(1));
+        out.add(toLong(5));
+        assertEquals(new ArrayList<>(out, Factors.factors(5));
+    }
+
+    @Test
+    public void test() {
+        assertEquals(new ArrayList<>(), Factors.factors(-6));
+    }
+
+    @Test
+    public void test() {
+        ArrayList<Long> out = new ArrayList<Long>();
+        out.add(toLong(1));
+        out.add(toLong(2));
+        out.add(toLong(3));
+        out.add(toLong(6));
+        assertEquals(new ArrayList<>(out, Factors.factors(6));
+    }
+
+    private Long toLong(int n) {
+        return Long.valueOf(n);
+    }
+}
