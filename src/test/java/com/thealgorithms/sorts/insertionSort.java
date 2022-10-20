e96f3a47c169c7cd106f241666e941f9fddc7257
parte-Ajinkya <112530584+parte-Ajinkya@users.noreply.github.com>
2022-10-20 11:40:17 +0530

    Create insertionSort.java

---
+++ src/test/java/com/thealgorithms/sorts/insertionSort.java
@@ -0,0 +1,45 @@
+// Java program for implementation of Insertion Sort
+public class InsertionSort {
+	/*Function to sort array using insertion sort*/
+	void sort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+
+			/* Move elements of arr[0..i-1], that are
+			greater than key, to one position ahead
+			of their current position */
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+
+	/* A utility function to print array of size n*/
+	static void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+
+		System.out.println();
+	}
+
+	// Driver method
+	public static void main(String args[])
+	{
+		int arr[] = { 12, 11, 13, 5, 6 };
+
+		InsertionSort ob = new InsertionSort();
+		ob.sort(arr);
+
+		printArray(arr);
+	}
+};
+
+
+/* This code is contributed by Rajat Mishra. */
