bfc181b421830fba99ed42781e3370d60a9f4042
Ujjwal Aggarwal <96613056+UjjwalAggarwal-1@users.noreply.github.com>
2022-10-20 06:01:13 +0530

    Update ReverseString.java

--- src/main/java/com/thealgorithms/misc/ReverseString.java
+++ src/main/java/com/thealgorithms/misc/ReverseString.java
@@ -1,6 +1,9 @@
+//the reverseString function prints out the input string in reverse order, character by character
+//and for the check for base case, we stop the function whenever we reach the first (index 0) character of the string, for which the length of the string is 1
+
 import java.util.*;
 
-public class revString {
+public class ReverseString {
     public static void reverseString(String s, int length){
         System.out.print(s.charAt(length-1));
         if (length==1){
