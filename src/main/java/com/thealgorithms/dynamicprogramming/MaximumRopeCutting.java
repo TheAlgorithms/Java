d7de439f03625a034822b7f2c1bcbe0691db2222
Atharva Joshi <69634375+atharva20-coder@users.noreply.github.com>
2022-10-22 00:19:55 +0530

    Update MaximumRopeCutting.java

--- src/main/java/com/thealgorithms/dynamicprogramming/MaximumRopeCutting.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/MaximumRopeCutting.java
@@ -4,21 +4,16 @@ class RopeCutting
 	{
 		if(n == 0) return 0;
 		if(n < 0)  return -1;
- 
 		int res = Math.max(maxCuts(n-a, a, b, c), 
 		          Math.max(maxCuts(n-b, a, b, c), 
 		          maxCuts(n-c, a, b, c)));
- 
 		if(res == -1)
 			return -1;
- 
 		return res + 1; 
 	}
-  
     public static void main(String [] args) 
     {
     	int n = 5, a = 2, b = 1, c = 5;
-    	
     	System.out.println(maxCuts(n, a, b, c));
     }
 }
