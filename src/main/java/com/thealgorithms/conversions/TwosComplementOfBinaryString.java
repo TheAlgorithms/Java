c2b3b2b8473af1e584f7cf7c9472fbb513b55e34
Atharva Joshi <69634375+atharva20-coder@users.noreply.github.com>
2022-10-21 23:52:27 +0530

    Algo for Two's Complement of any Binary String

---
+++ src/main/java/com/thealgorithms/conversions/TwosComplementOfBinaryString.java
@@ -0,0 +1,43 @@
+// An efficient Java program to find 2's complement
+
+class Test
+{
+	// Method to find two's complement
+	static String findTwoscomplement(StringBuffer str)
+	{
+		int n = str.length();
+	
+		// Traverse the string to get first '1' from
+		// the last of string
+		int i;
+		for (i = n-1 ; i >= 0 ; i--)
+			if (str.charAt(i) == '1')
+				break;
+	
+		// If there exists no '1' concat 1 at the
+		// starting of string
+		if (i == -1)
+			return "1" + str;
+	
+		// Continue traversal after the position of
+		// first '1'
+		for (int k = i-1 ; k >= 0; k--)
+		{
+			//Just flip the values
+			if (str.charAt(k) == '1')
+				str.replace(k, k+1, "0");
+			else
+				str.replace(k, k+1, "1");
+		}
+	
+		// return the modified string
+		return str.toString();
+	}
+	
+	// Driver method
+	public static void main(String[] args)
+	{
+		StringBuffer str = new StringBuffer("00000101");
+		System.out.println(findTwoscomplement(str));
+	}
+}
