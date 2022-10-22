97e5c6d2f711f916086295851261b5b89bb3d200
Atharva Joshi <69634375+atharva20-coder@users.noreply.github.com>
2022-10-22 21:10:22 +0530

    Update TowerOfHanoi.java
    Added Scanner class

--- src/main/java/com/thealgorithms/dynamicprogramming/TowerOfHanoi.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/TowerOfHanoi.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
-package com.JournalDev;
-public class Main {
-    static void towerOfHanoi(int n, char from_rod, char to_rod, char helper_rod)
-    {
-        if (n == 1)
-        {
-            System.out.println("Take disk 1 from rod " +  from_rod + " to rod " + to_rod);
-            return;
-        }
-        towerOfHanoi(n-1, from_rod, helper_rod, to_rod);
-        System.out.println("Take disk " + n + " from rod " +  from_rod + " to rod " + to_rod);
-        towerOfHanoi(n-1, helper_rod, to_rod, from_rod);
-    }
+class TowerOfHanoi
+{
+	// Java recursive function to solve tower of hanoi puzzle
+	static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+	{
+		if (n == 1)
+		{
+			System.out.println("Move disk 1 from rod " + from_rod + " to rod " + to_rod);
+			return;
+		}
+		towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+		System.out.println("Move disk " + n + " from rod " + from_rod + " to rod " + to_rod);
+		towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+	}
 
     public static void main(String args[])
     {
