708e97f3710a6ab0b8ee1125079827c3178a7d36
Teuton Roy <popeyehk404@gmaill.com>
2022-10-22 19:28:27 +0530

    Radix Sort Test added

---
+++ src/test/java/com/thealgorithms/sorts/RadixSortTest.java
@@ -0,0 +1,44 @@
+package com.thealgorithms.sorts;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class RadixSortTest {
+
+    private RadixSort radixSort = new RadixSort();
+
+    @Test
+    public void testRadixSortEmptyArray() {
+        int[] array = {};
+        radixSort.radixSort(array);
+        int[] expected = {};
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testRadixSortSingleValueArray() {
+        int[] array = { 7 };
+        radixSort.radixSort(array);
+        int[] expected = { 7 };
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testRadixSort() {
+        int[] array = { 170, 45, 75, 90, 802, 24, 2, 66 };
+        radixSort.radixSort(array);
+        int[] expected = { 2, 24, 45, 66, 75, 90, 170, 802 };
+        assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testRadixSortBigNumbers() {
+        int[] array = { 170, 45, 75, 90, 802, 24, 2, 66, 1000000, 1000000000 };
+        radixSort.radixSort(array);
+        int[] expected = { 2, 24, 45, 66, 75, 90, 170, 802, 1000000, 1000000000 };
+        assertArrayEquals(expected, array);
+    }
+
+
+}
