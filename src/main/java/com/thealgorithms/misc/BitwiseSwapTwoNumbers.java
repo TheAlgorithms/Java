6551be860bcb5f6b5cbb8923c1c6a1c4afc9e18c
Akshada Joshi <90380328+joshiakshada0404@users.noreply.github.com>
2022-10-22 23:10:56 +0530

    BitwiseSwapTwoNumbers

---
+++ src/main/java/com/thealgorithms/misc/BitwiseSwapTwoNumbers.java
@@ -0,0 +1,23 @@
+public class SwapTwoNumbers 
+{  
+  static void swapNumbers(int x, int y)  
+  {  
+    System.out.println("Before swapping");      
+    System.out.println("x= " + x + ", y= " + y);  
+    x = x ^ y;  
+    y = x ^ y;  
+    x = x ^ y;  
+    System.out.println("After swapping");  
+    System.out.println("x= " + x + ", y= " + y);  
+  }  
+  public static void main(String[] args)   
+  {  
+    Scanner first = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter first number: ");
+    int x = first.nextInt();  // Read user input
+    Scanner second = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter second number : ");
+    int y = second.nextInt();  // Read user input
+    swapNumbers(x,y);  
+  }  
+}  
