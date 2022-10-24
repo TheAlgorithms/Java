2a9c38c0f7b18a01d27683055b2ffde2cbf467cb
Zaheudev <68231115+Zaheudev@users.noreply.github.com>
2022-10-24 13:03:49 +0300

    Update ReverseArraySort.java

--- src/main/java/com/thealgorithms/sorts/ReverseArraySort.java
+++ src/main/java/com/thealgorithms/sorts/ReverseArraySort.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class ReverseArraySort {
 
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
