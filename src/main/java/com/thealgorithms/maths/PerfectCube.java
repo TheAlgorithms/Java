2a7cf3dc50a36d7b2de62cb26dde487bb98290ba
Taranjeet Singh Kalsi <taranjeetkalsi15@gmail.com>
2022-10-25 16:25:06 +0530

    Added another function to PerfectCube.java
    Added another function to PerfectCube.java and fixed a testing mistake in line number 9

--- src/main/java/com/thealgorithms/maths/PerfectCube.java
+++ src/main/java/com/thealgorithms/maths/PerfectCube.java
@@ -6,12 +6,19 @@ package com.thealgorithms.maths;
 public class PerfectCube {
 
     public static void main(String[] args) {
-        assert !isPerfectCube(-1);
+        assert isPerfectCube(-1);
         assert isPerfectCube(0);
         assert isPerfectCube(1);
         assert !isPerfectCube(4);
         assert isPerfectCube(8);
         assert isPerfectCube(27);
+        
+        assert isPerfectCubeMathCbrt(-1);
+        assert isPerfectCubeMathCbrt(0);
+        assert isPerfectCubeMathCbrt(1);
+        assert !isPerfectCubeMathCbrt(4);
+        assert isPerfectCubeMathCbrt(8);
+        assert isPerfectCubeMathCbrt(27);
     }
 
     /**
@@ -24,5 +31,17 @@ public class PerfectCube {
     public static boolean isPerfectCube(int number) {
         int a = (int) Math.pow(number, 1.0 / 3);
         return a * a * a == number;
+    }
+    
+    /**
+     * Check if a number is perfect cube or not by using Math.cbrt function
+     *
+     * @param number number to check
+     * @return {@code true} if {@code number} is perfect cube, otherwise
+     * {@code false}
+     */
+    public static boolean isPerfectCubeMathCbrt(int number) {
+        double cubeRoot = Math.cbrt(number);
+        return cubeRoot == (int) cubeRoot;
     }
 }
