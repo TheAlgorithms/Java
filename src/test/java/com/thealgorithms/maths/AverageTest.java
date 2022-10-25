6235fd6505c0b7f19c527a6d0ad76456a7cae4f8
Philippe CANCELLIER <42130250+PhilippeCancellier@users.noreply.github.com>
2022-10-25 14:00:53 +0200

    Add tests for Average (#3652)

--- src/test/java/com/thealgorithms/maths/AverageTest.java
+++ src/test/java/com/thealgorithms/maths/AverageTest.java
@@ -5,10 +5,30 @@ import org.junit.jupiter.api.Test;
 
 public class AverageTest {
 
-    double[] numbers = { 3, 6, 9, 12, 15, 18, 21 };
+    private static final double SMALL_VALUE = 0.00001d;
 
     @Test
-    public void testAverage() {
-        Assertions.assertEquals(12, Average.average(numbers));
+    public void testAverage_double_12() {
+        double[] numbers = {3d, 6d, 9d, 12d, 15d, 18d, 21d};
+        Assertions.assertEquals(12d, Average.average(numbers), SMALL_VALUE);
     }
+
+    @Test
+    public void testAverage_double_20() {
+        double[] numbers = {5d, 10d, 15d, 20d, 25d, 30d, 35d};
+        Assertions.assertEquals(20d, Average.average(numbers), SMALL_VALUE);
+    }
+
+    @Test
+    public void testAverage_double_4_5() {
+        double[] numbers = {1d, 2d, 3d, 4d, 5d, 6d, 7d, 8d};
+        Assertions.assertEquals(4.5d, Average.average(numbers), SMALL_VALUE);
+    }
+
+    @Test
+    public void testAverage_int_5() {
+        int[] numbers = {2, 4, 10};
+        Assertions.assertEquals(5, Average.average(numbers));
+    }
+
 }
