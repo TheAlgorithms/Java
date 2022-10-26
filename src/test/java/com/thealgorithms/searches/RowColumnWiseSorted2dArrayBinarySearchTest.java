60cb43c9af645e9ddf2ec838d8e39f4eb11978e0
tanishkapetwal@gmail.com <tanishkapetwal@gmail.com>
2022-10-26 19:43:11 +0900

    Changed the code and applied binary search method.

--- src/test/java/com/thealgorithms/searches/RowColumnWiseSorted2dArrayBinarySearchTest.java
+++ src/test/java/com/thealgorithms/searches/RowColumnWiseSorted2dArrayBinarySearchTest.java
@@ -1,113 +1,29 @@
-package com.thealgorithms.searches;
+import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.Test;
-
-public class RowColumnWiseSorted2dArrayBinarySearchTest {
-
-  @Test
-  public void rowColumnSorted2dArrayBinarySearchTestMiddle() {
-    Integer[][] arr = {
-      { 10, 20, 30, 40 },
-      { 15, 25, 35, 45 },
-      { 18, 28, 38, 48 },
-      { 21, 31, 41, 51 },
-    };
-    Integer target = 35;
-    int[] ans = RowColumnWiseSorted2dArrayBinarySearch.search(arr, target);
-    int[] expected = { 1, 2 };
-    assertEquals(expected[0], ans[0]);
-    assertEquals(expected[1], ans[1]);
-  }
-
-  @Test
-  public void rowColumnSorted2dArrayBinarySearchTestSide() {
-    Integer[][] arr = {
-      { 10, 20, 30, 40 },
-      { 15, 25, 35, 45 },
-      { 18, 28, 38, 48 },
-      { 21, 31, 41, 51 },
-    };
-    Integer target = 48;
-    int[] ans = RowColumnWiseSorted2dArrayBinarySearch.search(arr, target);
-    int[] expected = { 2, 3 };
-    assertEquals(expected[0], ans[0]);
-    assertEquals(expected[1], ans[1]);
-  }
-
-  @Test
-  public void rowColumnSorted2dArray_BinarySearchTestUpper() {
-    Integer[][] arr = {
-      { 10, 20, 30, 40 },
-      { 15, 25, 35, 45 },
-      { 18, 28, 38, 48 },
-      { 21, 31, 41, 51 },
-    };
-    Integer target = 20;
-    int[] ans = RowColumnWiseSorted2dArrayBinarySearch.search(arr, target);
-    int[] expected = { 0, 1 };
-    assertEquals(expected[0], ans[0]);
-    assertEquals(expected[1], ans[1]);
-  }
-
-  @Test
-  public void rowColumnSorted2dArray_BinarySearchTestUpperSide() {
-    Integer[][] arr = {
-      { 10, 20, 30, 40 },
-      { 15, 25, 35, 45 },
-      { 18, 28, 38, 48 },
-      { 21, 31, 41, 51 },
-    };
-    Integer target = 40;
-    int[] ans = RowColumnWiseSorted2dArrayBinarySearch.search(arr, target);
-    int[] expected = { 0, 3 };
-    assertEquals(expected[0], ans[0]);
-    assertEquals(expected[1], ans[1]);
-  }
-
-  @Test
-  public void rowColumnSorted2dArray_BinarySearchTestLower() {
-    Integer[][] arr = {
-      { 10, 20, 30, 40 },
-      { 15, 25, 35, 45 },
-      { 18, 28, 38, 48 },
-      { 21, 31, 41, 51 },
-    };
-    Integer target = 31;
-    int[] ans = RowColumnWiseSorted2dArrayBinarySearch.search(arr, target);
-    int[] expected = { 3, 1 };
-    assertEquals(expected[0], ans[0]);
-    assertEquals(expected[1], ans[1]);
-  }
-
-  @Test
-  public void rowColumnSorted2dArray_BinarySearchTestLowerSide() {
-    Integer[][] arr = {
-      { 10, 20, 30, 40 },
-      { 15, 25, 35, 45 },
-      { 18, 28, 38, 48 },
-      { 21, 31, 41, 51 },
-    };
-    Integer target = 51;
-    int[] ans = RowColumnWiseSorted2dArrayBinarySearch.search(arr, target);
-    int[] expected = { 3, 3 };
-    assertEquals(expected[0], ans[0]);
-    assertEquals(expected[1], ans[1]);
-  }
-
-  @Test
-  public void rowColumnSorted2dArray_BinarySearchTestNotFound() {
-    Integer[][] arr = {
-      { 10, 20, 30, 40 },
-      { 15, 25, 35, 45 },
-      { 18, 28, 38, 48 },
-      { 21, 31, 41, 51 },
-    };
-    Integer target = 101;
-    int[] ans = RowColumnWiseSorted2dArrayBinarySearch.search(arr, target);
-    int[] expected = { -1, -1 };
-    assertEquals(expected[0], ans[0]);
-    assertEquals(expected[1], ans[1]);
-  }
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr={{10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}};
+        int target=37;
+        System.out.println(Arrays.toString(Search(arr,target)));
+    }
+    static int[] Search(int[][] arr,int target){
+        int row=0;
+        int col=arr.length-1;
+        while(row<=col){
+            int ele= (row+col)/2;
+            if(arr[row][ele]==target){
+                return new int[]{row,ele};
+            }
+            else if(arr[row][ele]<target){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
 }
