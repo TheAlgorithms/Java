6eef7e829bba0710db295d75646b66a2636fab77
Harshal <37841724+harshalkh@users.noreply.github.com>
2022-10-26 07:07:18 +0530

    Test case rename
    Co-authored-by: Yang Libin <contact@yanglibin.info>

--- src/test/java/com/thealgorithms/strings/RotationTest.java
+++ src/test/java/com/thealgorithms/strings/RotationTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class RotationTest {
 
     @Test
-    public void isLongestPalindromicSubstring() {
+    public void testRotation() {
         assertEquals("eksge", Rotation.rotation("geeks", 2));
         assertEquals("anasban", Rotation.rotation("bananas", 3));
         assertEquals("abracadabra", Rotation.rotation("abracadabra", 0));
