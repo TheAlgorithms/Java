6562e004738a8d5ab1cc1345f01e4879a85f9387
Khushi Jain <khushijain.mp@gmail.com>
2022-10-26 13:58:00 +0530

    khushi jain java code

---
+++ src/test/java/com/thealgorithms/strings/StringToInteger.java
@@ -0,0 +1,19 @@
+package com.thealgorithms.strings;
+
+
+import org.junit.jupiter.api.Test;
+
+public class StringToInteger {
+    @Test
+    public void Convert() {
+        String str = "1234";
+ 
+        int result1 = Integer.parseInt(str); // using Integer.parseInt() or
+        int result2 = Integer.valueOf(str); //using Integer.valueOf()
+ 
+        
+    }
+        
+    
+ 
+}
