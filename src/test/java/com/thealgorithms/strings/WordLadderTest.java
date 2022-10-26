a0d03e814a34e02cb469de4efdd38459e56335ba
Taranjeet Singh Kalsi <taranjeetkalsi15@gmail.com>
2022-10-26 18:44:03 +0530

    Update WordLadderTest.java & WordLadder.java (#3674)

--- src/test/java/com/thealgorithms/strings/WordLadderTest.java
+++ src/test/java/com/thealgorithms/strings/WordLadderTest.java
@@ -1,7 +1,6 @@
 package com.thealgorithms.strings;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import java.util.*;
 
@@ -9,9 +8,31 @@ public class WordLadderTest {
 
     @Test
     public void testWordLadder() {
-        String words1[] = { "hot", "dot", "dog", "lot", "log", "cog" };
-        assertEquals(5, WordLadder.ladderLength("hit", "cog", Arrays.asList(words1)));
-        String words2[] = { "hot", "dot", "dog", "lot", "log" };
-        assertEquals(0, WordLadder.ladderLength("hit", "cog", Arrays.asList(words2)));
+       
+        /**
+         * Test 1:
+         * Input: beginWord = "hit", endWord = "cog", wordList =
+         * ["hot","dot","dog","lot","log","cog"]
+         * Output: 5
+         * Explanation: One shortest transformation sequence is
+         * "hit" -> "hot" -> "dot" -> "dog" -> cog"
+         * which is 5 words long.
+         */
+
+        List<String> wordList1 = Arrays.asList("hot", "dot", "dog", "lot", "log", "cog");
+        assertEquals(WordLadder.ladderLength("hit", "cog", wordList1), 5);
+
+        /**
+         * Test 2:
+         * Input: beginWord = "hit", endWord = "cog", wordList =
+         * ["hot","dot","dog","lot","log"]
+         * Output: 0
+         * Explanation: The endWord "cog" is not in wordList,
+         * therefore there is no valid transformation sequence.
+         */
+
+        List<String> wordList2 = Arrays.asList("hot", "dot", "dog", "lot", "log");
+        assertEquals(WordLadder.ladderLength("hit", "cog", wordList2), 0);
+
      }
 }
