36d3f9a4531823230998bd563b16f2dea3a26999
Aidar Djamalbek <70245901+adjamalbek@users.noreply.github.com>
2022-10-27 17:53:28 -0400

    Update Valid_Parentheses.java
    file is not updating, have no idea why.

--- src/main/java/com/thealgorithms/strings/Valid_Parentheses.java
+++ src/main/java/com/thealgorithms/strings/Valid_Parentheses.java
@@ -1,9 +1,7 @@
 //        Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-//
-//                An input string is valid if:
-//
+//        An input string is valid if:
 //        Open brackets must be closed by the same type of brackets.
-//                Open brackets must be closed in the correct order.
+//        Open brackets must be closed in the correct order.
 //        Every close bracket has a corresponding open bracket of the same type.
 
 
