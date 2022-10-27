da218f9b27e06b3a59aced15ed922748497d6896
rashmibharambe <93034034+rashmibharambe@users.noreply.github.com>
2022-10-27 10:36:59 +0530

    Test case for CatalanNumber

--- src/test/java/com/thealgorithms/dynamicprogramming/CatalanNumberTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/CatalanNumberTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class CatalanNumberTest {
 
     @Test
-    public void testCatalanNumber {
+    public void testCatalanNumber() {
         assertEquals(14, CatalanNumber.findNthCatalan(5));
         assertEquals(16796, CatalanNumber.findNthCatalan(11));
     }
