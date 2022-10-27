6597c26bf237d7c264a2c821f8507214d6e6af8e
Taranjeet Singh Kalsi <taranjeetkalsi15@gmail.com>
2022-10-27 19:27:10 +0530

    fixed typo

--- src/test/java/com/thealgorithms/strings/PangramTest.java
+++ src/test/java/com/thealgorithms/strings/PangramTest.java
@@ -1,22 +1,20 @@
 package com.thealgorithms.strings;
 
-import static com.thealgorithms.strings.Pangram.isPangram;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 public class PangramTest {
 
     @Test
     public void testPangram() {
-        assertTrue(isPangram("The quick brown fox jumps over the lazy dog"));
-        assertFalse(isPangram("The quick brown fox jumps over the azy dog")); // L is missing
-        assertFalse(isPangram("+-1234 This string is not alphabetical"));
-        assertFalse(isPangram("\u0000/\\ Invalid characters are alright too"));
+        assertTrue(Pangram.isPangram("The quick brown fox jumps over the lazy dog"));
+        assertFalse(Pangram.isPangram("The quick brown fox jumps over the azy dog")); // L is missing
+        assertFalse(Pangram.isPangram("+-1234 This string is not alphabetical"));
+        assertFalse(Pangram.isPangram("\u0000/\\ Invalid characters are alright too"));
         
-        assertTrue(isPangramIndexOf("The quick brown fox jumps over the lazy dog"));
-        assertFalse(isPangramIndexOf("The quick brown fox jumps over the azy dog")); // L is missing
-        assertFalse(isPangramIndexOf("+-1234 This string is not alphabetical"));
-        assertFalse(isPangramIndexOf("\u0000/\\ Invalid characters are alright too"));
+        assertTrue(Pangram.isPangramIndexOf("The quick brown fox jumps over the lazy dog"));
+        assertFalse(Pangram.isPangramIndexOf("The quick brown fox jumps over the azy dog")); // L is missing
+        assertFalse(Pangram.isPangramIndexOf("+-1234 This string is not alphabetical"));
+        assertFalse(Pangram.isPangramIndexOf("\u0000/\\ Invalid characters are alright too"));
     }
 }
