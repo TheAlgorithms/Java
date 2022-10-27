541c7ad6814f65262173d67b7dce97421294833c
Taranjeet Singh Kalsi <taranjeetkalsi15@gmail.com>
2022-10-27 19:19:58 +0530

    Added Tests for new function
    Added Tests for isPangramIndexOf() function

--- src/test/java/com/thealgorithms/strings/PangramTest.java
+++ src/test/java/com/thealgorithms/strings/PangramTest.java
@@ -13,5 +13,10 @@ public class PangramTest {
         assertFalse(isPangram("The quick brown fox jumps over the azy dog")); // L is missing
         assertFalse(isPangram("+-1234 This string is not alphabetical"));
         assertFalse(isPangram("\u0000/\\ Invalid characters are alright too"));
+        
+        assertTrue(isPangramIndexOf("The quick brown fox jumps over the lazy dog"));
+        assertFalse(isPangramIndexOf("The quick brown fox jumps over the azy dog")); // L is missing
+        assertFalse(isPangramIndexOf("+-1234 This string is not alphabetical"));
+        assertFalse(isPangramIndexOf("\u0000/\\ Invalid characters are alright too"));
     }
 }
