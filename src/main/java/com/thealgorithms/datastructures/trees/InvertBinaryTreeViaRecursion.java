97285a848ba5a898c50430e164bec427344ddb1d
Aayush <kingaayusharora@gmail.com>
2022-10-30 19:38:40 +0530

    Invert Binary Tree Via Recursion Solution

---
+++ src/main/java/com/thealgorithms/datastructures/trees/InvertBinaryTreeViaRecursion.java
@@ -0,0 +1,49 @@
+package com.thealgorithms.datastructures.trees;
+
+
+
+public class InvertBinaryTreeviaRecursion {
+
+    //Question Link: https://leetcode.com/problems/invert-binary-tree/
+
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+
+            if (root.left == null && root.right == null) {
+                return new TreeNode(root.val, null, null);
+            }
+
+            //Recursion calls
+            TreeNode left = invertTree(root.right);
+            TreeNode right = invertTree(root.left);
+
+            TreeNode node = new TreeNode(root.val, left, right);
+            return node;
+
+        }
+    }
+}
