39876e0ae88b14cd822eb589e5d9bdd24d6b414a
Aidar Djamalbek <70245901+adjamalbek@users.noreply.github.com>
2022-10-31 00:52:41 +0600

    Update src/main/java/com/thealgorithms/strings/ValidParentheses.java
    Co-authored-by: Debasish Biswas <debasishbsws.abc@gmail.com>

--- src/main/java/com/thealgorithms/strings/ValidParentheses.java
+++ src/main/java/com/thealgorithms/strings/ValidParentheses.java
@@ -1,3 +1,4 @@
+package com.thealgorithms.strings;
 //        Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 //        An input string is valid if:
 //        Open brackets must be closed by the same type of brackets.
