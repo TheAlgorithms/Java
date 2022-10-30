ab9bd575d882b0a08111c550c294edb124b172ea
Aidar Djamalbek <70245901+adjamalbek@users.noreply.github.com>
2022-10-31 02:02:40 +0600

    Update PlusOneTest.java

--- src/test/java/com/thealgorithms/maths/PlusOneTest.java
+++ src/test/java/com/thealgorithms/maths/PlusOneTest.java
@@ -3,7 +3,7 @@ package com.thealgorithms.maths;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.Assert;
+import static org.junit.Assert;
 
 public class PlusOneTest {
 
