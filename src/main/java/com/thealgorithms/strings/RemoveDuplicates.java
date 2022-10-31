a2288f79d7e33bd36b2e7d7e7c0d65dffadaa53a
Aidar Djamalbek <70245901+adjamalbek@users.noreply.github.com>
2022-10-31 14:23:49 -0400

    Update RemoveDuplicates.java

--- src/main/java/com/thealgorithms/strings/RemoveDuplicates.java
+++ src/main/java/com/thealgorithms/strings/RemoveDuplicates.java
@@ -12,12 +12,12 @@ import java.util.*;
 
 public class RemoveDuplicates {
     public static int removeDuplicates(int[] nums) {
-        
         ArrayList<Integer> intList = new ArrayList<Integer>(nums.length);
-for (int i : nums)
-{
-    intList.add(i);
-}
+        for (int i : nums)
+            {
+                intList.add(i);
+            }
+        
         Set<Integer> s = new LinkedHashSet<Integer>(intList);
         int n = s.size();
         int x = 0;
