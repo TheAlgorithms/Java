2446d6fb13360100c1b1b014b3a1184b1bead4b8
Aidar Djamalbek <70245901+adjamalbek@users.noreply.github.com>
2022-10-31 15:55:21 -0400

    Update LongDivision.java
    Here is my own implementation of the Long Division for 2 integers 
    https://en.wikipedia.org/wiki/Long_division

--- src/main/java/com/thealgorithms/maths/LongDivision.java
+++ src/main/java/com/thealgorithms/maths/LongDivision.java
@@ -2,12 +2,7 @@
 //
 //        The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335
 //        would be truncated to -2.
-//
-//        Return the quotient after dividing dividend by divisor.
-//
-//        Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. 
-//        For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, 
-//        then return -231.
+//        My method used Long Division, here is the source "https://en.wikipedia.org/wiki/Long_division"
 
 package com.thealgorithms.maths;
 
