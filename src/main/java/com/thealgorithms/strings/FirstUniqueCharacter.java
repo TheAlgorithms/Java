2121b7d4ebe7a47646ef5fc9ada162154f292f98
Aidar Djamalbek <70245901+adjamalbek@users.noreply.github.com>
2022-10-31 14:29:31 -0400

    Create FirstUniqueCharacter.java
    Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.

---
+++ src/main/java/com/thealgorithms/strings/FirstUniqueCharacter.java
@@ -0,0 +1,27 @@
+// Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+
+package com.thealgorithms.strings;
+
+public class FirstUniqueCharacter {
+    public static int firstUniqChar(String s) {
+        char [] arr = s.toCharArray();
+        for(int i=0; i<arr.length;i++){
+            int count = 0;
+            for(int j=0; j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                    if(count==2){
+                        break;
+                    }
+                }
+            }
+            
+            if(count==1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
