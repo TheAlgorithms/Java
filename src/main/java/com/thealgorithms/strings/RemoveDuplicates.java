68b9adf0596afc7debf56a1d95da95db218522b2
Aidar Djamalbek <70245901+adjamalbek@users.noreply.github.com>
2022-11-01 20:58:09 +0600

    Update RemoveDuplicates.java

--- src/main/java/com/thealgorithms/strings/RemoveDuplicates.java
+++ src/main/java/com/thealgorithms/strings/RemoveDuplicates.java
@@ -3,7 +3,6 @@
 // Since it is impossible to change the length of the array in some languages, you must instead have the result be placed 
 // in the first part of the array nums. More formally, if there are k elements after removing the duplicates, 
 // then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
-
 // Return k after placing the final result in the first k slots of nums.
 
 
