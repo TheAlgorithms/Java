c58866de68a5f73b985e21bf473936d18b1256cc
Aidar Djamalbek <70245901+adjamalbek@users.noreply.github.com>
2022-11-01 16:04:41 +0600

    Update MyAtoi.java

--- src/main/java/com/thealgorithms/strings/MyAtoi.java
+++ src/main/java/com/thealgorithms/strings/MyAtoi.java
@@ -1,4 +1,4 @@
-// Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+// Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function). Here is my implementation
 
 package com.thealgorithms.strings;
 
