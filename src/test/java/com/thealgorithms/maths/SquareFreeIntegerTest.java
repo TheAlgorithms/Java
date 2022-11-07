73ea32ac965c7ded788641ccb0196ff062cbb58a
Akshay Dubey <akshay.dubey055@gmail.com>
2022-11-07 09:39:27 +0530

    fix: Fix failing build
    Changed static import for assertEquals()
    Closes #3402

--- src/test/java/com/thealgorithms/maths/SquareFreeIntegerTest.java
+++ src/test/java/com/thealgorithms/maths/SquareFreeIntegerTest.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.maths;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.List;
