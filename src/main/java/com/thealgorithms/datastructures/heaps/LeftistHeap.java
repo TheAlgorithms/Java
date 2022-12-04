fb957931af7a4ba0cb6301279ce974d139caeced
Adrian Paras <aparas@terpmail.umd.edu>
2022-12-04 13:39:47 -0500

    Adding LeftistHeap

---
+++ src/main/java/com/thealgorithms/datastructures/heaps/LeftistHeap.java
@@ -0,0 +1,107 @@
+package com.thealgorithms.datastructures.heaps;
+
+public class LeftistHeap {
+	private class Node {
+		private int element, npl;
+		private Node left, right;
+
+		// Node constructor setting the data element and left/right pointers to null
+		private Node(int element) {
+			this.element = element;
+			left = right = null;
+			npl = 0;
+		}
+	}
+
+	private Node root;
+
+	// Constructor
+	public LeftistHeap() {
+		root = null;
+	}
+
+	// Checks if heap is empty
+	public boolean isEmpty() {
+		return root == null;
+	}
+
+	// Resets structure to initial state
+	public void clear() {
+		// We will put head is null
+		root = null;
+	}
+
+	// Merge function that merges the contents of another leftist heap with the
+	// current one
+	public void merge(LeftistHeap rhs) {
+		// If the present function is rhs then we ignore the merge
+		if (this == rhs)
+			return;
+
+		root = merge(root, rhs.root);
+		rhs.root = null;
+	}
+
+	// Function merge with two Nodes a and b
+	public Node merge(Node a, Node b) {
+		if (a == null)
+			return b;
+
+		if (b == null)
+			return a;
+
+		// Violates leftist property, so must do a swap
+		if (a.element > b.element) {
+			Node temp = a;
+			a = b;
+			b = temp;
+		}
+
+		// Now we call the function merge to merge a and b
+		a.right = merge(a.right, b);
+
+		// Violates leftist property so must swap here
+		if (a.left == null) {
+			a.left = a.right;
+			a.right = null;
+		} else {
+			if (a.left.npl < a.right.npl) {
+				Node temp = a.left;
+				a.left = a.right;
+				a.right = temp;
+			}
+			a.npl = a.right.npl + 1;
+		}
+		return a;
+	}
+
+	// Function insert. Uses the merge function to add the data
+	public void insert(int a) {
+		root = merge(new Node(a), root);
+	}
+
+	// Returns and removes the minimum element in the heap
+	public int extract_min() {
+		// If is empty return -1
+		if (isEmpty())
+			return -1;
+
+		int min = root.element;
+		root = merge(root.left, root.right);
+		return min;
+	}
+
+	// Function printing an in order traversal of the data structure
+	public void in_order() {
+		in_order_aux(root);
+	}
+
+	// Auxiliary function for in_order
+	private void in_order_aux(Node n) {
+		if (n == null)
+			return;
+		in_order_aux(n.left);
+		System.out.print(n.element + " ");
+		in_order_aux(n.right);
+	}
+}
