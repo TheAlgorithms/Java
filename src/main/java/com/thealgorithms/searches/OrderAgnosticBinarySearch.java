04b8d12f21ceadbdf68a01337b45453c7619f2be
vinay satrasala <outfit.vinay960@gmail.com>
2022-12-20 22:31:17 +0530

    Changed from loop to recursion

--- src/main/java/com/thealgorithms/searches/OrderAgnosticBinarySearch.java
+++ src/main/java/com/thealgorithms/searches/OrderAgnosticBinarySearch.java
@@ -5,7 +5,8 @@ package com.thealgorithms.searches;
 /* Order Agnostic Binary Search is an algorithm where we do not know whether the given
    sorted array is ascending or descending order.
    We declare a boolean variable to find whether the array is ascending order.
-   In the while loop, we use the two pointer method (start and end) to get the middle element.
+   Insted of  while loop, we use recursion and two pointer method (start and end) to get the middle element.
+   when the search space become a single element then start= end in that case of element not equal to target return -1
    if the middle element is equal to our target element, then that is the answer.
    If not, then we check if the array is ascending or descending order.
    Depending upon the condition, respective statements will be executed and we will get our answer.
@@ -14,34 +15,27 @@ package com.thealgorithms.searches;
  public class OrderAgnosticBinarySearch {
 
      static int BinSearchAlgo(int arr[], int start, int end, int target) {
-
-         // Checking whether the given array is ascending order
-         boolean AscOrd = arr[start] < arr[end];
-
-         while (start <= end) {
-             int middle = start + (end - start) / 2;
-
-             // Check if the desired element is present at the middle position
-             if (arr[middle] == target)
-                 return middle;       // returns the index of the middle element
-
-             // Ascending order
-             if (AscOrd) {
-                 if (arr[middle] < target)
-                     start = middle + 1;
-                 else
-                     end = middle - 1;
-             }
-
-             // Descending order
-             else {
-                 if (arr[middle] > target)
-                     start = middle + 1;
-                 else
-                     end = middle - 1;
-             }
-         }
-         // Element is not present
-         return -1;
+        int middle = start + (end-middle)/2;
+        boolean isAsending=arr[start]<arr[end];
+        if(arr[middle]== target){
+            return middle;
+        }
+        if(start == end && arr[middle]!=target){
+            return -1;
+        }
+        if(isAsending){
+            if(target<arr[start]){
+                end=middle-1;
+            }else{
+                start=mid+1;
+            }
+        }else{
+            if(target<arr[start]){
+                start=middle+1;
+            }else{
+                end=middle-1;
+            }
+        }
+        return trail(arr, start, end, target);
      }
- }
+} 
