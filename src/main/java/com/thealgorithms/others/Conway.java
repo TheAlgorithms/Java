ddaceade69020f2a51b1c548ce66fda68169883b
Bruno Eloi <bruno.eloi@minfin.fed.be>
2023-01-11 12:07:05 +0100

    Add constants for the limits

--- src/main/java/com/thealgorithms/others/Conway.java
+++ src/main/java/com/thealgorithms/others/Conway.java
@@ -5,47 +5,53 @@ import java.util.*;
 public class Conway {
 
     /*
-    * This class will generate the conway sequence also known as the look and say sequence n (I just added a prompt to ask the user the starting point of the sequence).
-    * This works with any characters (of course it's better with numbers)
-    * To generate a member of the sequence from the previous member, read off the digits of the previous member, counting the number of digits in groups of the same digit. For example:
-    *1 is read off as "one 1" or 11.
-    *11 is read off as "two 1s" or 21.
-    *21 is read off as "one 2, one 1" or 1211.
-    *1211 is read off as "one 1, one 2, two 1s" or 111221.
-    *111221 is read off as "three 1s, two 2s, one 1" or 312211.
-    * https://en.wikipedia.org/wiki/Look-and-say_sequence
-    * */
+     * This class will generate the conway sequence also known as the look and say sequence.
+     * To generate a member of the sequence from the previous member, read off the digits of the previous member, counting the number of digits in groups of the same digit. For example:
+     *1 is read off as "one 1" or 11.
+     *11 is read off as "two 1s" or 21.
+     *21 is read off as "one 2, one 1" or 1211.
+     *1211 is read off as "one 1, one 2, two 1s" or 111221.
+     *111221 is read off as "three 1s, two 2s, one 1" or 312211.
+     * https://en.wikipedia.org/wiki/Look-and-say_sequence
+     * */
+
     private static final StringBuilder builder = new StringBuilder();
+
+    private static final Integer MAX_ITERATION_ALLOWED = 50;
+    private static final Integer MIN_ITERATION_ALLOWED = 1;
 
     public static void main(String[]args) {
         Scanner scanner;
         String originalString;
+        int maxIteration;
         System.out.println("Enter any character you want : ");
-        scanner = new Scanner(System.in);originalString = scanner.nextLine();
+        scanner = new Scanner(System.in);
+        originalString = scanner.nextLine();
 
-        List<String> strings = generateList(originalString);
+        do {
+        System.out.println("Enter the maximum of iteration (max allowed 50) : ");
+        scanner = new Scanner(System.in);
+        maxIteration = scanner.nextInt();
+        }
+        while (maxIteration < MIN_ITERATION_ALLOWED || maxIteration > MAX_ITERATION_ALLOWED);
+        List<String> strings = generateList(originalString, maxIteration);
         System.out.println(strings);
-
     }
 
-    protected static List<String> generateList(String originalString) {
+    protected static List<String> generateList(String originalString, int maxIteration) {
         List<String> numbers = new ArrayList<>();
-        for(int i=0; i<20; i++) {
-            originalString =   generateNextElement(originalString);
+        for(int i=0; i<maxIteration; i++) {
+            originalString = generateNextElement(originalString);
             numbers.add(originalString);
-            }
+        }
         return numbers;
     }
 
 
     public static String generateNextElement(String originalString) {
-
         builder.setLength(0);
         String[] stp = originalString.split("(?<=(.))(?!\\1)");
         Arrays.stream(stp).forEach(s -> builder.append(s.length()).append(s.charAt(0)));
         return builder.toString();
     }
-
-
-
 }
