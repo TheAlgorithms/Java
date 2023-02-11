a136d1aa45e82f0dd1c34397dd5844bf99abe21a
SpecialistSteak <102715674+SpecialistSteak@users.noreply.github.com>
2023-02-11 17:06:30 +0530

    added introsort

---
+++ src/test/java/com/thealgorithms/sorts/IntrospectiveSortTest.java
@@ -0,0 +1,57 @@
+package com.thealgorithms.sorts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntrospectiveSortTest {
+    @Test
+    // valid test case
+    public void StrandSortNonDuplicateTest() {
+        Integer[] expectedArray = {1, 2, 3, 4, 5};
+        Integer[] actualList = new IntrospectiveSort().sort(expectedArray);
+        assertArrayEquals(expectedArray, actualList);
+    }
+
+    @Test
+    // valid test case
+    public void StrandSortDuplicateTest() {
+        Integer[] expectedArray = {2, 2, 2, 5, 7};
+        Integer[] actualList = new IntrospectiveSort().sort(expectedArray);
+        assertArrayEquals(expectedArray, actualList);
+    }
+
+    @Test
+    // valid test case
+    public void StrandSortEmptyTest() {
+        Integer[] expectedArray = {};
+        Integer[] actualList = new IntrospectiveSort().sort(expectedArray);
+        assertArrayEquals(expectedArray, actualList);
+    }
+
+    @Test
+    // valid test case
+    public void StrandSortNullTest() {
+        Integer[] expectedArray = null;
+        assertThrows(NullPointerException.class, () -> {
+            new IntrospectiveSort().sort(expectedArray);
+        });
+    }
+
+    @Test
+    // valid test case
+    public void StrandSortNegativeTest() {
+        Integer[] expectedArray = {-1, -2, -3, -4, -5};
+        Integer[] actualList = new IntrospectiveSort().sort(expectedArray);
+        assertArrayEquals(expectedArray, actualList);
+    }
+
+    @Test
+    // valid test case
+    public void StrandSortNegativeAndPositiveTest() {
+        Integer[] expectedArray = {-1, -2, -3, 4, 5};
+        Integer[] actualList = new IntrospectiveSort().sort(expectedArray);
+        assertArrayEquals(expectedArray, actualList);
+    }
+}
