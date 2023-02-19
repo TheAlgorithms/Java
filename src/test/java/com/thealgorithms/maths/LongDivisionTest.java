cb77fc42e1d7fba3e4e99db2d084bc8d769dcf67
David Cederström <davidced@kth.se>
2023-02-19 12:36:59 +0100

    test:: Added additional tests in order to improve coverage for LongDivision function #21

--- src/test/java/com/thealgorithms/maths/LongDivisionTest.java
+++ src/test/java/com/thealgorithms/maths/LongDivisionTest.java
@@ -25,4 +25,36 @@ public class LongDivisionTest {
         assertEquals(10, LongDivision.divide(105,10));
     }
 
+
+    /* The following tests are added according to task 3.5.2 */
+
+
+    // Requirement: Dividend (negative), divisor (positive), returns correct integer after division
+    // Tests the case where the dividend is less than 0. Was not tested before. 
+    @Test
+    void testNegativeDividend() {
+        assertEquals(-1, LongDivision.divide(-5,3));
+    }
+	
+    // Requirement: Dividend (positive), divisor (positive), returns correct integer after division
+    // Tests the case where the dividend is less than the divisor. The test should return 0 in this case. 
+    @Test
+    void testDividendLessThanDivisor() {
+        assertEquals(0, LongDivision.divide(3,5));
+    }
+
+    // Requirement: Dividend (neither), divisor (positive), returns correct integer after division
+    // Tests the case where the dividend is 0. This should return a 0.
+    @Test
+    void testDividendIsZero() {
+        assertEquals(0, LongDivision.divide(0,5));  
+    }
+
+    // Requirement: Dividend (positive), divisor (neither), returns correct integer after division
+    // Tests the case where the divisor is 0. This should return a 0. DOESNT WORK, DOESNT HAVE A CASE FOR IF THE DIVISOR IS 0
+    //@Test
+    //void testDivisionByZero() {
+    //    assertEquals(0, LongDivision.divide(5,0));  
+    //}
+
 }
