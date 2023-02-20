4e1de95fb25ee056491ada6e20ae53638e1a8b56
rsssss12 <104445931+rsssss12@users.noreply.github.com>
2023-02-20 18:34:33 +0100

    fix: Implement manual test coverage for the CRCAlgorithm. #7

--- src/test/java/com/thealgorithms/CoverageTest.java
+++ src/test/java/com/thealgorithms/CoverageTest.java
@@ -8,18 +8,10 @@ import com.thealgorithms.others.CRCAlgorithm;
 
 public class CoverageTest {
     public static void main(String[] args) {
-        BinaryTree t = new BinaryTree();
-        t.put(3);
-        t.put(5);
-        t.find(3);
-        t.find(10);
-
-        System.out.println("\n===== BinaryTree coverage test =====\n");
-        //t.getCoverage();
-
         CRCAlgorithm c = new CRCAlgorithm("1001", 10, 0.1);
 
         c.generateRandomMess();
+        c.divideMessageWithP(true);
         c.divideMessageWithP(false);
         c.changeMess();
 
