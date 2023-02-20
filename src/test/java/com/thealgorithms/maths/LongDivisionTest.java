884455aa4f76ada0fab7df0f9807d79b6b711d04
David Cederström <davidced@kth.se>
2023-02-20 11:03:05 +0100

    test:: Added further tests to improve coverage and test for edge case when divisor is zero #21

--- src/test/java/com/thealgorithms/maths/LongDivisionTest.java
+++ src/test/java/com/thealgorithms/maths/LongDivisionTest.java
@@ -11,46 +11,17 @@ import java.util.Arrays;
 public class LongDivisionTest {
 	
     private static boolean[] branchCoverage = new boolean[17];
-    private static boolean[] tempBranchCoverage = new boolean[17];
 
-    @BeforeAll
-    static void printCoverageBefore(){
-      System.out.println(Arrays.toString(branchCoverage));
-    }
     // Requirement: Dividend (positive) is  greater than divisor (positive), returns correct integer after division
     @Test
     void testOne() {
         assertEquals(3, LongDivision.divide(10,3));
-
-        LongDivision.divide(10,3);
-        tempBranchCoverage = LongDivision.getBranchCoverage();
-            
-        // Update the coverage array
-        for (int i = 0; i < branchCoverage.length; i++) {
-          if (tempBranchCoverage[i]) {
-              branchCoverage[i] = true;
-          }
-        }
-
-        //System.out.println(Arrays.toString(branchCoverage));
     }
 
     // Requirement: Dividend (positive) is  greater than divisor (negative), returns correct integer after division
     @Test
     void testTwo() {
         assertEquals(-2, LongDivision.divide(7,-3));
-
-        LongDivision.divide(7,-3);
-        tempBranchCoverage = LongDivision.getBranchCoverage();
-
-        // Update the coverage array
-        for (int i = 0; i < branchCoverage.length; i++) {
-          if (tempBranchCoverage[i]) {
-              branchCoverage[i] = true;
-          }
-        }
-                
-        //System.out.println(Arrays.toString(branchCoverage));
     }
   
     // Requirement: Dividend (positive) is  greater than divisor (negative), returns correct integer after division
@@ -58,18 +29,6 @@ public class LongDivisionTest {
     @Test
     void testThree() {
         assertEquals(10, LongDivision.divide(105,10));
-
-        LongDivision.divide(105,10);
-        tempBranchCoverage = LongDivision.getBranchCoverage();
-
-        // Update the coverage array
-        for (int i = 0; i < branchCoverage.length; i++) {
-          if (tempBranchCoverage[i]) {
-              branchCoverage[i] = true;
-          }
-        }
-                
-        //System.out.println(Arrays.toString(branchCoverage));
     }
 
 
@@ -81,18 +40,6 @@ public class LongDivisionTest {
     @Test
     void testNegativeDividend() {
         assertEquals(-1, LongDivision.divide(-5,3));
-
-        LongDivision.divide(-5,3);
-        tempBranchCoverage = LongDivision.getBranchCoverage();
-
-        // Update the coverage array
-        for (int i = 0; i < branchCoverage.length; i++) {
-          if (tempBranchCoverage[i]) {
-              branchCoverage[i] = true;
-          }
-        }
-                
-        //System.out.println(Arrays.toString(branchCoverage));
     }
 	
     // Requirement: Dividend (positive), divisor (positive), returns correct integer after division
@@ -100,18 +47,6 @@ public class LongDivisionTest {
     @Test
     void testDividendLessThanDivisor() {
         assertEquals(0, LongDivision.divide(3,5));
-
-        LongDivision.divide(3,5);
-        tempBranchCoverage = LongDivision.getBranchCoverage();
-
-        // Update the coverage array
-        for (int i = 0; i < branchCoverage.length; i++) {
-          if (tempBranchCoverage[i]) {
-              branchCoverage[i] = true;
-          }
-        }
-        
-        //System.out.println(Arrays.toString(branchCoverage));
     }
 
     // Requirement: Dividend (neither), divisor (positive), returns correct integer after division
@@ -119,32 +54,14 @@ public class LongDivisionTest {
     @Test
     void testDividendIsZero() {
         assertEquals(0, LongDivision.divide(0,5));
-
-        LongDivision.divide(0, 5);
-        tempBranchCoverage = LongDivision.getBranchCoverage();
-
-        // Update the coverage array
-        for (int i = 0; i < branchCoverage.length; i++) {
-          if (tempBranchCoverage[i]) {
-              branchCoverage[i] = true;
-          }
-        }
-        
-        //System.out.println(Arrays.toString(branchCoverage));
-    }
-
-    @AfterAll
-    static void printCoverageAfter() {
-      System.out.println(Arrays.toString(branchCoverage));  
-      LongDivision.resetCoverage();
     }
 
     // Requirement: Dividend (positive), divisor (neither), returns correct integer after division
     // Tests the case where the divisor is 0. This should return a 0. DOESNT WORK, DOESNT HAVE A CASE FOR IF THE DIVISOR IS 0
-    //@Test
-    //void testDivisionByZero() {
-    //    assertEquals(0, LongDivision.divide(5,0));  
-    //}
+    @Test
+    void testDivisionByZero() {
+        assertEquals(0, LongDivision.divide(5,0));  
+    }
 
 
 }
