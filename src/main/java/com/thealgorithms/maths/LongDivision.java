7ddd6a6009be9da6237f27ffdad6a2fd469e5e11
David Cederström <davidced@kth.se>
2023-02-20 10:00:32 +0100

    test:: Added further implementation to the tests to improve coverage #21

--- src/main/java/com/thealgorithms/maths/LongDivision.java
+++ src/main/java/com/thealgorithms/maths/LongDivision.java
@@ -7,19 +7,47 @@
 package com.thealgorithms.maths;
 
 public class LongDivision {
+
+    private static boolean[] branchesTaken = new boolean[17];
+
+    // Method to get the coverage information
+    public static boolean[] getBranchCoverage() {
+        return branchesTaken;
+    }
+
+    public static void resetCoverage() {
+        for (int i = 0; i < 17; i++) {
+            branchesTaken[i] = false;
+        }
+    }
+
 public static int divide(int dividend, int divisor) {
+
+        // Creating a boolean array to keep track of which branches are taken (holds coverage info)
+        int numberOfBranches = 17;
+        //boolean[] branchesTaken = new boolean[numberOfBranches];
+        for (int i = 0; i < numberOfBranches; i++) {
+            branchesTaken[i] = false;
+        }
+
         long new_dividend_1 = dividend;
         long new_divisor_1 = divisor;
 
-        if (dividend < 0) {
+        // Branch ID: 1
+        if (dividend < 0) {      
+            branchesTaken[0] = true;                     
             new_dividend_1 = new_dividend_1 * -1;
         }
-        if (divisor < 0) {
+        // Branch ID: 2
+        if (divisor < 0) {     
+            branchesTaken[1] = true;                             
             new_divisor_1 = new_divisor_1 * -1;
         }
-
-        if (dividend == 0 || new_dividend_1 < new_divisor_1) {
-            return 0;
+        // Branch ID: 3
+        if (dividend == 0 || new_dividend_1 < new_divisor_1) {  
+            branchesTaken[2] = true;
+            printCoverageInformation(branchesTaken);
+            return 0;                                           
         }
 
         StringBuilder answer = new StringBuilder();
@@ -29,50 +57,104 @@ public static int divide(int dividend, int divisor) {
 
         String remainder = "";
 
-
-        for (int i = 0; i < dividend_string.length(); i++) {
+        // Branch ID: 4
+        for (int i = 0; i < dividend_string.length(); i++) {    
+            branchesTaken[3] = true;                            
             String part_v1 = remainder + "" + dividend_string.substring(last_index, i + 1);
             long part_1 = Long.parseLong(part_v1);
-            if (part_1 > new_divisor_1) {
+            // Branch ID: 5
+            if (part_1 > new_divisor_1) {  
+                branchesTaken[4] = true;                                                 
                 int quotient = 0;
-                while (part_1 >= new_divisor_1) {
+                // Branch ID: 6
+                while (part_1 >= new_divisor_1) {      
+                    branchesTaken[5] = true;                                     
                     part_1 = part_1 - new_divisor_1;
                     quotient++;
                 }
                 answer.append(quotient);
-            } else if (part_1 == new_divisor_1) {
+            }
+            // Branch ID: 7 
+            else if (part_1 == new_divisor_1) {    
+                branchesTaken[6] = true;                                       
                 int quotient = 0;
-                while (part_1 >= new_divisor_1) {
+                // Branch ID: 8
+                while (part_1 >= new_divisor_1) {      
+                    branchesTaken[7] = true;                                    
                     part_1 = part_1 - new_divisor_1;
                     quotient++;
                 }
                 answer.append(quotient);
-            } else if (part_1 == 0) {
+            } 
+            // Branch ID: 9
+            else if (part_1 == 0) {   
+                branchesTaken[8] = true;                                                 
                 answer.append(0);
-            } else if (part_1 < new_divisor_1) {
+            } 
+            // Branch ID: 10
+            else if (part_1 < new_divisor_1) {   
+                branchesTaken[9] = true;                                         
                 answer.append(0);
             }
-            if (!(part_1 == 0)) {
+            // Branch ID: 11
+            if (!(part_1 == 0)) {       
+                branchesTaken[10] = true;                                                    
                 remainder = String.valueOf(part_1);
-            }else{
+            }
+            // Branch ID: 12
+            else{         
+                branchesTaken[11] = true;                                                             
                 remainder = "";
             }
 
             last_index++;
         }
-
-        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
-            try {
-                return Integer.parseInt(answer.toString()) * (-1);
-            } catch (NumberFormatException e) {
-                return -2147483648;
+        // Branch ID: 13
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {    
+            branchesTaken[12] = true;           
+            // Branch ID: 14
+            try {   
+                branchesTaken[13] = true;         
+                printCoverageInformation(branchesTaken);                                                               
+                return Integer.parseInt(answer.toString()) * (-1);                          
+            } 
+            // Branch ID: 15
+            catch (NumberFormatException e) {   
+                branchesTaken[14] = true;   
+                printCoverageInformation(branchesTaken);                                       
+                return -2147483648;                                                         
             }
         }
-        try {
-            return Integer.parseInt(answer.toString());
-        } catch (NumberFormatException e) {
-            return 2147483647;
+        // Branch ID: 16
+        try {        
+            branchesTaken[15] = true;      
+            printCoverageInformation(branchesTaken);                                                                 
+            return Integer.parseInt(answer.toString());                                   
+        } 
+        // Branch ID: 17
+        catch (NumberFormatException e) {   
+            branchesTaken[16] = true;     
+            printCoverageInformation(branchesTaken);                                        
+            return 2147483647;                                                             
         }
+    }
 
+    public static void printCoverageInformation(boolean[] array){
+        float numberOfTrue = 0;
+        float numberOfFalse = 0;
+        float percentage = 0;
+        for (boolean b : array) {
+            if (b){
+                numberOfTrue++;
+            }
+            else if (!b){
+                numberOfFalse++;
+            }
+            //System.out.println(b);
+        }
+        percentage = (numberOfTrue/numberOfFalse) * 100;
+        //System.out.println("Number of true: " + numberOfTrue);
+        //System.out.println("Number of false: " + numberOfFalse);
+        System.out.println("PERCENTAGE COVERAGE: " + percentage);
     }
 }
--- src/test/java/com/thealgorithms/maths/LongDivisionTest.java
+++ src/test/java/com/thealgorithms/maths/LongDivisionTest.java
@@ -1,21 +1,56 @@
 package com.thealgorithms.maths;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+
 public class LongDivisionTest {
 	
+    private static boolean[] branchCoverage = new boolean[17];
+    private static boolean[] tempBranchCoverage = new boolean[17];
+
+    @BeforeAll
+    static void printCoverageBefore(){
+      System.out.println(Arrays.toString(branchCoverage));
+    }
     // Requirement: Dividend (positive) is  greater than divisor (positive), returns correct integer after division
     @Test
     void testOne() {
         assertEquals(3, LongDivision.divide(10,3));
+
+        LongDivision.divide(10,3);
+        tempBranchCoverage = LongDivision.getBranchCoverage();
+            
+        // Update the coverage array
+        for (int i = 0; i < branchCoverage.length; i++) {
+          if (tempBranchCoverage[i]) {
+              branchCoverage[i] = true;
+          }
+        }
+
+        //System.out.println(Arrays.toString(branchCoverage));
     }
 
     // Requirement: Dividend (positive) is  greater than divisor (negative), returns correct integer after division
     @Test
     void testTwo() {
         assertEquals(-2, LongDivision.divide(7,-3));
+
+        LongDivision.divide(7,-3);
+        tempBranchCoverage = LongDivision.getBranchCoverage();
+
+        // Update the coverage array
+        for (int i = 0; i < branchCoverage.length; i++) {
+          if (tempBranchCoverage[i]) {
+              branchCoverage[i] = true;
+          }
+        }
+                
+        //System.out.println(Arrays.toString(branchCoverage));
     }
   
     // Requirement: Dividend (positive) is  greater than divisor (negative), returns correct integer after division
@@ -23,6 +58,18 @@ public class LongDivisionTest {
     @Test
     void testThree() {
         assertEquals(10, LongDivision.divide(105,10));
+
+        LongDivision.divide(105,10);
+        tempBranchCoverage = LongDivision.getBranchCoverage();
+
+        // Update the coverage array
+        for (int i = 0; i < branchCoverage.length; i++) {
+          if (tempBranchCoverage[i]) {
+              branchCoverage[i] = true;
+          }
+        }
+                
+        //System.out.println(Arrays.toString(branchCoverage));
     }
 
 
@@ -34,6 +81,18 @@ public class LongDivisionTest {
     @Test
     void testNegativeDividend() {
         assertEquals(-1, LongDivision.divide(-5,3));
+
+        LongDivision.divide(-5,3);
+        tempBranchCoverage = LongDivision.getBranchCoverage();
+
+        // Update the coverage array
+        for (int i = 0; i < branchCoverage.length; i++) {
+          if (tempBranchCoverage[i]) {
+              branchCoverage[i] = true;
+          }
+        }
+                
+        //System.out.println(Arrays.toString(branchCoverage));
     }
 	
     // Requirement: Dividend (positive), divisor (positive), returns correct integer after division
@@ -41,13 +100,43 @@ public class LongDivisionTest {
     @Test
     void testDividendLessThanDivisor() {
         assertEquals(0, LongDivision.divide(3,5));
+
+        LongDivision.divide(3,5);
+        tempBranchCoverage = LongDivision.getBranchCoverage();
+
+        // Update the coverage array
+        for (int i = 0; i < branchCoverage.length; i++) {
+          if (tempBranchCoverage[i]) {
+              branchCoverage[i] = true;
+          }
+        }
+        
+        //System.out.println(Arrays.toString(branchCoverage));
     }
 
     // Requirement: Dividend (neither), divisor (positive), returns correct integer after division
     // Tests the case where the dividend is 0. This should return a 0.
     @Test
     void testDividendIsZero() {
-        assertEquals(0, LongDivision.divide(0,5));  
+        assertEquals(0, LongDivision.divide(0,5));
+
+        LongDivision.divide(0, 5);
+        tempBranchCoverage = LongDivision.getBranchCoverage();
+
+        // Update the coverage array
+        for (int i = 0; i < branchCoverage.length; i++) {
+          if (tempBranchCoverage[i]) {
+              branchCoverage[i] = true;
+          }
+        }
+        
+        //System.out.println(Arrays.toString(branchCoverage));
+    }
+
+    @AfterAll
+    static void printCoverageAfter() {
+      System.out.println(Arrays.toString(branchCoverage));  
+      LongDivision.resetCoverage();
     }
 
     // Requirement: Dividend (positive), divisor (neither), returns correct integer after division
@@ -56,5 +145,6 @@ public class LongDivisionTest {
     //void testDivisionByZero() {
     //    assertEquals(0, LongDivision.divide(5,0));  
     //}
+
 
 }
