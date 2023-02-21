69b1199655da20765a3d2677bb32c0362fb8beb2
HumbleFool <111487330+HumbleFool830@users.noreply.github.com>
2023-02-21 15:48:29 +0000

    added small changes like class names

--- src/main/java/com/thealgorithms/searches/sortOrderAgnosticBinarySearch.java
+++ src/main/java/com/thealgorithms/searches/sortOrderAgnosticBinarySearch.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.searches;
 import java.util.*;
-public class agnosticBinarySearch {
+public class sortOrderAgnosticBinarySearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of an array");
@@ -13,14 +13,13 @@ public class agnosticBinarySearch {
         System.out.println("enter key element to search in a array");
         int key=sc.nextInt();
         sc.close();
-        int keyIndexPosition =agnosticbinarySearch(arr,key);
-        System.out.println(keyIndexPosition);        
+        System.out.println(find(arr, key));     // function calling     
         
     }
-    public static int agnosticbinarySearch(int arr[],int key){
+    public static int find(int arr[],int key){
         int start = 0;
         int end = arr.length-1;
-        boolean arrDescending=arr[start]>arr[end];  //checking for Array is in ascending order or descending order.
+        boolean arrDescending = arr[start]>arr[end];  //checking for Array is in ascending order or descending order.
         while(start<=end){
             int mid = end-start/2;
             if (arr[mid]==key){
--- src/test/java/com/thealgorithms/searches/sortOrderAgnosticBinarySearchTest.java
+++ src/test/java/com/thealgorithms/searches/sortOrderAgnosticBinarySearchTest.java
@@ -4,22 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class agnosticBinarySearchTest{
+public class sortOrderAgnosticBinarySearchTest{
 
     @Test
-    public void Testascending(){
+    public void testAscending(){
         int arr[] = {1,2,3,4,5};// for ascending order.
         int target = 2;
-        int ans=agnosticBinarySearch.agnosticbinarySearch(arr, target);
+        int ans=sortOrderAgnosticBinarySearch.find(arr, target);
         int excepted = 1;
         assertEquals(excepted,ans);
     }
 
     @Test
-    public void Testdecending(){
+    public void testDescending(){
         int arr[] = {5,4,3,2,1};// for descending order.
         int target = 2;
-        int ans=agnosticBinarySearch.agnosticbinarySearch(arr, target);
+        int ans=sortOrderAgnosticBinarySearch.find(arr, target);
         int excepted = 3;
         assertEquals(excepted,ans );
     }
