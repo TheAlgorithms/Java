b9762a3271f588d01ed30ec6d4f31076c6b64f3d
DurgaSanthosh <75644571+DurgaSanthosh@users.noreply.github.com>
2023-02-23 08:14:46 +0530

    Update priorityScheduling.java

--- src/main/java/com/thealgorithms/scheduling/priorityScheduling.java
+++ src/main/java/com/thealgorithms/scheduling/priorityScheduling.java
@@ -1,26 +1,88 @@
-public class PriorityDemo {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-    public static void main(String[] args) {
-        Thread t1 = new Thread(new MyRunnable(), "Thread 1");
-        Thread t2 = new Thread(new MyRunnable(), "Thread 2");
-        Thread t3 = new Thread(new MyRunnable(), "Thread 3");
+public class PriorityScheduling {
+    private ArrayList<Process> processes;
+    private ArrayList<String> schedule;
 
-        // set thread priorities
-        t1.setPriority(Thread.MAX_PRIORITY);
-        t2.setPriority(Thread.NORM_PRIORITY);
-        t3.setPriority(Thread.MIN_PRIORITY);
-
-        // start the threads
-        t1.start();
-        t2.start();
-        t3.start();
+    public PriorityScheduling(ArrayList<Process> processes) {
+        this.processes = processes;
+        schedule = new ArrayList<>();
     }
 
-    static class MyRunnable implements Runnable {
-        public void run() {
-            for (int i = 0; i < 5; i++) {
-                System.out.println(Thread.currentThread().getName() + " is running");
+    public void scheduleProcesses() {
+        int time = 0;
+        int size = processes.size();
+        int executed = 0;
+        Process running = null;
+
+        while (executed < size) {
+            ArrayList<Process> ready = new ArrayList<>();
+            for (Process process : processes) {
+                if (process.getArrivalTime() <= time && !process.isCompleted()) {
+                    ready.add(process);
+                }
             }
+            if (ready.isEmpty()) {
+                time++;
+                continue;
+            }
+            running = Collections.min(ready, Comparator.comparing(Process::getPriority));
+            running.execute();
+            schedule.add(running.getId());
+            if (running.isCompleted()) {
+                executed++;
+            }
+            time++;
         }
+    }
+
+    public ArrayList<String> getSchedule() {
+        return schedule;
+    }
+}
+
+class Process {
+    private String id;
+    private int arrivalTime;
+    private int burstTime;
+    private int priority;
+    private int remainingTime;
+
+    public Process(String id, int arrivalTime, int burstTime, int priority) {
+        this.id = id;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.priority = priority;
+        this.remainingTime = burstTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void execute() {
+        remainingTime--;
+    }
+
+    public boolean isCompleted() {
+        return remainingTime == 0;
     }
 }
