65bc15e7e2d3dca3f8a87759f9effa76ac6f7aac
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-02-26 20:12:53 +0530

    (Fixes #3359)

--- src/test/java/com/thealgorithms/backtracking/AllPathsFromSourceToTargetTest.java
+++ src/test/java/com/thealgorithms/backtracking/AllPathsFromSourceToTargetTest.java
@@ -2,7 +2,7 @@ package com.thealgorithms.backtracking;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.assertArrayEquals;
-
+import java.util.*;
 import com.thealgorithms.backtracking.AllPathsFromSourceToTarget;
 import org.junit.jupiter.api.Test;
 
