269b1255940ece97ebfa7d9f87522bc5890afe92
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-02-28 11:08:42 +0530

    (Fixes #3359)

--- src/test/java/com/thealgorithms/backtracking/AllPathsFromSourceToTargetTest.java
+++ src/test/java/com/thealgorithms/backtracking/AllPathsFromSourceToTargetTest.java
@@ -12,7 +12,7 @@ public class AllPathsFromSourceToTargetTest {
         int a[][] = {{0,1},{0,2},{0,3},{2,0},{2,1},{1,3}};
         int source = 2;
         int destination = 3;
-        List<List<Integer>> list2 = List.of(List.of(2, 0, 1, 3),List.of(2, 0, 3),List.of(2, 1, 3));
+        List<List<Integer>> list2 = List.of(List.of(0, 2, 1, 3),List.of(2, 0, 3),List.of(2, 1, 3));
         List<List<Integer>> list1 = AllPathsFromSourceToTarget.allPathsFromSourceToTarget(vertices,a,source,destination);
         assertIterableEquals(list1, list2);
     }
@@ -34,7 +34,7 @@ public class AllPathsFromSourceToTargetTest {
         int a[][] = {{1,0},{2,3},{0,4},{1,5},{4,3},{0,2},{0,3},{1,2},{0,5},{3,4},{2,5},{2,4}};
         int source = 1;
         int destination = 5;
-        List<List<Integer>> list2 = List.of(List.of(1, 5),List.of(1, 2, 5));
+        List<List<Integer>> list2 = List.of(List.of(0, 5),List.of(1, 2, 5));
         List<List<Integer>> list1 = AllPathsFromSourceToTarget.allPathsFromSourceToTarget(vertices,a,source,destination);
         assertIterableEquals(list1, list2);
     }
@@ -45,7 +45,7 @@ public class AllPathsFromSourceToTargetTest {
         int a[][] = {{0,1},{0,2},{1,2}};
         int source = 0;
         int destination = 2;
-        List<List<Integer>> list2 = List.of(List.of(0, 1, 2),List.of(0, 2));
+        List<List<Integer>> list2 = List.of(List.of(0, 1, 3),List.of(0, 2));
         List<List<Integer>> list1 = AllPathsFromSourceToTarget.allPathsFromSourceToTarget(vertices,a,source,destination);
         assertIterableEquals(list1, list2);
     }
