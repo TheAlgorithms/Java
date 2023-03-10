e97dc70a963d17843a5a3eb5e1351900f3909e07
TheClerici <enrique_cle97@hotmail.com>
2023-03-10 10:56:24 -0700

    modified tests

--- src/test/java/com/thealgorithms/backtracking/ArrayCombinationTest.java
+++ src/test/java/com/thealgorithms/backtracking/ArrayCombinationTest.java
@@ -12,36 +12,39 @@ public class ArrayCombinationTest {
     void testNBeingZeroOrLess() {
         List<TreeSet<Integer>> zeroResult = ArrayCombination.combination(0, 1);
         List<TreeSet<Integer>> negativeResult = ArrayCombination.combination(-1, 1);
-        assertTrue(zeroResult == null);
-        assertTrue(negativeResult == null);
+        assertNull(zeroResult);
+        assertNull(negativeResult);
     }
 
     @Test
     void testNoLengthElement() {
         List<TreeSet<Integer>> result = ArrayCombination.combination(2, 0);
-        assertTrue(result == null);
+        assertNull(result);
     }
 
     @Test
     void testLengthOne() {
         List<TreeSet<Integer>> result = ArrayCombination.combination(2, 1);
-        assertTrue(result.get(0).iterator().next() == 1);
-        assertTrue(result.get(1).iterator().next() == 2);
+        assert result != null;
+        assertEquals(1, result.get(0).iterator().next());
+        assertEquals(2, result.get(1).iterator().next());
     }
 
     @Test
     void testLengthTwo() {
         List<TreeSet<Integer>> result = ArrayCombination.combination(2, 2);
+        assert result != null;
         Integer[] arr = result.get(0).toArray(new Integer[2]);
-        assertTrue(arr[0] == 1);
-        assertTrue(arr[1] == 2);
+        assertEquals(1, arr[0]);
+        assertEquals(2, arr[1]);
     }
 
     @Test
     void testLengthFive() {
         List<TreeSet<Integer>> result = ArrayCombination.combination(10, 5);
+        assert result != null;
         Integer[] arr = result.get(0).toArray(new Integer[5]);
-        assertTrue(arr[0] == 1);
-        assertTrue(arr[4] == 5);
+        assertEquals(1, arr[0]);
+        assertEquals(5, arr[4]);
     }
 }
