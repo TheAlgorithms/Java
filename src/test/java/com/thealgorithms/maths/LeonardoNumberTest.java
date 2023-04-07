f6cad1d7a8654162c0137e935d0751addf3bfd66
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2023-04-07 14:51:58 -0400

    redundant comments removed

--- src/test/java/com/thealgorithms/maths/LeonardoNumberTest.java
+++ src/test/java/com/thealgorithms/maths/LeonardoNumberTest.java
@@ -6,27 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LeonardoNumberTest {
     @Test
-        //Negative Leonardo Number
     void leonardoNumberNegative() {
         assertThrows(ArithmeticException.class, ()-> LeonardoNumber.leonardoNumber(-1));
     }
     @Test
-        //Zero Leonardo Number
     void leonardoNumberZero() {
         assertEquals(1, LeonardoNumber.leonardoNumber(0));
     }
     @Test
-        //First Leonardo Number
     void leonardoNumberOne() {
         assertEquals(1, LeonardoNumber.leonardoNumber(1));
     }
     @Test
-        //Fifth Leonardo Number
     void leonardoNumberFive() {
         assertEquals(15, LeonardoNumber.leonardoNumber(5));
     }
     @Test
-        //Twentieth Leonardo Number
     void leonardoNumberTwenty() {
         assertEquals(21891 , LeonardoNumber.leonardoNumber(20));
     }
