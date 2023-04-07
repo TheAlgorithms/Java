4d8a6b85da51c2d499fb59fded8cad7d93014a65
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-07 18:40:36 +0530

    Update IntersectionOfArray.java

--- src/main/java/com/thealgorithms/datastructures/hashmap/hashing/IntersectionOfArray.java
+++ src/main/java/com/thealgorithms/datastructures/hashmap/hashing/IntersectionOfArray.java
@@ -1,24 +1,22 @@
 import java.util.HashSet;
 
 public class IntersectionOfArray {
-    public static int intersection(int[] a,int[] b){
-        HashSet<Integer> set = new HashSet<>();
-        int count = 0;
-        for (int i=0;i<a.length;i++) {
-            set.add(a[i]);
-        }
 
-        for (int j=0; j < b.length; j++) {
-            if (set.contains(b[j])) {
-                count++;
-                set.remove(b[j]);
+ // This method takes two integer arrays, 'a' and 'b', as input and finds the count of common elements.
+ // It uses a HashSet to keep track of elements in array 'a' and uses it to compare with elements in array 'b'.
+  public static int intersection(int[] a, int[] b) {
+            HashSet<Integer> set = new HashSet<>();
+            int count = 0;
+            for (int i = 0; i < a.length; i++) {
+                set.add(a[i]);
             }
+
+            for (int j = 0; j < b.length; j++) {
+                if (set.contains(b[j])) {
+                    count++;
+                    set.remove(b[j]);
+                }
+            }
+            return count; // give count of common elements
         }
-        return count; //give count of common elements
-    }
-    public static void main(String[] args) {
-        int[] nums = {1,2,5,1,3,1,5,1};
-        int[] nums1 = {9,5,5,8};
-        System.out.println(intersection(nums,nums1));
-    }
 }
