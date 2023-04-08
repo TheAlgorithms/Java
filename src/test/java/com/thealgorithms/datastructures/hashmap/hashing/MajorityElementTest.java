f8259f01450a69835930718a5a562903ef08e055
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-08 23:14:09 +0530

    Create MajorityElementTest.java

---
+++ src/test/java/com/thealgorithms/datastructures/hashmap/hashing/MajorityElementTest.java
@@ -0,0 +1,42 @@
+import src.main.java.com.thealgorithms.datastructures.hashmap.hashing.MajorityElement.majority
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MajorityElementTest{
+    @Test
+    void testMajorityWithSingleMajorityElement() {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9};
+        List<Integer> expected = new ArrayList<>();
+        expected.add(9);
+        List<Integer> actual = MajorityElement.majority(nums);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testMajorityWithMultipleMajorityElements() {
+        int[] nums = {1, 2, 3, 3, 3, 4, 4, 4};
+        List<Integer> expected = new ArrayList<>();
+        expected.add(3);
+        expected.add(4);
+        List<Integer> actual = MajorityElement.majority(nums);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testMajorityWithNoMajorityElement() {
+        int[] nums = {1, 2, 3, 4, 5, 6};
+        List<Integer> expected = new ArrayList<>();
+        List<Integer> actual = MajorityElement.majority(nums);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testMajorityWithEmptyArray() {
+        int[] nums = {};
+        List<Integer> expected = new ArrayList<>();
+        List<Integer> actual = MajorityElement.majority(nums);
+        assertEquals(expected, actual);
+    }
+}
