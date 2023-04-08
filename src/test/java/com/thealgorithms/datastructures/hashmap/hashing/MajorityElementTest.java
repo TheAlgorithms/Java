f6329f1d08aa48258e5915403f3a766d2e6f9f27
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-08 23:28:42 +0530

    Update MajorityElementTest.java

--- src/test/java/com/thealgorithms/datastructures/hashmap/hashing/MajorityElementTest.java
+++ src/test/java/com/thealgorithms/datastructures/hashmap/hashing/MajorityElementTest.java
@@ -1,4 +1,6 @@
-import com.thealgorithms.datastructures.hashmap.hashing.MajorityElement.*;
+package com.thealgorithms.datastructures.hashmap.hashing;
+
+import com.thealgorithms.datastructures.hashmap.hashing.MajorityElement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
