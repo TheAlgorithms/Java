5c1f2304466cb257b5189f7df1454f88d6c32f68
vil02 <vil02@o2.pl>
2023-05-28 20:08:45 +0200

    tests: add missing test cases

--- src/test/java/com/thealgorithms/maths/FindMaxTest.java
+++ src/test/java/com/thealgorithms/maths/FindMaxTest.java
@@ -7,10 +7,26 @@ import org.junit.jupiter.api.Test;
 public class FindMaxTest {
 
     @Test
-    public void testFindMaxValue() {
+    public void testFindMax0() {
         assertEquals(
             10,
             FindMax.findMax(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })
+        );
+    }
+
+    @Test
+    public void testFindMax1() {
+        assertEquals(
+            7,
+            FindMax.findMax(new int[] { 6, 3, 5, 1, 7, 4, 1 })
+        );
+    }
+
+    @Test
+    public void testFindMax2() {
+        assertEquals(
+            10,
+            FindMax.findMax(new int[] { 10, 0 })
         );
     }
 }
