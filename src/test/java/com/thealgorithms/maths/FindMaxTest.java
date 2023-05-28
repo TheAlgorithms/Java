96c1a96647c947f8f0c531ade3bc18967359e0ea
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2023-05-28 22:45:13 +0200

    Fix empty input handling in FindMax (#4206)

--- src/test/java/com/thealgorithms/maths/FindMaxTest.java
+++ src/test/java/com/thealgorithms/maths/FindMaxTest.java
@@ -1,16 +1,41 @@
 package com.thealgorithms.maths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class FindMaxTest {
 
     @Test
-    public void testFindMaxValue() {
+    public void testFindMax0() {
         assertEquals(
             10,
             FindMax.findMax(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })
+        );
+    }
+
+    @Test
+    public void testFindMax1() {
+        assertEquals(
+            7,
+            FindMax.findMax(new int[] { 6, 3, 5, 1, 7, 4, 1 })
+        );
+    }
+
+    @Test
+    public void testFindMax2() {
+        assertEquals(
+            10,
+            FindMax.findMax(new int[] { 10, 0 })
+        );
+    }
+
+    @Test
+    public void testFindMaxThrowsExceptionForEmptyInput() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> FindMax.findMax(new int[]{})
         );
     }
 }
