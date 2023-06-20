61a901f9e1fbb2d8f6705084322046605597ace5
vil02 <vil02@o2.pl>
2023-06-20 18:22:00 +0200

    tests: add tests for SieveOfEratosthenes

---
+++ src/test/java/com/thealgorithms/others/SieveOfEratosthenesTest.java
@@ -0,0 +1,40 @@
+package com.thealgorithms.others;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SieveOfEratosthenesTest {
+    @Test
+    public void testfFndPrimesTill1() {
+        assertArrayEquals(new int[] {}, SieveOfEratosthenes.findPrimesTill(1));
+    }
+
+    @Test
+    public void testfFndPrimesTill2() {
+        assertArrayEquals(new int[] {2}, SieveOfEratosthenes.findPrimesTill(2));
+    }
+
+    @Test
+    public void testfFndPrimesTill4() {
+        var primesTill4 = new int[] {2, 3};
+        assertArrayEquals(primesTill4, SieveOfEratosthenes.findPrimesTill(3));
+        assertArrayEquals(primesTill4, SieveOfEratosthenes.findPrimesTill(4));
+    }
+
+    @Test
+    public void testfFndPrimesTill40() {
+        var primesTill40 = new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+        assertArrayEquals(primesTill40, SieveOfEratosthenes.findPrimesTill(37));
+        assertArrayEquals(primesTill40, SieveOfEratosthenes.findPrimesTill(38));
+        assertArrayEquals(primesTill40, SieveOfEratosthenes.findPrimesTill(39));
+        assertArrayEquals(primesTill40, SieveOfEratosthenes.findPrimesTill(40));
+    }
+
+    @Test
+    public void testfFndPrimesTill240() {
+        var primesTill240 = new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239};
+        assertArrayEquals(primesTill240, SieveOfEratosthenes.findPrimesTill(239));
+        assertArrayEquals(primesTill240, SieveOfEratosthenes.findPrimesTill(240));
+    }
+}
