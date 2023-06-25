d82d180f03203e1e060001046c8521a48a191927
THIRUMURUGAN <125580604+THIRU-1074@users.noreply.github.com>
2023-06-25 12:54:54 +0530

    -formatting modified

--- src/main/java/com/thealgorithms/maths/DeterminantOfMatrix.java
+++ src/main/java/com/thealgorithms/maths/DeterminantOfMatrix.java
@@ -4,23 +4,23 @@ import java.util.*;
  * Determinant of Matrix :https://textbooks.math.gatech.edu/ila/determinants-cofactors.html
  */
 public class DeterminantOfMatrix {
-	public static double[][] SUB_MAT(double[][] mat,int order,int c) {
+	public static double[][] submatrix(double[][] mat,int order,int c) {
 		//creating reference for the sub matrix
-		double[][] sub_mat=new double[order-1][order-1];
+		double[][] subMat=new double[order-1][order-1];
 		//sub_c stores the column column index of the sub matrix
-		int sub_c;
+		int subC;
 		for(int i=1;i<order;i++) {	
 			//making the column count zero after each row-addition
-			sub_c=0;
+			subC=0;
 			for(int k=0;k<order;k++) {
 				if(k==c) {
 					continue;//skipping the column where the mat[0][c] of OPER is in
 				}
-				sub_mat[i-1][sub_c]=mat[i][k];
-				sub_c+=1;//increasing the column count
+				subMat[i-1][subC]=mat[i][k];
+				subC+=1;//increasing the column count
 			}
 		}
-		return(sub_mat);
+		return(subMat);
 	}
 	public static double DET(double[][] mat,int order) {
 		/*determinant of the matrix with one element(order 1) :
@@ -32,7 +32,7 @@ public class DeterminantOfMatrix {
 	}
 	public static double OPER(double[][] mat,int ord) {
 		int check;
-		double del=0;
+		double det=0;
 		for(int c=0;c<ord;++c) {
 			if(c%2==0) {
 				check=-1;
@@ -40,9 +40,9 @@ public class DeterminantOfMatrix {
 			else {
 				check=1;
 			}
-			del+=(check*(mat[0][c])*(DET(SUB_MAT(mat,ord,c),ord-1)));
+			det+=(check*(mat[0][c])*(DET(submatrix(mat,ord,c),ord-1)));
 		}
-		return(del);
+		return(det);
 	}
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
