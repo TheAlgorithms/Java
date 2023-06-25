112fe069fade4358f60af29ea3c7d8c3d86e7521
THIRUMURUGAN <125580604+THIRU-1074@users.noreply.github.com>
2023-06-25 11:19:20 +0530

    -determinant code modification

--- src/main/java/com/thealgorithms/maths/DeterminantOfMatrix.java
+++ src/main/java/com/thealgorithms/maths/DeterminantOfMatrix.java
@@ -1,55 +1,73 @@
 package com.thealgorithms.maths;
-
 import java.util.*;
-
 /*
- * @author Ojasva Jain
- * Determinant of Matrix Wikipedia link : https://en.wikipedia.org/wiki/Determinant
+ * Determinant of Matrix :https://textbooks.math.gatech.edu/ila/determinants-cofactors.html
  */
 public class DeterminantOfMatrix {
-
-    // Determinant calculator
-    //@return determinant of the input matrix
-    static int determinant(int[][] a, int n) {
-        int det = 0, sign = 1, p = 0, q = 0;
-        if (n == 1) {
-            det = a[0][0];
-        } else {
-            int[][] b = new int[n - 1][n - 1];
-            for (int x = 0; x < n; x++) {
-                p = 0;
-                q = 0;
-                for (int i = 1; i < n; i++) {
-                    for (int j = 0; j < n; j++) {
-                        if (j != x) {
-                            b[p][q++] = a[i][j];
-                            if (q % (n - 1) == 0) {
-                                p++;
-                                q = 0;
-                            }
-                        }
-                    }
-                }
-                det = det + a[0][x] * determinant(b, n - 1) * sign;
-                sign = -sign;
-            }
-        }
-        return det;
-    }
-
-    // Driver Method
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        // Input Matrix
-        System.out.println("Enter matrix size (Square matrix only)");
-        int n = in.nextInt();
-        System.out.println("Enter matrix");
-        int[][] a = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                a[i][j] = in.nextInt();
-            }
-        }
-        System.out.println(determinant(a, n));
-    }
+	public static double[][] SUB_MAT(double[][] mat,int order,int c) {
+		//creating reference for the sub matrix
+		double[][] sub_mat=new double[order-1][order-1];
+		//sub_c stores the column column index of the sub matrix
+		int sub_c;
+		for(int i=1;i<order;i++) {	
+			//making the column count zero after each row-addition
+			sub_c=0;
+			for(int k=0;k<order;k++) {
+				if(k==c) {
+					continue;//skipping the column where the mat[0][c] of OPER is in
+				}
+				sub_mat[i-1][sub_c]=mat[i][k];
+				sub_c+=1;//increasing the column count
+			}
+		}
+		return(sub_mat);
+	}
+	public static double DET(double[][] mat,int order) {
+		/*determinant of the matrix with one element(order 1) :
+		 is the element itself*/
+		if (order==1) {
+			return(mat[0][0]);
+		}
+		return(OPER(mat,order));
+	}
+	public static double OPER(double[][] mat,int ord) {
+		int check;
+		double del=0;
+		for(int c=0;c<ord;++c) {
+			if(c%2==0) {
+				check=-1;
+			}
+			else {
+				check=1;
+			}
+			del+=(check*(mat[0][c])*(DET(SUB_MAT(mat,ord,c),ord-1)));
+		}
+		return(del);
+	}
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int order=in.nextInt();
+		in.nextLine();
+		double[][] mat=new double[order][order];
+		//getting the input elements of the matrix
+		for (int i = 0; i < order; ++i) {
+	        String line = in.nextLine().trim();
+	        String[] elements = line.split(" ");
+	        for (int j = 0; j < order; ++j) {
+	            if (elements[j].trim().isEmpty()) {
+	                System.out.println("Invalid input. Please enter all elements.");
+	                return;
+	            }
+	            mat[i][j] = Double.parseDouble(elements[j]);
+	        }
+	    }
+		if(order==1) {
+			/*determinant of the matrix with one element(order 1) :
+			 is the element itself*/
+			System.out.println(mat[0][0]);
+		}
+		else {
+		System.out.println(OPER(mat,order));//calling the OPER(operation) function
+		}
+	}
 }
