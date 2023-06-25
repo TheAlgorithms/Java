27468b0953d26c803bb68d1f86b3c66de415e005
THIRUMURUGAN <125580604+THIRU-1074@users.noreply.github.com>
2023-06-25 18:53:13 +0530

    --modified test case errors

--- src/test/java/com/thealgorithms/maths/DeterminantOfMatrixTest.java
+++ src/test/java/com/thealgorithms/maths/DeterminantOfMatrixTest.java
@@ -15,10 +15,10 @@ public class DeterminantOfMatrixTest {
         assertEquals(22.0, DeterminantOfMatrix.operation(matrix2, 2));
 
         double[][] matrix3 = { { -1.0, 2.5, 3.0 }, { 4.0, -5.2, 6.0 }, { 7.3, -8.1, 9.6 } };
-        assertEquals(58.9, DeterminantOfMatrix.operation(matrix3, 3));
+        assertEquals(31.50000000000001, DeterminantOfMatrix.operation(matrix3, 3));
 
-        double[][] matrix4 = { { -1.2, 2.3, -3.4, 4.5 }, { 5.6, -6.7, 7.8, -8.9 }, { -9.0, 10.1, -11.2, 12.3 }, { 13.4, -14.5, 15.6, -16.7 } };
-        assertEquals(2549.1228, DeterminantOfMatrix.operation(matrix4, 4));
+        double[][] matrix4 = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 0, 1, 2}, {3, 4, 5, 6} };
+        assertEquals(0.0, DeterminantOfMatrix.operation(matrix4, 4));
 
     }
 }
