e8aa3c2b5860be3c712f02e2c9262bffbb79c549
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2023-06-26 14:48:44 +0300

    Fixed style

--- src/test/java/com/thealgorithms/datastructures/trees/CheckIfBinaryTreeBalancedTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/CheckIfBinaryTreeBalancedTest.java
@@ -19,7 +19,7 @@ public class CheckIfBinaryTreeBalancedTest {
 
     @Test
     public void testOneNode() {
-        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[]{Integer.MIN_VALUE});
+        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[] {Integer.MIN_VALUE});
         assertTrue(CheckIfBinaryTreeBalanced.isBalancedRecursive(root));
         assertTrue(CheckIfBinaryTreeBalanced.isBalancedIterative(root));
     }
@@ -33,7 +33,7 @@ public class CheckIfBinaryTreeBalancedTest {
 */
     @Test
     public void testBinaryTreeIsBalancedEqualSubtreeHeights() {
-        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[]{9, 7, 13, 3, 8, 10, 20});
+        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[] {9, 7, 13, 3, 8, 10, 20});
         assertTrue(CheckIfBinaryTreeBalanced.isBalancedRecursive(root));
         assertTrue(CheckIfBinaryTreeBalanced.isBalancedIterative(root));
     }
@@ -47,7 +47,7 @@ public class CheckIfBinaryTreeBalancedTest {
 */
     @Test
     public void testBinaryTreeIsBalancedWithDifferentHeights() {
-        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[]{9, 7, 13, 3, 8});
+        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[] {9, 7, 13, 3, 8});
         assertTrue(CheckIfBinaryTreeBalanced.isBalancedRecursive(root));
         assertTrue(CheckIfBinaryTreeBalanced.isBalancedIterative(root));
     }
@@ -61,7 +61,7 @@ public class CheckIfBinaryTreeBalancedTest {
     */
     @Test
     public void testBinaryTreeNotBalanced() {
-        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[]{9, 7, null, 3, 8});
+        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[] {9, 7, null, 3, 8});
         assertFalse(CheckIfBinaryTreeBalanced.isBalancedRecursive(root));
         assertFalse(CheckIfBinaryTreeBalanced.isBalancedIterative(root));
     }
@@ -77,7 +77,7 @@ public class CheckIfBinaryTreeBalancedTest {
 */
     @Test
     public void testBinaryTreeNotBalancedBecauseLeftTreeNotBalanced() {
-        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[]{9, 7, 13, 3, 8, null, null, 11});
+        final BinaryTree.Node root = TreeTestUtils.createTree(new Integer[] {9, 7, 13, 3, 8, null, null, 11});
         assertFalse(CheckIfBinaryTreeBalanced.isBalancedRecursive(root));
         assertFalse(CheckIfBinaryTreeBalanced.isBalancedIterative(root));
     }
