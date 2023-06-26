7672e7669fe1ff4293aa6b5fb3ee80a7419a9818
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2023-06-26 14:53:25 +0300

    Fixed style

--- src/test/java/com/thealgorithms/datastructures/trees/CheckIfBinaryTreeBalancedTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/CheckIfBinaryTreeBalancedTest.java
@@ -1,9 +1,9 @@
 package com.thealgorithms.datastructures.trees;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Test check both implemented ways, iterative and recursive algorithms.
