4b023ce8fd1dd48a25cea0aaac293f4c96fecc4d
Debasish Biswas <debasishbsws.abc@gmail.com>
2023-07-01 13:16:18 +0000

    to check if it fails

--- src/main/java/com/thealgorithms/audiofilters/IIRFilter.java
+++ src/main/java/com/thealgorithms/audiofilters/IIRFilter.java
@@ -8,7 +8,7 @@ package com.thealgorithms.audiofilters;
  */
 public class IIRFilter {
 
-	private final int order;
+private final int order;
 	private final double[] coeffsA;
 	private final double[] coeffsB;
 	private final double[] historyX;
