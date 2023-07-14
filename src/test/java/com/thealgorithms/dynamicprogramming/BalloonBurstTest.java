68da468394172dfa671eb8d014a905faccc73125
Shiva953 <b22070@students.iitmandi.ac.in>
2023-07-14 13:27:48 +0530

    added tests for balloon burst program

---
+++ src/test/java/com/thealgorithms/dynamicprogramming/BalloonBurstTest.java
@@ -0,0 +1,43 @@
+package com.thealgorithms.dynamicprogramming;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class BalloonBurstTest {
+    private final BalloonBurst balloonBurst = new BalloonBurst();
+
+    @Test
+    public void testMaxCoinsCase1() {
+        int[] nums = {3, 1, 5, 8};
+        int expected = 167;
+        assertEquals(expected, balloonBurst.maxCoins(nums));
+    }
+
+    @Test
+    public void testMaxCoinsCase2() {
+        int[] nums = {2, 4, 3, 5, 1};
+        int expected = 110;
+        assertEquals(expected, balloonBurst.maxCoins(nums));
+    }
+
+    @Test
+    public void testMaxCoinsCase3() {
+        int[] nums = {1, 2, 3};
+        int expected = 12;
+        assertEquals(expected, balloonBurst.maxCoins(nums));
+    }
+
+    @Test
+    public void testMaxCoinsCase4() {
+        int[] nums = {5};
+        int expected = 5;
+        assertEquals(expected, balloonBurst.maxCoins(nums));
+    }
+
+    @Test
+    public void testMaxCoinsCase5() {
+        int[] nums = {};
+        int expected = 0;
+        assertEquals(expected, balloonBurst.maxCoins(nums));
+    }
+}
