06ef351987706c6fba2786c8024545376b42485a
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2023-07-24 18:25:40 +0200

    Add tests for `SumOfArithmeticSeries` (#4256)

--- src/main/java/com/thealgorithms/maths/SumOfArithmeticSeries.java
+++ src/main/java/com/thealgorithms/maths/SumOfArithmeticSeries.java
@@ -10,22 +10,8 @@ package com.thealgorithms.maths;
  * <p>
  * Wikipedia: https://en.wikipedia.org/wiki/Arithmetic_progression
  */
-public class SumOfArithmeticSeries {
-
-    public static void main(String[] args) {
-        /* 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 */
-        assert Double.compare(55.0, sumOfSeries(1, 1, 10)) == 0;
-
-        /* 1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19 */
-        assert Double.compare(100.0, sumOfSeries(1, 2, 10)) == 0;
-
-        /* 1 + 11 + 21 + 31 + 41 + 51 + 61 + 71 + 81 + 91 */
-        assert Double.compare(460.0, sumOfSeries(1, 10, 10)) == 0;
-
-        /* 0.1 + 0.2 + 0.3 + 0.4 + 0.5 + 0.6 + 0.7 + 0.8 + 0.9 + 1.0 */
-        assert Double.compare(5.5, sumOfSeries(0.1, 0.1, 10)) == 0;
-
-        assert Double.compare(49600.0, sumOfSeries(1, 10, 100)) == 0;
+public final class SumOfArithmeticSeries {
+    private SumOfArithmeticSeries() {
     }
 
     /**
@@ -36,7 +22,10 @@ public class SumOfArithmeticSeries {
      * @param numOfTerms the total terms of an arithmetic series
      * @return sum of given arithmetic series
      */
-    private static double sumOfSeries(double firstTerm, double commonDiff, int numOfTerms) {
+    public static double sumOfSeries(final double firstTerm, final double commonDiff, final int numOfTerms) {
+        if (numOfTerms < 0) {
+            throw new IllegalArgumentException("numOfTerms nonnegative.");
+        }
         return (numOfTerms / 2.0 * (2 * firstTerm + (numOfTerms - 1) * commonDiff));
     }
 }
