98b1d085fdefb6f087f3e52c866bf46de2eb578d
Piotr Idzik <vil02@o2.pl>
2023-07-25 19:13:54 +0000

    tests: add test case with `-`

--- src/test/java/com/thealgorithms/others/StackPostfixNotationTest.java
+++ src/test/java/com/thealgorithms/others/StackPostfixNotationTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class StackPostfixNotationTest {
     @Test
     public void testEvaluate() {
-        final Map<String, Integer> testCases = Map.ofEntries(entry("1 1 +", 2), entry("2 3 *", 6), entry("6 2 /", 3), entry("5 2 + 3 *", 21), entry("-5", -5));
+        final Map<String, Integer> testCases = Map.ofEntries(entry("1 1 +", 2), entry("2 3 *", 6), entry("6 2 /", 3), entry("-5 -2 -", -3), entry("5 2 + 3 *", 21), entry("-5", -5));
         for (final var tc : testCases.entrySet()) {
             assertEquals(tc.getValue(), StackPostfixNotation.postfixEvaluate(tc.getKey()));
         }
