71dbb240e4ff17fa34878dea9df515d103956404
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2023-07-25 18:01:48 +0300

    Fixed style

--- src/test/java/com/thealgorithms/maths/FibonacciJavaStreamsTest.java
+++ src/test/java/com/thealgorithms/maths/FibonacciJavaStreamsTest.java
@@ -1,10 +1,9 @@
 package com.thealgorithms.maths;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.math.BigDecimal;
 import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Albina Gimaletdinova on 25/07/2023
