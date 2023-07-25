b493e0f0041e76cc7b846ae58712627dbc9d652d
Albina Gimaletdinova <gimaletdinovaalbina@gmail.com>
2023-07-25 17:54:13 +0300

    FibonacciJavaStreams refactoring, added unit tests

---
+++ src/test/java/com/thealgorithms/maths/FibonacciJavaStreamsTest.java
@@ -0,0 +1,73 @@
+package com.thealgorithms.maths;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Albina Gimaletdinova on 25/07/2023
+ */
+public class FibonacciJavaStreamsTest {
+    private static final String EXCEPTION_MESSAGE = "Input index cannot be null or negative!";
+
+    @Test
+    public void testWithNegativeIndexShouldThrowException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> FibonacciJavaStreams.calculate(new BigDecimal(-1)));
+        assertEquals(EXCEPTION_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    public void testCheckTheFirst4SequenceElements() {
+        checkElement(BigDecimal.ZERO, BigDecimal.ZERO);
+        checkElement(BigDecimal.ONE, BigDecimal.ONE);
+        checkElement(new BigDecimal(2), BigDecimal.ONE);
+        checkElement(new BigDecimal(3), new BigDecimal(2));
+    }
+
+    @Test
+    public void testCheck10thSequenceElement() {
+        checkElement(new BigDecimal(10), new BigDecimal(55));
+    }
+
+    @Test
+    public void testCheck20thSequenceElement() {
+        checkElement(new BigDecimal(20), new BigDecimal(6765));
+    }
+
+    @Test
+    public void testCheck30thSequenceElement() {
+        checkElement(new BigDecimal(30), new BigDecimal(832040));
+    }
+
+    @Test
+    public void testCheck40thSequenceElement() {
+        checkElement(new BigDecimal(40), new BigDecimal(102334155));
+    }
+
+    @Test
+    public void testCheck50thSequenceElement() {
+        checkElement(new BigDecimal(50), new BigDecimal(12586269025L));
+    }
+
+    @Test
+    public void testCheck100thSequenceElement() {
+        checkElement(new BigDecimal(100), new BigDecimal("354224848179261915075"));
+    }
+
+    @Test
+    public void testCheck200thSequenceElement() {
+        checkElement(new BigDecimal(200), new BigDecimal("280571172992510140037611932413038677189525"));
+    }
+
+    private static void checkElement(BigDecimal index, BigDecimal expected) {
+        // when
+        Optional<BigDecimal> result = FibonacciJavaStreams.calculate(index);
+
+        // then
+        assertTrue(result.isPresent());
+        assertEquals(result.get(), expected);
+    }
+}
