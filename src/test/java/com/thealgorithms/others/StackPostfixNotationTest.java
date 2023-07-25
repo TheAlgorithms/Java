326b6feceefb33f01b821e37346c53d8f424cd8b
Piotr Idzik <vil02@o2.pl>
2023-07-25 18:42:36 +0000

    tests: add testcase with single integer

--- src/test/java/com/thealgorithms/others/StackPostfixNotationTest.java
+++ src/test/java/com/thealgorithms/others/StackPostfixNotationTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Test;
 
 public class StackPostfixNotationTest {
     @Test
-    public void testGetWithNewObject() {
-        final Map<String, Integer> testCases = Map.ofEntries(entry("1 1 +", 2), entry("2 3 *", 6), entry("6 2 /", 3), entry("5 2 + 3 *", 21));
+    public void testEvaluate() {
+        final Map<String, Integer> testCases = Map.ofEntries(entry("1 1 +", 2), entry("2 3 *", 6), entry("6 2 /", 3), entry("5 2 + 3 *", 21), entry("-5", -5));
         for (final var tc : testCases.entrySet()) {
             assertEquals(tc.getValue(), StackPostfixNotation.postfixEvaluate(tc.getKey()));
         }
