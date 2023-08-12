6ce68212624ec9273c931994f67db1698d01293b
Abrar Shaha <73398955+shahabrar7746@users.noreply.github.com>
2023-08-12 23:06:14 +0530

    Update Even.java

--- src/main/java/com/thealgorithms/bitmanipulation/Even.java
+++ src/main/java/com/thealgorithms/bitmanipulation/Even.java
@@ -1,19 +1,12 @@
 import java.util.Scanner;
 
 public class Even {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int number = scanner.nextInt();
-
-        int evenMask = 1 << 0; // 0000 0001
-
-        // Check if the least significant bit of the number is set
-        if ((number & evenMask) != 0) {
-            System.out.println("The number is even.");
-        } else {
-            System.out.println("The number is odd.");
-        }
+static int isEven(int number){
+  int evenMask = 1 << 0;
+    if((number & evenMask) != 0){
+    return 0;
     }
+    return -1;
+}
+   
 }
