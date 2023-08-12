c6a0f83d330346eaed3aa489df63de973a023785
Abrar Shaha <73398955+shahabrar7746@users.noreply.github.com>
2023-08-13 00:01:46 +0530

    Create Even.java

---
+++ src/main/java/com/thealgorithms/bitmanipulation/Even.java
@@ -0,0 +1,12 @@
+package com.thealgorithms.bitmanipulation;
+
+public class Even {
+public static int isEven(int number){
+  int evenMask = 1 << 0;
+    if((number & evenMask) != 0){
+    return 1;
+    }
+    return -1;
+}
+   
+}
