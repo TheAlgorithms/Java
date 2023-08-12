4d71394e7c6d44eee877d4734546275c89bbd1f4
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-08-12 16:03:36 +0530

    linter solved2

--- src/main/java/com/thealgorithms/bitmanipulation/ReverseBits.java
+++ src/main/java/com/thealgorithms/bitmanipulation/ReverseBits.java
@@ -9,7 +9,7 @@ public class ReverseBits {
 
     public static int reverseBits(int n) {
         int result = 0;
-        int bitCount = 32; // Assuming integers are 32 bits in Java   
+        int bitCount = 32;
         for (int i = 0; i < bitCount; i++) {
             result <<= 1; // Left shift the result to make space for the next bit
             result |= (n & 1); // OR operation to set the least significant bit of result with the current bit of n
