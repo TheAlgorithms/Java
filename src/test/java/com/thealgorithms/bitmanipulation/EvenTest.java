a57cbe88c9c90534976de9fad701bbf9b87332c0
Abrar Shaha <73398955+shahabrar7746@users.noreply.github.com>
2023-08-12 23:38:29 +0530

    Update EvenTest.java

--- src/test/java/com/thealgorithms/bitmanipulation/EvenTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/EvenTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 class EvenTest {
-@test
+@Test
 void testEven(){
 assertEquals(1, Even.isEven(2));
 assertEquals(-1, Even.isEven(3));
