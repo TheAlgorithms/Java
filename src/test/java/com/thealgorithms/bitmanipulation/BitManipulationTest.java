c81f1c625c5a9e792cf796be8b10a32d89a1197e
Rashed <rashedulislam8741@yahoo.com>
2023-08-12 13:50:55 +0600

    updated test class

--- src/test/java/com/thealgorithms/bitmanipulation/BitManipulationTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/BitManipulationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BitManipulationTest {
+public class BitManipulationTest {
 
     @Test
     void isEven() {
