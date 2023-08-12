b1d4bd58b55eba3b55e59b323f07afb899f63053
Rashed <rashedulislam8741@yahoo.com>
2023-08-12 13:59:07 +0600

    added comment

--- src/main/java/com/thealgorithms/bitmanipulation/BitManipulation.java
+++ src/main/java/com/thealgorithms/bitmanipulation/BitManipulation.java
@@ -1,6 +1,12 @@
 package com.thealgorithms.bitmanipulation;
 
 public class BitManipulation {
+    /**
+     * Check if a number is odd or even
+     *
+     * @param input a number
+     * @return {@code true} if given number is even, otherwise {@code false}
+     */
     public static boolean isEven(int value) {
         return ((value & 1) != 1);
     }
