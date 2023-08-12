45f966fa6e561b7b176ff2b16c7384c2774d602d
BamaCharanChhandogi <b.c.chhandogi@gmailcom>
2023-08-12 23:17:55 +0530

    linter Problem Solved

--- src/test/java/com/thealgorithms/bitmanipulation/IsEvenTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/IsEvenTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
- class IsEvenTest {
+class IsEvenTest {
     @Test
     void testIsEven() {
         assertEquals(true, IsEven.isEven(2));
