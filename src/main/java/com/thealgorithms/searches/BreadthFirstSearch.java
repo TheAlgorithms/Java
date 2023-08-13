18848574be543d01245b0e871c264f06592cc2b9
Snir Turgeman <snir.tur@gmail.com>
2023-08-13 09:29:26 +0300

    Make DFS and BFS search algorithms generic (fixes #4229) (#4230)

--- src/main/java/com/thealgorithms/searches/BreadthFirstSearch.java
+++ src/main/java/com/thealgorithms/searches/BreadthFirstSearch.java
@@ -1,32 +1,45 @@
 package com.thealgorithms.searches;
 
-import com.thealgorithms.searches.DepthFirstSearch.Node;
-import java.util.ArrayDeque;
-import java.util.Optional;
-import java.util.Queue;
+import com.thealgorithms.datastructures.Node;
+import java.util.*;
 
 /**
  * @author: caos321
  * @date: 31 October 2021 (Sunday)
+ * @wiki: https://en.wikipedia.org/wiki/Breadth-first_search
  */
-public class BreadthFirstSearch {
-    public static Optional<Node> search(final Node node, final String name) {
-        if (node.getName().equals(name)) {
+public class BreadthFirstSearch<T> {
+
+    private final List<T> visited = new ArrayList<>();
+
+    public Optional<Node<T>> search(final Node<T> node, final T value) {
+        if (node == null) {
+            return Optional.empty();
+        }
+        if (node.getValue().equals(value)) {
+            // add root node to visited
+            visited.add(value);
             return Optional.of(node);
         }
+        visited.add(node.getValue());
 
-        Queue<Node> queue = new ArrayDeque<>(node.getSubNodes());
+        Queue<Node<T>> queue = new ArrayDeque<>(node.getChildren());
 
         while (!queue.isEmpty()) {
-            final Node current = queue.poll();
+            final Node<T> current = queue.poll();
+            visited.add(current.getValue());
 
-            if (current.getName().equals(name)) {
+            if (current.getValue().equals(value)) {
                 return Optional.of(current);
             }
 
-            queue.addAll(current.getSubNodes());
+            queue.addAll(current.getChildren());
         }
 
         return Optional.empty();
+    }
+
+    public List<T> getVisited() {
+        return visited;
     }
 }
