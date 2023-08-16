71285fb49802ca15fd6824e73df8a7babfa786a2
Rashed <rashedulislam8741@yahoo.com>
2023-08-16 14:19:37 +0600

    add binary and operator

---
+++ src/main/java/com/thealgorithms/bitmanipulation/ANDOperator.java
@@ -0,0 +1,7 @@
+package com.thealgorithms.bitmanipulation;
+
+public class ANDOperator {
+    public static int andOperator(int a, int b) {
+        return (a & b);
+    }
+}
---
+++ src/test/java/com/thealgorithms/bitmanipulation/ANDOperatorTest.java
@@ -0,0 +1,17 @@
+package com.thealgorithms.bitmanipulation;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ANDOperatorTest {
+
+    @Test
+    void andOperator() {
+        assertEquals(8, ANDOperator.andOperator(10, 12));
+        assertEquals(0, ANDOperator.andOperator(3, 8));
+        assertEquals(18, ANDOperator.andOperator(22, 27));
+        assertEquals(0, ANDOperator.andOperator(-3, 2));
+    }
+}
