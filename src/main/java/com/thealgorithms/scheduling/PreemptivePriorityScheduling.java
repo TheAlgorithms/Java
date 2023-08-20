5fd80cdc8d46b87f776e8438d9bea7f5adad8845
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-08-20 17:35:19 +0530

    minor linter solved

--- src/main/java/com/thealgorithms/scheduling/PreemptivePriorityScheduling.java
+++ src/main/java/com/thealgorithms/scheduling/PreemptivePriorityScheduling.java
@@ -22,6 +22,7 @@ class Process {
 }
 
 public class PreemptivePriorityScheduling {
+    
     public static List<String> preemptivePriorityScheduling(List<Process> processes) { 
         List<String> ganttChart = new ArrayList<>();
         PriorityQueue<Process> readyQueue = new PriorityQueue<>(Comparator.comparingInt(p -> - p.priority));
