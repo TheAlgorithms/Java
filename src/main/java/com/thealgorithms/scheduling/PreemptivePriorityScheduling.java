3c885d6dd9566068ed5aeb2822af3555e214def9
Andrii Siriak <siryaka@gmail.com>
2023-08-20 22:36:18 +0300

    Update src/main/java/com/thealgorithms/scheduling/PreemptivePriorityScheduling.java

--- src/main/java/com/thealgorithms/scheduling/PreemptivePriorityScheduling.java
+++ src/main/java/com/thealgorithms/scheduling/PreemptivePriorityScheduling.java
@@ -22,7 +22,6 @@ class Process {
 }
 
 public class PreemptivePriorityScheduling {
-    
     public static List<String> preemptivePriorityScheduling(List<Process> processes) { 
         List<String> ganttChart = new ArrayList<>();
         PriorityQueue<Process> readyQueue = new PriorityQueue<>(Comparator.comparingInt(p -> - p.priority));
