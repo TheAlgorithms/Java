d6d80bfe152fce447cefe59d98861598d0d5dd74
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-08-23 01:14:12 +0530

    clang linter solved

--- src/main/java/com/thealgorithms/bitmanipulation/IndexOfRightMostSetBit.java
+++ src/main/java/com/thealgorithms/bitmanipulation/IndexOfRightMostSetBit.java
@@ -8,13 +8,13 @@ package com.thealgorithms.bitmanipulation;
 public class IndexOfRightMostSetBit {
     public static int indexOfRightMostSetBit(int n) {
         if (n == 0) {
-            return -1;  // No set bits
+            return -1; // No set bits
         }
 
         // Handle negative numbers by finding the two's complement
         if (n < 0) {
             n = -n;
-            n = n & (~n + 1);  // Get the rightmost set bit in positive form
+            n = n & (~n + 1); // Get the rightmost set bit in positive form
         }
 
         int index = 0;
