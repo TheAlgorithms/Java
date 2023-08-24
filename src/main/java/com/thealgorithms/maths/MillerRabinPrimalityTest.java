6ad5e6cb9a459ceeadb813fe08481f0a5b81913d
Tomáš Kratochvíl <kratochviltom00@gmail.com>
2023-08-24 17:32:31 +0200

    Update code with doozyX

--- src/main/java/com/thealgorithms/maths/MillerRabinPrimalityTest.java
+++ src/main/java/com/thealgorithms/maths/MillerRabinPrimalityTest.java
@@ -18,8 +18,7 @@ public class MillerRabinPrimalityTest {
      */
 
     public static boolean millerRabin(long n, int k) { // returns true if n is probably prime, else returns false.
-        if (n < 4)
-            return n == 2 || n == 3;
+        if (n < 4) return n == 2 || n == 3;
 
         int s = 0;
         long d = n - 1;
@@ -30,15 +29,13 @@ public class MillerRabinPrimalityTest {
         Random rnd = new Random();
         for (int i = 0; i < k; i++) {
             long a = 2 + rnd.nextLong() % (n - 3);
-            if (checkComposite(n, a, d, s))
-                return false;
+            if (checkComposite(n, a, d, s)) return false;
         }
         return true;
     }
 
     public static boolean deterministicMillerRabin(long n) { // returns true if n is prime, else returns false.
-        if (n < 2)
-            return false;
+        if (n < 2) return false;
 
         int r = 0;
         long d = n - 1;
@@ -47,29 +44,25 @@ public class MillerRabinPrimalityTest {
             r++;
         }
 
-        for (int a : new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
-            if (n == a)
-                return true;
-            if (checkComposite(n, a, d, r))
-                return false;
+        for (int a : new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
+            if (n == a) return true;
+            if (checkComposite(n, a, d, r)) return false;
         }
         return true;
     }
 
     private static boolean checkComposite(long n, long a, long d, int s) {
         long x = power(a, d, n);
-        if (x == 1 || x == n - 1)
-            return false;
+        if (x == 1 || x == n - 1) return false;
         for (int r = 1; r < s; r++) {
             x = power(x, 2, n);
-            if (x == n - 1)
-                return false;
+            if (x == n - 1) return false;
         }
         return true;
     }
 
     private static long power(long x, long y, long p) {
-        long res = 1;     // Initialize result
+        long res = 1; // Initialize result
 
         x = x % p; // Update x if it is more than or equal to p
 
@@ -77,11 +70,10 @@ public class MillerRabinPrimalityTest {
 
         while (y > 0) {
             // If y is odd, multiply x with result
-            if ((y & 1) == 1)
-                res = moduloMultiplication(res, x, p);
+            if ((y & 1) == 1) res = moduloMultiplication(res, x, p);
 
             // y must be even now
-            y = y>>1; // y = y/2
+            y = y >> 1; // y = y/2
             x = moduloMultiplication(x, x, p);
         }
         return res;
@@ -92,8 +84,8 @@ public class MillerRabinPrimalityTest {
         long aLo = a & ((1 << 24) - 1);
         long bHi = b >> 24;
         long bLo = b & ((1 << 24) - 1);
-        long result = ((((aHi*bHi << 16) % m) << 16) % m) << 16;
-        result += ((aLo*bHi+aHi*bLo) << 24) + aLo*bLo;
+        long result = ((((aHi * bHi << 16) % m) << 16) % m) << 16;
+        result += ((aLo * bHi + aHi * bLo) << 24) + aLo * bLo;
         return result % m;
     }
 
@@ -116,11 +108,9 @@ public class MillerRabinPrimalityTest {
             long number = rnd.nextLong(0, 3000);
             if (!millerRabin(number, iterations)) {
                 System.out.println(number + " is not a prime");
-            }
-            else if (deterministicMillerRabin(number)) {
+            } else if (deterministicMillerRabin(number)) {
                 System.out.println(number + " is a prime");
-            }
-            else {
+            } else {
                 System.out.println(number + " is not a prime, but Miller-Rabin said it is");
             }
         }
