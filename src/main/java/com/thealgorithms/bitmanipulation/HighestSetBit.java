51e93f914582d1948cbdf887d04a1a540658dc94
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-08-31 01:37:52 +0530

    change code for efficient

--- src/main/java/com/thealgorithms/bitmanipulation/HighestSetBit.java
+++ src/main/java/com/thealgorithms/bitmanipulation/HighestSetBit.java
@@ -1,26 +1,30 @@
 package com.thealgorithms.bitmanipulation;
+import java.util.Optional;
 
 /**
  * Find Highest Set Bit
  * @author Bama Charan Chhandogi (https://github.com/BamaCharanChhandogi)
  */
 
-public class HighestSetBit {
-    public static int findHighestSetBit(int num) {
+public final class HighestSetBit {
+    private HighestSetBit() {
+    }
+
+    public final static Optional<Integer> findHighestSetBit(int num) {
         if (num < 0) {
             throw new IllegalArgumentException("Input cannot be negative");
         }
-
+        
         if (num == 0) {
-            return -1; // No set bits
+            return Optional.empty();
         }
-
+        
         int position = 0;
         while (num > 0) {
             num >>= 1;
             position++;
         }
-
-        return position - 1;
+    
+        return Optional.of(position - 1);
     }
 }
