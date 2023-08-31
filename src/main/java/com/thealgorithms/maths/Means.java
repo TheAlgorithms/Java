905937c9c2342f3fb69d8ab4957dd48459189005
punit <punitpatel324@gmail.com>
2023-08-31 13:10:24 +0530

    Accomodated chagnes for review comments

---
+++ src/main/java/com/thealgorithms/maths/Means.java
@@ -0,0 +1,38 @@
+package com.thealgorithms.maths;
+
+import java.util.List;
+
+/**
+ * https://en.wikipedia.org/wiki/Mean
+ * <p>
+ * by: Punit Patel
+ */
+public final class Means {
+
+    /**
+     * Arithmetic / Pythagorean Mean = (x₁ + x₂ + ... +  xₙ) / n
+     */
+    public static Double arithmetic(final List<Double> numbers) {
+        if (numbers.isEmpty()) throw new IllegalArgumentException("Emtpy list given for Mean computation.");
+
+        return numbers.stream().reduce((x, y) -> x + y).get() / numbers.size();
+    }
+
+    /**
+     * Geometric Mean = (x₁ · x₂ · ... · xₙ) ^ 1/n
+     */
+    public static Double geometric(final List<Double> numbers) {
+        if (numbers.isEmpty()) throw new IllegalArgumentException("Emtpy list given for Mean computation.");
+
+        return Math.pow(numbers.stream().reduce((x, y) -> x * y).get(), 1.0 / numbers.size());
+    }
+
+    /**
+     * Harmonic Mean = n / (1/x₁ + 1/x₂ + ... +  1/xₙ)
+     */
+    public static double harmonic(final List<Double> numbers) {
+        if (numbers.isEmpty()) throw new IllegalArgumentException("Emtpy list given for Mean computation.");
+
+        return numbers.size() / numbers.stream().map(x -> 1 / x).reduce((x, y) -> x + y).get();
+    }
+}
