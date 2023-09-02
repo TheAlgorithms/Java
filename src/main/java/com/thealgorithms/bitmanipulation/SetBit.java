167d3f29e9f4e2fba7b567db6502bcfa860d543d
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-02 11:02:28 +0200

    Update SetBit.java

--- src/main/java/com/thealgorithms/bitmanipulation/SetBit.java
+++ src/main/java/com/thealgorithms/bitmanipulation/SetBit.java
@@ -3,7 +3,7 @@ package com.thealgorithms.bitmanipulation;
  * Sets a specific bit to 1
  */
 
-public class IsEven {
+public class SetBit {
         public static int setBit(int num, int bit) {
                 return num | (1<<bit);
         }
