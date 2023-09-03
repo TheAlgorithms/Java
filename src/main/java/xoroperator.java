0cf03f3225fb7aafc620d19cd41197b1f9db7b78
AYUSH <ayushabhinav007@gmail.com>
2023-09-03 19:55:11 +0530

    added xor code

---
+++ src/main/java/xoroperator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class xoroperator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a ^ b);
+
+    }
+}
