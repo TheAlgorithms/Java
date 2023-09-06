8223c74eb52115104170410a5eafb85c411b7813
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-06 15:10:50 +0200

    Update SetBit.java

--- src/test/java/com/thealgorithms/bitmanipulation/SetBit.java
+++ src/test/java/com/thealgorithms/bitmanipulation/SetBit.java
@@ -1,15 +1,14 @@
 package com.thealgorithms.bitmanipulation;
 
-
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
 
 
 class SetBitTest {
     @Test
     void testSetBit() {
-        assertEquals(5, SetBit.setBit(4,0));
-        assertEquals(3, SetBit.setBit(3,1));
+        assertEquals(5, SetBit.setBit(4, 0));
+        assertEquals(3, SetBit.setBit(3, 1));
     }
 }
