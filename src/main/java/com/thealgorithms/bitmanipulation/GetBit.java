f15d7986baff82f43e1a446b35d59c9b296c63cf
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-07 15:35:23 +0200

    Update GetBit.java

--- src/main/java/com/thealgorithms/bitmanipulation/GetBit.java
+++ src/main/java/com/thealgorithms/bitmanipulation/GetBit.java
@@ -1,3 +1,8 @@
+package com.thealgorithms.bitmanipulation;
+/**
+ * Returns the value of bit from num located at get
+ */
+
 public class GetBit {
     public static int getBit(int num, int get) {
         return (num>>get) & 1;
