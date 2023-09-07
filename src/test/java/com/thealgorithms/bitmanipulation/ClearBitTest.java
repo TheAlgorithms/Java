5ec8fc26a0113bd13c1dfdf23c81bfa98687f9c5
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-07 15:18:24 +0200

    Update ClearBitTest.java

--- src/test/java/com/thealgorithms/bitmanipulation/ClearBitTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/ClearBitTest.java
@@ -1,11 +1,13 @@
-import org.junit.Test;
+package com.thealgorithms.bitmanipulation;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class ClearBitTest {
     @Test
     public void clearBitTest() {
-        assertEquals(5, ClearBit.clearBit(7,1));
-        assertEquals(5, ClearBit.clearBit(5,1));
+        assertEquals(5, ClearBit.clearBit(7, 1));
+        assertEquals(5, ClearBit.clearBit(5, 1));
     }
 }
