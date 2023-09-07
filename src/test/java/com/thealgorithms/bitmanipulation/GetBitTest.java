9fa41d5df2af35bf6055a9738c7697765d525f5a
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-07 15:32:25 +0200

    Update GetBitTest.java

--- src/test/java/com/thealgorithms/bitmanipulation/GetBitTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/GetBitTest.java
@@ -1,11 +1,13 @@
-import org.junit.Test;
+package com.thealgorithms.bitmanipulation;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class GetBitTest {
     @Test
     public void getBitTest() {
-        assertEquals(1, GetBit.getBit(7,1));
-        assertEquals(0, GetBit.getBit(5,1));
+        assertEquals(1, GetBit.getBit(7, 1));
+        assertEquals(0, GetBit.getBit(5, 1));
     }
 }
