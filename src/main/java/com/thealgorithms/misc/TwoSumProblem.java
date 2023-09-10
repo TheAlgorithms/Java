241136c0f372781b19758e9d036df72a888161f0
BamaCharanChhandogi <b.c.chhandogi@gmail.com>
2023-09-10 17:43:13 +0530

    improve code

--- src/main/java/com/thealgorithms/misc/TwoSumProblem.java
+++ src/main/java/com/thealgorithms/misc/TwoSumProblem.java
@@ -14,17 +14,13 @@ public class TwoSumProblem {
     public static int[] twoSum(int[] arr, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], i);
-        }
-        for (int i = 0; i < arr.length; i++) {
-            int arrP = arr[i];
-            int rem = target - arrP;
+            int rem = target - arr[i];
             if (map.containsKey(rem)) {
                 int index = map.get(rem);
-                if (index == i) {
-                    continue;
-                }
-                return new int[] {i, index};
+                return new int[] {index, i};
+            }
+            if( !map.containsKey(arr[i])) {
+                map.put(arr[i], i);
             }
         }
         return new int[] {};
