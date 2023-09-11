cd1f8723bffaf708601ef62c99bcf2345bd5aa3c
Manan Solanki <mananrsolanki@gmail.com>
2023-09-11 16:05:03 +0530

    Linter solved

--- src/main/java/com/thealgorithms/dynamicprogramming/Knapsack.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/Knapsack.java
@@ -10,6 +10,8 @@ package com.thealgorithms.dynamicprogramming;
  */
 public final class Knapsack {
 
+    private Knapsack() {};
+
     /**
      * Solves the 0-1 Knapsack problem using Dynamic Programming.
      *
