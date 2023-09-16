57d1b7d1061cf45157481033fa78d88a57ecb410
Prathamesh Sirdesai <75674945+sirdesai22@users.noreply.github.com>
2023-09-16 10:08:18 +0530

    Update FourSumProblem.java

--- src/main/java/com/thealgorithms/misc/FourSumProblem.java
+++ src/main/java/com/thealgorithms/misc/FourSumProblem.java
@@ -15,46 +15,36 @@ public class FourSumProblem {
         }
         FourSumProblem solution = new FourSumProblem();
         List<List<Integer>> result = solution.fourSum(nums, target);
-        
         System.out.println("Unique Quadruplets:");
         for (List<Integer> quad : result) {
             System.out.println(quad);
         }
-        
         scan.close();
     }
-
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         List<List<Integer>> quadruplets = new ArrayList<>();
-
         for (int i = 0; i < nums.length - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue; // Skip duplicates
-            }
-            
+            }  
             for (int j = i + 1; j < nums.length - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) {
                     continue; // Skip duplicates
-                }
-                
+                }                
                 int left = j + 1;
-                int right = nums.length - 1;
-                
+                int right = nums.length - 1;                
                 while (left < right) {
-                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
-                    
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];                    
                     if (sum == target) {
                         List<Integer> quadruplet = Arrays.asList(nums[i], nums[j], nums[left], nums[right]);
-                        quadruplets.add(quadruplet);
-                        
+                        quadruplets.add(quadruplet);                    
                         while (left < right && nums[left] == nums[left + 1]) {
                             left++;
                         }
                         while (left < right && nums[right] == nums[right - 1]) {
                             right--;
                         }
-                        
                         left++;
                         right--;
                     } else if (sum < target) {
@@ -65,7 +55,6 @@ public class FourSumProblem {
                 }
             }
         }
-
         return quadruplets;
     }
 }
