a4641cedf4baca1823ad1b3b745ee4c452c3ac4a
Ansh <govardhanshah456@gmail.com>
2023-09-21 14:40:41 +0530

    Made Median Generic

--- src/main/java/com/thealgorithms/misc/MedianOfRunningArray.java
+++ src/main/java/com/thealgorithms/misc/MedianOfRunningArray.java
@@ -1,16 +1,15 @@
 package com.thealgorithms.misc;
 
-import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
 /**
  * @author shrutisheoran
  */
-public class MedianOfRunningArray<Value extends Number & Comparable<Value>> {
+public class MedianOfRunningArray<T extends Number & Comparable<T>> {
 
-    private PriorityQueue<Value> maxHeap;
-    private PriorityQueue<Value> minHeap;
+    private PriorityQueue<T> maxHeap;
+    private PriorityQueue<T> minHeap;
 
     // Constructor
     public MedianOfRunningArray() {
@@ -22,8 +21,8 @@ public class MedianOfRunningArray<Value extends Number & Comparable<Value>> {
       Inserting lower half of array to max Heap
       and upper half to min heap
      */
-    public void insert(Value e) {
-        if (!minHeap.isEmpty() && e.compareValueo(minHeap.peek()) < 0) {
+    public void insert(T e) {
+        if (!minHeap.isEmpty() && e.compareTo(minHeap.peek()) < 0) {
             maxHeap.offer(e);
             if (maxHeap.size() > minHeap.size() + 1) {
                 minHeap.offer(maxHeap.poll());
@@ -39,42 +38,43 @@ public class MedianOfRunningArray<Value extends Number & Comparable<Value>> {
     /*
       Returns median at any given point
      */
-    public Value median() {
+    public T median() {
         if (maxHeap.isEmpty() && minHeap.isEmpty()) {
             throw new IllegalArgumentException("Enter at least 1 element, Median of empty list is not defined!");
         }
 
         if (maxHeap.size() == minHeap.size()) {
-        	Value maxHeapValueop = maxHeap.peek();
-            Value minHeapValueop = minHeap.peek();
-            return calculateAverage(maxHeapValueop, minHeapValueop);
+        	T maxHeapTop = maxHeap.peek();
+            T minHeapTop = minHeap.peek();
+            return calculateAverage(maxHeapTop, minHeapTop);
         }
         return maxHeap.size() > minHeap.size() ? maxHeap.peek() : minHeap.peek();
     }
     
-    private Value calculateAverage(Value a, Value b) {
+    private T calculateAverage(T a, T b) {
         if (a instanceof Integer) {
             int sum = ((Integer) a) + ((Integer) b);
-            return (Value) Integer.valueOf(sum / 2);
+            return (T) Integer.valueOf(sum / 2);
         } else if (a instanceof Double) {
             double sum = ((Double) a) + ((Double) b);
             double roundedAverage = Math.round(sum / 2.0 * 100.0) / 100.0;
-            return (Value) Double.valueOf(roundedAverage);
+            return (T) Double.valueOf(roundedAverage);
         } else if (a instanceof Float) {
             float sum = ((Float) a) + ((Float) b);
             float roundedAverage = (float) (Math.round(sum / 2.0 * 100.0) / 100.0);
-            return (Value) Float.valueOf(roundedAverage);
+            return (T) Float.valueOf(roundedAverage);
         } else if (a instanceof Long) {
             long sum = ((Long) a) + ((Long) b);
-            return (Value) Long.valueOf(sum / 2);
+            return (T) Long.valueOf(sum / 2);
         } else if (a instanceof Short) {
             int sum = ((Short) a) + ((Short) b);
-            return (Value) Short.valueOf((short) (sum / 2));
+            return (T) Short.valueOf((short) (sum / 2));
         } else if (a instanceof Byte) {
             int sum = ((Byte) a) + ((Byte) b);
-            return (Value) Byte.valueOf((byte) (sum / 2));
+            return (T) Byte.valueOf((byte) (sum / 2));
         } else {
             throw new IllegalArgumentException("Unsupported numeric type");
         }
     }
+    
 }
