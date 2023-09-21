92b45b91b96d97a1118ff87f8046f6d781b0d55c
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-21 08:01:57 +0200

    Update FindMin.java

--- src/main/java/com/thealgorithms/maths/FindMin.java
+++ src/main/java/com/thealgorithms/maths/FindMin.java
@@ -37,7 +37,7 @@ public class FindMin {
         int min = array[0];
         for (int i = 1; i < array.length; i++) {
             if (array[i] < min) {
-                min = value;
+                min = array[i];
             }
         }
         return min;
