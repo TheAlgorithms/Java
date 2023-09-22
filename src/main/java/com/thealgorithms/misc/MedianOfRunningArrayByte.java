84cd243bfb0290d03e96fbb5916e1d9464bd58ee
Ansh <govardhanshah456@gmail.com>
2023-09-22 10:14:27 +0530

    Made Median Generic

--- src/main/java/com/thealgorithms/misc/MedianOfRunningArrayByte.java
+++ src/main/java/com/thealgorithms/misc/MedianOfRunningArrayByte.java
@@ -1,7 +1,5 @@
 package com.thealgorithms.misc;
 
-
-
 final public class MedianOfRunningArrayByte extends MedianOfRunningArray<Byte> {
     @Override
     public Byte calculateAverage(final Byte a, final Byte b) {
