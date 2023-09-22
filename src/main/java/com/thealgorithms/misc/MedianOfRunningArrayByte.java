e9849c8e56e4e7fd4d086e5f6ee23333c841c83b
Ansh Shah <60037118+govardhanshah456@users.noreply.github.com>
2023-09-22 20:49:10 +0530

    Update src/main/java/com/thealgorithms/misc/MedianOfRunningArrayByte.java
    Co-authored-by: Piotr Idzik <65706193+vil02@users.noreply.github.com>

--- src/main/java/com/thealgorithms/misc/MedianOfRunningArrayByte.java
+++ src/main/java/com/thealgorithms/misc/MedianOfRunningArrayByte.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.misc;
 
-final public class MedianOfRunningArrayByte extends MedianOfRunningArray<Byte> {
+public final class MedianOfRunningArrayByte extends MedianOfRunningArray<Byte> {
     @Override
     public Byte calculateAverage(final Byte a, final Byte b) {
         return (byte) ((a + b) / 2);
