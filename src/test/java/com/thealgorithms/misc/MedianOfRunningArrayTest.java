de7ff951bb46da79169f49b63bfbec7a61845ea8
Ansh <govardhanshah456@gmail.com>
2023-09-22 21:06:24 +0530

    Made Median Generic

--- src/test/java/com/thealgorithms/misc/MedianOfRunningArrayTest.java
+++ src/test/java/com/thealgorithms/misc/MedianOfRunningArrayTest.java
@@ -2,6 +2,7 @@ package com.thealgorithms.misc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 /**
