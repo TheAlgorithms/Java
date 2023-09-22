5658b7f1088449699223948d7ed3a6ad4c5635bf
suchib <suchibansal2007@gmail.com>
2023-09-22 22:03:58 +0530

    return a string of the words in reverse order concatenated by a single space.
    Input: s = "the sky is blue"
    Output: "blue is sky the"

--- src/main/java/com/thealgorithms/strings/ReverseWordInString.java
+++ src/main/java/com/thealgorithms/strings/ReverseWordInString.java
@@ -1,5 +1,15 @@
 package com.thealgorithms.strings;
 
+
+/**
+ Given an input string s, reverse the order of the words.
+ A word is defined as a sequence of non-space characters.
+ The words in s will be separated by at least one space.
+ Return a string of the words in reverse order concatenated by a single space.
+ Note that s may contain leading or trailing spaces or multiple spaces between two words.
+ The returned string should only have a single space separating the words.Do not include any extra spaces
+ */
+
 public class ReverseWordInString {
 
     public static void main(String[] args) {
