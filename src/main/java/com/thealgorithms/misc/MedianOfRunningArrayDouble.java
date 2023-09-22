e5386009f08c00f7a5818b60e50c1d6bf934c34c
Ansh Shah <60037118+govardhanshah456@users.noreply.github.com>
2023-09-22 20:49:28 +0530

    Update src/main/java/com/thealgorithms/misc/MedianOfRunningArrayDouble.java
    Co-authored-by: Piotr Idzik <65706193+vil02@users.noreply.github.com>

--- src/main/java/com/thealgorithms/misc/MedianOfRunningArrayDouble.java
+++ src/main/java/com/thealgorithms/misc/MedianOfRunningArrayDouble.java
@@ -1,11 +1,8 @@
 package com.thealgorithms.misc;
 
-import java.text.DecimalFormat;
-
-final public class MedianOfRunningArrayDouble extends MedianOfRunningArray<Double> {
-    public static final DecimalFormat decfor = new DecimalFormat("0.00");
+public final class MedianOfRunningArrayDouble extends MedianOfRunningArray<Double> {
     @Override
     public Double calculateAverage(final Double a, final Double b) {
-        return Double.parseDouble(decfor.format((a + b) / 2.0d));
+        return (a + b) / 2.0d;
     }
 }
