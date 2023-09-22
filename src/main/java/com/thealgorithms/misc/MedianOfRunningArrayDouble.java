143b6b841ae2c5f25409696663b2eab75705fdd3
Ansh <govardhanshah456@gmail.com>
2023-09-22 10:15:39 +0530

    Made Median Generic

--- src/main/java/com/thealgorithms/misc/MedianOfRunningArrayDouble.java
+++ src/main/java/com/thealgorithms/misc/MedianOfRunningArrayDouble.java
@@ -3,7 +3,7 @@ package com.thealgorithms.misc;
 import java.text.DecimalFormat;
 
 final public class MedianOfRunningArrayDouble extends MedianOfRunningArray<Double> {
-	public static final DecimalFormat decfor = new DecimalFormat("0.00");
+    public static final DecimalFormat decfor = new DecimalFormat("0.00");
     @Override
     public Double calculateAverage(final Double a, final Double b) {
         return Double.parseDouble(decfor.format((a + b) / 2.0d));
--- src/main/java/com/thealgorithms/misc/MedianOfRunningArrayFloat.java
+++ src/main/java/com/thealgorithms/misc/MedianOfRunningArrayFloat.java
@@ -3,7 +3,7 @@ package com.thealgorithms.misc;
 import java.text.DecimalFormat;
 
 final public class MedianOfRunningArrayFloat extends MedianOfRunningArray<Float> {
-	public static final DecimalFormat decfor = new DecimalFormat("0.00");
+    public static final DecimalFormat decfor = new DecimalFormat("0.00");
     @Override
     public Float calculateAverage(final Float a, final Float b) {
         return Float.parseFloat(decfor.format((a + b) / 2f));
