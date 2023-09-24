bfc73e738572d7b65434692c477f013136f2b20f
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-24 09:18:05 +0200

    Update FindMinTest.java

--- src/test/java/com/thealgorithms/maths/FindMinTest.java
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class FindMinTest {
 
     @Test
-    public void findMinTest(){
+    public void findMinTest() {
         testFindMinValue(1, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
         test1(5, new int[] {5, 5, 5, 5, 5});
         test2(0, new int[] {0, 192, 384, 576});
@@ -18,23 +18,23 @@ public class FindMinTest {
     }
 
     public void testFindMinValue(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array);
+        assertEquals(result, FindMin.findMin(array));
     }
 
     public void test1(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array);
+        assertEquals(result, FindMin.findMin(array));
     }
 
     public void test2(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array);
+        assertEquals(result, FindMin.findMin(array));
     }
 
     public void test3(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array);
+        assertEquals(result, FindMin.findMin(array));
     }
 
     public void test4(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array);
+        assertEquals(result, FindMin.findMin(array));
     }
 
     public void testFindMinThrowsExceptionForEmptyInput() {
