f4afa259dd7cdda3a8f2dde1ed3c7b15a822f58c
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-24 08:42:03 +0200

    Update FindMinTest.java

--- src/test/java/com/thealgorithms/maths/FindMinTest.java
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -10,10 +10,10 @@ public class FindMinTest {
     @Test
     public void testFindMinValue() {
         assertEquals(1, FindMin.findMin(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
-        assertEquals(1, FindMin.findMin(new int[] {5, 5, 5, 5, 5}));
+        assertEquals(5, FindMin.findMin(new int[] {5, 5, 5, 5, 5}));
         assertEquals(0, FindMin.findMin(new int[] {0, 192, 384, 576}));
         assertEquals(-1, FindMin.findMin(new int[] {-1, 2, 5, 10}));
-        assertEquals(-10, FindMin.findMin(new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}));
+        assertEquals(-10, FindMin.findMin(new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0}));
         assertThrows(IllegalArgumentException.class, () -> FindMin.findMin(new int[] {}));
     }
 }
