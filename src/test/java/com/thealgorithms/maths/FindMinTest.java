0d4125c373fabf4842d9ba2fcad4ff142e96ee17
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-24 08:37:16 +0200

    Update FindMinTest.java

--- src/test/java/com/thealgorithms/maths/FindMinTest.java
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -10,30 +10,10 @@ public class FindMinTest {
     @Test
     public void testFindMinValue() {
         assertEquals(1, FindMin.findMin(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
-    }
-
-    @Test
-    public void test1() {
-        assertEquals(1, FindMin.findMin(new int[] {1, 3, 5, 7, 9}));
-    }
-
-    @Test
-    public void test2() {
+        assertEquals(1, FindMin.findMin(new int[] {5, 5, 5, 5, 5}));
         assertEquals(0, FindMin.findMin(new int[] {0, 192, 384, 576}));
-    }
-
-    @Test
-    public void test3() {
         assertEquals(-1, FindMin.findMin(new int[] {-1, 2, 5, 10}));
-    }
-
-    @Test
-    public void test4() {
-        assertEquals(-10, FindMin.findMin(new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0}));
-    }
-
-    @Test
-    public void testFindMinThrowsExceptionForEmptyInput() {
+        assertEquals(-10, FindMin.findMin(new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}));
         assertThrows(IllegalArgumentException.class, () -> FindMin.findMin(new int[] {}));
     }
 }
