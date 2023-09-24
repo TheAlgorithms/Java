acdfb650339c1f484a919a498e7905426d9680dc
teja <128917204+chinnuteja@users.noreply.github.com>
2023-09-24 21:59:17 +0530

    Update MinimumPathSum.java

--- src/main/java/com/thealgorithms/dynamicprogramming/MinimumPathSum.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/MinimumPathSum.java
@@ -48,23 +48,25 @@ public class MinimumPathSum {
     }
 
     public static int minimumPathSum(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        if (n == 0) {
-            return 0;
+          int m = grid.length, n = grid[0].length;
+
+        
+        for (int i = 1; i < n; i++) {
+            grid[0][i] += grid[0][i - 1];
         }
-        int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
-        for (int i = 0; i < n - 1; i++) {
-            dp[0][i + 1] = dp[0][i] + grid[0][i + 1];
+
+        
+        for (int i = 1; i < m; i++) {
+            grid[i][0] += grid[i - 1][0];
         }
-        for (int i = 0; i < m - 1; i++) {
-            dp[i + 1][0] = dp[i][0] + grid[i + 1][0];
-        }
+
+        
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
-                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
             }
         }
-        return dp[m - 1][n - 1];
+        
+        return grid[m - 1][n - 1];
     }
-}
+
