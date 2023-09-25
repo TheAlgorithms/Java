b40756ae35ca14b704c46ddf8dad08e1a6130e5d
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-25 19:20:20 +0200

    Update FindMinTest.java

--- src/test/java/com/thealgorithms/maths/FindMinTest.java
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -1,42 +1,34 @@
 package com.thealgorithms.maths;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
 
 public class FindMinTest {
 
+    @ParameterizedTest
+    @MethodSource("provideStringsForIsBlank")
+    void numberTests(int expected, int[] input) {
+        Assertions.assertEquals(expected, FindMin.findMin(input));
+    }
+
+    private static Stream<Arguments> provideStringsForIsBlank() {
+        return Stream.of(
+                Arguments.of(1, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}),
+                Arguments.of(5, new int[] {5, 5, 5, 5, 5}),
+                Arguments.of(0, new int[] {0, 192, 384, 576}),
+                Arguments.of(-1, new int[] {-1, 2, 5, 10}),
+                Arguments.of(-10, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1})
+        );
+    }
+
     @Test
-    public void findMinTest() {
-        testFindMinValue(1, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-        test1(5, new int[] {5, 5, 5, 5, 5});
-        test2(0, new int[] {0, 192, 384, 576});
-        test3(-1, new int[] {-1, 2, 5, 10});
-        test4(-10, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1});
-        testFindMinThrowsExceptionForEmptyInput();
-    }
-
-    public void testFindMinValue(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array));
-    }
-
-    public void test1(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array));
-    }
-
-    public void test2(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array));
-    }
-
-    public void test3(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array));
-    }
-
-    public void test4(int result, int[] array) {
-        assertEquals(result, FindMin.findMin(array));
-    }
-
     public void testFindMinThrowsExceptionForEmptyInput() {
         assertThrows(IllegalArgumentException.class, () -> FindMin.findMin(new int[] {}));
     }
