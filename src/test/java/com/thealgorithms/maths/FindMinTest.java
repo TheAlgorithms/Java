584344cc0d3d0a7f74c034535bf134f993797230
piotr.idzik <vil02@o2.pl>
2023-09-25 22:11:23 +0200

    tests: add test case with minimum not at index 0

--- src/test/java/com/thealgorithms/maths/FindMinTest.java
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -18,7 +18,7 @@ public class FindMinTest {
     }
 
     private static Stream<Arguments> provideStringsForIsBlank() {
-        return Stream.of(Arguments.of(1, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(5, new int[] {5, 5, 5, 5, 5}), Arguments.of(0, new int[] {0, 192, 384, 576}), Arguments.of(-1, new int[] {-1, 2, 5, 10}), Arguments.of(-10, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}));
+        return Stream.of(Arguments.of(1, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(5, new int[] {5, 5, 5, 5, 5}), Arguments.of(0, new int[] {0, 192, 384, 576}), Arguments.of(-1, new int[] {-1, 2, 5, 10}), Arguments.of(-10, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}), Arguments.of(-4, new int[] {4, -3, 8, 9, -4, -4, 10}));
     }
 
     @Test
