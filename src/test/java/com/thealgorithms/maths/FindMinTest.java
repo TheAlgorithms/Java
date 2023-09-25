d9f512733b24e60f6475857547664fb6c4978c2d
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-25 19:24:22 +0200

    Update FindMinTest.java

--- src/test/java/com/thealgorithms/maths/FindMinTest.java
+++ src/test/java/com/thealgorithms/maths/FindMinTest.java
@@ -1,14 +1,13 @@
 package com.thealgorithms.maths;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-
-import java.util.stream.Stream;
 
 public class FindMinTest {
 
@@ -19,13 +18,7 @@ public class FindMinTest {
     }
 
     private static Stream<Arguments> provideStringsForIsBlank() {
-        return Stream.of(
-                Arguments.of(1, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}),
-                Arguments.of(5, new int[] {5, 5, 5, 5, 5}),
-                Arguments.of(0, new int[] {0, 192, 384, 576}),
-                Arguments.of(-1, new int[] {-1, 2, 5, 10}),
-                Arguments.of(-10, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1})
-        );
+        return Stream.of(Arguments.of(1, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(5, new int[] {5, 5, 5, 5, 5}), Arguments.of(0, new int[] {0, 192, 384, 576}), Arguments.of(-1, new int[] {-1, 2, 5, 10}), Arguments.of(-10, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}));
     }
 
     @Test
