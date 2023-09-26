ad09ed5e890f564fbb07c658f4ed81f0ebcde24a
janmesh Singh <Janmesh.js@gmail.com>
2023-09-26 23:21:18 +0530

    Added WildcardMatchingTests

---
+++ src/test/java/com/thealgorithms/dynamicprogramming/WildcardMatchingTest.java
@@ -0,0 +1,28 @@
+package com.thealgorithms.dynamicprogramming;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class WildcardMatchingTest {
+
+    @Test
+    public void testMatchingPattern() {
+        assertTrue(WildcardMatching.isMatch("aa", "a*"));
+        assertTrue(WildcardMatching.isMatch("adceb", "*a*b"));
+
+    }
+
+    @Test
+    public void testNonMatchingPattern() {
+        assertFalse(WildcardMatching.isMatch("cb", "?a"));
+        assertFalse(WildcardMatching.isMatch("acdcb", "a*c?b"));
+        assertFalse(WildcardMatching.isMatch("mississippi", "m*issi*iss?*i"));
+
+
+    }
+
+    @Test
+    public void testEmptyPattern() {
+        assertTrue(WildcardMatching.isMatch("", ""));
+        assertFalse(WildcardMatching.isMatch("abc", ""));
+    }
+}
