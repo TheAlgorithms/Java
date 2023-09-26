cc45f29ba89e7492a61e7633615c660a1465e00a
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2023-09-26 21:25:15 +0200

    Apply suggestions from code review
    - add test case with unsorted array,
    - test FindMax

--- src/test/java/com/thealgorithms/maths/FindMaxTest.java
+++ src/test/java/com/thealgorithms/maths/FindMaxTest.java
@@ -18,11 +18,11 @@ public class FindMaxTest {
     }
 
     private static Stream<Arguments> inputStream() {
-        return Stream.of(Arguments.of(10, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(5, new int[] {5, 5, 5, 5, 5}), Arguments.of(0, new int[] {-1, 0}), Arguments.of(-1, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}));
+        return Stream.of(Arguments.of(10, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(5, new int[] {5, 5, 5, 5, 5}), Arguments.of(0, new int[] {-1, 0}), Arguments.of(-1, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}), Arguments.of(9, new int[] {3, -2, 3, 9, -4, -4, 8}));
     }
 
     @Test
     public void testFindMaxThrowsExceptionForEmptyInput() {
-        assertThrows(IllegalArgumentException.class, () -> FindMin.findMin(new int[] {}));
+        assertThrows(IllegalArgumentException.class, () -> FindMax.findMax(new int[] {}));
     }
 }
