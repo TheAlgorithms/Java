d96608af5ee5443bd9ee8adc6e40de51327af9db
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-26 11:14:48 +0200

    Update FindMaxTest.java

--- src/test/java/com/thealgorithms/maths/FindMaxTest.java
+++ src/test/java/com/thealgorithms/maths/FindMaxTest.java
@@ -1,5 +1,7 @@
 package com.thealgorithms.maths;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,22 +9,20 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertThrows; // Add this import
-
-public class FindMaxTest {
+public class FindMinTest {
 
     @ParameterizedTest
     @MethodSource("inputStream")
-    void numberTests(int[] input) {
-        Assertions.assertEquals(Arrays.stream(input).max().orElseThrow(), FindMax.findMax(input));
+    void numberTests(int expected, int[] input) {
+        Assertions.assertEquals(expected, FindMin.findMin(input));
     }
 
     private static Stream<Arguments> inputStream() {
-        return Stream.of(Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(new int[] {5, 5, 5, 5, 5}), Arguments.of(new int[] {-1, 0}), Arguments.of(new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}));
+        return Stream.of(Arguments.of(10, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(5, new int[] {5, 5, 5, 5, 5}), Arguments.of(0, new int[] {-1, 0}), Arguments.of(-1, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}));
     }
 
     @Test
-    public void testFindMaxThrowsExceptionForEmptyInput() {
-        assertThrows(IllegalArgumentException.class, () -> FindMax.findMax(new int[] {}));
+    public void testFindMinThrowsExceptionForEmptyInput() {
+        assertThrows(IllegalArgumentException.class, () -> FindMin.findMin(new int[] {}));
     }
 }
