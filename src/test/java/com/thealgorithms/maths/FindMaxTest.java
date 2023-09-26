b26f353cc1d484f76bd4f64079e8aeff40786fb0
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-26 11:07:48 +0200

    Changing MaxFindTest

--- src/test/java/com/thealgorithms/maths/FindMaxTest.java
+++ src/test/java/com/thealgorithms/maths/FindMaxTest.java
@@ -1,25 +1,24 @@
 package com.thealgorithms.maths;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows; // Add this import
 
 public class FindMaxTest {
 
-    @Test
-    public void testFindMax0() {
-        assertEquals(10, FindMax.findMax(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    @ParameterizedTest
+    @MethodSource("inputStream")
+    void numberTests(int[] input) {
+        Assertions.assertEquals(Arrays.stream(input).max().orElseThrow(), FindMax.findMax(input));
     }
 
-    @Test
-    public void testFindMax1() {
-        assertEquals(7, FindMax.findMax(new int[] {6, 3, 5, 1, 7, 4, 1}));
-    }
-
-    @Test
-    public void testFindMax2() {
-        assertEquals(10, FindMax.findMax(new int[] {10, 0}));
+    private static Stream<Arguments> inputStream() {
+        return Stream.of(Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(new int[] {5, 5, 5, 5, 5}), Arguments.of(new int[] {-1, 0}), Arguments.of(new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}));
     }
 
     @Test
