c875d6f406e75ec724e90daccfe75be11f995ee6
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-27 09:57:28 +0200

    Tests

---
+++ src/test/java/com/thealgorithms/bitmanipulation/SingleBitOperatorTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SingleBitOperatorTest {
+
+    @Test
+    public void flipBitTest() {
+        assertEquals(1, SingleBitOperators.flipBit(3, 1));
+        assertEquals(11, SingleBitOperators.flipBit(3, 3));
+    }
+
+    @Test
+    public void setBitTest(){
+        assertEquals(5, SingleBitOperators.setBit(4, 0));
+        assertEquals(4, SingleBitOperators.setBit(4, 2));
+
+    }
+
+    @Test
+    public void clearBitTest() {
+        assertEquals(5, SingleBitOperators.clearBit(7,1));
+        assertEquals(5, SingleBitOperators.clearBit(5,1));
+    }
+
+    @Test
+    public void getBitTest(){
+        assertEquals(0, SingleBitOperators.getBit(6, 0));
+        assertEquals(1, SingleBitOperators.getBit(7, 1));
+    }
+
+    @Test
+    public void getFirstBitTest(){
+        assertEquals(1, SingleBitOperators.getFirstBit(11));
+    }
+}
