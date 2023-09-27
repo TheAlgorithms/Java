4ad886f1a4e52cec2fd9b6f590823a6ee8428a67
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-27 09:56:30 +0200

    SingleBitOperators

---
+++ src/main/java/com/thealgorithms/bitmanipulation/SingleBitOperators.java
@@ -0,0 +1,24 @@
+public class SingleBitOperators {
+
+    public static int flipBit(int num, int bit){
+        return num ^ (1 << bit);
+    }
+
+    public static int setBit(int num, int bit) {
+        return num | (1<<bit);
+    }
+
+    public static int clearBit(int num, int clear) {
+        return num & ~(1 << clear);
+    }
+
+    public static int getBit(int num, int get){
+        return ((num>>get) & 1);
+    }
+
+    public static int getFirstBit(int num){
+        return num & 1;
+    }
+
+
+}
