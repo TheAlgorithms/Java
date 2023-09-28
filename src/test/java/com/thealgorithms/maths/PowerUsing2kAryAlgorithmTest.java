db860ee30acc92a76ed323b062b22bd95ed5cfd9
RazatAggarwal <razat.aggarwal@outlook.com>
2023-09-29 03:57:14 +0530

    corrected formatting of files (#4424)

--- src/test/java/com/thealgorithms/maths/PowerUsing2kAryAlgorithmTest.java
+++ src/test/java/com/thealgorithms/maths/PowerUsing2kAryAlgorithmTest.java
@@ -8,23 +8,22 @@ import org.junit.jupiter.api.Test;
  * @author Razat Aggarwal (https://github.com/razat-thapar)
  */
 class PowerUsing2kAryAlgorithmTest {
+	
+    @Test
+    void testPowerUsing2kAryAlgorithmTest() {
+        assertEquals(32L, PowerUsing2kAryAlgorithm.pow(2, 5, 1000000007));
+        assertEquals(754573817L, PowerUsing2kAryAlgorithm.pow(5, 100000, 1000000007));
+        assertEquals(1L, PowerUsing2kAryAlgorithm.pow(5, 0, 1000000007));
+    }
+	
+    @Test
+    void testPowerUsing2kAryAlgorithmTestForWrongInputs() {
+        Exception exception1 = assertThrows(IllegalArgumentException.class, () -> PowerUsing2kAryAlgorithm.pow(5, -2, 1000000007));
+         assertEquals("exponent and mod can't be negative!", exception1.getMessage());
 
-	@Test
-	void testPowerUsing2kAryAlgorithmTest() {
-        assertEquals(32L, PowerUsing2kAryAlgorithm.pow(2, 5,1000000007));
-        assertEquals(754573817L, PowerUsing2kAryAlgorithm.pow(5,100000,1000000007));	
-        assertEquals(1L, PowerUsing2kAryAlgorithm.pow(5,0,1000000007));	
-	}
-	
-	@Test
-	void testPowerUsing2kAryAlgorithmTestForWrongInputs() {
-		Exception exception1 = assertThrows(IllegalArgumentException.class, () -> PowerUsing2kAryAlgorithm.pow(5,-2,1000000007));
-        assertEquals("exponent and mod can't be negative!", exception1.getMessage());
-        
-		Exception exception2 = assertThrows(IllegalArgumentException.class, () -> PowerUsing2kAryAlgorithm.pow(5,2,-1000000007));
-        assertEquals("exponent and mod can't be negative!", exception2.getMessage());
-	}
-	
-	
+        Exception exception2 = assertThrows(IllegalArgumentException.class, () -> PowerUsing2kAryAlgorithm.pow(5, 2, -1000000007));
+         assertEquals("exponent and mod can't be negative!", exception2.getMessage());
 
+    }
+	
 }
