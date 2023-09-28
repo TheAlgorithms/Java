3de87ac4cb38660220a94bfe7922f1a26905fa31
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-28 09:21:42 +0200

    Update SingleBitOperatorTest.java

--- src/test/java/com/thealgorithms/bitmanipulation/SingleBitOperatorTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/SingleBitOperatorTest.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.bitmanipulation;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,25 +8,25 @@ public class SingleBitOperatorTest {
 
     @Test
     public void flipBitTest() {
-        assertEquals(1, SingleBitOperators.flipBit(3, 1));
-        assertEquals(11, SingleBitOperators.flipBit(3, 3));
+        assertEquals(1, SingleBitOperator.flipBit(3, 1));
+        assertEquals(11, SingleBitOperator.flipBit(3, 3));
     }
 
     @Test
     public void setBitTest() {
-        assertEquals(5, SingleBitOperators.setBit(4, 0));
-        assertEquals(4, SingleBitOperators.setBit(4, 2));
+        assertEquals(5, SingleBitOperator.setBit(4, 0));
+        assertEquals(4, SingleBitOperator.setBit(4, 2));
     }
 
     @Test
     public void clearBitTest() {
-        assertEquals(5, SingleBitOperators.clearBit(7, 1));
-        assertEquals(5, SingleBitOperators.clearBit(5, 1));
+        assertEquals(5, SingleBitOperator.clearBit(7, 1));
+        assertEquals(5, SingleBitOperator.clearBit(5, 1));
     }
 
     @Test
     public void getBitTest() {
-        assertEquals(0, SingleBitOperators.getBit(6, 0));
-        assertEquals(1, SingleBitOperators.getBit(7, 1));
+        assertEquals(0, SingleBitOperator.getBit(6, 0));
+        assertEquals(1, SingleBitOperator.getBit(7, 1));
     }
 }
