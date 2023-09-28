0f339b180cc9caa325000ce8c5aeaf2d59892f46
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-28 09:19:02 +0200

    Update and rename SingleBitOperators.java to SingleBitOperator.java

--- src/main/java/com/thealgorithms/bitmanipulation/SingleBitOperator.java
+++ src/main/java/com/thealgorithms/bitmanipulation/SingleBitOperator.java
@@ -4,29 +4,29 @@ package com.thealgorithms.bitmanipulation;
  * Author: lukasb1b (https://github.com/lukasb1b)
  */
 
-public class SingleBitOperators {
+public class final SingleBitOperators {
     /**
      * Flip the bit at position 'bit' in 'num'
      */
-    public static int flipBit(int num, int bit) {
+    public static int flipBit(final int num, final int bit) {
         return num ^ (1 << bit);
     }
     /**
      * Set the bit at position 'bit' to 1 in the 'num' variable
      */
-    public static int setBit(int num, int bit) {
+    public static int setBit(final int num,final int bit) {
         return num | (1 << bit);
     }
     /**
      * Clears the bit located at 'bit' from 'num'
      */
-    public static int clearBit(int num, int bit) {
+    public static int clearBit(final int num,final int bit) {
         return num & ~(1 << bit);
     }
     /**
      * Get the bit located at 'bit' from 'num'
      */
-    public static int getBit(int num, int bit) {
+    public static int getBit(final int num,final int bit) {
         return ((num >> bit) & 1);
     }
 }
