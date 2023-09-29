1c8d34d19228ac3abd9cd63921aa8f95b2b90f17
zaidiyazdan <117353242+zaidiyazdan@users.noreply.github.com>
2023-09-29 23:26:49 +0530

    changes made according to format linter

--- src/main/java/com/thealgorithms/others/SpiralMatrixII.java
+++ src/main/java/com/thealgorithms/others/SpiralMatrixII.java
@@ -1,10 +1,3 @@
-/*
-Problem statement:
-Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order
-Examples: 
-Input: n = 3
-Output: [[1,2,3],[8,9,4],[7,6,5]]
-*/
 package com.thealgorithms.others;
 
 import java.util.Arrays;
