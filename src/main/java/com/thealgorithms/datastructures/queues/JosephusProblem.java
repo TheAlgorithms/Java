ed7b30b4bfea4427cab9a74a5ae4d6fd9db1996e
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-09-29 19:49:35 +0530

    Create JosephusProblem.java

---
+++ src/main/java/com/thealgorithms/datastructures/queues/JosephusProblem.java
@@ -0,0 +1,13 @@
+	static int Josephus(int n,int k) {
+		Queue<Integer> queue=new LinkedList<>();
+		for(int i=1;i<=n;i++) {
+			queue.offer(i);
+		}
+		while(queue.size()>1) {
+			for(int i=0;i<k-1;i++) {
+				queue.offer(queue.poll());//removing the top and enqueuing it in queue
+			}
+			queue.poll();
+		}
+		return queue.peek();	
+	}
