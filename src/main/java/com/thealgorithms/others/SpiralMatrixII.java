911a1d6f3be573cb775308eb2ac1a473a92e610c
zaidiyazdan <117353242+zaidiyazdan@users.noreply.github.com>
2023-09-29 22:27:05 +0530

    Added solution to spiral matrix II

---
+++ src/main/java/com/thealgorithms/others/SpiralMatrixII.java
@@ -0,0 +1,55 @@
+package com.thealgorithms.others;
+
+import java.util.Arrays;
+
+/*Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order
+Example:
+Input: n = 3
+Output: [[1,2,3],[8,9,4],[7,6,5]]
+*/
+
+public class SpiralMatrixII{
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int num = 1;
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+
+        while (num <= n * n) {
+            // Traverse right
+            for (int i = left; i <= right && num <= n * n; i++) {
+                result[top][i] = num++;
+            }
+            top++;
+
+            // Traverse down
+            for (int i = top; i <= bottom && num <= n * n; i++) {
+                result[i][right] = num++;
+            }
+            right--;
+
+            // Traverse left
+            for (int i = right; i >= left && num <= n * n; i--) {
+                result[bottom][i] = num++;
+            }
+            bottom--;
+
+            // Traverse up
+            for (int i = bottom; i >= top && num <= n * n; i--) {
+                result[i][left] = num++;
+            }
+            left++;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SpiralMatrixII solution = new SpiralMatrixII();
+        int n = 3;
+        int[][] result = solution.generateMatrix(n);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(Arrays.toString(result[i]));
+        }
+    }
+}
