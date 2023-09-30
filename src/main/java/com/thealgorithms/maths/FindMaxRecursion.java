107ae993d3f8a99a41c6d91a99e2dfbe769c03d9
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-30 12:46:46 +0200

    Update FindMaxRecursion.java

--- src/main/java/com/thealgorithms/maths/FindMaxRecursion.java
+++ src/main/java/com/thealgorithms/maths/FindMaxRecursion.java
@@ -2,8 +2,7 @@ package com.thealgorithms.maths;
 
 public final class FindMaxRecursion {
 
-    private FindMaxRecursion() {
-        
+    private FindMaxRecursion() {       
     }
     /**
      * Get max of array using divide and conquer algorithm
