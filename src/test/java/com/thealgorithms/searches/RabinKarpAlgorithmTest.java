5caed218da2cc4bfbe3f5d7837c8e15133de3005
Arin <136636751+asapekia@users.noreply.github.com>
2023-10-01 02:15:59 +0530

    formatting issue resolved

--- src/test/java/com/thealgorithms/searches/RabinKarpAlgorithmTest.java
+++ src/test/java/com/thealgorithms/searches/RabinKarpAlgorithmTest.java
@@ -6,17 +6,22 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class RabinKarpAlgorithmTest {
+  RabinKarpAlgorithm RKA = new RabinKarpAlgorithm();
+  int q = 101;
 
-    RabinKarpAlgorithm RKA = new RabinKarpAlgorithm();
-    int q = 101;
-
-    @ParameterizedTest
-    @CsvSource({ "This is an example for rabin karp algorithmn, algorithmn", "AAABBDDG, AAA", "AAABBCCBB, BBCC",
-        "AAAABBBBCCC, CCC", "ABCBCBCAAB, AADB" })
-    void RabinKarpAlgorithmTestExample(String txt, String pat) {
-
+  @ParameterizedTest
+  @CsvSource(
+    {
+      "This is an example for rabin karp algorithmn, algorithmn",
+      "AAABBDDG, AAA",
+      "AAABBCCBB, BBCC",
+      "AAAABBBBCCC, CCC",
+      "ABCBCBCAAB, AADB"
+    }
+  )
+  void RabinKarpAlgorithmTestExample(String txt, String pat) {
     int indexFromOurAlgorithm = RKA.search(pat, txt, q);
     int indexFromLinearSearch = txt.indexOf(pat);
     assertEquals(indexFromOurAlgorithm, indexFromLinearSearch);
-    }
+  }
 }
