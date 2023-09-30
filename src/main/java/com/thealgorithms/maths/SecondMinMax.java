00bd64fd06d8d5786ea92d7756f78d7f305a2584
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-09-30 11:21:11 +0530

    Maven Build Error Resolved

--- src/main/java/com/thealgorithms/maths/SecondMinMax.java
+++ src/main/java/com/thealgorithms/maths/SecondMinMax.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.maths;
 
-public class SecondMinMax {
+public final class SecondMinMax {
 
     /**
      * @brief Finds the Second minimum / maximum value from the array
@@ -10,37 +10,39 @@ public class SecondMinMax {
      * @author Bharath Sanjeevi ( https://github.com/BharathSanjeeviT )
      */
 
-    public static int findSecondMin(int[] arr) {
+    private SecondMinMax() {
+    }
+
+    public static int findSecondMin(final int[] arr) {
         int secMin = Integer.MAX_VALUE, min = Integer.MAX_VALUE;
-        if (arr.length == 0)
-            throw new IllegalArgumentException("Cant find Second Minimum for empty array");
-        else if (arr.length == 1)
-            throw new IllegalArgumentException("Cant find Second Minimum for single element");
-        else
-            for (int num : arr)
-                if (num < min) {
-                    secMin = min;
-                    min = num;
-                } else if ((num < secMin) && (num != min))
-                    secMin = num;
-        if (secMin == Integer.MAX_VALUE) throw new IllegalArgumentException("Cant find Second Minimum in array full of same numbers");
+        checkInput( arr ) ;
+        for (final int num : arr)
+            if (num < min) {
+                secMin = min;
+                min = num;
+            } else if ((num < secMin) && (num != min))
+                secMin = num;
+        if (secMin == Integer.MAX_VALUE) throw new IllegalArgumentException("Cant find Second Maximum / Minimum in array full of same numbers");
         return secMin;
     }
 
-    public static int findSecondMax(int[] arr) {
+    public static int findSecondMax(final int[] arr) {
         int secMax = Integer.MIN_VALUE, max = Integer.MIN_VALUE;
-        if (arr.length == 0)
-            throw new IllegalArgumentException("Cant find Second Maximum for empty array");
-        else if (arr.length == 1)
-            throw new IllegalArgumentException("Cant find Second Maximum for single element");
-        else
-            for (int num : arr)
-                if (num > max) {
-                    secMax = max;
-                    max = num;
-                } else if ((num > secMax) && (num != max))
-                    secMax = num;
-        if (secMax == Integer.MIN_VALUE) throw new IllegalArgumentException("Cant find Second Maximum in array full of same numbers");
+        checkInput( arr ) ;
+        for (final int num : arr)
+            if (num > max) {
+                secMax = max;
+                max = num;
+            } else if ((num > secMax) && (num != max))
+                secMax = num;
+        if (secMax == Integer.MIN_VALUE) throw new IllegalArgumentException("Cant find Second Maximum / Minimum in array full of same numbers");
         return secMax;
+    }
+
+    private static void checkInput(final int[] arr) {
+        if (arr.length == 0)
+            throw new IllegalArgumentException("Cant find Second Maximum / Minimum for empty array");
+        else if (arr.length == 1)
+            throw new IllegalArgumentException("Cant find Second Maximum / Minimum for single element");
     }
 }
