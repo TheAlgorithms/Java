8e920774902bdb1702817aa4f4271b87511ddae5
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-09-30 23:56:18 +0530

    Added Program to find Second Min / Max

--- src/test/java/com/thealgorithms/maths/SecondMinMaxTest.java
+++ src/test/java/com/thealgorithms/maths/SecondMinMaxTest.java
@@ -52,6 +52,7 @@ public class SecondMinMaxTest {
     }
 
     private static Stream<Arguments> inputStream() {
-        return Stream.of(Arguments.of(new TestCase(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2, 9)), Arguments.of(new TestCase(new int[] {5, 5, 4, 5, 5}, 5, 4)), Arguments.of(new TestCase(new int[] {-1, 0}, 0, -1)), Arguments.of(new TestCase(new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}, -9, -2)), Arguments.of(new TestCase(new int[] {3, -2, 3, 9, -4, -4, 8}, -2, 8)));
+        return Stream.of(Arguments.of(new TestCase(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 2, 9)), Arguments.of(new TestCase(new int[] {5, 4, 5, 5, 5}, 5, 4)), Arguments.of(new TestCase(new int[] {-1, 0}, 0, -1)),
+            Arguments.of(new TestCase(new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}, -9, -2)), Arguments.of(new TestCase(new int[] {3, -2, 3, 9, -4, -4, 8}, -2, 8)));
     }
 }
