718ca30e8df8118b45b8b727352a467f2f238e3a
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-09-30 11:07:06 +0530

    Clang-lint-error resolved

--- src/test/java/com/thealgorithms/maths/SecondMinMaxTest.java
+++ src/test/java/com/thealgorithms/maths/SecondMinMaxTest.java
@@ -12,28 +12,28 @@ public class SecondMinMaxTest {
     private static final String EXP_MSG_ARR_SAME_ELE = "Cant find Second Maximum / Minimum in array full of same numbers";
 
     @Test
-    public void testForEmptyInputArray(){
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()->SecondMinMax.findSecondMin(new int[]{}));
+    public void testForEmptyInputArray() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> SecondMinMax.findSecondMin(new int[] {}));
         assertEquals(exception.getMessage(), EXP_MSG_ARR_IS_EMPTY);
     }
 
     @Test
-    public void testForArrayWithSingleElement(){
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()->SecondMinMax.findSecondMax(new int[]{1}));
+    public void testForArrayWithSingleElement() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> SecondMinMax.findSecondMax(new int[] {1}));
         assertEquals(exception.getMessage(), EXP_MSG_ARR_SINGLE_ELE);
     }
 
     @Test
-    public void testForArrayWithSameElements(){
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()->SecondMinMax.findSecondMin(new int[]{1,1,1,1}));
+    public void testForArrayWithSameElements() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> SecondMinMax.findSecondMin(new int[] {1, 1, 1, 1}));
         assertEquals(exception.getMessage(), EXP_MSG_ARR_SAME_ELE);
     }
 
     @Test
-    public void testForValidOutput(){
-        assertEquals(2, SecondMinMax.findSecondMin(new int[]{1,2}));
-        assertEquals(1, SecondMinMax.findSecondMax(new int[]{1,2}));
-        assertEquals(2, SecondMinMax.findSecondMin(new int[]{10,1,2,4,6,3,5,7,3,2,5,7,4,2,1,212,343,565,2232,65565}));
-        assertEquals(2232, SecondMinMax.findSecondMax(new int[]{10,1,2,4,6,3,5,7,3,2,5,7,4,2,1,212,343,565,2232,65565}));
+    public void testForValidOutput() {
+        assertEquals(2, SecondMinMax.findSecondMin(new int[] {1, 2}));
+        assertEquals(1, SecondMinMax.findSecondMax(new int[] {1, 2}));
+        assertEquals(2, SecondMinMax.findSecondMin(new int[] {10, 1, 2, 4, 6, 3, 5, 7, 3, 2, 5, 7, 4, 2, 1, 212, 343, 565, 2232, 65565}));
+        assertEquals(2232, SecondMinMax.findSecondMax(new int[] {10, 1, 2, 4, 6, 3, 5, 7, 3, 2, 5, 7, 4, 2, 1, 212, 343, 565, 2232, 65565}));
     }
 }
