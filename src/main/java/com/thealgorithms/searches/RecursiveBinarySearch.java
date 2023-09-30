c489b1c0dd67f386d8e889c7437ce9912d5e026f
Kstrella <90272570+Kstrella@users.noreply.github.com>
2023-09-30 17:22:52 -0400

    Added an Implementation for a Recursive Binary Search

--- src/main/java/com/thealgorithms/searches/RecursiveBinarySearch.java
+++ src/main/java/com/thealgorithms/searches/RecursiveBinarySearch.java
@@ -15,31 +15,25 @@ public class RecursiveBinarySearch {
      * @author Kevin Estrella (https://github.com/Kstrella)
      */
 
-    public static int recBinarySearch(int[] arr, int start, int end, int target)
-    {
+    public static int recBinarySearch(int[] arr, int start, int end, int target) {
         if (end >= start) {
             // Find the middle of the array
             int mid = start + (end - start) / 2;
 
             // If the target is the middle of the array return it
-            if (arr[mid] == target)
-                return mid;
+            if (arr[mid] == target) return mid;
 
             // If target is smaller than mid, then we search the left sub-array
-            if (arr[mid] > target)
-                return recBinarySearch(arr, start, mid - 1, target);
+            if (arr[mid] > target) return recBinarySearch(arr, start, mid - 1, target);
 
             // If target is greater than mid, then we search the right sub-array
             return recBinarySearch(arr, mid + 1, end, target);
         }
-
-        //If the target does not exist in the array
         return -1;
     }
 
-    public static void main(String[] args)
-    {
-        int[] arr = {1, 2, 4, 7, 10, 22, 31, 40,55,97,100};
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 7, 10, 22, 31, 40, 55, 97, 100};
         int target = 13;
         int result = recBinarySearch(arr, 0, arr.length - 1, target);
         if (result == -1)
