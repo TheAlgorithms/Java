eb7dd974a3daf7b2218b62c69030c990ac799b3e
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-09-30 13:23:51 +0530

    Test Arguements are Streamed

--- src/test/java/com/thealgorithms/maths/SecondMinMaxTest.java
+++ src/test/java/com/thealgorithms/maths/SecondMinMaxTest.java
@@ -1,26 +1,31 @@
 package com.thealgorithms.maths;
 
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class SecondMinMaxTest {
 
-    private static final String EXP_MSG_ARR_IS_EMPTY = "Cant find Second Maximum / Minimum for empty array";
-    private static final String EXP_MSG_ARR_SINGLE_ELE = "Cant find Second Maximum / Minimum for single element";
-    private static final String EXP_MSG_ARR_SAME_ELE = "Cant find Second Maximum / Minimum in array full of same numbers";
+    private static final String EXP_MSG_ARR_LEN_LESS_2 = "Input array must have length of at least two";
+    private static final String EXP_MSG_ARR_SAME_ELE = "Input array should have at least 2 distinct elements";
 
     @Test
     public void testForEmptyInputArray() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> SecondMinMax.findSecondMin(new int[] {}));
-        assertEquals(exception.getMessage(), EXP_MSG_ARR_IS_EMPTY);
+        assertEquals(exception.getMessage(), EXP_MSG_ARR_LEN_LESS_2);
     }
 
     @Test
     public void testForArrayWithSingleElement() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> SecondMinMax.findSecondMax(new int[] {1}));
-        assertEquals(exception.getMessage(), EXP_MSG_ARR_SINGLE_ELE);
+        assertEquals(exception.getMessage(), EXP_MSG_ARR_LEN_LESS_2);
     }
 
     @Test
@@ -29,11 +34,23 @@ public class SecondMinMaxTest {
         assertEquals(exception.getMessage(), EXP_MSG_ARR_SAME_ELE);
     }
 
-    @Test
-    public void testForValidOutput() {
-        assertEquals(2, SecondMinMax.findSecondMin(new int[] {1, 2}));
-        assertEquals(1, SecondMinMax.findSecondMax(new int[] {1, 2}));
-        assertEquals(2, SecondMinMax.findSecondMin(new int[] {10, 1, 2, 4, 6, 3, 5, 7, 3, 2, 5, 7, 4, 2, 1, 212, 343, 565, 2232, 65565}));
-        assertEquals(2232, SecondMinMax.findSecondMax(new int[] {10, 1, 2, 4, 6, 3, 5, 7, 3, 2, 5, 7, 4, 2, 1, 212, 343, 565, 2232, 65565}));
+    @ParameterizedTest
+    @MethodSource("inputStreamMin")
+    void numberTestsMin(int expected, int[] input) {
+        Assertions.assertEquals(expected, SecondMinMax.findSecondMin(input));
+    }
+
+    private static Stream<Arguments> inputStreamMin() {
+        return Stream.of(Arguments.of(2, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(5, new int[] {5, 4, 5, 5, 5}), Arguments.of(0, new int[] {-1, 0}), Arguments.of(-9, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}), Arguments.of(-2, new int[] {3, -2, 3, 9, -4, -4, 8}));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputStreamMax")
+    void numberTestsMax(int expected, int[] input) {
+        Assertions.assertEquals(expected, SecondMinMax.findSecondMax(input));
+    }
+
+    private static Stream<Arguments> inputStreamMax() {
+        return Stream.of(Arguments.of(9, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), Arguments.of(4, new int[] {5, 5, 4, 5, 5}), Arguments.of(-1, new int[] {-1, 0}), Arguments.of(-2, new int[] {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1}), Arguments.of(8, new int[] {3, -2, 3, 9, -4, -4, 8}));
     }
 }
