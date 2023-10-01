58e4b331c2d10010e4bbe37bd33408ae80ace698
ANKIT SAHA <85545712+Ankit-Saha08@users.noreply.github.com>
2023-10-01 12:29:43 +0530

    Update src/main/java/com/thealgorithms/strings/Pangram.java
    Co-authored-by: Debasish Biswas <debasishbsws.dev@gmail.com>

--- src/main/java/com/thealgorithms/strings/Pangram.java
+++ src/main/java/com/thealgorithms/strings/Pangram.java
@@ -23,8 +23,8 @@ public class Pangram {
      * @param s The String to check
      * @return {@code true} if s is a Pangram, otherwise {@code false}
      */
+     // alternative approach using Java Collection Framework
     public static boolean isPangramOrNot(String s) {
-        // an approach using Java Collection Framework
         HashSet<Character> alpha = new HashSet<Character>();
         s = s.trim().toLowerCase();
         for (int i = 0; i < s.length(); i++)
