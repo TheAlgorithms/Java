2fd36b371211678c7768b07babd54b2a3020afd6
Aman <84791435+Aman28801@users.noreply.github.com>
2023-10-01 21:18:24 +0530

    Added MirrorOfMatrixTest.java

---
+++ src/test/java/com/thealgorithms/misc/MirrorOfMatrixTest.java
@@ -0,0 +1,63 @@
+package com.thealgorithms.misc;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class MirrorOfMatrixTest {
+
+    @Test
+    public void testMirrorWithEmptyMatrix() {
+        int[][] arr = new int[0][0];
+        MirrorOfMatrix.Mirror(arr);
+        // Since the matrix is empty, there's nothing to mirror.
+        // So, the expected result is also an empty matrix.
+        int[][] expected = new int[0][0];
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testMirrorWithSingleRowMatrix() {
+        int[][] arr = { { 1, 2, 3, 4 } };
+        MirrorOfMatrix.Mirror(arr);
+        // The expected result is the reverse of the input row.
+        int[][] expected = { { 4, 3, 2, 1 } };
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testMirrorWithMultipleRowMatrix() {
+        int[][] arr = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        MirrorOfMatrix.Mirror(arr);
+        // The expected result is each row reversed.
+        int[][] expected = {
+                { 3, 2, 1 },
+                { 6, 5, 4 },
+                { 9, 8, 7 }
+        };
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testMirrorWithOddLengthRows() {
+        int[][] arr = {
+                { 1, 2, 3 },
+                { 4, 5, 6, 7 },
+                { 8, 9 }
+        };
+        MirrorOfMatrix.Mirror(arr);
+        // The expected result is each row reversed.
+        int[][] expected = {
+                { 3, 2, 1 },
+                { 7, 6, 5, 4 },
+                { 9, 8 }
+        };
+        assertArrayEquals(expected, arr);
+    }
+}
+
