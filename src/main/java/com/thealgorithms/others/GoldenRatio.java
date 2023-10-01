e208eaddaa0183587259a2b23421cfb69172b097
Jaideep Kumar Singh <cs2027@global.org.in>
2023-10-01 21:27:52 +0530

    Add files via upload

---
+++ src/main/java/com/thealgorithms/others/GoldenRatio.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GoldenRatio {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(nthFibonacci(n));
+    }
+
+    public static long nthFibonacci(long n) {
+        double a = Math.sqrt(5);
+        double b = Math.pow((1 + a) * 0.5, n);
+        double c = Math.pow((1 - a) * 0.5, n);
+        double ans = (1 / a) * (b - c);
+        return (long) ans;
+    }
+}
