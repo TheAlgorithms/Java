1dbf067ac9e5ac70a6ccb4c88b2a7281d6d3e517
Anshuman Khatua <anshumankhatua78@gmail.com>
2023-10-02 04:12:22 +0530

    update v5

--- src/main/java/com/thealgorithms/dynamicprogramming/painter'sProblem.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/painter'sProblem.java
@@ -17,43 +17,43 @@ class PainterProblem {
      *
      */
      static int partition(int[] arr, int n, int k){
-         /**
-          *  If there is only one painter --> base case
-          *  If there is only one board --> base case
-           */
-         if (k == 1) {
-             return sum(arr, 0, n - 1);
-         }
-         if (n == 1) {
-             return arr[0];
-         }
-         /*
-          * this is the maximum amount of time that can be considered if the
-          * input of the boards array  is infinity.
-          */
+        /**
+         *  If there is only one painter --> base case
+         *  If there is only one board --> base case
+         */
+        if (k == 1) {
+            return sum(arr, 0, n - 1);
+        }
+        if (n == 1) {
+            return arr[0];
+        }
+        /*
+         * this is the maximum amount of time that can be considered if the
+         * input of the boards array  is infinity.
+         */
 
-         int max = Integer.MAX_VALUE;
+        int max = Integer.MAX_VALUE;
 
-         /*
-          * find minimum of all possible maximum k -1 partitions
-          * to the left of arr[i], with i elements, put k-1th divider
-          * between arr[i-1] & arr[i] to get k-th partition.
-          */
+        /*
+         * find minimum of all possible maximum k -1 partitions
+         * to the left of arr[i], with i elements, put k-1th divider
+         * between arr[i-1] & arr[i] to get k-th partition.
+         */
 
-         for (int i = 0; i <= n; i++) {
-             max = Math.min(max,Math.max(partition(arr,i,k-1),sum(arr,i,n-1)));
-         }
+        for (int i = 0; i <= n; i++) {
+            max = Math.min(max,Math.max(partition(arr,i,k-1),sum(arr,i,n-1)));
+        }
 
-         return max;
-     }
+        return max;
+    }
     /**
      * This method calculates the sum between given two index.
      * @param An integer array same, A start index and A end index.
      * @return total sum of array in between the specified index.
      */
-    static int sum(int [] arr , int start ,  int end){
+    static int sum(int[] arr, int start,  int end){
         int sum =  0;
-        for (int i = start; i <=  end; i++) {
+        for (int i = start; i <= end; i++) {
             sum += arr[i];
         }
         return sum;
@@ -61,10 +61,9 @@ class PainterProblem {
 
     // Main method for a small test case.
     public static void main(String[] args) {
-        int [] arr = { 30, 80, 67, 54, 90, 54, 76};
+        int[] arr = {30, 80, 67, 54, 90, 54, 76};
         int n = arr.length;
         int k = 2;
-        System.out.println(partition(arr,n,k));
+        System.out.println(partition(arr, n, k));
     }
-
 }
