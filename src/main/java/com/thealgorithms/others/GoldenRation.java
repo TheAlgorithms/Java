26511b27077124ea931732522223c975af6402f4
Jaideep Kumar Singh <jaideepkumarsingh14@gmail.com>
2023-10-01 21:16:59 +0530

    Update GoldenRation.java

--- src/main/java/com/thealgorithms/others/GoldenRation.java
+++ src/main/java/com/thealgorithms/others/GoldenRation.java
@@ -2,14 +2,14 @@ package com.thealgorithms.others;
 
 import java.util.*;
 
-class GoldenRatio{
+class GoldenRatio {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
     int n = input.nextInt();
     System.out.println(nthFibonacci(n));
   }
 
-  public static long nthFibonacci(long n){
+  public static long nthFibonacci(long n) {
     double a = Math.sqrt(5);
     double b = Math.pow((1 + a) * 0.5, n);
     double c = Math.pow((1 - a) * 0.5, n);
