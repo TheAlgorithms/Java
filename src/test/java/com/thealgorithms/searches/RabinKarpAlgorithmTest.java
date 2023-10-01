723d56da6b5cb77814016da3e4a37751bba32a22
vil02 <vil02@o2.pl>
2023-10-01 18:02:00 +0200

    tests: add missing test case

--- src/test/java/com/thealgorithms/searches/RabinKarpAlgorithmTest.java
+++ src/test/java/com/thealgorithms/searches/RabinKarpAlgorithmTest.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.searches;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -9,7 +9,7 @@ class RabinKarpAlgorithmTest {
     RabinKarpAlgorithm RKA = new RabinKarpAlgorithm();
 
     @ParameterizedTest
-    @CsvSource({"This is an example for rabin karp algorithmn, algorithmn, 101", "AAABBDDG, AAA, 137", "AAABBCCBB, BBCC, 131", "AAAABBBBCCC, CCC, 41", "ABCBCBCAAB, AADB, 293", "Algorithm The Algorithm, Algorithm, 101"})
+    @CsvSource({"This is an example for rabin karp algorithmn, algorithmn, 101", "AAABBDDG, AAA, 137", "AAABBCCBB, BBCC, 101", "AAABBCCBB, BBCC, 131", "AAAABBBBCCC, CCC, 41", "ABCBCBCAAB, AADB, 293", "Algorithm The Algorithm, Algorithm, 101"})
     void RabinKarpAlgorithmTestExample(String txt, String pat, int q) {
         int indexFromOurAlgorithm = RKA.search(pat, txt, q);
         int indexFromLinearSearch = txt.indexOf(pat);
