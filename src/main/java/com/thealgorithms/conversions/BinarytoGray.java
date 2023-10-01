a73251069ab5c45b0646e5e7eb10de0ec8bf99b1
Sanket <sanketpatil7467@gmail.com>
2023-10-02 01:02:59 +0530

    done_changes_binaryToGray

--- src/main/java/com/thealgorithms/conversions/BinarytoGray.java
+++ src/main/java/com/thealgorithms/conversions/BinarytoGray.java
@@ -19,37 +19,35 @@ import java.io.IOException;
 import java.util.*;
 class BinarytoGray{
     //Function to convert given binary code to gray code.
-    public String binarytogray(String bin)
-    {
+    public String binaryToGray(String bin) {
         StringBuilder sb = new StringBuilder();
-        // Initializing a 2d matrice with one column for given binary code and 
-        // another column for converted gray code
-        char[][] matrice=new char[bin.length()][2];
+        /*
+        Initializing a 2d matrice with one column for given binary code and 
+        another column for converted gray code
+        */
+        char[][] matrice = new char[bin.length()][2];
 
         int i;
-        //Assigning the Initial Values
+        // Assigning the Initial Values
         matrice[0][0] = (bin.charAt(0));
-        matrice[0][1] = (bin.charAt(0));//MSB of Gray code is same as that of given Binary code
-        for(i = 1 ; i < bin.length() ; i++)
-        {
-            //taking the XOR of current index character of binary code with 
-            // the previous index character of binary code
-            char x=(bin.charAt(i)==bin.charAt(i-1))?'0':'1';
-            matrice[i][1]=x;
-            matrice[i][0]=bin.charAt(i);
-        }
+        matrice[0][1] = (bin.charAt(0)); // MSB of Gray code is same as that of given Binary code
+        for (i = 1; i < bin.length(); i++) {
+            // taking the XOR of current index character of binary code with
+            //  the previous index character of binary code
+            char x = (bin.charAt(i) == bin.charAt(i - 1)) ? '0' : '1';
+            matrice[i][1] = x;
+            matrice[i][0] = bin.charAt(i);
+         }
 
-        for(i = 0 ; i < bin.length() ; i++)
-        {
+        for (i = 0; i < bin.length(); i++) {
             sb.append(matrice[i][1]);
         }
         return sb.toString();
     }
 
-    public static void main(String as[]) throws IOException
-    {
-        BinarytoGray obj=new BinarytoGray();
-        String bin="1001001";//Given Binary Code
-        System.out.println(obj.binarytogray(bin));
+    public static void main(String as[]) throws IOException {
+        BinarytoGray obj = new BinarytoGray();
+        String bin = "1001001"; // Given Binary Code
+        System.out.println(obj.binaryToGray(bin));
     }   
 }
--- src/main/java/com/thealgorithms/conversions/GraytoBinary.java
+++ src/main/java/com/thealgorithms/conversions/GraytoBinary.java
@@ -14,35 +14,33 @@ Gray to Binary:
 
 import java.io.IOException;
 import java.util.*;
-class GraytoBinary{
-    //Function to convert given binary code to gray code.
-    public String graytobinary(String gray)
-    {
+class GraytoBinary {
+    // Function to convert given binary code to gray code.
+    public String grayToBinary(String gray) {
         StringBuilder sb = new StringBuilder();
-        // Initializing a 2d matrice with one column for given gray code and 
-        // another column for converted binary code
-        char[][] matrice=new char[gray.length()][2];
+        /*
+        Initializing a 2d matrice with one column for given gray code and 
+        another column for converted binary code
+        */
+        char[][] matrice = new char[gray.length()][2];
 
         int i;
-        //Assigning the Initial Values
-        matrice[0][0]=(gray.charAt(0));
-        matrice[0][1]=(gray.charAt(0));//MSB of binary code is same as that of given Gray code
 
-        for(i=1;i<gray.length();i++)
-        {
-            matrice[i][0]=gray.charAt(i);
-            // if the current character is '0' 
-            // we copy the previous character of the binary code
-            if(gray.charAt(i)=='0')
-                matrice[i][1]=(matrice[i-1][1]);
+        // Assigning the Initial Values
+        matrice[0][0] = (gray.charAt(0));
+        matrice[0][1] = (gray.charAt(0)); // MSB of binary code is same as that of given Gray code
+
+        for (i = 1; i < gray.length(); i++) {
+            matrice[i][0] = gray.charAt(i);
+            // if the current character is '0' we copy the previous character of the binary code
+            if (gray.charAt(i) == '0') matrice[i][1] = (matrice[i - 1][1]);
             // else we copy the flipped value of the previous gray code character
             // i.e if its '0' we take '1' and vice-versa
             else
-                matrice[i][1]=(matrice[i-1][1]=='0')?'1':'0';
+                matrice[i][1] = (matrice[i - 1][1] == '0') ? '1' : '0';
         }
 
-        for(i=0;i<gray.length();i++)
-        {
+        for (i = 0; i < gray.length(); i++) {
             sb.append(matrice[i][1]);
         
         }
@@ -51,8 +49,8 @@ class GraytoBinary{
 
     public static void main(String as[]) throws IOException
     {
-        GraytoBinary obj=new GraytoBinary();
-        String gray="1101101";//Given Gray Code
-        System.out.println(obj.graytobinary(gray));
+        GraytoBinary obj = new GraytoBinary();
+        String gray = "1101101"; // Given Gray Code
+        System.out.println(obj.grayToBinary(gray));
     }   
 }
