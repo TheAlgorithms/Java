6d08ab25d749cad3be51e0c27fcb2d563f182b1d
Vineetttt <vineetchotaliya30@gmail.com>
2023-10-02 02:39:06 +0530

    modify dijkstras and adding JUnit Tests

--- src/main/java/com/thealgorithms/greedyalgorithms/DijkstrasAlgorithm.java
+++ src/main/java/com/thealgorithms/greedyalgorithms/DijkstrasAlgorithm.java
@@ -1,68 +1,39 @@
 package com.thealgorithms.greedyalgorithms;
 
-import java.util.ArrayList;
 import java.util.PriorityQueue;
-@SuppressWarnings("unchecked")
 
-//Link : https://en.wikipedia.org/wiki/Dijkstra's_algorithm
+// Link: https://en.wikipedia.org/wiki/Dijkstra's_algorithm
 
 public class DijkstrasAlgorithm {
-    static class Edge{
-        int src;
-        int dest;
-        int wt;
 
-        public Edge(int s,int d,int w){
-            this.src = s;
-            this.dest = d;
-            this.wt = w;
-        }
-    }
-    static void createGraph(ArrayList<Edge> graph[]){
-        for(int i=0;i<graph.length;i++){
-            graph[i] = new ArrayList<>();
-        }
-        graph[0].add(new Edge(0,1,2));
-        graph[0].add(new Edge(0,2,4));
-
-        graph[1].add(new Edge(1,3,7));
-        graph[1].add(new Edge(1,2,1));
-
-        graph[2].add(new Edge(2,4,3));
-
-        graph[3].add(new Edge(3,5,1));
-
-        graph[4].add(new Edge(4,3,2));
-        graph[4].add(new Edge(4,5,5));
-    }
-
-    static class Pair implements Comparable<Pair>{
+    static class Pair implements Comparable<Pair> {
         int n;
         int path;
 
-        public Pair(int n,int path){
+        public Pair(int n, int path) {
             this.n = n;
             this.path = path;
         }
 
         @Override
-        public int compareTo(Pair p2){
-            return this.path-p2.path;
+        public int compareTo(Pair p2) {
+            return this.path - p2.path;
         }
     }
 
     // Function to perform Dijkstra's algorithm to find shortest paths
-    public static void dijkstra(ArrayList<Edge> graph[], int src) {
-        int dist[] = new int[graph.length];
+    public static int[] dijkstra(int[][] graph, int src) {
+        int n = graph.length;
+        int[] dist = new int[n];
 
         // Initialize distances to all nodes as infinity except the source node
-        for (int i = 0; i < graph.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (i != src) {
                 dist[i] = Integer.MAX_VALUE;
             }
         }
 
-        boolean vis[] = new boolean[graph.length];
+        boolean[] vis = new boolean[n];
         PriorityQueue<Pair> pq = new PriorityQueue<>();
         pq.add(new Pair(src, 0));
 
@@ -74,32 +45,27 @@ public class DijkstrasAlgorithm {
                 vis[curr.n] = true;
 
                 // Explore neighbors of the current node
-                for (int i = 0; i < graph[curr.n].size(); i++) {
-                    Edge e = graph[curr.n].get(i);
-                    int u = e.src;
-                    int v = e.dest;
-                    int wt = e.wt;
+                for (int v = 0; v < n; v++) {
+                    if (graph[curr.n][v] != 0) { // Check if there is an edge
+                        int wt = graph[curr.n][v];
 
-                    // Relaxation step: Update the distance if a shorter path is found
-                    if (dist[u] + wt < dist[v]) {
-                        dist[v] = dist[u] + wt;
-                        pq.add(new Pair(v, dist[v]));
+                        // Relaxation step: Update the distance if a shorter path is found
+                        if (dist[curr.n] != Integer.MAX_VALUE && dist[curr.n] + wt < dist[v]) {
+                            dist[v] = dist[curr.n] + wt;
+                            pq.add(new Pair(v, dist[v]));
+                        }
                     }
                 }
             }
         }
 
-        // Print the shortest distances to each node
+        return dist;
+    }
+
+    public static void printShortestDistance(int[] dist) {
         for (int i = 0; i < dist.length; i++) {
             System.out.print(dist[i] + " ");
         }
         System.out.println();
-    }
-    public static void main(String[] args) {
-        int V = 6;
-        ArrayList<Edge> graph[] = new ArrayList[V];
-        createGraph(graph);
-
-        dijkstra(graph,0);
     }
 }
