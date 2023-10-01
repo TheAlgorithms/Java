a9671e08b01ad78e6b973d4df7841a2ded07422a
Nimit Panwar <71833404+nejim3h@users.noreply.github.com>
2023-10-01 10:16:28 +0530

    Update ZigZagConversionTest.java

--- src/test/java/com/thealgorithms/misc/ZigZagConversionTest.java
+++ src/test/java/com/thealgorithms/misc/ZigZagConversionTest.java
@@ -1,7 +1,7 @@
 package com.thealgorithms.misc;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class ZigZagConversionTest {
 
