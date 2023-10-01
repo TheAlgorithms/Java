9f04fbdb75afc26351178853a85fb0b85a4a0af4
suchib <suchibansal2007@gmail.com>
2023-10-01 13:36:16 +0530

    added test cases

---
+++ src/test/java/com/thealgorithms/strings/ReverseWordInStringTest.java
@@ -0,0 +1,14 @@
+package com.thealgorithms.strings;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.Test;
+
+public class ReverseWordInStringTest {
+
+    @Test
+    public void testCorrectReverseWordsInTheString() {
+        assertEquals("blue is Sky", ReverseWordInString.reverseWords("Sky is blue"));
+    }
+}
