18ec54f6ce9826ec34dd6d7c3147739f294be4b6
ashwinbekal <ashwin.cy21@sahyadri.edu.in>
2023-10-01 18:46:19 +0530

    Compound Interest Calculator

---
+++ src/main/java/CompoundD.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class CompoundInterest{
+    double monthlyInterestRate,cdvalue,deposit,apy;
+    int maturityPeriod;
+    void Compute(){
+        System.out.print("Enter the Initial Deposit Amount: ");
+        try (Scanner input = new Scanner(System.in)) {
+            deposit=input.nextDouble();
+            System.out.print("Enter the Annual Percentage Yield: ");
+            apy=input.nextDouble();
+            System.out.print("Enter the Maturity Period (No. of Months): ");
+            maturityPeriod=input.nextInt();
+        }
+        monthlyInterestRate=deposit*(.0001*apy);
+        System.out.println("The Monthly Interest Rate is: "+monthlyInterestRate);
+        double cdvalue;
+        System.out.println("Month"+"\t"+"CD Value");
+        for (int i = 1; i <= maturityPeriod; i++) {
+            cdvalue=(deposit+deposit*apy/1200);
+            deposit=cdvalue;
+            System.out.println(i+"\t"+cdvalue);
+        }
+    }
+}
+public class CompoundD{
+    public static void main(String[] args) {
+        CompoundInterest c=new CompoundInterest();
+        c.Compute();
+        System.out.print("");
+    }
+}
