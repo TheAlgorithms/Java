c3e91f50c8bb98278624d35c67be29cc3513f715
Dhruv127 <dhruvqagarwal@gmail.com>
2023-10-01 21:39:40 +0530

    Add solution for Loot Houses problem

---
+++ src/main/java/com/thealgorithms/dynamicprogramming/LootHouses.java
@@ -0,0 +1,58 @@
+package com.thealgorithms.dynamicprogramming;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class LootHouses {
+    // Recursive function to calculate maximum loot using given indices
+    public static int maxLootRecursive(List<Integer> houses, int index) {
+        if (index >= houses.size()) {
+            return 0;
+        }
+
+        // Calculate maximum loot if we take the current house
+        int take = houses.get(index) + maxLootRecursive(houses, index + 2);
+
+        // Calculate maximum loot if we skip the current house
+        int notTake = maxLootRecursive(houses, index + 1);
+
+        // Return the maximum of the two options
+        return Math.max(take, notTake);
+    }
+
+    // Dynamic programming function to calculate maximum loot
+    public static int maxLootDP(List<Integer> houses) {
+        int n = houses.size();
+        int[] dp = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i >= n) {
+                dp[i] = 0;
+            } else {
+                int take = houses.get(i);
+
+                if (i + 2 < n) {
+                    take += dp[i + 2];
+                }
+
+                int notTake = dp[i + 1];
+
+                dp[i] = Math.max(take, notTake);
+            }
+        }
+
+        return dp[0];
+    }
+
+    public static void main(String[] args) {
+        List<Integer> houses = Arrays.asList(2, 7, 13, 8, 1);
+
+        // Calculate maximum loot using recursion
+        int maxLootRecursion = maxLootRecursive(houses, 0);
+        System.out.println("Maximum loot (Recursive): " + maxLootRecursion);
+
+        // Calculate maximum loot using dynamic programming
+        int maxLootDynamic = maxLootDP(houses);
+        System.out.println("Maximum loot (Dynamic Programming): " + maxLootDynamic);
+    }
+}
