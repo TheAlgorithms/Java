42f3c205f3b93356e990aa0ab542db4e7bc9c4c2
ANKIT SAHA <85545712+Ankit-Saha08@users.noreply.github.com>
2023-10-02 20:45:29 +0530

    Updated linting errors in LongestPalindromicSubstring.java

--- src/main/java/com/thealgorithms/strings/LongestPalindromicSubstring.java
+++ src/main/java/com/thealgorithms/strings/LongestPalindromicSubstring.java
@@ -1,8 +1,9 @@
 package com.thealgorithms.strings;
 
 // Longest Palindromic Substring
-import java.util.Scanner;
 import java.util.Collections;
+import java.util.Scanner;
+
 
 class LongestPalindromicSubstring {
 
@@ -18,28 +19,27 @@ class LongestPalindromicSubstring {
 }
 
 class Solution {
-  private boolean Palindrome(String str){
-    str = str.toLowerCase();
-    String rev = "";
-    for (int i=str.length()-1; i>-1; i--) rev += str.charAt(i);
+    private boolean Palindrome(String str) {
+        str = str.toLowerCase();
+        String rev = "";
+        for (int i = str.length() - 1; i > -1; i--) rev += str.charAt(i);
     if (str.equals(rev) return true;
     return false;
-  }
-
-  public String longestPalindrome(String str) {
-    // brute-force approach using Java Collections 
-    ArrayList<String> al = new ArrayList<>();
-    for (int i=0; i<str.length() ; i++){
-      for (int j=i+1; j<str.length()-1; j++){
-        String sub = str.substring(i, j);
-        boolean flag = Palindrome(sub);
-        if (flag == true)
-          al.add(sub);
-      }
     }
-    Collections.sort(al);
-    String res = al.get(al.size()-1);
-    if (res.length()<2) res = "none";
-    return res;
-  }
+
+    public String longestPalindrome(String str) {
+        // brute-force approach using Java Collections 
+        ArrayList<String> al = new ArrayList<>();
+        for (int i = 0; i < str.length() ; i++) {
+            for (int j = i + 1; j < str.length() - 1; j++) {
+                String sub = str.substring(i, j);
+                boolean flag = Palindrome(sub);
+                if (flag == true) al.add(sub);
+            }
+        }
+        Collections.sort(al);
+        String res = al.get(al.size() - 1);
+        if (res.length() < 2) res = "none";
+        return res;
+    }
 }
