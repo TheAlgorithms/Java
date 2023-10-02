9814dc3d627fd1b3dc69637a96caab87e893704d
ANKIT SAHA <85545712+Ankit-Saha08@users.noreply.github.com>
2023-10-02 20:54:37 +0530

    made few fixes in syntax LongestPalindromicSubstring.java

--- src/main/java/com/thealgorithms/strings/LongestPalindromicSubstring.java
+++ src/main/java/com/thealgorithms/strings/LongestPalindromicSubstring.java
@@ -1,7 +1,7 @@
 package com.thealgorithms.strings;
 
 // Longest Palindromic Substring
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -28,18 +28,18 @@ class Solution {
     }
 
     public String longestPalindrome(String str) {
-    // brute-force approach using Java Collections 
-    ArrayList<String> al = new ArrayList<>();
-    for (int i = 0; i < str.length() ; i++) {
-        for (int j = i + 1; j < str.length() - 1; j++) {
-            String sub = str.substring(i, j);
-            boolean flag = Palindrome(sub);
-            if (flag == true) al.add(sub);
-        }
-    }
-    Collections.sort(al);
-    String res = al.get(al.size() - 1);
-    if (res.length() < 2) res = "none";
-    return res;
-    }
+        // brute-force approach using Java Collections
+        ArrayList<String> al = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length() - 1; j++) {
+                String sub = str.substring(i, j);
+                boolean flag = Palindrome(sub);
+                if (flag == true) al.add(sub);
+            }
+         }
+        Collections.sort(al);
+        String res = al.get(al.size() - 1);
+        if (res.length() < 2) res = "none";
+        return res;
+     }
 }
