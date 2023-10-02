76a736758619d62e4faf342963727ea51a83fc95
mouss3abbb <mouss3abbb@gmail.com>
2023-10-02 13:11:03 +0300

    Fixed clang formatting

--- src/test/java/com/thealgorithms/misc/FourSumProblemTest.java
+++ src/test/java/com/thealgorithms/misc/FourSumProblemTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 /**
