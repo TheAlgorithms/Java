f8cf31796bf5665ebff08ef8b48964ad619a9e98
Anup Omkar <anup_omkar@intuit.com>
2023-10-02 22:42:17 +0530

    feat: adding matrix rank algorithm

---
+++ src/main/java/com/thealgorithms/maths/MatrixRank.java
@@ -0,0 +1,62 @@
+package com.thealgorithms.maths;
+
+/**
+ *
+ *
+ * <h1>Rank of Matrix</h1>
+ *
+ * <p>
+ * The rank of a matrix is the largest number of linearly independent rows/columns of the matrix.
+ * The rank is not only defined for square matrices.
+ * </p>
+ *
+ * <p>
+ * The rank of a matrix can also be defined as the largest order of any non-zero minor in the matrix.
+ * </p>
+ *
+ * <p> Read more about rank of matrix <a href="https://en.wikipedia.org/wiki/Rank_(linear_algebra)">here</a></p>
+ *
+ * @author Anup Omkar
+ */
+public class MatrixRank {
+
+    // Small value to handle precision issues with floating point numbers
+    private final static double EPSILON = 1e-10;
+
+    /**
+     * Returns rank of matrix
+     *
+     * @param matrix matrix whose rank is to be found
+     * @return rank of matrix
+     */
+    public static int computeRank(double[][] matrix) {
+        int numRows = matrix.length;
+        int numColumns = matrix[0].length;
+        int rank = 0;
+
+        boolean[] rowMarked = new boolean[numRows];
+        for(int colIndex = 0; colIndex < numColumns; ++colIndex) {
+            int pivotRow;
+            for(pivotRow = 0; pivotRow < numRows; ++pivotRow) {
+                if(!rowMarked[pivotRow] && Math.abs(matrix[pivotRow][colIndex]) > EPSILON) {
+                    break;
+                }
+            }
+            if(pivotRow != numRows) {
+                ++rank;
+                rowMarked[pivotRow] = true;
+                for(int nextCol = colIndex + 1; nextCol < numColumns; ++nextCol) {
+                    matrix[pivotRow][nextCol] /= matrix[pivotRow][colIndex];
+                }
+                for(int otherRow = 0; otherRow < numRows; ++otherRow) {
+                    if(otherRow != pivotRow && Math.abs(matrix[otherRow][colIndex]) > EPSILON) {
+                        for(int col2 = colIndex + 1; col2 < numColumns; ++col2) {
+                            matrix[otherRow][col2] -= matrix[pivotRow][col2] * matrix[otherRow][colIndex];
+                        }
+                    }
+                }
+            }
+        }
+        return rank;
+    }
+}
---
+++ src/test/java/com/thealgorithms/maths/MatrixRankTest.java
@@ -0,0 +1,35 @@
+package com.thealgorithms.maths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class MatrixRankTest {
+
+    @Test
+    void computeRank() {
+
+        double[][] identityMatrix = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+        assertEquals(3, MatrixRank.computeRank(identityMatrix));
+
+        double[][] zeroMatrix = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        assertEquals(0, MatrixRank.computeRank(zeroMatrix));
+
+        double[][] square1x1Matrix = {{1}};
+        assertEquals(1, MatrixRank.computeRank(square1x1Matrix));
+
+        double[][] square2x2Matrix = {{1, 2}, {3, 4}};
+        assertEquals(2, MatrixRank.computeRank(square2x2Matrix));
+
+        double[][] square3x3Matrix = {{3, -1, 2}, {-3, 1, 2}, {-6, 2, 4}};
+        assertEquals(2, MatrixRank.computeRank(square3x3Matrix));
+
+        double[][] square4x4Matrix = {{2, 3, 0, 1}, {1, 0, 1, 2}, {-1, 1, 1, -2}, {1, 5, 3, -1}};
+        assertEquals(3, MatrixRank.computeRank(square4x4Matrix));
+
+        double[][] rectangular2X3Matrix = {{1, 2, 3}, {3, 6, 9}};
+        assertEquals(1, MatrixRank.computeRank(rectangular2X3Matrix));
+
+        double[][] rectangular3X4Matrix = {{0.25, 0.5, 0.75, 2}, {1.5, 3, 4.5, 6}, {1, 2, 3, 4}};
+        assertEquals(2, MatrixRank.computeRank(rectangular3X4Matrix));
+    }
+}
