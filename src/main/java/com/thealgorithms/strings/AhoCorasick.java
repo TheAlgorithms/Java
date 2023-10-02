6a75149c91bb27c467150513306591ab5eef7f91
blaze <prabhatkumarverma42@gmail.com>
2023-10-02 18:17:35 +0530

    Change all class fields to private, added initializeSuffixLinksForChildNodesOfTheRoot() method, hashmap string search position (also has previous index based search), removed java.util.*

--- src/main/java/com/thealgorithms/strings/AhoCorasick.java
+++ src/main/java/com/thealgorithms/strings/AhoCorasick.java
@@ -1,6 +1,10 @@
 package com.thealgorithms.strings;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 
 /**
  * Aho-Corasick String Matching Algorithm Implementation
@@ -16,27 +20,66 @@ public class AhoCorasick {
 
     // Trie Node Class
     class Node {
-        HashMap<Character, Node> child = new HashMap<>();
-        Node suffix_link;
-        Node output_link;
-        int pattern_ind;
+        private HashMap<Character, Node> child = new HashMap<>();
+        private Node suffixLink;
+        private Node outputLink;
+        private int patternInd;
 
         Node() {
-            this.suffix_link = null;
-            this.output_link = null;
-            this.pattern_ind = -1;
+            this.suffixLink = null;
+            this.outputLink = null;
+            this.patternInd = -1;
+        }
+
+        public HashMap<Character, Node> getChild() {
+            return child;
+        }
+
+        public Node getSuffixLink() {
+            return suffixLink;
+        }
+
+        public void setSuffixLink(Node suffixLink) {
+            this.suffixLink = suffixLink;
+        }
+
+        public Node getOutputLink() {
+            return outputLink;
+        }
+
+        public void setOutputLink(Node outputLink) {
+            this.outputLink = outputLink;
+        }
+
+        public int getPatternInd() {
+            return patternInd;
+        }
+
+        public void setPatternInd(int patternInd) {
+            this.patternInd = patternInd;
         }
     }
 
-    Node root = null; // The root node of the Aho-Corasick trie
+    private Node root = null; // The root node of the Aho-Corasick trie
     private ArrayList<ArrayList<Integer>> res; // Stores the positions where patterns are found in the text
     private String[] patterns; // patterns according to which Trie is constructed
+    private Queue<Node> q; // Used for BFS traversal
+
+    private Map<String, ArrayList<Integer>> positionByString; // store positions by string pattern
+
+    // Constructor initializes the positionByString map
+    public AhoCorasick() {
+        positionByString = new HashMap<>();
+    }
 
     // Clears the Aho-Corasick data structures
     public void clear() {
-        root = null;
+        this.root = null;
         if (res != null) {
-            res.clear();
+            this.res.clear();
+        }
+        if (positionByString != null) {
+            this.positionByString.clear(); // Clear the positionByString map
         }
     }
 
@@ -56,58 +99,64 @@ public class AhoCorasick {
                 char c = patterns[i].charAt(j); // Get the current character
 
                 // Check if the current node has a child for the current character
-                if (curr.child.containsKey(c)) {
-                    curr = curr.child.get(c); // Update the current node to the child node
+                if (curr.getChild().containsKey(c)) {
+                    curr = curr.getChild().get(c); // Update the current node to the child node
                 } else {
                     // If no child node exists, create a new one and add it to the current node's children
                     Node nn = new Node();
-                    curr.child.put(c, nn);
+                    curr.getChild().put(c, nn);
                     curr = nn; // Update the current node to the new child node
                 }
             }
-            curr.pattern_ind = i; // Store the index of the pattern in the current leaf node
+            curr.setPatternInd(i); // Store the index of the pattern in the current leaf node
+        }
+    }
+
+    // Initialize suffix links for child nodes of the root
+    private void initializeSuffixLinksForChildNodesOfTheRoot() {
+        for (char rc : root.getChild().keySet()) {
+            Node childNode = root.getChild().get(rc);
+            q.add(childNode); // Add child node to the queue
+            childNode.setSuffixLink(root); // Set suffix link to the root
         }
     }
 
     // Builds the suffix links and output links in the Aho-Corasick trie
     public void buildSuffixAndOutputLinks() {
-        root.suffix_link = root; // Initialize the suffix link of the root to itself
-        Queue<Node> q = new LinkedList<>(); // Initialize a queue for BFS traversal
+        root.setSuffixLink(root); // Initialize the suffix link of the root to itself
+        q = new LinkedList<>(); // Initialize a queue for BFS traversal
 
         // Initialize suffix links for child nodes of the root
-        for (char rc : root.child.keySet()) {
-            q.add(root.child.get(rc)); // Add child node to the queue
-            root.child.get(rc).suffix_link = root; // Set suffix link to the root
-        }
+        initializeSuffixLinksForChildNodesOfTheRoot();
 
         while (!q.isEmpty()) {
             Node currentState = q.poll(); // Get the current node for processing
 
             // Iterate through child nodes of the current node
-            for (char cc : currentState.child.keySet()) {
-                Node currentChild = currentState.child.get(cc); // Get the child node
-                Node parentSuffix = currentState.suffix_link; // Get the parent's suffix link
+            for (char cc : currentState.getChild().keySet()) {
+                Node currentChild = currentState.getChild().get(cc); // Get the child node
+                Node parentSuffix = currentState.getSuffixLink(); // Get the parent's suffix link
 
                 // Calculate the suffix link for the child based on parent's suffix link
-                while (!parentSuffix.child.containsKey(cc) && parentSuffix != root) {
-                    parentSuffix = parentSuffix.suffix_link;
+                while (!parentSuffix.getChild().containsKey(cc) && parentSuffix != root) {
+                    parentSuffix = parentSuffix.getSuffixLink();
                 }
 
                 // Set the calculated suffix link or default to root
-                if (parentSuffix.child.containsKey(cc)) {
-                    currentChild.suffix_link = parentSuffix.child.get(cc);
+                if (parentSuffix.getChild().containsKey(cc)) {
+                    currentChild.setSuffixLink(parentSuffix.getChild().get(cc));
                 } else {
-                    currentChild.suffix_link = root;
+                    currentChild.setSuffixLink(root);
                 }
 
                 q.add(currentChild); // Add the child node to the queue for further processing
             }
 
             // Establish output links for nodes to efficiently identify patterns within patterns
-            if (currentState.suffix_link.pattern_ind >= 0) {
-                currentState.output_link = currentState.suffix_link;
+            if (currentState.getSuffixLink().getPatternInd() >= 0) {
+                currentState.setOutputLink(currentState.getSuffixLink());
             } else {
-                currentState.output_link = currentState.suffix_link.output_link;
+                currentState.setOutputLink(currentState.getSuffixLink().getOutputLink());
             }
         }
     }
@@ -119,26 +168,26 @@ public class AhoCorasick {
             char ch = text.charAt(i); // Get the current character in the text
 
             // Check if the current node has a child for the current character
-            if (parent.child.containsKey(ch)) {
-                parent = parent.child.get(ch); // Update the current node to the child node
+            if (parent.getChild().containsKey(ch)) {
+                parent = parent.getChild().get(ch); // Update the current node to the child node
 
                 // If the current node represents a pattern, record its position in res
-                if (parent.pattern_ind > -1) {
-                    res.get(parent.pattern_ind).add(i);
+                if (parent.getPatternInd() > -1) {
+                    res.get(parent.getPatternInd()).add(i);
                 }
 
-                Node output_link = parent.output_link;
+                Node outputLink = parent.getOutputLink();
                 // Follow output links to find and record positions of other patterns
-                while (output_link != null) {
-                    res.get(output_link.pattern_ind).add(i);
-                    output_link = output_link.output_link;
+                while (outputLink != null) {
+                    res.get(outputLink.getPatternInd()).add(i);
+                    outputLink = outputLink.getOutputLink();
                 }
             } else {
                 // If no child node exists for the character, backtrack using suffix links
-                while (parent != root && !parent.child.containsKey(ch)) {
-                    parent = parent.suffix_link;
+                while (parent != root && !parent.getChild().containsKey(ch)) {
+                    parent = parent.getSuffixLink();
                 }
-                if (parent.child.containsKey(ch)) {
+                if (parent.getChild().containsKey(ch)) {
                     i--; // Decrement i to reprocess the same character
                 }
             }
@@ -147,17 +196,18 @@ public class AhoCorasick {
     }
 
     /*
-     * returns the list of list containing the indexes of words in patterns/dictonary
-     * list index represet word.
-     * eg - list[0] cointains the list of start-index of word pattern[0]
-     *      list[1] cointains the list of start-index of word pattern[1]
+     * returns the list of list containing the indexes of words in patterns/dictionary
+     * list index represents word.
+     * eg - list[0] contains the list of start-index of word pattern[0]
+     *      list[1] contains the list of start-index of word pattern[1]
      *      ......
-     *      ......]
-     *      list[n] cointains the list of start-index of word pattern[n]
+     *      ......
+     *      list[n] contains the list of start-index of word pattern[n]
      */
     public ArrayList<ArrayList<Integer>> getWordsIndexList() {
         return res;
     }
+
     // Returns the count of occurrences of each pattern in the text
     public ArrayList<Integer> getRepeatCountOfWords() {
         ArrayList<Integer> countOfWords = new ArrayList<>();
@@ -166,7 +216,8 @@ public class AhoCorasick {
         }
         return countOfWords;
     }
-    // by default res contains end-points. This function convert those
+
+    // by default res contains end-points. This function converts those
     // endpoints to start points
     public void setUpStartPoints() {
         for (int i = 0; i < patterns.length; i++) {
@@ -177,5 +228,24 @@ public class AhoCorasick {
                 }
             }
         }
+    }
+
+    // return root
+    public Node getRoot() {
+        return root;
+    }
+
+    // Create a Map mapping patterns to positions
+    public void createPositionByStringMap() {
+        for (int i = 0; i < patterns.length; i++) {
+            String pattern = patterns[i];
+            ArrayList<Integer> positions = res.get(i);
+            positionByString.put(pattern, new ArrayList<>(positions));
+        }
+    }
+
+    // Get the positionByString map
+    public Map<String, ArrayList<Integer>> getPositionByString() {
+        return positionByString;
     }
 }
