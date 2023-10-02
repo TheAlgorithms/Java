27d8d4a2ef08428c5beaa9eb51b95dede10ee587
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2023-10-02 14:24:20 +0530

    final code push with linting changes

--- src/main/java/com/thealgorithms/bitmanipulation/CountNumberOfSetBits.java
+++ src/main/java/com/thealgorithms/bitmanipulation/CountNumberOfSetBits.java
@@ -1,14 +1,14 @@
 package com.thealgorithms.bitmanipulation;
 
 /**
- * Find the total number of set bit in a number.
+ * Algorithm to find the total number of set bits in a number.
  * @author Dhriendra Pratap Singh aliad (hanisntsolo) (https://github.com/hanisntsolo)
  */
 public class CountNumberOfSetBits {
-  public static int countNumberOfSetBits(int n) {
-    if(n == 0) {
-      return 0;
+    public static int countNumberOfSetBits(int n) {
+        if(n == 0) {
+            return 0;
+        }
+        return (n & 1) + countNumberOfSetBits(n >> 1);
     }
-    return (n & 1) + countNumberOfSetBits(n >> 1);
-  }
 }
--- src/test/java/com/thealgorithms/bitmanipulation/CountNumberOfSetBitsTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/CountNumberOfSetBitsTest.java
@@ -5,11 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Test for finding the total number of set bits in a number.
+ * @author Dhriendra Pratap Singh aliad (hanisntsolo) (https://github.com/hanisntsolo)
+ */
 public class CountNumberOfSetBitsTest {
 
   @Test
-  public void testCountNumberOfSetBits() {
-      assertEquals(4, CountNumberOfSetBits.countNumberOfSetBits(15));
-      assertNotEquals(1, CountNumberOfSetBits.countNumberOfSetBits(3));
-  }
+    public void testCountNumberOfSetBits() {
+        assertEquals(4, CountNumberOfSetBits.countNumberOfSetBits(15));
+        assertNotEquals(1, CountNumberOfSetBits.countNumberOfSetBits(3));
+    }
 }
