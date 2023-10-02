c61bb8b1b8f932e3fa7f4e39ffe3204a57f5a192
mouss3abbb <mouss3abbb@gmail.com>
2023-10-02 13:04:27 +0300

    Extracted methods with meaningful names

--- src/main/java/com/thealgorithms/misc/FourSumProblem.java
+++ src/main/java/com/thealgorithms/misc/FourSumProblem.java
@@ -1,21 +1,52 @@
 package com.thealgorithms.misc;
+
 import java.util.*;
 
 /**
  * Solution for The Four Sum Problem.
+ *
  * @author mouss3abbb
  */
-public class FourSumProblem {
+public final class FourSumProblem {
 
     /*
     In the hashing solution, we maintain a hashmap of the sum of each pair of elements in the array.
     We then iterate over each pair of elements in the array and check if the required sum to be equal to target is found in the map previously.
     The map now acts as a frequency table. However, we also need to check that the indices of all four elements are distinct, that's why we maintain a pair of indices in the map itself and compare it with the pair of elements we are at right now.
     */
-    public static List<List<Integer>> Hashing(ArrayList<Integer> arr, int target) {
+    public static List<List<Integer>> hashing(final ArrayList<Integer> arr, final int target) {
         Set<List<Integer>> set = new HashSet<>();
         HashMap<Integer, List<Integer>> map = new HashMap<>();
+        // Find the sum of each pair of elements and store it in the map
+        findAllPairSum(arr, map);
+        // We iterate over each pair of elements and check if their sum added to any value in the map will equal the target
+        for (int i = 0; i < arr.size(); i++) {
+            for (int j = i + 1; j < arr.size(); j++) {
+                checkRemainingSumAvailability(arr, target, i, j, map, set);
+            }
+        }
+        return new ArrayList<>(set);
+    }
 
+    private static void checkRemainingSumAvailability(ArrayList<Integer> arr, int target, int i, int j, HashMap<Integer, List<Integer>> map, Set<List<Integer>> set) {
+        int remainingSum = target - (arr.get(i) + arr.get(j));
+        // We need to check is we have taken any of these indices before to avoid taking the same index twice.
+        if (isThisIndexTaken(i, j, map, remainingSum)) {
+            List<Integer> sum = new ArrayList<>();
+            sum.add(arr.get(i));
+            sum.add(arr.get(j));
+            sum.add(arr.get(map.get(remainingSum).get(0)));
+            sum.add(arr.get(map.get(remainingSum).get(1)));
+            Collections.sort(sum);
+            set.add(sum);
+        }
+    }
+
+    private static boolean isThisIndexTaken(int i, int j, HashMap<Integer, List<Integer>> map, int remainingSum) {
+        return map.containsKey(remainingSum) && map.get(remainingSum).get(0) != j && map.get(remainingSum).get(1) != j && map.get(remainingSum).get(0) != i && map.get(remainingSum).get(1) != i;
+    }
+
+    private static void findAllPairSum(ArrayList<Integer> arr, HashMap<Integer, List<Integer>> map) {
         for (int i = 0; i < arr.size(); i++) {
             for (int j = i + 1; j < arr.size(); j++) {
                 List<Integer> idx = new ArrayList<>();
@@ -24,21 +55,5 @@ public class FourSumProblem {
                 map.put(arr.get(i) + arr.get(j), idx);
             }
         }
-
-        for (int i = 0; i < arr.size(); i++) {
-            for (int j = i + 1; j < arr.size(); j++) {
-                int t = target - (arr.get(i) + arr.get(j));
-                if (map.containsKey(t) && map.get(t).get(0) != j && map.get(t).get(1) != j && map.get(t).get(0) != i && map.get(t).get(1) != i) {
-                    List<Integer> sum = new ArrayList<>();
-                    sum.add(arr.get(i));
-                    sum.add(arr.get(j));
-                    sum.add(arr.get(map.get(t).get(0)));
-                    sum.add(arr.get(map.get(t).get(1)));
-                    Collections.sort(sum);
-                    set.add(sum);
-                }
-            }
-        }
-        return new ArrayList<>(set);
     }
 }
--- src/test/java/com/thealgorithms/misc/FourSumProblemTest.java
+++ src/test/java/com/thealgorithms/misc/FourSumProblemTest.java
@@ -1,16 +1,17 @@
 package com.thealgorithms.misc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import org.apache.commons.lang3.tuple.Pair;
+
 import org.junit.jupiter.api.Test;
 
 /**
  * Test cases for The Four Sum Problem.
+ *
  * @author mouss3abbb
  */
 
@@ -21,7 +22,7 @@ public class FourSumProblemTest {
         final var values = new ArrayList<>(List.of(-2, 5, 0, 6));
         Collections.sort(values);
         final int target = 9;
-        final var result = FourSumProblem.Hashing(values, target).get(0);
+        final var result = FourSumProblem.hashing(values, target).get(0);
         Collections.sort(result);
         assertEquals(values, result);
     }
@@ -30,30 +31,22 @@ public class FourSumProblemTest {
     void testFourSumNoSolution() {
         final var values = new ArrayList<>(List.of(-2, 5, 0, 6));
         final int target = 3;
-        assertTrue(FourSumProblem.Hashing(values, target).isEmpty());
+        assertTrue(FourSumProblem.hashing(values, target).isEmpty());
     }
 
     @Test
-    void testTwoSumMultipleSolutions() {
+    void testFourSumMultipleSolutions() {
         final var values = new ArrayList<>(List.of(-2, 5, 0, 6, 5, 5, 0, -1));
         final var solution1 = new ArrayList<>(List.of(-2, 5, 0, 6));
         final var solution2 = new ArrayList<>(List.of(5, 5, 0, -1));
         Collections.sort(solution1);
         Collections.sort(solution2);
         final int target = 9;
-        final var result1 = FourSumProblem.Hashing(values, target).get(0);
+        final var result1 = FourSumProblem.hashing(values, target).get(0);
         Collections.sort(result1);
-        final var result2 = FourSumProblem.Hashing(values, target).get(1);
+        final var result2 = FourSumProblem.hashing(values, target).get(1);
         Collections.sort(result2);
         assertEquals(solution1, result1);
         assertEquals(solution2, result2);
-    }
-
-    @Test
-    void testTwoSumMultipleSolution() {
-        final int[] values = {3, 4, 3, 3};
-        final int target = 6;
-        final var expected = Pair.of(0, 2); // values[0] + values[2] = 3 + 3 = 6
-        assertEquals(expected, TwoSumProblem.twoSum(values, target).get());
     }
 }
