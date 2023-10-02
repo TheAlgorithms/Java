f06d0d0108213707badc26429068ea0d02d30316
Aman <84791435+Aman28801@users.noreply.github.com>
2023-10-03 02:15:12 +0530

    Linting Issue

--- src/test/java/com/thealgorithms/misc/MirrorOfMatrixTest.java
+++ src/test/java/com/thealgorithms/misc/MirrorOfMatrixTest.java
@@ -1,7 +1,8 @@
 package com.thealgorithms.misc;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class MirrorOfMatrixTest {
 
