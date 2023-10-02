7d88379a10ca830eae908248b931cb676b6275ef
Forest Defenders Club NFTs <bvkaura@gmail.com>
2023-10-02 16:53:35 +0530

    added Abstract Factory Design pattern algorithm in java

---
+++ src/main/java/com/thealgorithms/abstractFactoryPattern/IBgClass.java
@@ -0,0 +1,5 @@
+package abstractFactoryPattern;
+
+public class IBgClass {
+
+}
---
+++ src/main/java/com/thealgorithms/abstractFactoryPattern/mainClass.java
@@ -0,0 +1,18 @@
+package abstractFactoryPattern;
+import java.util.*;
+import java.util.concurrent.TimeUnit;  
+
+class mainClass {
+    public static void main(String[] args) {
+        mainClass clazz = new mainClass();
+        clazz.mainWork();
+        
+    }
+    
+    private void mainWork() {
+        // create 2 theme factories Dark and Light themes that generate UI components of themes respectively
+        // The components should be relative like white text on dark theme and vice versa.
+    }
+    
+    
+    }
