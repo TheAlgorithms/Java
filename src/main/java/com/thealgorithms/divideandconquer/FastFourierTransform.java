79630197bb9549697e267728d85683ab26574e54
Sheetal Neeraj <42382485+sheetalneeraj@users.noreply.github.com>
2023-10-03 21:23:31 +0530

    Update FastFourierTransform.java

--- src/main/java/com/thealgorithms/divideandconquer/FastFourierTransform.java
+++ src/main/java/com/thealgorithms/divideandconquer/FastFourierTransform.java
@@ -8,32 +8,37 @@ package com.thealgorithms.divideandconquer;
 // Reference: https://en.wikipedia.org/wiki/Fast_Fourier_transform
 
 
-public class FastFourierTransform {
+public static double[][] FastFourierTransform(double[][] x) {
+    int n = x.length;
 
-    public static Complex[] fft(Complex[] x) {
-        int n = x.length;
-
-        // Base case
-        if (n == 1) {
-            return x;
-        }
-
-        // Recursively compute the FFT of the even and odd terms
-        Complex[] even = new Complex[n / 2];
-        Complex[] odd = new Complex[n / 2];
-        for (int i = 0; i < n / 2; i++) {
-            even[i] = x[2 * i];
-            odd[i] = x[2 * i + 1];
-        }
-        Complex[] evenFFT = fft(even);
-        Complex[] oddFFT = fft(odd);
-
-        // Combine the results to compute the FFT of the original sequence
-        Complex[] result = new Complex[n];
-        for (int k = 0; k < n / 2; k++) {
-            result[k] = evenFFT[k].add(oddFFT[k].multiply(Complex.polar(1.0, -2.0 * Math.PI * k / n)));
-            result[k + n / 2] = evenFFT[k].subtract(oddFFT[k].multiply(Complex.polar(1.0, -2.0 * Math.PI * k / n)));
-        }
-        return result;
+    // Base case
+    if (n == 1) {
+        return x;
     }
+
+    // Recursively compute the FFT of the even and odd terms
+    double[][] even = new double[n / 2][2];
+    double[][] odd = new double[n / 2][2];
+    for (int i = 0; i < n / 2; i++) {
+        even[i][0] = x[2 * i][0];
+        even[i][1] = x[2 * i][1];
+        odd[i][0] = x[2 * i + 1][0];
+        odd[i][1] = x[2 * i + 1][1];
+    }
+    double[][] evenFFT = FastFourierTransform(even);
+    double[][] oddFFT = FastFourierTransform(odd);
+
+    // Combine the results to compute the FFT of the original sequence
+    double[][] result = new double[n][2];
+    for (int k = 0; k < n / 2; k++) {
+        double angle = -2.0 * Math.PI * k / n;
+        double cos = Math.cos(angle);
+        double sin = Math.sin(angle);
+
+        result[k][0] = evenFFT[k][0] + oddFFT[k][0] * cos - oddFFT[k][1] * sin;
+        result[k][1] = evenFFT[k][1] + oddFFT[k][0] * sin + oddFFT[k][1] * cos;
+        result[k + n / 2][0] = evenFFT[k][0] - oddFFT[k][0] * cos + oddFFT[k][1] * sin;
+        result[k + n / 2][1] = evenFFT[k][1] - oddFFT[k][0] * sin - oddFFT[k][1] * cos;
+    }
+    return result;
 }
