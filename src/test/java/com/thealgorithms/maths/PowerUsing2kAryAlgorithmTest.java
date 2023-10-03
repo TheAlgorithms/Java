a72c2ab79ab85d24a26e4136cb2b8800daec8ce2
RazatAggarwal <razat.aggarwal@outlook.com>
2023-10-03 17:00:28 +0530

    Added power using 2kary method java and test file.

---
+++ src/test/java/com/thealgorithms/maths/PowerUsing2kAryAlgorithmTest.java
@@ -0,0 +1,27 @@
+package com.thealgorithms.maths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+/**
+ * Test case for Power using 2k-ary algorithm
+ * @author Razat Aggarwal (https://github.com/razat-thapar)
+ */
+class PowerUsing2kAryAlgorithmTest {
+
+    @Test
+    void testPowerUsing2kAryAlgorithmTest() {
+        assertEquals(32L, PowerUsing2kAryAlgorithm.pow(2, 5, 1000000007));
+        assertEquals(754573817L, PowerUsing2kAryAlgorithm.pow(5, 100000, 1000000007));
+        assertEquals(1L, PowerUsing2kAryAlgorithm.pow(5, 0, 1000000007));
+    }
+
+    @Test
+    void testPowerUsing2kAryAlgorithmTestForWrongInputs() {
+        Exception exception1 = assertThrows(IllegalArgumentException.class, () -> PowerUsing2kAryAlgorithm.pow(5, -2, 1000000007));
+        assertEquals("exponent and mod can't be negative!", exception1.getMessage());
+
+        Exception exception2 = assertThrows(IllegalArgumentException.class, () -> PowerUsing2kAryAlgorithm.pow(5, 2, -1000000007));
+        assertEquals("exponent and mod can't be negative!", exception2.getMessage());
+    }
+}
