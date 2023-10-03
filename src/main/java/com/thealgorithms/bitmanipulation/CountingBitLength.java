ec67731bd6e9336ec79eac8de1158a8f987db7bb
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-10-03 23:43:08 +0200

    Add files via upload

---
+++ src/main/java/com/thealgorithms/bitmanipulation/CountingBitLength.java
@@ -0,0 +1,24 @@
+package com.thealgorithms.bitmanipulation;
+
+/*
+ * Author: lukasb1b (https://github.com/lukasb1b)
+ */
+
+public class CountingBitLength {
+    private CountingBitLength(){
+
+    }
+
+    /*
+     * Counts the number of bits from num
+     */
+    public static int countingBitLength(int num) {
+        int result = 0;
+
+        while (!(num == 0 || num == -1)) {
+            result++;
+            num = num>>1;
+        }
+        return result;
+    }
+}
