7ff6f01cc12e448de73ff297e8bb3e687ac06cb8
Vineetttt <vineetchotaliya30@gmail.com>
2023-10-03 13:38:13 +0530

    linter job fixes

--- src/main/java/com/thealgorithms/greedyalgorithms/ActivitySelection.java
+++ src/main/java/com/thealgorithms/greedyalgorithms/ActivitySelection.java
@@ -8,8 +8,8 @@ import java.util.Comparator;
 
 public class ActivitySelection {
     // Function to perform activity selection
-    public static ArrayList<Integer> activitySelection(int start[], int end[]) {
-        int n = start.length;
+    public static ArrayList<Integer> activitySelection(int startTimes[], int endTimes[]) {
+        int n = startTimes.length;
         int activities[][] = new int[n][3];
 
         // Create a 2D array to store activities and their start/end times.
@@ -17,24 +17,25 @@ public class ActivitySelection {
 
         for (int i = 0; i < n; i++) {
             activities[i][0] = i; // Assign activity index
-            activities[i][1] = start[i]; // Assign start time
-            activities[i][2] = end[i]; // Assign end time
+            activities[i][1] = startTimes[i]; // Assign start time
+            activities[i][2] = endTimes[i]; // Assign end time
         }
 
         // Sort activities by their end times in ascending order.
-        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
-        int lastEnd;         
-        ArrayList<Integer> ans = new ArrayList<>();
-        ans.add(activities[0][0]);
-        lastEnd = activities[0][2]; 
+        Arrays.sort(activities, Comparator.comparingDouble(activity -> activity[2]));
+        int lastEndTime;
+        ArrayList<Integer> selectedActivities = new ArrayList<>();
+        selectedActivities.add(activities[0][0]);
+        lastEndTime = activities[0][2];
 
         // Iterate through sorted activities to select compatible ones.
         for (int i = 1; i < n; i++) {
-            if (activities[i][1] >= lastEnd) {                
-                ans.add(activities[i][0]); 
-                lastEnd = activities[i][2]; 
+            if (activities[i][1] >= lastEndTime) {
+                selectedActivities.add(activities[i][0]);
+                lastEndTime = activities[i][2];
             }
         }
-        return ans;
+        return selectedActivities;
     }
 }
+
