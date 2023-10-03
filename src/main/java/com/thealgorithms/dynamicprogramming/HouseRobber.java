2d88a8cf499f609bbe67aca516999dc98a8d6b3e
Md Faizan Khan <FKHAN20040@GMAIL.COM>
2023-10-04 01:26:09 +0530

    Adding House Robber Problem in DynamicProgramming #4508

--- src/main/java/com/thealgorithms/dynamicprogramming/HouseRobber.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/HouseRobber.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.dynamicprogramming;
 
-/*  Problem link: https://leetcode.com/problems/house-robber/description/
+/*  Wikipedia link: https://en.wikipedia.org/wiki/Dynamic_programming
 
     Problem Statement:
     You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
