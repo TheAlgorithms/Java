b31d21dd4b59ff9c36bc3885503c380bd86f5ac9
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-10-03 23:41:35 +0200

    Add files via upload

---
+++ src/test/java/com/thealgorithms/bitmanipulation/CountingBitLengthTest.java
@@ -0,0 +1,22 @@
+package com.thealgorithms.bitmanipulation;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class CountingBitLengthTest {
+    static Stream<Object[]> inputStream() {
+        return Stream.of(
+                new Object[]{3, 5},
+                new Object[]{0, 0},
+                new Object[]{2, -3}
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputStream")
+    void bitLengthTest(int expected, int num) {
+        Assertions.assertEquals(expected, CountingBitLength.countingBitLength(num));
+    }
+}
