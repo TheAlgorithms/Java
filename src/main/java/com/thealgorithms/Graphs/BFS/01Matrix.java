b00fbee783d8b11da20da75a6954bf1f191691ab
VadirajKranam <vadirajraokaranam@gmail.com>
2023-10-04 22:34:18 +0530

    added graph problems and their solution with problem link

---
+++ src/main/java/com/thealgorithms/Graphs/BFS/01Matrix.java
@@ -0,0 +1,73 @@
+class Trio{
+    int r,c,d;
+    Trio(int r,int c,int d)
+    {
+        this.r=r;
+        this.c=c;
+        this.d=d;
+    }
+}
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int[][] ans=new int[mat.length][mat[0].length];
+        for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat[i].length;j++)
+            {
+                if(mat[i][j]==1)
+                {
+                    ans[i][j]=function(i,j,mat);
+                }
+                else
+                {
+                    ans[i][j]=0;
+                }
+            }
+        }
+        return ans;
+    }
+    public int function(int row,int col,int[][] mat)
+    {
+        Queue<Trio> q=new LinkedList<>();
+        int[][] vis=new int[mat.length][mat[0].length];
+        for(int[] v:vis)
+        {
+            Arrays.fill(v,-1);
+        }
+        q.offer(new Trio(row,col,0));
+        int minDis=Integer.MAX_VALUE;
+        while(!q.isEmpty())
+        {
+            int cd=q.peek().d;
+            int r=q.peek().r;
+            int c=q.peek().c;
+            vis[r][c]=1;
+            q.poll();
+            if(mat[r][c]==0)
+            {
+                return cd;
+            }
+            else
+            {
+                if(r-1>=0 && vis[r-1][c]==-1)
+                {
+                    q.offer(new Trio(r-1,c,cd+1));
+                }
+                if(r+1<mat.length && vis[r+1][c]==-1)
+                {
+                   q.offer(new Trio(r+1,c,cd+1)); 
+                }
+                if(c-1>=0 && vis[r][c-1]==-1)
+                {
+                    q.offer(new Trio(r,c-1,cd+1));
+                }
+                if(c+1<mat[0].length && vis[r][c+1]==-1)
+                {
+                    q.offer(new Trio(r,c+1,cd+1));
+                }
+            }
+        }
+        return 0;
+    }
+}
+//leetcode problem link : https://leetcode.com/problems/01-matrix/
---
+++ src/main/java/com/thealgorithms/Graphs/BFS/RottenOranges.java
@@ -0,0 +1,67 @@
+class Pair{
+      int row,col,tm;
+      Pair(int row,int col,int tm)
+      {
+          this.row=row;
+          this.col=col;
+          this.tm=tm;
+      }
+}
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int[][] vis=new int[grid.length][grid[0].length];
+        int cntFresh=0;
+         Queue<Pair> q=new LinkedList<>();
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[i].length;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.offer(new Pair(i,j,0));
+                    vis[i][j]=2;
+                }
+                else
+                {
+                    vis[i][j]=0;
+                }
+                 if(grid[i][j]==1)
+                {
+                    cntFresh++;
+                }
+            }
+        }
+        int cnt=0;
+        int t=0;
+        int[] dr={-1,0,+1,0};
+        int[] dc={0,+1,0,-1};
+        while(!q.isEmpty())
+        {
+            int r=q.peek().row;
+            int c=q.peek().col;
+            int tm=q.peek().tm;
+            q.poll();
+            if(t!=tm)
+            {
+                t=tm;
+            }
+            for(int i=0;i<4;i++)
+            {
+                int nrow=r+dr[i];
+                int ncol=c+dc[i];
+                if(nrow>=0 && nrow<grid.length && ncol>=0 && ncol<grid[0].length && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1)
+                {
+                    vis[nrow][ncol]=2;
+                    q.offer(new Pair(nrow,ncol,t+1));
+                    cnt++;
+                }
+            }
+        }
+        if(cnt!=cntFresh)
+        {
+            return -1;
+        }
+        return t;
+    }
+}
+//leetcode problem link : https://leetcode.com/problems/rotting-oranges/
