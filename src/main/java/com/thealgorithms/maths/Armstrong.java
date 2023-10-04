b35d94921a5ff914e521632f1d107ddea1e95efa
satyabarghav <satyabarghav1@gmail.com>
2023-10-04 10:47:00 +0530

    Made changes to the code to correct the Logic of Armstrong Number

--- src/main/java/com/thealgorithms/maths/Armstrong.java
+++ src/main/java/com/thealgorithms/maths/Armstrong.java
@@ -1,11 +1,12 @@
 package com.thealgorithms.maths;
 
 /**
- * An Armstrong number is equal to the sum of the cubes of its digits. For
- * example, 370 is an Armstrong number because 3*3*3 + 7*7*7 + 0*0*0 = 370. An
- * Armstrong number is often called Narcissistic number.
+ It is a number that is equal to the sum of its own digits, each raised to the power of the number of digits. For
+ * example, 370 is an Armstrong number because 3*3*3 + 7*7*7 + 0*0*0 = 370.
+ * 1634 is an Armstrong number because 1*1*1*1 + 6*6*6*6 + 3*3*3*3 + 4*4*4*4 = 1634
+ * An Armstrong number is often called Narcissistic number.
  *
- * @author Vivek
+ * @author satyabarghav
  */
 public class Armstrong {
 
@@ -16,12 +17,14 @@ public class Armstrong {
      * @return {@code true} if given number is armstrong number, {@code false}
      * otherwise
      */
-    public boolean isArmstrong(int number) {
+    public static boolean isArmstrong(int number) {
         long sum = 0;
+        String numberAsString = Integer.toString(number); // Converting the Given number to string 
+        int power = numberAsString.length(); // extract the length of the number i.e; Number of Digits
         long number2 = number;
         while (number2 > 0) {
             long mod = number2 % 10;
-            sum += Math.pow(mod, 3);
+            sum += Math.pow(mod, power); // The digit being raised to the Number of Digits and added to the sum.
             number2 /= 10;
         }
         return sum == number;
