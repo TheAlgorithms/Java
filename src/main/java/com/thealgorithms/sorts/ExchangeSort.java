1336bd47353038e5004e655d26c2ade5b4af48c5
Absharulhuda Sayed <absharulhuda@gmail.com>
2023-10-04 13:22:27 +0530

    implemented ExchangeSort under sort alogorithms

--- src/main/java/com/thealgorithms/sorts/ExchangeSort.java
+++ src/main/java/com/thealgorithms/sorts/ExchangeSort.java
@@ -3,7 +3,7 @@ package com.thealgorithms.sorts;
 public class ExchangeSort {
     public static void main(String[] args) {
         int[] arr = {5, 2, 9, 3, 6};
-        exchangeSort(arr); // Call the exchangeSort method to sort the array
+        exchangeSort(arr); // Call the optimizedExchangeSort method to sort the array
 
         System.out.println("Sorted Array:");
         for (int num : arr) {
@@ -13,18 +13,24 @@ public class ExchangeSort {
 
     public static void exchangeSort(int[] arr) {
         int n = arr.length;
+        boolean swapped;
 
-        // Outer loop iterates through each element in the array, except the last one.
         for (int i = 0; i < n - 1; i++) {
-            // Inner loop compares the current element with all the elements to its right.
-            for (int j = i + 1; j < n; j++) {
-                // If the current element is greater than the element to its right, swap them.
-                if (arr[i] > arr[j]) {
-                    // Swap arr[i] and arr[j]
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
                 }
+            }
+
+            // If no swaps were made in this iteration, the array is already sorted.
+            if (!swapped) {
+                break;
             }
         }
     }
