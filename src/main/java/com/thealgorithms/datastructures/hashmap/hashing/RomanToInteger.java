dd44fb542c01a8b6c9dd4bb6f028d5d12eda852f
Sayak123456 <nandysayak552@gmail.com>
2023-10-04 10:33:33 +0530

    Roman To Integer Updated

--- src/main/java/com/thealgorithms/datastructures/hashmap/hashing/RomanToInteger.java
+++ src/main/java/com/thealgorithms/datastructures/hashmap/hashing/RomanToInteger.java
@@ -13,25 +13,25 @@ public class RomanToInteger {
    @return an integer value of the roman number
    */
     public static int romanToInt(String s) {
-        HashMap<Character,Integer> hm=new HashMap<>();
-        hm.put('I',1);
-        hm.put('V',5);
-        hm.put('X',10);
-        hm.put('L',50);
-        hm.put('C',100);
-        hm.put('D',500);
-        hm.put('M',1000);
+        HashMap<Character,Integer> hm = new HashMap<>();
+        hm.put('I', 1);
+        hm.put('V', 5);
+        hm.put('X', 10);
+        hm.put('L', 50);
+        hm.put('C', 100);
+        hm.put('D', 500);
+        hm.put('M', 1000);
 
-        int i=s.length()-1;
-        int ans=0;
+        int i = s.length() - 1;
+        int ans = 0;
 
-        while(i>=0){
-            if(i>0 && hm.get(s.charAt(i))>hm.get(s.charAt(i-1))){
-                ans=ans+hm.get(s.charAt(i))-hm.get(s.charAt(i-1));
+        while (i >= 0) {
+            if (i > 0 && hm.get(s.charAt(i)) > hm.get(s.charAt(i - 1))) {
+                ans = ans + hm.get(s.charAt(i)) - hm.get(s.charAt(i - 1));
                 i--;
             }
             else{
-                ans+=hm.get(s.charAt(i));
+                ans += hm.get(s.charAt(i));
             }
             i--;
         }
