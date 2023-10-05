aba0ab110737c81cc8336da0715346c0536280b1
Salik <salikashraf987@gmail.com>
2023-10-05 11:49:42 +0500

    fix other two lines format

--- src/main/java/com/thealgorithms/geometry/PolyIntersect.java
+++ src/main/java/com/thealgorithms/geometry/PolyIntersect.java
@@ -26,8 +26,7 @@ public class PolyIntersect {
         List<Point> intersections = new ArrayList<Point>();
         for (Polygon p : polygons) {
             Point[] polygonIntersects = getIntersections(p, x1, y1, x2, y2);
-            for (Point pt : polygonIntersects)
-                intersections.add(pt);
+            for (Point pt : polygonIntersects) intersections.add(pt);
         }
         return intersections.toArray(new Point[] {});
     }
@@ -36,8 +35,7 @@ public class PolyIntersect {
         double d = (l2y2 - l2y1) * (l1x2 - l1x1) - (l2x2 - l2x1) * (l1y2 - l1y1);
         double na = (l2x2 - l2x1) * (l1y1 - l2y1) - (l2y2 - l2y1) * (l1x1 - l2x1);
         double nb = (l1x2 - l1x1) * (l1y1 - l2y1) - (l1y2 - l1y1) * (l1x1 - l2x1);
-        if (d == 0D)
-            return null;
+        if (d == 0D) return null;
 
         double ua = na / d;
         double ub = nb / d;
