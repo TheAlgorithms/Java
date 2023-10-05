184f0ee93f50cbb030611f389152302363435a3d
pardaz-banu <45086957+pardaz-banu@users.noreply.github.com>
2023-10-05 09:01:17 -0400

    Create Introduction.java

---
+++ Introduction.java
@@ -0,0 +1,9 @@
+import java.util.*;  
+class Introduction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Welcome "+ name +" to Hackoctoberfest 2023");
+    }
+}
