a934ea3817b8ccdc8856e1d4ac407a4bb36d5698
Salik <salikashraf987@gmail.com>
2023-10-05 11:40:11 +0500

    Fixed formatting

--- src/main/java/com/thealgorithms/geometry/PolyIntersect.java
+++ src/main/java/com/thealgorithms/geometry/PolyIntersect.java
@@ -1,13 +1,12 @@
 package com.thealgorithms.geometry;
-//http://www.java2s.com/example/java/2d-graphics/get-polygon-intersections.html
+
 import java.awt.Point;
 import java.awt.Polygon;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PolyIntersect {
-    public static Point[] getIntersections(Polygon p, int l1x1, int l1y1,
-                                           int l1x2, int l1y2) {
+    public static Point[] getIntersections(Polygon p, int l1x1, int l1y1, int l1x2, int l1y2) {
         List<Point> intersections = new ArrayList<Point>();
         for (int i = 0; i < p.npoints; i++) {
             int j = i == p.npoints - 1 ? 0 : i + 1;
@@ -16,16 +15,14 @@ public class PolyIntersect {
             int l2y1 = p.ypoints[i];
             int l2y2 = p.ypoints[j];
 
-            Point intersect = getIntersection(l1x1, l1y1, l1x2, l1y2, l2x1,
-                    l2y1, l2x2, l2y2);//from  www  .j  a v a  2  s .  co  m
+            Point intersect = getIntersection(l1x1, l1y1, l1x2, l1y2, l2x1, l2y1, l2x2, l2y2);
             if (intersect != null)
                 intersections.add(intersect);
         }
         return intersections.toArray(new Point[] {});
     }
 
-    public static Point[] getIntersections(List<Polygon> polygons, int x1,
-                                           int y1, int x2, int y2) {
+    public static Point[] getIntersections(List<Polygon> polygons, int x1, int y1, int x2, int y2) {
         List<Point> intersections = new ArrayList<Point>();
         for (Polygon p : polygons) {
             Point[] polygonIntersects = getIntersections(p, x1, y1, x2, y2);
@@ -35,14 +32,10 @@ public class PolyIntersect {
         return intersections.toArray(new Point[] {});
     }
 
-    public static Point getIntersection(int l1x1, int l1y1, int l1x2,
-                                        int l1y2, int l2x1, int l2y1, int l2x2, int l2y2) {
-        double d = (l2y2 - l2y1) * (l1x2 - l1x1) - (l2x2 - l2x1)
-                * (l1y2 - l1y1);
-        double na = (l2x2 - l2x1) * (l1y1 - l2y1) - (l2y2 - l2y1)
-                * (l1x1 - l2x1);
-        double nb = (l1x2 - l1x1) * (l1y1 - l2y1) - (l1y2 - l1y1)
-                * (l1x1 - l2x1);
+    public static Point getIntersection(int l1x1, int l1y1, int l1x2, int l1y2, int l2x1, int l2y1, int l2x2, int l2y2) {
+        double d = (l2y2 - l2y1) * (l1x2 - l1x1) - (l2x2 - l2x1) * (l1y2 - l1y1);
+        double na = (l2x2 - l2x1) * (l1y1 - l2y1) - (l2y2 - l2y1) * (l1x1 - l2x1);
+        double nb = (l1x2 - l1x1) * (l1y1 - l2y1) - (l1y2 - l1y1) * (l1x1 - l2x1);
         if (d == 0D)
             return null;
 
@@ -57,7 +50,8 @@ public class PolyIntersect {
 
         return null;
     }
-    //This Method has been created separately to test the code
+
+    // This Method has been created separately to test the code
     public static void main(String[] args) {
         // Create a Polygon object (for example, a triangle)
         Polygon polygon = new Polygon();
