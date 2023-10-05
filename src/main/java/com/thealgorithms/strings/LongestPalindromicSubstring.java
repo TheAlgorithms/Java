88f26115d94d93d6b3389c62f8fde8198e2bcfaf
ANKIT SAHA <85545712+Ankit-Saha08@users.noreply.github.com>
2023-10-05 10:56:19 +0530

    Updated LongestPalindromicSubstring.java
    Applied Java Collection Framework approach

--- src/main/java/com/thealgorithms/strings/LongestPalindromicSubstring.java
+++ src/main/java/com/thealgorithms/strings/LongestPalindromicSubstring.java
@@ -1,6 +1,8 @@
 package com.thealgorithms.strings;
 
 // Longest Palindromic Substring
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 class LongestPalindromicSubstring {
@@ -11,38 +13,33 @@ class LongestPalindromicSubstring {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the string: ");
         str = sc.nextLine();
-        System.out.println("Longest substring is : " + s.longestPalindrome(str));
+        System.out.println("Longest Palindromic substring is : " + s.longestPalindrome(str));
         sc.close();
     }
 }
 
 class Solution {
-
-    public String longestPalindrome(String s) {
-        if (s == null || s.length() == 0) {
-            return "";
-        }
-        int n = s.length();
-        String maxStr = "";
-        for (int i = 0; i < n; ++i) {
-            for (int j = i; j < n; ++j) {
-                if (isValid(s, i, j)) {
-                    if (j - i + 1 > maxStr.length()) { // update maxStr
-                        maxStr = s.substring(i, j + 1);
-                    }
-                }
-            }
-        }
-        return maxStr;
+    private boolean Palindrome(String str) {
+        str = str.toLowerCase();
+        String rev = "";
+        for (int i = str.length() - 1; i > -1; i--) rev += str.charAt(i);
+        if (str.equals(rev)) return true;
+        return false;
     }
 
-    private boolean isValid(String s, int lo, int hi) {
-        int n = hi - lo + 1;
-        for (int i = 0; i < n / 2; ++i) {
-            if (s.charAt(lo + i) != s.charAt(hi - i)) {
-                return false;
+    public String longestPalindrome(String str) {
+        // brute-force approach using Java Collections
+        ArrayList<String> al = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length() - 1; j++) {
+                String sub = str.substring(i, j);
+                boolean flag = Palindrome(sub);
+                if (flag == true) al.add(sub);
             }
         }
-        return true;
+        Collections.sort(al);
+        String res = al.get(al.size() - 1);
+        if (res.length() < 2) res = "none";
+        return res;
     }
 }
