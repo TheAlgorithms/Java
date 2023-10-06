a054e5ac7faac01b5ed94bc41b37b730e8f0d522
Imran Ansari <75426751+stasim101@users.noreply.github.com>
2023-10-06 21:52:56 +0530

    Solution to #4630
    https://github.com/TheAlgorithms/Java/issues/4630

--- src/test/java/com/thealgorithms/strings/LongestCommonPrefixTest.java
+++ src/test/java/com/thealgorithms/strings/LongestCommonPrefixTest.java
@@ -1,9 +1,7 @@
 package com.thealgorithms.strings;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -12,21 +10,21 @@ import org.apache.commons.lang3.StringUtils;
  */
 class LongestCommonPrefixTest {
 
-	@Test
-	void testGetLongestCommonPrefix() {
-		String[] strs = { "flower", "flow", "flight", "flair", "flag" };
-		assertEquals("fl", LongestCommonPrefix.getLongestCommonPrefix(strs));
-	}
+    @Test
+    void testGetLongestCommonPrefix() {
+        String[] strs = {"flower", "flow", "flight", "flair", "flag"};
+        assertEquals("fl", LongestCommonPrefix.getLongestCommonPrefix(strs));
+    }
 
-	@Test
-	void testGetLongestCommonPrefixWithOneInvalidString() {
-		String[] strs = { "flower", "flow", "flight", "flair", "random" };
-		assertEquals(StringUtils.EMPTY, LongestCommonPrefix.getLongestCommonPrefix(strs));
-	}
+    @Test
+    void testGetLongestCommonPrefixWithOneInvalidString() {
+        String[] strs = {"flower", "flow", "flight", "flair", "random"};
+        assertEquals(StringUtils.EMPTY, LongestCommonPrefix.getLongestCommonPrefix(strs));
+    }
 
-	@Test
-	void testGetLongestCommonPrefixRandomString() {
-		String[] strs = { "this", "is", "to", "test", "randomly" };
-		assertEquals(StringUtils.EMPTY, LongestCommonPrefix.getLongestCommonPrefix(strs));
-	}
+    @Test
+    void testGetLongestCommonPrefixRandomString() {
+        String[] strs = {"this", "is", "to", "test", "randomly"};
+        assertEquals(StringUtils.EMPTY, LongestCommonPrefix.getLongestCommonPrefix(strs));
+    }
 }
