6bdd7d913c7b539a72290bfe7dc36df99f59e343
vil02 <vil02@o2.pl>
2023-10-06 20:09:34 +0200

    tests: express as `ParameterizedTest`

--- src/test/java/com/thealgorithms/strings/ReverseWordsInStringTest.java
+++ src/test/java/com/thealgorithms/strings/ReverseWordsInStringTest.java
@@ -1,23 +1,20 @@
 package com.thealgorithms.strings;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class ReverseWordsInStringTest {
-
-    @Test
-    public void testCorrectReverseWordsInTheString() {
-        assertEquals("blue is Sky", ReverseWordsInString.reverseWordsInString("Sky is blue"));
+    @ParameterizedTest
+    @MethodSource("inputStream")
+    void numberTests(String expected, String input) {
+        Assertions.assertEquals(expected, ReverseWordsInString.reverseWordsInString(input));
     }
 
-    @Test
-    public void testCorrectReverseWordsInTheStringWithWhiteSpace() {
-        assertEquals("blue is Sky", ReverseWordsInString.reverseWordsInString("Sky \n is \n \n blue"));
-    }
-
-    @Test
-    public void testReverseWordsInStringForEmpty() {
-        assertEquals("", ReverseWordsInString.reverseWordsInString(""));
+    private static Stream<Arguments> inputStream() {
+        return Stream.of(Arguments.of("blue is Sky", "Sky is blue"), Arguments.of("blue is Sky", "Sky \n is \t \n  blue "), Arguments.of("", ""), Arguments.of("", "    "), Arguments.of("", "\t"));
     }
 }
