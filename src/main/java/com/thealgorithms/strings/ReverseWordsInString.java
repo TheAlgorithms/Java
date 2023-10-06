6a406f550ca25193e11a21178eda2809d2bf8018
suchib <suchibansal2007@gmail.com>
2023-10-06 11:27:05 +0530

    documented method

--- src/main/java/com/thealgorithms/strings/ReverseWordsInString.java
+++ src/main/java/com/thealgorithms/strings/ReverseWordsInString.java
@@ -3,19 +3,18 @@ package com.thealgorithms.strings;
 import java.util.Arrays;
 import java.util.Collections;
 
-/**
- * Given an input string s, reverse the order of the words.
- * A word is defined as a sequence of non-space characters.
- * The words in s will be separated by at least one space.
- * Return a string of the words in reverse order concatenated by a single space.
- * Note that s may contain leading or trailing spaces or multiple spaces between two words.
- * The returned string should only have a single space separating the words.Do not include any extra spaces
- */
 
 public final class ReverseWordsInString {
 
     private ReverseWordsInString() {
     }
+
+    /**
+     * Reverse word in the string
+     *
+     * @param s contains sentence
+       @return Reverse word of {@code s}
+     */
 
     public static String reverseWordsInString(final String s) {
         var words = s.trim().split("\\s+");
