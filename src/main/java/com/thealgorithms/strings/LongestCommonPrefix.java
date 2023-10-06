a23a517ff7c160de3cda81324628eb379430102a
Imran Ansari <75426751+stasim101@users.noreply.github.com>
2023-10-06 21:50:59 +0530

    Solution to #4630
    https://github.com/TheAlgorithms/Java/issues/4630

--- src/main/java/com/thealgorithms/strings/LongestCommonPrefix.java
+++ src/main/java/com/thealgorithms/strings/LongestCommonPrefix.java
@@ -7,38 +7,37 @@ import org.apache.commons.lang3.StringUtils;
  * @note This class is free from sonar lint issues
  */
 public class LongestCommonPrefix {
-	/**
-	 * Using a private constructor is to prevent the instantiation of a class
-	 */
-	private LongestCommonPrefix() {
-	}
+    /**
+     * Using a private constructor is to prevent the instantiation of a class
+     */
+    private LongestCommonPrefix() {
+    }
 
-	/**
-	 * @param strs: an array of string to check if its elements has common prefix
-	 * @return: a string which is a common prefix for all the Strings present in
-	 *          input array
-	 */
-	public static String getLongestCommonPrefix(String[] strs) {
-		String longestCommonPrefix = StringUtils.EMPTY;
+    /**
+     * @param strs: an array of string to check if its elements has common prefix
+     * @return: a string which is a common prefix for all the Strings present in
+     * input array
+     */
+    public static String getLongestCommonPrefix(String[] strs) {
+        String longestCommonPrefix = StringUtils.EMPTY;
 
-		// Length of input array
-		final int len = strs.length;
+        // Length of input array
+        final int len = strs.length;
 
-		if (len != 0) {
-			longestCommonPrefix = strs[0];
+        if (len != 0) {
+            longestCommonPrefix = strs[0];
 
-			for (int i = 0; i < len; i++) {
+            for (int i = 0; i < len; i++) {
 
-				// Iterate till string has longestCommonPrefix as substring
-				while (strs[i].indexOf(longestCommonPrefix) != 0) {
+                // Iterate till string has longestCommonPrefix as substring
+                while (strs[i].indexOf(longestCommonPrefix) != 0) {
 
-					// Reduce the length of longestCommonPrefix by 1
-					longestCommonPrefix = longestCommonPrefix.substring(0, longestCommonPrefix.length() - 1);
-					if (longestCommonPrefix.isEmpty())
-						return longestCommonPrefix;
-				}
-			}
-		}
-		return longestCommonPrefix;
-	}
+                    // Reduce the length of longestCommonPrefix by 1
+                    longestCommonPrefix = longestCommonPrefix.substring(0, longestCommonPrefix.length() - 1);
+                    if (longestCommonPrefix.isEmpty()) return longestCommonPrefix;
+                }
+            }
+        }
+        return longestCommonPrefix;
+    }
 }
