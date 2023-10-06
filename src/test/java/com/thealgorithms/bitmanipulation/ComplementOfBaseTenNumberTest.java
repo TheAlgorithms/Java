c4b53312d375f9c3142bc1d7d5515fd3e0aab1d2
CrioUser <crio-user@criodo.com>
2023-10-06 21:41:01 +0530

    Adding ComplementOfBaseTenNumber Algo

---
+++ src/test/java/com/thealgorithms/bitmanipulation/ComplementOfBaseTenNumberTest.java
@@ -0,0 +1,21 @@
+package com.thealgorithms.bitmanipulation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case for Complement of Base 10 Number
+ * @author Akshit Kumar Chandora (https://github.com/axitchandora)
+ */
+
+public class ComplementOfBaseTenNumberTest {
+
+    @Test
+    void testBitwiseComplement() {
+        assertEquals(7, ComplementOfBaseTenNumber.bitwiseComplement(8));
+        assertEquals(5, ComplementOfBaseTenNumber.bitwiseComplement(10));
+        assertEquals(3, ComplementOfBaseTenNumber.bitwiseComplement(40));
+    }
+    
+}
