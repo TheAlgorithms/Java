faf8cbf880e8125d3cd5c64f64f0537415b862c5
Sayak123456 <nandysayak552@gmail.com>
2023-10-07 23:42:10 +0530

    Added more test cases to RomanToIntegerTest.java

--- src/test/java/com/thealgorithms/conversions/RomanToIntegerTest.java
+++ src/test/java/com/thealgorithms/conversions/RomanToIntegerTest.java
@@ -10,5 +10,13 @@ public class RomanToIntegerTest {
     public void testRomanToInteger() {
         assertEquals(1994, RomanToInteger.romanToInt("MCMXCIV"));
         assertEquals(58, RomanToInteger.romanToInt("LVIII"));
+        assertEquals(3000, RomanToInteger.romanToInt("MMM"));
+        assertEquals(1515, RomanToInteger.romanToInt("MDXV"));
+        assertEquals(2010, RomanToInteger.romanToInt("MMX"));
+        assertEquals(520, RomanToInteger.romanToInt("DXX"));
+        assertEquals(65, RomanToInteger.romanToInt("LXV"));
+        assertEquals(1600, RomanToInteger.romanToInt("MDC"));
+        assertEquals(162, RomanToInteger.romanToInt("CLXII"));
+        assertEquals(1666, RomanToInteger.romanToInt("MDCLXVI"));
     }
 }
