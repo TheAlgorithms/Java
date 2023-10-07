a1cbda6b4113be30153a5601f862d10bd9bd7f00
blaze <prabhatkumarverma42@gmail.com>
2023-10-07 23:11:03 +0530

    updated - broken down constructor to relavent parts, made string final, made res local to searchIn(), doxygen-like style

--- src/main/java/com/thealgorithms/strings/AhoCorasick.java
+++ src/main/java/com/thealgorithms/strings/AhoCorasick.java
@@ -1,23 +1,21 @@
-/*********************************************************************************
- *                                                                               *
- *          Aho-Corasick String Matching Algorithm Implementation                *
- *                                                                               *
- * This code implements the Aho-Corasick algorithm, which is used for efficient  *
- * string matching in a given text. It can find multiple patterns simultaneously *
- *                 and records their positions in the text.                      *
- *                                                                               *
- *                      Author: Prabhat-Kumar-42                                 *
- *               GitHub: https://github.com/Prabhat-Kumar-42                     *
- *                                                                               *
- *********************************************************************************/
+/*
+ * Aho-Corasick String Matching Algorithm Implementation
+ *
+ * This code implements the Aho-Corasick algorithm, which is used for efficient
+ * string matching in a given text. It can find multiple patterns simultaneously
+ * and records their positions in the text.
+ *
+ * Author: Prabhat-Kumar-42
+ * GitHub: https://github.com/Prabhat-Kumar-42
+ */
 
 package com.thealgorithms.strings;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
+import java.util.Map;
 
 public class AhoCorasick {
 
@@ -68,39 +66,26 @@ public class AhoCorasick {
     public class Trie {
 
         private Node root = null; // Root node of the trie
+        private final String[] patterns; // patterns according to which Trie is constructed
 
-        /*
-         * For simplification of methods, declared the below as class attributes
-         *
-         * res is the list of list containing the indexes of words in patterns/dictionary
-         * list index represents word.
-         * eg - list[0] contains the list of start-index of word pattern[0]
-         *      list[1] contains the list of start-index of word pattern[1]
-         *      ......
-         *      ......
-         *      list[n] contains the list of start-index of word pattern[n]
-         */
+        public Node getTrieRoot() {
+            return root;
+        }
 
-        private ArrayList<ArrayList<Integer>> res; // Stores positions where patterns are found in the text
-        private String[] patterns; // patterns according to which Trie is constructed
-
-        /*
-         *  Constructor :
-         *        -> Builds Trie according to patterns
-         *        -> Builds Suffix Links and OutPut Links
-         *
-         *  @param patterns : patterns according to which Trie is constructed
-         */
         public Trie(String[] patterns) {
             root = new Node(); // Initialize the root of the trie
-            res = new ArrayList<>(patterns.length); // Initialize the result data structure
             this.patterns = patterns;
-            // Loop through each input pattern
+            buildTrie();
+            buildSuffixAndOutputLinks(); 
+        }
+        
+        // builds AcoCorasick Trie
+        private void buildTrie() {
+
+            // Loop through each input pattern and building Trie
             for (int i = 0; i < patterns.length; i++) {
-                res.add(new ArrayList<Integer>()); // Initialize a list to store positions of the current pattern
-
                 Node curr = root; // Start at the root of the trie for each pattern
-
+        
                 // Loop through each character in the current pattern
                 for (int j = 0; j < patterns[i].length(); j++) {
                     char c = patterns[i].charAt(j); // Get the current character
@@ -117,17 +102,25 @@ public class AhoCorasick {
                 }
                 curr.setPatternInd(i); // Store the index of the pattern in the current leaf node
             }
+        }
 
-            // Build suffix and output links after constructing the trie
-            root.setSuffixLink(root); // Initialize the suffix link of the root to itself
-            Queue<Node> q = new LinkedList<>(); // Initialize a queue for BFS traversal
-
-            // Initialize suffix links for child nodes of the root
+        // Initialize suffix links for child nodes of the root
+        private void initializeSuffixLinksForChildNodesOfTheRoot(Queue<Node> q) {
             for (char rc : root.getChild().keySet()) {
                 Node childNode = root.getChild().get(rc);
                 q.add(childNode); // Add child node to the queue
                 childNode.setSuffixLink(root); // Set suffix link to the root
             }
+        }
+
+        // Builds the suffix links and output links in the trie
+        private void buildSuffixAndOutputLinks() {
+            root.setSuffixLink(root); // Initialize the suffix link of the root to itself
+            Queue<Node> q = new LinkedList<>(); // Initialize a queue for BFS traversal
+
+            // Initialize suffix links for child nodes of the root
+            initializeSuffixLinksForChildNodesOfTheRoot(q);
+
             while (!q.isEmpty()) {
                 Node currentState = q.poll(); // Get the current node for processing
 
@@ -162,7 +155,22 @@ public class AhoCorasick {
 
         // Searches for patterns in the input text and records their positions
         public ArrayList<ArrayList<Integer>> searchIn(String text) {
+         /*
+          * res is the list of list containing the indexes of words in patterns/dictionary
+          * list index represents word.
+          * eg - list[0] contains the list of start-index of word pattern[0]
+          *      list[1] contains the list of start-index of word pattern[1]
+          *      ......
+          *      ......
+          *      list[n] contains the list of start-index of word pattern[n]
+          */
+            ArrayList<ArrayList<Integer>> res = new ArrayList<>(patterns.length); // Stores positions where patterns are found in the text
             Node parent = root; // Start searching from the root node
+            
+            for(int i = 0; i < patterns.length; i++) {
+              res.add(new ArrayList<Integer>()); // Initialize a list to store positions of the current pattern
+            }
+            
             for (int i = 0; i < text.length(); i++) {
                 char ch = text.charAt(i); // Get the current character in the text
 
@@ -191,31 +199,31 @@ public class AhoCorasick {
                     }
                 }
             }
-
-            /*
-             * By default res contains end-points.
-             * This function converts those endpoints to start points
-             */
-            for (int i = 0; i < patterns.length; i++) {
-                if (!res.get(i).isEmpty()) {
-                    for (int j = 0; j < res.get(i).size(); j++) {
-                        int endpoint = res.get(i).get(j);
-                        res.get(i).set(j, endpoint - patterns[i].length() + 1);
-                    }
-                }
-            }
+            setUpStartPoints(res);
             return res;
         }
+      // by default res contains end-points. This function converts those
+      // endpoints to start points
+      private void setUpStartPoints(ArrayList<ArrayList<Integer>> res) {
+          for (int i = 0; i < patterns.length; i++) {
+              if (!res.get(i).isEmpty()) {
+                  for (int j = 0; j < res.get(i).size(); j++) {
+                      int endpoint = res.get(i).get(j);
+                      res.get(i).set(j, endpoint - patterns[i].length() + 1);
+                  }
+              }
+          }
+      }
     }
 
-    // Public static method to search for patterns in text
+    // method to search for patterns in text
     public static Map<String, ArrayList<Integer>> search(String text, String[] patterns) {
         Trie trie = new AhoCorasick().new Trie(patterns);
         ArrayList<ArrayList<Integer>> res = trie.searchIn(text);
         return convert(res, patterns);
     }
 
-    // Private static method for converting results to a map
+    // method for converting results to a map
     private static Map<String, ArrayList<Integer>> convert(ArrayList<ArrayList<Integer>> res, String[] patterns) {
         Map<String, ArrayList<Integer>> positionByString = new HashMap<>();
         for (int i = 0; i < patterns.length; i++) {
@@ -226,3 +234,4 @@ public class AhoCorasick {
         return positionByString;
     }
 }
+
