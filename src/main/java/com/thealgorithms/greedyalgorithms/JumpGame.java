27a1bc44dc5afe831686bed6ba06371b0c783763
Rohan Dey <deyrohan16305@gmail.com>
2023-10-07 08:55:55 +0530

    Update JumpGame.java

--- src/main/java/com/thealgorithms/greedyalgorithms/JumpGame.java
+++ src/main/java/com/thealgorithms/greedyalgorithms/JumpGame.java
@@ -6,10 +6,10 @@ public class JumpGame {
 
     boolean canJump(int[] nums) {
         int reachable = 0;
-       for(int i = 0; i < nums.length; i ++) {
+        for(int i = 0; i < nums.length; i ++) {
            if(i > reachable) return false;
            reachable = Math.max(reachable, i + nums[i]);
-       } 
-       return true;
+        } 
+        return true;
     }
 }
