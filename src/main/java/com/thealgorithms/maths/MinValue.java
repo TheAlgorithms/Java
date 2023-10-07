c4811dc305467677d98739cb6b69088feb11fabd
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-10-07 19:02:45 +0200

    Update MinValue.java

--- src/main/java/com/thealgorithms/maths/MinValue.java
+++ src/main/java/com/thealgorithms/maths/MinValue.java
@@ -3,22 +3,6 @@ package com.thealgorithms.maths;
 import java.util.Random;
 
 public class MinValue {
-
-    /**
-     * Driver Code
-     */
-    public static void main(String[] args) {
-        Random rand = new Random();
-
-        /* test 100 times using rand numbers */
-        for (int i = 1; i <= 100; ++i) {
-            /* generate number from -50 to 49 */
-            int a = rand.nextInt(100) - 50;
-            int b = rand.nextInt(100) - 50;
-            assert min(a, b) == Math.min(a, b);
-        }
-    }
-
     /**
      * Returns the smaller of two {@code int} values. That is, the result the
      * argument closer to the value of {@link Integer#MIN_VALUE}. If the
