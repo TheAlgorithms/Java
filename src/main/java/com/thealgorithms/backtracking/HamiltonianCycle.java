2f199425490ebfb66ba5926e3fb1797f396b7127
Vanshika Dargan <vanshikadargan.vd@gmail.com>
2023-10-07 15:26:19 +0530

    linting tool used

--- src/main/java/com/thealgorithms/backtracking/HamiltonianCycle.java
+++ src/main/java/com/thealgorithms/backtracking/HamiltonianCycle.java
@@ -4,37 +4,42 @@ import java.util.*;
 
 /**
  * @author Vanshika Dargan (<a href="https://github.com/Vanshika-Dargan"></a>)
- * 
+ *
  * Problem Statement:
- * 
+ *
  * Given an undirected graph, the task is to determine whether the
  * graph contains a Hamiltonian cycle or not. If it contains, then
  * print the path.
- * 
+ *
  * Hamiltonian Cycle in a graph is a cycle that visits every vertex
  * of the graph exactly once and returns to the starting vertex.
- * 
+ *
  * Example:
  * Input:- graph:
- * (0)--(1)--(2) 
- * |   / \    | 
- * |  /   \   | 
- * | /     \  | 
- * (3)       (4) 
- * 
+ * (0)--(1)--(2)
+ * |   / \    |
+ * |  /   \   |
+ * | /     \  |
+ * (3)       (4)
+ *
  * vertices: 5
- * 
+ *
  * Output: 0 -> 1 -> 2 -> 4 -> 3 -> 0
- * 
+ *
  * Wikipedia link:- https://en.wikipedia.org/wiki/Hamiltonian_path
  */
-public class HamiltonianCycle {
+public
+class HamiltonianCycle {
 
-    private final int V;
-    public static int path[];
-    private static Set<Integer> visited;
+private
+    final int V;
+public
+    static int path[];
+private
+    static Set<Integer> visited;
 
-    public HamiltonianCycle(int V) {
+public
+    HamiltonianCycle(int V) {
         this.V = V;
         path = new int[V];
         visited = new HashSet<>();
@@ -43,7 +48,8 @@ public class HamiltonianCycle {
     /*
      * This function is a utility function to print the path.
      */
-    public void printPath() {
+public
+    void printPath() {
         for (int i = 0; i < V; i++) {
             System.out.print(path[i] + " -> ");
         }
@@ -54,7 +60,8 @@ public class HamiltonianCycle {
      * This function is a helper function that finds the Hamiltonian cycle
      * and populates the path array.
      */
-    private boolean hamiltonianCycleUtil(int[][] graph, int curr) {
+private
+    boolean hamiltonianCycleUtil(int[][] graph, int curr) {
         // If all vertices are included in the Hamiltonian cycle
         if (curr == V) {
             // and if there is an edge from the last vertex in the path to the start vertex
@@ -77,7 +84,7 @@ public class HamiltonianCycle {
                         return true;
                     }
                     // Remove the vertex from the path if it doesn't yield the solution
-                    visited.remove(visited.size()-1);
+                    visited.remove(visited.size() - 1);
                 }
             }
         }
@@ -88,7 +95,8 @@ public class HamiltonianCycle {
     /*
      * This function finds the Hamiltonian cycle, if any, and prints its path.
      */
-    public static int findHamiltonianCycle(int[][] graph, int vertices) {
+public
+    static int findHamiltonianCycle(int[][] graph, int vertices) {
         HamiltonianCycle hm = new HamiltonianCycle(vertices);
 
         path[0] = 0;
@@ -102,3 +110,5 @@ public class HamiltonianCycle {
         return 0;
     }
 }
+
+
