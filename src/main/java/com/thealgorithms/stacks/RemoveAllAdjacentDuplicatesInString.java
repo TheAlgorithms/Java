bc966647bc7944580bc6f4b5b64adefade1774d4
Akshit Kumar Chandora <axitchandora@gmail.com>
2023-10-07 14:20:44 +0530

    Adding Remove All Adjacent Duplicates In String
    Signed-off-by: Akshit Kumar Chandora <axitchandora@gmail.com>

--- src/main/java/com/thealgorithms/stacks/RemoveAllAdjacentDuplicatesInString.java
+++ src/main/java/com/thealgorithms/stacks/RemoveAllAdjacentDuplicatesInString.java
@@ -1,29 +1,29 @@
 package com.thealgorithms.stacks;
 
 /**
- * Remove All Adjacent Duplicates In String
- * In a string S, remove all adjacent duplicate characters recursively
- *  to generate the resultant string
+ * Remove All Adjacent Duplicates In String In a string S, remove all adjacent duplicate characters
+ * recursively to generate the resultant string
+ * 
  * @author Akshit Kumar Chandora (https://github.com/axitchandora)
  */
 
 public class RemoveAllAdjacentDuplicatesInString {
-  public static String removeDuplicates(String s) {
-    // Create a StringBuilder to use as a stack
-    StringBuilder stack = new StringBuilder();
+    public static String removeDuplicates(String s) {
+        // Create a StringBuilder to use as a stack
+        StringBuilder stack = new StringBuilder();
 
-    // Process each character in s
-    for (char c : s.toCharArray()) {
-      int length = stack.length();
-      // If the stack is not empty and the current character is the same as the
-      // top of the stack, pop the character from the stack
-      if (length > 0 && c == stack.charAt(length - 1))
-        stack.deleteCharAt(length - 1);
-      else // Push the current character onto the stack
-        stack.append(c);
+        // Process each character in s
+        for (char c : s.toCharArray()) {
+            int length = stack.length();
+            // If the stack is not empty and the current character is the same as the
+            // top of the stack, pop the character from the stack
+            if (length > 0 && c == stack.charAt(length - 1))
+                stack.deleteCharAt(length - 1);
+            else // Push the current character onto the stack
+                stack.append(c);
+        }
+
+        // Convert the stack to a string
+        return stack.toString();
     }
-
-    // Convert the stack to a string
-    return stack.toString();
-  }
 }
--- src/test/java/com/thealgorithms/stacks/RemoveAllAdjacentDuplicatesInStringTest.java
+++ src/test/java/com/thealgorithms/stacks/RemoveAllAdjacentDuplicatesInStringTest.java
@@ -6,18 +6,16 @@ import org.junit.jupiter.api.Test;
 
 /**
  * Test case for Remove All Adjacent Duplicates In String
+ * 
  * @author Akshit Kumar Chandora (https://github.com/axitchandora)
  */
 
 public class RemoveAllAdjacentDuplicatesInStringTest {
 
-  @Test
-  void testRemoveDuplicates() {
-    assertEquals(
-        "", RemoveAllAdjacentDuplicatesInString.removeDuplicates("abccba"));
-    assertEquals(
-        "fbar", RemoveAllAdjacentDuplicatesInString.removeDuplicates("foobar"));
-    assertEquals("abcd",
-                 RemoveAllAdjacentDuplicatesInString.removeDuplicates("abcd"));
-  }
+    @Test
+    void testRemoveDuplicates() {
+        assertEquals("", RemoveAllAdjacentDuplicatesInString.removeDuplicates("abccba"));
+        assertEquals("fbar", RemoveAllAdjacentDuplicatesInString.removeDuplicates("foobar"));
+        assertEquals("abcd", RemoveAllAdjacentDuplicatesInString.removeDuplicates("abcd"));
+    }
 }
