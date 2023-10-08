43623d8b2bb5ff52e5884f3e9ec095ab14d13e6c
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-10-08 20:43:30 +0200

    Update src/test/java/com/thealgorithms/maths/MinValueTest.java
    Co-authored-by: Piotr Idzik <65706193+vil02@users.noreply.github.com>

--- src/test/java/com/thealgorithms/maths/MinValueTest.java
+++ src/test/java/com/thealgorithms/maths/MinValueTest.java
@@ -9,5 +9,6 @@ public class MinValueTest {
     public void minTest() {
         assertEquals(-1, MinValue.min(-1, 3));
         assertEquals(2, MinValue.min(3, 2));
+        assertEquals(5, MinValue.min(5, 5));
     }
 }
