37cf8ee28bd917d0802cf43f22ba39b8d4f8b370
Pronay Debnath <pronayd4455@gmail.com>
2023-10-09 00:31:18 +0530

    Update FibonacciNumberTest.java

--- src/main/java/com/thealgorithms/maths/FibonacciNumberTest.java
+++ src/main/java/com/thealgorithms/maths/FibonacciNumberTest.java
@@ -11,23 +11,23 @@ public class FibonacciNumberTest {
     public void testNthFibonacci(int input) {
         int result = FibonacciNumber.nthFibonacci(input);
         switch (input) {
-            case 0:
-                assertEquals(0, result);
-                break;
-            case 1:
-                assertEquals(1, result);
-                break;
-            case 2:
-                assertEquals(1, result);
-                break;
-            case 5:
-                assertEquals(5, result);
-                break;
-            case 10:
-                assertEquals(55, result);
-                break;
-            default:
-                fail("Unexpected input value: " + input);
+        case 0:
+            assertEquals(0, result);
+            break;
+        case 1:
+            assertEquals(1, result);
+            break;
+        case 2:
+            assertEquals(1, result);
+            break;
+        case 5:
+            assertEquals(5, result);
+            break;
+        case 10:
+            assertEquals(55, result);
+            break;
+        default:
+            fail("Unexpected input value: " + input);
         }
     }
 }
