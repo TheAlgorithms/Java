eb52e31526403b17d4cc167a55bc40d4e8764ad5
Pronay Debnath <pronayd4455@gmail.com>
2023-10-09 00:35:58 +0530

    Update FibonacciNumber.java

--- src/main/java/com/thealgorithms/maths/FibonacciNumber.java
+++ src/main/java/com/thealgorithms/maths/FibonacciNumber.java
@@ -3,18 +3,30 @@ public final class FibonacciNumber {
     }
 
     // Function to find the nth Fibonacci number using the golden ratio formula
-    public static int nthFibonacci(int n) {
-        // Calculate the square root of 5 as a double
-        double sqrt5 = Math.sqrt(5);
+    public static long nthFibonacci(int n) {
+        // Find the last 'n' for which the nth Fibonacci number fits into a long
+        int lastFibThatFitsInLong = 0;
+        long prev = 0;
+        long current = 1;
+        while (current <= Long.MAX_VALUE) {
+            long next = prev + current;
+            prev = current;
+            current = next;
+            lastFibThatFitsInLong++;
+        }
 
-        // Calculate the golden ratio (phi) and its conjugate (psi) as doubles
-        double phi = (1 + sqrt5) / 2;
-        double psi = (1 - sqrt5) / 2;
+        // If 'n' is beyond what fits in a long, use a different method
+        if (n > lastFibThatFitsInLong) {
+            throw new IllegalArgumentException("Input 'n' is too large to fit into a long.");
+        }
 
         // Calculate the nth Fibonacci number using the golden ratio formula
-        int fibonacci = (int) ((1.0 / sqrt5) * (Math.pow(phi, n) - Math.pow(psi, n)));
+        double sqrt5 = Math.sqrt(5);
+        double phi = (1 + sqrt5) / 2;
+        double psi = (1 - sqrt5) / 2;
+        long fibonacci = (long) ((1.0 / sqrt5) * (Math.pow(phi, n) - Math.pow(psi, n)));
 
-        // Return the result
+        // Return the result as a long
         return fibonacci;
     }
 }
