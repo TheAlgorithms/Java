61c8ae83194d9692461c6f0d1135f57fe59d739c
Pronay Debnath <pronayd4455@gmail.com>
2023-10-08 20:28:46 +0530

    Update FibonacciNumberTest.java

--- src/main/java/com/thealgorithms/maths/FibonacciNumberTest.java
+++ src/main/java/com/thealgorithms/maths/FibonacciNumberTest.java
@@ -10,27 +10,33 @@ public class FibonacciNumberTest {
         FibonacciNumber<Integer> searcher = new FibonacciNumber<>()
         
         // Test case 1: Fibonacci number at position 0
-        int result1 = FibonacciNumber.nthFibonacci(0);
+        int result1 = 
+            FibonacciNumber.nthFibonacci(0);
         assertEquals(0, result1);
 
         // Test case 2: Fibonacci number at position 1
-        int result2 = FibonacciNumber.nthFibonacci(1);
+        int result2 = 
+            FibonacciNumber.nthFibonacci(1);
         assertEquals(1, result2);
 
         // Test case 3: Fibonacci number at position 2
-        int result3 = FibonacciNumber.nthFibonacci(2);
+        int result3 = 
+            FibonacciNumber.nthFibonacci(2);
         assertEquals(1, result3);
 
         // Test case 4: Fibonacci number at position 5
-        int result4 = FibonacciNumber.nthFibonacci(5);
+        int result4 = 
+            FibonacciNumber.nthFibonacci(5);
         assertEquals(5, result4);
 
         // Test case 5: Fibonacci number at a large position (e.g., 10)
-        int result5 = FibonacciNumber.nthFibonacci(10);
+        int result5 = 
+            FibonacciNumber.nthFibonacci(10);
         assertEquals(55, result5);
 
         // Test case 6: Fibonacci number at a negative position (should return -1)
-        int result6 = FibonacciNumber.nthFibonacci(-3);
+        int result6 = 
+            FibonacciNumber.nthFibonacci(-3);
         assertEquals(-1, result6);
     }
 }
