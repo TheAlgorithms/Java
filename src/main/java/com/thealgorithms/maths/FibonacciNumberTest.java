aa0265e5578a0427ae0bf33aa788921af6b9fab6
Pronay Debnath <pronayd4455@gmail.com>
2023-10-09 00:28:00 +0530

    Update FibonacciNumberTest.java

--- src/main/java/com/thealgorithms/maths/FibonacciNumberTest.java
+++ src/main/java/com/thealgorithms/maths/FibonacciNumberTest.java
@@ -1,36 +1,33 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test; // Import the JUnit 5 Test annotation
+import org.junit.jupiter.params.ParameterizedTest;
+
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class FibonacciNumberTest {
 
-    @Test
-    public void testNthFibonacciZero() {
-        int result = FibonacciNumber.nthFibonacci(0);
-        assertEquals(0, result);
-    }
-
-    @Test
-    public void testNthFibonacciOne() {
-        int result = FibonacciNumber.nthFibonacci(1);
-        assertEquals(1, result);
-    }
-
-    @Test
-    public void testNthFibonacciTwo() {
-        int result = FibonacciNumber.nthFibonacci(2);
-        assertEquals(1, result);
-    }
-
-    @Test
-    public void testNthFibonacciFive() {
-        int result = FibonacciNumber.nthFibonacci(5);
-        assertEquals(5, result);
-    }
-
-    @Test
-    public void testNthFibonacciLarge() {
-        int result = FibonacciNumber.nthFibonacci(10);
-        assertEquals(55, result);
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 5, 10})
+    public void testNthFibonacci(int input) {
+        int result = FibonacciNumber.nthFibonacci(input);
+        switch (input) {
+            case 0:
+                assertEquals(0, result);
+                break;
+            case 1:
+                assertEquals(1, result);
+                break;
+            case 2:
+                assertEquals(1, result);
+                break;
+            case 5:
+                assertEquals(5, result);
+                break;
+            case 10:
+                assertEquals(55, result);
+                break;
+            default:
+                fail("Unexpected input value: " + input);
+        }
     }
 }
