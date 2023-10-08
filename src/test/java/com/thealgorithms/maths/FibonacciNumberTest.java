dd6078c6f9cb4ea487cd6bc84f09141ca77258f5
Pronay Debnath <pronayd4455@gmail.com>
2023-10-09 00:44:05 +0530

    Create FibonacciNumberTest.java

---
+++ src/test/java/com/thealgorithms/maths/FibonacciNumberTest.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class FibonacciNumberTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 5, 10, 20, 30})
+    public void testNthFibonacci(int n) {
+        long result = FibonacciNumber.nthFibonacci(n);
+        long expectedResult = calculateFibonacciUsingLoop(n);
+        assertEquals(expectedResult, result);
+    }
+
+    private long calculateFibonacciUsingLoop(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        long prev = 0;
+        long current = 1;
+        for (int i = 2; i <= n; i++) {
+            long next = prev + current;
+            prev = current;
+            current = next;
+        }
+        return current;
+    }
+}
