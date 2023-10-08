565a687985ee4477bde7fa08e66f595aa91603a6
Pronay Debnath <pronayd4455@gmail.com>
2023-10-08 21:00:29 +0530

    Update FibonacciNumberTest.java

--- src/main/java/com/thealgorithms/maths/FibonacciNumberTest.java
+++ src/main/java/com/thealgorithms/maths/FibonacciNumberTest.java
@@ -1,6 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; // Import the JUnit 5 Test annotation
 
 public class FibonacciNumberTest {
 
