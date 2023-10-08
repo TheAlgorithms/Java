253cda0e881c70bcb141337b0aa8a7ed9db47b88
Pronay Debnath <pronayd4455@gmail.com>
2023-10-09 00:26:28 +0530

    Update FibonacciNumber.java

--- src/main/java/com/thealgorithms/maths/FibonacciNumber.java
+++ src/main/java/com/thealgorithms/maths/FibonacciNumber.java
@@ -1,11 +1,9 @@
-import java.util.*;
-
-public final class FibonacciNumber<T extends Number> {
+public final class FibonacciNumber {
     private FibonacciNumber() {
     }
 
     // Function to find the nth Fibonacci number using the golden ratio formula
-    public static <T extends Number> int nthFibonacci(T n) {
+    public static int nthFibonacci(int n) {
         // Calculate the square root of 5 as a double
         double sqrt5 = Math.sqrt(5);
 
@@ -13,11 +11,8 @@ public final class FibonacciNumber<T extends Number> {
         double phi = (1 + sqrt5) / 2;
         double psi = (1 - sqrt5) / 2;
 
-        // Convert the input value to an int
-        int result = n.intValue();
-
         // Calculate the nth Fibonacci number using the golden ratio formula
-        int fibonacci = (int) ((1.0 / sqrt5) * (Math.pow(phi, result) - Math.pow(psi, result)));
+        int fibonacci = (int) ((1.0 / sqrt5) * (Math.pow(phi, n) - Math.pow(psi, n)));
 
         // Return the result
         return fibonacci;
