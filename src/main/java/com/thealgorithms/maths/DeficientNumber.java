6bf0c8ff249415a808d48aceab7cf768354b0a21
Arjun <110415386+Arjun-A3@users.noreply.github.com>
2023-10-09 09:26:00 +0530

    added DeficientNumber

--- src/main/java/com/thealgorithms/maths/DeficientNumber.java
+++ src/main/java/com/thealgorithms/maths/DeficientNumber.java
@@ -4,20 +4,34 @@ import java.util.*;
 //A deficient number is a number for which the sum of its proper divisors is less than n.
 //This  program  takes an integer n as input and outputs "Yes" if it is a deficient number, and "No" otherwise.
 public class DeficientNumberTest {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the number:");
-        long n = in.nextLong();
-        long t = n;
-        long sum =1;
-        int sqr = (int)Math.sqrt(n);
-        for(int i=2;i<=sqr;i++){
-            if(n%i==0)
-            sum+=i+n/i;
+    /**
+     * Check if {@code n} is palindrome number or not
+     *
+     * @param number the number
+     * @return {@code true} if {@code n} is palindrome number, otherwise
+     * {@code false}
+     */
+
+    public boolean isDeficientNumber(long number){
+        if (number < 0) {
+            throw new IllegalArgumentException("Input parameter must not be negative!");
         }
-        if(sqr*sqr==n)
+        long t = number;
+        long sum =1;
+        int sqr = (int)Math.sqrt(number);
+        for(int i=2;i<=sqr;i++){
+            if(number%i==0)
+            sum+=i+number/i;
+        }
+        if(sqr*sqr==number)
         sum-=sqr;
-        System.out.println(sum<t?"Yes":"No");
+        return sum<t;
     }
+    // public static void main(String[] args){
+        // Scanner in = new Scanner(System.in);
+        // System.out.println("Enter the number:");
+        // long n = in.nextLong();
+        //System.out.println(isDeficientNumber(n));
+    // }
     
 }
