7932b14c90277883717b4a7ebecff1f455cd5a19
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-10-09 18:22:54 +0530

    Update longestcommonsubsequenceandsubstringproblem.java

--- src/main/java/com/thealgorithms/dynamicprogramming/longestcommonsubsequenceandsubstringproblem.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/longestcommonsubsequenceandsubstringproblem.java
@@ -1,15 +1,14 @@
-This is a new dynamic programming problem. I have published a research paper about this problem under the guidance of Professor Rao Li (University of South Carolina, Aiken) along with my friend. The paper has been accepted by the Journal of Mathematics and Informatics.The problem is a variation of the standard longest common subsequence problem. It says that-->
-"Suppose there are two strings, X and Y. Now we need to find the longest
-string, which is a subsequence of X and a substring of Y."
+// This is a new dynamic programming problem. I have published a research paper about this problem under the guidance of Professor Rao Li (University of South Carolina, Aiken) along with my friend. The paper has been accepted by the Journal of Mathematics and Informatics.The problem is a variation of the standard longest common subsequence problem. It says that-->
+// "Suppose there are two strings, X and Y. Now we need to find the longest
+// string, which is a subsequence of X and a substring of Y."
 
-Link of the paper--> http://www.researchmathsci.org/JMIart/JMI-v25-8.pdf
+// Link of the paper--> http://www.researchmathsci.org/JMIart/JMI-v25-8.pdf
 
-  import java.util.Scanner;
+import java.util.Scanner;
 
- class LCSubseqSubstr {
+class LCSubseqSubstr {
 
- public static void main(String[] args) {
-
+  public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.print("Input the first string: ");
 
@@ -21,31 +20,43 @@ Link of the paper--> http://www.researchmathsci.org/JMIart/JMI-v25-8.pdf
 
     int[][] W1 = new int[m + 1][n + 1];
 
-    System.out.println("The longest string which is a subsequence of " + X + " and a " +
-    "substring of " + Y + " is " + LCSS(X, Y, m, n, W1));
-   System.out.println("The length of the longest string which is a subsequence of " + X +
-   " and a substring of " + Y + " is " + LCSS(X, Y, m, n, W1).length());
+    System.out.println(
+      "The longest string which is a subsequence of " +
+      X +
+      " and a " +
+      "substring of " +
+      Y +
+      " is " +
+      LCSS(X, Y, m, n, W1)
+    );
+    System.out.println(
+      "The length of the longest string which is a subsequence of " +
+      X +
+      " and a substring of " +
+      Y +
+      " is " +
+      LCSS(X, Y, m, n, W1).length()
+    );
   }
 
- public static String LCSS(String X, String Y, int m, int n, int[][] W) {
-int maxLength = 0; // keeps the max length of LCSS
- int lastIndexOnY = n; // keeps the last index of LCSS in Y
- W = new int[m + 1][n + 1];
+  public static String LCSS(String X, String Y, int m, int n, int[][] W) {
+    int maxLength = 0; // keeps the max length of LCSS
+    int lastIndexOnY = n; // keeps the last index of LCSS in Y
+    W = new int[m + 1][n + 1];
 
-   for (int i = 1; i <= m; i++) {
-     for (int j = 1; j <= n; j++) {
-      if (X.charAt(i - 1) == Y.charAt(j - 1)) {
-       W[i][j] = W[i - 1][j - 1] + 1;
-     }
-    else {
-       W[i][j] = W[i - 1][j];
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+          W[i][j] = W[i - 1][j - 1] + 1;
+        } else {
+          W[i][j] = W[i - 1][j];
+        }
+        if (W[i][j] > maxLength) {
+          maxLength = W[i][j];
+          lastIndexOnY = j;
+        }
+      }
     }
-    if (W[i][j] > maxLength) {
-     maxLength = W[i][j];
-     lastIndexOnY = j;
-     }
-   }
+    return Y.substring(lastIndexOnY - maxLength, lastIndexOnY);
   }
- return Y.substring(lastIndexOnY - maxLength, lastIndexOnY);
- }
 }
