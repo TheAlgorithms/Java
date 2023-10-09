8871b0f58d2335d59e8e8dab27eaadb8f3ed1c2e
D.Sunil <sunilnitdgp5@gmail.com>
2023-10-09 13:36:14 +0000

    Formatted the code

--- src/test/java/com/thealgorithms/dynamicprogramming/RodCuttingTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/RodCuttingTest.java
@@ -47,6 +47,7 @@ public class RodCuttingTest {
         int[] negativePrice = {2, 5, 13};
         assertThrows(NegativeArraySizeException.class, () -> RodCutting.cutRod(negativePrice, -3));
         
+
         // Test case 7: Negative prices in the price array (should return 13)
         int[] negativePrices = {2, -5, 13};
         assertEquals(13, RodCutting.cutRod(negativePrices, 3));
@@ -57,6 +58,7 @@ public class RodCuttingTest {
         // Test case 8: Rod length is 0 (should return 0 as the rod cannot be cut)
         int[] emptyPrice = {};
         assertEquals(0, RodCutting.cutRod(emptyPrice, 0));
+        
         
         // Test case 9: Rod length is greater than the length of the price array (invalid input)
         int[] price6 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
