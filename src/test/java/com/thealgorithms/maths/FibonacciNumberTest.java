556aa8fe4a7270839f35450f0d69ed06e45fd1ab
Pronay Debnath <pronayd4455@gmail.com>
2023-10-11 01:05:56 +0530

    Update FibonacciNumberTest.java

--- src/test/java/com/thealgorithms/maths/FibonacciNumberTest.java
+++ src/test/java/com/thealgorithms/maths/FibonacciNumberTest.java
@@ -5,28 +5,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class FibonacciNumberTest {
 
+    private FibonacciCalculator calculator = new FibonacciCalculator();
+
     @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 5, 10, 20, 30})
+    @ValueSource(ints = {0, 1, 2, 5, 10, 20, 30, 92}) // Include 92 as the upper limit
     public void testNthFibonacci(int n) {
         long result = FibonacciNumber.nthFibonacci(n);
-        long expectedResult = FibonacciCalculator.calculateFibonacciUsingLoop(n);
+        long expectedResult = calculator.calculateFibonacciUsingLoop(n);
         assertEquals(expectedResult, result);
-    }
-}
-
-class FibonacciCalculator {
-
-    public static long calculateFibonacciUsingLoop(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        long prev = 0;
-        long current = 1;
-        for (int i = 2; i <= n; i++) {
-            long next = prev + current;
-            prev = current;
-            current = next;
-        }
-        return current;
     }
 }
