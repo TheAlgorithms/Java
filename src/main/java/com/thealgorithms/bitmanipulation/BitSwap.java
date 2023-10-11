fe550c47336dbe76e5fa29c852dcaded61f137ee
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-10-11 16:28:25 +0200

    Create BitSwap.java

---
+++ src/main/java/com/thealgorithms/bitmanipulation/BitSwap.java
@@ -0,0 +1,18 @@
+public final BitSwap {
+  private BitSwap() {
+  }
+/*
+ * Swaps the bits at the position swap1 and swap2 from bit
+ */
+  
+  public static int bitSwap(int bit, int swap1, int swap2) {
+    int val1 = (bit >> swap1) & 1;
+    int val2 = (bit >> swap2) & 1;
+
+    if (val1 != val2) {
+        bit = bit ^ (1 << swap1);
+        bit = bit ^ (1 << swap2);
+    }
+    return bit;
+  }
+}
