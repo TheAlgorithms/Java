bbb3a6a0af01aea28a57978c3606ad235bb95402
Pronay Debnath <pronayd4455@gmail.com>
2023-10-11 23:51:20 +0530

    Update FibonacciNumberTest.java

--- src/test/java/com/thealgorithms/maths/FibonacciNumberTest.java
+++ src/test/java/com/thealgorithms/maths/FibonacciNumberTest.java
@@ -5,13 +5,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class FibonacciNumberTest {
 
-    private FibCalc calculator = new FibCalc();
-
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 5, 10, 20, 30, 92}) // Include 92 as the upper limit
     public void testNthFibonacci(int n) {
         long result = FibonacciNumber.nthFibonacci(n);
-        long expectedResult = calculator.calculateFibonacciUsingLoop(n);
+        long expectedResult = FibCalc.calculateFibonacciUsingLoop(n); // Call the static method directly
         assertEquals(expectedResult, result);
     }
 }
