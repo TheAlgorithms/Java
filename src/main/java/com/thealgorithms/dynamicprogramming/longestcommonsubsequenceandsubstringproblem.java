eebb30f6dbfd83439b80327310f2bbadc1c71947
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2023-10-11 17:36:01 +0530

    Update longestcommonsubsequenceandsubstringproblem.java

--- src/main/java/com/thealgorithms/dynamicprogramming/longestcommonsubsequenceandsubstringproblem.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/longestcommonsubsequenceandsubstringproblem.java
@@ -12,32 +12,30 @@ import java.util.Scanner;
 
 class LCSubseqSubstr {
 
-public
-  static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.print("Input the first string: ");
+ public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.print("Input the first string: ");
 
-    String X = input.nextLine();
-    System.out.print("Input the second string: ");
+      String X = input.nextLine();
+      System.out.print("Input the second string: ");
 
-    String Y = input.nextLine();
-    int m = X.length(), n = Y.length();
+      String Y = input.nextLine();
+      int m = X.length(), n = Y.length();
 
-    int[][] W1 = new int[m + 1][n + 1];
+      int[][] W1 = new int[m + 1][n + 1];
 
-    System.out.println("The longest string which is a subsequence of " + X +
+      System.out.println("The longest string which is a subsequence of " + X +
                        " and a " + "substring of " + Y + " is " +
                        LCSS(X, Y, m, n, W1));
-    System.out.println(
-        "The length of the longest string which is a subsequence of " + X +
-        " and a substring of " + Y + " is " + LCSS(X, Y, m, n, W1).length());
+      System.out.println(
+        "The length of the longest string which is a subsequence of " + X +" and a substring of " 
++ Y + " is " + LCSS(X, Y, m, n, W1).length());
   }
 
-public
-  static String LCSS(String X, String Y, int m, int n, int[][] W) {
-    int maxLength = 0;    // keeps the max length of LCSS
-    int lastIndexOnY = n; // keeps the last index of LCSS in Y
-    W = new int[m + 1][n + 1];
+ public static String LCSS(String X, String Y, int m, int n, int[][] W) {
+     int maxLength = 0;    // keeps the max length of LCSS
+     int lastIndexOnY = n; // keeps the last index of LCSS in Y
+     W = new int[m + 1][n + 1];
 
     for (int i = 1; i <= m; i++) {
       for (int j = 1; j <= n; j++) {
@@ -49,8 +47,8 @@ public
         if (W[i][j] > maxLength) {
           maxLength = W[i][j];
           lastIndexOnY = j;
-        }
-      }
+         }
+       }
     }
     return Y.substring(lastIndexOnY - maxLength, lastIndexOnY);
   }
