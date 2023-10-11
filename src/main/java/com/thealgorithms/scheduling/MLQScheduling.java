41ddccb127ba7ba88abbb1e600164be85fc10623
Aiyush Kumar <aiyushkanojia@gmail.com>
2023-10-11 11:08:15 +0530

    Tried to Fix Clang-format error

--- src/main/java/com/thealgorithms/scheduling/MLQScheduling.java
+++ src/main/java/com/thealgorithms/scheduling/MLQScheduling.java
@@ -11,77 +11,73 @@ import java.util.Queue;
  * <a href="https://www.geeksforgeeks.org/multilevel-queue-mlq-cpu-scheduling/">...</a>
  */
 
-class Process1 {
+class ProcessTask {
+    String name;
+    int priority;
+    int burstTime;
 
-  String name;
-  int priority;
-  int burstTime;
-
-  public Process1(String name, int priority, int burstTime) {
-    this.name = name;
-    this.priority = priority;
-    this.burstTime = burstTime;
-  }
+    public ProcessTask(String name, int priority, int burstTime) {
+        this.name = name;
+        this.priority = priority;
+        this.burstTime = burstTime;
+    }
 }
 
 public class MLQScheduling {
+    ArrayList<Queue<ProcessTask>> queues;
+    int numQueues;
 
-  ArrayList<Queue<Process1>> queues;
-  int numQueues;
-
-  public MLQScheduling(int numQueues) {
-    this.numQueues = numQueues;
-    queues = new ArrayList<>(numQueues);
-    for (int i = 0; i < numQueues; i++) {
-      queues.add(new LinkedList<>());
-    }
-  }
-
-  public void addProcess(Process1 process) {
-    // Add a process to the highest-priority queue (Queue 0)
-    queues.get(0).add(process);
-  }
-
-  public void run() {
-    while (!queues.get(0).isEmpty() || !queues.get(numQueues - 1).isEmpty()) {
-      // Start from the highest-priority queue (Queue 0)
-      for (int i = 0; i < numQueues; i++) {
-        Queue<Process1> currentQueue = queues.get(i);
-        if (!currentQueue.isEmpty()) {
-          // Get the next process from the current queue
-          Process1 currentProcess = currentQueue.poll();
-          System.out.println("Running process: " + currentProcess.name);
-
-          // Simulate process execution (reduce burst time)
-          int remainingBurstTime = currentProcess.burstTime - 1;
-          if (remainingBurstTime > 0) {
-            // Move the process to a lower-priority queue
-            int nextQueueIndex = Math.min(i + 1, numQueues - 1);
-            currentProcess.burstTime = remainingBurstTime;
-            currentProcess.priority = nextQueueIndex; // Update priority
-            queues.get(nextQueueIndex).add(currentProcess);
-            System.out.println("Moving process to Queue " + nextQueueIndex);
-          } else {
-            // Process has completed
-            System.out.println(
-              "-> Process " + currentProcess.name + " has completed."
-            );
-          }
+    public MLQScheduling(int numQueues) {
+        this.numQueues = numQueues;
+        queues = new ArrayList<>(numQueues);
+        for (int i = 0; i < numQueues; i++) {
+          queues.add(new LinkedList<>());
         }
-      }
     }
-    System.out.println("**All Process are completed**");
-  }
 
-  public static void main(String[] args) {
-    MLQScheduling scheduler = new MLQScheduling(3); // Three priority queues
-    // Add processes to the scheduler with different priorities and burst times
-    scheduler.addProcess(new Process1("P1", 0, 5));
-    scheduler.addProcess(new Process1("P2", 1, 4));
-    scheduler.addProcess(new Process1("P3", 2, 3));
-    scheduler.addProcess(new Process1("P4", 0, 2));
+    public void addProcess(ProcessTask process) {
+        // Add a process to the highest-priority queue (Queue 0)
+        queues.get(0).add(process);
+    }
 
-    // Run the scheduler
-    scheduler.run();
-  }
+    public void run() {
+        while (!queues.get(0).isEmpty() || !queues.get(numQueues - 1).isEmpty()) {
+          // Start from the highest-priority queue (Queue 0)
+            for (int i = 0; i < numQueues; i++) {
+                Queue<ProcessTask> currentQueue = queues.get(i);
+                if (!currentQueue.isEmpty()) {
+                // Get the next process from the current queue
+                ProcessTask currentProcess = currentQueue.poll();
+                System.out.println("Running process: " + currentProcess.name);
+    
+                // Simulate process execution (reduce burst time)
+                int remainingBurstTime = currentProcess.burstTime - 1;
+                    if (remainingBurstTime > 0) {
+                        // Move the process to a lower-priority queue
+                        int nextQueueIndex = Math.min(i + 1, numQueues - 1);
+                        currentProcess.burstTime = remainingBurstTime;
+                        currentProcess.priority = nextQueueIndex; // Update priority
+                        queues.get(nextQueueIndex).add(currentProcess);
+                        System.out.println("Moving process to Queue " + nextQueueIndex);
+                    } else {
+                        // Process has completed
+                        System.out.println("-> Process " + currentProcess.name + " has completed.");
+                    }
+                }
+            }
+        }
+        System.out.println("**All Process are completed**");
+    }
+
+    public static void main(String[] args) {
+        MLQScheduling scheduler = new MLQScheduling(3); // Three priority queues
+        // Add processes to the scheduler with different priorities and burst times
+        scheduler.addProcess(new ProcessTask("P1", 0, 5));
+        scheduler.addProcess(new ProcessTask("P2", 1, 4));
+        scheduler.addProcess(new ProcessTask("P3", 2, 3));
+        scheduler.addProcess(new ProcessTask("P4", 0, 2));
+    
+        // Run the scheduler
+        scheduler.run();
+    }
 }
