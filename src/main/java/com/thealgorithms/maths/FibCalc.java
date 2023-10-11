bcae36fd3753943f7e8ed48fff779adc93fb2c7c
Pronay Debnath <pronayd4455@gmail.com>
2023-10-11 23:48:16 +0530

    Create FibCalc.java

---
+++ src/main/java/com/thealgorithms/maths/FibCalc.java
@@ -0,0 +1,16 @@
+public class FibCalc {
+
+    public static long calculateFibonacciUsingLoop(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        long prev = 0;
+        long current = 1;
+        for (int i = 2; i <= n; i++) {
+            long next = prev + current;
+            prev = current;
+            current = next;
+        }
+        return current;
+    }
+}
