cb49f1853ed76904fd012e42b5ee250beeb9afc7
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-10-11 16:33:50 +0200

    Create BitSwapTest.java

---
+++ src/test/java/com/thealgorithms/bitmanipulation/BitSwapTest.java
@@ -0,0 +1,13 @@
+package com.thealgorithms.bitmanipulation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public final BitSwapTest {
+  @Test
+    void testHighestSetBit() {
+      assertEquals(3, BitSwap.bitSwap(3, 0, 1));
+      assertEquals(5, BitSwap.bitSwap(6, 0, 1));
+    }
+}
