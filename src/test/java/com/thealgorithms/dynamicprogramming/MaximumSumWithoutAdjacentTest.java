5ad9f957e0c9bffc6dac11c8a20a20956e575d38
Swarnim Shukla <swarnim0300@gmail.com>
2023-10-13 00:11:55 +0530

    Added test

---
+++ src/test/java/com/thealgorithms/dynamicprogramming/MaximumSumWithoutAdjacentTest.java
@@ -0,0 +1,12 @@
+package com.thealgorithms.dynamicprogramming;
+
+import dynamicprogramming.MaximumSumWithoutAdjacent;
+import org.junit.jupiter.api.Test;
+
+public class MaximumSumWithoutAdjacentTest {
+    @Test
+    public void testfindMaxSumWithoutAdjacent() {
+        int[][] sampleInput = new int[][]{{1, 2, 4, 3, 2}, {4, 2, 1, 3, 2}};
+        assert MaximumSumWithoutAdjacent.findMaxSumWithoutAdjacent(sampleInput) == 10;
+    }
+}
