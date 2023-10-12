32608b0e320271cf1bef36dce1f4ead983ece58b
Anup Omkar <anup_omkar@intuit.com>
2023-10-12 21:43:09 +0530

    fix: adding comments, refactor and handling edge cases

--- src/test/java/com/thealgorithms/maths/MatrixRankTest.java
+++ src/test/java/com/thealgorithms/maths/MatrixRankTest.java
@@ -1,36 +1,31 @@
 package com.thealgorithms.maths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class MatrixRankTest {
 
+    private static Stream<Arguments> inputStream() {
+        return Stream.of(Arguments.of(3, new double[][] {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}), Arguments.of(0, new double[][] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}), Arguments.of(1, new double[][] {{1}}), Arguments.of(2, new double[][] {{1, 2}, {3, 4}}),
+            Arguments.of(2, new double[][] {{3, -1, 2}, {-3, 1, 2}, {-6, 2, 4}}), Arguments.of(3, new double[][] {{2, 3, 0, 1}, {1, 0, 1, 2}, {-1, 1, 1, -2}, {1, 5, 3, -1}}), Arguments.of(1, new double[][] {{1, 2, 3}, {3, 6, 9}}),
+            Arguments.of(2, new double[][] {{0.25, 0.5, 0.75, 2}, {1.5, 3, 4.5, 6}, {1, 2, 3, 4}}));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputStream")
+    void computeRankTests(int expectedRank, double[][] matrix) {
+        assertEquals(expectedRank, MatrixRank.computeRank(matrix));
+    }
+
     @Test
-    void computeRank() {
-
-        double[][] identityMatrix = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-        assertEquals(3, MatrixRank.computeRank(identityMatrix));
-
-        double[][] zeroMatrix = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-        assertEquals(0, MatrixRank.computeRank(zeroMatrix));
-
-        double[][] square1x1Matrix = {{1}};
-        assertEquals(1, MatrixRank.computeRank(square1x1Matrix));
-
-        double[][] square2x2Matrix = {{1, 2}, {3, 4}};
-        assertEquals(2, MatrixRank.computeRank(square2x2Matrix));
-
-        double[][] square3x3Matrix = {{3, -1, 2}, {-3, 1, 2}, {-6, 2, 4}};
-        assertEquals(2, MatrixRank.computeRank(square3x3Matrix));
-
-        double[][] square4x4Matrix = {{2, 3, 0, 1}, {1, 0, 1, 2}, {-1, 1, 1, -2}, {1, 5, 3, -1}};
-        assertEquals(3, MatrixRank.computeRank(square4x4Matrix));
-
-        double[][] rectangular2X3Matrix = {{1, 2, 3}, {3, 6, 9}};
-        assertEquals(1, MatrixRank.computeRank(rectangular2X3Matrix));
-
-        double[][] rectangular3X4Matrix = {{0.25, 0.5, 0.75, 2}, {1.5, 3, 4.5, 6}, {1, 2, 3, 4}};
-        assertEquals(2, MatrixRank.computeRank(rectangular3X4Matrix));
+    void computeRankWithJaggedArray() {
+        double[][] jaggedArray = {{1, 2}, {10}, {100, 200, 300}};
+        assertThrows(IllegalArgumentException.class, () -> MatrixRank.computeRank(jaggedArray));
     }
 }
