d85c849582f8b4f8c4e178bebf56c467379eb4e7
Pronay Debnath <pronayd4455@gmail.com>
2023-10-12 19:57:59 +0530

    Update FibonacciNumber.java

--- src/main/java/com/thealgorithms/maths/FibonacciNumber.java
+++ src/main/java/com/thealgorithms/maths/FibonacciNumber.java
@@ -5,20 +5,20 @@ public final class FibonacciNumber {
     }
 
     // Compute the limit for n that fits in a long
-    private static final int argLimit = 92;
+    private static final int argLimit = 100;
 
     public static BigInteger nthFibonacci(int n) {
         if (n > argLimit) {
             throw new IllegalArgumentException("Input 'n' is too large to fit into a long.");
         }
 
-        BigInteger a = BigInteger.ZERO;
-        BigInteger b = BigInteger.ONE;
-        for (int i = 2; i <= n; i++) {
-            BigInteger next = a.add(b);
-            a = b;
-            b = next;
-        }
-        return b;
+        // Calculate the nth Fibonacci number using the golden ratio formula
+        final double sqrt5 = Math.sqrt(5);
+        final double phi = (1 + sqrt5) / 2;
+        final double psi = (1 - sqrt5) / 2;
+        double result = (Math.pow(phi, n) - Math.pow(psi, n)) / sqrt5;
+
+        // Round to the nearest integer and convert to BigInteger
+        return BigInteger.valueOf(Math.round(result));
     }
 }
