53545a6ad4af07aa41c150db89d49af8a59e0369
Pronay Debnath <pronayd4455@gmail.com>
2023-10-12 22:52:06 +0530

    Update Fibonacci.java

--- src/main/java/com/thealgorithms/maths/Fibonacci.java
+++ src/main/java/com/thealgorithms/maths/Fibonacci.java
@@ -5,20 +5,20 @@ public class Fibonacci {
         if (n < 0) {
             throw new IllegalArgumentException("Input 'n' must be a non-negative integer.");
         }
-    
+
         if (n <= 1) {
             return BigInteger.valueOf(n);
         }
-    
+
         BigInteger prev = BigInteger.ZERO;
         BigInteger current = BigInteger.ONE;
-        
+
         for (int i = 2; i <= n; i++) {
             BigInteger next = prev.add(current);
             prev = current;
             current = next;
         }
-        
+
         return current;
     }
 }
