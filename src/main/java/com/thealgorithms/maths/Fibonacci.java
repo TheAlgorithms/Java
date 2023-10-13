77f46359a4a2957d9a4a5ee597ec03c9db6ccefa
Pronay Debnath <pronayd4455@gmail.com>
2023-10-13 20:32:32 +0530

    Update Fibonacci.java

--- src/main/java/com/thealgorithms/maths/Fibonacci.java
+++ src/main/java/com/thealgorithms/maths/Fibonacci.java
@@ -2,9 +2,22 @@ package com.thealgorithms.maths;
 
 import java.math.BigInteger;
 
+/**
+ * This class provides methods for calculating Fibonacci numbers using BigInteger for large values of 'n'.
+ */
 public final class Fibonacci {
+
     private Fibonacci() {
+        // Private constructor to prevent instantiation of this utility class.
     }
+
+    /**
+     * Calculates the nth Fibonacci number.
+     *
+     * @param n The index of the Fibonacci number to calculate.
+     * @return The nth Fibonacci number as a BigInteger.
+     * @throws IllegalArgumentException if the input 'n' is a negative integer.
+     */
     public static BigInteger calFib(final int n) {
         if (n < 0) {
             throw new IllegalArgumentException("Input 'n' must be a non-negative integer.");
@@ -13,7 +26,7 @@ public final class Fibonacci {
         if (n <= 1) {
             return BigInteger.valueOf(n);
         }
-
+        
         BigInteger prev = BigInteger.ZERO;
         BigInteger current = BigInteger.ONE;
 
