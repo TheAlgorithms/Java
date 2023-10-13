eb49d0e70dd23bc3a85983957f0bde2e297273ba
jamestitus299 <titusjames299@gmail.com>
2023-10-13 12:05:59 +0530

    missingNumber

---
+++ src/main/java/com/thealgorithms/bitmanipulation/MissingNumber.java
@@ -0,0 +1,11 @@
+package com.thealgorithms.bitmanipulation;
+
+/**
+ * Find the missing number in a array of consecutive integers
+ */
+
+public class MissingNumber {
+    public static boolean missingNumber() {
+        
+    }
+}
