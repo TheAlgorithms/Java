440da9651ccde91e22e37bb5f498b2a694fa8562
Pronay Debnath <pronayd4455@gmail.com>
2023-10-13 20:41:34 +0530

    Update FibonacciNumberGoldenRationTest.java

--- src/test/java/com/thealgorithms/maths/FibonacciNumberGoldenRationTest.java
+++ src/test/java/com/thealgorithms/maths/FibonacciNumberGoldenRationTest.java
@@ -11,21 +11,21 @@ public class FibonacciNumberGoldenRationTest {
     @Test
     public void returnsCorrectValues() {
         for (int n = 0; n <= FibonacciNumber.MAX_ARG; ++n) {
-            final var actual = BigInteger.valueOf(FibonacciNumber.nthFibonacci(n));
-            final var expected = Fibonacci.calFib(n);
+            final var actual = BigInteger.valueOf(FibonacciNumber.nthFibonaccicompute(n));
+            final var expected = Fibonacci.calFibcompute(n);
             assertEquals(expected, actual);
         }
     }
 
     @Test
     public void throwsIllegalArgumentExceptionForNegativeInput() {
-        assertThrows(IllegalArgumentException.class, () -> { FibonacciNumber.nthFibonacci(-1); });
+        assertThrows(IllegalArgumentException.class, () -> { FibonacciNumber.nthFibonaccicompute(-1); });
     }
 
     @Test
     public void throwsIllegalArgumentExceptionForLargeInput() {
         assertThrows(IllegalArgumentException.class, () -> {
-            FibonacciNumber.nthFibonacci(FibonacciNumber.MAX_ARG+1);
+            FibonacciNumber.nthFibonaccicompute(FibonacciNumber.MAX_ARG+1);
         });
     }
 }
