a32824098dba7161dbac58bbf4a81e3c6bf7aabe
Pronay Debnath <pronayd4455@gmail.com>
2023-10-13 10:21:50 +0530

    Update FibonacciNumberTest.java

--- src/test/java/com/thealgorithms/maths/FibonacciNumberTest.java
+++ src/test/java/com/thealgorithms/maths/FibonacciNumberTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
@@ -12,5 +13,20 @@ public class FibonacciNumberTest {
             final var expected = Fibonacci.calFib(n);
             assertEquals(expected, actual);
         }
+    }
+
+    @Test
+    public void throwsIllegalArgumentExceptionForNegativeInput() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            FibonacciNumber.nthFibonacci(-1);
+        });
+    }
+
+    @Test
+    public void throwsIllegalArgumentExceptionForLargeInput() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            // You can choose a large value for n here, e.g., 1000
+            FibonacciNumber.nthFibonacci(1000);
+        });
     }
 }
