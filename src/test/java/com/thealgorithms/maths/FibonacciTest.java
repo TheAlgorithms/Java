f722307ad6954b3bb7db0fbf0df78e4dc5426772
Pronay Debnath <pronayd4455@gmail.com>
2023-10-13 20:42:25 +0530

    Update FibonacciTest.java

--- src/test/java/com/thealgorithms/maths/FibonacciTest.java
+++ src/test/java/com/thealgorithms/maths/FibonacciTest.java
@@ -9,28 +9,28 @@ import org.junit.jupiter.api.Test;
 public class FibonacciTest {
     @Test
     public void checkValueAtZero() {
-        assertEquals(BigInteger.ZERO, Fibonacci.calFib(0));
+        assertEquals(BigInteger.ZERO, Fibonacci.calFibcompute(0));
     }
 
     @Test
     public void checkValueAtOne() {
-        assertEquals(BigInteger.ONE, Fibonacci.calFib(1));
+        assertEquals(BigInteger.ONE, Fibonacci.calFibcompute(1));
     }
 
     @Test
     public void checkValueAtTwo() {
-        assertEquals(BigInteger.ONE, Fibonacci.calFib(2));
+        assertEquals(BigInteger.ONE, Fibonacci.calFibcompute(2));
     }
 
     @Test
     public void checkRecurrenceRelation() {
         for (int i = 0; i < 100; ++i) {
-            assertEquals(Fibonacci.calFib(i + 2), Fibonacci.calFib(i + 1).add(Fibonacci.calFib(i)));
+            assertEquals(Fibonacci.calFibcompute(i + 2), Fibonacci.calFibcompute(i + 1).add(Fibonacci.calFibcompute(i)));
         }
     }
 
     @Test
     public void checkNegativeInput() {
-        assertThrows(IllegalArgumentException.class, () -> { Fibonacci.calFib(-1); });
+        assertThrows(IllegalArgumentException.class, () -> { Fibonacci.calFibcompute(-1); });
     }
 }
