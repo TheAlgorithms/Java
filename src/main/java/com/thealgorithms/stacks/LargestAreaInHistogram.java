7f7c5dd1f7a50ef296e34e673218374992794529
SHIWANSHI TRIPATHI <112747153+TRIPATHISHIWANSHI@users.noreply.github.com>
2023-10-13 15:21:22 +0530

    Create LargestAreaInHistogram.java

---
+++ src/main/java/com/thealgorithms/stacks/LargestAreaInHistogram.java
@@ -0,0 +1,42 @@
+package com.thealgorithms.stacks;
+
+import java.util.Stack;
+
+public class LargestAreaInHistogram {
+    static int getMaxArea(int arr[]) {
+        int temp = 0;
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            int current = arr[i];
+
+            // Checking for previous area
+            Stack<Integer> leftStack = new Stack<>();
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] >= arr[i]) {
+                    current += arr[i];
+                } else {
+                    break;
+                }
+            }
+
+            // Checking for next area
+            Stack<Integer> rightStack = new Stack<>();
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] >= arr[i]) {
+                    current += arr[i];
+                } else {
+                    break;
+                }
+            }
+
+            temp = Math.max(temp, current);
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {6, 2, 5, 4, 5, 6};
+        System.out.println(getMaxArea(a));
+    }
+}
