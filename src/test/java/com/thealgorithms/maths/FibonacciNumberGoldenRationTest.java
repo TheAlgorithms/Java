f054ad61e5b33c88fc3bb331baa7c19c435098b4
Pronay Debnath <pronayd4455@gmail.com>
2023-10-13 20:35:43 +0530

    Update and rename FibonacciNumberTest.java to FibonacciNumberGoldenRationTest.java

--- src/test/java/com/thealgorithms/maths/FibonacciNumberGoldenRationTest.java
+++ src/test/java/com/thealgorithms/maths/FibonacciNumberGoldenRationTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 
-public class FibonacciNumberTest {
+public class FibonacciNumberGoldenRationTest {
 
     @Test
     public void returnsCorrectValues() {
