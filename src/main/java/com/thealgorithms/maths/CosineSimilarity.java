ec58194c8e82723a3188ffdf2e839d2e01cee130
kmtGryffindor20 <tripathikmt20@gmail.com>
2023-10-14 01:00:02 +0530

    Minor Changes

--- src/main/java/com/thealgorithms/maths/CosineSimilarity.java
+++ src/main/java/com/thealgorithms/maths/CosineSimilarity.java
@@ -16,7 +16,7 @@ public class CosineSimilarity {
 
 
     // Function to compute the euclidean magnitude of the vector.
-    public static double vectorMagnitude(double[] vector){
+    public static double vectorMagnitude(double[] vector) {
         double magnitude = 0;
         for (int i = 0; i < vector.length; i++) {
             magnitude += vector[i] * vector[i];
@@ -25,22 +25,22 @@ public class CosineSimilarity {
     }
     
     // Function to compute the cosine similarity between vector1 and vector2
-    public static double cosineSimilarity(double vector1[], double vector2[]){
+    public static double cosineSimilarity(double vector1[], double vector2[]) {
         int lengthOfSmallerVector = vector1.length < vector2.length ? vector1.length : vector2.length;
         double dotProduct = 0;
 
-        for (int i = 0; i < lengthOfSmallerVector; i++) 
-        {
+        for (int i = 0; i < lengthOfSmallerVector; i++) {
             dotProduct += vector1[i] * vector2[i];
         }
 
         double vector1Magnitude = vectorMagnitude(vector1);
         double vector2Magnitude = vectorMagnitude(vector2);
 
-        if (vector1Magnitude == 0 || vector2Magnitude == 0)
+        if (vector1Magnitude == 0 || vector2Magnitude == 0) {
             throw new IllegalArgumentException("Cosine Similarity cannot be computed. Zero Vectors are not allowed.");
+        }
 
-        double cosineSimilarityValue = (dotProduct)/(vector1Magnitude * vector2Magnitude);
+        double cosineSimilarityValue = (dotProduct) / (vector1Magnitude * vector2Magnitude);
         return cosineSimilarityValue;
         
     }
@@ -50,7 +50,7 @@ public class CosineSimilarity {
 
         System.out.println("Enter the length of 1st Vector");
         int len1 = sc.nextInt();
-        double [] vector1 = new double[len1];
+        double[] vector1 = new double[len1];
         System.out.println("Enter the vector");
         for (int i = 0; i < len1; i++) {
             vector1[i] = sc.nextInt();
@@ -58,7 +58,7 @@ public class CosineSimilarity {
 
         System.out.println("Enter the length of 2nd Vector");
         int len2 = sc.nextInt();
-        double [] vector2 = new double[len2];
+        double[] vector2 = new double[len2];
         System.out.println("Enter the vector");
         for (int i = 0; i < len2; i++) {
             vector2[i] = sc.nextInt();
