473c7b40ed248292e98c1da0b76299fcd9f51c60
Aditi Bansal <142652964+Aditi22Bansal@users.noreply.github.com>
2023-10-15 00:56:46 +0530

    Update IsEven.java

--- src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
+++ src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
@@ -8,9 +8,4 @@ public class IsEven {
     public static boolean isEven(int number) {
         return (number & 1) == 0;
     }
-
-    static {
-        boolean res = isEven(78);
-        System.out.println("Is 78 even? " + res);
-    }
 }
