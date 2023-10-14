8c16fab539db00629c747ca8ca70795fb45fee25
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-10-14 14:15:58 +0200

    Update src/test/java/com/thealgorithms/bitmanipulation/BitSwapTest.java
    Co-authored-by: Piotr Idzik <65706193+vil02@users.noreply.github.com>

--- src/test/java/com/thealgorithms/bitmanipulation/BitSwapTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/BitSwapTest.java
@@ -8,5 +8,6 @@ public class BitSwapTest {
     void testHighestSetBit() {
         assertEquals(3, BitSwap.bitSwap(3, 0, 1));
         assertEquals(5, BitSwap.bitSwap(6, 0, 1));
+        assertEquals(7, BitSwap.bitSwap(7, 1, 1));
     }
 }
