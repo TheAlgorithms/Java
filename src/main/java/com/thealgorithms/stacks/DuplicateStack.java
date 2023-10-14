86634fdac34507d734e12c610699a440ff1111d4
Riya Khandelwal <riyakhandelwal@jklu.edu.in>
2023-10-14 06:10:38 +0530

    Update DuplicateStack.java

--- src/main/java/com/thealgorithms/stacks/DuplicateStack.java
+++ src/main/java/com/thealgorithms/stacks/DuplicateStack.java
@@ -1,8 +1,7 @@
-
 import java.util.Scanner;
 
 public class DuplicateStack {
-    static int[] St;
+    static int[] stack;
     static int max;
     static int top = -1;
 
@@ -12,7 +11,7 @@ public class DuplicateStack {
         System.out.println("Enter the maximum in the stack");
         max = scanner.nextInt();
 
-        St = new int[max];
+        stack = new int[max];
 
         System.out.println("Enter the values of elements");
         for (int i = 0; i < max; i++) {
@@ -22,7 +21,7 @@ public class DuplicateStack {
         System.out.println("Stack is:");
         display();
 
-        duplicate(St);
+        duplicate(stack);
     }
 
     public static void push(int val) {
@@ -30,28 +29,22 @@ public class DuplicateStack {
             System.out.println("Stack Overflow");
         } else {
             top++;
-            St[top] = val;
+            stack[top] = val;
         }
     }
 
     public static void display() {
         System.out.println("The elements in the stack are:");
         for (int j = top; j >= 0; j--) {
-            System.out.print(St[j] + " ");
+            System.out.print(stack[j] + " ");
         }
         System.out.println();
     }
 
     public static void duplicate(int[] stack) {
-        int[] dupStack = new int[max];
-
-        for (int j = top; j >= 0; j--) {
-            dupStack[j] = stack[j];
-        }
-
         System.out.println("\nDuplicate Stack is:");
         for (int i = top; i >= 0; i--) {
-            System.out.print(dupStack[i] + " ");
+            System.out.print(stack[i] + " ");
         }
     }
 }
