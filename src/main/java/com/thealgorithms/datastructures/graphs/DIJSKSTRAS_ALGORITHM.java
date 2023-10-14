65f01d346c055f63ae8528586ed4e56b98301e3d
SujalBagade <sujalbagade123@gmail.com>
2023-10-14 10:36:07 +0530

    Description

--- src/main/java/com/thealgorithms/datastructures/graphs/DIJSKSTRAS_ALGORITHM.java
+++ src/main/java/com/thealgorithms/datastructures/graphs/DIJSKSTRAS_ALGORITHM.java
@@ -1,4 +1,18 @@
 /*
+    This Java program implements Dijkstra's shortest path algorithm, which is a popular algorithm for finding the shortest paths between nodes in a graph, which may represent, for example, road networks. Here's a breakdown of the code:
+
+The class dijkstras is declared to contain the Dijkstra's algorithm implementation.
+The class has an instance variable k which is initialized to 9. This variable represents the number of vertices in the graph.
+The method minDist is used to find the vertex with the minimum distance value, from the set of vertices not yet included in the shortest path tree.
+The print method is used to print the distances from the source vertex to all other vertices.
+The dijkstra method is the main implementation of the Dijkstra's algorithm. It initializes an array dist to store the shortest distance from the source to each vertex and a Boolean array Set to keep track of vertices included in the shortest path tree.
+The provided graph is represented as a 2D array graph with 9 vertices. The adjacency matrix representation is used, where graph[i][j] represents the weight of the edge from vertex i to vertex j.
+The main method creates an instance of the dijkstras class and initializes a sample graph. It then calls the dijkstra method with the graph and a source vertex (in this case, the source vertex is 0).
+The expected output of the program is the shortest distances from the source vertex to all other vertices, as shown in the comment at the end of the code.
+
+This program can be useful for finding the shortest paths between nodes in various types of graphs, such as road networks, computer networks, and more. It provides a simple implementation of the Dijkstra's algorithm for educational purposes.
+*/
+/*
 Refer https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
 for better understanding
  */
