e0e4acf913d7583bc489a131e8a454c96fb1357e
Aditi Bansal <142652964+Aditi22Bansal@users.noreply.github.com>
2023-10-14 19:44:37 +0530

    Update IsPowerTwo.java

--- src/main/java/com/thealgorithms/bitmanipulation/IsPowerTwo.java
+++ src/main/java/com/thealgorithms/bitmanipulation/IsPowerTwo.java
@@ -6,6 +6,7 @@ package com.thealgorithms.bitmanipulation;
  */
 
 public class IsPowerTwo {
+    
     public static boolean isPowerTwo(int number) {
         if (number <= 0) {
             return false;
@@ -13,4 +14,13 @@ public class IsPowerTwo {
         int ans = number & (number - 1);
         return ans == 0;
     }
+
+//Adding the main function 
+public static void main(String[] args) {
+    //taking the entered list as example
+    int[] list_test1 = {1, 2, 3, 4, 24, 48};
+    for (int n : list_test1) {
+        System.out.println(n + " is power of 2: " + isPowerTwo(n));
+    }
+}
 }
