bdff59d122b1a1d15bd689f0e1995aed8870e24e
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-10-14 15:21:19 +0530

    Minor_fixes

--- src/main/java/com/thealgorithms/datastructures/stacks/Stack_using_queues.java
+++ src/main/java/com/thealgorithms/datastructures/stacks/Stack_using_queues.java
@@ -40,12 +40,11 @@ class Stack_using_queues {
         }
 
         static int top() {
-            if (q2.isEmpty())
-                return -1;
-            return q2.peek();
+            if (q2.isEmpty()) return -1;
+             return q2.peek();
         }
 
-        static int size() { 
+        static int size() {
             return q2.size(); 
         }
     }
@@ -54,33 +53,32 @@ class Stack_using_queues {
     public static void main(String[] args) {
         Stack stack = new Stack();
         Scanner in = new Scanner(System.in);
-        int choice,element;
+        int choice, element;
         System.out.println("Enter 1 to push,2 to pop ,3 to peek elements and 4 for the size and -1 to exit ");
-        choice=in.nextInt();
-        while(choice==1 || choice==2 || choice==3 || choice==4) {
-            switch(choice) {
-                case 1:
-                    System.out.println("Enter the element to be pushed:");
-                    element=in.nextInt();
-                    stack.push(element);
-                    break;
-                case 2:
-                    System.out.println("Poped element "+stack.pop());
-                    break;
-                case 3:
-                    System.out.println("Topmost element "+stack.top());
-                    break;
-                case 4:
-                    System.out.println("Size of the stack "+stack.size());
-                    break;
-                default:
-                    System.out.println("Enter correctly");
-                    
-            }
-            System.out.println("Enter 1 to push,2 to pop ,3 to peek elements and 4 for the size and -1 to exit ");
-            choice = in.nextInt();
-        }
-    }
-}
+        choice = in.nextInt();
+        while (choice == 1 || choice == 2 || choice == 3 || choice == 4) {
+            switch (choice) {
+            case 1:
+                System.out.println("Enter the element to be pushed:");
+                element = in.nextInt();
+                stack.push(element);
+                break;
+            case 2:
+                System.out.println("Poped element " + stack.pop());
+                break;
+            case 3:
+                System.out.println("Topmost element " + stack.top());
+                break;
+            case 4:
+                System.out.println("Size of the stack " + stack.size());
+                break;
+            default:
+                System.out.println("Enter correctly");
+             }
+             System.out.println("Enter 1 to push,2 to pop ,3 to peek elements and 4 for the size and -1 to exit ");
+             choice = in.nextInt();
+         }
+     }
+ }
 
 
