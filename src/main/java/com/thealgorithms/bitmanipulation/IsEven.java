9558bb63926a0e89344af4cbf6fcc955b172d24d
Astha167 <147143057+Astha167@users.noreply.github.com>
2023-10-14 20:11:05 +0530

    Update IsEven.java

--- src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
+++ src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
@@ -1,4 +1,4 @@
-package com.thealgorithms.bitmanipulation;
+//package com.thealgorithms.bitmanipulation;
 
 /**
  * Converts any Octal Number to a Binary Number
@@ -9,4 +9,18 @@ public class IsEven {
     public static boolean isEven(int number) {
         return (number & 1) == 0;
     }
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 10; // Replace 10 with the number you want to check
+        boolean res = IsEven.isEven(n);
+
+        if (res) {
+            System.out.println(n + " is even.");
+        } else {
+            System.out.println(n + " is odd.");
+        }
+    }
+}
+
 }
