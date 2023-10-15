9e70aa5c55fd26332adaccc206ceaf47f42725c5
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2023-10-15 09:01:31 +0200

    style: remove redundant blank line

--- src/main/java/com/thealgorithms/bitmanipulation/BitSwap.java
+++ src/main/java/com/thealgorithms/bitmanipulation/BitSwap.java
@@ -7,7 +7,6 @@ public final class BitSwap {
      * @brief Swaps the bits at the position posA and posB from data
      */
     public static int bitSwap(int data, final int posA, final int posB) {
-
         if (SingleBitOperations.getBit(data, posA) != SingleBitOperations.getBit(data, posB)) {
             data ^= (1 << posA) ^ (1 << posB);
         }
