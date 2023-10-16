3086bcacfa74ebedb28b01a00ccbb5594560e13c
Sumitha Nair <61596065+sumitha1998@users.noreply.github.com>
2023-10-16 11:34:33 +0530

    Create WordBreak.java

---
+++ src/main/java/com/thealgorithms/dynamicprogramming/WordBreak.java
@@ -0,0 +1,33 @@
+/**
+ * In this implementation, the wordBreak function takes a string s and a List of words wordDict as arguments.
+ * It returns a boolean value indicating whether the string can be segmented into words from the word dictionary.
+ */
+
+import java.util.*;
+
+public class WordBreak {
+  public static boolean wordBreak(String s, List<String> wordDict) {
+    int n = s.length();
+    boolean[] dp = new boolean[n + 1];
+    dp[0] = true;
+
+    for(int i = 1; i <= n; i++) {
+      for(int j = 0; j < i; j++) {
+        if(dp[j] && wordDict.contains(s.substring(j,i))){
+          dp[i] = true;
+          break;
+        }
+      }
+    }
+
+    return dp[n];
+  }
+
+  public static void main(String[] args){
+    String s =  "applepenapple";
+    List<String> wordDict = Arrays.asList("apple","pen");
+
+    boolean result = wordBreak(s, wordDict);
+    System.out.println("Can the string be segmented?" + result);
+  }
+}
