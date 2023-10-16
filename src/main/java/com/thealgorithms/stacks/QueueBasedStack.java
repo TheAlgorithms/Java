b38997c864d9d66f091f69406332d333bbd2e3a4
tarasimic <tarasimic42@gmail.com>
2023-10-16 13:34:36 +0200

    feat(stack): Implement queue-based stack

---
+++ src/main/java/com/thealgorithms/stacks/QueueBasedStack.java
@@ -0,0 +1,53 @@
+package com.thealgorithms.stacks;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueBasedStack {
+
+    private Queue<Object> queue1;
+    private Queue<Object> queue2;
+
+    public QueueBasedStack() {
+        this.queue1 = new LinkedList<>();
+        this.queue2 = new LinkedList<>();
+    }
+
+    public void push(Object element){
+        queue1.add(element);
+        while(!queue2.isEmpty()){
+            queue1.add(queue2.remove());
+        }
+
+        Queue<Object> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    public Object pop(){
+        if(queue2.isEmpty()){
+            return -1;
+        }else {
+            return queue2.remove();
+        }
+    }
+
+    public Object top(){
+        if(queue2.isEmpty()){
+            return -1;
+        }else {
+            return queue2.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        QueueBasedStack stack = new QueueBasedStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push("Juice");
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
