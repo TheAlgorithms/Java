7c0acf364c0b29ad7ef6bff4f75fd093f5a18444
Sumitha Nair <61596065+sumitha1998@users.noreply.github.com>
2023-10-16 11:49:17 +0530

    Update WordBreak.java

--- src/main/java/com/thealgorithms/dynamicprogramming/WordBreak.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/WordBreak.java
@@ -6,28 +6,28 @@
 import java.util.*;
 
 public class WordBreak {
-  public static boolean wordBreak(String s, List<String> wordDict) {
-    int n = s.length();
-    boolean[] dp = new boolean[n + 1];
-    dp[0] = true;
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
 
-    for(int i = 1; i <= n; i++) {
-      for(int j = 0; j < i; j++) {
-        if(dp[j] && wordDict.contains(s.substring(j,i))){
-          dp[i] = true;
-          break;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(dp[j] && wordDict.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
         }
-      }
+
+        return dp[n];
     }
 
-    return dp[n];
-  }
+    public static void main(String[] args){
+        String s =  "applepenapple";
+        List<String> wordDict = Arrays.asList("apple","pen");
 
-  public static void main(String[] args){
-    String s =  "applepenapple";
-    List<String> wordDict = Arrays.asList("apple","pen");
-
-    boolean result = wordBreak(s, wordDict);
-    System.out.println("Can the string be segmented?" + result);
-  }
+        boolean result = wordBreak(s, wordDict);
+        System.out.println("Can the string be segmented?" + result);
+    }
 }
