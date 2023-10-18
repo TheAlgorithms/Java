9c09a574a31e72c81da50f6bb9f0e8a60627e822
Pratham Kandari <prathamkandari123@gmail.com>
2023-10-18 23:05:58 +0530

    Sorting

---
+++ src/main/java/com/thealgorithms/sorts/Bubblesort2.java
@@ -0,0 +1,39 @@
+// Bubble sort in Java
+
+import java.util.Arrays;
+
+class Main {
+
+  // perform the bubble sort
+  static void bubbleSort(int array[]) {
+    int size = array.length;
+    
+    // loop to access each array element
+    for (int i = 0; i < size - 1; i++)
+    
+      // loop to compare array elements
+      for (int j = 0; j < size - i - 1; j++)
+
+        // compare two adjacent elements
+        // change > to < to sort in descending order
+        if (array[j] > array[j + 1]) {
+
+          // swapping occurs if elements
+          // are not in the intended order
+          int temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+  }
+
+  public static void main(String args[]) {
+      
+    int[] data = { -2, 45, 0, 11, -9 };
+    
+    // call method using class name
+    Main.bubbleSort(data);
+    
+    System.out.println("Sorted Array in Ascending Order:");
+    System.out.println(Arrays.toString(data));
+  }
+}
