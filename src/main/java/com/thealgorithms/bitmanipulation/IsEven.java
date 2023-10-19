3d851d4db0123c25e66a9b0e2999626930182aed
Aditi Bansal <142652964+Aditi22Bansal@users.noreply.github.com>
2023-10-19 19:59:38 +0530

    Update IsEven.java

--- src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
+++ src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
@@ -1,12 +1,20 @@
 package com.thealgorithms.bitmanipulation;
 
 /**
- * Converts any Octal Number to a Binary Number
+ * Checks whether a number is even
  * @author Bama Charan Chhandogi (https://github.com/BamaCharanChhandogi)
  */
 
 public class IsEven {
     public static boolean isEven(int number) {
         return (number & 1) == 0;
+    }
+public static void main(String[] args) {
+        int num = 42; // You can change this number to test other values
+        if (isEven(num)) {
+            System.out.println(num + " is even.");
+        } else {
+            System.out.println(num + " is odd.");
+        }
     }
 }
