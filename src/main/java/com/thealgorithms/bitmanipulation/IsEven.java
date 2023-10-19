547f4e39fc3b324b19f33cd4a76a76bc510a3b43
Aditi Bansal <142652964+Aditi22Bansal@users.noreply.github.com>
2023-10-19 23:27:45 +0530

    Update IsEven.java

--- src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
+++ src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
@@ -9,13 +9,4 @@ public class IsEven {
     public static boolean isEven(int number) {
         return (number & 1) == 0;
     }
-    public static void main(String[] args) {
-        int num = 42; // You can change this number to test other values
-        if (isEven(num)) {
-            System.out.println(num + " is even.");
-        } 
-        else {
-            System.out.println(num + " is odd.");
-        }
-    }
 }
