4388ac110210a300a6a0e810bc4e12d0094b0fda
sachin <sachin.baghel1234@gmail.com>
2023-10-21 04:12:41 +0530

    Formatting fix

--- src/test/java/com/thealgorithms/dynamicprogramming/WordBreakProblemTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/WordBreakProblemTest.java
@@ -20,14 +20,4 @@ public class WordBreakProblemTest {
         assertFalse(WordBreakProblem.wordBreak("i told you to not wait here", dictionaryList));
         assertFalse(WordBreakProblem.wordBreak("noproblem", dictionaryList));
     }
-
-    @Test
-    void testInputsFalse() {
-        String[] dictionary = {"wait", "here", "i", "told", "you", "to", " "};
-        List<String> dictionaryList = Arrays.asList(dictionary);
-        assertTrue(WordBreakProblem.wordBreak("hereyou", dictionaryList));
-        assertTrue(WordBreakProblem.wordBreak("i told you to wait here", dictionaryList));
-        assertFalse(WordBreakProblem.wordBreak("i told you to not wait here", dictionaryList));
-        assertFalse(WordBreakProblem.wordBreak("noproblem", dictionaryList));
-    }
 }
