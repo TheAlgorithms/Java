0683b53bc9025248bea77b479a601d2f1522ffd1
sachin <sachin.baghel1234@gmail.com>
2023-10-21 03:48:51 +0530

    Fixing Formatting

--- src/test/java/com/thealgorithms/dynamicprogramming/WordBreakProblemTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/WordBreakProblemTest.java
@@ -1,12 +1,12 @@
 package com.thealgorithms.dynamicprogramming;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Sachin Baghel (https://github.com/sachin10fi/)
@@ -22,4 +22,4 @@ class WordBreakProblemTest {
         assertFalse(WordBreakProblem.wordBreak("i told you to not wait here", dictionaryList));
         assertFalse(WordBreakProblem.wordBreak("noproblem", dictionaryList));
     }
-   }
+}
