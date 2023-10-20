4c2fc4a682e9d9133f50b686944fd5e4da35ba52
kbadal19 <k.badal19@gmail.com>
2023-10-20 19:40:25 +0530

    added flattening of nested list algorithm

--- src/main/java/com/thealgorithms/stacks/FlatteningNestedList.java
+++ src/main/java/com/thealgorithms/stacks/FlatteningNestedList.java
@@ -1,46 +1,17 @@
 package com.thealgorithms.stacks;
-
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FlatteningNestedList {
-    // This function takes a nested list and returns a flattened list.
     public static List<Object> flattenList(List<Object> nestedList) {
-        // Create a list to store the flattened elements.
         List<Object> flatList = new ArrayList<>();
-
-        // Create a stack to process elements.
-        Deque<Object> stack = new ArrayDeque<>();
-
-        // Push all elements of the input nested list onto the stack.
         for (Object item : nestedList) {
-            stack.push(item);
-        }
-
-        // Process elements on the stack until it's empty.
-        while (!stack.isEmpty()) {
-            // Pop the top element from the stack.
-            Object current = stack.pop();
-
-            // If the current element is a list, we further process it.
-            if (current instanceof List) {
-                // Cast it to a list and process its elements.
-                @SuppressWarnings("unchecked")
-                List<Object> sublist = (List<Object>) current;
-
-                // Push the elements of the sublist onto the stack for processing.
-                for (Object subItem : sublist) {
-                    stack.push(subItem);
-                }
+            if (item instanceof List) {
+                flatList.addAll(flattenList((List<Object>) item));
             } else {
-                // If the current element is not a list, add it to the flatList.
-                flatList.add(current);
+                flatList.add(item);
             }
         }
-
-        // Return the flattened list.
         return flatList;
     }
 }
