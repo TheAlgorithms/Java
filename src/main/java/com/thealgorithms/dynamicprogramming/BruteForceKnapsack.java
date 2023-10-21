1e9472b415b6840500063cac8843de047b874fcf
Pratyush Joshi <86593014+PratyushJoshi@users.noreply.github.com>
2023-10-22 00:41:48 +0530

    Update BruteForceKnapsack.java
    In this dynamic programming approach, we use a 2D array dp to store the maximum values for different subproblems, and we fill it in a bottom-up manner. This solution is more efficient and avoids recomputing the same subproblems, which is a significant improvement over the naive recursive solution.

--- src/main/java/com/thealgorithms/dynamicprogramming/BruteForceKnapsack.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/BruteForceKnapsack.java
@@ -1,29 +1,24 @@
 package com.thealgorithms.dynamicprogramming;
 
-/* A Naive recursive implementation
-of 0-1 Knapsack problem */
-public class BruteForceKnapsack {
-    // Returns the maximum value that
-    // can be put in a knapsack of
-    // capacity W
+public class DynamicProgrammingKnapsack {
+    // Returns the maximum value that can be put in a knapsack of capacity W
     static int knapSack(int W, int[] wt, int[] val, int n) {
-        // Base Case
-        if (n == 0 || W == 0) {
-            return 0;
+        int[][] dp = new int[n + 1][W + 1];
+
+        // Build the DP table
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
         }
 
-        // If weight of the nth item is
-        // more than Knapsack capacity W,
-        // then this item cannot be included
-        // in the optimal solution
-        if (wt[n - 1] > W) {
-            return knapSack(W, wt, val, n - 1);
-        } // Return the maximum of two cases:
-        // (1) nth item included
-        // (2) not included
-        else {
-            return Math.max(val[n - 1] + knapSack(W - wt[n - 1], wt, val, n - 1), knapSack(W, wt, val, n - 1));
-        }
+        return dp[n][W];
     }
 
     // Driver code
