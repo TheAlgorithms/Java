38b7d7352a638383d9cc139674bb57fc17836fff
Swarnim Shukla <swarnim0300@gmail.com>
2023-10-22 04:14:21 +0530

    Added New Problem Statement MaximumSumWithoutAdjacent

--- src/main/java/com/thealgorithms/dynamicprogramming/MaximumSumWithoutAdjacent.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/MaximumSumWithoutAdjacent.java
@@ -7,20 +7,20 @@ package com.thealgorithms.dynamicprogramming;
  */
 public class MaximumSumWithoutAdjacent {
     public static void main(String[] args) {
-        int[][] input = new int[][]{{1,2,4,3,2},{4,2,1,3,2}};
+        int[][] input = new int[][] {{1, 2, 4, 3, 2}, {4, 2, 1, 3, 2}};
         System.out.println(findMaxSumWithoutAdjacent(input));
     }
 
-    //We approach this problem by using dynamic programming where we are computing max
-    //sum till each column index in the dp array of length equal to number of columns.
-    // To avoid using extra memory we are reusing the first row of input 2D array.
+    // We approach this problem by using dynamic programming where we are computing max
+    // sum till each column index in the dp array of length equal to number of columns.
+    //  To avoid using extra memory we are reusing the first row of input 2D array.
     public static int findMaxSumWithoutAdjacent(int[][] input) {
-        //Without Using extra space
+        // Without Using extra space
         input[0][0] = Math.max(input[0][0], input[1][0]);
-        input[0][1] = Math.max(Math.max(input[0][1], input[1][1]),input[0][0]);
-        for(int i=2;i<input[0].length;i++){
-            input[0][i] = Math.max(Math.max(input[0][i], input[1][i])+ input[0][i-2], input[0][i-1]);
+        input[0][1] = Math.max(Math.max(input[0][1], input[1][1]), input[0][0]);
+        for (int i = 2; i < input[0].length; i++) {
+            input[0][i] = Math.max(Math.max(input[0][i], input[1][i]) + input[0][i - 2], input[0][i - 1]);
         }
-        return input[0][input[0].length-1];
+        return input[0][input[0].length - 1];
     }
 }
--- src/test/java/com/thealgorithms/dynamicprogramming/MaximumSumWithoutAdjacentTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/MaximumSumWithoutAdjacentTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 public class MaximumSumWithoutAdjacentTest {
     @Test
     public void testfindMaxSumWithoutAdjacent() {
-        int[][] sampleInput = new int[][]{{1, 2, 4, 3, 2}, {4, 2, 1, 3, 2}};
+        int[][] sampleInput = new int[][] {{1, 2, 4, 3, 2}, {4, 2, 1, 3, 2}};
         assert MaximumSumWithoutAdjacent.findMaxSumWithoutAdjacent(sampleInput) == 10;
     }
 }
