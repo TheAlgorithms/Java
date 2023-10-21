011f3ba0a35f8cfaebb471fc0aad0f62c0f25f1d
Arjun Gupta <146109559+Arjun8124@users.noreply.github.com>
2023-10-21 15:38:08 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class arrays{
+  public static void main(String[] args){
+    System.out.println("Enter your numbers to be added in list:-);
+    Scanner read=new Scanner (System.in);
+    int []lst=new int[10];
+    for(int i=0;i<lst.length;i++){
+      lst[i]=read.nextInt();
+    }
+    int j=0;
+    while(j<lst.length){
+      System.out.print(lst[i]+"\t");
+      j++;
+    }
+  }
+}
