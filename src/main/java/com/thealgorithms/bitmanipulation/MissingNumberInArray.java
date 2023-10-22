1767158201e5803c3dd808a9ea4aafe4117ccec2
Yashjain1602 <yasjain201@gmail.com>
2023-10-22 15:52:07 +0530

    Final commit

--- src/main/java/com/thealgorithms/bitmanipulation/MissingNumberInArray.java
+++ src/main/java/com/thealgorithms/bitmanipulation/MissingNumberInArray.java
@@ -5,12 +5,12 @@ package com.thealgorithms.bitmanipulation;
  * @author Yash Jain (https://github.com/Yashjain1602)
  */
 
- public class MissingNumberInArray {
+public class MissingNumberInArray {
     public static int findMissingNumber(int[] nums) {
         int n = nums.length;
         int temp1 = nums[0];
         int temp2 = 1;
-        
+    
         for (int i = 1; i < n; i++) {
             temp1 ^= nums[i];
         }
@@ -18,5 +18,5 @@ package com.thealgorithms.bitmanipulation;
             temp2 ^= i;
         }
         return (temp1 ^ temp2);
-}
+    }
 }
