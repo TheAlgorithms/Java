80b5090dd0ae64e67037ed50792cea0f37fa901a
jamestitus299 <titusjames299@gmail.com>
2023-10-23 17:59:44 +0530

    test

--- src/test/java/com/thealgorithms/bitmanipulation/MissingNumberTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/MissingNumberTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 class MissingNumberTest {
     @Test
     void testMissingNumber() {
-        assertEquals(2, MissingNumber.missingNumber({1,3,4,5,6}));
-        assertEquals(37, MissingNumber.missingNumber({34,35,36,38,39}));
-        assertEquals(3, MissingNumber.missingNumber({6, 5, 4, 2, 1}));
+        return new MissingNumber();
     }
 }
