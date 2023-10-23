dc35d6fb501a619ab9720560bde77b4844c86583
YuLuo <1481556636@qq.com>
2023-10-23 22:55:08 +0800

    fix: fix code lint

--- src/main/java/com/thealgorithms/maths/SquareRootWithDichotomy.java
+++ src/main/java/com/thealgorithms/maths/SquareRootWithDichotomy.java
@@ -8,32 +8,32 @@ package com.thealgorithms.maths;
 
 public class SquareRootWithDichotomy {
 
-	public static void main(String[] args) {
-		assert squareRootWithDichotomy(4) == Math.sqrt(4);
-		System.out.println(squareRootWithDichotomy(4) + " " + (int) Math.sqrt(4));
-		assert squareRootWithDichotomy(8) == Math.sqrt(8);
-		System.out.println(squareRootWithDichotomy(8) + " " + (int) Math.sqrt(8));
-	}
+    public static void main(String[] args) {
+        assert squareRootWithDichotomy(4) == Math.sqrt(4);
+        System.out.println(squareRootWithDichotomy(4) + " " + (int) Math.sqrt(4));
+        assert squareRootWithDichotomy(8) == Math.sqrt(8);
+        System.out.println(squareRootWithDichotomy(8) + " " + (int) Math.sqrt(8));
+    }
 
-	public static int squareRootWithDichotomy(int x) {
+    public static int squareRootWithDichotomy(int x) {
 
-		if (x == 0) {
-			return 0;
-		}
+        if (x == 0) {
+            return 0;
+        }
 
-		int left = 0, right = x, res = -1;
+        int left = 0, right = x, res = -1;
 
-		while (left <= right) {
-			int mid = left + (right - left) / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-			if ((long) mid * mid <= x) {
-				res = mid;
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
+            if ((long) mid * mid <= x) {
+                res = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
 
-		return res;
-	}
+        return res;
+    }
 }
