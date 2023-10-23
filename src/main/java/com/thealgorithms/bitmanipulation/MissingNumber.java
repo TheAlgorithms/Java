ca2988d8a57adef7960d515d72ac19085dfa46ff
YASH JAIN <94926833+Yashjain1602@users.noreply.github.com>
2023-10-23 19:44:40 +0530

    MissingNumber.java

--- src/main/java/com/thealgorithms/bitmanipulation/MissingNumber.java
+++ src/main/java/com/thealgorithms/bitmanipulation/MissingNumber.java
@@ -8,7 +8,9 @@ package com.thealgorithms.bitmanipulation;
 public class MissingNumber {
     public static int findMissingNumber(int[] nums) {
         int n = nums.length;
+        // For xor of all the elements in array
         int temp1 = nums[0];
+        // For xor of all the elements from 1 to n+1
         int temp2 = 1;
         for (int i = 1; i < n; i++) {
             temp1 ^= nums[i];
