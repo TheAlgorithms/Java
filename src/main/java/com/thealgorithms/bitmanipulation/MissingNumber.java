d0082f74a2dd75f1c874cc0bf5d4fe03fca57ff9
YASH JAIN <94926833+Yashjain1602@users.noreply.github.com>
2023-10-23 19:58:48 +0530

    MissingNumber.java
    References :  https://www.geeksforgeeks.org/bits-manipulation-important-tactics/

--- src/main/java/com/thealgorithms/bitmanipulation/MissingNumber.java
+++ src/main/java/com/thealgorithms/bitmanipulation/MissingNumber.java
@@ -12,9 +12,11 @@ public class MissingNumber {
         int temp1 = nums[0];
         // For xor of all the elements from 1 to n+1
         int temp2 = 1;
+        // For xor of all the elements in array
         for (int i = 1; i < n; i++) {
             temp1 ^= nums[i];
         }
+        // For xor of all the elements from 2 to n+1
         for (int i = 2; i <= n + 1; i++) {
             temp2 ^= i;
         }
