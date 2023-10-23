4551d4469b7bf415a07fe14851bd136eef26dfcf
Gopal0Gupta <114791914+Gopal0Gupta@users.noreply.github.com>
2023-10-23 07:16:00 +0530

    Update BinaryExponentiation.java

--- src/main/java/com/thealgorithms/divideandconquer/BinaryExponentiation.java
+++ src/main/java/com/thealgorithms/divideandconquer/BinaryExponentiation.java
@@ -12,28 +12,41 @@ package com.thealgorithms.divideandconquer;
 
 public class BinaryExponentiation {
 
-    // recursive function to calculate a to the power of b
-    public static long calculatePower(long x, long y) {
-        if (y == 0) {
+    // Recursive function to calculate a number raised to a power
+    public static long calculatePower(long base, long exponent) {
+        // Base case: If the exponent is 0, return 1.
+        if (exponent == 0) {
             return 1;
         }
-        long val = calculatePower(x, y / 2);
-        if (y % 2 == 0) {
-            return val * val;
+
+        // Divide the exponent by 2 and calculate the result for half of it.
+        long resultHalf = calculatePower(base, exponent / 2);
+
+        // If the exponent is even, multiply the result by itself.
+        if (exponent % 2 == 0) {
+            return resultHalf * resultHalf;
+        } else {
+            // If the exponent is odd, multiply the result by itself and the base.
+            return resultHalf * resultHalf * base;
         }
-        return val * val * x;
     }
 
-    // iterative function to calculate a to the power of b
-    long power(long N, long M) {
-        long power = N, sum = 1;
-        while (M > 0) {
-            if ((M & 1) == 1) {
-                sum *= power;
+    // Iterative function to calculate a number raised to a power
+    long power(long base, long exponent) {
+        long result = 1;
+        long baseValue = base;
+
+        while (exponent > 0) {
+            // Check if the least significant bit of the exponent is 1.
+            if (exponent % 2 == 1) {
+                result *= baseValue;
             }
-            power = power * power;
-            M = M >> 1;
+
+            // Square the base value and halve the exponent.
+            baseValue *= baseValue;
+            exponent /= 2;
         }
-        return sum;
+
+        return result;
     }
 }
