f9720f9b0fcfaf3cf9089ac59f1bf93eaa727bbb
Sooraj-Mlahi <92318624+Sooraj-Mlahi@users.noreply.github.com>
2023-10-24 14:00:04 +0500

    Update Pow.java
    Modified the pow method in the Pow class to handle double type for the base and integer type for the exponent. Added special handling for the case where the base is 0 and the exponent is a positive integer, ensuring that the result is always 0. Also added additional test cases to validate the updated implementation.

--- src/main/java/com/thealgorithms/maths/Pow.java
+++ src/main/java/com/thealgorithms/maths/Pow.java
@@ -8,21 +8,31 @@ public class Pow {
         assert pow(0, 2) == Math.pow(0, 2); // == 0
         assert pow(2, 10) == Math.pow(2, 10); // == 1024
         assert pow(10, 2) == Math.pow(10, 2); // == 100
+
+        // Additional test cases
+        assert pow(2.00000, 10) == 1024.00000;
+        assert pow(2.10000, 3) == 9.26100;
+        assert pow(0, 5) == 0; // Special case: 0^b where b is positive should always be 0
     }
 
     /**
      * Returns the value of the first argument raised to the power of the second
-     * argument
+     * argument.
      *
      * @param a the base.
      * @param b the exponent.
      * @return the value {@code a}<sup>{@code b}</sup>.
      */
-    public static long pow(int a, int b) {
-        long result = 1;
-        for (int i = 1; i <= b; i++) {
+    public static double pow(double a, int b) {
+        // Special case: 0^b where b is positive should always be 0
+        if (a == 0 && b > 0) {
+            return 0;
+        }
+
+        double result = 1;
+        for (int i = 0; i < Math.abs(b); i++) {
             result *= a;
         }
-        return result;
+        return b >= 0 ? result : 1 / result;
     }
 }
