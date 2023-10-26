fb1016b620eabfabf93fbde342f0d1d96d26e4fc
Tushar2001bbdu <146729736+Tushar2001bbdu@users.noreply.github.com>
2023-10-26 23:31:51 +0530

    Update mergesort.java

--- mergesort.java
+++ mergesort.java
@@ -1,59 +1,54 @@
 import java.util.Arrays;
 
-public class mergesort {
-    public static void main(String args[])
-    {
-        int[] arr={3,4,5,6,8,12};
-        int[] ar=mergesort.mergeSort(arr);
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 5, 6, 8, 12};
+        int[] ar = mergeSort(arr);
         System.out.println(Arrays.toString(ar));
     }
-    static int[] mergeSort(int[] arr)
-    {
-        if(arr.length==1)
-        {
+
+    static int[] mergeSort(int[] arr) {
+        if (arr.length <= 1) {
             return arr;
         }
 
-            else{int mid=(arr.length)/2;
-            int[] arr1 = mergeSort(Arrays.copyOfRange(arr,0,mid));
-            int[] arr2 = mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
-            return merge(arr1,arr2);}
+        int mid = arr.length / 2;
+        int[] arr1 = Arrays.copyOfRange(arr, 0, mid);
+        int[] arr2 = Arrays.copyOfRange(arr, mid, arr.length);
 
+        arr1 = mergeSort(arr1);
+        arr2 = mergeSort(arr2);
+
+        return merge(arr1, arr2);
     }
-    static int[] merge(int[] first,int[] second)
-    {
-        int[] res=new int[first.length+second.length];
-        int i=0,j=0,k=0;
-        while(i<first.length  && j<second.length )
-        {
-            if(first[i]<second[j])
-            {
-             res[k]=first[i];
 
-             i++;
+    static int[] merge(int[] first, int[] second) {
+        int[] res = new int[first.length + second.length];
+        int i = 0, j = 0, k = 0;
 
-            }
-            else
-            {
-                res[k]=second[j];
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                res[k] = first[i];
+                i++;
+            } else {
+                res[k] = second[j];
                 j++;
             }
             k++;
         }
-        while(i<first.length)
-        {
-            res[k]=first[i];
+
+        while (i < first.length) {
+            res[k] = first[i];
             i++;
             k++;
         }
-        while(j<second.length)
-        {
-            res[k]=second[j];
 
-
-                    j++;
+        while (j < second.length) {
+            res[k] = second[j];
+            j++;
             k++;
         }
-      return res;
+
+        return res;
     }
 }
