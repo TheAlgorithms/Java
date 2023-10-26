a257bc796b42f3144ae7fa448fdad1916983a692
Yusen Nian <u7572670@anu.edu.au>
2023-10-26 19:17:40 +0800

    Implementation of merge sort using java #4466

--- src/main/java/com/thealgorithms/datastructures/heaps/HeapSort.java
+++ src/main/java/com/thealgorithms/datastructures/heaps/HeapSort.java
@@ -12,14 +12,13 @@ import java.util.Arrays;
 public class HeapSort {
 
     public HeapSort() {
-
     }
 
-    public static void heapSort(int testArr[]){
-        for(int i = testArr.length / 2 - 1; i >= 0 ; i--){
+    public static void heapSort(int testArr[]) {
+        for (int i = testArr.length / 2 - 1; i >= 0; i--) {
             fixHeap(testArr, i, testArr.length);
         }
-        for(int j = testArr.length - 1; j > 0; j--){
+        for (int j = testArr.length - 1; j > 0; j--) {
             int temp = testArr[0];
             testArr[0] = testArr[j];
             testArr[j] = temp;
@@ -28,16 +27,16 @@ public class HeapSort {
         System.out.println(Arrays.toString(testArr));
     }
 
-    public static void fixHeap(int testArr[], int i, int length){
+    public static void fixHeap(int testArr[], int i, int length) {
         int temp = testArr[i];
-        for(int k = 2 * i + 1; k < length; k = 2 * k + 1){
-            if(k + 1 < length && testArr[k + 1] > testArr[k]){
+        for (int k = 2 * i + 1; k < length; k = 2 * k + 1) {
+            if (k + 1 < length && testArr[k + 1] > testArr[k]) {
                 k++;
             }
-            if(testArr[k] > temp){
+            if (testArr[k] > temp) {
                 testArr[i] = testArr[k];
                 i = k;
-            }else{
+            } else {
                 break;
             }
         }
