81d4b1b1d304763ddf1d361d6ad85e96807194c0
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-10-27 20:06:52 +0530

    Created a function to convert uppercase

---
+++ src/test/java/com/thealgorithms/strings/wordUpperCase.java
@@ -0,0 +1,27 @@
+public class WordUpperCase {
+
+    public static String toUpperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "hii, i am abhishek .";
+
+        System.out.println(toUpperCase(str));
+    }
+}
