d27519d674b47acbf6fe183c06b5aad82e92d264
Yashjain1602 <yasjain201@gmail.com>
2023-10-28 11:02:30 +0530

    OptimalBst

--- src/main/java/com/thealgorithms/dynamicprogramming/OptimalBinarySearchTree.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/OptimalBinarySearchTree.java
@@ -14,17 +14,14 @@ public class OptimalBinarySearchTree {
     // Helper function to calculate the sum of frequencies from index i to j
     static int Sum(int freq[], int i, int j) {
         int s = 0;
-        for (int k = i; k <= j; k++)
-            s += freq[k];
+        for (int k = i; k <= j; k++) s += freq[k];
         return s;
     }
 
     // Recursive function to find the optimal cost of a BST using memoization
     static int optCostMemoized(int freq[], int i, int j) {
-        if (i < 0 || j < 0)
-            return 0;
-        if (cost[i][j] != 0)
-            return cost[i][j];
+        if (i < 0 || j < 0) return 0;
+        if (cost[i][j] != 0) return cost[i][j];
         int fsum = Sum(freq, i, j);
         int Min = Integer.MAX_VALUE;
         for (int r = i; r <= j; r++) {
