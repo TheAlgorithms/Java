ca1198aac6c3aa75c6becd9aa7f463c420742afd
Yashjain1602 <yasjain201@gmail.com>
2023-10-28 12:02:49 +0530

    sudoku

--- src/main/java/com/thealgorithms/maths/CheckValidityOfSudokuBoardConfiguration.java
+++ src/main/java/com/thealgorithms/maths/CheckValidityOfSudokuBoardConfiguration.java
@@ -78,7 +78,6 @@ public class CheckValidityOfSudokuBoardConfiguration {
     public static void main(String[] args) {
         char[][] board = new char[][] {{'5', '3', '.', '.', '7', '.', '.', '.', '.'}, {'6', '.', '.', '1', '9', '5', '.', '.', '.'}, {'.', '9', '8', '.', '.', '.', '.', '6', '.'}, {'8', '.', '.', '.', '6', '.', '.', '.', '3'}, {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
             {'7', '.', '.', '.', '2', '.', '.', '.', '6'}, {'.', '6', '.', '.', '.', '.', '2', '8', '.'}, {'.', '.', '.', '4', '1', '9', '.', '.', '5'}, {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
- 
         // Function call to check if the configuration is valid
         System.out.println((isValidConfig(board, 9) ? "YES" : "NO"));
     }
