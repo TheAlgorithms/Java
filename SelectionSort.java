b1136b731598a60edb85435a83879af6f1ed3ec3
Shiv Prakash Singh <144921828+shivprakashsinghjii@users.noreply.github.com>
2023-10-29 11:28:33 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,27 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the element at index i
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        System.out.println("Sorted array:");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+}
