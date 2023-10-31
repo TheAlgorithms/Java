30761e2d9ffdcc82fb335db800720f3c379f36c6
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-10-31 22:42:49 +0530

    updated the code in test case

--- src/main/java/com/thealgorithms/strings/WordUpperCase.java
+++ src/main/java/com/thealgorithms/strings/WordUpperCase.java
@@ -1,23 +1,26 @@
 package com.thealgorithms.strings;
 
 public class WordUpperCase {
-    public static String toUpperCase(String s) {
-        if (s == null || s.isEmpty()) {
-            return s;
+    public static void main(String[] args) {
+        String str = "hii my name is abhishek";
+        System.out.println(toUpperCase(str));
+    }
+
+    public static String toUpperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+        if (str == null || str.isEmpty()) {
+            return str; // Handle null or empty input gracefully
         }
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
 
-        StringBuilder sb = new StringBuilder();
-        boolean capitalizeNext = true;
-
-        for (char c : s.toCharArray()) {
-            if (Character.isWhitespace(c)) {
-                capitalizeNext = true;
-                sb.append(c);
-            } else if (capitalizeNext) {
-                sb.append(Character.toUpperCase(c));
-                capitalizeNext = false;
             } else {
-                sb.append(c);
+                sb.append(str.charAt(i));
             }
         }
         return sb.toString();
