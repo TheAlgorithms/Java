8a11fd01acc54a14e98df4b4a8373db989c735d3
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-10-31 10:40:26 +0530

    added test case

---
+++ src/main/java/com/thealgorithms/strings/WordUpperCase.java
@@ -0,0 +1,29 @@
+package com.thealgorithms.strings;
+
+public class WordUpperCase {
+    public static void main(String[] args) {
+        String str = "hii my name is abhishek";
+        System.out.println(toUpperCase(str));
+    }
+
+
+    public static String toUpperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+    
+}
