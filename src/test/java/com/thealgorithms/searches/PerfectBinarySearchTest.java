527ce5f23153b60d5acb6d818ce75ca862760a16
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-10-31 22:27:17 +0530

    update the code

--- src/test/java/com/thealgorithms/searches/PerfectBinarySearchTest.java
+++ src/test/java/com/thealgorithms/searches/PerfectBinarySearchTest.java
@@ -1,6 +1,7 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.thealgorithms.searches;
 
-import com.thealgorithms.searches.PerfectBinarySearch;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 /**
