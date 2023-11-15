33d5a8e8e566ded6da98f81e4fbc838f937927ba
sricharan200 <148656478+sricharan200@users.noreply.github.com>
2023-11-15 17:30:37 -0500

    Update StackUsingQueues.java

--- src/main/java/com/thealgorithms/stacks/StackUsingQueues.java
+++ src/main/java/com/thealgorithms/stacks/StackUsingQueues.java
@@ -1,107 +1,50 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-public class StackUsingQueues {
-    public static void main(String[] args) {
-        StackQueues stack = new StackQueues();
+public class StackUsingQueuesTest {
+    private StackUsingQueues.StackQueues stack;
 
-        // push operation
+    @Before
+    public void setUp() {
+        stack = new StackUsingQueues.StackQueues();
+    }
+
+    @Test
+    public void testPushAndTop() {
         stack.push(5);
         stack.push(2);
         stack.push(9);
 
-        // top operation
-        System.out.println("Top element: " + stack.top()); 
-
-        // pop operation
-        System.out.println("Popped element: " + stack.pop()); 
-
-        // top element
-        System.out.println("Current top element: " + stack.top()); 
-        
-        System.out.println("\n");
-        
-         // push operation
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-
-        // top operation
-        System.out.println("Top element: " + stack.top()); 
-
-        // pop operation
-        System.out.println("Popped element: " + stack.pop()); 
-
-        // top element
-        System.out.println("Current top element: " + stack.top()); 
-        
-        System.out.println("\n");
-        
-         // push operation
-        stack.push(4);
-        stack.push(7);
-        stack.push(1);
-
-        // top operation
-        System.out.println("Top element: " + stack.top()); 
-
-        // pop operation
-        System.out.println("Popped element: " + stack.pop()); 
-
-        // top element
-        System.out.println("Current top element: " + stack.top()); 
-        
-        
+        assertEquals(9, stack.top());
     }
 
-    static class StackQueues {
-        private Queue<Integer> q1;
-        private Queue<Integer> q2;
+    @Test
+    public void testPushAndPop() {
+        stack.push(5);
+        stack.push(2);
+        stack.push(9);
 
-        public StackQueues() {
-            q1 = new LinkedList<>();
-            q2 = new LinkedList<>();
-        }
+        assertEquals(9, stack.pop());
+        assertEquals(2, stack.pop());
+    }
 
-        // Push operation
-        public void push(int x) {
-            q1.add(x);
-        }
+    @Test
+    public void testIsEmpty() {
+        assertTrue(stack.isEmpty());
 
-        // Pop operation
-        public int pop() {
-            if (isEmpty()) {
-                throw new IllegalStateException("Stack is empty");
-            }
-            while (q1.size() > 1) {
-                q2.add(q1.remove());
-            }
-            int popValue = q1.remove();
-            Queue<Integer> temp = q1;
-            q1 = q2;
-            q2 = temp;
-            return popValue;
-        }
+        stack.push(5);
 
-        // Top operation
-        public int top() {
-            if (isEmpty()) {
-                throw new IllegalStateException("Stack is empty");
-            }
-            while (q1.size() > 1) {
-                q2.add(q1.remove());
-            }
-            int top_res = q1.peek();
-            q2.add(q1.remove());
-            Queue<Integer> temp = q1;
-            q1 = q2;
-            q2 = temp;
-            return top_res;
-        }
+        assertFalse(stack.isEmpty());
+    }
 
-        // Check if stack is empty
-        public boolean isEmpty() {
-            return q1.isEmpty() && q2.isEmpty();
-        }
+    @Test
+    public void testPopOnEmptyStack() {
+        stack.pop();
+    }
+
+    @Test
+    public void testTopOnEmptyStack() {
+        stack.top();
     }
 }
