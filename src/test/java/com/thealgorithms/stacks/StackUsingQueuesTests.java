eb10feb70f950194f7ee36b95138932a5022fd87
sricharan200 <148656478+sricharan200@users.noreply.github.com>
2023-11-16 18:22:28 -0500

    Create StackUsingQueuesTests.java

---
+++ src/test/java/com/thealgorithms/stacks/StackUsingQueuesTests.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StackUsingQueuesTest {
+    
+    private StackUsingQueues stack;
+
+    @Before
+    public void setUp() {
+        stack = new StackUsingQueues();
+    }
+
+    @Test
+    public void testPushAndTop() {
+        stack.push(1);
+        stack.push(2);
+        assertEquals("Top element should be 2 after pushing 1 and 2", 2, stack.top());
+    }
+
+    @Test
+    public void testPop() {
+        stack.push(1);
+        stack.push(2);
+        assertEquals("Popped element should be 2", 2, stack.pop());
+        assertEquals("Top element should now be 1", 1, stack.top());
+    }
+
+    @Test
+    public void testEmpty() {
+        assertTrue("Stack should be empty initially", stack.empty());
+        stack.push(1);
+        assertFalse("Stack should not be empty after pushing an element", stack.empty());
+        stack.pop();
+        assertTrue("Stack should be empty after popping the only element", stack.empty());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testPopEmptyStack() {
+        stack.pop();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testTopEmptyStack() {
+        stack.top();
+    }
+}
