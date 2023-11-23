bcf8dbe368a636c0f6dec7be2b403ce6bf8edaa1
itakurah <122729995+itakurah@users.noreply.github.com>
2023-11-23 15:01:20 +0100

    clang format

--- src/test/java/com/thealgorithms/datastructures/crdt/GCounterTest.java
+++ src/test/java/com/thealgorithms/datastructures/crdt/GCounterTest.java
@@ -1,8 +1,8 @@
 package com.thealgorithms.datastructures.crdt;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GCounterTest {
     @Test
