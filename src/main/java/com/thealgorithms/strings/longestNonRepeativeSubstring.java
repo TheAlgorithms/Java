4212caf1f55c88152cf8b954cd5ab47c82cd52f0
Shaikismail3233 <153603834+Shaikismail3233@users.noreply.github.com>
2023-12-21 11:50:49 +0530

    Update longestNonRepeativeSubstring.java

--- src/main/java/com/thealgorithms/strings/longestNonRepeativeSubstring.java
+++ src/main/java/com/thealgorithms/strings/longestNonRepeativeSubstring.java
@@ -1,41 +1,56 @@
-package com.thealgorithms.strings;
+package nonrepetivesubstring;
 
 import java.util.HashMap;
 
-class longestNonRepeativeSubstring {
+public class Substring {
 
-    public static int lengthOfLongestSubstring(String s) {
-        int max = 0, start = 0, i = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
+	public static int main(String[] args) {
+		// TODO Auto-generated method stub
+		String tr="wassidssapi";
+		int start=0,asl=0,i=0;
+		
+		HashMap<Character,Integer> storage = new HashMap<>();
+		
+		while(i< tr.length())
+			{
+			char temp= tr.charAt(i);
+			
+			if(!storage.containsKey(temp)) storage.put(temp,0);
+			
+			else if(tr.charAt(start)== temp) start++;
+			
+			else if(tr.charAt(i-1) == temp) {
+			if (asl< storage.size()) {
+				asl= storage.size();
+				storage= new HashMap<>();
+				start=i;
+				i--;}
+			else
+			{
+			if(asl<storage.size()) asl= storage.size(); 
+			
+			while(tr.charAt(start)!= temp) {
+				storage.remove(tr.charAt(start));
+				start++;
+			}start++;
+			}
+			i++;
+						
+				
+			}
+				
+			}
+			
+		
+	
+			
+			
+			
+			
+			
+			if (asl<storage.size()) asl=storage.size();
+			return asl;
 
-        while (i < s.length()) {
-            char temp = s.charAt(i);
+		}	}
 
-            // adding key to map if not present
-            if (!map.containsKey(temp)) map.put(temp, 0);
-            // checking if the first value is the dublicate value
-            else if (s.charAt(start) == temp)
-                start++;
-            // checking if the previous value is dublicate value
-            else if (s.charAt(i - 1) == temp) {
-                if (max < map.size()) max = map.size();
-                map = new HashMap<>();
-                start = i;
-                i--;
-            }
-            // last possible place where dublicate value can be is between start and i
-            else {
-                if (max < map.size()) max = map.size();
-                while (s.charAt(start) != temp) {
-                    map.remove(s.charAt(start));
-                    start++;
-                }
-                start++;
-            }
-
-            i++;
-        }
-        if (max < map.size()) max = map.size();
-        return max;
-    }
 }
