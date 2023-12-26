d6eaf075f3832b229c37dd09d2a58e17a82e4a94
AthinaSw <swingathina@gmail.com>
2023-12-26 14:14:44 +0200

    Requested changes on PR #4984 : Parameterized tests

--- src/test/java/com/thealgorithms/maths/AutoCorrelationTest.java
+++ src/test/java/com/thealgorithms/maths/AutoCorrelationTest.java
@@ -1,27 +1,36 @@
 package com.thealgorithms.maths;
 
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 
 /**
  * Test class for AutoCorrelation class
  *
  * @author Athina-Frederiki Swinkels
- * @version 1.0
+ * @version 2.0
  */
+
 public class AutoCorrelationTest {
 
-    @Test
-    void testAutoCorrelation(){
-        double[] x={1,2,1,1};
-        double[] y={1,2,3,4,5};
-        double[] xx={1,3,5,7,5,3,1};
-        double[] yy={5,14,26,40,55,40,26,14,5};
+    @ParameterizedTest
+    @CsvSource({"1;2;1;1, 1;3;5;7;5;3;1", "1;2;3, 3;8;14;8;3", "1.5;2.3;3.1;4.2, 6.3;14.31;23.6;34.79;23.6;14.31;6.3"})
 
-        assertArrayEquals(xx,AutoCorrelation.autoCorrelation(x));
-        assertArrayEquals(yy,AutoCorrelation.autoCorrelation(y));
+    public void testAutoCorrelationParameterized(String input, String expected) {
+        double[] array = convertStringToArray(input);
+        double[] expectedResult = convertStringToArray(expected);
+
+        double[] result = AutoCorrelation.autoCorrelation(array);
+
+        assertArrayEquals(expectedResult, result, 0.0001);
     }
 
+    private double[] convertStringToArray(String input) {
+        String[] elements = input.split(";");
+        double[] result = new double[elements.length];
+        for (int i = 0; i < elements.length; i++) {
+            result[i] = Double.parseDouble(elements[i]);
+        }
+        return result;
+    }
 }
--- src/test/java/com/thealgorithms/maths/CrossCorrelationTest.java
+++ src/test/java/com/thealgorithms/maths/CrossCorrelationTest.java
@@ -1,30 +1,36 @@
 package com.thealgorithms.maths;
 
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 
 /**
  * Test class for CrossCorrelation class
  *
  * @author Athina-Frederiki Swinkels
- * @version 1.0
+ * @version 2.0
  */
 public class CrossCorrelationTest {
 
-    @Test
-    void testCrossCorrelation(){
-        double[] x={1,2,1,1};
-        double[] y={1,1,2,1};
-        double[] x_y={1,4,6,6,5,2,1};
-        double[] x1={1,2,3};
-        double[] y1={1,2,3,4,5};
-        double[] x1_y1={5,14,26,20,14,8,3};
-        double[] y1_x1={3,8,14,20,26,14,5};
-        assertArrayEquals(x_y,CrossCorrelation.crossCorrelation(x,y));
-        assertArrayEquals(x1_y1,CrossCorrelation.crossCorrelation(x1,y1));
-        assertArrayEquals(y1_x1,CrossCorrelation.crossCorrelation(y1,x1));
+    @ParameterizedTest
+    @CsvSource({"1;2;1;1, 1;1;2;1, 1;4;6;6;5;2;1", "1;2;3, 1;2;3;4;5, 5;14;26;20;14;8;3", "1;2;3;4;5, 1;2;3, 3;8;14;20;26;14;5", "1.5;2.3;3.1;4.2, 1.1;2.2;3.3, 4.95;10.89;16.94;23.21;12.65;4.62"})
+
+    public void testCrossCorrelationParameterized(String input1, String input2, String expected) {
+        double[] array1 = convertStringToArray(input1);
+        double[] array2 = convertStringToArray(input2);
+        double[] expectedResult = convertStringToArray(expected);
+
+        double[] result = CrossCorrelation.crossCorrelation(array1, array2);
+
+        assertArrayEquals(expectedResult, result, 0.0001);
     }
 
+    private double[] convertStringToArray(String input) {
+        String[] elements = input.split(";");
+        double[] result = new double[elements.length];
+        for (int i = 0; i < elements.length; i++) {
+            result[i] = Double.parseDouble(elements[i]);
+        }
+        return result;
+    }
 }
