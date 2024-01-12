b7c73561ad19dcc7b4db4cb34ce5ec0fb4fe1cb4
Sarthak Chaudhary <86872379+SarthakChaudhary46@users.noreply.github.com>
2024-01-12 23:34:40 +0530

    Create ArrayRightRotationTest.java

---
+++ src/test/java/com/thealgorithms/others/ArrayRightRotationTest.java
@@ -0,0 +1,17 @@
+package com.thealgorithms.others;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ArrayRightRotationTest {
+
+    @Test
+    void testArrayRightRotation() {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        int[] expected = {5, 6, 7, 1, 2, 3, 4};
+        int[] result = ArrayRightRotation.rotateRight(arr, k);
+        assertArrayEquals(expected, result);
+    }
+}
