037fc3a65d7fff3995b6a02a490454fe3b68f20f
Nikos Strafiotis <nstrafio@csd.auth.gr>
2024-01-31 20:46:15 +0200

    7th attempt

--- src/main/java/com/thealgorithms/datastructures/graphs/Welsh_Powell.java
+++ src/main/java/com/thealgorithms/datastructures/graphs/Welsh_Powell.java
@@ -106,5 +106,4 @@ public class Welsh_Powell {
             return colors; // Return the array of colors for each vertex
         }
     }
-
 }
--- src/test/java/com/thealgorithms/datastructures/graphs/WelshPowellAlgorithmTest.java
+++ src/test/java/com/thealgorithms/datastructures/graphs/WelshPowellAlgorithmTest.java
@@ -3,43 +3,42 @@ package com.thealgorithms.datastructures.graphs;
 import com.thealgorithms.datastructures.graphs.Welsh_Powell.WPGraph;
 public class WelshPowellAlgorithmTest {
 
-        public static void main(String[] args) {
-            // Creating the graph for the test case
-            int numVertices = 6; // Number of vertices
-            WPGraph graph = new WPGraph(numVertices);
+    public static void main(String[] args) {
+        // Creating the graph for the test case
+        int numVertices = 6; // Number of vertices
+        WPGraph graph = new WPGraph(numVertices);
 
-            // Adding the edges
-            graph.addEdge(0, 1);
-            graph.addEdge(0, 2);
-            graph.addEdge(1, 3);
-            graph.addEdge(2, 3);
-            graph.addEdge(2, 4);
-            graph.addEdge(3, 4);
-            graph.addEdge(3, 5);
-            graph.addEdge(4, 5);
+        // Adding the edges
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.addEdge(3, 4);
+        graph.addEdge(3, 5);
+        graph.addEdge(4, 5);
 
-            // Executing the method
-            int[] colors = graph.welshPowellColoring();
+        // Executing the method
+        int[] colors = graph.welshPowellColoring();
 
-            // Checking the results
-            boolean testPassed = true;
-            for (int i = 0; i < numVertices; i++) {
-                for (int neighbor : graph.adjLists[i]) {
-                    if (colors[i] == colors[neighbor]) {
-                        testPassed = false;
-                        break;
-                    }
-                }
-                if (!testPassed) {
+        // Checking the results
+        boolean testPassed = true;
+        for (int i = 0; i < numVertices; i++) {
+            for (int neighbor : graph.adjLists[i]) {
+                if (colors[i] == colors[neighbor]) {
+                    testPassed = false;
                     break;
                 }
             }
-
-            if (testPassed) {
-                System.out.println("Test Passed.");
-            } else {
-                System.out.println("Test Failed.");
+            if (!testPassed) {
+                break;
             }
         }
 
+        if (testPassed) {
+            System.out.println("Test Passed.");
+        } else {
+            System.out.println("Test Failed.");
+        }
+    }
 }
