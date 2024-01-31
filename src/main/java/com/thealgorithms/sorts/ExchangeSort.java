65c70a5ec0a07d546b5684d90776cd9e11ad1108
vedant kasar <vedantvvk5@gmail.com>
2024-02-01 01:40:28 +0530

    added documentaion of algoritum

--- src/main/java/com/thealgorithms/sorts/ExchangeSort.java
+++ src/main/java/com/thealgorithms/sorts/ExchangeSort.java
@@ -3,10 +3,23 @@ package com.thealgorithms.sorts;
 import static com.thealgorithms.sorts.SortUtils.*;
 
 /**
- * Exchange Sort (Cocktail Shaker Sort) implementation.
+ * ExchangeSort is an implementation of the Exchange Sort algorithm.
+ *
+ * <p>
+ * Exchange sort works by comparing each element with all subsequent elements,
+ * swapping where needed, to ensure the correct placement of each element
+ * in the final sorted order. It iteratively performs this process for each
+ * element in the array. While it lacks the advantage of bubble sort in
+ * detecting sorted lists in one pass, it can be more efficient than bubble sort
+ * due to a constant factor (one less pass over the data to be sorted; half as
+ * many total comparisons) in worst-case scenarios.
+ * </p>
+ *
+ * <p>
+ * Reference: https://en.wikipedia.org/wiki/Sorting_algorithm#Exchange_sort
+ * </p>
  *
  * @author 555vedant (Vedant Kasar)
- * @see SortAlgorithm
  */
 class ExchangeSort implements SortAlgorithm {
 
@@ -22,18 +35,28 @@ class ExchangeSort implements SortAlgorithm {
         int n = array.length;
 
         for (int i = 0; i < n - 1; i++) {
-            // Last i elements are already sorted, so we don't need to check them
-            for (int j = 0; j < n - i - 1; j++) {
-                // Swap if the element found is greater than the next element
-                if (array[j].compareTo(array[j + 1]) > 0) {
-                    // Swap array[j] and array[j+1]
-                    T temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+            for (int j = i + 1; j < n; j++) {
+                if (array[i].compareTo(array[j]) > 0) {
+                    // Use a generic swap method
+                    swap(array, i, j);
                 }
             }
         }
 
-        return array; // Return the sorted array
+        return array;
+    }
+
+    /**
+     * Generic swap method to swap elements at given indices in an array.
+     *
+     * @param array the array in which elements are to be swapped.
+     * @param i     the index of the first element.
+     * @param j     the index of the second element.
+     * @param <T>   the type of elements in the array.
+     */
+    private <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
