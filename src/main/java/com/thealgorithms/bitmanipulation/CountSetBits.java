09aa914bb6ad87dc3b561b42622c699e617f2dad
Pankaj Kumar Bind <73558583+Pankaj-Bind@users.noreply.github.com>
2024-03-01 22:21:08 +0530

    Update CountSetBits.java

--- src/main/java/com/thealgorithms/bitmanipulation/CountSetBits.java
+++ src/main/java/com/thealgorithms/bitmanipulation/CountSetBits.java
@@ -4,11 +4,7 @@ package com.thealgorithms.bitmanipulation;
 public final class CountSetBits {
     private CountSetBits() {
     }
-
-    /*
-    * CountSetBits class provides a method to count the number of set bits (1s) in an integer.
-    * Implementation by Pankaj Kumar Bind (https://github.com/Pankaj-Bind).
-    */
+    
     public static int countSetBits(int n) {
         int count = 0;
         while (n > 0) {
