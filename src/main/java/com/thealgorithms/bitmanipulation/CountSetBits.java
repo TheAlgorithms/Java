642d1daffff62c71d48e02087d81e7957f7b5b56
Pankaj Kumar Bind <73558583+Pankaj-Bind@users.noreply.github.com>
2024-03-01 22:24:56 +0530

    Update CountSetBits.java

--- src/main/java/com/thealgorithms/bitmanipulation/CountSetBits.java
+++ src/main/java/com/thealgorithms/bitmanipulation/CountSetBits.java
@@ -4,7 +4,6 @@ package com.thealgorithms.bitmanipulation;
 public final class CountSetBits {
     private CountSetBits() {
     }
-    
     public static int countSetBits(int n) {
         int count = 0;
         while (n > 0) {
