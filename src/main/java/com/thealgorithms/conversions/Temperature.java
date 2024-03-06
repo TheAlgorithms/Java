c8a5733e2b2a9d9848ca83f2a23dc40e65f17e3f
Mark Myers <markmyersCS@gmail.com>
2024-03-06 18:46:14 -0500

    #5042. Added temperature conversions

---
+++ src/main/java/com/thealgorithms/conversions/Temperature.java
@@ -0,0 +1,74 @@
+package com.thealgorithms.conversions;
+
+/**
+ * Converts temperature from one scale to another
+ *
+ * @author Mark Myers
+ */
+public class Temperature {
+
+    //For non-scientific cases these approximations are accurate enough.
+    double nineDivideByFive = (double) 9 / 5;
+    double fiveDivideByNine = (double) 5 / 9;
+
+    /**
+     * Converts from Celsius to Fahrenheit scales
+     *
+     * @param Celsius: the temperature in Celsius
+     * @return temperature in Fahrenheit
+     */
+    double CelsiusToFahrenheit(double Celsius) {
+        return (Celsius * nineDivideByFive) + 32;
+    }
+
+    /**
+     * Converts from Fahrenheit to Kelvin scales
+     *
+     * @param Celsius: the temperature in Celsius
+     * @return temperature in Kelvin
+     */
+    double CelsiusToKelvin(double Celsius) {
+        return Celsius + 273.15;
+    }
+
+    /**
+     * Converts from Fahrenheit to Celsius scales
+     *
+     * @param Fahrenheit: the temperature in Fahrenheit
+     * @return temperature in Celsius
+     */
+    double FahrenheitToCelsius(double Fahrenheit) {
+        return ((Fahrenheit - 32) * fiveDivideByNine);
+    }
+
+    /**
+     * Converts from Fahrenheit to Kelvin scales
+     *
+     * @param Fahrenheit: the temperature in Fahrenheit
+     * @return temperature in Kelvin
+     */
+    double FahrenheitToKelvin(double Fahrenheit) {
+        return ((Fahrenheit - 32) * fiveDivideByNine + 273.15);
+    }
+
+    /**
+     * Converts from Kelvin to Fahrenheit scales
+     *
+     * @param Kelvin: the temperature in Kelvin
+     * @return temperature in Celsius
+     */
+    double KelvinToCelsius(double Kelvin) {
+        return Kelvin - 273.15;
+    }
+
+    /**
+     * Converts from Kelvin to Fahrenheit scales
+     *
+     * @param Kelvin: the temperature in Kelvin
+     * @return temperature in Kelvin
+     */
+    double KelvinToFahrenheit(double Kelvin) {
+        return ((Kelvin - 273.15) * nineDivideByFive + 32);
+    }
+
+}
---
+++ src/test/java/com/thealgorithms/conversions/TemperatureTest.java
@@ -0,0 +1,40 @@
+package com.thealgorithms.conversions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TemperatureTest {
+    Temperature temperature = new Temperature();
+
+    @Test
+    public void VerifyTemperatureConversionsZero() {
+        assertEquals(32.0, temperature.CelsiusToFahrenheit(0.0), "VerifyTemperatureConversionsZero: Temperature from Celsius To Fahrenheit incorrect");
+        assertEquals(273.15, temperature.CelsiusToKelvin(0.0), "VerifyTemperatureConversionsZero: Temperature from Celsius To Kelvin incorrect");
+        assertEquals(-17.77, temperature.FahrenheitToCelsius(0.0), 0.01, "VerifyTemperatureConversionsZero: Temperature from Fahrenheit To Celsius incorrect");
+        assertEquals(255.37, temperature.FahrenheitToKelvin(0.0), 0.01, "VerifyTemperatureConversionsZero: Temperature from Fahrenheit to Kelvin incorrect");
+        assertEquals(-273.15, temperature.KelvinToCelsius(0.0), "VerifyTemperatureConversionsZero: Temperature from Kelvin to Fahrenheit incorrect");
+        assertEquals(-459.67, temperature.KelvinToFahrenheit(0.0), 0.01, "VerifyTemperatureConversionsZero: Temperature from Kelvin to Fahrenheit incorrect");
+    }
+
+    @Test
+    //At -40 degrees Fahrenheit and Celsius are the same
+    public void VerifyTemperatureConversionsNeg40() {
+        assertEquals(-40.0, temperature.CelsiusToFahrenheit(-40.0), "VerifyTemperatureConversionsNeg40: Temperature from Celsius To Fahrenheit incorrect");
+        assertEquals(233.15, temperature.CelsiusToKelvin(-40.0), 0.01, "VerifyTemperatureConversionsNeg40: Temperature from Celsius To Kelvin incorrect");
+        assertEquals(-40.0, temperature.FahrenheitToCelsius(-40.0), 0.01, "VerifyTemperatureConversionsNeg40: Temperature from Fahrenheit To Celsius incorrect");
+        assertEquals(233.15, temperature.FahrenheitToKelvin(-40.0), 0.01, "VerifyTemperatureConversionsNeg40: Temperature from Fahrenheit to Kelvin incorrect");
+        assertEquals(-313.15, temperature.KelvinToCelsius(-40.0), "VerifyTemperatureConversionsNeg40: Temperature from Kelvin to Fahrenheit incorrect");
+        assertEquals(-531.67, temperature.KelvinToFahrenheit(-40.0), 0.01, "VerifyTemperatureConversionsNeg40: Temperature from Kelvin to Fahrenheit incorrect");
+    }
+
+    @Test
+    public void VerifyTemperatureConversionsPlainValue() {
+        assertEquals(33.8, temperature.CelsiusToFahrenheit(1.0), "VerifyTemperatureConversionsPlainValue: Temperature from Celsius To Fahrenheit incorrect");
+        assertEquals(275.15, temperature.CelsiusToKelvin(2.0), "VerifyTemperatureConversionsPlainValue: Temperature from Celsius To Kelvin incorrect");
+        assertEquals(-16.11, temperature.FahrenheitToCelsius(3.0), 0.01, "VerifyTemperatureConversionsPlainValue: Temperature from Fahrenheit To Celsius incorrect");
+        assertEquals(257.59, temperature.FahrenheitToKelvin(4.0), 0.01, "VerifyTemperatureConversionsPlainValue:Temperature from Fahrenheit to Kelvin incorrect");
+        assertEquals(-268.15, temperature.KelvinToCelsius(5.0), "VerifyTemperatureConversionsPlainValue:Temperature from Kelvin to Fahrenheit incorrect");
+        assertEquals(-448.87, temperature.KelvinToFahrenheit(6.0), 0.01, "VerifyTemperatureConversionsPlainValue: Temperature from Kelvin to Fahrenheit incorrect");
+    }
+}
