2da17e5d01e8b376b54a917d73e69a9bea60c054
Mark Myers <markmyersCS@gmail.com>
2024-03-06 19:20:09 -0500

    #5042. addressed pull request requirements (fixed imports)

--- src/test/java/com/thealgorithms/conversions/TemperatureTest.java
+++ src/test/java/com/thealgorithms/conversions/TemperatureTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class TemperatureTest {
     Temperature temperature = new Temperature();
 
