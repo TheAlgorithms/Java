642812d27e21c7dd0a5d88b195b4ef4ccd992ea3
Lucas <100660343+lulunac27a@users.noreply.github.com>
2024-03-29 22:24:54 -0500

    Create ModularExponentiation.java
    Add modular exponentiation in Java

---
+++ src/main/java/com/thealgorithms/maths/ModularExponentiation.java
@@ -0,0 +1,27 @@
+package com.thealgorithms.maths;
+public class ModularExponentiation {
+  /**
+   * Calculate modular exponentiation, exponentiation with modulus
+   * @param x base number
+   * @param y exponent
+   * @param m modulus
+   * @return (x^y)%m
+   */
+  public int modExp(int x, int y, int m) {
+    int result = 1;
+    if (m < 1) {
+      throw new IllegalArgumentException("Modulus must be positive integer");
+    }
+    if (x == 0 || m == 1) {
+      return 0;
+    }
+    while (y > 0) {
+      if ((y & 1) == 1) {
+        result = (result * x) % m;
+      }
+      y = y >> 1;
+      x = (x * x) % m;
+    }
+    return result;
+  }
+}
