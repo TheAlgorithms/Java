ecb239b76c21bdefe9fe82c169b582c5b0be2da0
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-04-03 19:45:01 -0500

    Format code and change function name to `ModPow`

--- src/test/java/com/thealgorithms/maths/ModularExponentiationTest.java
+++ src/test/java/com/thealgorithms/maths/ModularExponentiationTest.java
@@ -5,19 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 public class ModularExponentiationTest {
-  @Test
-  public void testModExp() {
-    assertEquals(3, ModularExponentiation.modExp(2, 3, 5));
-    assertEquals(9, ModularExponentiation.modExp(3, 6, 20));
-    assertEquals(1, ModularExponentiation.modExp(5, 0, 3));
-    assertEquals(0, ModularExponentiation.modExp(0, 2, 5));
-    assertEquals(1, ModularExponentiation.modExp(1, 100, 25));
-    IllegalArgumentException exception =
-        assertThrows(IllegalArgumentException.class,
-                     () -> ModularExponentiation.modExp(4, 10, -15));
-    assertEquals("Modulus must be positive integer", exception.getMessage());
-    IllegalArgumentException exception2 =
-        assertThrows(IllegalArgumentException.class,
-                     () -> ModularExponentiation.modExp(5, -3, 12));
-  }
+    @Test
+    public void testModPow() {
+        assertEquals(3, ModularExponentiation.modPow(2, 3, 5));
+        assertEquals(9, ModularExponentiation.modPow(3, 6, 20));
+        assertEquals(1, ModularExponentiation.modPow(5, 0, 3));
+        assertEquals(0, ModularExponentiation.modPow(0, 2, 5));
+        assertEquals(1, ModularExponentiation.modPow(1, 100, 25));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> ModularExponentiation.modPow(4, 10, -15));
+        assertEquals("Modulus must be positive integer", exception.getMessage());
+        IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class, () -> ModularExponentiation.modPow(5, -3, 12));
+    }
 }
