a7bf0a7f3e33cff27df80f004833309e1beb1e05
Maria Paszkiewicz SCC <maria.paszkiewicz@kit.edu>
2024-04-21 20:55:48 +0200

    Remove unused import.

--- src/test/java/com/thealgorithms/maths/NumberOfDigitsTest.java
+++ src/test/java/com/thealgorithms/maths/NumberOfDigitsTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.function.IntFunction;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
