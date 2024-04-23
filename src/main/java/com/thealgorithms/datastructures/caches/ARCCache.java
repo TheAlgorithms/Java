4d5a3963049e67980b4a1c7e2b36037b6118c5e4
Adarsh Pandey <165834547+devxadarsh@users.noreply.github.com>
2024-04-24 00:28:50 +0530

    Update ARCCache.java
    Main Method Removed for ARCCache.java

--- src/main/java/com/thealgorithms/datastructures/caches/ARCCache.java
+++ src/main/java/com/thealgorithms/datastructures/caches/ARCCache.java
@@ -68,36 +68,4 @@ public class ARCCache<K, V> {
             totalCount--;
         }
     }
-
-    public static void main(String[] args) {
-
-        // Defining capacity of cache Using Map
-        ARCCache<String, Integer> cache = new ARCCache<>(5);
-        cache.put("A", 1);
-        cache.put("B", 2);
-        cache.put("C", 3);
-        cache.put("D", 4);
-        cache.put("E", 5);
-
-        // Printing Cache contents using enhanced for loop
-        System.out.println("Cache contents:");
-        for (Map.Entry<String, Integer> entry : cache.cache.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-
-        // Accessing an existing key
-        System.out.println("Value of key 'B': " + cache.get("B"));
-
-        // Adding a new key
-        cache.put("F", 6);
-
-        // Printing Cache contents after adding Key using enhanced for loop
-        System.out.println("Cache contents after adding key 'F':");
-        for (Map.Entry<String, Integer> entry : cache.cache.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
-
-        // Accessing a non existing key
-        System.out.println("Value of key 'A': " + cache.get("A"));
-    }
 }
