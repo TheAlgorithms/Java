0bf5319c5e7aed54389e21867b8b3103a980f290
rozi26 <76447029+rozi26@users.noreply.github.com>
2024-05-02 15:56:57 +0300

    make the code ready for pull request

--- src/main/java/com/thealgorithms/maths/ComplexNumberUtil.java
+++ src/main/java/com/thealgorithms/maths/ComplexNumberUtil.java
@@ -263,15 +263,4 @@ public class ComplexNumberUtil {
 
         return multiply(divide(MINUS_I, TWO), ln(divide(add(num, PLUS_I), subtract(num, PLUS_I))));
     }
-
-    public ComplexNumber add(ComplexNumber added)
-    {
-        return new ComplexNumber(1,1);
-    }
-
-    public static void main(String[] args)
-    {
-        ComplexNumber num1 = new ComplexNumber(1,1);
-        ComplexNumber num2 =  new ComplexNumber(2,3);
-    }
 }
