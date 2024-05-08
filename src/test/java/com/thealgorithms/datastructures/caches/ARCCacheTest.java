04be24359d971a97136fbf1966a51ce33fca900f
devxadarsh <devxadarsh@gmail.com>
2024-05-08 13:24:50 +0530

    Updated Imports

--- src/test/java/com/thealgorithms/datastructures/caches/ARCCacheTest.java
+++ src/test/java/com/thealgorithms/datastructures/caches/ARCCacheTest.java
@@ -1,6 +1,7 @@
 package com.thealgorithms.datastructures.caches;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
