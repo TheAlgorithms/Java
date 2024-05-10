5603bf74eeafa2d9dacba4e438001aeda3d901f2
vil02 <vil02@o2.pl>
2024-05-10 21:56:35 +0200

    style: make `SubsetCount` a proper utility

--- src/test/java/com/thealgorithms/dynamicprogramming/SubsetCountTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/SubsetCountTest.java
@@ -5,27 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class SubsetCountTest {
-    public static SubsetCount obj = new SubsetCount();
-
     @Test
     void hasMultipleSubset() {
         int[] arr = new int[] {1, 2, 3, 3};
-        assertEquals(3, obj.getCount(arr, 6));
+        assertEquals(3, SubsetCount.getCount(arr, 6));
     }
     @Test
     void singleElementSubset() {
         int[] arr = new int[] {1, 1, 1, 1};
-        assertEquals(4, obj.getCount(arr, 1));
+        assertEquals(4, SubsetCount.getCount(arr, 1));
     }
 
     @Test
     void hasMultipleSubsetSO() {
         int[] arr = new int[] {1, 2, 3, 3};
-        assertEquals(3, obj.getCountSO(arr, 6));
+        assertEquals(3, SubsetCount.getCountSO(arr, 6));
     }
     @Test
     void singleSubsetSO() {
         int[] arr = new int[] {1, 1, 1, 1};
-        assertEquals(1, obj.getCountSO(arr, 4));
+        assertEquals(1, SubsetCount.getCountSO(arr, 4));
     }
 }
