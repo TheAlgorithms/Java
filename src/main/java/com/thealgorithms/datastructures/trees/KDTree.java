d58489aeb775acc0a0c0f8e0ca4248b16033e7f7
C9 <chrisgodswill115@gmail.com>
2024-05-12 03:08:32 -0700

    style: enable EqualsHashCode in checkstyle

--- src/main/java/com/thealgorithms/datastructures/trees/KDTree.java
+++ src/main/java/com/thealgorithms/datastructures/trees/KDTree.java
@@ -75,7 +75,6 @@ public class KDTree {
         @Override
         public boolean equals(Object obj) {
             if (obj instanceof Point other) {
-                if (other.getDimension() != this.getDimension()) return false;
                 return Arrays.equals(other.coordinates, this.coordinates);
             }
             return false;
