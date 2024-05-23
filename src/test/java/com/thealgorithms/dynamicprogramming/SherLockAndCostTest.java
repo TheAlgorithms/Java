b53f161bb65eb1e04322b4bfeff522fd2415828f
Goutham <ggowthamsankar@gmail.com>
2024-05-23 10:33:54 +0530

    Update the formatting SherLockAndCost.java & SherLockAndCostTest.java

--- src/test/java/com/thealgorithms/dynamicprogramming/SherLockAndCostTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/SherLockAndCostTest.java
@@ -10,51 +10,51 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SherLockAndCostTest {
 
-    @Test
-    public void testPositiveIntegers() {
-        List<Integer> inputList = Arrays.asList(1, 2, 3, 4, 5);
-        int result = SherLockAndCost.sherlockAndCostProblem(inputList);
-        assertEquals(8, result);
-    }
+	@Test
+	public void testPositiveIntegers() {
+		List<Integer> inputList = Arrays.asList(1, 2, 3, 4, 5);
+		int result = SherLockAndCost.sherlockAndCostProblem(inputList);
+		assertEquals(8, result);
+	}
 
-    @Test
-    public void testNegativeInteger() {
-        List<Integer> list = Arrays.asList(-1, -2, -3, -4, -5);
-        int result = SherLockAndCost.sherlockAndCostProblem(list);
-        assertEquals(0, result);
-    }
+	@Test
+	public void testNegativeInteger() {
+		List<Integer> list = Arrays.asList(-1, -2, -3, -4, -5);
+		int result = SherLockAndCost.sherlockAndCostProblem(list);
+		assertEquals(0, result);
+	}
 
-    @Test
-    public void testMixedElements() {
-        List<Integer> list = Arrays.asList(-1, 1);
-        int result = SherLockAndCost.sherlockAndCostProblem(list);
-        assertEquals(0, result);
-    }
+	@Test
+	public void testMixedElements() {
+		List<Integer> list = Arrays.asList(-1, 1);
+		int result = SherLockAndCost.sherlockAndCostProblem(list);
+		assertEquals(0, result);
+	}
 
-    @Test
-    public void testEdge() {
-        List<Integer> list = Arrays.asList(-1, 2, 3, 4, 5);
-        int result = SherLockAndCost.sherlockAndCostProblem(list);
-        assertEquals(8, result);
-    }
+	@Test
+	public void testEdge() {
+		List<Integer> list = Arrays.asList(-1, 2, 3, 4, 5);
+		int result = SherLockAndCost.sherlockAndCostProblem(list);
+		assertEquals(8, result);
+	}
 
-    @Test
-    public void testNullElements() {
-        List<Integer> list = Arrays.asList(null, null, null, null);
-        assertThrows(NullPointerException.class, () -> SherLockAndCost.sherlockAndCostProblem(list));
-    }
+	@Test
+	public void testNullElements() {
+		List<Integer> list = Arrays.asList(null, null, null, null);
+		assertThrows(NullPointerException.class, () -> SherLockAndCost.sherlockAndCostProblem(list));
+	}
 
-    @Test
-    public void testEmptyList() {
-        List<Integer> list = Arrays.asList();
-        int result = SherLockAndCost.sherlockAndCostProblem(list);
-        assertEquals(0, result);
-    }
+	@Test
+	public void testEmptyList() {
+		List<Integer> list = Arrays.asList();
+		int result = SherLockAndCost.sherlockAndCostProblem(list);
+		assertEquals(0, result);
+	}
 
-    @Test
-    public void testSingleFList() {
-        List<Integer> list = Arrays.asList(500);
-        int result = SherLockAndCost.sherlockAndCostProblem(list);
-        assertEquals(0, result);
-    }
+	@Test
+	public void testSingleFList() {
+		List<Integer> list = Arrays.asList(500);
+		int result = SherLockAndCost.sherlockAndCostProblem(list);
+		assertEquals(0, result);
+	}
 }
