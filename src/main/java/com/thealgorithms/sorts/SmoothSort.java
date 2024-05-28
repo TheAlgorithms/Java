9e5cb32e09a902b237f3789a260240e5e9a7d603
JAPNITSINGH <truejswalia@gmail.com>
2024-05-28 14:29:18 +0530

    Adding Skeleton

---
+++ src/main/java/com/thealgorithms/sorts/SmoothSort.java
@@ -0,0 +1,11 @@
+package com.thealgorithms.sorts;
+
+public class SmoothSort implements SortAlgorithm {
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(T[] unsorted) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sort'");
+    }
+    
+}
