8a668ce3a164af888e3d0a45c3951f844c2222d1
cpu-pixel <utkarshsax2004@gmail.com>
2024-05-29 21:45:35 +0530

    style: enable MemberName in checkstyle

--- src/test/java/com/thealgorithms/divideandconquer/StrassenMatrixMultiplicationTest.java
+++ src/test/java/com/thealgorithms/divideandconquer/StrassenMatrixMultiplicationTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 class StrassenMatrixMultiplicationTest {
 
-    StrassenMatrixMultiplication SMM = new StrassenMatrixMultiplication();
+    StrassenMatrixMultiplication smm = new StrassenMatrixMultiplication();
 
     // Strassen Matrix Multiplication can only be allplied to matrices of size 2^n
     // and has to be a Square Matrix
@@ -16,7 +16,7 @@ class StrassenMatrixMultiplicationTest {
         int[][] a = {{1, 2}, {3, 4}};
         int[][] b = {{5, 6}, {7, 8}};
         int[][] expResult = {{19, 22}, {43, 50}};
-        int[][] actResult = SMM.multiply(a, b);
+        int[][] actResult = smm.multiply(a, b);
         assertArrayEquals(expResult, actResult);
     }
 
@@ -25,7 +25,7 @@ class StrassenMatrixMultiplicationTest {
         int[][] a = {{1, 2, 5, 4}, {9, 3, 0, 6}, {4, 6, 3, 1}, {0, 2, 0, 6}};
         int[][] b = {{1, 0, 4, 1}, {1, 2, 0, 2}, {0, 3, 1, 3}, {1, 8, 1, 2}};
         int[][] expResult = {{7, 51, 13, 28}, {18, 54, 42, 27}, {11, 29, 20, 27}, {8, 52, 6, 16}};
-        int[][] actResult = SMM.multiply(a, b);
+        int[][] actResult = smm.multiply(a, b);
         assertArrayEquals(expResult, actResult);
     }
 
@@ -35,7 +35,7 @@ class StrassenMatrixMultiplicationTest {
         int[][] a = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
         int[][] b = {{1, -2, -3, 4}, {4, -3, -2, 1}, {5, -6, -7, 8}, {8, -7, -6, -5}};
         int[][] expResult = {{56, -54, -52, 10}, {128, -126, -124, 42}, {200, -198, -196, 74}, {272, -270, -268, 106}};
-        int[][] actResult = SMM.multiply(a, b);
+        int[][] actResult = smm.multiply(a, b);
         assertArrayEquals(expResult, actResult);
     }
 }
