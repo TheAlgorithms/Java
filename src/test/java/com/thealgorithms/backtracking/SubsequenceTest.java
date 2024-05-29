822a69489c164786fe7dbac5d5b9dcb4a70c3f09
alxkm <>
2024-05-29 20:06:48 +0300

    Fix test data format

--- src/test/java/com/thealgorithms/backtracking/SubsequenceTest.java
+++ src/test/java/com/thealgorithms/backtracking/SubsequenceTest.java
@@ -1,11 +1,11 @@
 package com.thealgorithms.backtracking;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 import java.util.ArrayList;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import org.junit.jupiter.api.Test;
 
 public class SubsequenceTest {
 
@@ -18,27 +18,14 @@ public class SubsequenceTest {
 
     @Test
     void testLengthOfTwo() {
-        List<List<Integer>> expected = List.of(
-                List.of(),
-                List.of(2),
-                List.of(1),
-                List.of(1, 2)
-        );
+        List<List<Integer>> expected = List.of(List.of(), List.of(2), List.of(1), List.of(1, 2));
         List<List<Object>> actual = Subsequence.generateAllSubsequences(List.of(1, 2));
         assertIterableEquals(expected, actual);
     }
 
     @Test
     void testLengthOfThree() {
-        List<List<String>> expected = List.of(
-                List.of(),
-                List.of("C"),
-                List.of("B"),
-                List.of("B", "C"),
-                List.of("A"),
-                List.of("A", "C"),
-                List.of("A", "B"),
-                List.of("A", "B", "C")
+        List<List<String>> expected = List.of(List.of(), List.of("C"), List.of("B"), List.of("B", "C"), List.of("A"), List.of("A", "C"), List.of("A", "B"), List.of("A", "B", "C")
         );
         List<List<Object>> actual = Subsequence.generateAllSubsequences(List.of("A", "B", "C"));
         assertIterableEquals(expected, actual);
@@ -46,15 +33,7 @@ public class SubsequenceTest {
 
     @Test
     void testLengthOfThreeInteger() {
-        List<List<Integer>> expected = List.of(
-                List.of(),
-                List.of(3),
-                List.of(2),
-                List.of(2, 3),
-                List.of(1),
-                List.of(1, 3),
-                List.of(1, 2),
-                List.of(1, 2, 3)
+        List<List<Integer>> expected = List.of(List.of(), List.of(3), List.of(2), List.of(2, 3), List.of(1), List.of(1, 3), List.of(1, 2), List.of(1, 2, 3)
         );
         List<List<Object>> actual = Subsequence.generateAllSubsequences(List.of(1, 2, 3));
         assertIterableEquals(expected, actual);
