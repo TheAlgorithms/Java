6ffdc6b2bbfc22fc4a5cad19ef9d601ca7ef8041
alxkm <>
2024-05-30 12:48:39 +0300

    Fix formatting

--- src/test/java/com/thealgorithms/backtracking/SubsequenceTest.java
+++ src/test/java/com/thealgorithms/backtracking/SubsequenceTest.java
@@ -20,8 +20,8 @@ public class SubsequenceTest {
 
     static Stream<SubsequenceTestData> getSubsequenceTestData() {
         return Stream.of(new SubsequenceTestData(new ArrayList<>(), List.of(List.of())), new SubsequenceTestData(List.of(1, 2), List.of(List.of(), List.of(2), List.of(1), List.of(1, 2))),
-                new SubsequenceTestData(List.of("A", "B", "C"), List.of(List.of(), List.of("C"), List.of("B"), List.of("B", "C"), List.of("A"), List.of("A", "C"), List.of("A", "B"), List.of("A", "B", "C"))),
-                new SubsequenceTestData(List.of(1, 2, 3), List.of(List.of(), List.of(3), List.of(2), List.of(2, 3), List.of(1), List.of(1, 3), List.of(1, 2), List.of(1, 2, 3))));
+            new SubsequenceTestData(List.of("A", "B", "C"), List.of(List.of(), List.of("C"), List.of("B"), List.of("B", "C"), List.of("A"), List.of("A", "C"), List.of("A", "B"), List.of("A", "B", "C"))),
+            new SubsequenceTestData(List.of(1, 2, 3), List.of(List.of(), List.of(3), List.of(2), List.of(2, 3), List.of(1), List.of(1, 3), List.of(1, 2), List.of(1, 2, 3))));
     }
 
     record SubsequenceTestData(List<Object> input, List<List<Object>> expected) {
