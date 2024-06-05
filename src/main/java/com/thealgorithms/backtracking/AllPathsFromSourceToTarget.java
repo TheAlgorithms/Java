400c8a2d70536adcc43695ac22186d28255eecc8
JeysiStrok <joseluisherrero96@gmail.com>
2024-06-05 15:38:20 +0200

    Cleanup class AllPathsFromSourceToTarget

--- src/main/java/com/thealgorithms/backtracking/AllPathsFromSourceToTarget.java
+++ src/main/java/com/thealgorithms/backtracking/AllPathsFromSourceToTarget.java
@@ -3,25 +3,32 @@
  * Github : https://github.com/siddhant2002
  */
 
-/** Program description - To find all possible paths from source to destination*/
-
-/**Wikipedia link -> https://en.wikipedia.org/wiki/Shortest_path_problem */
+/** Program description - To find all possible paths from source to destination */
+/** Wikipedia link -> https://en.wikipedia.org/wiki/Shortest_path_problem */
 package com.thealgorithms.backtracking;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class AllPathsFromSourceToTarget {
 
-    // No. of vertices in graph
+    /**
+     * Common variables to "AllPathsFromSourceToTarget" class.
+     *
+     * 1) Number of vertices in graph.
+     * 2) List to store the paths from source to destiny.
+     * 3) Adjacency list.
+     */
     private final int v;
-
-    // To store the paths from source to destination
     static List<List<Integer>> nm = new ArrayList<>();
-    // adjacency list
     private ArrayList<Integer>[] adjList;
 
-    // Constructor
+    /**
+    * Constructor.
+    * @param vertices Number of vertices.
+    */
+    
     public AllPathsFromSourceToTarget(int vertices) {
 
         // initialise vertex count
@@ -31,7 +38,10 @@ public class AllPathsFromSourceToTarget {
         initAdjList();
     }
 
-    // utility method to initialise adjacency list
+    /**
+    * Creates an ArrayList for each position in adjList.
+    */
+    
     private void initAdjList() {
         adjList = new ArrayList[v];
 
@@ -40,37 +50,56 @@ public class AllPathsFromSourceToTarget {
         }
     }
 
-    // add edge from u to v
+    /**
+    * Add edge from u to v
+    *
+    * @param u Value to select list.
+    * @param v Value to introduce.
+    */
+    
     public void addEdge(int u, int v) {
-        // Add v to u's list.
         adjList[u].add(v);
     }
 
+    /**
+    *
+    * @param s Value of source
+    * @param d Value of destiny.
+    */
+    
     public void storeAllPaths(int s, int d) {
+        // Array composed by boolean´s objects with v like lenght
         boolean[] isVisited = new boolean[v];
+        // This ArrayList stores the track.
         ArrayList<Integer> pathList = new ArrayList<>();
-
         // add source to path[]
         pathList.add(s);
-        // Call recursive utility
+        // Call recursive function utility.
         storeAllPathsUtil(s, d, isVisited, pathList);
     }
 
-    // A recursive function to print all paths from 'u' to 'd'.
-    // isVisited[] keeps track of vertices in current path.
-    // localPathList<> stores actual vertices in the current path
+    /**
+    *
+    * Function to StoreAllPaths
+    * 
+    * @param u Value of source
+    * @param d Value of destiny
+    * @param isVisited array to keeps track of vertices in current path.
+    * @param localPathList stores actual vertices in the current path
+    */
+    
     private void storeAllPathsUtil(Integer u, Integer d, boolean[] isVisited, List<Integer> localPathList) {
 
+        //If node u is equals to d the path has benn completly found it.
         if (u.equals(d)) {
             nm.add(new ArrayList<>(localPathList));
             return;
         }
 
-        // Mark the current node
+        // Mark the current node as visited.
         isVisited[u] = true;
 
         // Recursion for all the vertices adjacent to current vertex
-
         for (Integer i : adjList[u]) {
             if (!isVisited[i]) {
                 // store current node in path[]
@@ -82,21 +111,33 @@ public class AllPathsFromSourceToTarget {
             }
         }
 
-        // Mark the current node
+        // Mark the current node as false to release it and be used by others.
         isVisited[u] = false;
     }
 
-    // Driver program
+    /**
+    *
+    * Driver program
+    *
+    * @param vertices number of vertices of the graph
+    * @param a represents the aristas of the graph.
+    * @param source value of source.
+    * @param destination value of destiny.
+    * @return A List composed by diferent lists that stores all the possible paths to the destiny.
+    */
+    
     public static List<List<Integer>> allPathsFromSourceToTarget(int vertices, int[][] a, int source, int destination) {
         // Create a sample graph
         AllPathsFromSourceToTarget g = new AllPathsFromSourceToTarget(vertices);
+
+        // edges are added
         for (int[] i : a) {
             g.addEdge(i[0], i[1]);
-            // edges are added
         }
-        g.storeAllPaths(source, destination);
+
         // method call to store all possible paths
+        g.storeAllPaths(source, destination);
+        
         return nm;
-        // returns all possible paths from source to destination
     }
 }
