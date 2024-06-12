af5664eb70e1985302d3171e0bba8a91a7087bc2
Samuel Facchinello <samuel.facchinello@piksel.com>
2024-06-12 17:05:59 +0200

    refactor

--- src/test/java/com/thealgorithms/ciphers/ColumnarTranspositionCipherTest.java
+++ src/test/java/com/thealgorithms/ciphers/ColumnarTranspositionCipherTest.java
@@ -1,23 +1,34 @@
 package com.thealgorithms.ciphers;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class ColumnarTranspositionCipherTest {
 
-    String keywordForExample = "asd215";
-    String wordBeingEncrypted = "This is a test of the Columnar Transposition Cipher";
-
-    @Test
-    void encrpyter() {
-        assertNotEquals(wordBeingEncrypted, ColumnarTranspositionCipher.encrpyter(wordBeingEncrypted, keywordForExample));
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void encrypt(String key, String word) {
+        assertNotEquals(word, ColumnarTranspositionCipher.encrypt(word, key));
     }
 
-    @Test
-    void decrypter() {
-        String encrypted=ColumnarTranspositionCipher.encrpyter(wordBeingEncrypted, keywordForExample);
-        assertEquals(wordBeingEncrypted, ColumnarTranspositionCipher.decrypter());
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void decrypt(String key, String word) {
+        String encrypted = ColumnarTranspositionCipher.encrypt(word, key);
+        assertEquals(word, ColumnarTranspositionCipher.decrypt(encrypted, key));
+    }
+
+    private static Stream<Arguments> provideTestCases() {
+        return Stream.of(Arguments.of("asd215", "This is a test of the Columnar Transposition Cipher"), //
+                Arguments.of("test123456", "test"), //
+                Arguments.of("my long secret pass 1234567890", "short message"), //
+                Arguments.of("secret", "My secret message") //
+        );
     }
 }
