f5d72aa9a40293369c3743367edd5bde0ea37832
Samuel Facchinello <samuel.facchinello@piksel.com>
2024-06-12 20:02:01 +0200

    fix pmd

--- src/test/java/com/thealgorithms/backtracking/CombinationTest.java
+++ src/test/java/com/thealgorithms/backtracking/CombinationTest.java
@@ -2,7 +2,6 @@ package com.thealgorithms.backtracking;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.TreeSet;
