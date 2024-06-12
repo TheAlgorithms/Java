667403b87f41d1fe5acf23d22ba3681a62b58b82
Samuel Facchinello <4256795+samuelfac@users.noreply.github.com>
2024-06-12 17:14:27 +0200

    Update src/main/java/com/thealgorithms/strings/LetterCombinationsOfPhoneNumber.java
    Co-authored-by: Piotr Idzik <65706193+vil02@users.noreply.github.com>

--- src/main/java/com/thealgorithms/strings/LetterCombinationsOfPhoneNumber.java
+++ src/main/java/com/thealgorithms/strings/LetterCombinationsOfPhoneNumber.java
@@ -8,17 +8,7 @@ import java.util.Map;
 public final class LetterCombinationsOfPhoneNumber {
 
     // Mapping of numbers to corresponding letters on a phone keypad
-    private static final Map<Integer, String> MAP_OF_CHARS = Map.of( //
-        0, "", //
-        1, "", //
-        2, "abc", //
-        3, "def", //
-        4, "ghi", //
-        5, "jkl", //
-        6, "mno", //
-        7, "pqrs", //
-        8, "tuv", //
-        9, "wxyz");
+    private static final Map<Integer, String> MAP_OF_CHARS = Map.of(0, "", 1, "", 2, "abc", 3, "def", 4, "ghi", 5, "jkl", 6, "mno", 7, "pqrs", 8, "tuv", 9, "wxyz");
 
     private LetterCombinationsOfPhoneNumber() {
         // Prevent instantiation
