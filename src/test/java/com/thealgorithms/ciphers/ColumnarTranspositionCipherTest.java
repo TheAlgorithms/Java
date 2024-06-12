7f441c6da1c0b35f11af2cbbbe29bb0cd119bd16
Samuel Facchinello <samuel.facchinello@piksel.com>
2024-06-12 17:10:45 +0200

    fix clang

--- src/test/java/com/thealgorithms/ciphers/ColumnarTranspositionCipherTest.java
+++ src/test/java/com/thealgorithms/ciphers/ColumnarTranspositionCipherTest.java
@@ -26,9 +26,9 @@ class ColumnarTranspositionCipherTest {
 
     private static Stream<Arguments> provideTestCases() {
         return Stream.of(Arguments.of("asd215", "This is a test of the Columnar Transposition Cipher"), //
-                Arguments.of("test123456", "test"), //
-                Arguments.of("my long secret pass 1234567890", "short message"), //
-                Arguments.of("secret", "My secret message") //
+            Arguments.of("test123456", "test"), //
+            Arguments.of("my long secret pass 1234567890", "short message"), //
+            Arguments.of("secret", "My secret message") //
         );
     }
 }
