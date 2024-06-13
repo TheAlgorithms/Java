06d49ced305423dba456fcb710a74506b2169b57
Samuel Facchinello <samuel.facchinello@piksel.com>
2024-06-13 10:21:21 +0200

    change keypad to list

--- src/main/java/com/thealgorithms/strings/LetterCombinationsOfPhoneNumber.java
+++ src/main/java/com/thealgorithms/strings/LetterCombinationsOfPhoneNumber.java
@@ -3,12 +3,11 @@ package com.thealgorithms.strings;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 public final class LetterCombinationsOfPhoneNumber {
 
     // Mapping of numbers to corresponding letters on a phone keypad
-    private static final Map<Integer, String> KEYPAD = Map.of(0, " ", 1, "", 2, "abc", 3, "def", 4, "ghi", 5, "jkl", 6, "mno", 7, "pqrs", 8, "tuv", 9, "wxyz");
+    private static final List<String> KEYPAD = List.of(" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz");
 
     private LetterCombinationsOfPhoneNumber() {
     }
