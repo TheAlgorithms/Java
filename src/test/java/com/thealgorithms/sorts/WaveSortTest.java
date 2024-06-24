a6e6a11c8e1f8e6b5ddd94a2c099f51f9b145939
alxklm <alx@alx.com>
2024-06-24 23:50:04 +0200

    Checkstyle: Fix wrong align

--- src/test/java/com/thealgorithms/sorts/WaveSortTest.java
+++ src/test/java/com/thealgorithms/sorts/WaveSortTest.java
@@ -2,13 +2,14 @@ package com.thealgorithms.sorts;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
 
 public class WaveSortTest {
-    record InputData(Integer[] array) {}
+    record InputData(Integer[] array) {
+    }
 
     @ParameterizedTest
     @MethodSource("inputStream")
@@ -18,8 +19,7 @@ public class WaveSortTest {
     }
 
     private static Stream<InputData> inputStream() {
-        return Stream.of(new InputData(new Integer[]{7, 7, 11, 3, 4, 5, 15}), new InputData(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8}), new InputData(new Integer[]{8, 7, 6, 5, 4, 3, 2, 1}), new InputData(new Integer[]{3, 3, 3, 3}),
-                new InputData(new Integer[]{-1, -3, -2, -4, -6, -5}), new InputData(new Integer[]{5, 3, 1, 2, 9, 7, 6, 8, 4, 0}), new InputData(new Integer[]{1}), new InputData(new Integer[]{2, 1}), new InputData(new Integer[]{}),
-                new InputData(new Integer[]{0, 5, -3, 2, -1, 4, -2, 1, 3}));
+        return Stream.of(new InputData(new Integer[] {7, 7, 11, 3, 4, 5, 15}), new InputData(new Integer[] {1, 2, 3, 4, 5, 6, 7, 8}), new InputData(new Integer[] {8, 7, 6, 5, 4, 3, 2, 1}), new InputData(new Integer[] {3, 3, 3, 3}), new InputData(new Integer[] {-1, -3, -2, -4, -6, -5}),
+                new InputData(new Integer[] {5, 3, 1, 2, 9, 7, 6, 8, 4, 0}), new InputData(new Integer[] {1}), new InputData(new Integer[] {2, 1}), new InputData(new Integer[] {}), new InputData(new Integer[] {0, 5, -3, 2, -1, 4, -2, 1, 3}));
     }
 }
