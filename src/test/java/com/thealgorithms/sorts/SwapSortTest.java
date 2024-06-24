a5b4c6173f711ddc6a73350d9db2deca625ed762
Alex Klymenko <alexanderklmn@gmail.com>
2024-06-24 09:49:01 +0300

    fix: avoid infinite loop in `SwapSort` (#5248)
    ---------
    
    Co-authored-by: vil02 <65706193+vil02@users.noreply.github.com>

---
+++ src/test/java/com/thealgorithms/sorts/SwapSortTest.java
@@ -0,0 +1,8 @@
+package com.thealgorithms.sorts;
+
+public class SwapSortTest extends SortingAlgorithmTest {
+    @Override
+    SortAlgorithm getSortAlgorithm() {
+        return new SwapSort();
+    }
+}
