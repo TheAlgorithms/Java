464a8c592841235f4837f048b5459f29f8524f93
alxklm <alx@alx.com>
2024-06-24 23:52:17 +0200

    Checkstyle: Fix wrong align for second line

--- src/test/java/com/thealgorithms/sorts/WaveSortTest.java
+++ src/test/java/com/thealgorithms/sorts/WaveSortTest.java
@@ -20,6 +20,6 @@ public class WaveSortTest {
 
     private static Stream<InputData> inputStream() {
         return Stream.of(new InputData(new Integer[] {7, 7, 11, 3, 4, 5, 15}), new InputData(new Integer[] {1, 2, 3, 4, 5, 6, 7, 8}), new InputData(new Integer[] {8, 7, 6, 5, 4, 3, 2, 1}), new InputData(new Integer[] {3, 3, 3, 3}), new InputData(new Integer[] {-1, -3, -2, -4, -6, -5}),
-                new InputData(new Integer[] {5, 3, 1, 2, 9, 7, 6, 8, 4, 0}), new InputData(new Integer[] {1}), new InputData(new Integer[] {2, 1}), new InputData(new Integer[] {}), new InputData(new Integer[] {0, 5, -3, 2, -1, 4, -2, 1, 3}));
+            new InputData(new Integer[] {5, 3, 1, 2, 9, 7, 6, 8, 4, 0}), new InputData(new Integer[] {1}), new InputData(new Integer[] {2, 1}), new InputData(new Integer[] {}), new InputData(new Integer[] {0, 5, -3, 2, -1, 4, -2, 1, 3}));
     }
 }
