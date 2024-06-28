6fe684c3a851fc048967b696122b599e897af9d0
Alex Klymenko <alx@alx.com>
2024-06-28 13:39:10 +0200

    Refactor: Adding test common approach, adding javadocs, renaming variables

--- src/main/java/com/thealgorithms/sorts/SelectionSort.java
+++ src/main/java/com/thealgorithms/sorts/SelectionSort.java
@@ -1,46 +1,39 @@
 package com.thealgorithms.sorts;
 
+/**
+ * This class provides an implementation of the selection sort algorithm.
+ * It sorts an array of elements in increasing order using an iterative approach.
+ */
 public class SelectionSort implements SortAlgorithm {
 
     /**
-     * Generic selection sort algorithm in increasing order.
+     * Sorts an array of comparable elements in increasing order using the selection sort algorithm.
      *
-     * @param arr the array to be sorted.
-     * @param <T> the class of array.
-     * @return sorted array.
+     * @param array the array to be sorted
+     * @param <T> the class of array elements
+     * @return the sorted array
      */
     @Override
-    public <T extends Comparable<T>> T[] sort(T[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
+    public <T extends Comparable<T>> T[] sort(T[] array) {
+        if (array.length == 0) {
+            return array;
+        }
+
+        // One by one move the boundary of the unsorted subarray
+        for (int i = 0; i < array.length - 1; i++) {
+            // Find the minimum element in the unsorted array
             int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[minIndex].compareTo(arr[j]) > 0) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j].compareTo(array[minIndex]) < 0) {
                     minIndex = j;
                 }
             }
+
+            // Swap the found minimum element with the first element
             if (minIndex != i) {
-                SortUtils.swap(arr, i, minIndex);
+                SortUtils.swap(array, i, minIndex);
             }
         }
-        return arr;
-    }
-
-    /**
-     * Driver Code
-     */
-    public static void main(String[] args) {
-        Integer[] arr = {4, 23, 6, 78, 1, 54, 231, 9, 12};
-        SelectionSort selectionSort = new SelectionSort();
-        Integer[] sorted = selectionSort.sort(arr);
-        for (int i = 0; i < sorted.length - 1; ++i) {
-            assert sorted[i] <= sorted[i + 1];
-        }
-
-        String[] strings = {"c", "a", "e", "b", "d"};
-        String[] sortedStrings = selectionSort.sort(strings);
-        for (int i = 0; i < sortedStrings.length - 1; ++i) {
-            assert strings[i].compareTo(strings[i + 1]) <= 0;
-        }
+        return array;
     }
 }
