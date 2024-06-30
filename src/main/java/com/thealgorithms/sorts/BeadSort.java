d9313bebfa784bd5b62cedeca052adccd41fe8a9
Alex Klymenko <alx@alx.com>
2024-06-30 21:12:35 +0200

    cleanup: BeadSort and BeadSortTest, adding javadocs

--- src/main/java/com/thealgorithms/sorts/BeadSort.java
+++ src/main/java/com/thealgorithms/sorts/BeadSort.java
@@ -1,40 +1,53 @@
 package com.thealgorithms.sorts;
 
-// BeadSort Algorithm(wikipedia) : https://en.wikipedia.org/wiki/Bead_sort
-// BeadSort can't sort negative number, Character, String. It can sort positive number only
+import java.util.Arrays;
 
+/**
+ * BeadSort cannot sort negative numbers, characters, or strings.
+ * It only works for non-negative integers.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Bead_sort">BeadSort Algorithm (Wikipedia)</a>
+ */
 public class BeadSort {
-    public int[] sort(int[] unsorted) {
-        int[] sorted = new int[unsorted.length];
-        int max = 0;
-        for (int i = 0; i < unsorted.length; i++) {
-            max = Math.max(max, unsorted[i]);
+    /**
+     * Sorts the given array using the BeadSort algorithm.
+     *
+     * @param array The array of non-negative integers to be sorted.
+     * @return The sorted array.
+     * @throws IllegalArgumentException If the array contains negative numbers.
+     */
+    public int[] sort(int[] array) {
+        if (array.length == 0) {
+            return array;
         }
 
-        char[][] grid = new char[unsorted.length][max];
+        if (Arrays.stream(array).anyMatch(s -> s < 0)) {
+            throw new IllegalArgumentException("BeadSort cannot sort negative numbers.");
+        }
+
+        int[] sorted = new int[array.length];
+        int max = Arrays.stream(array).max().orElse(0);
+
+        char[][] grid = new char[array.length][max];
         int[] count = new int[max];
 
-        for (int i = 0; i < unsorted.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < max; j++) {
                 grid[i][j] = '-';
             }
         }
 
-        for (int i = 0; i < max; i++) {
-            count[i] = 0;
-        }
-
-        for (int i = 0; i < unsorted.length; i++) {
+        for (int i = 0, arrayLength = array.length; i < arrayLength; i++) {
             int k = 0;
-            for (int j = 0; j < unsorted[i]; j++) {
+            for (int j = 0; j < array[i]; j++) {
                 grid[count[max - k - 1]++][k] = '*';
                 k++;
             }
         }
 
-        for (int i = 0; i < unsorted.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             int k = 0;
-            for (int j = 0; j < max && grid[unsorted.length - 1 - i][j] == '*'; j++) {
+            for (int j = 0; j < max && grid[array.length - 1 - i][j] == '*'; j++) {
                 k++;
             }
             sorted[i] = k;
