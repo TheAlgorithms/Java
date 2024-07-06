ada56e0493e61251b44062a3f834c4538ba65498
Alex Klymenko <alx@alx.com>
2024-07-06 17:01:27 +0200

    checkstyle: fix import ordering

--- src/test/java/com/thealgorithms/sorts/CountingSortTest.java
+++ src/test/java/com/thealgorithms/sorts/CountingSortTest.java
@@ -2,9 +2,9 @@ package com.thealgorithms.sorts;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.util.stream.Stream;
 
 public class CountingSortTest {
 
