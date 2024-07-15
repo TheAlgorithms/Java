ed7a1994921fbb3c463bbd8d3895f1b81c306a5b
alxklm <alx@alx.com>
2024-07-15 20:11:21 +0200

    refactor: adding possibility to sort negative numbers. Improve tests. Improving code readability

--- src/main/java/com/thealgorithms/sorts/RadixSort.java
+++ src/main/java/com/thealgorithms/sorts/RadixSort.java
@@ -1,5 +1,7 @@
 package com.thealgorithms.sorts;
 
+import com.thealgorithms.maths.NumberOfDigits;
+
 import java.util.Arrays;
 
 /**
@@ -7,6 +9,8 @@ import java.util.Arrays;
  * It sorts an array of integers in increasing order.
  */
 public final class RadixSort {
+    private static final int BASE = 10;
+
     private RadixSort() {
     }
 
@@ -21,12 +25,45 @@ public final class RadixSort {
             return array;
         }
 
-        final int max = Arrays.stream(array).max().getAsInt();
-        for (int exp = 1; max / exp > 0; exp *= 10) {
-            countingSortByDigit(array, exp);
+        int[] negatives = Arrays.stream(array).filter(x -> x < 0).map(x -> -x).toArray();
+        int[] positives = Arrays.stream(array).filter(x -> x >= 0).toArray();
+
+        if (negatives.length > 0) {
+            radixSort(negatives);
+            reverseAndNegate(negatives);
         }
 
-        return array;
+        if (positives.length > 0) {
+            radixSort(positives);
+        }
+
+        int[] sortedArray = new int[array.length];
+        mergeNegativeAndPositiveArrays(negatives, sortedArray, positives);
+
+        return sortedArray;
+    }
+
+    private static void mergeNegativeAndPositiveArrays(int[] negatives, int[] sortedArray, int[] positives) {
+        System.arraycopy(negatives, 0, sortedArray, 0, negatives.length);
+        System.arraycopy(positives, 0, sortedArray, negatives.length, positives.length);
+    }
+
+    private static void radixSort(int[] array) {
+        final int max = Arrays.stream(array).max().getAsInt();
+        for (int i = 0, exp = 1; i < NumberOfDigits.numberOfDigits(max); i++, exp *= BASE) {
+            countingSortByDigit(array, exp);
+        }
+    }
+
+    private static void reverseAndNegate(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = -array[array.length - 1 - i];
+            array[array.length - 1 - i] = -temp;
+        }
+        if (array.length % 2 != 0) {
+            array[array.length / 2] = -array[array.length / 2];
+        }
     }
 
     /**
@@ -37,23 +74,39 @@ public final class RadixSort {
      */
     private static void countingSortByDigit(int[] array, int exp) {
         int[] output = new int[array.length];
-        int[] count = new int[10];
+        int[] count = new int[BASE];
 
+        countDigits(array, exp, count);
+        accumulateCounts(count);
+        buildOutput(array, exp, output, count);
+        copyOutput(array, output);
+    }
+
+    private static void countDigits(int[] array, int exp, int[] count) {
         for (int i = 0; i < array.length; i++) {
-            count[(array[i] / exp) % 10]++;
+            count[getDigit(array[i], exp)]++;
         }
+    }
 
-        for (int i = 1; i < 10; i++) {
+    private static int getDigit(int number, int position) {
+        return (number / position) % BASE;
+    }
+
+    private static void accumulateCounts(int[] count) {
+        for (int i = 1; i < BASE; i++) {
             count[i] += count[i - 1];
         }
+    }
 
+    private static void buildOutput(int[] array, int exp, int[] output, int[] count) {
         for (int i = array.length - 1; i >= 0; i--) {
-            int digit = (array[i] / exp) % 10;
+            int digit = getDigit(array[i], exp);
             output[count[digit] - 1] = array[i];
             count[digit]--;
         }
+    }
 
+    private static void copyOutput(int[] array, int[] output) {
         System.arraycopy(output, 0, array, 0, array.length);
     }
 }
-// Written by James Mc Dermott(theycallmemac)
--- src/test/java/com/thealgorithms/sorts/RadixSortTest.java
+++ src/test/java/com/thealgorithms/sorts/RadixSortTest.java
@@ -18,6 +18,10 @@ public class RadixSortTest {
         return Stream.of(Arguments.of(new int[] {170, 45, 75, 90, 802, 24, 2, 66}, new int[] {2, 24, 45, 66, 75, 90, 170, 802}), Arguments.of(new int[] {3, 3, 3, 3}, new int[] {3, 3, 3, 3}), Arguments.of(new int[] {9, 4, 6, 8, 14, 3}, new int[] {3, 4, 6, 8, 9, 14}),
             Arguments.of(new int[] {10, 90, 49, 2, 1, 5, 23}, new int[] {1, 2, 5, 10, 23, 49, 90}), Arguments.of(new int[] {1, 3, 4, 2, 7, 8}, new int[] {1, 2, 3, 4, 7, 8}), Arguments.of(new int[] {}, new int[] {}), Arguments.of(new int[] {1}, new int[] {1}),
             Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}), Arguments.of(new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}),
-            Arguments.of(new int[] {1000000000, 999999999, 888888888, 777777777}, new int[] {777777777, 888888888, 999999999, 1000000000}));
+            Arguments.of(new int[] {1000000000, 999999999, 888888888, 777777777}, new int[] {777777777, 888888888, 999999999, 1000000000}), Arguments.of(new int[] {123, 9, 54321, 123456789, 0}, new int[] {0, 9, 123, 54321, 123456789}),
+            Arguments.of(new int[] {-170, 45, -75, 90, -802, 24, -2, 66}, new int[] {-802, -170, -75, -2, 24, 45, 66, 90}), Arguments.of(new int[] {-3, -3, -3, -3}, new int[] {-3, -3, -3, -3}),
+            Arguments.of(new int[] {-9, -4, -6, -8, -14, -3}, new int[] {-14, -9, -8, -6, -4, -3}), Arguments.of(new int[] {10, -90, 49, -2, 1, -5, 23}, new int[] {-90, -5, -2, 1, 10, 23, 49}),
+            Arguments.of(new int[] {-1, -3, -4, -2, -7, -8}, new int[] {-8, -7, -4, -3, -2, -1}), Arguments.of(new int[] {1, -1, 0, -100, 100}, new int[] {-100, -1, 0, 1, 100}),
+            Arguments.of(new int[] {-1000000000, 999999999, -888888888, 777777777}, new int[] {-1000000000, -888888888, 777777777, 999999999}), Arguments.of(new int[] {-123, 9, -54321, 123456789, 0}, new int[] {-54321, -123, 0, 9, 123456789}));
     }
 }
