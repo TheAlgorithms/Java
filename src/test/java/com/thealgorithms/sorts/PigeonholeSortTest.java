59a671b9f96c69fce26142c3adc5c01f9151c44c
Alex Klymenko <alx@alx.com>
2024-07-21 18:48:03 +0200

    checkstyle: fix formatting

--- src/test/java/com/thealgorithms/sorts/PigeonholeSortTest.java
+++ src/test/java/com/thealgorithms/sorts/PigeonholeSortTest.java
@@ -19,21 +19,12 @@ public class PigeonholeSortTest {
     }
 
     private static Stream<Arguments> provideArraysForPigeonholeSort() {
-        return Stream.of(
-                Arguments.of(new int[] {}, new int[] {}),
-                Arguments.of(new int[] {4}, new int[] {4}),
-                Arguments.of(new int[] {6, 1, 99, 27, 15, 23, 36}, new int[] {1, 6, 15, 23, 27, 36, 99}),
-                Arguments.of(new int[] {6, 1, 27, 15, 23, 27, 36, 23}, new int[] {1, 6, 15, 23, 23, 27, 27, 36}),
-                Arguments.of(new int[] {5, 5, 5, 5, 5}, new int[] {5, 5, 5, 5, 5}),
-                Arguments.of(new int[] {1, 2, 3, 4, 5}, new int[] {1, 2, 3, 4, 5}),
-                Arguments.of(new int[] {5, 4, 3, 2, 1}, new int[] {1, 2, 3, 4, 5})
-        );
+        return Stream.of(Arguments.of(new int[] {}, new int[] {}), Arguments.of(new int[] {4}, new int[] {4}), Arguments.of(new int[] {6, 1, 99, 27, 15, 23, 36}, new int[] {1, 6, 15, 23, 27, 36, 99}), Arguments.of(new int[] {6, 1, 27, 15, 23, 27, 36, 23}, new int[] {1, 6, 15, 23, 23, 27, 27, 36}),
+            Arguments.of(new int[] {5, 5, 5, 5, 5}, new int[] {5, 5, 5, 5, 5}), Arguments.of(new int[] {1, 2, 3, 4, 5}, new int[] {1, 2, 3, 4, 5}), Arguments.of(new int[] {5, 4, 3, 2, 1}, new int[] {1, 2, 3, 4, 5}));
     }
 
     @Test
     public void testWithNegativeNumbers() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            PigeonholeSort.sort(new int[] {3, 1, 4, 1, 5, -9});
-        });
+        assertThrows(IllegalArgumentException.class, () -> PigeonholeSort.sort(new int[] {3, 1, 4, 1, 5, -9}));
     }
 }
