ca598758923f5ea0e3876b7bf7887a6bf2999f87
Alex Klymenko <alx@alx.com>
2024-07-21 19:01:57 +0200

    checkstyle: make class final

--- src/main/java/com/thealgorithms/sorts/PigeonholeSort.java
+++ src/main/java/com/thealgorithms/sorts/PigeonholeSort.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class PigeonholeSort {
+public final class PigeonholeSort {
     private PigeonholeSort() {
     }
 
