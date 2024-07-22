c0b8737f9aa89ef8dc71a031be845916c50076cc
JAPNITSINGH <truejswalia@gmail.com>
2024-07-22 19:04:36 +0530

    Rework on formatting.

--- src/main/java/com/thealgorithms/datastructures/heaps/LeonardoHeap.java
+++ src/main/java/com/thealgorithms/datastructures/heaps/LeonardoHeap.java
@@ -1,11 +1,10 @@
 package com.thealgorithms.datastructures.heaps;
 
+import com.thealgorithms.bitmanipulation.SingleBitOperations;
+import com.thealgorithms.maths.LeonardoNumber;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import com.thealgorithms.bitmanipulation.SingleBitOperations;
-import com.thealgorithms.maths.LeonardoNumber;
 
 /**
  * Wikipedia: https://en.wikipedia.org/wiki/Smoothsort
