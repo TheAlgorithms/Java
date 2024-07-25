77f1345c47d44cc8c8ec223b27f70cc4c1b0b0f9
Sir Abe <102336201+AbeTheAmatuer@users.noreply.github.com>
2024-07-25 17:40:16 -0500

    Refactor: Cleanup for Combination.java
    Just changed class name, variable names and comments to make it more clear and understandable.

--- src/main/java/com/thealgorithms/backtracking/CombinationGenerator.java
+++ src/main/java/com/thealgorithms/backtracking/CombinationGenerator.java
@@ -5,28 +5,32 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeSet;
 
+
 /**
- * Finds all permutations of given array
+ * Finds all combinations of a given array that are a specific length by using backtracking.
  * @author Alan Piao (<a href="https://github.com/cpiao3">git-Alan Piao</a>)
  */
-public final class Combination {
-    private Combination() {
+public final class CombinationGenerator {
+    // Default private constructor to prevent instantiation
+
+    private CombinationGenerator() {
     }
 
-    private static int length;
+    private static int length; //Define length outside all methods so that it can be accessed anywhere in the class.
+
 
     /**
      * Find all combinations of given array using backtracking
-     * @param arr the array.
-     * @param n length of combination
+     * @param arr the array we want to find combinations of.
+     * @param desiredLength the length of our desired combinations.
      * @param <T> the type of elements in the array.
-     * @return a list of all combinations of length n. If n == 0, return null.
+     * @return a list of all combinations of length desiredLength. If desiredLength is 0, return null.
      */
-    public static <T> List<TreeSet<T>> combination(T[] arr, int n) {
-        if (n == 0) {
+    public static <T> List<TreeSet<T>> combination(T[] arr, int desiredLength) {
+        if (desiredLength == 0) {
             return null;
         }
-        length = n;
+        length = desiredLength;
         T[] array = arr.clone();
         Arrays.sort(array);
         List<TreeSet<T>> result = new LinkedList<>();
@@ -34,9 +38,10 @@ public final class Combination {
         return result;
     }
 
+    
     /**
      * Backtrack all possible combinations of a given array
-     * @param arr the array.
+     * @param arr the array from earlier.
      * @param index the starting index.
      * @param currSet set that tracks current combination
      * @param result the list contains all combination.
