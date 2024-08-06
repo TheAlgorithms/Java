09740a9eb31d755969726fe5e9a53396c800eabb
Alex Klymenko <alx@alx.com>
2024-08-06 21:31:11 +0200

    checkstyle: fix incorrect import order

--- src/test/java/com/thealgorithms/sorts/FlashSortTest.java
+++ src/test/java/com/thealgorithms/sorts/FlashSortTest.java
@@ -1,11 +1,11 @@
 package com.thealgorithms.sorts;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FlashSortTest extends SortingAlgorithmTest {
     @Override
