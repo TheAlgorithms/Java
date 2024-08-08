42b866e499427956d13751cd3adba1ae69aee690
Bayram Turgut <137455737+bayramtturgutt@users.noreply.github.com>
2024-08-08 21:09:04 +0300

    Spc.java

--- src/main/java/com/thealgorithms/geometry/GrahamScan.java
+++ src/main/java/com/thealgorithms/geometry/GrahamScan.java
@@ -3,6 +3,7 @@ package com.thealgorithms.geometry;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Stack;
+import java.util.ArrayList;
 
 /**
  * A Java program that computes the convex hull using the Graham Scan algorithm.
@@ -16,41 +17,49 @@ import java.util.Stack;
  * https://algs4.cs.princeton.edu/99hull/GrahamScan.java.html
  */
 public class GrahamScan {
+
     private final Stack<Point> hull = new Stack<>();
+
     public GrahamScan(Point[] points) {
         // Pre-process points: sort by y-coordinate, then by polar order with respect to the first point
         Arrays.sort(points);
         Arrays.sort(points, 1, points.length, points[0].polarOrder());
+
         hull.push(points[0]);
-    // Find the first point not equal to points[0]/firstNonEqualIndex and the first point not collinear firstNonCollinearIndex with the previous points
-    int firstNonEqualIndex;
-    for (firstNonEqualIndex = 1; firstNonEqualIndex < points.length; firstNonEqualIndex++) {
-        if (!points[0].equals(points[firstNonEqualIndex])) {
-            break;
+
+        // Find the first point not equal to points[0]/firstNonEqualIndex 
+        // and the first point not collinear firstNonCollinearIndex with the previous points
+        int firstNonEqualIndex;
+        for (firstNonEqualIndex = 1; firstNonEqualIndex < points.length; firstNonEqualIndex++) {
+            if (!points[0].equals(points[firstNonEqualIndex])) {
+                break;
+            }
+        }
+
+        if (firstNonEqualIndex == points.length) {
+            return;
+        }
+
+        int firstNonCollinearIndex;
+        for (firstNonCollinearIndex = firstNonEqualIndex + 1; firstNonCollinearIndex < points.length; firstNonCollinearIndex++) {
+            if (Point.orientation(points[0], points[firstNonEqualIndex], points[firstNonCollinearIndex]) != 0) {
+                break;
+            }
+        }
+
+        hull.push(points[firstNonCollinearIndex - 1]);
+
+        // Process the remaining points and update the hull
+        for (int i = firstNonCollinearIndex; i < points.length; i++) {
+            Point top = hull.pop();
+            while (Point.orientation(hull.peek(), top, points[i]) <= 0) {
+                top = hull.pop();
+            }
+            hull.push(top);
+            hull.push(points[i]);
         }
     }
-    if (firstNonEqualIndex == points.length) {
-        return;
-    }
-    
-    int firstNonCollinearIndex;
-    for (firstNonCollinearIndex = firstNonEqualIndex + 1; firstNonCollinearIndex < points.length; firstNonCollinearIndex++) {
-        if (Point.orientation(points[0], points[firstNonEqualIndex], points[firstNonCollinearIndex]) != 0) {
-            break;
-        }
-    }
-    hull.push(points[firstNonCollinearIndex - 1]);
-    
-    // Process the remaining points and update the hull
-    for (int i = firstNonCollinearIndex; i < points.length; i++) {
-        Point top = hull.pop();
-        while (Point.orientation(hull.peek(), top, points[i]) <= 0) {
-            top = hull.pop();
-        }
-        hull.push(top);
-        hull.push(points[i]);
-    }
-    
+
     /**
      * @return An iterable collection of points representing the convex hull.
      */
