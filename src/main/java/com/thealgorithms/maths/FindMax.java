11a6374d65e8b28b52afc8b31eaf7b7416fa5f71
ilyasseyounes <162178256+ilyasseyounes1@users.noreply.github.com>
2024-08-10 18:27:38 +0100

    Update

--- src/main/java/com/thealgorithms/maths/FindMax.java
+++ src/main/java/com/thealgorithms/maths/FindMax.java
@@ -11,15 +11,15 @@ public final class FindMax {
      * @exception IllegalArgumentException input array is empty
      * @return the maximum value stored in the input array
      */
-    public static int findMax(final int[] array) {
+    public static int findMax( final int[] array) {
         int n = array.length;
         if (n == 0) {
             throw new IllegalArgumentException("Array must be non-empty.");
         }
-        int max = array[0];
+        int max = array [0];
         for (int i = 1; i < n; i++) {
-            if (array[i] > max) {
-                max = array[i];
+            if (array [i] > max) {
+                max = array [i];
             }
         }
         return max;
