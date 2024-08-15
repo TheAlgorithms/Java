db1f444f938baada14555a25dbe391d7546b856d
alxkm <alx@alx.com>
2024-08-16 00:10:23 +0200

    checkstyle: fix import

--- src/test/java/com/thealgorithms/conversions/DecimalToOctalTest.java
+++ src/test/java/com/thealgorithms/conversions/DecimalToOctalTest.java
@@ -1,10 +1,10 @@
 package com.thealgorithms.conversions;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class DecimalToOctalTest {
     @ParameterizedTest
