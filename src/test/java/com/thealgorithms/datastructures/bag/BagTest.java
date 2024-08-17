f293315c8e400e9b82e9b1724458bd9035acc2c2
alxkm <alx@alx.com>
2024-08-17 12:46:38 +0200

    checkstyle: fix "avoid *"

--- src/test/java/com/thealgorithms/datastructures/bag/BagTest.java
+++ src/test/java/com/thealgorithms/datastructures/bag/BagTest.java
@@ -1,6 +1,10 @@
 package com.thealgorithms.datastructures.bag;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import com.thealgorithms.datastructures.bags.Bag;
 import java.util.Iterator;
