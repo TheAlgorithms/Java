f882c63bfd04b8f3692ee8763c623609d55b3fb3
alxkm <alx@alx.com>
2024-08-17 12:06:02 +0200

    checkstyle: fix formatting

--- src/test/java/com/thealgorithms/conversions/DecimalToBinaryTest.java
+++ src/test/java/com/thealgorithms/conversions/DecimalToBinaryTest.java
@@ -1,6 +1,7 @@
 package com.thealgorithms.conversions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
