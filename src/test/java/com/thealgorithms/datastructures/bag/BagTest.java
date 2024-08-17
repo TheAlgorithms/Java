271474aaaab90a6b423720ef90e2e7b625ce5d3f
alxkm <alx@alx.com>
2024-08-17 12:41:51 +0200

    checkstyle: fix to many imports

--- src/test/java/com/thealgorithms/datastructures/bag/BagTest.java
+++ src/test/java/com/thealgorithms/datastructures/bag/BagTest.java
@@ -1,10 +1,6 @@
 package com.thealgorithms.datastructures.bag;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.thealgorithms.datastructures.bags.Bag;
 import java.util.Iterator;
