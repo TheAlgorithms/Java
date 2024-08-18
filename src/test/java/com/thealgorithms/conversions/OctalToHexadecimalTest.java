68fe694bcef4e3725fea828d918bb0c41ebed12a
alxkm <alx@alx.com>
2024-08-18 16:22:54 +0200

    fix: static import

--- src/test/java/com/thealgorithms/conversions/OctalToHexadecimalTest.java
+++ src/test/java/com/thealgorithms/conversions/OctalToHexadecimalTest.java
@@ -1,14 +1,13 @@
 package com.thealgorithms.conversions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OctalToHexadecimalTest {
 
     @Test
     public void testOctalToHexadecimal() {
-        assertEquals("1EA", OctalToHexadecimal.decimalToHex(OctalToHexadecimal.octToDec("752")));
-        assertEquals("15E", OctalToHexadecimal.decimalToHex(OctalToHexadecimal.octToDec("536")));
+        Assertions.assertEquals("1EA", OctalToHexadecimal.decimalToHex(OctalToHexadecimal.octToDec("752")));
+        Assertions.assertEquals("15E", OctalToHexadecimal.decimalToHex(OctalToHexadecimal.octToDec("536")));
     }
 }
