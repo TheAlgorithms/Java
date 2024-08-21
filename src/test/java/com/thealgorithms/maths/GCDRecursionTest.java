da5e35a8b6916ec3115aac81fec27048689571fd
alxkm <alx@alx.com>
2024-08-21 23:00:53 +0200

    checkstyle: fix formatting

--- src/test/java/com/thealgorithms/maths/GCDRecursionTest.java
+++ src/test/java/com/thealgorithms/maths/GCDRecursionTest.java
@@ -1,10 +1,12 @@
 package com.thealgorithms.maths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class GCDRecursionTest {
 
