fed1525e25d7ad93e792737dac21192b85380271
Alex Klymenko <alx@alx.com>
2024-08-21 22:42:03 +0200

    refactor: LongestNonRepetitiveSubstring

--- src/test/java/com/thealgorithms/strings/LongestNonRepetitiveSubstringTest.java
+++ src/test/java/com/thealgorithms/strings/LongestNonRepetitiveSubstringTest.java
@@ -3,13 +3,13 @@ package com.thealgorithms.strings;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class LongestNonRepeativeSubstringTest {
+public class LongestNonRepetitiveSubstringTest {
 
     @Test
     public void palindrome() {
         String input1 = "HelloWorld";
         String input2 = "javaIsAProgrammingLanguage";
-        Assertions.assertEquals(LongestNonRepeativeSubstring.lengthOfLongestSubstring(input1), 5);
-        Assertions.assertEquals(LongestNonRepeativeSubstring.lengthOfLongestSubstring(input2), 9);
+        Assertions.assertEquals(LongestNonRepetitiveSubstring.lengthOfLongestSubstring(input1), 5);
+        Assertions.assertEquals(LongestNonRepetitiveSubstring.lengthOfLongestSubstring(input2), 9);
     }
 }
