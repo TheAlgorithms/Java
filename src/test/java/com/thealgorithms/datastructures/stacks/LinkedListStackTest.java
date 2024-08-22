b2d888edd3869318704c3c2c3fead3e8cb7bd6e5
alxkm <alx@alx.com>
2024-08-22 21:29:00 +0200

    checkstyle: fix formatting

--- src/test/java/com/thealgorithms/datastructures/stacks/LinkedListStackTest.java
+++ src/test/java/com/thealgorithms/datastructures/stacks/LinkedListStackTest.java
@@ -4,10 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.NoSuchElementException;
 
 public class LinkedListStackTest {
 
