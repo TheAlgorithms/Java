d7ea4438f9fd44db693bbe3064eeef7a74d2b0de
alxkm <alx@alx.com>
2024-08-22 21:35:50 +0200

    checkstyle: fix import ordering

--- src/test/java/com/thealgorithms/others/FloydTriangleTest.java
+++ src/test/java/com/thealgorithms/others/FloydTriangleTest.java
@@ -1,8 +1,8 @@
 package com.thealgorithms.others;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class FloydTriangleTest {
 
