7e4b666c2a72ef76b4466e3875a688ef0fe43085
alxkm <alx@alx.com>
2024-08-23 23:12:10 +0200

    checkstyle: fix formatting import

--- src/test/java/com/thealgorithms/maths/FindKthNumberTest.java
+++ src/test/java/com/thealgorithms/maths/FindKthNumberTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
 import java.util.Random;
-
 import org.junit.jupiter.api.Test;
 
 public class FindKthNumberTest {
