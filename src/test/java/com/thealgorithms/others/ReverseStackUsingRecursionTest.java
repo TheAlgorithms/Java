4d24992ceed4854b19dee159b4d2721a074ae90e
alxkm <alx@alx.com>
2024-08-24 22:58:03 +0200

    refactor: fix formatting

--- src/test/java/com/thealgorithms/others/ReverseStackUsingRecursionTest.java
+++ src/test/java/com/thealgorithms/others/ReverseStackUsingRecursionTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
 import java.util.Stack;
+import org.junit.jupiter.api.Test;
 
 public class ReverseStackUsingRecursionTest {
 
