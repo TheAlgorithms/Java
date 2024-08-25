4871a7ac37f9f3890760d8fea3d91acd62f82dda
alxkm <alx@alx.com>
2024-08-25 22:36:09 +0200

    checkstyle: fix bugspot wrong check using record

--- src/test/java/com/thealgorithms/strings/AnagramsTest.java
+++ src/test/java/com/thealgorithms/strings/AnagramsTest.java
@@ -4,43 +4,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class AnagramsTest {
 
-    private static Stream<Arguments> anagramTestData() {
-        return Stream.of(Arguments.of("late", "tale", true), Arguments.of("late", "teal", true), Arguments.of("listen", "silent", true), Arguments.of("hello", "olelh", true), Arguments.of("hello", "world", false), Arguments.of("deal", "lead", true), Arguments.of("binary", "brainy", true),
-            Arguments.of("adobe", "abode", true), Arguments.of("cat", "act", true), Arguments.of("cat", "cut", false));
+    record AnagramTestCase(String input1, String input2, boolean expected) {
+    }
+
+    private static Stream<AnagramTestCase> anagramTestData() {
+        return Stream.of(new AnagramTestCase("late", "tale", true), new AnagramTestCase("late", "teal", true), new AnagramTestCase("listen", "silent", true), new AnagramTestCase("hello", "olelh", true), new AnagramTestCase("hello", "world", false), new AnagramTestCase("deal", "lead", true), new AnagramTestCase("binary", "brainy", true), new AnagramTestCase("adobe", "abode", true), new AnagramTestCase("cat", "act", true), new AnagramTestCase("cat", "cut", false));
     }
 
     @ParameterizedTest
     @MethodSource("anagramTestData")
-    void testApproach1(String s, String t, boolean expected) {
-        assertEquals(expected, Anagrams.approach1(s, t));
+    void testApproach1(AnagramTestCase testCase) {
+        assertEquals(testCase.expected(), Anagrams.approach1(testCase.input1(), testCase.input2()));
     }
 
     @ParameterizedTest
     @MethodSource("anagramTestData")
-    void testApproach2(String s, String t, boolean expected) {
-        assertEquals(expected, Anagrams.approach2(s, t));
+    void testApproach2(AnagramTestCase testCase) {
+        assertEquals(testCase.expected(), Anagrams.approach2(testCase.input1(), testCase.input2()));
     }
 
     @ParameterizedTest
     @MethodSource("anagramTestData")
-    void testApproach3(String s, String t, boolean expected) {
-        assertEquals(expected, Anagrams.approach3(s, t));
+    void testApproach3(AnagramTestCase testCase) {
+        assertEquals(testCase.expected(), Anagrams.approach3(testCase.input1(), testCase.input2()));
     }
 
     @ParameterizedTest
     @MethodSource("anagramTestData")
-    void testApproach4(String s, String t, boolean expected) {
-        assertEquals(expected, Anagrams.approach4(s, t));
+    void testApproach4(AnagramTestCase testCase) {
+        assertEquals(testCase.expected(), Anagrams.approach4(testCase.input1(), testCase.input2()));
     }
 
     @ParameterizedTest
     @MethodSource("anagramTestData")
-    void testApproach5(String s, String t, boolean expected) {
-        assertEquals(expected, Anagrams.approach5(s, t));
+    void testApproach5(AnagramTestCase testCase) {
+        assertEquals(testCase.expected(), Anagrams.approach5(testCase.input1(), testCase.input2()));
     }
 }
