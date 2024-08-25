7dabf3c8f3c182e7f976fe75ecdd2416190b27f3
alxkm <alx@alx.com>
2024-08-25 21:11:23 +0200

    checkstyle: fix import ordering

--- src/test/java/com/thealgorithms/others/EulersFunctionTest.java
+++ src/test/java/com/thealgorithms/others/EulersFunctionTest.java
@@ -3,11 +3,10 @@ package com.thealgorithms.others;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 class EulersFunctionTest {
 
