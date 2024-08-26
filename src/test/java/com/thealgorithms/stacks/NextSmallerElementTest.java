5775bf423eec28923442eb82b2e43afeda7b35db
alxkm <alx@alx.com>
2024-08-27 00:00:52 +0200

    checkstyle: fix formatting

--- src/test/java/com/thealgorithms/stacks/NextSmallerElementTest.java
+++ src/test/java/com/thealgorithms/stacks/NextSmallerElementTest.java
@@ -19,14 +19,8 @@ class NextSmallerElementTest {
     }
 
     static Stream<Arguments> provideTestCases() {
-        return Stream.of(
-                Arguments.of(new int[]{2, 7, 3, 5, 4, 6, 8}, new int[]{-1, 2, 2, 3, 3, 4, 6}),
-                Arguments.of(new int[]{5}, new int[]{-1}),
-                Arguments.of(new int[]{1, 2, 3, 4, 5}, new int[]{-1, 1, 2, 3, 4}),
-                Arguments.of(new int[]{5, 4, 3, 2, 1}, new int[]{-1, -1, -1, -1, -1}),
-                Arguments.of(new int[]{4, 5, 2, 25}, new int[]{-1, 4, -1, 2}),
-                Arguments.of(new int[]{}, new int[]{})
-        );
+        return Stream.of(Arguments.of(new int[] {2, 7, 3, 5, 4, 6, 8}, new int[] {-1, 2, 2, 3, 3, 4, 6}), Arguments.of(new int[] {5}, new int[] {-1}), Arguments.of(new int[] {1, 2, 3, 4, 5}, new int[] {-1, 1, 2, 3, 4}), Arguments.of(new int[] {5, 4, 3, 2, 1}, new int[] {-1, -1, -1, -1, -1}),
+            Arguments.of(new int[] {4, 5, 2, 25}, new int[] {-1, 4, -1, 2}), Arguments.of(new int[] {}, new int[] {}));
     }
 
     @Test
