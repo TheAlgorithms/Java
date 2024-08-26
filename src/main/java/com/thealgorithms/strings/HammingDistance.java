c5b73ec742d0522d65331ee02a143f54041845ed
Alex Klymenko <alexanderklmn@gmail.com>
2024-08-26 16:37:17 +0200

    refactor: `HammingDistance` (#5404)
    * refactor: HammingDistance
    
    * checkstyle: fix formatting
    
    ---------
    
    Co-authored-by: alxkm <alx@alx.com>

--- src/main/java/com/thealgorithms/strings/HammingDistance.java
+++ src/main/java/com/thealgorithms/strings/HammingDistance.java
@@ -1,34 +1,45 @@
 package com.thealgorithms.strings;
 
-/* In information theory, the Hamming distance between two strings of equal length
-is the number of positions at which the corresponding symbols are different.
-https://en.wikipedia.org/wiki/Hamming_distance
-*/
+/**
+ * Class for calculating the Hamming distance between two strings of equal length.
+ * <p>
+ * The Hamming distance is the number of positions at which the corresponding symbols are different.
+ * It is used in information theory, coding theory, and computer science.
+ * </p>
+ * @see <a href="https://en.wikipedia.org/wiki/Hamming_distance">Hamming distance - Wikipedia</a>
+ */
 public final class HammingDistance {
     private HammingDistance() {
     }
 
     /**
-     * calculate the hamming distance between two strings of equal length
+     * Calculates the Hamming distance between two strings of equal length.
+     * <p>
+     * The Hamming distance is defined only for strings of equal length. If the strings are not
+     * of equal length, this method throws an {@code IllegalArgumentException}.
+     * </p>
      *
      * @param s1 the first string
      * @param s2 the second string
-     * @return {@code int} hamming distance
-     * @throws Exception
+     * @return the Hamming distance between the two strings
+     * @throws IllegalArgumentException if the lengths of {@code s1} and {@code s2} are not equal
      */
-    public static int calculateHammingDistance(String s1, String s2) throws Exception {
-        if (s1.length() != s2.length()) {
-            throw new Exception("String lengths must be equal");
+    public static int calculateHammingDistance(String s1, String s2) {
+        if (s1 == null || s2 == null) {
+            throw new IllegalArgumentException("Strings must not be null");
         }
 
-        int stringLength = s1.length();
-        int counter = 0;
+        if (s1.length() != s2.length()) {
+            throw new IllegalArgumentException("String lengths must be equal");
+        }
 
-        for (int i = 0; i < stringLength; i++) {
+        int distance = 0;
+
+        for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
-                counter++;
+                distance++;
             }
         }
-        return counter;
+        return distance;
     }
 }
