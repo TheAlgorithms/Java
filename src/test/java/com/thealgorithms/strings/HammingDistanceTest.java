d1445c1514927d422e692043ceb76ec915d7fc92
alxkm <alx@alx.com>
2024-08-26 15:40:37 +0200

    checkstyle: fix formatting

--- src/test/java/com/thealgorithms/strings/HammingDistanceTest.java
+++ src/test/java/com/thealgorithms/strings/HammingDistanceTest.java
@@ -3,13 +3,12 @@ package com.thealgorithms.strings;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 class HammingDistanceTest {
 
