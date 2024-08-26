9100a0c4b442a11f2f18b4db4dd401cb7351c649
alxkm <alx@alx.com>
2024-08-26 23:39:52 +0200

    refactor: ReturnSubsequence

--- src/main/java/com/thealgorithms/others/ReturnSubsequence.java
+++ src/main/java/com/thealgorithms/others/ReturnSubsequence.java
@@ -1,46 +1,37 @@
 package com.thealgorithms.others;
 
-import java.util.Scanner;
-
+/**
+ * Class for generating all subsequences of a given string.
+ */
 public final class ReturnSubsequence {
     private ReturnSubsequence() {
     }
 
-    public static void main(String[] args) {
-        System.out.println("Enter String: ");
-        Scanner s = new Scanner(System.in);
-        String givenString = s.next(); // given string
-        String[] subsequence = returnSubsequence(givenString); // calling returnSubsequence() function
-        System.out.println("Subsequences : ");
-        // print the given array of subsequences
-        for (int i = 0; i < subsequence.length; i++) {
-            System.out.println(subsequence[i]);
-        }
-        s.close();
-    }
-
     /**
-     * @param givenString
-     * @return subsequence
+     * Generates all subsequences of the given string.
+     *
+     * @param input The input string.
+     * @return An array of subsequences.
      */
-    private static String[] returnSubsequence(String givenString) {
-        if (givenString.length() == 0) { // in it // If string is empty we will create an array of
-                                         // size=1 and insert "" (Empty string)
-            String[] ans = new String[1];
-            ans[0] = "";
-            return ans;
+    public static String[] getSubsequences(String input) {
+        if (input.isEmpty()) {
+            return new String[] {""}; // Return array with an empty string if input is empty
         }
-        String[] smallAns = returnSubsequence(givenString.substring(1)); // recursive call to get subsequences of substring starting from index
-        // position=1
 
-        String[] ans = new String[2 * smallAns.length]; // Our answer will be an array off string of size=2*smallAns
-        System.arraycopy(smallAns, 0, ans, 0, smallAns.length);
+        // Recursively find subsequences of the substring (excluding the first character)
+        String[] smallerSubsequences = getSubsequences(input.substring(1));
 
-        for (int k = 0; k < smallAns.length; k++) {
-            ans[k + smallAns.length] = givenString.charAt(0) + smallAns[k]; // Insert character at index=0 of the given
-                                                                            // substring in front of every string
-            // in smallAns
+        // Create an array to hold the final subsequences, double the size of smallerSubsequences
+        String[] result = new String[2 * smallerSubsequences.length];
+
+        // Copy the smaller subsequences directly to the result array
+        System.arraycopy(smallerSubsequences, 0, result, 0, smallerSubsequences.length);
+
+        // Prepend the first character of the input string to each of the smaller subsequences
+        for (int i = 0; i < smallerSubsequences.length; i++) {
+            result[i + smallerSubsequences.length] = input.charAt(0) + smallerSubsequences[i];
         }
-        return ans;
+
+        return result;
     }
 }
