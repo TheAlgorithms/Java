ca00cadb354859734dd18649b5ef7912e8967002
alxkm <alx@alx.com>
2024-08-27 23:47:00 +0200

    refactor: LongestNonRepetitiveSubstring

--- src/test/java/com/thealgorithms/strings/LongestNonRepetitiveSubstringTest.java
+++ src/test/java/com/thealgorithms/strings/LongestNonRepetitiveSubstringTest.java
@@ -1,15 +1,22 @@
 package com.thealgorithms.strings;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class LongestNonRepetitiveSubstringTest {
 
-    @Test
-    public void palindrome() {
-        String input1 = "HelloWorld";
-        String input2 = "javaIsAProgrammingLanguage";
-        Assertions.assertEquals(LongestNonRepetitiveSubstring.lengthOfLongestSubstring(input1), 5);
-        Assertions.assertEquals(LongestNonRepetitiveSubstring.lengthOfLongestSubstring(input2), 9);
+    private static Stream<Arguments> provideTestCases() {
+        return Stream.of(Arguments.of("", 0), Arguments.of("a", 1), Arguments.of("abcde", 5), Arguments.of("aaaaa", 1), Arguments.of("abca", 3), Arguments.of("abcdeabc", 5),
+                Arguments.of("a1b2c3", 6), Arguments.of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 62), Arguments.of("aabb", 2), Arguments.of("abcdefghijabc", 10));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void testLengthOfLongestSubstring(String input, int expectedLength) {
+        assertEquals(expectedLength, LongestNonRepetitiveSubstring.lengthOfLongestSubstring(input));
     }
 }
