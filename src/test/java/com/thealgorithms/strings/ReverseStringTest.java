89873bc56ddd57a556eed2ce6a0f6ee0022dab9c
alxkm <alx@alx.com>
2024-08-27 10:46:54 +0200

    refactor: refactor testing into two methods

--- src/test/java/com/thealgorithms/strings/ReverseStringTest.java
+++ src/test/java/com/thealgorithms/strings/ReverseStringTest.java
@@ -2,15 +2,27 @@ package com.thealgorithms.strings;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class ReverseStringTest {
 
+    private static Stream<Arguments> testCases() {
+        return Stream.of(Arguments.of("Hello World", "dlroW olleH"), Arguments.of("helloworld", "dlrowolleh"), Arguments.of("123456789", "987654321"), Arguments.of("", ""), Arguments.of("A", "A"), Arguments.of("ab", "ba"), Arguments.of("  leading and trailing spaces  ", "  secaps gniliart dna gnidael  "), Arguments.of("!@#$%^&*()", ")(*&^%$#@!"), Arguments.of("MixOf123AndText!", "!txeTdnA321fOxiM"));
+    }
+
     @ParameterizedTest
-    @CsvSource({"'Hello World', 'dlroW olleH'", "'helloworld', 'dlrowolleh'", "'123456789', '987654321'", "'', ''", "'A', 'A'", "'ab', 'ba'", "'  leading and trailing spaces  ', '  secaps gniliart dna gnidael  '", "'!@#$%^&*()', ')(*&^%$#@!'", "'MixOf123AndText!', '!txeTdnA321fOxiM'"})
+    @MethodSource("testCases")
     public void testReverseString(String input, String expectedOutput) {
         assertEquals(expectedOutput, ReverseString.reverse(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    public void testReverseString2(String input, String expectedOutput) {
         assertEquals(expectedOutput, ReverseString.reverse2(input));
     }
 }
