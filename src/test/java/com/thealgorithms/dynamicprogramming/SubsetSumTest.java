f4103db5907d5b944495b1f3af49dba32266d063
alxkm <alx@alx.com>
2024-08-29 21:55:06 +0200

    checkstyle: fix formatting

--- src/test/java/com/thealgorithms/dynamicprogramming/SubsetSumTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/SubsetSumTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class SubsetSumTest {
 
-    record TestCase(int[] arr, int sum, boolean expected) {}
+    record TestCase(int[] arr, int sum, boolean expected) {
+    }
 
     @ParameterizedTest
     @MethodSource("provideTestCases")
@@ -17,7 +18,7 @@ class SubsetSumTest {
     }
 
     private static Stream<TestCase> provideTestCases() {
-        return Stream.of(new TestCase(new int[] {50, 4, 10, 15, 34}, 64, true), new TestCase(new int[] {50, 4, 10, 15, 34}, 99, true), new TestCase(new int[] {50, 4, 10, 15, 34}, 5, false),
-            new TestCase(new int[] {50, 4, 10, 15, 34}, 66, false), new TestCase(new int[] {}, 0, true), new TestCase(new int[] {1, 2, 3}, 6, true), new TestCase(new int[] {1, 2, 3}, 7, false), new TestCase(new int[] {3, 34, 4, 12, 5, 2}, 9, true));
+        return Stream.of(new TestCase(new int[] {50, 4, 10, 15, 34}, 64, true), new TestCase(new int[] {50, 4, 10, 15, 34}, 99, true), new TestCase(new int[] {50, 4, 10, 15, 34}, 5, false), new TestCase(new int[] {50, 4, 10, 15, 34}, 66, false), new TestCase(new int[] {}, 0, true),
+            new TestCase(new int[] {1, 2, 3}, 6, true), new TestCase(new int[] {1, 2, 3}, 7, false), new TestCase(new int[] {3, 34, 4, 12, 5, 2}, 9, true));
     }
 }
