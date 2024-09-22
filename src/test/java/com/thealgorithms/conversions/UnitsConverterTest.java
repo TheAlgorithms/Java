8cf21f7d66e264471eb8f94c8202e8aa9fcb7db1
vil02 <65706193+vil02@users.noreply.github.com>
2024-09-22 10:34:27 +0200

    fix: handle Null Dereference in `UnitsConverter`

--- src/test/java/com/thealgorithms/conversions/UnitsConverterTest.java
+++ src/test/java/com/thealgorithms/conversions/UnitsConverterTest.java
@@ -4,6 +4,7 @@ import static java.util.Map.entry;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,13 @@ public class UnitsConverterTest {
         final UnitsConverter someConverter = new UnitsConverter(Map.ofEntries(entry(Pair.of("A", "B"), new AffineConverter(10.0, -20.0))));
         assertThrows(IllegalArgumentException.class, () -> someConverter.convert("A", "A", 20.0));
         assertThrows(IllegalArgumentException.class, () -> someConverter.convert("B", "B", 20.0));
+    }
+
+    @Test
+    void testConvertThrowsForUnknownUnits() {
+        final UnitsConverter someConverter = new UnitsConverter(Map.ofEntries(entry(Pair.of("A", "B"), new AffineConverter(10.0, -20.0))));
+        assertThrows(NoSuchElementException.class, () -> someConverter.convert("A", "X", 20.0));
+        assertThrows(NoSuchElementException.class, () -> someConverter.convert("X", "A", 20.0));
+        assertThrows(NoSuchElementException.class, () -> someConverter.convert("X", "Y", 20.0));
     }
 }
