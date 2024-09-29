d89416dee90e66c84277ee73c8ad71e87a7ba024
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2024-09-29 18:45:25 +0300

    Add Function volumeFrustum To Calculate Frustum Of Cone Then Test It.

--- src/test/java/com/thealgorithms/maths/VolumeTest.java
+++ src/test/java/com/thealgorithms/maths/VolumeTest.java
@@ -32,5 +32,9 @@ public class VolumeTest {
 
         /* test pyramid */
         assertTrue(Volume.volumePyramid(10, 3) == 10.0);
+
+        /* test frustum */
+        assertTrue(Volume.volumeFrustum(3, 5, 7) == 359.188760060433);
+
     }
 }
