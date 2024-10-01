1f6f98a8600c0650e3336d1699172a51a5e04859
Luiz <lcsjuniorsi@gmail.com>
2024-10-01 00:58:08 -0300

    fix clang format

--- src/main/java/com/thealgorithms/ciphers/XORCipher.java
+++ src/main/java/com/thealgorithms/ciphers/XORCipher.java
@@ -8,33 +8,32 @@ import java.util.Base64;
  * A simple implementation of XOR cipher that, given a key, allows to encrypt and decrypt a plaintext.
  *
  * @author <a href="https://github.com/lcsjunior">lcsjunior</a>
- * 
+ *
  */
 public class XORCipher {
 
-	private static final Charset CS_DEFAULT = StandardCharsets.UTF_8;
+    private static final Charset CS_DEFAULT = StandardCharsets.UTF_8;
 
-	private XORCipher() {
-	}
+    private XORCipher() {
+    }
 
-	private static byte[] xor(final byte[] inputBytes, final byte[] keyBytes) {
-	    byte[] outputBytes = new byte[inputBytes.length];
-	    for (int i = 0; i < inputBytes.length; ++i) {
-	        outputBytes[i] = (byte) (inputBytes[i] ^ keyBytes[i % keyBytes.length]);
-	    }
-	    return outputBytes;
-	}
+    private static byte[] xor(final byte[] inputBytes, final byte[] keyBytes) {
+        byte[] outputBytes = new byte[inputBytes.length];
+        for (int i = 0; i < inputBytes.length; ++i) {
+            outputBytes[i] = (byte) (inputBytes[i] ^ keyBytes[i % keyBytes.length]);
+        }
+        return outputBytes;
+    }
 
-	public static String encrypt(final String plaintext, final String key) {
-		byte[] plaintextBytes = plaintext.getBytes(CS_DEFAULT);
-		byte[] keyBytes = key.getBytes(CS_DEFAULT);
-		return Base64.getEncoder().encodeToString(xor(plaintextBytes, keyBytes));
-	}
+    public static String encrypt(final String plaintext, final String key) {
+        byte[] plaintextBytes = plaintext.getBytes(CS_DEFAULT);
+        byte[] keyBytes = key.getBytes(CS_DEFAULT);
+        return Base64.getEncoder().encodeToString(xor(plaintextBytes, keyBytes));
+    }
 
-	public static String decrypt(final String cipher, final String key) {
-		byte[] cipherBytes = Base64.getDecoder().decode(cipher);
-		byte[] keyBytes = key.getBytes(CS_DEFAULT);
-		return new String(xor(cipherBytes, keyBytes), CS_DEFAULT);
-	}
-
+    public static String decrypt(final String cipher, final String key) {
+        byte[] cipherBytes = Base64.getDecoder().decode(cipher);
+        byte[] keyBytes = key.getBytes(CS_DEFAULT);
+        return new String(xor(cipherBytes, keyBytes), CS_DEFAULT);
+    }
 }
--- src/test/java/com/thealgorithms/ciphers/XORCipherTest.java
+++ src/test/java/com/thealgorithms/ciphers/XORCipherTest.java
@@ -8,13 +8,12 @@ class XORCipherTest {
 
     @Test
     void shouldEncryptAndDecryptTest() {
-    	// given
-    	String plaintext = "My t&xt th@t will be ençrypted...";
-    	String key = "My ç&cret key!";
-    	// when
-    	String cipherText = XORCipher.encrypt(plaintext, key);
-    	// then
-    	assertEquals(XORCipher.decrypt(cipherText, key), plaintext);
+        // given
+        String plaintext = "My t&xt th@t will be ençrypted...";
+        String key = "My ç&cret key!";
+        // when
+        String cipherText = XORCipher.encrypt(plaintext, key);
+        // then
+        assertEquals(XORCipher.decrypt(cipherText, key), plaintext);
     }
-
 }
