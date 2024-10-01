3e9faea749f79c857c42d7bdd77fcb3133e472f1
Ritisha Pande <96540421+riti2601@users.noreply.github.com>
2024-10-01 22:51:18 +0530

    Update MonoAlphabetic.java

--- src/main/java/com/thealgorithms/ciphers/MonoAlphabetic.java
+++ src/main/java/com/thealgorithms/ciphers/MonoAlphabetic.java
@@ -8,68 +8,65 @@ public class MonoAlphabetic {
         String name = read.nextLine();
         read.nextLine();
 
-        System.out.println("Welcome "+name+"!\nDo you want to encrypt data or decrypt the data?\nFor encryption enter: 1\nFor decryption enter: 2");
-        int x = read.nextInt();
+        System.out.println("Welcome " + name + "!\nDo you want to encrypt data or decrypt the data?\nFor encryption enter: 1\nFor decryption enter: 2");        int x = read.nextInt();
         read.nextLine();
 
         String key = "MNBVCXZLKJHGFDSAPOIUYTREWQ";
 
         switch (x) {
             case 1:
-                System.out.println("\nPlease enter the data that is to be encrypted, we will be using MonoAlphabetic Cipher to encrypt the data.");
-                String data = read.nextLine().toUpperCase();;
-                
-                String encryptedData = encrypt(data,key);
-                System.out.println("Encrypted data: " + encryptedData);
-                break;
-            
-            case 2:
-                System.out.println("\nPlease enter the data that is to be decrypted, we will be using MonoAlphabetic Cipher to decrypt the data.");
-                data = read.nextLine().toUpperCase();;
-                
-                String decryptedData = decrypt(data,key);
-                System.out.println("Decrypted data: " + decryptedData);
-                break;
-        
-            default:
-                System.out.println("The input was invalid. Kindly restart.");
-                break;
-        }
-
-        
+                 System.out.println("\nPlease enter the data that is to be encrypted, we will be using MonoAlphabetic Cipher to encrypt the data.");
+                 String data = read.nextLine().toUpperCase();
+ 
+                 String encryptedData = encrypt(data, key);
+                 System.out.println("Encrypted data: " + encryptedData);
+                 break;
+ 
+             case 2:
+                 System.out.println("\nPlease enter the data that is to be decrypted, we will be using MonoAlphabetic Cipher to decrypt the data.");
+                 data = read.nextLine().toUpperCase();
+ 
+                 String decryptedData = decrypt(data, key);
+                 System.out.println("Decrypted data: " + decryptedData);
+                 break;
+ 
+             default:
+                 System.out.println("The input was invalid. Kindly restart.");
+                 break;        
+        }  
     }
 
-    public static String encrypt(String data, String key){
+    public static String encrypt(String data, String key) {
         int idx;
         char c;
         StringBuffer sb = new StringBuffer(data);
 
-        for(int i=0; i<sb.length(); i++){
-            idx = sb.charAt(i)-65;
+        for(int i=0; i<sb.length(); i++) {
+            idx = sb.charAt(i) - 65;
             c = key.charAt(idx);
             sb.setCharAt(i, c);
         }
         return new String(sb);
     }
 
-    public static String decrypt(String data, String key){
+    public static String decrypt(String data, String key) {
         int idx;
         char c;
         StringBuffer sb = new StringBuffer(data);
 
-        for(int i=0; i<sb.length(); i++){
+        for(int i=0; i<sb.length(); i++) {
             c = sb.charAt(i);
-            idx = getIndex(c,key);
+            idx = getIndex(c, key);
             c = (char) (idx + 65);
             sb.setCharAt(i, c);
         }
         return new String(sb);
     }
 
-    public static int getIndex(char c, String key){
+    public static int getIndex(char c, String key) {
         int idx = -1;
-        for(int i=0; i<key.length();i++){
-            if(key.charAt(i) == c){
+        for(int i=0; i<key.length();i++) {
+            if(key.charAt(i) == c) {
                 idx = i;
             }
         }
