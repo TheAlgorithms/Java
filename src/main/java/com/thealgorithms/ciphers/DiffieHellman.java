835783dca068c013aa3aeb2b767bf964d4e06606
Ritisha Pande <96540421+riti2601@users.noreply.github.com>
2024-10-01 22:10:31 +0530

    Create DiffieHellman.java

---
+++ src/main/java/com/thealgorithms/ciphers/DiffieHellman.java
@@ -0,0 +1,33 @@
+import java.math.BigInteger;
+import java.util.*;
+
+public class DiffieHellman {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Hello User! \nEnter your name:");
+        String name = read.nextLine();
+        read.nextLine(); 
+        System.out.println("Welcome "+name+"!");
+
+        BigInteger n,g,x,y,k1,k2,A,B;
+
+        System.out.println("Enter two prime numbers: ");
+        n = new BigInteger(read.next());
+        g = new BigInteger(read.next());
+
+        System.out.println("Person A : Enter your secret number");
+        x = new BigInteger(read.next());
+        A = g.modPow(x, n);
+
+        System.out.println("Person B : Enter your secret number");
+        y = new BigInteger(read.next());
+        B = g.modPow(y, n);
+
+        k1 = B.modPow(x, n);
+        k2 = A.modPow(y, n);
+
+        System.out.println("A's secret key: "+k1);
+        System.out.println("B's secret key: "+k2);
+  
+    }
+}
