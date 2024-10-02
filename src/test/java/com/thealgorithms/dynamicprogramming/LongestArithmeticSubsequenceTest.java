47cccfef333cafe61ff89882cab5ccee9d65fa81
Tejaswi Tyagi <98461855+tejaswi0910@users.noreply.github.com>
2024-10-02 10:37:19 +0000

    clang format changes

--- src/test/java/com/thealgorithms/dynamicprogramming/LongestArithmeticSubsequenceTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/LongestArithmeticSubsequenceTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.apache.commons.lang3.ArrayUtils;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
