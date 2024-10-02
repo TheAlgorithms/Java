06c996e56c1bb68eda4040ca6fbc9260b106783e
HemanthTigga <hemumani73@gmail.com>
2024-10-02 17:57:08 +0530

    Added Maximum Sum of Non-Adjacent Elements Algorithm

--- src/main/java/com/thealgorithms/dynamicprogramming/MaxSumNonAdjacent.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/MaxSumNonAdjacent.java
@@ -16,43 +16,30 @@ it (and keep the sum up to the previous element).*/
 // "https://medium.com/@amitrajit_bose/max-sum-of-non-adjacent-elements-a04ebc4f2602"
 
 public class MaxSumNonAdjacent {
-    // This function recursively calculates the maximum possible sum
-    // by considering or not considering the current element.
-    static int solveUtil(int ind, int[] arr, int[] dp) {
-        // If the index is negative, there are no elements left to consider.
-        if (ind < 0) {
+    static int solveUtil(int index, int[] arr, int[] dp) {
+        if (index < 0) {
             return 0;
         }
 
-        // If the index is 0, there is only one element to consider, so return its
-        // value.
-        if (ind == 0) {
-            return arr[ind];
+        if (index == 0) {
+            return arr[index];
         }
 
-        // If we have already calculated the result for this index, return it.
-        if (dp[ind] != -1) {
-            return dp[ind];
+        if (dp[index] != -1) {
+            return dp[index];
         }
 
-        // Calculate the maximum sum by either picking the current element or not
-        // picking it.
-        int pick = arr[ind] + solveUtil(ind - 2, arr, dp);
-        int nonPick = solveUtil(ind - 1, arr, dp);
+        int pick = arr[index] + solveUtil(index - 2, arr, dp);
+        int nonPick = solveUtil(index - 1, arr, dp);
 
-        // Store the maximum of the two options in the dp array for future reference.
-        return dp[ind] = Math.max(pick, nonPick);
+        return dp[index] = Math.max(pick, nonPick);
     }
 
-    // This function initializes the dp array and calls the recursive solver.
     static int solve(int n, int[] arr) {
         int[] dp = new int[n];
 
-        // Initialize the dp array with -1 to indicate that values are not calculated
-        // yet.
         Arrays.fill(dp, -1);
 
-        // Call the recursive solver for the last index (n-1).
         return solveUtil(n - 1, arr, dp);
     }
 }
