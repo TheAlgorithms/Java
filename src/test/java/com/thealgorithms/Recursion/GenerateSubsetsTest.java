d3d154e9324aa04592483ddc60edc7a0f2bcfb7b
Tuhinm2002 <75078694+Tuhinm2002@users.noreply.github.com>
2024-10-02 16:13:40 +0530

    Update GenerateSubsetsTest.java

--- src/test/java/com/thealgorithms/Recursion/GenerateSubsetsTest.java
+++ src/test/java/com/thealgorithms/Recursion/GenerateSubsetsTest.java
@@ -5,14 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
-public final class GenerateSubsetsTest {
+public class GenerateSubsetsTest {
 
     @Test
     void subsetRecursionTestOne() {
         String str = "abc";
         String[] expected = new String[] {"abc", "ab", "ac", "a", "bc", "b", "c", ""};
 
-        List<String> ans = GenerateSubsets.subsetRecursion("", str);
+        List<String> ans = GenerateSubsets.subsetRecursion(str);
         assertArrayEquals(ans.toArray(), expected);
     }
 
@@ -21,7 +21,7 @@ public final class GenerateSubsetsTest {
         String str = "cbf";
         String[] expected = new String[] {"cbf", "cb", "cf", "c", "bf", "b", "f", ""};
 
-        List<String> ans = GenerateSubsets.subsetRecursion("", str);
+        List<String> ans = GenerateSubsets.subsetRecursion(str);
         assertArrayEquals(ans.toArray(), expected);
     }
 
@@ -30,7 +30,7 @@ public final class GenerateSubsetsTest {
         String str = "aba";
         String[] expected = new String[] {"aba", "ab", "aa", "a", "ba", "b", "a", ""};
 
-        List<String> ans = GenerateSubsets.subsetRecursion("", str);
+        List<String> ans = GenerateSubsets.subsetRecursion(str);
         assertArrayEquals(ans.toArray(), expected);
     }
 }
