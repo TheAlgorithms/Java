3ec762783b4e3a96ba686e6c1771de17e834b2f7
mjk22071998 <mjk22071998@gmail.com>
2024-10-02 16:56:07 +0500

    Upgraded test from junit 4 to junit 5

--- src/test/java/com/thealgorithms/datastructures/lists/SortedlinkedListTest.java
+++ src/test/java/com/thealgorithms/datastructures/lists/SortedlinkedListTest.java
@@ -1,10 +1,10 @@
 package com.thealgorithms.datastructures.lists;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class SortedLinkedListTest {
     
