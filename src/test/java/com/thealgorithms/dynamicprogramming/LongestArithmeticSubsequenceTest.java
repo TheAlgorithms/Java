f6cd84318828b6b9b1268ca659316210ccd2bfef
Tejaswi Tyagi <98461855+tejaswi0910@users.noreply.github.com>
2024-10-02 10:17:01 +0000

    clang format changes done

--- src/test/java/com/thealgorithms/dynamicprogramming/LongestArithmeticSubsequenceTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/LongestArithmeticSubsequenceTest.java
@@ -4,10 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.stream.Stream;
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.apache.commons.lang3.ArrayUtils;
+
 
 public class LongestArithmeticSubsequenceTest {
 
@@ -25,6 +26,6 @@ public class LongestArithmeticSubsequenceTest {
     }
 
     private static Stream<Arguments> provideTestCases() {
-        return Stream.of(Arguments.of(new int[]{3, 6, 9, 12, 15}, 5), Arguments.of(new int[]{1, 7, 10, 13, 14, 19}, 4), Arguments.of(new int[]{1, 2, 3, 4}, 4), Arguments.of(new int[]{}, 0), Arguments.of(new int[]{10}, 1), Arguments.of(new int[]{9, 4, 7, 2, 10}, 3));
+        return Stream.of(Arguments.of(new int[] {3, 6, 9, 12, 15}, 5), Arguments.of(new int[] {1, 7, 10, 13, 14, 19}, 4), Arguments.of(new int[] {1, 2, 3, 4}, 4), Arguments.of(new int[] {}, 0), Arguments.of(new int[] {10}, 1), Arguments.of(new int[] {9, 4, 7, 2, 10}, 3));
     }
 }
