107258044031af19e8b02a9d0e94972301aa9b14
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-02 16:43:26 +0530

    Remove unused import

--- src/test/java/com/thealgorithms/backtracking/MColoringTest.java
+++ src/test/java/com/thealgorithms/backtracking/MColoringTest.java
@@ -1,6 +1,5 @@
 package com.thealgorithms.backtracking;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
