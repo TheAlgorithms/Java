bf1002a551933f2be51c6bead67ee7af0d250827
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-02 20:12:34 +0530

    Remove test

--- src/test/java/com/thealgorithms/backtracking/SudokuSolverTest.java
+++ src/test/java/com/thealgorithms/backtracking/SudokuSolverTest.java
@@ -33,23 +33,6 @@ class SudokuSolverTest {
     }
 
     /**
-     * Test case for unsolvable Sudoku puzzle.
-     */
-    @Test
-    void testForUnsolvableSudoku() {
-        int[][] board = {{5, 3, 0, 0, 7, 0, 0, 0, 0}, {6, 0, 0, 1, 9, 5, 0, 0, 0}, {0, 9, 8, 0, 0, 0, 0, 6, 0}, {8, 0, 0, 0, 6, 0, 0, 0, 3}, {4, 0, 0, 8, 0, 3, 0, 0, 1}, {7, 0, 0, 0, 2, 0, 0, 0, 6}, {0, 6, 0, 0, 0, 0, 2, 8, 0}, {0, 0, 0, 4, 1, 9, 0, 0, 5}, {0, 0, 0, 0, 8, 0, 0, 7, 9}};
-
-        // Modify the puzzle to make it unsolvable (e.g., repeat numbers)
-        board[0][0] = 5;
-
-        // Attempt to solve the unsolvable puzzle
-        boolean solved = SudokuSolver.solveSudoku(board);
-
-        // Check that the puzzle is not solvable
-        assertFalse(solved, "The Sudoku puzzle should be unsolvable.");
-    }
-
-    /**
      * Test case for an already solved Sudoku puzzle.
      */
     @Test
