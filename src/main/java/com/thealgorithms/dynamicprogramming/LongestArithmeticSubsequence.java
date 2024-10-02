bd24402d2c47668ebc30dbfec5661af37067b5fc
Tejaswi Tyagi <98461855+tejaswi0910@users.noreply.github.com>
2024-10-02 10:11:48 +0000

    Added exception for null case

--- src/main/java/com/thealgorithms/dynamicprogramming/LongestArithmeticSubsequence.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/LongestArithmeticSubsequence.java
@@ -17,24 +17,24 @@ final class LongestArithmeticSubsequence {
      * @return the length of the longest arithmetic subsequence
      */
     public static int getLongestArithmeticSubsequenceLength(int[] nums) {
+        // Throws an exception if nums is null.
+        if (nums == null) {
+            throw new IllegalArgumentException("Input array cannot be null");
+        }
+        
         // If the array is empty or has only one element, return its length.
-        if (nums == null || nums.length == 0) {
-            return 0;
+        if (nums.length == 1 || nums.length == 0) {
+            return nums.length;
         }
 
-        if (nums.length == 1) {
-            return 1;
-        }
-
-        int n = nums.length;
-        HashMap<Integer, Integer>[] dp = new HashMap[n];
+        HashMap<Integer, Integer>[] dp = new HashMap[nums.length];
         int maxLength = 2;
 
         // Initialize dp array
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.length; i++) {
             dp[i] = new HashMap<>();
             for (int j = 0; j < i; j++) {
-                int diff = nums[i] - nums[j];
+                final int diff = nums[i] - nums[j];
                 dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
                 maxLength = Math.max(maxLength, dp[i].get(diff));
             }
