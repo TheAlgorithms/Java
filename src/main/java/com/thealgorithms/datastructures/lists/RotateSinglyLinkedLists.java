76b490c9f0dd2fa24a41a60d67fd1dbeccfb353e
Swastik-Swarup-Dash <143310346+Swastik-Swarup-Dash@users.noreply.github.com>
2024-10-02 16:29:52 +0530

    refactor:RotateSinglyLinkedLists.java making minute changes

--- src/main/java/com/thealgorithms/datastructures/lists/RotateSinglyLinkedLists.java
+++ src/main/java/com/thealgorithms/datastructures/lists/RotateSinglyLinkedLists.java
@@ -1,52 +1,34 @@
 package com.thealgorithms.datastructures.lists;
 
 /**
- * Rotate a singly linked list
- * 
+ * Rotate a singly linked list to the right
  */
-
 public class RotateSinglyLinkedLists {
-
     public Node rotateRight(Node head, int k) {
         if (head == null || head.next == null || k == 0) {
             return head;
         }
 
-        int length = getLength(head);
+        Node last = head;
+        int length = 1;
+        while (last.next != null) {
+            last = last.next;
+            length++;
+        }
+
         k = k % length;
         if (k == 0) {
             return head;
         }
 
-        Node last = getLastNode(head);
-        last.next = head;
-
-        int stepsToNewHead = length - k;
         Node newLast = head;
-        while (stepsToNewHead > 1) {
+        for (int i = 0; i < length - k - 1; i++) {
             newLast = newLast.next;
-            stepsToNewHead--;
         }
-        head = newLast.next;
+
+        Node newHead = newLast.next;
         newLast.next = null;
-        return head;
-    }
-
-    private int getLength(Node head) {
-        int length = 0;
-        Node current = head;
-        while (current != null) {
-            length++;
-            current = current.next;
-        }
-        return length;
-    }
-
-    private Node getLastNode(Node head) {
-        Node last = head;
-        while (last.next != null) {
-            last = last.next;
-        }
-        return last;
+        last.next = head;
+        return newHead;
     }
 }
