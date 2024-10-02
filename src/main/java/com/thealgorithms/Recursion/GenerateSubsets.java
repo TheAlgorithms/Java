e49a4142b172417c94217d63d25657c178da89e6
Tuhinm2002 <75078694+Tuhinm2002@users.noreply.github.com>
2024-10-02 14:53:54 +0530

    Update and rename Subsets.java to GenerateSubsets.java

---
+++ src/main/java/com/thealgorithms/Recursion/GenerateSubsets.java
@@ -0,0 +1,28 @@
+package com.thealgorithms.Recursion;
+
+// program to find power set of a string
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateSubsets {
+
+    public static List<String> subsetRecursion(String p, String up) {
+        if (up.isEmpty()) {
+            List<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        // Taking the character
+        char ch = up.charAt(0);
+        // Adding the character in the recursion
+        List<String> left = subsetRecursion(p + ch, up.substring(1));
+        // Not adding the character in the recursion
+        List<String> right = subsetRecursion(p, up.substring(1));
+
+        left.addAll(right);
+
+        return left;
+    }
+}
