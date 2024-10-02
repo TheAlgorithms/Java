25be88e4a9b958cd8715da86019131731fb04b3a
mjk22071998 <mjk22071998@gmail.com>
2024-10-02 18:09:29 +0500

    formatting of test file corrected

--- src/test/java/com/thealgorithms/datastructures/lists/SortedLinkedListTest.java
+++ src/test/java/com/thealgorithms/datastructures/lists/SortedLinkedListTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class SortedLinkedListTest {
-    
+
     @Test
     public void testInsert() {
         SortedLinkedList list = new SortedLinkedList();
