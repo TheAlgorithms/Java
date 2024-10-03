268271d46357ee60f1faa6183c3b324774286232
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-03 20:36:51 +0530

    Fix

--- src/test/java/com/thealgorithms/divideandconquer/ClosestPairTest.java
+++ src/test/java/com/thealgorithms/divideandconquer/ClosestPairTest.java
@@ -2,7 +2,6 @@ package com.thealgorithms.divideandconquer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
