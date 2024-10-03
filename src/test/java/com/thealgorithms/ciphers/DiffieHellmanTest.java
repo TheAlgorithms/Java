c54c74af3a5e5fbaf46f3ad4da39522857ac56ab
Ritisha Pande <96540421+riti2601@users.noreply.github.com>
2024-10-03 09:25:02 +0530

    Update DiffieHellmanTest.java

--- src/test/java/com/thealgorithms/ciphers/DiffieHellmanTest.java
+++ src/test/java/com/thealgorithms/ciphers/DiffieHellmanTest.java
@@ -28,11 +28,11 @@ public class DiffieHellmanTest {
 
     // Provide test data for public key calculation
     private static Stream<Arguments> providePublicKeyData() {
-        return Stream.of(Arguments.of(new BigInteger("5"), new BigInteger("6"), new BigInteger("23"), new BigInteger("8")), Arguments.of(new BigInteger("2"), new BigInteger("5"), new BigInteger("13"), new BigInteger("6")));
+        return Stream.of(Arguments.of(BigInteger.valueOf(5), BigInteger.valueOf(6), BigInteger.valueOf(23), BigInteger.valueOf(8)), Arguments.of(BigInteger.valueOf(2), BigInteger.valueOf(5), BigInteger.valueOf(13), BigInteger.valueOf(6)));
     }
 
     // Provide test data for shared secret calculation
     private static Stream<Arguments> provideSharedSecretData() {
-        return Stream.of(Arguments.of(new BigInteger("5"), new BigInteger("6"), new BigInteger("23"), new BigInteger("8"), new BigInteger("13")), Arguments.of(new BigInteger("2"), new BigInteger("5"), new BigInteger("13"), new BigInteger("6"), new BigInteger("2")));
+        return Stream.of(Arguments.of(BigInteger.valueOf(5), BigInteger.valueOf(6), BigInteger.valueOf(23), BigInteger.valueOf(8), BigInteger.valueOf(13)), Arguments.of(BigInteger.valueOf(2), BigInteger.valueOf(5), BigInteger.valueOf(13), BigInteger.valueOf(6), BigInteger.valueOf(2)));
     }
 }
