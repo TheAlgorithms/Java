f5f27466365eda9fc36d5864ed6e8c681f4f215c
Ritisha Pande <96540421+riti2601@users.noreply.github.com>
2024-10-03 09:10:31 +0530

    Update MonoAlphabeticTest.java

--- src/test/java/com/thealgorithms/ciphers/MonoAlphabeticTest.java
+++ src/test/java/com/thealgorithms/ciphers/MonoAlphabeticTest.java
@@ -9,29 +9,29 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 public class MonoAlphabeticTest {
 
-    private static final String key = "MNBVCXZLKJHGFDSAPOIUYTREWQ";
+    private static final String KEY = "MNBVCXZLKJHGFDSAPOIUYTREWQ";
 
     // Test for encryption
     @ParameterizedTest
     @MethodSource("provideEncryptionData")
-    public void testEncrypt(String data, String key, String expected) {
-        assertEquals(expected, MonoAlphabetic.encrypt(data, key));
+    public void testEncrypt(String data, String KEY, String expected) {
+        assertEquals(expected, MonoAlphabetic.encrypt(data, KEY));
     }
 
     // Test for decryption
     @ParameterizedTest
     @MethodSource("provideDecryptionData")
-    public void testDecrypt(String data, String key, String expected) {
-        assertEquals(expected, MonoAlphabetic.decrypt(data, key));
+    public void testDecrypt(String data, String KEY, String expected) {
+        assertEquals(expected, MonoAlphabetic.decrypt(data, KEY));
     }
 
     // Provide test data for encryption
     private static Stream<Arguments> provideEncryptionData() {
-        return Stream.of(Arguments.of("HELLO", key, "LCGGS"), Arguments.of("JAVA", key, "JMTM"));
+        return Stream.of(Arguments.of("HELLO", KEY, "LCGGS"), Arguments.of("JAVA", KEY, "JMTM"));
     }
 
     // Provide test data for decryption
     private static Stream<Arguments> provideDecryptionData() {
-        return Stream.of(Arguments.of("LCGGS", key, "HELLO"), Arguments.of("JMTM", key, "JAVA"));
+        return Stream.of(Arguments.of("LCGGS", KEY, "HELLO"), Arguments.of("JMTM", KEY, "JAVA"));
     }
 }
