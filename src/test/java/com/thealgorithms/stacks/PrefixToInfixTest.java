ff1ad5900231546aa3b46a672074c39ae3bb0e2d
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-03 19:53:09 +0530

    Fix clang error

--- src/test/java/com/thealgorithms/stacks/PrefixToInfixTest.java
+++ src/test/java/com/thealgorithms/stacks/PrefixToInfixTest.java
@@ -18,8 +18,7 @@ class PrefixToInfixTest {
     }
 
     static Stream<Arguments> provideValidPrefixToInfixTestCases() {
-        return Stream.of(
-            Arguments.of("A", "A"), // Single operand
+        return Stream.of(Arguments.of("A", "A"), // Single operand
             Arguments.of("+AB", "(A+B)"), // Addition
             Arguments.of("*+ABC", "((A+B)*C)"), // Addition and multiplication
             Arguments.of("-+A*BCD", "((A+(B*C))-D)"), // Mixed operators
