09517279d415c233f344a31e285163363fbc59ba
Tejaswi Tyagi <98461855+tejaswi0910@users.noreply.github.com>
2024-10-03 10:57:58 +0530

    Update src/test/java/com/thealgorithms/dynamicprogramming/LongestArithmeticSubsequenceTest.java
    Co-authored-by: Piotr Idzik <65706193+vil02@users.noreply.github.com>

--- src/test/java/com/thealgorithms/dynamicprogramming/LongestArithmeticSubsequenceTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/LongestArithmeticSubsequenceTest.java
@@ -24,8 +24,7 @@ public class LongestArithmeticSubsequenceTest {
     }
 
     @Test
-    void testNullInput() {
-        // Verify that an IllegalArgumentException is thrown when nums is null
+    void testGetLongestArithmeticSubsequenceLengthThrowsForNullInput() {
         assertThrows(IllegalArgumentException.class, () -> LongestArithmeticSubsequence.getLongestArithmeticSubsequenceLength(null));
     }
 
