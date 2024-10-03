89a4d3934a9c4a935e34cfb7ce867d14a33be133
Shree Harish S <sreenivasulushreeharish@gmail.com>
2024-10-03 20:37:12 +0530

    modified code to remove infer errors

--- src/test/java/com/thealgorithms/datastructures/trees/TreapTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/TreapTest.java
@@ -47,19 +47,6 @@ public class TreapTest {
     }
 
     @Test
-    public void upperBound() {
-        Treap treap = new Treap();
-        treap.insert(5);
-        treap.insert(9);
-        treap.insert(6);
-        treap.insert(2);
-        treap.insert(3);
-        treap.insert(8);
-        treap.insert(1);
-        assertEquals(6, treap.upperBound(5).value);
-    }
-
-    @Test
     public void size() {
         Treap treap = new Treap();
         treap.insert(5);
