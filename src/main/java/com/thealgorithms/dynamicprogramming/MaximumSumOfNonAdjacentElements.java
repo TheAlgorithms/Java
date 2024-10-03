dea0c18a299f59aa58472b99a88733a8ef3de0b5
Guhapriya01 <guhapriyadharmaraj08@gmail.com>
2024-10-03 18:19:25 +0530

    formatted using LLVM clang-formatter

--- src/main/java/com/thealgorithms/dynamicprogramming/MaximumSumOfNonAdjacentElements.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/MaximumSumOfNonAdjacentElements.java
@@ -7,8 +7,8 @@ package com.thealgorithms.dynamicprogramming;
  * https://takeuforward.org/data-structure/maximum-sum-of-non-adjacent-elements-dp-5/
  */
 final class MaximumSumOfNonAdjacentElements {
-private
-  MaximumSumOfNonAdjacentElements() {}
+
+  private MaximumSumOfNonAdjacentElements() {}
 
   /**
    * Approach 1: Uses a dynamic programming array to store the maximum sum at
@@ -17,8 +17,7 @@ private
    * @param arr The input array of integers.
    * @return The maximum sum of non-adjacent elements.
    */
-public
-  static int getMaxSumApproach1(int[] arr) {
+  public static int getMaxSumApproach1(int[] arr) {
     int n = arr.length;
     int[] dp = new int[n];
 
@@ -53,8 +52,7 @@ public
    * @param arr The input array of integers.
    * @return The maximum sum of non-adjacent elements.
    */
-public
-  static int getMaxSumApproach2(int[] arr) {
+  public static int getMaxSumApproach2(int[] arr) {
     int n = arr.length;
 
     // Two variables to keep track of previous two results:
