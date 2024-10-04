06f6fd4471d681df599aa01c16fca40a0a9887cd
prayas7102 <prayas.prithvirajpratap7@example.com>
2024-10-04 15:15:50 +0530

    static imports are placed at the top.

--- src/test/java/com/thealgorithms/bitmanipulation/LowestSetBitTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/LowestSetBitTest.java
@@ -1,7 +1,7 @@
 package com.thealgorithms.bitmanipulation;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class LowestSetBitTest {
 
