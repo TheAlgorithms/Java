defd98ee224b7b2675471cd2e6664032721cae83
bennybebo <burstein.20@buckeyemail.osu.edu>
2024-10-04 14:31:10 -0400

    Added Autokey cipher and AutokeyTest.java

---
+++ src/test/java/com/thealgorithms/ciphers/AutokeyTest.java
@@ -0,0 +1,36 @@
+package com.thealgorithms.ciphers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class AutokeyCipherTest {
+
+    AutokeyCipher autokeyCipher = new AutokeyCipher();
+
+    @Test
+    void autokeyEncryptTest() {
+        // given
+        String plaintext = "MEET AT DAWN";
+        String keyword = "QUEEN";
+
+        // when
+        String cipherText = autokeyCipher.encrypt(plaintext, keyword);
+
+        // then
+        assertEquals("QKUC YT DVZN", cipherText);
+    }
+
+    @Test
+    void autokeyDecryptTest() {
+        // given
+        String ciphertext = "QKUC YT DVZN";
+        String keyword = "QUEEN";
+
+        // when
+        String plainText = autokeyCipher.decrypt(ciphertext, keyword);
+
+        // then
+        assertEquals("MEET AT DAWN", plainText);
+    }
+}
