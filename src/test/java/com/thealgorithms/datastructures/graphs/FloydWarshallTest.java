416000f5eaeaf6c020521b6f89a6041cf4745fe6
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-05 20:10:32 +0530

    Fix clang errors

--- src/test/java/com/thealgorithms/datastructures/graphs/FloydWarshallTest.java
+++ src/test/java/com/thealgorithms/datastructures/graphs/FloydWarshallTest.java
@@ -9,13 +9,13 @@ class FloydWarshallTest {
     @Test
     void testSmallGraph() {
         int[][] adjacencyMatrix = {{0, 0, 0, 0}, // Ignored row (0 index)
-                {0, 0, 3, FloydWarshall.INFINITY}, {0, FloydWarshall.INFINITY, 0, 1}, {0, FloydWarshall.INFINITY, FloydWarshall.INFINITY, 0}};
+            {0, 0, 3, FloydWarshall.INFINITY}, {0, FloydWarshall.INFINITY, 0, 1}, {0, FloydWarshall.INFINITY, FloydWarshall.INFINITY, 0}};
 
         FloydWarshall fw = new FloydWarshall(3);
         fw.floydwarshall(adjacencyMatrix);
 
         int[][] expectedDistanceMatrix = {{0, 0, 0, 0}, // Ignored row (0 index)
-                {0, 0, 3, 4}, {0, FloydWarshall.INFINITY, 0, 1}, {0, FloydWarshall.INFINITY, FloydWarshall.INFINITY, 0}};
+            {0, 0, 3, 4}, {0, FloydWarshall.INFINITY, 0, 1}, {0, FloydWarshall.INFINITY, FloydWarshall.INFINITY, 0}};
 
         assertArrayEquals(expectedDistanceMatrix, fw.getDistanceMatrix());
     }
