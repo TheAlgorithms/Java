d12468003e4ac50a2bbf4886a67cf71a9bb4f2f1
Tuhinm2002 <75078694+Tuhinm2002@users.noreply.github.com>
2024-10-05 21:50:22 +0530

    Update UniqueSubsequencesCount.java

--- src/main/java/com/thealgorithms/dynamicprogramming/UniqueSubsequencesCount.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/UniqueSubsequencesCount.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 // Author -> https://github.com/Tuhinm2002
 
-public class UniqueSubsequencesCount {
+public final class UniqueSubsequencesCount {
 
     private UniqueSubsequencesCount() {
         throw new UnsupportedOperationException("Utility class");
