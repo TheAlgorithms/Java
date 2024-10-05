d99c476099152028cd682efe755194ec6ddab0fc
Tuhinm2002 <75078694+Tuhinm2002@users.noreply.github.com>
2024-10-05 21:50:34 +0530

    Update UniqueSubsequencesCountTest.java

--- src/test/java/com/thealgorithms/dynamicprogramming/UniqueSubsequencesCountTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/UniqueSubsequencesCountTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class UniqueSubsequencesCountTest {
+public final class UniqueSubsequencesCountTest {
 
     @Test
     void subseqCountTestOne() {
