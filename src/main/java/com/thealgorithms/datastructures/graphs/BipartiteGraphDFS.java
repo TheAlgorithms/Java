19c492d4b559606d5c6d6b82817f9ce15fc4b6c4
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-06 12:38:36 +0530

    Refactor `BipartiteGraphDFS.java`, add Junit tests

--- src/main/java/com/thealgorithms/datastructures/graphs/BipartiteGraphDFS.java
+++ src/main/java/com/thealgorithms/datastructures/graphs/BipartiteGraphDFS.java
@@ -7,17 +7,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * Given an adjacency list of a graph adj of V no. of vertices having 0 based
- * index. Check whether the graph is bipartite or not.
+ * This class provides a method to check if a given graph is bipartite using Depth-First Search (DFS).
+ * A bipartite graph is a graph whose vertices can be divided into two disjoint sets such that no two graph vertices
+ * within the same set are adjacent.
  *
+ * Example:
  * Input : {{0, 1, 0, 1}, {1, 0, 1, 0}, {0, 1, 0, 1}, {1, 0, 1, 0}}
- *
  * Output : YES
  */
 public final class BipartiteGraphDFS {
     private BipartiteGraphDFS() {
     }
 
+    /**
+     * Helper method to perform DFS and check if the graph is bipartite.
+     *
+     * @param v     Number of vertices in the graph
+     * @param adj   Adjacency list of the graph
+     * @param color Array to store colors assigned to vertices
+     * @param node  Current node being processed
+     * @return      True if the graph is bipartite, otherwise false
+     */
     private static boolean bipartite(int v, ArrayList<ArrayList<Integer>> adj, int[] color, int node) {
         if (color[node] == -1) {
             color[node] = 1;
@@ -35,11 +45,17 @@ public final class BipartiteGraphDFS {
         return true;
     }
 
+    /**
+     * Method to check if the graph is bipartite.
+     *
+     * @param v   Number of vertices in the graph
+     * @param adj Adjacency list of the graph
+     * @return    True if the graph is bipartite, otherwise false
+     */
     public static boolean isBipartite(int v, ArrayList<ArrayList<Integer>> adj) {
         // Code here
         int[] color = new int[v + 1];
         Arrays.fill(color, -1);
-
         for (int i = 0; i < v; i++) {
             if (color[i] == -1) {
                 if (!bipartite(v, adj, color, i)) {
@@ -50,6 +66,12 @@ public final class BipartiteGraphDFS {
         return true;
     }
 
+    /**
+     * Main method to read input and check if the graph is bipartite.
+     *
+     * @param args Command line arguments
+     * @throws IOException If an I/O error occurs
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine().trim());
