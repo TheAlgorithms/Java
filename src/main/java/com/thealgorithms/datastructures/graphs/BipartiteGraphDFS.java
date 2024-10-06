177658016713ec856ba90980e0b7c668cee42055
Hardik Pawar <97388607+Hardvan@users.noreply.github.com>
2024-10-06 23:28:09 +0530

    Update BipartiteGraphDFS.java

--- src/main/java/com/thealgorithms/datastructures/graphs/BipartiteGraphDFS.java
+++ src/main/java/com/thealgorithms/datastructures/graphs/BipartiteGraphDFS.java
@@ -1,8 +1,5 @@
 package com.thealgorithms.datastructures.graphs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
