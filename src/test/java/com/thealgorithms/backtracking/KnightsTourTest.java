fa9b1def282933aa617d5800ae2ae48b769de812
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-06 10:13:12 +0530

    Fix

--- src/test/java/com/thealgorithms/backtracking/KnightsTourTest.java
+++ src/test/java/com/thealgorithms/backtracking/KnightsTourTest.java
@@ -1,13 +1,11 @@
 package com.thealgorithms.backtracking;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 
 public class KnightsTourTest {
 
