3ae67cdfa27f281aa0c12615c51d45639f24b174
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-06 12:41:02 +0530

    Fix

--- src/main/java/com/thealgorithms/datastructures/graphs/AStar.java
+++ src/main/java/com/thealgorithms/datastructures/graphs/AStar.java
@@ -1,7 +1,6 @@
 package com.thealgorithms.datastructures.graphs;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
