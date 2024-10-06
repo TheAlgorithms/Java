e7917517b80b6325662e33c8dc230103d6ff6483
Rupa-Rd <roopa.69.spr@gmail.com>
2024-10-06 15:44:57 +0530

    Final fix

--- src/main/java/com/thealgorithms/dynamicprogramming/SubsetSum.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/SubsetSum.java
@@ -35,7 +35,7 @@ public final class SubsetSum {
     }
 
     /*
-    Space Optimized solution using 1D boolean array 
+    Space Optimized solution using 1D boolean array
     Time Complexity: O(n * sum)
     Space complexity: O(sum)
     */
