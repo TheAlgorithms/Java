d7961552e8e0eddd9d3ffa5fbf0ef6a97dea3002
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-07 15:33:11 +0530

    Fix

--- src/main/java/com/thealgorithms/backtracking/WordPatternMatcher.java
+++ src/main/java/com/thealgorithms/backtracking/WordPatternMatcher.java
@@ -45,8 +45,7 @@ public class WordPatternMatcher {
      * @param strMap Map to store string to pattern character mappings.
      * @return True if the pattern matches, False otherwise.
      */
-    private static boolean backtrack(String pattern, String inputString, int patternIndex, int strIndex,
-                                     Map<Character, String> patternMap, Map<String, Character> strMap) {
+    private static boolean backtrack(String pattern, String inputString, int patternIndex, int strIndex, Map<Character, String> patternMap, Map<String, Character> strMap) {
         if (patternIndex == pattern.length() && strIndex == inputString.length()) {
             return true;
         }
