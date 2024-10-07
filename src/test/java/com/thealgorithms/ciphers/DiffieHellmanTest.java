40274c204b2206672d6c58bfc426c476b6c758f6
Ritisha Pande <96540421+riti2601@users.noreply.github.com>
2024-10-07 15:03:26 +0530

    Update DiffieHellmanTest.java

--- src/test/java/com/thealgorithms/ciphers/DiffieHellmanTest.java
+++ src/test/java/com/thealgorithms/ciphers/DiffieHellmanTest.java
@@ -12,27 +12,29 @@ public class DiffieHellmanTest {
 
     // Test for public value calculation using instance methods
     @ParameterizedTest
-    @MethodSource("providePublicKeyData")
-    public void testCalculatePublicValue(BigInteger base, BigInteger secret, BigInteger prime, BigInteger expected) {
+    @MethodSource("provideTestData")
+    public void testCalculatePublicValue(BigInteger base, BigInteger secret, BigInteger prime, BigInteger publicExpected, BigInteger sharedExpected) {
         DiffieHellman dh = new DiffieHellman(base, secret, prime); // Create an instance of DiffieHellman
-        assertEquals(expected, dh.calculatePublicValue()); // Call instance method
+        assertEquals(publicExpected, dh.calculatePublicValue()); // Call instance method
     }
 
     // Test for shared secret calculation using instance methods
     @ParameterizedTest
-    @MethodSource("provideSharedSecretData")
-    public void testCalculateSharedSecret(BigInteger base, BigInteger secret, BigInteger prime, BigInteger otherPublicValue, BigInteger expected) {
+    @MethodSource("provideTestData")
+    public void testCalculateSharedSecret(BigInteger base, BigInteger secret, BigInteger prime, BigInteger publicExpected, BigInteger sharedExpected) {
         DiffieHellman dh = new DiffieHellman(base, secret, prime); // Create an instance of DiffieHellman
-        assertEquals(expected, dh.calculateSharedSecret(otherPublicValue)); // Call instance method
+        assertEquals(sharedExpected, dh.calculateSharedSecret(publicExpected)); // Call instance method
     }
 
-    // Provide test data for public key calculation
-    private static Stream<Arguments> providePublicKeyData() {
-        return Stream.of(Arguments.of(BigInteger.valueOf(5), BigInteger.valueOf(6), BigInteger.valueOf(23), BigInteger.valueOf(8)), Arguments.of(BigInteger.valueOf(2), BigInteger.valueOf(5), BigInteger.valueOf(13), BigInteger.valueOf(6)));
+    // Provide test data for both public key and shared secret calculation
+    private static Stream<Arguments> provideTestData() {
+        return Stream.of(
+            createTestArgs(5, 6, 23, 8, 13), createTestArgs(2, 5, 13, 6, 2));
     }
 
-    // Provide test data for shared secret calculation
-    private static Stream<Arguments> provideSharedSecretData() {
-        return Stream.of(Arguments.of(BigInteger.valueOf(5), BigInteger.valueOf(6), BigInteger.valueOf(23), BigInteger.valueOf(8), BigInteger.valueOf(13)), Arguments.of(BigInteger.valueOf(2), BigInteger.valueOf(5), BigInteger.valueOf(13), BigInteger.valueOf(6), BigInteger.valueOf(2)));
+    // Helper method for arguments
+    private static Arguments createTestArgs(long base, long secret, long prime, long publicExpected, long sharedExpected) {
+        return Arguments.of(
+            BigInteger.valueOf(base), BigInteger.valueOf(secret), BigInteger.valueOf(prime), BigInteger.valueOf(publicExpected), BigInteger.valueOf(sharedExpected));
     }
 }
