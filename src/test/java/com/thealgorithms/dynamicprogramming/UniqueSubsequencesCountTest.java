8819fdb87825e4c85cdbb61a7e19f47162609d6e
Tuhinm2002 <75078694+Tuhinm2002@users.noreply.github.com>
2024-10-07 22:16:50 +0530

    Update UniqueSubsequencesCountTest.java

--- src/test/java/com/thealgorithms/dynamicprogramming/UniqueSubsequencesCountTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/UniqueSubsequencesCountTest.java
@@ -4,26 +4,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public final class UniqueSubsequencesCountTest {
+public class UniqueSubsequencesCountTest {
 
     @Test
     void subseqCountTestOne() {
         String s = "abc";
 
-        assertEquals(7, UniqueSubsequencesCount.subseqCount(s));
+        assertEquals(7, UniqueSubsequencesCount.countSubsequences(s));
     }
 
     @Test
     void subseqCountTestTwo() {
         String s = "abcdashgdhas";
 
-        assertEquals(3592, UniqueSubsequencesCount.subseqCount(s));
+        assertEquals(3592, UniqueSubsequencesCount.countSubsequences(s));
     }
 
     @Test
     void subseqCountTestThree() {
-        String s = "aaaaa";
+        String s = "a";
 
-        assertEquals(5, UniqueSubsequencesCount.subseqCount(s));
+        assertEquals(1, UniqueSubsequencesCount.countSubsequences(s));
+    }
+
+    @Test
+    void subseqCountTestFour() {
+        String s = "a b";
+
+        assertEquals(7, UniqueSubsequencesCount.countSubsequences(s));
+    }
+
+    @Test
+    void subseqCountTestFive() {
+        String s = "a1b2";
+
+        assertEquals(15, UniqueSubsequencesCount.countSubsequences(s));
+    }
+
+    @Test
+    void subseqCountTesSix() {
+        String s = "AaBb";
+        assertEquals(15, UniqueSubsequencesCount.countSubsequences(s));
+    }
+
+    @Test
+    void subseqCountTesSeven() {
+        String s = "abab";
+        assertEquals(11, UniqueSubsequencesCount.countSubsequences(s));
     }
 }
