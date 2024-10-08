b0accfc769584dd152da231757067429ee6efc86
Jayeshrdeotalu <jr11deotalu@gmail.com>
2024-10-08 17:40:52 +0530

    fixed pmd

--- src/main/java/com/thealgorithms/Recursion/RegexMatchingRecursive.java
+++ src/main/java/com/thealgorithms/Recursion/RegexMatchingRecursive.java
@@ -10,7 +10,7 @@ public class RegexMatchingRecursive {
         }
 
         // Check if the first character of s matches the first character of p.
-        boolean firstMatch = (!s.isEmpty() && p.charAt(0) == s.charAt(0) || p.charAt(0) == '.');
+        boolean firstMatch = !s.isEmpty() && p.charAt(0) == s.charAt(0) || p.charAt(0) == '.';
 
         // So...If there's a '*' in the second position of p i.e., p.charAt(1) == '*'
         // it means we can either:
