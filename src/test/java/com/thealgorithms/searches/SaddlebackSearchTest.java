2ffe3f275bb9e5c115e5db014d852c08d89cfb0f
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-09 12:48:23 +0530

    Fix

--- src/test/java/com/thealgorithms/searches/SaddlebackSearchTest.java
+++ src/test/java/com/thealgorithms/searches/SaddlebackSearchTest.java
@@ -12,16 +12,10 @@ class SaddlebackSearchTest {
      */
     @Test
     void testFindElementExists() {
-        int[][] arr = {
-                {-10, -5, -3, 4, 9},
-                {-6, -2, 0, 5, 10},
-                {-4, -1, 1, 6, 12},
-                {2, 3, 7, 8, 13},
-                {100, 120, 130, 140, 150}
-        };
+        int[][] arr = {{-10, -5, -3, 4, 9}, {-6, -2, 0, 5, 10}, {-4, -1, 1, 6, 12}, {2, 3, 7, 8, 13}, {100, 120, 130, 140, 150}};
 
         int[] result = SaddlebackSearch.find(arr, arr.length - 1, 0, 4);
-        assertArrayEquals(new int[]{0, 3}, result, "Element 4 should be found at (0, 3)");
+        assertArrayEquals(new int[] {0, 3}, result, "Element 4 should be found at (0, 3)");
     }
 
     /**
@@ -29,16 +23,10 @@ class SaddlebackSearchTest {
      */
     @Test
     void testFindElementNotExists() {
-        int[][] arr = {
-                {-10, -5, -3, 4, 9},
-                {-6, -2, 0, 5, 10},
-                {-4, -1, 1, 6, 12},
-                {2, 3, 7, 8, 13},
-                {100, 120, 130, 140, 150}
-        };
+        int[][] arr = {{-10, -5, -3, 4, 9}, {-6, -2, 0, 5, 10}, {-4, -1, 1, 6, 12}, {2, 3, 7, 8, 13}, {100, 120, 130, 140, 150}};
 
         int[] result = SaddlebackSearch.find(arr, arr.length - 1, 0, 1000);
-        assertArrayEquals(new int[]{-1, -1}, result, "Element 1000 should not be found");
+        assertArrayEquals(new int[] {-1, -1}, result, "Element 1000 should not be found");
     }
 
     /**
@@ -46,16 +34,10 @@ class SaddlebackSearchTest {
      */
     @Test
     void testFindSmallestElement() {
-        int[][] arr = {
-                {-10, -5, -3, 4, 9},
-                {-6, -2, 0, 5, 10},
-                {-4, -1, 1, 6, 12},
-                {2, 3, 7, 8, 13},
-                {100, 120, 130, 140, 150}
-        };
+        int[][] arr = {{-10, -5, -3, 4, 9}, {-6, -2, 0, 5, 10}, {-4, -1, 1, 6, 12}, {2, 3, 7, 8, 13}, {100, 120, 130, 140, 150}};
 
         int[] result = SaddlebackSearch.find(arr, arr.length - 1, 0, -10);
-        assertArrayEquals(new int[]{0, 0}, result, "Element -10 should be found at (0, 0)");
+        assertArrayEquals(new int[] {0, 0}, result, "Element -10 should be found at (0, 0)");
     }
 
     /**
@@ -63,16 +45,10 @@ class SaddlebackSearchTest {
      */
     @Test
     void testFindLargestElement() {
-        int[][] arr = {
-                {-10, -5, -3, 4, 9},
-                {-6, -2, 0, 5, 10},
-                {-4, -1, 1, 6, 12},
-                {2, 3, 7, 8, 13},
-                {100, 120, 130, 140, 150}
-        };
+        int[][] arr = {{-10, -5, -3, 4, 9}, {-6, -2, 0, 5, 10}, {-4, -1, 1, 6, 12}, {2, 3, 7, 8, 13}, {100, 120, 130, 140, 150}};
 
         int[] result = SaddlebackSearch.find(arr, arr.length - 1, 0, 150);
-        assertArrayEquals(new int[]{4, 4}, result, "Element 150 should be found at (4, 4)");
+        assertArrayEquals(new int[] {4, 4}, result, "Element 150 should be found at (4, 4)");
     }
 
     /**
@@ -93,7 +69,7 @@ class SaddlebackSearchTest {
         int[][] arr = {{5}};
 
         int[] result = SaddlebackSearch.find(arr, 0, 0, 5);
-        assertArrayEquals(new int[]{0, 0}, result, "Element 5 should be found at (0, 0)");
+        assertArrayEquals(new int[] {0, 0}, result, "Element 5 should be found at (0, 0)");
     }
 
     /**
@@ -104,6 +80,6 @@ class SaddlebackSearchTest {
         int[][] arr = {{5}};
 
         int[] result = SaddlebackSearch.find(arr, 0, 0, 10);
-        assertArrayEquals(new int[]{-1, -1}, result, "Element 10 should not be found in single element array");
+        assertArrayEquals(new int[] {-1, -1}, result, "Element 10 should not be found in single element array");
     }
 }
