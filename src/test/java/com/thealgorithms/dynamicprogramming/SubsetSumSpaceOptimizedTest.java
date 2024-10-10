768cc9d52fbc99448cb78fd21e01d62d888b43d9
Rupa-Rd <roopa.69.spr@gmail.com>
2024-10-10 07:37:11 +0530

    Checkstyle issue is solved

--- src/test/java/com/thealgorithms/dynamicprogramming/SubsetSumSpaceOptimizedTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/SubsetSumSpaceOptimizedTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-class SubsetSumSpaceOptimizedTest {
+
+public class SubsetSumSpaceOptimizedTest {
+
     @Test
     void basicCheck() {
         assertTrue(SubsetSumSpaceOptimized.isSubsetSum(new int[] {7, 3, 2, 5, 8}, 14));
@@ -12,6 +14,5 @@ class SubsetSumSpaceOptimizedTest {
         assertTrue(SubsetSumSpaceOptimized.isSubsetSum(new int[] {1, 7, 2, 9, 10}, 13));
         assertFalse(SubsetSumSpaceOptimized.isSubsetSum(new int[] {1, 2, 7, 10, 9}, 14));
         assertFalse(SubsetSumSpaceOptimized.isSubsetSum(new int[] {2, 15, 1, 6, 7}, 4));
-        
     }
 }
