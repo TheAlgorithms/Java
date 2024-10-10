ae8a736843c1872c7604171a2054d91348cc7853
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-10 19:52:31 +0530

    Fix

--- src/test/java/com/thealgorithms/bitmanipulation/ClearLeftmostSetBitTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/ClearLeftmostSetBitTest.java
@@ -9,8 +9,8 @@ public class ClearLeftmostSetBitTest {
     @Test
     public void testClearLeftmostSetBit() {
         assertEquals(10, ClearLeftmostSetBit.clearLeftmostSetBit(26)); // 11010 -> 01010
-        assertEquals(0, ClearLeftmostSetBit.clearLeftmostSetBit(1));   // 1 -> 0
-        assertEquals(3, ClearLeftmostSetBit.clearLeftmostSetBit(7));   // 111 -> 011
-        assertEquals(2, ClearLeftmostSetBit.clearLeftmostSetBit(6));  // 0110 -> 0010
+        assertEquals(0, ClearLeftmostSetBit.clearLeftmostSetBit(1)); // 1 -> 0
+        assertEquals(3, ClearLeftmostSetBit.clearLeftmostSetBit(7)); // 111 -> 011
+        assertEquals(2, ClearLeftmostSetBit.clearLeftmostSetBit(6)); // 0110 -> 0010
     }
 }
