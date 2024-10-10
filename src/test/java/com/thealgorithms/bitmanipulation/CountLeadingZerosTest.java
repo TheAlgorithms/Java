e816c8402bfbe7e5c81ad44a8dd49b2761c713de
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-10 19:57:23 +0530

    Fix

--- src/test/java/com/thealgorithms/bitmanipulation/CountLeadingZerosTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/CountLeadingZerosTest.java
@@ -8,9 +8,9 @@ public class CountLeadingZerosTest {
 
     @Test
     public void testCountLeadingZeros() {
-        assertEquals(29, CountLeadingZeros.countLeadingZeros(5));   // 000...0101 has 29 leading zeros
-        assertEquals(32, CountLeadingZeros.countLeadingZeros(0));   // 000...0000 has 32 leading zeros
-        assertEquals(31, CountLeadingZeros.countLeadingZeros(1));   // 000...0001 has 31 leading zeros
-        assertEquals(0, CountLeadingZeros.countLeadingZeros(-1));   // No leading zeros in negative number (-1)
+        assertEquals(29, CountLeadingZeros.countLeadingZeros(5)); // 000...0101 has 29 leading zeros
+        assertEquals(32, CountLeadingZeros.countLeadingZeros(0)); // 000...0000 has 32 leading zeros
+        assertEquals(31, CountLeadingZeros.countLeadingZeros(1)); // 000...0001 has 31 leading zeros
+        assertEquals(0, CountLeadingZeros.countLeadingZeros(-1)); // No leading zeros in negative number (-1)
     }
 }
