c0a2ad66ce0f2a35697d00c9ae834d30807418f1
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-10 20:24:49 +0530

    Fix

--- src/test/java/com/thealgorithms/bitmanipulation/BinaryPalindromeCheckTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/BinaryPalindromeCheckTest.java
@@ -9,7 +9,7 @@ public class BinaryPalindromeCheckTest {
 
     @Test
     public void testIsBinaryPalindrome() {
-        assertTrue(BinaryPalindromeCheck.isBinaryPalindrome(9));  // 1001 is a palindrome
+        assertTrue(BinaryPalindromeCheck.isBinaryPalindrome(9)); // 1001 is a palindrome
         assertFalse(BinaryPalindromeCheck.isBinaryPalindrome(10)); // 1010 is not a palindrome
         assertTrue(BinaryPalindromeCheck.isBinaryPalindrome(0)); // 0 is a palindrome
         assertTrue(BinaryPalindromeCheck.isBinaryPalindrome(1)); // 1 is a palindrome
