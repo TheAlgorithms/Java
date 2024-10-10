1424515372aad53767b3470e538cd29d631a0690
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-10 20:17:24 +0530

    Fix

--- src/main/java/com/thealgorithms/bitmanipulation/HammingDistance.java
+++ src/main/java/com/thealgorithms/bitmanipulation/HammingDistance.java
@@ -1,6 +1,18 @@
 package com.thealgorithms.bitmanipulation;
 
-public class HammingDistance {
+/**
+ * The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+ * Given two integers x and y, calculate the Hamming distance.
+ * Example:
+ * Input: x = 1, y = 4
+ * Output: 2
+ * Explanation: 1 (0001) and 4 (0100) have 2 differing bits.
+ *
+ * @author Hardvan
+ */
+public final class HammingDistance {
+    private HammingDistance() {
+    }
 
     /**
      * Calculates the Hamming distance between two integers.
