b0655feb70a0ccc4c72afe8728e8ceb34746182b
Mayank <rs.mayank24@gmail.com>
2024-10-11 20:42:54 +0530

    Documentation for Palindrome.java for novices

--- src/main/java/com/thealgorithms/strings/Palindrome.java
+++ src/main/java/com/thealgorithms/strings/Palindrome.java
@@ -16,6 +16,12 @@ final class Palindrome {
      */
     public static boolean isPalindrome(String s) {
         return ((s == null || s.length() <= 1) || s.equals(new StringBuilder(s).reverse().toString()));
+        
+        //above return statement can be understood as
+        //if length of s is 0(null) or s is a string of a single character
+        //or if s is equals to (build a new string builder initializer,
+        //    reverse it and then make it into a new String) reversed s String
+    
     }
 
     /**
@@ -26,15 +32,20 @@ final class Palindrome {
      * {@code false}
      */
     public static boolean isPalindromeRecursion(String s) {
-        if (s == null || s.length() <= 1) {
-            return true;
+        if (s == null || s.length() <= 1) {   //checking if s is null or it is a single character
+            return true;                        
         }
 
-        if (s.charAt(0) != s.charAt(s.length() - 1)) {
+        if (s.charAt(0) != s.charAt(s.length() - 1)) { //checking if first and last character are not equal
             return false;
         }
 
         return isPalindromeRecursion(s.substring(1, s.length() - 1));
+
+        //if they are equal, then use a modified String s that excludes its first and last char as next parameter
+        //thus we are recursively checking first and last character and at end we come to middle of s
+        //if s length is odd, at the end parameter's length will be 1 and will return true from first condition
+        //if its odd, then lenght will be null at the end
     }
 
     /**
@@ -45,11 +56,13 @@ final class Palindrome {
      * {@code false}
      */
     public static boolean isPalindromeTwoPointer(String s) {
-        if (s == null || s.length() <= 1) {
+        if (s == null || s.length() <= 1) {     //checking if s is null(its length 0) or its a single char
             return true;
         }
-        for (int i = 0, j = s.length() - 1; i < j; ++i, --j) {
-            if (s.charAt(i) != s.charAt(j)) {
+        for (int i = 0, j = s.length() - 1; i < j; ++i, --j) {   //taken two variables i and j,
+        //i's value will increase by 1, j will decrease by 1 after each loop
+        
+            if (s.charAt(i) != s.charAt(j)) {   //checking first and last charcter, second and second last and so on...
                 return false;
             }
         }
