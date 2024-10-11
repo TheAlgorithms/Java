7fac25f76f3f63e3c32a397144c1d3b895ed5bbb
bennybebo <burstein.20@buckeyemail.osu.edu>
2024-10-11 15:24:46 -0400

    fix

--- src/test/java/com/thealgorithms/ciphers/ADFGVXCipherTest.java
+++ src/test/java/com/thealgorithms/ciphers/ADFGVXCipherTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class ADFGVXCipher {
+class ADFGVXCipherTest {
 
 	ADFGVXCipher adfgvxCipher = new ADFGVXCipher();
 
