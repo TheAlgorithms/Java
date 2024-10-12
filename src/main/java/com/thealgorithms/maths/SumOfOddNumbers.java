c4942a4103c75b100aa8f0917acf888aeeda5a94
Byte Bender <136265142+BYT-Bender@users.noreply.github.com>
2024-10-12 13:26:26 +0530

    Update SumOfOddNumbers.java

--- src/main/java/com/thealgorithms/maths/SumOfOddNumbers.java
+++ src/main/java/com/thealgorithms/maths/SumOfOddNumbers.java
@@ -2,14 +2,12 @@ package com.thealgorithms.maths;
 
 /**
  * This program calculates the sum of the first n odd numbers.
- * 
+ *
  * https://www.cuemath.com/algebra/sum-of-odd-numbers/
  */
 
-public final class SumOfOddNumbers
-{
-    private SumOfOddNumbers()
-    {
+public final class SumOfOddNumbers {
+    private SumOfOddNumbers() {
     }
 
     /**
@@ -18,10 +16,8 @@ public final class SumOfOddNumbers
      * @param n the number of odd numbers to sum
      * @return sum of the first n odd numbers
      */
-    public static int sumOfFirstNOddNumbers(final int n)
-    {
-        if (n < 0)
-        {
+    public static int sumOfFirstNOddNumbers(final int n) {
+        if (n < 0) {
             throw new IllegalArgumentException("n must be non-negative.");
         }
         return n * n;
