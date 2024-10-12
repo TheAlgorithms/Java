6acf62125b65d358a94c9e7835e44d31fe3c1c51
ANANT JAIN <139585700+anant-jain01@users.noreply.github.com>
2024-10-13 00:36:50 +0530

    Update StalinSort.java

--- src/main/java/com/thealgorithms/sorts/StalinSort.java
+++ src/main/java/com/thealgorithms/sorts/StalinSort.java
@@ -1,12 +1,10 @@
 package com.thealgorithms.sorts;
 
-/**
- * @author Anant Jain (https://github.com/anant-jain01)
- * @see https://medium.com/@kaweendra/the-ultimate-sorting-algorithm-6513d6968420
- */
-public class StalinSort implements SortAlgorithm {
+import java.util.Arrays;
 
+public class StalinSort implements SortAlgorithm {
     public <T extends Comparable<T>> T[] sort(T[] array) {
+        validateInput(array);
         int currentIndex = 0;
 
         for (int i = 1; i < array.length; i++) {
@@ -16,26 +14,23 @@ public class StalinSort implements SortAlgorithm {
             }
         }
 
-        // Create a result array with sorted elements
         T[] result = (T[]) new Comparable[currentIndex + 1];
         System.arraycopy(array, 0, result, 0, currentIndex + 1);
-
         return result;
     }
 
-    // Driver Program
+    private void validateInput(final Comparable<?>[] array) {
+        if (array.length == 0) {
+            throw new IllegalArgumentException("Input array must not be empty.");
+        }
+    }
+
     public static void main(String[] args) {
-        // Integer Input
         Integer[] integers = {4, 23, 6, 78, 1, 54, 231, 9, 12};
-
         StalinSort stalinSort = new StalinSort();
-
-        // print a sorted array
         SortUtils.print(stalinSort.sort(integers));
-
-        // String Input
+        
         String[] strings = {"c", "a", "e", "b", "d"};
-
         SortUtils.print(stalinSort.sort(strings));
     }
 }
