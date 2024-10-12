3104b1150fbc0152b3c634cb1cbd95f0fe280056
ANANT JAIN <139585700+anant-jain01@users.noreply.github.com>
2024-10-13 00:18:53 +0530

    Update StalinSortTest.java

--- src/test/java/com/thealgorithms/sorts/StalinSortTest.java
+++ src/test/java/com/thealgorithms/sorts/StalinSortTest.java
@@ -19,22 +19,11 @@ public class StalinSortTest {
     }
 
     private static Stream<Arguments> provideArraysForStalinSort() {
-        return Stream.of(
-            Arguments.of(new Integer[] {4}, new Integer[] {4}),
-            Arguments.of(new Integer[] {4, 23, 6, 78, 1, 54, 231, 9, 12}, new Integer[] {4, 23, 78, 231}),
-            Arguments.of(new Integer[] {5, 5, 5, 5, 5}, new Integer[] {5, 5, 5, 5, 5}),
-            Arguments.of(new Integer[] {1, 2, 3, 4, 5}, new Integer[] {1, 2, 3, 4, 5}),
-            Arguments.of(new Integer[] {5, 4, 3, 2, 1}, new Integer[] {5}),
-            Arguments.of(new String[] {"c", "a", "e", "b", "d"}, new String[] {"c", "e"}),
-            Arguments.of(new Integer[] {-1, -2, -3, 4, 5}, new Integer[] {-1, 4, 5}),
-            Arguments.of(new Integer[] {0, -1, -2, 1, 2}, new Integer[] {0, 1, 2}),
-            Arguments.of(new Integer[] {3, -1, 0, -5, 2}, new Integer[] {3}),
-            Arguments.of(new Integer[] {10, 10, 10, 10}, new Integer[] {10, 10, 10, 10}),
-            Arguments.of(new Integer[] {1, 2, 2, 3, 3, 3, 4}, new Integer[] {1, 2, 2, 3, 3, 3, 4}),
-            Arguments.of(new Integer[] {Integer.MAX_VALUE, Integer.MIN_VALUE, 0}, new Integer[] {Integer.MAX_VALUE}),
-            Arguments.of(new Character[] {'d', 'a', 'c', 'b'}, new Character[] {'d'}),
-            Arguments.of(new Integer[] {100, 200, 150, 300, 250, 400, 350, 450, 500}, new Integer[] {100, 200, 300, 400, 500})
-        );
+              return Stream.of(Arguments.of(new Integer[] {4}, new Integer[] {4}), Arguments.of(new Integer[] {4, 23, 6, 78, 1, 54, 231, 9, 12}, new Integer[] {4, 23, 78, 231}), Arguments.of(new Integer[] {5, 5, 5, 5, 5}, new Integer[] {5, 5, 5, 5, 5}),
+          Arguments.of(new Integer[] {1, 2, 3, 4, 5}, new Integer[] {1, 2, 3, 4, 5}), Arguments.of(new Integer[] {5, 4, 3, 2, 1}, new Integer[] {5}), Arguments.of(new String[] {"c", "a", "e", "b", "d"}, new String[] {"c", "e"}),
+          Arguments.of(new Integer[] {-1, -2, -3, 4, 5}, new Integer[] {-1, 4, 5}), Arguments.of(new Integer[] {0, -1, -2, 1, 2}, new Integer[] {0, 1, 2}), Arguments.of(new Integer[] {3, -1, 0, -5, 2}, new Integer[] {3}),
+          Arguments.of(new Integer[] {10, 10, 10, 10}, new Integer[] {10, 10, 10, 10}), Arguments.of(new Integer[] {1, 2, 2, 3, 3, 3, 4}, new Integer[] {1, 2, 2, 3, 3, 3, 4}), Arguments.of(new Integer[] {Integer.MAX_VALUE, Integer.MIN_VALUE, 0}, new Integer[] {Integer.MAX_VALUE}),
+          Arguments.of(new Character[] {'d', 'a', 'c', 'b'}, new Character[] {'d'}), Arguments.of(new Integer[] {100, 200, 150, 300, 250, 400, 350, 450, 500}, new Integer[] {100, 200, 300, 400, 500}));
     }
 
     @Test
