9926906dcc4447f170b312ebedbbbd23d2338ca9
ANANT JAIN <139585700+anant-jain01@users.noreply.github.com>
2024-10-13 01:06:48 +0530

    Update StalinSortTest.java

--- src/test/java/com/thealgorithms/sorts/StalinSortTest.java
+++ src/test/java/com/thealgorithms/sorts/StalinSortTest.java
@@ -1,7 +1,7 @@
 package com.thealgorithms.sorts;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
 
 public class StalinSortTest {
 
