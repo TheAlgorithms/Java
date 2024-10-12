ac7b48f49466f91103a5953d974cfc5e6754f885
ANANT JAIN <139585700+anant-jain01@users.noreply.github.com>
2024-10-13 00:08:06 +0530

    Update StalinSortTest.java

--- src/test/java/com/thealgorithms/sorts/StalinSortTest.java
+++ src/test/java/com/thealgorithms/sorts/StalinSortTest.java
@@ -20,38 +20,21 @@ public class StalinSortTest {
 
     private static Stream<Arguments> provideArraysForStalinSort() {
         return Stream.of(
-            // Test empty array
-            Arguments.of(new Integer[] {}, new Integer[] {}),
-
-            // Test single element array
             Arguments.of(new Integer[] {4}, new Integer[] {4}),
+    Arguments.of(new Integer[] {4, 23, 6, 78, 1, 54, 231, 9, 12}, new Integer[] {4, 23, 78, 231}), 
+    Arguments.of(new Integer[] {5, 5, 5, 5, 5}, new Integer[] {5, 5, 5, 5, 5}), 
+    Arguments.of(new Integer[] {1, 2, 3, 4, 5}, new Integer[] {1, 2, 3, 4, 5}), 
+    Arguments.of(new Integer[] {5, 4, 3, 2, 1}, new Integer[] {5}), 
+    Arguments.of(new String[] {"c", "a", "e", "b", "d"}, new String[] {"c", "e"}),
+    Arguments.of(new Integer[] {-1, -2, -3, 4, 5}, new Integer[] {-1, 4, 5}), 
+    Arguments.of(new Integer[] {0, -1, -2, 1, 2}, new Integer[] {0, 1, 2}),
+    Arguments.of(new Integer[] {3, -1, 0, -5, 2}, new Integer[] {3}),
+    Arguments.of(new Integer[] {10, 10, 10, 10}, new Integer[] {10, 10, 10, 10}), 
+    Arguments.of(new Integer[] {1, 2, 2, 3, 3, 3, 4}, new Integer[] {1, 2, 2, 3, 3, 3, 4}),
+    Arguments.of(new Integer[] {Integer.MAX_VALUE, Integer.MIN_VALUE, 0}, new Integer[] {Integer.MAX_VALUE}), 
+    Arguments.of(new Character[] {'d', 'a', 'c', 'b'}, new Character[] {'d'}), 
+    Arguments.of(new Integer[] {100, 200, 150, 300, 250, 400, 350, 450, 500}, new Integer[] {100, 200, 300, 400, 500}));
 
-            // Test array with multiple elements
-            Arguments.of(new Integer[] {4, 23, 6, 78, 1, 54, 231, 9, 12}, new Integer[] {4, 23, 78, 231}),
-            Arguments.of(new Integer[] {5, 5, 5, 5, 5}, new Integer[] {5, 5, 5, 5, 5}),
-            Arguments.of(new Integer[] {1, 2, 3, 4, 5}, new Integer[] {1, 2, 3, 4, 5}),
-            Arguments.of(new Integer[] {5, 4, 3, 2, 1}, new Integer[] {5}),
-            Arguments.of(new String[] {"c", "a", "e", "b", "d"}, new String[] {"c", "e"}),
-
-            // Test with negative numbers
-            Arguments.of(new Integer[] {-1, -2, -3, 4, 5}, new Integer[] {-1, 4, 5}),
-            Arguments.of(new Integer[] {0, -1, -2, 1, 2}, new Integer[] {0, 1, 2}),
-
-            // Test mixed values (positive, negative, zero)
-            Arguments.of(new Integer[] {3, -1, 0, -5, 2}, new Integer[] {3}),
-
-            // Test with duplicate values
-            Arguments.of(new Integer[] {10, 10, 10, 10}, new Integer[] {10, 10, 10, 10}),
-            Arguments.of(new Integer[] {1, 2, 2, 3, 3, 3, 4}, new Integer[] {1, 2, 2, 3, 3, 3, 4}),
-            
-            // Test large numbers
-            Arguments.of(new Integer[] {Integer.MAX_VALUE, Integer.MIN_VALUE, 0}, new Integer[] {Integer.MAX_VALUE}),
-            
-            // Test with characters
-            Arguments.of(new Character[] {'d', 'a', 'c', 'b'}, new Character[] {'d'}),
-            
-            // Test with a large dataset
-            Arguments.of(new Integer[] {100, 200, 150, 300, 250, 400, 350, 450, 500}, new Integer[] {100, 200, 300, 400, 500})
         );
     }
 
