c201cfd9d51b1098af05a1d7b2ef1560c122f1d7
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-13 12:32:36 +0530

    Fix

--- src/test/java/com/thealgorithms/stacks/CelebrityFinderTest.java
+++ src/test/java/com/thealgorithms/stacks/CelebrityFinderTest.java
@@ -8,21 +8,13 @@ public class CelebrityFinderTest {
 
     @Test
     public void testCelebrityExists() {
-        int[][] party = {
-            {0, 1, 1},
-            {0, 0, 1},
-            {0, 0, 0}
-        };
+        int[][] party = {{0, 1, 1}, {0, 0, 1}, {0, 0, 0}};
         assertEquals(2, CelebrityFinder.findCelebrity(party));
     }
 
     @Test
     public void testNoCelebrity() {
-        int[][] party = {
-            {0, 1, 0},
-            {1, 0, 1},
-            {1, 1, 0}
-        };
+        int[][] party = {{0, 1, 0}, {1, 0, 1}, {1, 1, 0}};
         assertEquals(-1, CelebrityFinder.findCelebrity(party));
     }
 }
