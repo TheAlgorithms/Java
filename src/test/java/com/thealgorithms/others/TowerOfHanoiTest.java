fa79c2fb0d8ab04ac47c7f31ea53f5f0fdd7360c
lorduke22 <giulio.tantaro@gmail.com>
2024-10-13 14:06:39 +0200

    Fix merge conflict

--- src/test/java/com/thealgorithms/others/TowerOfHanoiTest.java
+++ src/test/java/com/thealgorithms/others/TowerOfHanoiTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class TowerOfHanoiTest {
-  
+
     @Test
     public void testHanoiWithOneDisc() {
         List<String> result = new ArrayList<>();
