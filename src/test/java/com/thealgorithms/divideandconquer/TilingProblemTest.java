ecfbcf53c6c36d73db5c5827591b22ac90305a71
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-13 08:42:02 +0530

    Fix

--- src/test/java/com/thealgorithms/divideandconquer/TilingProblemTest.java
+++ src/test/java/com/thealgorithms/divideandconquer/TilingProblemTest.java
@@ -8,10 +8,7 @@ public class TilingProblemTest {
 
     @Test
     public void testTilingSize2() {
-        int[][] expected = {
-                {1, 1},
-                {1, 0}
-        };
+        int[][] expected = {{1, 1}, {1, 0}};
         int[][] result = TilingProblem.solveTiling(2, 1, 1);
         assertArrayEquals(expected, result);
     }
