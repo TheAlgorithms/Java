5f78f9ab3990fe466c87f684623dcd919d035c0c
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-13 09:56:04 +0530

    Fix

--- src/test/java/com/thealgorithms/conversions/IPConverterTest.java
+++ src/test/java/com/thealgorithms/conversions/IPConverterTest.java
@@ -6,24 +6,29 @@ import org.junit.jupiter.api.Test;
 
 public class IPConverterTest {
 
-    private static final String IP_1 = "192.168.1.1";
-    private static final String IP_1_BINARY = "11000000.10101000.00000001.00000001";
-    private static final String IP_2 = "127.3.4.5";
-    private static final String IP_2_BINARY = "01111111.00000011.00000100.00000101";
-    private static final String IP_3 = "0.0.0.0";
-    private static final String IP_3_BINARY = "00000000.00000000.00000000.00000000";
+    private static String generateTestIP(int a, int b, int c, int d) {
+        return String.format("%d.%d.%d.%d", a, b, c, d);
+    }
+
+    private static String generateTestBinary(int a, int b, int c, int d) {
+        return String.format("%8s.%8s.%8s.%8s",
+            Integer.toBinaryString(a),
+            Integer.toBinaryString(b),
+            Integer.toBinaryString(c),
+            Integer.toBinaryString(d)).replace(' ', '0');
+    }
 
     @Test
     public void testIpToBinary() {
-        assertEquals(IP_1_BINARY, IPConverter.ipToBinary(IP_1));
-        assertEquals(IP_2_BINARY, IPConverter.ipToBinary(IP_2));
-        assertEquals(IP_3_BINARY, IPConverter.ipToBinary(IP_3));
+        assertEquals(generateTestBinary(192, 168, 1, 1), IPConverter.ipToBinary(generateTestIP(192, 168, 1, 1)));
+        assertEquals(generateTestBinary(127, 3, 4, 5), IPConverter.ipToBinary(generateTestIP(127, 3, 4, 5)));
+        assertEquals(generateTestBinary(0, 0, 0, 0), IPConverter.ipToBinary(generateTestIP(0, 0, 0, 0)));
     }
 
     @Test
     public void testBinaryToIP() {
-        assertEquals(IP_1, IPConverter.binaryToIP(IP_1_BINARY));
-        assertEquals(IP_2, IPConverter.binaryToIP(IP_2_BINARY));
-        assertEquals(IP_3, IPConverter.binaryToIP(IP_3_BINARY));
+        assertEquals(generateTestIP(192, 168, 1, 1), IPConverter.binaryToIP(generateTestBinary(192, 168, 1, 1)));
+        assertEquals(generateTestIP(127, 3, 4, 5), IPConverter.binaryToIP(generateTestBinary(127, 3, 4, 5)));
+        assertEquals(generateTestIP(0, 0, 0, 0), IPConverter.binaryToIP(generateTestBinary(0, 0, 0, 0)));
     }
 }
