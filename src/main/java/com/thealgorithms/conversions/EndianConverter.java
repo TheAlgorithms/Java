0b5011e6fbe7202e680e4022d15f2e2faadbd941
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-13 09:21:02 +0530

    feat: Add `EndianConverter` new algorithm with Junit tests

---
+++ src/main/java/com/thealgorithms/conversions/EndianConverter.java
@@ -0,0 +1,21 @@
+package com.thealgorithms.conversions;
+
+/**
+ * Converts between big-endian and little-endian formats.
+ * Big-endian is the most significant byte first, while little-endian is the least significant byte first.
+ * Big-endian to little-endian: 0x12345678 -> 0x78563412
+ *
+ * Little-endian to big-endian: 0x12345678 -> 0x78563412
+ *
+ * @author Hardvan
+ */
+public class EndianConverter {
+
+    public static int bigToLittleEndian(int value) {
+        return Integer.reverseBytes(value);
+    }
+
+    public static int littleToBigEndian(int value) {
+        return Integer.reverseBytes(value);
+    }
+}
