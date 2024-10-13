73df2f884aa93e52964470707402ec70fb23e428
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-13 09:36:23 +0530

    Fix

--- src/test/java/com/thealgorithms/conversions/IPConverterTest.java
+++ src/test/java/com/thealgorithms/conversions/IPConverterTest.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.Test;
 
 public class IPConverterTest {
 
+    private static final String IP_1 = "192.168.1.1";
+    private static final String IP_1_BINARY = "11000000.10101000.00000001.00000001";
+    private static final String IP_2 = "127.3.4.5";
+    private static final String IP_2_BINARY = "01111111.00000011.00000100.00000101";
+    private static final String IP_3 = "0.0.0.0";
+    private static final String IP_3_BINARY = "00000000.00000000.00000000.00000000";
+
     @Test
     public void testIpToBinary() {
-        assertEquals("11000000.10101000.00000001.00000001", IPConverter.ipToBinary("192.168.1.1"));
-        assertEquals("01111111.00000011.00000100.00000101", IPConverter.ipToBinary("127.3.4.5"));
-        assertEquals("00000000.00000000.00000000.00000000", IPConverter.ipToBinary("0.0.0.0"));
+        assertEquals(IP_1_BINARY, IPConverter.ipToBinary(IP_1));
+        assertEquals(IP_2_BINARY, IPConverter.ipToBinary(IP_2));
+        assertEquals(IP_3_BINARY, IPConverter.ipToBinary(IP_3));
     }
 
     @Test
     public void testBinaryToIP() {
-        assertEquals("192.168.1.1", IPConverter.binaryToIP("11000000.10101000.00000001.00000001"));
-        assertEquals("127.3.4.5", IPConverter.binaryToIP("01111111.00000011.00000100.00000101"));
-        assertEquals("0.0.0.0", IPConverter.binaryToIP("00000000.00000000.00000000.00000000"));
+        assertEquals(IP_1, IPConverter.binaryToIP(IP_1_BINARY));
+        assertEquals(IP_2, IPConverter.binaryToIP(IP_2_BINARY));
+        assertEquals(IP_3, IPConverter.binaryToIP(IP_3_BINARY));
     }
 }
