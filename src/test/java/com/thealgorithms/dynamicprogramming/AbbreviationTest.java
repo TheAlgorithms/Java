04c0ebc3a5c64390895033febce45c321fa12e3f
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-14 09:19:52 +0530

    Fix

--- src/test/java/com/thealgorithms/dynamicprogramming/AbbreviationTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/AbbreviationTest.java
@@ -18,27 +18,27 @@ public class AbbreviationTest {
     private static Stream<Arguments> provideTestCases() {
         return Stream.of(
             // Example test case from problem description
-            Arguments.of("daBcd", "ABC", true),
+            Arguments.of("daBcd", "ABC", Boolean.TRUE),
 
             // Test case where transformation is impossible
-            Arguments.of("dBcd", "ABC", false),
+            Arguments.of("dBcd", "ABC", Boolean.FALSE),
 
             // Test case with exact match (all uppercase)
-            Arguments.of("ABC", "ABC", true),
+            Arguments.of("ABC", "ABC", Boolean.TRUE),
 
             // Test case where input string contains all required letters plus extra lowercase letters
-            Arguments.of("aAbBcC", "ABC", true),
+            Arguments.of("aAbBcC", "ABC", Boolean.TRUE),
 
             // Test case with only lowercase letters in input
-            Arguments.of("abcd", "ABCD", true),
+            Arguments.of("abcd", "ABCD", Boolean.TRUE),
 
             // Test case with an empty second string (b)
-            Arguments.of("abc", "", true),
+            Arguments.of("abc", "", Boolean.TRUE),
 
             // Test case with an empty first string (a) but non-empty second string (b)
-            Arguments.of("", "A", false),
+            Arguments.of("", "A", Boolean.FALSE),
 
             // Complex case with interleaved letters
-            Arguments.of("daBcAbCd", "ABCD", false));
+            Arguments.of("daBcAbCd", "ABCD", Boolean.FALSE));
     }
 }
