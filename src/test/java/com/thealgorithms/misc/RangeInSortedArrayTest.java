4345eebfb7f9cd3f1df7ec11ea9d7cdcc6175a83
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-14 08:43:02 +0530

    Fix

--- src/test/java/com/thealgorithms/misc/RangeInSortedArrayTest.java
+++ src/test/java/com/thealgorithms/misc/RangeInSortedArrayTest.java
@@ -3,68 +3,33 @@ package com.thealgorithms.misc;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class RangeInSortedArrayTest {
 
-    @Test
-    public void testSortedRangeWithMultipleOccurrences() {
-        int[] nums = {1, 2, 3, 3, 3, 4, 5};
-        assertArrayEquals(new int[] {2, 4}, RangeInSortedArray.sortedRange(nums, 3), "Range for key 3 should be [2, 4]");
+    @ParameterizedTest(name = "Test case {index}: {3}")
+    @MethodSource("provideSortedRangeTestCases")
+    void testSortedRange(int[] nums, int key, int[] expectedRange, String description) {
+        assertArrayEquals(expectedRange, RangeInSortedArray.sortedRange(nums, key), description);
     }
 
-    @Test
-    public void testSortedRangeWithSingleOccurrence() {
-        int[] nums = {1, 2, 3, 3, 3, 4, 5};
-        assertArrayEquals(new int[] {5, 5}, RangeInSortedArray.sortedRange(nums, 4), "Range for key 4 should be [5, 5]");
+    private static Stream<Arguments> provideSortedRangeTestCases() {
+        return Stream.of(Arguments.of(new int[] {1, 2, 3, 3, 3, 4, 5}, 3, new int[] {2, 4}, "Range for key 3 with multiple occurrences"), Arguments.of(new int[] {1, 2, 3, 3, 3, 4, 5}, 4, new int[] {5, 5}, "Range for key 4 with single occurrence"),
+            Arguments.of(new int[] {0, 1, 2}, 3, new int[] {-1, -1}, "Range for non-existent key"), Arguments.of(new int[] {}, 1, new int[] {-1, -1}, "Range in empty array"), Arguments.of(new int[] {1, 1, 1, 2, 3, 4, 5, 5, 5}, 1, new int[] {0, 2}, "Range for key at start"),
+            Arguments.of(new int[] {1, 1, 1, 2, 3, 4, 5, 5, 5}, 5, new int[] {6, 8}, "Range for key at end"));
     }
 
-    @Test
-    public void testSortedRangeWithNoOccurrences() {
-        int[] nums = {0, 1, 2};
-        assertArrayEquals(new int[] {-1, -1}, RangeInSortedArray.sortedRange(nums, 3), "Range for key 3 should be [-1, -1] since it doesn't exist");
+    @ParameterizedTest(name = "Test case {index}: {3}")
+    @MethodSource("provideGetCountLessThanTestCases")
+    void testGetCountLessThan(int[] nums, int key, int expectedCount, String description) {
+        assertEquals(expectedCount, RangeInSortedArray.getCountLessThan(nums, key), description);
     }
 
-    @Test
-    public void testSortedRangeInEmptyArray() {
-        int[] nums = {};
-        assertArrayEquals(new int[] {-1, -1}, RangeInSortedArray.sortedRange(nums, 1), "Range for any key in an empty array should be [-1, -1]");
-    }
-
-    @Test
-    public void testSortedRangeAtStartAndEnd() {
-        int[] nums = {1, 1, 1, 2, 3, 4, 5, 5, 5};
-        assertArrayEquals(new int[] {0, 2}, RangeInSortedArray.sortedRange(nums, 1), "Range for key 1 should be [0, 2]");
-        assertArrayEquals(new int[] {6, 8}, RangeInSortedArray.sortedRange(nums, 5), "Range for key 5 should be [6, 8]");
-    }
-
-    @Test
-    public void testGetCountLessThanWithExistingKey() {
-        int[] nums = {1, 2, 3, 3, 4, 5};
-        assertEquals(4, RangeInSortedArray.getCountLessThan(nums, 3), "Count of elements less than 3 should be 2");
-    }
-
-    @Test
-    public void testGetCountLessThanWithNonExistingKey() {
-        int[] nums = {1, 2, 3, 3, 4, 5};
-        assertEquals(5, RangeInSortedArray.getCountLessThan(nums, 4), "Count of elements less than 4 should be 5");
-    }
-
-    @Test
-    public void testGetCountLessThanWithAllSmallerElements() {
-        int[] nums = {1, 2, 2, 3};
-        assertEquals(4, RangeInSortedArray.getCountLessThan(nums, 5), "Count of elements less than 5 should be 4");
-    }
-
-    @Test
-    public void testGetCountLessThanWithNoSmallerElements() {
-        int[] nums = {2, 3, 4, 5};
-        assertEquals(0, RangeInSortedArray.getCountLessThan(nums, 1), "Count of elements less than 1 should be 0");
-    }
-
-    @Test
-    public void testGetCountLessThanWithEmptyArray() {
-        int[] nums = {};
-        assertEquals(0, RangeInSortedArray.getCountLessThan(nums, 1), "Count of elements less than 1 in an empty array should be 0");
+    private static Stream<Arguments> provideGetCountLessThanTestCases() {
+        return Stream.of(Arguments.of(new int[] {1, 2, 3, 3, 4, 5}, 3, 4, "Count of elements less than existing key"), Arguments.of(new int[] {1, 2, 3, 3, 4, 5}, 4, 5, "Count of elements less than non-existing key"), Arguments.of(new int[] {1, 2, 2, 3}, 5, 4, "Count with all smaller elements"),
+            Arguments.of(new int[] {2, 3, 4, 5}, 1, 0, "Count with no smaller elements"), Arguments.of(new int[] {}, 1, 0, "Count in empty array"));
     }
 }
