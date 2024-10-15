cb923bb920d5995f44ec4fe3c32e4b5ef9840fc1
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-15 13:07:29 +0530

    Fix

--- src/test/java/com/thealgorithms/bitmanipulation/IsPowerTwoTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/IsPowerTwoTest.java
@@ -26,26 +26,26 @@ public class IsPowerTwoTest {
     }
 
     private static Stream<Arguments> provideNumbersForPowerTwo() {
-        return Stream.of(Arguments.of(1, true), // 2^0
-            Arguments.of(2, true), // 2^1
-            Arguments.of(4, true), // 2^2
-            Arguments.of(8, true), // 2^3
-            Arguments.of(16, true), // 2^4
-            Arguments.of(32, true), // 2^5
-            Arguments.of(64, true), // 2^6
-            Arguments.of(128, true), // 2^7
-            Arguments.of(256, true), // 2^8
-            Arguments.of(1024, true), // 2^10
-            Arguments.of(0, false), // 0 is not a power of two
-            Arguments.of(-1, false), // Negative number
-            Arguments.of(-2, false), // Negative number
-            Arguments.of(-4, false), // Negative number
-            Arguments.of(3, false), // 3 is not a power of two
-            Arguments.of(5, false), // 5 is not a power of two
-            Arguments.of(6, false), // 6 is not a power of two
-            Arguments.of(15, false), // 15 is not a power of two
-            Arguments.of(1000, false), // 1000 is not a power of two
-            Arguments.of(1023, false) // 1023 is not a power of two
+        return Stream.of(Arguments.of(1, Boolean.TRUE), // 2^0
+            Arguments.of(2, Boolean.TRUE), // 2^1
+            Arguments.of(4, Boolean.TRUE), // 2^2
+            Arguments.of(8, Boolean.TRUE), // 2^3
+            Arguments.of(16, Boolean.TRUE), // 2^4
+            Arguments.of(32, Boolean.TRUE), // 2^5
+            Arguments.of(64, Boolean.TRUE), // 2^6
+            Arguments.of(128, Boolean.TRUE), // 2^7
+            Arguments.of(256, Boolean.TRUE), // 2^8
+            Arguments.of(1024, Boolean.TRUE), // 2^10
+            Arguments.of(0, Boolean.FALSE), // 0 is not a power of two
+            Arguments.of(-1, Boolean.FALSE), // Negative number
+            Arguments.of(-2, Boolean.FALSE), // Negative number
+            Arguments.of(-4, Boolean.FALSE), // Negative number
+            Arguments.of(3, Boolean.FALSE), // 3 is not a power of two
+            Arguments.of(5, Boolean.FALSE), // 5 is not a power of two
+            Arguments.of(6, Boolean.FALSE), // 6 is not a power of two
+            Arguments.of(15, Boolean.FALSE), // 15 is not a power of two
+            Arguments.of(1000, Boolean.FALSE), // 1000 is not a power of two
+            Arguments.of(1023, Boolean.FALSE) // 1023 is not a power of two
         );
     }
 }
