8d799c65412af465246acebbf4ba73ddd9f6d2df
Hardik Pawar <97388607+Hardvan@users.noreply.github.com>
2024-10-15 17:40:54 +0530

    Update Sparsity.java

--- src/main/java/com/thealgorithms/misc/Sparsity.java
+++ src/main/java/com/thealgorithms/misc/Sparsity.java
@@ -14,6 +14,7 @@ final class Sparsity {
     }
 
     /*
+     * @param mat the input matrix
      * @return Sparsity of matrix
      *
      * where sparsity = number of zeroes/total elements in matrix
