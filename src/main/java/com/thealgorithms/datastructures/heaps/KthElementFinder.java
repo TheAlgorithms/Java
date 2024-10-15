5ef4c96319f1e55eb9ab84f8ff7c1c2f0ff5b351
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-15 12:18:47 +0530

    feat: Add `KthElementFinder` new algorithm with Junit tests

---
+++ src/main/java/com/thealgorithms/datastructures/heaps/KthElementFinder.java
@@ -0,0 +1,27 @@
+package com.thealgorithms.datastructures.heaps;
+
+import java.util.PriorityQueue;
+
+public class KthElementFinder {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+
+    public static int findKthSmallest(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        for (int num : nums) {
+            maxHeap.offer(num);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        return maxHeap.peek();
+    }
+}
