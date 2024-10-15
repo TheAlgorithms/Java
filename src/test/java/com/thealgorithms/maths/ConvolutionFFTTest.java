59b050e477f6506ccd2bbac77c76dcdd82b17666
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-15 15:30:13 +0530

    Fix

--- src/test/java/com/thealgorithms/maths/ConvolutionFFTTest.java
+++ src/test/java/com/thealgorithms/maths/ConvolutionFFTTest.java
@@ -3,6 +3,7 @@ package com.thealgorithms.maths;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -24,7 +25,7 @@ public class ConvolutionFFTTest {
     /**
      * Helper method to compare two complex signals for equality within a small margin of error.
      */
-    private void assertComplexArrayEquals(ArrayList<FFT.Complex> expected, ArrayList<FFT.Complex> result, double delta) {
+    private void assertComplexArrayEquals(List<FFT.Complex> expected, List<FFT.Complex> result, double delta) {
         assertEquals(expected.size(), result.size(), "Signal lengths are not equal.");
         for (int i = 0; i < expected.size(); i++) {
             FFT.Complex expectedValue = expected.get(i);
