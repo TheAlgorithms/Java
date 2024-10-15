bff1ecbd9526ad3ace5b0db19a319d5d32ae6299
ANANT JAIN <139585700+anant-jain01@users.noreply.github.com>
2024-10-15 21:12:06 +0530

    Update StalinSort.java

--- src/main/java/com/thealgorithms/sorts/StalinSort.java
+++ src/main/java/com/thealgorithms/sorts/StalinSort.java
@@ -6,7 +6,6 @@ public class StalinSort implements SortAlgorithm {
         if (array.length == 0) {
             return array;
         }
-
         int currentIndex = 0;
         for (int i = 1; i < array.length; i++) {
             if (array[i].compareTo(array[currentIndex]) >= 0) {
@@ -14,22 +13,9 @@ public class StalinSort implements SortAlgorithm {
                 array[currentIndex] = array[i];
             }
         }
-
         // Create a result array with sorted elements
         T[] result = (T[]) java.lang.reflect.Array.newInstance(array.getClass().getComponentType(), currentIndex + 1);
         System.arraycopy(array, 0, result, 0, currentIndex + 1);
         return result;
-    }
-
-    // Driver Program
-    public static void main(String[] args) {
-        // Integer Input
-        Integer[] integers = {4, 23, 6, 78, 1, 54, 231, 9, 12};
-        StalinSort stalinSort = new StalinSort();
-        // print a sorted array
-        SortUtils.print(stalinSort.sort(integers));
-        // String Input
-        String[] strings = {"c", "a", "e", "b", "d"};
-        SortUtils.print(stalinSort.sort(strings));
     }
 }
