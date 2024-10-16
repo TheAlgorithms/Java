0707a130b94ef36b784dfe19ceb85d162ac33360
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-16 08:33:56 +0530

    Fix

--- src/test/java/com/thealgorithms/conversions/IPv6ConverterTest.java
+++ src/test/java/com/thealgorithms/conversions/IPv6ConverterTest.java
@@ -12,11 +12,17 @@ public class IPv6ConverterTest {
         + "0."
         + "2."
         + "128";
-    private static final String EXPECTED_IPV6_MAPPED = ":"+":ff"+"ff"
-        + ":19"+"2."+"0."
+    private static final String EXPECTED_IPV6_MAPPED = ":"
+        + ":ff"
+        + "ff"
+        + ":19"
+        + "2."
+        + "0."
         + "2.128";
     private static final String INVALID_IPV6_MAPPED = "2001:"
-        + "db8"+":"+":1";
+        + "db8"
+        + ":"
+        + ":1";
     private static final String INVALID_IPV4 = "999."
         + "999."
         + "999."
