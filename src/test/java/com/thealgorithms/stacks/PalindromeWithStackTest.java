b193a0a1dd264c47f33ec8d28328205788fb4577
S-M-J-I <jishanlion@gmail.com>
2024-10-18 14:58:18 +0600

    added palindrome checker using stack

---
+++ src/test/java/com/thealgorithms/stacks/PalindromeWithStackTest.java
@@ -0,0 +1,59 @@
+package com.thealgorithms.stacks;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PalindromeWithStackTest {
+
+    private PalindromeWithStack palindromeChecker;
+
+    @BeforeEach
+    public void setUp() {
+        palindromeChecker = new PalindromeWithStack();
+    }
+
+    @Test
+    public void testValidOne() {
+        String testString = "Racecar";
+        assertTrue(palindromeChecker.checkPalindrome(testString));
+    }
+
+    @Test
+    public void testInvalidOne() {
+        String testString = "James";
+        assertFalse(palindromeChecker.checkPalindrome(testString));
+    }
+
+    @Test
+    public void testValidTwo() {
+        String testString = "madam";
+        assertTrue(palindromeChecker.checkPalindrome(testString));
+    }
+
+    @Test
+    public void testInvalidTwo() {
+        String testString = "pantry";
+        assertFalse(palindromeChecker.checkPalindrome(testString));
+    }
+
+    @Test
+    public void testValidThree() {
+        String testString = "RaDar";
+        assertTrue(palindromeChecker.checkPalindrome(testString));
+    }
+
+    @Test
+    public void testInvalidThree() {
+        String testString = "Win";
+        assertFalse(palindromeChecker.checkPalindrome(testString));
+    }
+
+    @Test
+    public void testBlankString() {
+        String testString = "";
+        assertTrue(palindromeChecker.checkPalindrome(testString));
+    }
+}
