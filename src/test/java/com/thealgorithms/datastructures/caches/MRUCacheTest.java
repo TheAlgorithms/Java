00cca7441178216d4a3e8fa10a2ef3180d7db924
Hardik Pawar <97388607+Hardvan@users.noreply.github.com>
2024-10-23 13:52:11 +0530

    Update MRUCacheTest.java

--- src/test/java/com/thealgorithms/datastructures/caches/MRUCacheTest.java
+++ src/test/java/com/thealgorithms/datastructures/caches/MRUCacheTest.java
@@ -1,9 +1,7 @@
 package com.thealgorithms.datastructures.caches;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
