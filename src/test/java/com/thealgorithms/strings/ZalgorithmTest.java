7bee21b028ae5721ad135651472de19254281dbc
Mrinal Chauhan <mrinalchauhan18.com>
2024-10-26 01:39:42 +0530

    fix: updated code to fix errors

--- src/test/java/com/thealgorithms/strings/ZalgorithmTest.java
+++ src/test/java/com/thealgorithms/strings/ZalgorithmTest.java
@@ -1,6 +1,7 @@
 package com.thealgorithms.strings;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
