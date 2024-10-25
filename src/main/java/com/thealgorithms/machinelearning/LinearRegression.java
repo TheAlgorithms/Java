08192cddf85080b25a38a582f22dbd6140599355
Gowtham Kamalasekar <tkgowthamkamal@gmail.com>
2024-10-25 12:59:20 +0530

    Create LinearRegression.java

---
+++ src/main/java/com/thealgorithms/machinelearning/LinearRegression.java
@@ -0,0 +1,55 @@
+package com.thealgorithms.machinelearning;
+
+import java.util.ArrayList;
+
+class LinearRegression {
+    private ArrayList<Double> dependentX = new ArrayList<Double>();
+    private ArrayList<Double> independentY = new ArrayList<Double>();
+    private double m;
+    private double c;
+
+    public LinearRegression(ArrayList<Double> dependentX, ArrayList<Double> independentY) {
+        this.dependentX = dependentX;
+        this.independentY = independentY;
+        this.Equate();
+    }
+
+    private double Sumation(ArrayList<Double> arr){
+        double sum = 0.0;
+
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr.get(i);
+        }
+
+        return sum;
+    }
+
+    private ArrayList<Double> MultiplyNumber(ArrayList<Double> arr1, ArrayList<Double> arr2) {
+        ArrayList<Double> temp = new ArrayList<Double>();
+        for(int i = 0; i < arr1.size(); i++) {
+            temp.add((arr1.get(i) * arr2.get(i)));
+        }
+        return temp;
+    }
+
+    private void Equate(){
+        int n = dependentX.size();
+        this.m = (n * Sumation(MultiplyNumber(independentY, dependentX)) - (Sumation(dependentX) * Sumation(independentY)));
+        this.m = this.m / (n * (Sumation(MultiplyNumber(dependentX, dependentX))) - (Sumation(dependentX) * Sumation(dependentX)));
+
+        this.c = (Sumation(independentY) * Sumation(MultiplyNumber(dependentX, dependentX)) - (Sumation(dependentX) * Sumation(MultiplyNumber(independentY, dependentX))));
+        this.c = this.c / (n * (Sumation(MultiplyNumber(dependentX, dependentX))) - (Sumation(dependentX) * Sumation(dependentX)));
+    }
+
+    public double getM(){
+        return this.m;
+    }
+
+    public double getC(){
+        return this.c;
+    }
+
+    public double PredictForX(double x) {
+        return (this.m * x) + this.c;
+    }
+}
