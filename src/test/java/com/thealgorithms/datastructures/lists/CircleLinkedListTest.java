80c43eb900f6ae020c2814a3224f2233de2e8ae4
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-25 17:38:10 +0530

    Fix

--- src/test/java/com/thealgorithms/datastructures/lists/CircleLinkedListTest.java
+++ src/test/java/com/thealgorithms/datastructures/lists/CircleLinkedListTest.java
@@ -1,9 +1,11 @@
 package com.thealgorithms.datastructures.lists;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CircleLinkedListTest {
 
