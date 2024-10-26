611b2289a46af6e45c84d18cfd922a5654899205
Chiefpatwal <pankajpatwal1224@gmail.com>
2024-10-26 14:49:07 +0530

    updated

--- src/test/java/com/thealgorithms/slidingwindow/MinimumConsecutiveCardsTest.java
+++ src/test/java/com/thealgorithms/slidingwindow/MinimumConsecutiveCardsTest.java
@@ -1,7 +1,8 @@
 package com.thealgorithms.slidingwindow;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit tests for the MinimumConsecutiveCards class.
@@ -13,9 +14,9 @@ public class MinimumConsecutiveCardsTest {
     @Test
     public void testMinimumCardPickup() {
         // Test cases for the minimumCardPickup method
-        assertEquals(2, MinimumConsecutiveCards.minimumCardPickup(new int[]{1, 2, 3, 2, 4, 1}));
-        assertEquals(-1, MinimumConsecutiveCards.minimumCardPickup(new int[]{1, 2, 3, 4, 5}));
-        assertEquals(2, MinimumConsecutiveCards.minimumCardPickup(new int[]{1, 1, 1, 1}));
-        assertEquals(2, MinimumConsecutiveCards.minimumCardPickup(new int[]{2, 2, 3, 4, 5}));
+        assertEquals(2, MinimumConsecutiveCards.minimumCardPickup(new int[] {1, 2, 3, 2, 4, 1}));
+        assertEquals(-1, MinimumConsecutiveCards.minimumCardPickup(new int[] {1, 2, 3, 4, 5}));
+        assertEquals(2, MinimumConsecutiveCards.minimumCardPickup(new int[] {1, 1, 1, 1}));
+        assertEquals(2, MinimumConsecutiveCards.minimumCardPickup(new int[] {2, 2, 3, 4, 5}));
     }
 }
