8b4dfb68bad5eacd649c78f928a7039fc54b6578
SarthakBhushan <sarthakbhushan7s@gmail.com>
2024-10-27 14:15:56 +0530

    Update IsEven.java

--- src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
+++ src/main/java/com/thealgorithms/bitmanipulation/IsEven.java
@@ -12,3 +12,20 @@ public final class IsEven {
         return (number & 1) == 0;
     }
 }
+
+/**
+ * Take input from user 
+ * Checks whether a number is even
+ * @Sarthak Bhushan
+ */
+import java.util.Scanner;
+public static int void(String[] args){
+    Scanner sc= new Scanner(System.in);
+    System.out.print("Enter a number:");
+    int number= sc.nextInt();
+    if(number%2=0){
+        System.out.print("Number is Even");
+    }else{
+        System.out.print("Test Fails");
+    }
+}
