9b18ee317e07e26ccee4fe2def7d1a5490e54930
Chiefpatwal <pankajpatwal1224@gmail.com>
2024-10-28 01:19:51 +0530

    Updated BinaryTree and BinaryTreeTest with encapsulation and test cases

--- src/test/java/com/thealgorithms/datastructures/trees/BinaryTreeTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/BinaryTreeTest.java
@@ -1,13 +1,12 @@
 package com.thealgorithms.datastructures.trees;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
 public class BinaryTreeTest {
 
-    // checks that adding populating the tree and searching for data
-    // retrieves the expected data
     @Test
     void test1() {
         BinaryTree t = new BinaryTree();
@@ -17,12 +16,11 @@ public class BinaryTreeTest {
         t.put(9);
         t.put(12);
 
-        assertEquals(t.find(5).data, 5);
-        assertEquals(t.find(7).data, 7);
+        assertNotNull(t.find(5));
+        assertEquals(5, t.find(5).data);
+        assertEquals(7, t.find(7).data);
     }
 
-    // checks that removing data from the tree
-    // properly removes and makes the new root the expected new root
     @Test
     void test2() {
         BinaryTree t = new BinaryTree();
@@ -35,11 +33,10 @@ public class BinaryTreeTest {
         t.remove(5);
         t.remove(7);
 
-        assertEquals(t.getRoot().data, 9);
+        assertNotNull(t.getRoot(), "Root should not be null after removals");
+        assertEquals(9, t.getRoot().data);
     }
 
-    // checks that removing an unexistend node returns false
-    //  as specified by the documentation of the function
     @Test
     void test3() {
         BinaryTree t = new BinaryTree();
@@ -49,13 +46,10 @@ public class BinaryTreeTest {
         t.put(9);
         t.put(12);
 
-        assertEquals(t.remove(9), true);
-        assertEquals(t.remove(398745987), false);
+        assertEquals(true, t.remove(9));
+        assertEquals(false, t.remove(398745987));
     }
 
-    // check if the bfs, inOrder, preOrder and postOrder functions
-    // worg as expected, also increases the coverage measures in
-    // JaCoCo
     @Test
     void test4() {
         BinaryTree t = new BinaryTree();
@@ -65,12 +59,12 @@ public class BinaryTreeTest {
         t.put(9);
         t.put(12);
 
-        t.bfs(t.find(12));
+        t.bfs(t.getRoot());
         t.inOrder(t.getRoot());
         t.preOrder(t.getRoot());
         t.postOrder(t.getRoot());
 
-        assertEquals(t.remove(9), true);
-        assertEquals(t.remove(398745987), false);
+        assertEquals(true, t.remove(9));
+        assertEquals(false, t.remove(398745987));
     }
 }
