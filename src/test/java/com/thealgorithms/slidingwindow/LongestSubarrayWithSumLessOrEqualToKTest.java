647c4a8fd87a0e1cc02906c8900ea618c9c5756b
Chiefpatwal <pankajpatwal1224@gmail.com>
2024-10-27 16:56:29 +0530

    formatted clang

--- src/test/java/com/thealgorithms/slidingwindow/LongestSubarrayWithSumLessOrEqualToKTest.java
+++ src/test/java/com/thealgorithms/slidingwindow/LongestSubarrayWithSumLessOrEqualToKTest.java
@@ -14,10 +14,10 @@ public class LongestSubarrayWithSumLessOrEqualToKTest {
     @Test
     public void testLongestSubarrayWithSumLEK() {
         // Test cases for the longestSubarrayWithSumLEK method
-        assertEquals(3, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[]{1, 2, 3, 4, 5}, 11));
-        assertEquals(2, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[]{2, 1, 1, 1, 1}, 3));
-        assertEquals(5, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[]{1, 2, 3, 4, 5}, 15));
-        assertEquals(0, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[]{10, 20, 30}, 5));
-        assertEquals(4, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[]{3, 1, 2, 1, 1}, 5));
+        assertEquals(3, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[] {1, 2, 3, 4, 5}, 11));
+        assertEquals(2, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[] {2, 1, 1, 1, 1}, 3));
+        assertEquals(5, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[] {1, 2, 3, 4, 5}, 15));
+        assertEquals(0, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[] {10, 20, 30}, 5));
+        assertEquals(4, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[] {3, 1, 2, 1, 1}, 5));
     }
 }
