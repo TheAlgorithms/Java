fba3e41b387b899d34364bcdaa9070757b39d9a4
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-29 10:45:41 +0530

    Fix

--- src/test/java/com/thealgorithms/datastructures/trees/AVLSimpleTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/AVLSimpleTest.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.datastructures.trees;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
