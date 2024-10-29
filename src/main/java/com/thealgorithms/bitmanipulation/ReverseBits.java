c3bcdfde235d6ec852fc386343ad9c2b2183e050
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-10-29 10:07:28 +0530

    Fix

--- src/main/java/com/thealgorithms/bitmanipulation/ReverseBits.java
+++ src/main/java/com/thealgorithms/bitmanipulation/ReverseBits.java
@@ -32,9 +32,9 @@ public final class ReverseBits {
         int result = 0;
         int bitCount = 32;
         for (int i = 0; i < bitCount; i++) {
-            result <<= 1;
-            result |= (n & 1);
-            n >>= 1;
+            result <<= 1; // Left shift the result to make space for the next bit
+            result |= (n & 1); // OR operation to set the least significant bit of result with the current bit of n
+            n >>= 1; // Right shift n to move on to the next bit
         }
         return result;
     }
