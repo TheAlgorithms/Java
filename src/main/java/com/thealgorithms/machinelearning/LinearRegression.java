51021addee8ec5b74aae8174c4178bccf40272f5
Gowtham Kamalasekar <tkgowthamkamal@gmail.com>
2024-10-30 10:40:24 +0530

    Update LinearRegression.java formats

--- src/main/java/com/thealgorithms/machinelearning/LinearRegression.java
+++ src/main/java/com/thealgorithms/machinelearning/LinearRegression.java
@@ -2,6 +2,7 @@
 package com.thealgorithms.machinelearning;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author : Gowtham Kamalasekar
@@ -15,22 +16,21 @@ import java.util.ArrayList;
  */
 
 class LinearRegression {
-  private ArrayList<Double> dependentX = new ArrayList<Double>();
-  private ArrayList<Double> independentY = new ArrayList<Double>();
-  private double m;
-  private double c;
+    private List<Double> dependentX = new ArrayList<Double>();
+    private List<Double> independentY = new ArrayList<Double>();
+    private double m;
+    private double c;
 
   /**
    * @param : X (dependent variable), Y (independent variable) as ArrayList
    */
-  LinearRegression(ArrayList<Double> dependentX,
-                          ArrayList<Double> independentY) {
+  LinearRegression(List<Double> dependentX, List<Double> independentY) {
     this.dependentX = dependentX;
     this.independentY = independentY;
     this.equate();
   }
 
-  private double sumation(ArrayList<Double> arr) {
+  private double sumation(List<Double> arr) {
     double sum = 0.0;
 
     for (int i = 0; i < arr.size(); i++) {
@@ -40,9 +40,8 @@ class LinearRegression {
     return sum;
   }
 
-  private ArrayList<Double> multiplyNumber(ArrayList<Double> arr1,
-                                           ArrayList<Double> arr2) {
-    ArrayList<Double> temp = new ArrayList<Double>();
+  private List<Double> multiplyNumber(List<Double> arr1, List<Double> arr2) {
+    List<Double> temp = new ArrayList<Double>();
     for (int i = 0; i < arr1.size(); i++) {
       temp.add((arr1.get(i) * arr2.get(i)));
     }
@@ -51,22 +50,22 @@ class LinearRegression {
 
   private void equate() {
     int n = dependentX.size();
-    this.m = (n * sumation(multiplyNumber(independentY, dependentX))
-              - (sumation(dependentX) * sumation(independentY)));
-    this.m = this.m / (n * (sumation(multiplyNumber(dependentX, dependentX)))
-                      - (sumation(dependentX) * sumation(dependentX)));
+    this.m = (n * sumation(multiplyNumber(independentY, dependentX)) - (sumation(dependentX) * sumation(independentY)));
+    this.m = this.m / (n * (sumation(multiplyNumber(dependentX, dependentX))) - (sumation(dependentX) * sumation(dependentX)));
 
-    this.c = (sumation(independentY)
-                *  sumation(multiplyNumber(dependentX, dependentX))
-              - (sumation(dependentX)
-               * sumation(multiplyNumber(independentY, dependentX))));
-    this.c = this.c / (n * (sumation(multiplyNumber(dependentX, dependentX)))
-                      - (sumation(dependentX) * sumation(dependentX)));
+    this.c = (sumation(independentY) *  sumation(multiplyNumber(dependentX, dependentX)) - (sumation(dependentX) * sumation(multiplyNumber(independentY, dependentX))));
+    this.c = this.c / (n * (sumation(multiplyNumber(dependentX, dependentX))) - (sumation(dependentX) * sumation(dependentX)));
   }
 
-  public double getM() { return this.m; }
+  public double getM() { 
+    return this.m; 
+  }
 
-  public double getC() { return this.c; }
+  public double getC() { 
+    return this.c; 
+  }
 
-  public double predictForX(double x) { return (this.m * x) + this.c; }
+  public double predictForX(double x) {
+    return (this.m * x) + this.c; 
+  }
 }
