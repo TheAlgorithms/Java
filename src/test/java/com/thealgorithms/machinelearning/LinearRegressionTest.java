fd339e7cca13598c06c02ed28b833054453f8580
Gowtham Kamalasekar <tkgowthamkamal@gmail.com>
2024-10-30 12:09:00 +0530

    Update LinearRegressionTest.java with clang format

--- src/test/java/com/thealgorithms/machinelearning/LinearRegressionTest.java
+++ src/test/java/com/thealgorithms/machinelearning/LinearRegressionTest.java
@@ -11,7 +11,7 @@ class LinearRegressionTest {
     void testLinearRegression() {
         ArrayList<Double> dependentX = new ArrayList<>();
         ArrayList<Double> independentY = new ArrayList<>();
-    
+
         dependentX.add(1.0);
         independentY.add(2.0);
         dependentX.add(2.0);
@@ -22,14 +22,14 @@ class LinearRegressionTest {
         independentY.add(5.0);
         dependentX.add(5.0);
         independentY.add(6.0);
-    
+
         // Create LinearRegression object
         LinearRegression lr = new LinearRegression(dependentX, independentY);
-    
+
         // Check the slope (m) and intercept (c)
         assertEquals(1.0, lr.getM(), 0.001);
         assertEquals(1.0, lr.getC(), 0.001);
-    
+
         // Check prediction for X = 6
         double predictedY = lr.predictForX(6.0);
         assertEquals(7.0, predictedY, 0.001);
