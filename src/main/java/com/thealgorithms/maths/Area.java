28be9b2424c4f3a047b1ae4fb69a884eabf03202
OmPatel123456 <opatel@uoguelph.ca>
2024-10-29 23:29:46 -0400

    Added surface area calculation for pyramid

--- src/main/java/com/thealgorithms/maths/Area.java
+++ src/main/java/com/thealgorithms/maths/Area.java
@@ -192,4 +192,23 @@ public final class Area {
         }
         return Math.PI * radius * (radius + Math.pow(height * height + radius * radius, 0.5));
     }
+
+    /**
+* Calculate the surface area of a pyramid with a square base.
+*
+* @param sideLength side length of the square base
+* @param slantHeight slant height of the pyramid
+* @return surface area of the given pyramid
+*/
+    public static double surfaceAreaPyramid(final double sideLength, final double slantHeight) {
+        if (sideLength <= 0) {
+        throw new IllegalArgumentException("Must be a positive sideLength");
+        }
+        if (slantHeight <= 0) {
+        throw new IllegalArgumentException("Must be a positive slantHeight");
+        }
+        double baseArea = sideLength * sideLength;
+        double lateralSurfaceArea = 2 * sideLength * slantHeight;
+        return baseArea + lateralSurfaceArea;
+    }
 }
