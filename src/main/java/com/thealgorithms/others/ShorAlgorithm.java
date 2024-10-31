07700c57fd141a103b5ea6485ba5888957099688
Joelkurien <joelkurien7@gmail.com>
2024-10-31 15:21:01 +1100

    Clang Attemp

--- src/main/java/com/thealgorithms/others/ShorAlgorithm.java
+++ src/main/java/com/thealgorithms/others/ShorAlgorithm.java
@@ -3,10 +3,10 @@ package com.thealgorithms.others;
 import java.math.BigInteger;
 import java.util.Random;
 
-//The algorithm is referred from 
-//https://www.geeksforgeeks.org/shors-factorization-algorithm/
+// The algorithm is referred from 
+// https://www.geeksforgeeks.org/shors-factorization-algorithm/
 public class ShorAlgorithm {
-    //trying to find the order of exponent given the base and the number
+    // trying to find the order of exponent given the base and the number
     private int exponent(BigInteger base, BigInteger number) {
         BigInteger result = BigInteger.ONE;
         int increment = 0;
@@ -17,9 +17,9 @@ public class ShorAlgorithm {
         return increment;
     }
 
-    //implementing the shor algorithm
+    // implementing the shor algorithm
     public BigInteger[] shorAlgorithm(BigInteger number) {
-        if(number.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
+        if (number.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
             BigInteger p = number.divide(new BigInteger("2"));
             BigInteger q = new BigInteger("2");
             return new BigInteger[]{p, q};
@@ -32,20 +32,20 @@ public class ShorAlgorithm {
         } while (base.compareTo(BigInteger.ZERO) <= 0 || base.compareTo(number) >= 0);
 
         BigInteger hcf = base.gcd(number);
-        if(hcf.compareTo(BigInteger.ONE) > 0) {
+        if (hcf.compareTo(BigInteger.ONE) > 0) {
             return new BigInteger[]{hcf, number.divide(hcf)};
         }
 
         int result = exponent(base, number);
-        if(result % 2 != 0) return null;
+        if (result % 2 != 0) return null;
 
-        BigInteger congruentResult = base.modPow(BigInteger.valueOf(result/2), number);
-        if(congruentResult.equals(number.subtract(BigInteger.ONE))) return null;
+        BigInteger congruentResult = base.modPow(BigInteger.valueOf(result / 2), number);
+        if (congruentResult.equals(number.subtract(BigInteger.ONE))) return null;
 
         BigInteger p = congruentResult.add(BigInteger.ONE).gcd(number);
         BigInteger q = congruentResult.subtract(BigInteger.ONE).gcd(number);
 
-        if(!p.equals(BigInteger.ONE) && !q.equals(BigInteger.ONE))
+        if (!p.equals(BigInteger.ONE) && !q.equals(BigInteger.ONE))
             return new BigInteger[]{p, q};
         return null;
     }
