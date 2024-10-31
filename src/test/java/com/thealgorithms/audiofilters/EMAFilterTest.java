dcb85f8a763670d3f8b327cb106145ada92f830c
Shreya <95279016+Shreya123714@users.noreply.github.com>
2024-10-31 18:39:03 +0530

    Update EMAFilterTest.java

--- src/test/java/com/thealgorithms/audiofilters/EMAFilterTest.java
+++ src/test/java/com/thealgorithms/audiofilters/EMAFilterTest.java
@@ -36,7 +36,9 @@ public class EMAFilterTest {
 
         // Minimal smoothing (alpha close to 0)
         double[] resultMin = emaFilterMin.apply(audioSignal);
-
+        
+        assertArrayEquals(audioSignal, resultMax, 1e-5);
+        
         // Maximum smoothing (alpha = 1, output should match input)
         double[] resultMax = emaFilterMax.apply(audioSignal);
 
