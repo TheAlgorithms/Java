123f68305699c140629768404c409698b890de98
MohamedBoukthir <medboukthir7@gmail.com>
2024-11-02 20:38:37 +0100

    Declare FibonacciSeries class as final

--- src/main/java/com/thealgorithms/Recursion/FibonacciSeries.java
+++ src/main/java/com/thealgorithms/Recursion/FibonacciSeries.java
@@ -7,7 +7,7 @@ package com.thealgorithms.Recursion;
        FIBONACCI 0 1 1 2 3 5 8 13 21 34 55 ...
 */
 
-public class FibonacciSeries {
+public final class FibonacciSeries {
     private FibonacciSeries() {
         throw new UnsupportedOperationException("Utility class");
     }
