33dd8f5652661e6421edfeebb65c336653b892ec
Phat <nguyentanphat8694@gmail.com>
2024-12-21 15:23:13 +0700

    remove redundant import

--- src/test/java/com/thealgorithms/dynamicprogramming/EditDistanceTest.java
+++ src/test/java/com/thealgorithms/dynamicprogramming/EditDistanceTest.java
@@ -2,7 +2,6 @@ package com.thealgorithms.dynamicprogramming;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
