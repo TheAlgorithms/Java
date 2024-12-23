357da093b7e9ec554318657b0bbcf07c94b29a67
Stanislav Belogolov <shisoik@gmail.com>
2024-12-23 13:50:02 +0100

    Reformat VampireNumberTest not to use static imports at all to make Maven PMD check happy

--- src/test/java/com/thealgorithms/maths/VampireNumberTest.java
+++ src/test/java/com/thealgorithms/maths/VampireNumberTest.java
@@ -1,37 +1,32 @@
 package com.thealgorithms.maths;
 
-import static com.thealgorithms.maths.VampireNumber.isVampireNumber;
-import static com.thealgorithms.maths.VampireNumber.splitIntoSortedDigits;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class VampireNumberTest {
     @Test
     void areVampireNumbers() {
-        assertTrue(isVampireNumber(15, 93, true));
-        assertTrue(isVampireNumber(135, 801, true));
-        assertTrue(isVampireNumber(201, 600, true));
+        Assertions.assertTrue(VampireNumber.isVampireNumber(15, 93, true));
+        Assertions.assertTrue(VampireNumber.isVampireNumber(135, 801, true));
+        Assertions.assertTrue(VampireNumber.isVampireNumber(201, 600, true));
     }
 
     @Test
     void arePseudoVampireNumbers() {
-        assertTrue(isVampireNumber(150, 93, false));
-        assertTrue(isVampireNumber(546, 84, false));
-        assertTrue(isVampireNumber(641, 65, false));
+        Assertions.assertTrue(VampireNumber.isVampireNumber(150, 93, false));
+        Assertions.assertTrue(VampireNumber.isVampireNumber(546, 84, false));
+        Assertions.assertTrue(VampireNumber.isVampireNumber(641, 65, false));
     }
 
     @Test
     void areNotVampireNumbers() {
-        assertFalse(isVampireNumber(51, 39, false));
-        assertFalse(isVampireNumber(51, 39, true));
+        Assertions.assertFalse(VampireNumber.isVampireNumber(51, 39, false));
+        Assertions.assertFalse(VampireNumber.isVampireNumber(51, 39, true));
     }
 
     @Test
     void testSplitIntoSortedDigits() {
-        assertEquals("123", splitIntoSortedDigits(321));
-        assertEquals("02234", splitIntoSortedDigits(20, 324));
+        Assertions.assertEquals("123", VampireNumber.splitIntoSortedDigits(321));
+        Assertions.assertEquals("02234", VampireNumber.splitIntoSortedDigits(20, 324));
     }
 }
