c8f0a665cd470f3af560f5b933e2330eabbf0b55
Stanislav Belogolov <shisoik@gmail.com>
2024-12-23 13:40:16 +0100

    Reformat VampireNumberTest without * imports for asserts

--- src/test/java/com/thealgorithms/maths/VampireNumberTest.java
+++ src/test/java/com/thealgorithms/maths/VampireNumberTest.java
@@ -2,7 +2,9 @@ package com.thealgorithms.maths;
 
 import static com.thealgorithms.maths.VampireNumber.isVampireNumber;
 import static com.thealgorithms.maths.VampireNumber.splitIntoSortedDigits;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
