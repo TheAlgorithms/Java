194e1a99799ffa9ebd01e0aeaa3728f99983b358
itakurah <122729995+itakurah@users.noreply.github.com>
2025-02-13 12:32:46 +0100

    test clang format

--- src/test/java/com/thealgorithms/datastructures/crdt/LWWElementSetTest.java
+++ src/test/java/com/thealgorithms/datastructures/crdt/LWWElementSetTest.java
@@ -1,11 +1,10 @@
 package com.thealgorithms.datastructures.crdt;
 
-import java.time.Instant;
-import org.junit.jupiter.api.Test;
-
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Instant;
+import org.junit.jupiter.api.Test;
 
 class LWWElementSetTest {
 
