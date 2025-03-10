bcb26d1db937c3c5a77b4913d1df0031276da70d
Sadiul Hakim <sadiulhakim@gmail.com>
2025-03-10 12:46:35 +0600

    Removing all CheckStyle issues

--- src/test/java/com/thealgorithms/maths/MathBuilderTest.java
+++ src/test/java/com/thealgorithms/maths/MathBuilderTest.java
@@ -1,7 +1,7 @@
 package com.thealgorithms.maths;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
