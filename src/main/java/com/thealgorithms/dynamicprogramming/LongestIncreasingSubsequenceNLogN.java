153e3e1db3e07a169e1f355de0d460ece4bc50b1
Vusal Huseynov <vslh333222111@gmail.com>
2025-04-14 20:48:40 +0400

    Add LongestIncreasingSubsequenceNLogN class and corresponding test class

--- src/main/java/com/thealgorithms/dynamicprogramming/LongestIncreasingSubsequenceNLogN.java
+++ src/main/java/com/thealgorithms/dynamicprogramming/LongestIncreasingSubsequenceNLogN.java
@@ -1,4 +1,71 @@
 package com.thealgorithms.dynamicprogramming;
 
+/**
+ * Implementation of the Longest Increasing Subsequence (LIS) problem using
+ * an O(n log n) dynamic programming solution enhanced with binary search.
+ *
+ * @author Vusal Huseynov (https://github.com/huseynovvusal)
+ */
 public class LongestIncreasingSubsequenceNLogN {
+
+    /**
+     * Finds the index of the smallest element in the array that is greater than
+     * or equal to the target using binary search. If no such element exists,
+     * returns the index where the target can be inserted to maintain sorted order.
+     *
+     * @param arr    The array to search in (assumed to be sorted up to a certain point).
+     * @param target The target value to find the lower bound for.
+     * @return The index of the lower bound.
+     */
+    private static int lowerBound(int[] arr, int target) {
+        int l = 0, r = arr.length;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (target > arr[mid]) {
+                // Move right if target is greater than mid element
+                l = mid + 1;
+            } else {
+                // Move left if target is less than or equal to mid element
+                r = mid; 
+            }
+        }
+
+        // Return the index where the target can be inserted
+        return l; 
+    }
+
+    /**
+     * Calculates the length of the Longest Increasing Subsequence (LIS) in the given array.
+     *
+     * @param arr The input array of integers.
+     * @return The length of the LIS.
+     */
+    public static int lengthOfLIS(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0; // Return 0 for empty or null arrays
+        }
+
+        // tails[i] - the smallest end element of an increasing subsequence of length i+1
+        int[] tails = new int[arr.length];
+        // size - the length of the longest increasing subsequence found so far
+        int size = 0;
+
+        for (int x : arr) {
+            // Find the position to replace or extend the subsequence
+            int index = lowerBound(tails, x);
+
+            // Update the tails array with the current element
+            tails[index] = x;
+
+            // If the element extends the subsequence, increase the size
+            if (index == size) {
+                size++;
+            }
+        }
+
+    // Return the length of the LIS
+        return size;
+    }
 }
