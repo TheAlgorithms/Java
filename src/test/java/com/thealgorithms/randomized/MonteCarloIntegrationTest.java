1109024f4d5f7f1631ff678b76c6c8c462709a70
MuhammadEzzatHBK <muhammadaymanezzat@gmail.com>
2025-05-08 00:31:36 +0300

    #6219: Removing package imports

--- src/test/java/com/thealgorithms/randomized/MonteCarloIntegrationTest.java
+++ src/test/java/com/thealgorithms/randomized/MonteCarloIntegrationTest.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.randomized;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.function.Function;
 import org.junit.jupiter.api.Test;
