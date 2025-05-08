1ba58d2c085e7fb86aa468c910a7cd454847f76b
Vibhu Khera <vibhukhera3@gmail.com>
2025-05-08 23:48:31 +0530

    Build issue fix for swap function

--- src/main/java/com/thealgorithms/randomized/RandomizedQuickSort.java
+++ src/main/java/com/thealgorithms/randomized/RandomizedQuickSort.java
@@ -57,7 +57,10 @@ public final class RandomizedQuickSort {
      * @param j the second index
      */
     private static void swap(int[] arr, int i, int j) {
-        if (i == j) return; // Skip if indices are the same
+        // Skip if indices are the same
+        if (i == j) {
+            return;
+        } 
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
