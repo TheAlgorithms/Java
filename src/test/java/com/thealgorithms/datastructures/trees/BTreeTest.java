84ff2380c0a76fe32a9ef06399ad7cbb03f269bc
Soham Kamble <121136639+skamble2@users.noreply.github.com>
2025-05-26 10:30:47 -0500

    updated code to handle duplicate keys

--- src/test/java/com/thealgorithms/datastructures/trees/BTreeTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/BTreeTest.java
@@ -1,6 +1,8 @@
 package com.thealgorithms.datastructures.trees;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
