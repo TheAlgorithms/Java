5f4c7275bab5631edd10751b5f5228c1cef8c46d
vil02 <65706193+vil02@users.noreply.github.com>
2025-05-31 18:19:36 +0200

    style: do not use `ArgumentsProvider` as deprecated

--- src/test/java/com/thealgorithms/sorts/SpreadSortTest.java
+++ src/test/java/com/thealgorithms/sorts/SpreadSortTest.java
@@ -6,8 +6,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
-import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class SpreadSortTest extends SortingAlgorithmTest {
 
@@ -20,16 +19,13 @@ public class SpreadSortTest extends SortingAlgorithmTest {
         return new SpreadSort();
     }
 
-    static class ConstructorArgumentsProvider implements ArgumentsProvider {
-        @Override
-        public Stream<? extends Arguments> provideArguments(org.junit.jupiter.api.extension.ExtensionContext context) {
-            return Stream.of(Arguments.of(0, 16, 2, IllegalArgumentException.class), Arguments.of(16, 0, 2, IllegalArgumentException.class), Arguments.of(16, 16, 0, IllegalArgumentException.class), Arguments.of(1001, 16, 2, IllegalArgumentException.class),
-                Arguments.of(16, 1001, 2, IllegalArgumentException.class), Arguments.of(16, 16, 101, IllegalArgumentException.class));
-        }
+    private static Stream<Arguments> wrongConstructorInputs() {
+        return Stream.of(Arguments.of(0, 16, 2, IllegalArgumentException.class), Arguments.of(16, 0, 2, IllegalArgumentException.class), Arguments.of(16, 16, 0, IllegalArgumentException.class), Arguments.of(1001, 16, 2, IllegalArgumentException.class),
+            Arguments.of(16, 1001, 2, IllegalArgumentException.class), Arguments.of(16, 16, 101, IllegalArgumentException.class));
     }
 
     @ParameterizedTest
-    @ArgumentsSource(ConstructorArgumentsProvider.class)
+    @MethodSource("wrongConstructorInputs")
     void testConstructor(int insertionSortThreshold, int initialBucketCapacity, int minBuckets, Class<Exception> expectedException) {
         Executable executable = () -> new SpreadSort(insertionSortThreshold, initialBucketCapacity, minBuckets);
         assertThrows(expectedException, executable);
