ca488622600c39de6d80762fbad2d46c379133bf
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-06-03 14:57:15 -0500

    [#6258][BUG] Tests of Isomorphic

--- src/test/java/com/thealgorithms/strings/IsomorphicTest.java
+++ src/test/java/com/thealgorithms/strings/IsomorphicTest.java
@@ -3,29 +3,29 @@ package com.thealgorithms.strings;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public final class IsomorphicTest {
-    private IsomorphicTest() {
+
+    @ParameterizedTest
+    @MethodSource("isomorphicProvider")
+    public void testCheckStrings(String str1, String str2, boolean expected) {
+        assertEquals(expected, Isomorphic.checkStrings(str1, str2));
     }
-
-    @Test
-    public static void main(String[] args) {
-        String str1 = "abbbbaac";
-        String str2 = "kffffkkd";
-
-        String str3 = "xyxyxy";
-        String str4 = "bnbnbn";
-
-        String str5 = "ghjknnmm";
-        String str6 = "wertpopo";
-
-        String str7 = "aaammmnnn";
-        String str8 = "ggghhhbbj";
-
-        assertTrue(Isomorphic.checkStrings(str1, str2));
-        assertTrue(Isomorphic.checkStrings(str3, str4));
-        assertFalse(Isomorphic.checkStrings(str5, str6));
-        assertFalse(Isomorphic.checkStrings(str7, str8));
+    
+    private static Stream<Arguments> isomorphicProvider() {
+        return Stream.of(
+            Arguments.of("abbbbaac", "kffffkkd", true),
+            Arguments.of("xyxyxy", "bnbnbn", true),
+            Arguments.of("ghjknnmm", "wertpopo", false),
+            Arguments.of("aaammmnnn", "ggghhhbbj", false)
+        );
     }
 }
