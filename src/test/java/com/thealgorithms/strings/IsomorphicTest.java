f2d1f5645fa445b121d25e94d03c09c4c1e782a4
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-06-03 15:14:00 -0500

    removed unused imports

--- src/test/java/com/thealgorithms/strings/IsomorphicTest.java
+++ src/test/java/com/thealgorithms/strings/IsomorphicTest.java
@@ -1,11 +1,8 @@
 package com.thealgorithms.strings;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
