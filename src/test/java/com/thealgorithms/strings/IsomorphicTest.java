07bfcc06a42e855e579c2efc5e84402a1e36dae7
Raveendra Pujari <raveendra.dhananjaya@gmail.com>
2025-06-03 15:10:35 -0500

    with clang-format

--- src/test/java/com/thealgorithms/strings/IsomorphicTest.java
+++ src/test/java/com/thealgorithms/strings/IsomorphicTest.java
@@ -1,12 +1,10 @@
 package com.thealgorithms.strings;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -19,13 +17,8 @@ public final class IsomorphicTest {
     public void testCheckStrings(String str1, String str2, boolean expected) {
         assertEquals(expected, Isomorphic.checkStrings(str1, str2));
     }
-    
+
     private static Stream<Arguments> isomorphicProvider() {
-        return Stream.of(
-            Arguments.of("abbbbaac", "kffffkkd", true),
-            Arguments.of("xyxyxy", "bnbnbn", true),
-            Arguments.of("ghjknnmm", "wertpopo", false),
-            Arguments.of("aaammmnnn", "ggghhhbbj", false)
-        );
+        return Stream.of(Arguments.of("abbbbaac", "kffffkkd", true), Arguments.of("xyxyxy", "bnbnbn", true), Arguments.of("ghjknnmm", "wertpopo", false), Arguments.of("aaammmnnn", "ggghhhbbj", false));
     }
 }
