78a9c4004697c86b88e64668c560072e75bf68dc
Pau López Núñez <paulopeznunez@gmail.com>
2025-06-08 17:52:30 +0200

    Add Boyer-Moore string search algorithm

---
+++ src/main/java/com/thealgorithms/searches/BoyerMoore.java
@@ -0,0 +1,48 @@
+package com.thealgorithms.searches;
+
+/**
+ * Boyer-Moore string search algorithm
+ * Efficient algorithm for substring search.
+ * 
+ * @author Pau
+*/
+
+public class BoyerMoore {
+
+    private final int R;
+    private int[] right;
+    private String pattern;
+
+    public BoyerMoore(String pat) {
+        this.pattern = pat;
+        this.R = 256;
+        this.right = new int[R];
+        for (int c = 0; c < R; c++)
+            right[c] = -1;
+        for (int j = 0; j < pat.length(); j++)
+            right[pat.charAt(j)] = j;
+    }
+
+    public int search(String text) {
+        int m = pattern.length();
+        int n = text.length();
+        int skip;
+
+        for (int i = 0; i <= n - m; i += skip) {
+            skip = 0;
+            for (int j = m - 1; j >= 0; j--) {
+                if (pattern.charAt(j) != text.charAt(i + j)) {
+                    skip = Math.max(1, j - right[text.charAt(i + j)]);
+                    break;
+                }
+            }
+            if (skip == 0)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int search(String text, String pattern) {
+        return new BoyerMoore(pattern).search(text);
+    }
+}
