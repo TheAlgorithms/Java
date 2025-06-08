ed75b53d914c1d7f539dd6e014687a1a21a238ef
Pau López Núñez <paulopeznunez@gmail.com>
2025-06-08 18:38:37 +0200

    Update BoyerMooreTest.java

--- src/test/java/com/thealgorithms/searches/BoyerMooreTest.java
+++ src/test/java/com/thealgorithms/searches/BoyerMooreTest.java
@@ -3,7 +3,6 @@ package com.thealgorithms.searches;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import com.thealgorithms.searches.BoyerMoore; 
 
 public class BoyerMooreTest {
 
