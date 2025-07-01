c188912261983d93e7e4ba5cecfa6772d6577757
SChandrasekhar96 <scsekhar8006@gmail.com>
2025-07-01 19:40:35 +0530

    Add Binary Search Implemetation

--- src/main/java/com/thealgorithms/tree/BinarySearchTree.java
+++ src/main/java/com/thealgorithms/tree/BinarySearchTree.java
@@ -67,13 +67,13 @@ public class BinarySearchTree {
 
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
-        // tree.insert(50);
-        // tree.insert(30);    
-        // tree.insert(20);
-        // tree.insert(40);
-        // tree.insert(70);
-        // tree.insert(60);
-        // tree.insert(80);
+        tree.insert(50);
+        tree.insert(30);    
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
 
         System.out.println("Inorder traversal of the given tree");
         tree.inorder();
