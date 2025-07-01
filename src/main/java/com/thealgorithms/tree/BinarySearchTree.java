d8ec12484a0e6635679d623c48f1e062a87f9b48
SChandrasekhar96 <scsekhar8006@gmail.com>
2025-07-01 19:57:58 +0530

    Fix: clang format for BinarySearchTree.java

--- src/main/java/com/thealgorithms/tree/BinarySearchTree.java
+++ src/main/java/com/thealgorithms/tree/BinarySearchTree.java
@@ -3,62 +3,61 @@ package com.thealgorithms.tree;
 public class BinarySearchTree {
     class Node {
         int key;
-        Node left,right;
-        Node(int key){
+        Node left, right;
+        Node(int key) {
             this.key = key;
             left = right = null;
         }
-
     }
 
     Node root;
 
-    public void insert(int key){
-        root = insertRec(root,key);
+    public void insert(int key) {
+        root = insertRec(root, key);
     }
 
-    public Node insertRec(Node root, int key){
-        if(root == null){
+    public Node insertRec(Node root, int key) {
+        if (root == null) {
             return new Node(key);
         }
-        if(key < root.key){
-            root.left = insertRec(root.left,key);
-        }else if (key > root.key){
-            root.right = insertRec(root.right,key);
+        if (key < root.key) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.key) {
+            root.right = insertRec(root.right, key);
         }
         return root;
     }
 
-    public void inorder(){
+    public void inorder() {
         inorderRec(root);
     }
-    
-    void inorderRec(Node root){
-        if(root != null){
+
+    public void inorderRec(Node root) {
+        if (root != null) {
             inorderRec(root.left);
             System.out.print(root.key + " ");
             inorderRec(root.right);
         }
     }
 
-    public void preOrder(){
+    public void preOrder() {
         preOrderRec(root);
     }
 
-    void preOrderRec(Node root){
-        if(root !=null){
+    public void preOrderRec(Node root) {
+        if (root != null) {
             System.out.print(root.key + " ");
             preOrderRec(root.left);
             preOrderRec(root.right);
         }
     }
 
-    public void postOrder(){
+    public void postOrder() {
         postOrderRec(root);
     }
 
-    void postOrderRec(Node root){
-        if(root!=null){
+    public void postOrderRec(Node root) {
+        if (root != null) {
             postOrderRec(root.left);
             postOrderRec(root.right);
             System.out.print(root.key + " ");
@@ -68,7 +67,7 @@ public class BinarySearchTree {
     public static void main(String[] args) {
         BinarySearchTree tree = new BinarySearchTree();
         tree.insert(50);
-        tree.insert(30);    
+        tree.insert(30);
         tree.insert(20);
         tree.insert(40);
         tree.insert(70);
@@ -88,4 +87,3 @@ public class BinarySearchTree {
         System.out.println();
     }
 }
-
