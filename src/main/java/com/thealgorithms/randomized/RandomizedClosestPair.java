811de622fecbd71b1e69176c974540d3d6d07118
aditya-7562 <aditya.12114424@lpu.in>
2025-07-04 11:28:33 +0530

    Improved formatting of the code

--- src/main/java/com/thealgorithms/randomized/RandomizedClosestPair.java
+++ src/main/java/com/thealgorithms/randomized/RandomizedClosestPair.java
@@ -1,10 +1,10 @@
 package com.thealgorithms.randomized;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Random;
 
 /**
--- src/test/java/com/thealgorithms/randomized/RandomizedClosestPairTest.java
+++ src/test/java/com/thealgorithms/randomized/RandomizedClosestPairTest.java
@@ -9,38 +9,21 @@ public class RandomizedClosestPairTest {
 
     @Test
     public void testFindClosestPairDistance() {
-        Point[] points = {
-            new Point(1, 1),
-            new Point(2, 2),
-            new Point(3, 3),
-            new Point(8, 8),
-            new Point(13, 13)
-        };
-
+        Point[] points = {new Point(1, 1), new Point(2, 2), new Point(3, 3), new Point(8, 8), new Point(13, 13)};
         double result = RandomizedClosestPair.findClosestPairDistance(points);
         assertEquals(Math.sqrt(2), result, 0.00001);
     }
 
     @Test
     public void testWithIdenticalPoints() {
-        Point[] points = {
-            new Point(0, 0),
-            new Point(0, 0),
-            new Point(1, 1)
-        };
-
+        Point[] points = {new Point(0, 0), new Point(0, 0), new Point(1, 1)};
         double result = RandomizedClosestPair.findClosestPairDistance(points);
         assertEquals(0.0, result, 0.00001);
     }
 
     @Test
     public void testWithDistantPoints() {
-        Point[] points = {
-            new Point(0, 0),
-            new Point(5, 0),
-            new Point(10, 0)
-        };
-
+        Point[] points = {new Point(0, 0), new Point(5, 0), new Point(10, 0)};
         double result = RandomizedClosestPair.findClosestPairDistance(points);
         assertEquals(5.0, result, 0.00001);
     }
