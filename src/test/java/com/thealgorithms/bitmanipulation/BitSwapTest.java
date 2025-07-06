f8ed188bee9975f128ca328b4c404d359330a9ee
Alex Klymenko <alexanderklmn@gmail.com>
2025-07-06 14:22:32 +0200

    refactor: fix clang format issues

--- src/test/java/com/thealgorithms/bitmanipulation/BitSwapTest.java
+++ src/test/java/com/thealgorithms/bitmanipulation/BitSwapTest.java
@@ -37,46 +37,26 @@ class BitSwapTest {
     @ParameterizedTest(name = "Invalid positions: data={0}, posA={1}, posB={2} should throw")
     @MethodSource("provideInvalidPositions")
     void invalidPositionThrowsException(int data, int posA, int posB) {
-        assertThrows(IllegalArgumentException.class,
-                () -> BitSwap.bitSwap(data, posA, posB));
+        assertThrows(IllegalArgumentException.class, () -> BitSwap.bitSwap(data, posA, posB));
     }
 
     static Stream<Arguments> provideAdditionalCases() {
-        return Stream.of(
-                Arguments.of(3, 0, 1, 3),
-                Arguments.of(6, 0, 1, 5),
-                Arguments.of(7, 1, 1, 7)
-        );
+        return Stream.of(Arguments.of(3, 0, 1, 3), Arguments.of(6, 0, 1, 5), Arguments.of(7, 1, 1, 7));
     }
 
     static Stream<Arguments> provideDifferentBitsCases() {
-        return Stream.of(
-                Arguments.of(0b01, 0, 1, 0b10)
-        );
+        return Stream.of(Arguments.of(0b01, 0, 1, 0b10));
     }
 
     static Stream<Arguments> provideSameBitsCases() {
-        return Stream.of(
-                Arguments.of(0b111, 0, 2),
-                Arguments.of(0b0, 1, 3),
-                Arguments.of(0b1010, 1, 3),
-                Arguments.of(-1, 5, 5)
-        );
+        return Stream.of(Arguments.of(0b111, 0, 2), Arguments.of(0b0, 1, 3), Arguments.of(0b1010, 1, 3), Arguments.of(-1, 5, 5));
     }
 
     static Stream<Arguments> provideEdgeCases() {
-        return Stream.of(
-                Arguments.of(Integer.MIN_VALUE, 31, 0, 1),
-                Arguments.of(0, 0, 31, 0)
-        );
+        return Stream.of(Arguments.of(Integer.MIN_VALUE, 31, 0, 1), Arguments.of(0, 0, 31, 0));
     }
 
     static Stream<Arguments> provideInvalidPositions() {
-        return Stream.of(
-                Arguments.of(0, -1, 0),
-                Arguments.of(0, 0, 32),
-                Arguments.of(0, -5, 33),
-                Arguments.of(0, Integer.MIN_VALUE, Integer.MAX_VALUE)
-        );
+        return Stream.of(Arguments.of(0, -1, 0), Arguments.of(0, 0, 32), Arguments.of(0, -5, 33), Arguments.of(0, Integer.MIN_VALUE, Integer.MAX_VALUE));
     }
 }
