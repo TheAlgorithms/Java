59e980a2db22876babb79d7c68e55b3eeea58646
alxkm <alexanderklmn@gmail.com>
2025-07-13 13:03:53 +0200

    refactor: fix edge cases

--- src/main/java/com/thealgorithms/maths/Ceil.java
+++ src/main/java/com/thealgorithms/maths/Ceil.java
@@ -16,12 +16,19 @@ public final class Ceil {
      * @return the smallest double greater than or equal to {@code number}
      */
     public static double ceil(double number) {
-        long intPart = (long) number;
+        if (Double.isNaN(number) || Double.isInfinite(number) || number == 0.0 || number < Integer.MIN_VALUE || number > Integer.MAX_VALUE) {
+            return number;
+        }
 
+        if (number < 0.0 && number > -1.0) {
+            return -0.0;
+        }
+
+        long intPart = (long) number;
         if (number > 0 && number != intPart) {
             return intPart + 1.0;
         } else {
-            return (double) intPart;
+            return intPart;
         }
     }
 }
--- src/test/java/com/thealgorithms/maths/CeilTest.java
+++ src/test/java/com/thealgorithms/maths/CeilTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class CeilTest {
 
@@ -11,5 +14,17 @@ public class CeilTest {
     @CsvSource({"7.057, 8", "7.004, 8", "-13.004, -13", "0.98, 1", "-11.357, -11"})
     void testCeil(double input, int expected) {
         assertEquals(expected, Ceil.ceil(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource("edgeCaseProvider")
+    void testEdgeCases(TestData data) {
+        assertEquals(Ceil.ceil(data.input), data.expected);
+    }
+
+    record TestData(double input, double expected) {}
+
+    static Stream<TestData> edgeCaseProvider() {
+        return Stream.of(new TestData(Double.MAX_VALUE, Double.MAX_VALUE), new TestData(Double.MIN_VALUE, Math.ceil(Double.MIN_VALUE)), new TestData(0.0, Math.ceil(0.0)), new TestData(-0.0, Math.ceil(-0.0)), new TestData(Double.NaN, Math.ceil(Double.NaN)), new TestData(Double.NEGATIVE_INFINITY, Math.ceil(Double.NEGATIVE_INFINITY)), new TestData(Double.POSITIVE_INFINITY, Math.ceil(Double.POSITIVE_INFINITY)));
     }
 }
