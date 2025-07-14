e843dcdd96a9cef6b68a8e458d7a923c410b80de
alxkm <alexanderklmn@gmail.com>
2025-07-14 19:59:17 +0200

    checkstyle: fix import order

--- src/test/java/com/thealgorithms/stacks/MinStackUsingTwoStacksTest.java
+++ src/test/java/com/thealgorithms/stacks/MinStackUsingTwoStacksTest.java
@@ -3,8 +3,8 @@ package com.thealgorithms.stacks;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
 import java.util.EmptyStackException;
+import org.junit.jupiter.api.Test;
 
 public class MinStackUsingTwoStacksTest {
 
