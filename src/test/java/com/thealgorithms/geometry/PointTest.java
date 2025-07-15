028cff033c400057a15a040d06cdcb0098fe2897
Aaron <aaron@hochschule.de>
2025-07-15 19:25:48 +0200

    style: fixed imports to not use the '.*' form

--- src/test/java/com/thealgorithms/geometry/PointTest.java
+++ src/test/java/com/thealgorithms/geometry/PointTest.java
@@ -1,6 +1,8 @@
 package com.thealgorithms.geometry;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
