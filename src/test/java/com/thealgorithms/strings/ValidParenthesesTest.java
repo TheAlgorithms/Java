6ae89a82470b6528086c20fb68f090e40b24ea7b
alxkm <alexanderklmn@gmail.com>
2025-07-16 23:32:10 +0200

    style: fix import

--- src/test/java/com/thealgorithms/strings/ValidParenthesesTest.java
+++ src/test/java/com/thealgorithms/strings/ValidParenthesesTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.api.Test;
 
 public class ValidParenthesesTest {
 
