160f1e413ff6262db7f80ca3e57f7eac20feac2e
Madan H K <109542799+madanhk18@users.noreply.github.com>
2025-07-17 23:26:00 +0530

    Add HashSet data structure implementation for integers (#6304)

---
+++ src/main/java/com/thealgorithms/datastructures/hashset/HashSet.java
@@ -0,0 +1,63 @@
+
+package com.thealgorithms.datastructures.hashset;
+
+/**
+ * Example usage:
+ * <pre>
+ * HashSet set = new HashSet();
+ * set.add(5);
+ * set.add(10);
+ * System.out.println(set.contains(5)); // true
+ * set.remove(5);
+ * System.out.println(set.contains(5)); // false
+ * </pre>
+ */
+
+import java.util.LinkedList;
+
+/**
+ * A simple implementation of a HashSet for integers using separate chaining.
+ */
+public class HashSet {
+    private static final int INITIAL_CAPACITY = 16;
+    private LinkedList<Integer>[] buckets;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public HashSet() {
+        buckets = new LinkedList[INITIAL_CAPACITY];
+        for (int i = 0; i < INITIAL_CAPACITY; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+        size = 0;
+    }
+
+    private int hash(int key) {
+        return Math.abs(key) % buckets.length;
+    }
+
+    public void add(int key) {
+        int idx = hash(key);
+        if (!buckets[idx].contains(key)) {
+            buckets[idx].add(key);
+            size++;
+        }
+    }
+
+    public boolean contains(int key) {
+        int idx = hash(key);
+        return buckets[idx].contains(key);
+    }
+
+    public void remove(int key) {
+        int idx = hash(key);
+        if (buckets[idx].remove((Integer) key)) {
+            size--;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+}
+
