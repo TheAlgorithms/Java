d7f156187300fa31a67514c06cf0c883f4429074
alxkm <alexanderklmn@gmail.com>
2025-07-18 20:02:24 +0200

    testing: fix to many static imports

--- src/test/java/com/thealgorithms/datastructures/queues/DequeTest.java
+++ src/test/java/com/thealgorithms/datastructures/queues/DequeTest.java
@@ -1,10 +1,6 @@
 package com.thealgorithms.datastructures.queues;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
