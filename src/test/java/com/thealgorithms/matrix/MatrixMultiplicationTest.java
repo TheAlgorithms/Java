1ff92f53e93a0aefd1c1d6fc921be68c5e12f88a
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-07-19 10:01:06 +0530

    method for 2 by 2 matrix multiplication is created

--- src/test/java/com/thealgorithms/matrix/MatrixMultiplicationTest.java
+++ src/test/java/com/thealgorithms/matrix/MatrixMultiplicationTest.java
@@ -1,4 +1,19 @@
 package com.thealgorithms.matrix;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class MatrixMultiplicationTest {
+    @Test
+    void testMultiply2by2(){
+        double[][] matrixA = {{1.0,2.0},{3.0,4.0}};
+        double[][] matrixB = {{5.0,6.0},{7.0,8.0}};
+        double[][] expected = {{19.0, 22.0}, {43.0, 50.0}};
+
+        double[][] result = MatrixMultiplication.multiply(matrixA, matrixB);
+        assertMatrixEquals(expected, result);
+    }
+
 }
