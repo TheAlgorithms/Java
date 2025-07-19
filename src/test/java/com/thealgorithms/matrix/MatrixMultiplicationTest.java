4e4625ce5970281255df164121efa459df7270c6
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-07-19 10:14:51 +0530

    method created for 3by2 matrix multiply with 2by1 matrix

--- src/test/java/com/thealgorithms/matrix/MatrixMultiplicationTest.java
+++ src/test/java/com/thealgorithms/matrix/MatrixMultiplicationTest.java
@@ -20,6 +20,17 @@ public class MatrixMultiplicationTest {
         assertMatrixEquals(expected, result);   // Because assertEquals can fails due to floating point precision issues, Therfore use assertMatrixEquals
     }
 
+    @Test
+    void testMultiply3by2and1(){
+        double[][] matrixA = {{1.0,2.0},{3.0,4.0},{5.0,6.0}};
+        double[][] matrixB = {{7.0},{8.0}};
+        double[][] expected = {{23.0}, {53.0}, {83.0}};
+
+        double[][] result = MatrixMultiplication.multiply(matrixA, matrixB);
+        assertMatrixEquals(expected, result);   // Because assertEquals can fails due to floating point precision issues, Therfore use assertMatrixEquals
+    }
+
+
     private void assertMatrixEquals(double[][] expected, double[][] actual) {
         assertEquals(expected.length, actual.length, "Row count mismatch");
         for (int i = 0; i < expected.length; i++) {
