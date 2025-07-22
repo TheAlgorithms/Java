fc7f16ac07c50fbeb0a1280e2800b265e682e450
alxkm <alexanderklmn@gmail.com>
2025-07-22 21:31:38 +0200

    style: fix style issues

--- src/test/java/com/thealgorithms/datastructures/stacks/NodeStackTest.java
+++ src/test/java/com/thealgorithms/datastructures/stacks/NodeStackTest.java
@@ -2,13 +2,13 @@ package com.thealgorithms.datastructures.stacks;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class NodeStackTest {
 
