d87dad630dcfabc70887c89023781b11b138482e
dinishajais <dinishajais3010@gmail.com>
2025-07-24 18:52:03 +0530

    Added formatting

--- src/main/java/com/thealgorithms/sorts/SelectionSort.java
+++ src/main/java/com/thealgorithms/sorts/SelectionSort.java
@@ -1,30 +1,31 @@
 package com.thealgorithms.sorts;
 
 public class SelectionSort implements SortAlgorithm {
-    /**
-     * Sorts an array of comparable elements in increasing order using the selection sort algorithm.
-     *
-     * @param array the array to be sorted
-     * @param <T> the class of array elements
-     * @return the sorted array
-     */
-    @Override
-    public <T extends Comparable<T>> T[] sort(T[] array) {
+	/**
+	 * Sorts an array of comparable elements in increasing order using the selection
+	 * sort algorithm.
+	 *
+	 * @param array the array to be sorted
+	 * @param <T>   the class of array elements
+	 * @return the sorted array
+	 */
+	@Override
+	public <T extends Comparable<T>> T[] sort(T[] array) {
 
-        for (int i = 0; i < array.length - 1; i++) {
-            final int minIndex = findIndexOfMin(array, i);
-            SortUtils.swap(array, i, minIndex);
-        }
-        return array;
-    }
+		for (int i = 0; i < array.length - 1; i++) {
+			final int minIndex = findIndexOfMin(array, i);
+			SortUtils.swap(array, i, minIndex);
+		}
+		return array;
+	}
 
-    private static <T extends Comparable<T>> int findIndexOfMin(T[] array, final int startIndex) {
-        int minIndex = startIndex;
-        for (int i = startIndex + 1; i < array.length; i++) {
-            if (SortUtils.less(array[i],array[minIndex])) {
-                minIndex = i;
-            }
-        }
-        return minIndex;
-    }
+	private static <T extends Comparable<T>> int findIndexOfMin(T[] array, final int startIndex) {
+		int minIndex = startIndex;
+		for (int i = startIndex + 1; i < array.length; i++) {
+			if (SortUtils.less(array[i], array[minIndex])) {
+				minIndex = i;
+			}
+		}
+		return minIndex;
+	}
 }
