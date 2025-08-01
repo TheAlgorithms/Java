28eafded6a7dec69425fcadc70db34365ae0df88
sainath <nettemsainath868@gmail.com>
2025-08-01 15:28:57 +0530

    Add BubbleSort algorithm

--- src/main/java/com/thealgorithms/sorting/BubbleSort.java
+++ src/main/java/com/thealgorithms/sorting/BubbleSort.java
@@ -1,16 +1,21 @@
 package com.thealgorithms.sorting;
-
 public class BubbleSort {
+
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
+        boolean swapped;
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    // swap
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swapped = true;
                 }
             }
+            if (!swapped) break;
         }
     }
 }
