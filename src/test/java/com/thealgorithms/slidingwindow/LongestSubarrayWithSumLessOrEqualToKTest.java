84164dfa5492e4a0ff2aa04c94df51e86010b8eb
ArthBachhuka123 <bachhukaarth@gmail.com>
2025-08-04 12:11:58 +0530

    added new method

--- src/test/java/com/thealgorithms/slidingwindow/LongestSubarrayWithSumLessOrEqualToKTest.java
+++ src/test/java/com/thealgorithms/slidingwindow/LongestSubarrayWithSumLessOrEqualToKTest.java
@@ -13,10 +13,11 @@ public class LongestSubarrayWithSumLessOrEqualToKTest {
      * Tests for the longest subarray with a sum less than or equal to k.
      */
     @Test
-    public void testLongestSubarrayWithSumLEK() {
+    public static void testLongestSubarrayWithSumLEK() {
         assertEquals(3, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[] {1, 2, 3, 4}, 6)); // {1, 2, 3}
         assertEquals(4, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[] {1, 2, 3, 4}, 10)); // {1, 2, 3, 4}
         assertEquals(2, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[] {5, 1, 2, 3}, 5)); // {5}
         assertEquals(0, LongestSubarrayWithSumLessOrEqualToK.longestSubarrayWithSumLEK(new int[] {1, 2, 3}, 0)); // No valid subarray
     }
+   
 }
