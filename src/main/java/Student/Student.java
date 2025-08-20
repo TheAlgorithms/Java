984adcc1cc3b3c8d9e1e5274f9d3fd8443877ee7
UnpolishedNoob <banikpritom321@gmail.com>
2025-08-20 20:51:08 +0600

    Added student class in a new Student package

---
+++ src/main/java/Student/Student.java
@@ -0,0 +1,37 @@
+package Student;
+
+public class Student {
+    private String name;
+    private int roll;
+    private String dept;
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return this.name;
+    }
+
+    // Setter for Roll
+    public void setRoll(int roll){
+        this.roll=roll;
+    }
+
+    // Getter for Roll
+    public int getRoll(){
+        return this.roll;
+    }
+
+    // Setter for Department
+    public void setDept(String dept){
+        this.dept=dept;
+    }
+
+    // Getter for Department
+    public String getDept(){
+        return this.dept;
+    }
+}
