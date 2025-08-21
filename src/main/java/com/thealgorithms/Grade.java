284d395bd036e32d1feb1f95a70a538cb0e50827
RajorshiDas <das2107060@stud.kuet.ac.bd>
2025-08-21 20:56:06 +0600

    Added Grade.java with grading system

---
+++ src/main/java/com/thealgorithms/Grade.java
@@ -0,0 +1,11 @@
+package com.thealgorithms.misc;
+
+public class Grade {
+    public String getGrade(int score) {
+        if (score >= 90) return "A";
+        if (score >= 80) return "B";
+        if (score >= 70) return "C";
+        if (score >= 60) return "D";
+        return "F";
+    }
+}
