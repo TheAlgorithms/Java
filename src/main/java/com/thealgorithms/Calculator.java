c0918e6db5cf7e3b76da17a5e508cae2c168599c
RajorshiDas <das2107060@stud.kuet.ac.bd>
2025-08-21 20:45:55 +0600

    Added Calculator.java in master with divide() returning a string

--- src/main/java/com/thealgorithms/Calculator.java
+++ src/main/java/com/thealgorithms/Calculator.java
@@ -4,5 +4,8 @@ public class Calculator {
     public int add(int a, int b) { return a + b; }
     public int subtract(int a, int b) { return a - b; }
     public int multiply(int a, int b) { return a * b; }
-    public double divide(int a, int b) { return (b != 0) ? (double)a / b : 0; }
+    public double divide(int a, int b) {
+    if (b == 0) throw new ArithmeticException("Divide by zero!"); 
+    return (double) a / b;
+}
 }
