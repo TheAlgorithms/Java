338066e42ddcaad478e388e4309397eb9b267d34
Mustafiz <mustafizur.codes@gmail.com>
2025-08-21 19:17:55 +0600

    Add Student.java withgetters, setters, and  constructors

---
+++ src/main/java/com/thealgorithms/student.java
@@ -0,0 +1,37 @@
+package com.thealgorithms;
+
+public class student {
+    private String name;
+    private int age;
+    private String grade;
+
+    public student(String name, int age, String grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
