40fb84c3befdc24b3a39c0bdc0af27bf65f94b35
Esha Saha <sahaesha66@gmail.com>
2025-08-21 19:51:09 +0600

    person class with constructor, getter,setter added

---
+++ lab2/src/Person.java
@@ -0,0 +1,19 @@
+package lab2;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() { }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+}
