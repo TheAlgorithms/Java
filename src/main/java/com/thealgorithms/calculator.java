8cee71ab54014aec4ce1a380f24faff3da554e0e
Mustafiz <mustafizur.codes@gmail.com>
2025-08-21 19:22:12 +0600

    Add Calculator.java with arithmetic methods

---
+++ src/main/java/com/thealgorithms/calculator.java
@@ -0,0 +1,23 @@
+package com.thealgorithms;
+
+public class calculator {
+     public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed");
+        }
+        return a / b;
+    }
+    
+}
