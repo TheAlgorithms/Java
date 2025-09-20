48b8cc88e6f62fd05579e8740e641d576e8e05b7
Akash Biswas <akash41bt@gmail.com>
2025-09-20 14:59:41 +0600

    add cal.java file

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
+    }   
+}
