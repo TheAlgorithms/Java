06202a656a3c3c831bb12af6eaf0413becced06e
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-09-29 16:05:30 +0530

    Update MinimumWindowSubstringTest.java

--- src/test/java/com/thealgorithms/slidingwindow/MinimumWindowSubstringTest.java
+++ src/test/java/com/thealgorithms/slidingwindow/MinimumWindowSubstringTest.java
@@ -5,19 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 /**
- * Unit tests for the LongestSubstringWithoutRepeatingCharacters class.
+ * Unit tests for the MinimumWindowSubstring class.
  *
  * @author  (https://github.com/Chiefpatwal)
  */
 public class MinimumWindowSubstringTest {
 
     /**
-     * Tests for the longest subarray with a sum less than or equal to k.
+     * Tests for MinimumWindowSubstring.minWindow.
      */
     @Test
     public void testMinimumWindowSubstring() {
-        assertEquals(3, MinimumWindowSubstring.minWindow("ADOBECODEBANC","ABC"); // "BANC"
-        assertEquals(4, MinimumWindowSubstring.minWindow("a","a"); // "a"
-        assertEquals(2, MinimumWindowSubstring.minWindow("a","aa" // ""
+        assertEquals(4, MinimumWindowSubstring.minWindow("ADOBECODEBANC", "ABC")); // "BANC"
+        assertEquals(1, MinimumWindowSubstring.minWindow("a", "a")); // "a"
+        assertEquals(0, MinimumWindowSubstring.minWindow("a", "aa")); // ""
     }
 }
