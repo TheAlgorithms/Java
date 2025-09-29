3a553e83ef2dd98dd47b2b8e31e85e247c80e67c
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-09-29 18:24:19 +0530

    correct MinimumWindowSubstringTest.java

--- src/test/java/com/thealgorithms/slidingwindow/MinimumWindowSubstringTest.java
+++ src/test/java/com/thealgorithms/slidingwindow/MinimumWindowSubstringTest.java
@@ -16,8 +16,8 @@ public class MinimumWindowSubstringTest {
      */
     @Test
     public void testMinimumWindowSubstring() {
-        assertEquals(4, MinimumWindowSubstring.minWindow("ADOBECODEBANC", "ABC")); // "BANC"
-        assertEquals(1, MinimumWindowSubstring.minWindow("a", "a")); // "a"
-        assertEquals(0, MinimumWindowSubstring.minWindow("a", "aa")); // ""
+        assertEquals("BANC", MinimumWindowSubstring.minWindow("ADOBECODEBANC", "ABC")); // "BANC"
+        assertEquals("a", MinimumWindowSubstring.minWindow("a", "a")); // "a"
+        assertEquals("", MinimumWindowSubstring.minWindow("a", "aa")); // ""
     }
 }
