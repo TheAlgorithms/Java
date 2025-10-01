914bbee0858bbe4074940062031c6929bf0b4204
Sivasuthan Sukumar <153247727+Sivasuthan9@users.noreply.github.com>
2025-10-01 23:08:54 +0530

    Remove unnecessary blank line in BronKerboschTest

--- src/test/java/com/thealgorithms/graph/BronKerboschTest.java
+++ src/test/java/com/thealgorithms/graph/BronKerboschTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
