16204bef75b774847c88f4c5dc9977b5f9f8283b
Ruturaj Jadhav <ruturajjadhav122@gmail.com>
2025-10-01 19:45:55 +0530

    Update HappyNumberTest.java

--- src/test/java/com/thealgorithms/maths/HappyNumberTest.java
+++ src/test/java/com/thealgorithms/maths/HappyNumberTest.java
@@ -2,6 +2,7 @@ package com.thealgorithms.maths;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class HappyNumberTest {
