c0ff699a53a8e716c475765b9282d804712f69ff
Suraj Singh Chauhan <152488087+Suraj-1309@users.noreply.github.com>
2025-10-01 11:21:41 +0530

    Update MatrixRowPermutationTest.java

--- src/test/java/com/thealgorithms/matrix/MatrixRowPermutationTest.java
+++ src/test/java/com/thealgorithms/matrix/MatrixRowPermutationTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class MatrixRowPermutationTest {
