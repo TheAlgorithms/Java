c1a497d410d2cebba338dd166e0b69d64711ed95
the-yash-rajput <yash.rajput@turtlemint.com>
2025-10-03 13:28:44 +0530

    checkstyle fix

--- src/main/java/com/thealgorithms/maths/PiApproximation.java
+++ src/main/java/com/thealgorithms/maths/PiApproximation.java
@@ -28,7 +28,7 @@ public final class PiApproximation {
         double x;
         double y;
 
-        public Point(double x, double y) {
+        Point(double x, double y) {
             this.x = x;
             this.y = y;
         }
