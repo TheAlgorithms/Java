ff18b9db33f46d2a21611801bca6210e748e5d3a
Krishnan M <2318467@cognizant.com>
2025-10-04 18:09:24 +0530

    updated pretty Display and added BSTRecursiveGenericTest.java

--- src/test/java/com/thealgorithms/datastructures/trees/BSTRecursiveGenericTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/BSTRecursiveGenericTest.java
@@ -1,9 +1,14 @@
 package com.thealgorithms.datastructures.trees;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * Unit tests for BSTRecursiveGeneric class.
