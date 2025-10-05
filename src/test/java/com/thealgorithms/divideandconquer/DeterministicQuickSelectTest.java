dff12ad3f5c8aabc965963552cbd1821ee994e22
LUKMANul <lukmanulhakeem.kmagt3@gmail.com>
2025-10-06 01:24:40 +0530

    issue fixed.

--- src/test/java/com/thealgorithms/divideandconquer/DeterministicQuickSelectTest.java
+++ src/test/java/com/thealgorithms/divideandconquer/DeterministicQuickSelectTest.java
@@ -1,9 +1,9 @@
 package com.thealgorithms.divideandconquer;
 
-import org.junit.jupiter.api.Test; 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 public final class DeterministicQuickSelectTest {
 
