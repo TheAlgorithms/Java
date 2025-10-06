cff01471d7047d157ee2e33337133927d283d868
prerita <preritasaini1@gmail.com>
2025-10-06 18:42:39 +0530

    Fixes Error

--- src/main/java/com/thealgorithms/recursion/LongestIncreasingSubsequence.java
+++ src/main/java/com/thealgorithms/recursion/LongestIncreasingSubsequence.java
@@ -4,16 +4,15 @@ public class LongestIncreasingSubsequence {
 
     public static int solve(int[] nums, int prev, int idx) {
         // base case
-        if (idx == nums.length) return 0;
+        if (idx == nums.length) {
+            return 0;
+        }
 
         // process
-        // 1. include 
-        // 2. exclude
         int include = 0;
         if (prev == -1 || nums[idx] > nums[prev]) {
             include = 1 + solve(nums, idx, idx + 1);
         }
-
         int exclude = solve(nums, prev, idx + 1);
         return Math.max(include, exclude);
     }
@@ -24,7 +23,7 @@ public class LongestIncreasingSubsequence {
     }
 
     public static void main(String[] args) {
-        int[] nums = {10, 22, 9, 33, 21, 50, 41, 60};
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
         LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
         System.out.println("Length of LIS: " + lis.lengthOfLIS(nums));
     }
