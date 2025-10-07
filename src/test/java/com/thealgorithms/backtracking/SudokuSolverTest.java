f9f8ea30109952d959d30923b9fa6edad74c3b02
khalidx3 <khalidhasanx3.com>
2025-10-07 23:53:02 +0530

    Add SudokuSolverTt test file

---
+++ src/test/java/com/thealgorithms/backtracking/SudokuSolverTest.java
@@ -0,0 +1,35 @@
+package test.java.com.thealgorithms.backtracking;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SudokuSolverTest {
+
+      @Test
+      public void testSudokuSolverSolvesValidBoard() {
+            int[][] sudoku = {
+                        { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
+                        { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+                        { 0, 8, 7, 0, 0, 0, 0, 3, 1 },
+                        { 0, 0, 3, 0, 1, 0, 0, 8, 0 },
+                        { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+                        { 0, 5, 0, 0, 9, 0, 6, 0, 0 },
+                        { 1, 3, 0, 0, 0, 0, 2, 5, 0 },
+                        { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+                        { 0, 0, 5, 2, 0, 6, 3, 0, 0 }
+            };
+
+            boolean solved = SudokuSolver.sudokuSolver(sudoku, 0, 0);
+            assertTrue(solved, "SudokuSolver should solve a valid board");
+      }
+
+      @Test
+      public void testSudokuSolverRejectsInvalidBoard() {
+            int[][] sudoku = new int[9][9];
+            sudoku[0][0] = 5;
+            sudoku[0][1] = 5; // invalid duplicate in row
+
+            boolean solved = SudokuSolver.sudokuSolver(sudoku, 0, 0);
+            assertFalse(solved, "SudokuSolver should not solve an invalid board");
+      }
+}
