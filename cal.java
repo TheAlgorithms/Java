b8a8a5b248b490afb59ee567b9a6e4f07eb97564
Akritiiiiiiii <2405786@kiit.ac.in>
2025-10-08 13:11:19 +0530

    Create cal.java

---
+++ cal.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("=== Simple Calculator ===");
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Choose an operation: +  -  *  /");
+        char operator = sc.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else {
+                    System.out.println("Error! Division by zero is not allowed.");
+                    sc.close();
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                sc.close();
+                return;
+        }
+
+        System.out.println("Result: " + result);
+        sc.close();
+    }
+}
