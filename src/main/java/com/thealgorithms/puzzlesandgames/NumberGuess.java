6c9d83f53b3349d9bab0dc9c6232fd15d32e06bd
jenyyy4 <anuskaroy0410@gmail.com>
2025-10-08 16:54:37 +0530

    added url of algos and formatted

--- src/main/java/com/thealgorithms/puzzlesandgames/NumberGuess.java
+++ src/main/java/com/thealgorithms/puzzlesandgames/NumberGuess.java
@@ -1,10 +1,16 @@
+/**
+ * Simple Number Guessing Game.
+ * The player guesses a random number between 1 and 100.
+ * Reference: https://en.wikipedia.org/wiki/Guess_the_number
+ */
+
 package com.thealgorithms.puzzlesandgames;
 import java.util.Scanner;
 
 public class NumberGuess {
     public static void playGame() {
         Scanner sc = new Scanner(System.in);
-        int number = (int)(Math.random() * 100) + 1;
+        int number = (int) (Math.random() * 100) + 1;
         int guess = 0, tries = 0;
 
         System.out.println("🎯 Welcome to the Number Guessing Game!");
@@ -12,7 +18,7 @@ public class NumberGuess {
 
         while (true) {
             System.out.print("Enter your guess: ");
-            
+
             if (!sc.hasNextInt()) {
                 System.out.println("Please enter a valid number.");
                 sc.next();
