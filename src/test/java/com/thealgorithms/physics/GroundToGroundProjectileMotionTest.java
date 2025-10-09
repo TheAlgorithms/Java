5056384d1586da08355a5fc97869852ba3af4c0c
the-yash-rajput <yash.rajput@turtlemint.com>
2025-10-09 12:35:56 +0530

    Fix checkstyle issues.

--- src/test/java/com/thealgorithms/physics/GroundToGroundProjectileMotionTest.java
+++ src/test/java/com/thealgorithms/physics/GroundToGroundProjectileMotionTest.java
@@ -1,6 +1,7 @@
 package com.thealgorithms.physics;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
