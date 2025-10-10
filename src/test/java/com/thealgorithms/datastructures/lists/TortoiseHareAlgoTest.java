41214b4be27943a463a70b65af59f0140c39c2cd
TejasSingh022 <157150137+TejasSingh022@users.noreply.github.com>
2025-10-10 15:34:43 +0530

    Update TortoiseHareAlgoTest.java
    Replace .* import with specific imports

--- src/test/java/com/thealgorithms/datastructures/lists/TortoiseHareAlgoTest.java
+++ src/test/java/com/thealgorithms/datastructures/lists/TortoiseHareAlgoTest.java
@@ -1,6 +1,7 @@
 package com.thealgorithms.datastructures.lists;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
