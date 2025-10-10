fc1eaff197e4ee420ffa9e033525b656a1d6f28c
TejasSingh022 <157150137+TejasSingh022@users.noreply.github.com>
2025-10-10 15:14:31 +0530

    Update TortoiseHareAlgoTest.java
    Fixed line formatting build error

--- src/test/java/com/thealgorithms/datastructures/lists/TortoiseHareAlgoTest.java
+++ src/test/java/com/thealgorithms/datastructures/lists/TortoiseHareAlgoTest.java
@@ -1,7 +1,7 @@
 package com.thealgorithms.datastructures.lists;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class TortoiseHareAlgoTest {
 
