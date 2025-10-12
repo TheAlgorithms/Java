3fc73b60daffc233c7df4acffd853e55424d5386
konduri-lakshmi-prasanna <konduriprasanna22@gmail.com>
2025-10-12 21:58:56 +0530

    fix: move test file to correct directory and format with clang-format

--- src/test/java/com/thealgorithms/strings/StringRotationTest.java
+++ src/test/java/com/thealgorithms/strings/StringRotationTest.java
@@ -1,6 +1,7 @@
-package com.thealgorithms.strings;
+package com.thealgorithms;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class StringRotationTest {
