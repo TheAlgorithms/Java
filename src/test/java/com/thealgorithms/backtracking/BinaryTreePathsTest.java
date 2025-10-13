d927d0024fd9dacd14c70011d4428ed0ab2012a3
konduri-lakshmi-prasanna <konduriprasanna22@gmail.com>
2025-10-13 23:55:47 +0530

    Fix: reorder imports as per clang-format rules

--- src/test/java/com/thealgorithms/backtracking/BinaryTreePathsTest.java
+++ src/test/java/com/thealgorithms/backtracking/BinaryTreePathsTest.java
@@ -1,10 +1,10 @@
 package com.thealgorithms.backtracking;
 
-import org.junit.jupiter.api.Test;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 public class BinaryTreePathsTest {
 
