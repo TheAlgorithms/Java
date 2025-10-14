5e7b8d9eea8548735ea49363350ea91c11bebf93
Nikhilesh Satish <nik@Nikhileshs-Macbook-Air.local>
2025-10-14 19:16:24 +0530

    Add Tower Of Hanoi recursive implementation

--- src/main/java/com/thealgorithms/recursion/TowerOfHanoi.java
+++ src/main/java/com/thealgorithms/recursion/TowerOfHanoi.java
@@ -1,6 +1,7 @@
 package com.thealgorithms.recursion;
 
 public class TowerOfHanoi {
+
     public void solveHanoi(int n,char src,char dest,char aux)
     {
         if(n==1)
@@ -11,5 +12,7 @@ public class TowerOfHanoi {
         solveHanoi(n-1,src,aux,dest);
         System.out.println("Move disk "+n+" from "+src+" to "+dest);
         solveHanoi(n-1,aux,dest,src);
+
     }
+    
 }
