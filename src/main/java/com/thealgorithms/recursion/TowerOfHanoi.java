4eab717c4ea9462e41e4dea0cf001a3215696ae3
Nikhilesh Satish <nik@Nikhileshs-Macbook-Air.local>
2025-10-14 19:49:40 +0530

    Added Tower of Hanoi code using recursion and a sample test

--- src/main/java/com/thealgorithms/recursion/TowerOfHanoi.java
+++ src/main/java/com/thealgorithms/recursion/TowerOfHanoi.java
@@ -4,17 +4,13 @@ package com.thealgorithms.recursion;
 
 public class TowerOfHanoi {
 
-    public void solveHanoi(int n,char src,char dest,char aux)
-    {
-        if(n==1)
-        {
-            System.out.println("Move disk 1 from "+src+" to "+dest);
+    public static void solveHanoi(int n, char src, char dest, char aux) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + src + " to " + dest);
             return;
         }
-        solveHanoi(n-1,src,aux,dest);
-        System.out.println("Move disk "+n+" from "+src+" to "+dest);
-        solveHanoi(n-1,aux,dest,src);
-
+        solveHanoi(n - 1, src, aux, dest);
+        System.out.println("Move disk " + n + " from " + src + " to " + dest);
+        solveHanoi(n - 1, aux, dest, src);
     }
-    
 }
