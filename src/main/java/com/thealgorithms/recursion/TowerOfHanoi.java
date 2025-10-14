e7e52633eab2efa02b63a3aa937db6de70f12971
Nikhilesh Satish <nik@Nikhileshs-Macbook-Air.local>
2025-10-14 22:16:02 +0530

    Solution for Tower of Hanoi problem using recursion

--- src/main/java/com/thealgorithms/recursion/TowerOfHanoi.java
+++ src/main/java/com/thealgorithms/recursion/TowerOfHanoi.java
@@ -3,7 +3,8 @@ package com.thealgorithms.recursion;
 /*Tower of Hanoi Problem on Wikipedia - https://en.wikipedia.org/wiki/Tower_of_Hanoi */
 
 public final class TowerOfHanoi {
-    private TowerOfHanoi(){}
+    private TowerOfHanoi() {
+    }
     public static void solveHanoi(int n, char src, char dest, char aux) {
         if (n == 1) {
             System.out.println("Move disk 1 from " + src + " to " + dest);
