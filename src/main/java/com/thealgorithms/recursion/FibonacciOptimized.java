198277fc585c997fa5f1dcf6a7fe0c4f7fa78eb8
Prince Kotadia <kotadiaprince@gmail.com>
2025-10-17 23:27:58 +0530

    chaged Fibonacci Optimized

--- src/main/java/com/thealgorithms/recursion/FibonacciOptimized.java
+++ src/main/java/com/thealgorithms/recursion/FibonacciOptimized.java
@@ -7,17 +7,18 @@ package com.thealgorithms.recursion;
        FIBONACCI 0 1 1 2 3 5 8 13 21 34 55 ...
 */
 
-/*T his is optimized solution for fibonacci series. It computes fibonacci series in linear(O(n)) time complexity */
-
+/* This is optimized solution for fibonacci series. It computes fibonacci series in linear (O(n)) time complexity */
 public final class FibonacciOptimized {
-    static int fibonacci(int limit, int num1, int num2){
-        if(limit==1){
+
+    private FibonacciOptimized() {
+        // Prevent instantiation
+    }
+
+    static int fibonacci(int limit, int num1, int num2) {
+        if (limit == 1) {
             return num1;
-        }
-        else{
-            return fibonacci(limit-1, num2, num1+num2);
+        } else {
+            return fibonacci(limit - 1, num2, num1 + num2);
         }
     }
-    
 }
-
