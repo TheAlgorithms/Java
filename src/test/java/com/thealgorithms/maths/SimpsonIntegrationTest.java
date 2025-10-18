38e811f88eaf5f05224e4b88e8cd83d03f2c7a3a
labe2301 <labe2301@student.miun.se>
2025-10-19 00:51:07 +0200

    [feature] Increase line and mutation coverage for SimpsonIntegration

---
+++ src/test/java/com/thealgorithms/maths/SimpsonIntegrationTest.java
@@ -0,0 +1,61 @@
+package com.thealgorithms.maths;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpsonIntegrationTest {
+    private final SimpsonIntegration simpson = new SimpsonIntegration();
+
+    @Test
+    void shouldCalculateCorrectFunction() {
+        assertAll(
+            () -> assertEquals(-0.24893534183931973, simpson.f(3)),
+            () -> assertEquals(0.0, simpson.f(2)),
+            () -> assertEquals(4.0, simpson.f(0)),
+            () -> assertEquals(8.154845485377136, simpson.f(-1))
+        );
+    }
+
+    @Test
+    void shouldCalculateCorrectMethod() {
+        int n = 4;
+        double a = -1.0;
+        double b = 1.0;
+        double h = (b - a)/n;
+
+        double result = simpson.simpsonsMethod(n, h, a);
+        double expected = 8.51454379418048;
+        
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void shouldIncreaseAccuracy() {
+        int n1 = 10;
+        int n2 = 20;
+        double a = 1.0;
+        double b = 3.0;
+        double h1 = (b - a) / n1;
+        double h2 = (b - a) / n2;
+
+        double result1 = simpson.simpsonsMethod(n1, h1, a);
+        double result2 = simpson.simpsonsMethod(n2, h2, a);
+
+        assertTrue(Math.abs(result2 - result1) < Math.abs(result1));
+    }
+
+    @Test
+    void shouldThrow() {
+        int n = 3;
+        double a = -1.0;
+        double b = 1.0;
+        double h = (b - a) / n;
+
+        double result = simpson.simpsonsMethod(n, h, a);
+
+        assertTrue(Double.isFinite(result));
+    }
+}
