e911422046a5bd2d84b7e3b299fb212811750bb0
Joel Lansgren <lansgren84@gmail.com>
2025-10-18 20:43:32 +0300

    Added test to LSA for guarding against the parent and depth creation

--- src/test/java/com/thealgorithms/datastructures/trees/LowestCommonAncestorTest.java
+++ src/test/java/com/thealgorithms/datastructures/trees/LowestCommonAncestorTest.java
@@ -1,7 +1,8 @@
 package com.thealgorithms.datastructures.trees;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,6 +10,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LowestCommonAncestorTest {
     /**
@@ -25,28 +28,30 @@ public class LowestCommonAncestorTest {
      *        9   8
      * <pre>
      */
-    private final int[] treeInput = new int[] {0,1,0,2,1,5,5,6,2,4,2,3,3,7,7,9,7,8};
+    private final int[] treeInput = new int[] {0, 1, 0, 2, 1, 5, 5, 6, 2, 4, 2, 3, 3, 7, 7, 9, 7, 8};
+    private final int arrayLength = 10;
+    private final int[] parent = new int[arrayLength];
+    private final int[] depth = new int[arrayLength];
+    private final ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-    @ParameterizedTest
-    @CsvSource({"9,4,2", "5,6,5", "5,4,0", "3,8,3", "6,3,0", "3,3,3"})
-    void shouldReturnCorrectLCA(int v1, int v2, int expectedParent) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int actualParent;
-        int arrayLength = 10;
 
+    @BeforeEach
+    void setup(){
         for (int i = 0; i < arrayLength; i++) adj.add(new ArrayList<>());
 
-        for (int i = 0; i < treeInput.length - 1; i +=2) {
+        for (int i = 0; i < treeInput.length - 1; i += 2) {
             int to = treeInput[i];
             int from = treeInput[i+1];
 
             adj.get(to).add(from);
             adj.get(from).add(to);
         }
+    }
 
-        int[] parent = new int[arrayLength];
-        int[] depth = new int[arrayLength];
-
+    @ParameterizedTest
+    @CsvSource({"9,4,2", "5,6,5", "5,4,0", "3,8,3", "6,3,0", "3,3,3"})
+    @DisplayName("Should return correct common ancestor for any two nodes in the tree")
+    void shouldReturnCorrectLCA(int v1, int v2, int expectedParent) {
         try {
             Method dfs = LCA.class.getDeclaredMethod("dfs", ArrayList.class, int.class, int.class, int[].class, int[].class);
             Method getLCA = LCA.class.getDeclaredMethod("getLCA", int.class, int.class, int[].class, int[].class);
@@ -55,11 +60,37 @@ public class LowestCommonAncestorTest {
 
             dfs.invoke(null, adj, 0, -1, parent, depth);
 
-            actualParent = (int)getLCA.invoke(null, v1, v2, depth, parent);
+            assertEquals(expectedParent, getLCA.invoke(null, v1, v2, depth, parent));
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void shouldReturnCorrectDepthsForArray() {
+        try {
+            Method dfs = LCA.class.getDeclaredMethod("dfs", ArrayList.class, int.class, int.class, int[].class, int[].class);
+            dfs.setAccessible(true);
+
+            dfs.invoke(null, adj, 0, -1, parent, depth);
         } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
 
-        assertEquals(expectedParent, actualParent);
+        assertArrayEquals(new int[]{0, 1, 1, 2, 2, 2, 3, 3, 4, 4}, depth);
+    }
+
+    @Test
+    void shouldReturnCorrectParentsForArray() {
+        try {
+            Method dfs = LCA.class.getDeclaredMethod("dfs", ArrayList.class, int.class, int.class, int[].class, int[].class);
+            dfs.setAccessible(true);
+
+            dfs.invoke(null, adj, 0, -1, parent, depth);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertArrayEquals(new int[]{0, 0, 0, 2, 2, 1, 5, 3, 7, 7}, parent);
     }
 }
