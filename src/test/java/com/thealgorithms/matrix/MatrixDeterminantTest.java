1e6f31c8c6ce88aaf8d13fc03f235940297a4e34
Rony-2004 <mowazzemahmed19@gmail.com>
2025-10-26 15:10:08 +0530

    refactor(matrix): Clean up code and improve formatting in MatrixDeterminant and its tests

--- src/test/java/com/thealgorithms/matrix/MatrixDeterminantTest.java
+++ src/test/java/com/thealgorithms/matrix/MatrixDeterminantTest.java
@@ -1,8 +1,9 @@
 package com.thealgorithms.matrix;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 class MatrixDeterminantTest {
 
