bdc9369ddd1c353e8a65efa95371c0f98bf2e247
Joel Lansgren <lansgren84@gmail.com>
2025-10-27 21:49:46 +0200

    Made style changes
    According to Clang's fugly ass
    suggestions.

--- src/test/java/com/thealgorithms/maths/RomanNumeralUtilTest.java
+++ src/test/java/com/thealgorithms/maths/RomanNumeralUtilTest.java
@@ -9,25 +9,25 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class RomanNumeralUtilTest {
-		@ParameterizedTest()
-		@CsvSource({"2000,MM", "1,I", "2,II", "1003,MIII", "1004,MIV"})
-		void minimumMaximumTest(int testInt, String word) {
-            System.out.println(word + ": " + testInt);
-            assertEquals(RomanNumeralUtil.generate(testInt), word);
-		}
+    @ParameterizedTest()
+    @CsvSource({"2000,MM", "1,I", "2,II", "1003,MIII", "1004,MIV"})
+    void minimumMaximumTest(int testInt, String word) {
+        System.out.println(word + ": " + testInt);
+        assertEquals(RomanNumeralUtil.generate(testInt), word);
+    }
 
-		@Test
-		void calcSomeNumerals() {
-            assertThrows(IllegalArgumentException.class, () -> RomanNumeralUtil.generate(0));
-            assertThrows(IllegalArgumentException.class, () -> RomanNumeralUtil.generate(6000));
-		}
+    @Test
+    void calcSomeNumerals() {
+        assertThrows(IllegalArgumentException.class, () -> RomanNumeralUtil.generate(0));
+        assertThrows(IllegalArgumentException.class, () -> RomanNumeralUtil.generate(6000));
+    }
 
-		@Test
-		void shouldNotThrowOnBounds() {
-			int min = 1;
-			int max = 5999;
+    @Test
+    void shouldNotThrowOnBounds() {
+        int min = 1;
+        int max = 5999;
 
-			assertDoesNotThrow(() -> RomanNumeralUtil.generate(min), "Should not throw exception on " + min);
-			assertDoesNotThrow(() -> RomanNumeralUtil.generate(max), "Should not throw exception on " + max);
-		}
+        assertDoesNotThrow(() -> RomanNumeralUtil.generate(min), "Should not throw exception on " + min);
+        assertDoesNotThrow(() -> RomanNumeralUtil.generate(max), "Should not throw exception on " + max);
+    }
 }
