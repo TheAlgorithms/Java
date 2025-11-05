e33e9ca09286f8ac1d9a8310ee5aa0417ec0d467
AlexJauregui02 <alejandro.jauregui@encora.com>
2025-11-05 09:32:28 -0700

    renamed the test file

--- src/test/java/com/thealgorithms/conversions/TemperatureConverterTest.java
+++ src/test/java/com/thealgorithms/conversions/TemperatureConverterTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class TemperatureCoverterTest {
+public class TemperatureConverterTest {
 
     @Test
     public void testValidCelsiusToFahrenheit() {
