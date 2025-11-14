0f88632ebae6fe7d30a5624da3bc55c6f7dd3b18
guillermolara01 <guillermo.gonzalez@encora.com>
2025-11-14 12:06:51 -0600

    fix: removes the blank line in imports, style correction

--- src/test/java/com/thealgorithms/greedyalgorithms/KruskalAlgorithmTest.java
+++ src/test/java/com/thealgorithms/greedyalgorithms/KruskalAlgorithmTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
