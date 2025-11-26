935d9e8944a01bb2e6d878f78707285dbe7f03cd
utkarshishu2627@gmail.com <utkarshishu2627@gmail.com>
2025-11-26 07:27:07 +0530

    Add Z-Algorithm (string pattern matching) with tests

--- src/main/java/com/thealgorithms/strings/ZAlgorithm.java
+++ src/main/java/com/thealgorithms/strings/ZAlgorithm.java
@@ -1,3 +1,7 @@
+/*
+ * https://en.wikipedia.org/wiki/Z-algorithm
+ */
+
 package com.thealgorithms.strings;
 
 public class ZAlgorithm {
@@ -7,8 +11,10 @@ public class ZAlgorithm {
         int[] z = new int[n];
         int l = 0, r = 0;
         for (int i = 1; i < n; i++) {
-            if (i <= r) z[i] = Math.min(r - i + 1, z[i - l]);
-            while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i])) z[i]++;
+            if (i <= r)
+                z[i] = Math.min(r - i + 1, z[i - l]);
+            while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i]))
+                z[i]++;
             if (i + z[i] - 1 > r) {
                 l = i;
                 r = i + z[i] - 1;
@@ -22,7 +28,8 @@ public class ZAlgorithm {
         int[] z = zFunction(s);
         int p = pattern.length();
         for (int i = 0; i < z.length; i++) {
-            if (z[i] == p) return i - p - 1;
+            if (z[i] == p)
+                return i - p - 1;
         }
         return -1;
     }
