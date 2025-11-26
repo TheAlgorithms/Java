e02ca0430e56c33b515ddf409f04ba4914d5a514
Jayana Anjani Pavan Vardhan Naidu <vardhannaidu17@gmail.com>
2025-11-26 11:50:34 +0530

    Refactor JUnit import statements in BellmanFordTest
    Refactor import statements for JUnit assertions.

--- src/test/java/com/thealgorithms/graph/BellmanFordTest.java
+++ src/test/java/com/thealgorithms/graph/BellmanFordTest.java
@@ -1,7 +1,10 @@
 package com.thealgorithms.graph;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
