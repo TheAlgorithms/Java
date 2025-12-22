0bc3398826b09ee035aa028f11700c008e9d9dea
Koushik Sai <nyamathabadkoushik@gmail.com>
2025-12-22 11:54:03 +0530

    Apply clang-format to factorial recursion tests

--- src/test/java/com/thealgorithms/recursion/FactorialRecursionTest.java
+++ src/test/java/com/thealgorithms/recursion/FactorialRecursionTest.java
@@ -1,9 +1,9 @@
 package com.thealgorithms.recursion;
 
-import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
