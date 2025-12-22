dde4d4a323e0ae4f7a576653bc9513381b0fe924
Koushik Sai <nyamathabadkoushik@gmail.com>
2025-12-22 11:47:16 +0530

    Move recursive factorial to recursion package and remove duplicate

--- src/test/java/com/thealgorithms/recursion/FactorialRecursionTest.java
+++ src/test/java/com/thealgorithms/recursion/FactorialRecursionTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import com.thealgorithms.recursion.FactorialRecursion;
 
 public class FactorialRecursionTest {
     @ParameterizedTest
