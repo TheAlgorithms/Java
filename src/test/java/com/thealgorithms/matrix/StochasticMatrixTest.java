5c0454b2013adb44bd3a2f23aa2d97906d7e6773
Daniel Sánchez Núñez <dsannue@upv.edu.es>
2025-12-28 00:49:15 +0100

    Clean up import statements in StochasticMatrixTest
    Removed unused import statements for cleaner code.

--- src/test/java/com/thealgorithms/matrix/StochasticMatrixTest.java
+++ src/test/java/com/thealgorithms/matrix/StochasticMatrixTest.java
@@ -1,9 +1,7 @@
 package com.thealgorithms.matrix;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
