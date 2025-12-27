e085cfc0a30fb6b3d6664e499cb78e68566f8bc6
Daniel Sánchez Núñez <dsannue@upv.edu.es>
2025-12-28 00:54:06 +0100

    Remove unused import assertThrows from StochasticMatrixTest
    Removed unused import for assertThrows.

--- src/test/java/com/thealgorithms/matrix/StochasticMatrixTest.java
+++ src/test/java/com/thealgorithms/matrix/StochasticMatrixTest.java
@@ -1,7 +1,6 @@
 package com.thealgorithms.matrix;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
