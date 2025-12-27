2a421ac865a6677e18b75b03243c5d401b80806f
Daniel Sánchez Núñez <dsannue@upv.edu.es>
2025-12-28 00:37:54 +0100

    Fix formatting of matrix initialization in tests

--- src/test/java/com/thealgorithms/matrix/StochasticMatrixTest.java
+++ src/test/java/com/thealgorithms/matrix/StochasticMatrixTest.java
@@ -7,20 +7,20 @@ class StochasticMatrixTest {
 
     @Test
     void testRowStochasticMatrix() {
-        double[][] matrix = {{0.2, 0.5, 0.3},{0.1, 0.6, 0.3}};
+        double[][] matrix = {{0.2, 0.5, 0.3}, {0.1, 0.6, 0.3}};
         assertTrue(StochasticMatrix.isRowStochastic(matrix));
         assertFalse(StochasticMatrix.isColumnStochastic(matrix));
     }
 
     @Test
     void testColumnStochasticMatrix() {
-        double[][] matrix = {{0.4, 0.2},{0.6, 0.8}};
+        double[][] matrix = {{0.4, 0.2}, {0.6, 0.8}};
         assertTrue(StochasticMatrix.isColumnStochastic(matrix));
     }
 
     @Test
     void testInvalidMatrix() {
-        double[][] matrix = {{0.5, -0.5},{0.5, 1.5}};
+        double[][] matrix = {{0.5, -0.5}, {0.5, 1.5}};
         assertFalse(StochasticMatrix.isRowStochastic(matrix));
     }
 }
