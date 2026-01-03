159a709df963be8d210600dd7b07ab538b81ad7d
Swati Kumari <swatijha1803@gmail.com>
2026-01-03 10:15:14 +0530

    Reorder import statement for assertEquals

--- src/test/java/com/thealgorithms/maths/GCDUsingEuclidTest.java
+++ src/test/java/com/thealgorithms/maths/GCDUsingEuclidTest.java
@@ -1,7 +1,8 @@
 package com.thealgorithms.maths;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GCDUsingEuclidTest {
 
