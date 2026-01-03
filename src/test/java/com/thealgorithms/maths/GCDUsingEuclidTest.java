132dbd626a960f064316bc459f092babdf7cb053
Swati Kumari <swatijha1803@gmail.com>
2026-01-03 10:45:47 +0530

    Update GCDUsingEuclidTest.java

--- src/test/java/com/thealgorithms/maths/GCDUsingEuclidTest.java
+++ src/test/java/com/thealgorithms/maths/GCDUsingEuclidTest.java
@@ -1,10 +1,10 @@
 package com.thealgorithms.maths;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class GCDUsingEuclidTest {
+import org.junit.jupiter.api.Test;
+
+public class GCDUsingEuclidTest {
 
     @Test
     void testPositiveNumbers() {
