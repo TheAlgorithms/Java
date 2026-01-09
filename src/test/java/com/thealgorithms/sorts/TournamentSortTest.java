2c340c82ad8afb5f85dd442b6cd228b838f74ba7
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2026-01-09 04:41:37 +0200

    feat(sorts): add TournamentSort (winner-tree)

---
+++ src/test/java/com/thealgorithms/sorts/TournamentSortTest.java
@@ -0,0 +1,9 @@
+package com.thealgorithms.sorts;
+
+public class TournamentSortTest extends SortingAlgorithmTest {
+
+    @Override
+    SortAlgorithm getSortAlgorithm() {
+        return new TournamentSort();
+    }
+}
