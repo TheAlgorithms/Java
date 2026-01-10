8ea4494d0b4c9acaae594e9147fc0209e0147b58
tarunjgupta <tarunjgupta01136@gmail.com>
2026-01-10 16:16:51 +0530

    Fix clang-format issues in CountNiceSubarrays

--- src/test/java/com/thealgorithms/slidingwindow/CountNiceSubarraysTest.java
+++ src/test/java/com/thealgorithms/slidingwindow/CountNiceSubarraysTest.java
@@ -1,5 +1,7 @@
 package com.thealgorithms.slidingwindow;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class CountNiceSubarraysTest {
