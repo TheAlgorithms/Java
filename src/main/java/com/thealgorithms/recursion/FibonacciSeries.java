51f56cdf434dbf6fbfc097a80323dea2442a8248
Chahat <s.chahatpreet@icloud.com>
2026-01-16 20:41:13 -0600

    fix: make small adjustment

--- src/main/java/com/thealgorithms/recursion/FibonacciSeries.java
+++ src/main/java/com/thealgorithms/recursion/FibonacciSeries.java
@@ -11,7 +11,6 @@ package com.thealgorithms.recursion;
 
 public final class FibonacciSeries {
     private FibonacciSeries() {
-
         throw new UnsupportedOperationException("Utility class");
     }
 
