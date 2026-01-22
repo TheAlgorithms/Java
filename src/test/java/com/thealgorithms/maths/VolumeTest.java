e7758fee4a0291657afea114e19dd2fa1fe13b7b
vil02 <65706193+vil02@users.noreply.github.com>
2026-01-22 06:47:54 +0100

    style: include `UTAO_JUNIT_ASSERTION_ODDITIES_USE_ASSERT_NOT_EQUALS`

--- src/test/java/com/thealgorithms/maths/VolumeTest.java
+++ src/test/java/com/thealgorithms/maths/VolumeTest.java
@@ -1,6 +1,6 @@
 package com.thealgorithms.maths;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,30 +10,30 @@ public class VolumeTest {
     public void volume() {
 
         /* test cube */
-        assertTrue(Volume.volumeCube(7) == 343.0);
+        assertEquals(Volume.volumeCube(7), 343.0);
 
         /* test cuboid */
-        assertTrue(Volume.volumeCuboid(2, 5, 7) == 70.0);
+        assertEquals(Volume.volumeCuboid(2, 5, 7), 70.0);
 
         /* test sphere */
-        assertTrue(Volume.volumeSphere(7) == 1436.7550402417319);
+        assertEquals(Volume.volumeSphere(7), 1436.7550402417319);
 
         /* test cylinder */
-        assertTrue(Volume.volumeCylinder(3, 7) == 197.92033717615698);
+        assertEquals(Volume.volumeCylinder(3, 7), 197.92033717615698);
 
         /* test hemisphere */
-        assertTrue(Volume.volumeHemisphere(7) == 718.3775201208659);
+        assertEquals(Volume.volumeHemisphere(7), 718.3775201208659);
 
         /* test cone */
-        assertTrue(Volume.volumeCone(3, 7) == 65.97344572538566);
+        assertEquals(Volume.volumeCone(3, 7), 65.97344572538566);
 
         /* test prism */
-        assertTrue(Volume.volumePrism(10, 2) == 20.0);
+        assertEquals(Volume.volumePrism(10, 2), 20.0);
 
         /* test pyramid */
-        assertTrue(Volume.volumePyramid(10, 3) == 10.0);
+        assertEquals(Volume.volumePyramid(10, 3), 10.0);
 
         /* test frustum */
-        assertTrue(Volume.volumeFrustumOfCone(3, 5, 7) == 359.188760060433);
+        assertEquals(Volume.volumeFrustumOfCone(3, 5, 7), 359.188760060433);
     }
 }
