b23ca4e4a8bf781f34dca6007933f2695c33c43c
sriieeu <sriieeuu@gmail.com>
2026-01-22 23:00:46 +0530

    Fix PMD violation: replace for loop with while loop

--- src/main/java/com/thealgorithms/strings/KnuthMorrisPratt.java
+++ src/main/java/com/thealgorithms/strings/KnuthMorrisPratt.java
@@ -26,7 +26,8 @@ final class KnuthMorrisPratt {
         final int[] lps = new int[n];
         int len = 0;
         lps[0] = 0;
-        for (int i = 1; i < n;) {
+        int i = 1;
+        while (i < n) {
             if (pattern.charAt(i) == pattern.charAt(len)) {
                 len++;
                 lps[i] = len;
