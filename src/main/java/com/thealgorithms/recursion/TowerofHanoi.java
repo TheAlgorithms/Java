fde4b3b4e7cc0a04a10a1451011e58628bf0ecf2
mahalaxmidasari18 <mahalaxmidasari18@gmail.com>
2026-01-22 12:55:50 +0530

    Explaning the Steps

--- src/main/java/com/thealgorithms/recursion/TowerofHanoi.java
+++ src/main/java/com/thealgorithms/recursion/TowerofHanoi.java
@@ -5,9 +5,15 @@ public class TowerofHanoi{
         System.out.println("Tranfer disk"+n+"from"+src+"to"+dest);
         return;
       }
+      //tranfer top n-1 from src to helper using dest as helper
       towerofHanoi(n-1, src,  dest,helper);
+
+
+      //transfer nth from src to dest
       System.out.println("tranfer disk"+n+"from"+src+"to"+helper);
+
+      //transfer n-1 from helper to dest using src as "helper"
       towerofHanoi(n-1, helper,src,dest);
     }
-    
+
 }
