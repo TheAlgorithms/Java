16e48b7bf8960d28fcdfe7e2b2351e2d867a3517
itsKomal1508 <komalachutrathod@gmail.com>
2026-01-22 22:14:22 +0530

    Apply clang-format

--- src/test/java/com/thealgorithms/matrix/SpiralMatrixIITest.java
+++ src/test/java/com/thealgorithms/matrix/SpiralMatrixIITest.java
@@ -1,46 +1,26 @@
 package com.thealgorithms.matrix;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-
 import org.junit.jupiter.api.Test;
 
-class SpiralMatrixIITest {
+class SpiralMatrixTest {
 
-    private final SpiralMatrixII spiral = new SpiralMatrixII();
-
-    @Test
-    void testNEquals1() {
-        int[][] expected = {{1}};
-        assertArrayEquals(expected, spiral.generateMatrix(1));
-    }
+    SpiralMatrix spiral = new SpiralMatrix();
 
     @Test
     void testNEquals3() {
-        int[][] expected = {
-            {1, 2, 3},
-            {8, 9, 4},
-            {7, 6, 5}
-        };
+        int[][] expected = {{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
         assertArrayEquals(expected, spiral.generateMatrix(3));
     }
 
     @Test
     void testNEquals4() {
-        int[][] expected = {
-            {1, 2, 3, 4},
-            {12, 13, 14, 5},
-            {11, 16, 15, 6},
-            {10, 9, 8, 7}
-        };
+        int[][] expected = {{1, 2, 3, 4}, {12, 13, 14, 5}, {11, 16, 15, 6}, {10, 9, 8, 7}};
         assertArrayEquals(expected, spiral.generateMatrix(4));
     }
-	@Test
-	void testNEquals2() {
-		int[][] expected = {
-			{1, 2},
-			{4, 3}
-		};
-		assertArrayEquals(expected, spiral.generateMatrix(2));
-	}
 
+    @Test
+    void testNEquals2() {
+        int[][] expected = {{1, 2}, {4, 3}};
+        assertArrayEquals(expected, spiral.generateMatrix(2));
+    }
 }
