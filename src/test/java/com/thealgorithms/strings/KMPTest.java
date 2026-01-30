f2a5c1e9ac12a46dbbbdecb854b71d4f4099a754
Divyansh Saxena <divyanshsaxena@gmail.com>
2026-01-31 01:31:52 +0530

    first commit

---
+++ src/test/java/com/thealgorithms/strings/KMPTest.java
@@ -0,0 +1,21 @@
+package com.thealgorithms.strings;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class KMPTest {
+
+    @Test
+    public void testKMPMatcher() {
+        assertEquals(List.of(0, 1), KMP.kmpMatcher("AAAAABAAABA", "AAAA"));
+        assertEquals(List.of(0, 2), KMP.kmpMatcher("ABCABC", "ABC"));
+        assertEquals(List.of(10), KMP.kmpMatcher("ABABDABACDABABCABAB", "ABABCABAB"));
+        assertEquals(List.of(), KMP.kmpMatcher("ABCDE", "FGH"));
+        assertEquals(List.of(), KMP.kmpMatcher("A", "AA"));
+        assertEquals(List.of(0, 1, 2), KMP.kmpMatcher("AAA", "A"));
+        assertEquals(List.of(0), KMP.kmpMatcher("A", "A"));
+        assertEquals(List.of(), KMP.kmpMatcher("", "A"));
+        assertEquals(List.of(), KMP.kmpMatcher("A", ""));
+    }
+}
---
+++ src/test/java/com/thealgorithms/strings/RabinKarpTest.java
@@ -0,0 +1,21 @@
+package com.thealgorithms.strings;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class RabinKarpTest {
+
+    @Test
+    public void testRabinKarpSearch() {
+        assertEquals(List.of(0, 1), RabinKarp.search("AAAAABAAABA", "AAAA"));
+        assertEquals(List.of(0, 2), RabinKarp.search("ABCABC", "ABC"));
+        assertEquals(List.of(10), RabinKarp.search("ABABDABACDABABCABAB", "ABABCABAB"));
+        assertEquals(List.of(), RabinKarp.search("ABCDE", "FGH"));
+        assertEquals(List.of(), RabinKarp.search("A", "AA"));
+        assertEquals(List.of(0, 1, 2), RabinKarp.search("AAA", "A"));
+        assertEquals(List.of(0), RabinKarp.search("A", "A"));
+        assertEquals(List.of(), RabinKarp.search("", "A"));
+        assertEquals(List.of(), RabinKarp.search("A", ""));
+    }
+}
