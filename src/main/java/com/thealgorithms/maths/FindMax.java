4e823d328772f184e876fc0faa721516c4474f31
Muhammad Muneeb Mubashar <muhammadmuneebmubashar@gmail.com>
2026-02-09 10:21:29 -0800

    Update FindMax to use Math.max for maximum calculation
    Fix logic to find maximum value in the array.

--- src/main/java/com/thealgorithms/maths/FindMax.java
+++ src/main/java/com/thealgorithms/maths/FindMax.java
@@ -18,9 +18,7 @@ public final class FindMax {
         }
         int max = array[0];
         for (int i = 1; i < n; i++) {
-            if (array[i] > max) {
-                max = array[i];
-            }
+            max = Math.max(array[i] , array[i+1]);
         }
         return max;
     }
