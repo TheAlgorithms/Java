591b36fcf26b67c7b595e41278acaf3cc933f8df
maryamh12 <maryam_hazrati94@yahoo.com>
2026-03-12 16:17:53 +0000

    Fix indentation to satisfy clang-format.

--- src/main/java/com/thealgorithms/backtracking/WordSearch.java
+++ src/main/java/com/thealgorithms/backtracking/WordSearch.java
@@ -82,9 +82,8 @@ public class WordSearch {
         // DFS search
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-
                 if (board[i][j] == word.charAt(0) && dfs(board, i, j, word, 0)) {
-                        return true;
+                    return true;
                 }
             }
         }
