1b35e4b0d3ea68a7d2a138570e1ba5fd72d62b96
kvadrik <41710943+kvadrik@users.noreply.github.com>
2026-03-22 15:21:39 +0200

    Add correlation tests for various scenarios

--- src/test/java/com/thealgorithms/maths/CorrelationTest.java
+++ src/test/java/com/thealgorithms/maths/CorrelationTest.java
@@ -9,6 +9,7 @@ public class CorrelationTest {
 
     public static final double DELTA = 1e-9;
 
+    // Regular correlation test
     public void testCorrelationFirst() {
         double[] x = {1, 2, 3, 4};
         double[] y = {7, 1, 4, 9};
@@ -16,6 +17,7 @@ public class CorrelationTest {
         assertEquals(0.3319700011, Correlation.correlation(x, y, n), DELTA);
     }
 
+    // Regular correlation test (zero correlation)
     public void testCorrelationSecond() {
         double[] x = {1, 2, 3, 4};
         double[] y = {5, 0, 9, 2};
@@ -23,6 +25,7 @@ public class CorrelationTest {
         assertEquals(0, Correlation.correlation(x, y, n), DELTA);
     }
 
+    // Correlation with a constant variable is taken to be zero
     public void testCorrelationConstant() {
         double[] x = {1, 2, 3};
         double[] y = {4, 4, 4};
@@ -30,6 +33,7 @@ public class CorrelationTest {
         assertEquals(0, Correlation.correlation(x, y, n), DELTA);
     }
 
+    // Linear dependence gives correlation 1
     public void testCorrelationLinearDependence() {
         double[] x = {1, 2, 3, 4};
         double[] y = {6, 8, 10, 12};
@@ -37,6 +41,7 @@ public class CorrelationTest {
         assertEquals(1, Correlation.correlation(x, y, n), DELTA);
     }
 
+    // Inverse linear dependence gives correlation -1
     public void testCorrelationInverseLinearDependence() {
         double[] x = {1, 2, 3, 4, 5};
         double[] y = {18, 15, 12, 9, 6};
