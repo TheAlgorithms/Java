5609777dd664102b21cf5fb1f489eebd76896eac
Vansh Sharma <vansh.sharma7171717@gmail.com>
2026-04-01 17:04:42 +0530

    chore: final formatting and test fix for BigInteger

--- src/test/java/com/thealgorithms/maths/FactorialTest.java
+++ src/test/java/com/thealgorithms/maths/FactorialTest.java
@@ -2,6 +2,7 @@ package com.thealgorithms.maths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.math.BigInteger;
 import org.junit.jupiter.api.Test;
 
