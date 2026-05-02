adb3db5bbde2d0bf9a736342f355641c3688e234
Abdul-Rehman-svg <aminabibi7622123@gmail.com>
2026-05-02 12:29:56 +0500

    Update Anagrams.java
    fix: remove invalid reference [1] in Anagrams.java

--- src/main/java/com/thealgorithms/strings/Anagrams.java
+++ src/main/java/com/thealgorithms/strings/Anagrams.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 /**
  * An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
- * typically using all the original letters exactly once.[1]
+ * typically using all the original letters exactly once.
  * For example, the word anagram itself can be rearranged into nag a ram,
  * also the word binary into brainy and the word adobe into abode.
  * Reference from https://en.wikipedia.org/wiki/Anagram
