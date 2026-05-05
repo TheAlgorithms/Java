a8a8e13432ec3fb1c8b05b806a788939819e4702
trynafind-sumanyu <sumanyurajput2005@gmail.com>
2026-05-05 18:37:36 +0530

    Fixing the formats

--- src/main/java/com/thealgorithms/recursion/Permutations.java
+++ src/main/java/com/thealgorithms/recursion/Permutations.java
@@ -1,6 +1,10 @@
 package com.thealgorithms.recursion;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * This class provides methods to generate all permutations
--- src/test/java/com/thealgorithms/recursion/Permutations.java
+++ src/test/java/com/thealgorithms/recursion/Permutations.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 class PermutationsTest {
