50c8c1d23cdba65dd3d582bdb6f57fd849a7398d
AshishKumar1297 <ashishkumarsir8@gmail.com>
2026-05-12 14:39:17 +0530

    Fix: clean NeonNumber code and fix formatting

--- src/main/java/com/thealgorithms/maths/NeonNumber.java
+++ src/main/java/com/thealgorithms/maths/NeonNumber.java
@@ -1,30 +1,24 @@
 package com.thealgorithms.maths;
 
+/**
+ * Neon Number algorithm.
+ * A number whose sum of digits of its square equals the number itself.
+ * Example: 9^2=81⇒8+1=9
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Recreational_mathematics">Wikipedia</a>
+ */
 public class NeonNumber {
 
-    private NeonNumber(){
+    private NeonNumber() {
     }
-    /**
-     * Check if a number is Neon Number.
-     * A neon number is a number where a sum of digits of its square equals the number itself.
-     * Example : 9--> 9^2 = 81  --> 8+1 = 9
-     * @see <a href="https://en.wikipedia.org/wiki/Recreational_mathematics">
-     *         *    Wikipedia - Recreational Mathematics</a>
-     * @param number the number to check
-     * @return true if neon number, else --> false
-      */
 
-
-    public static boolean isNeon(int number)
-    {
-        int square = number*number;
+    public static boolean isNeon(int number) {
+        int square = number * number;
         int digitSum = 0;
-        while(square>0){
+        while (square > 0) {
             digitSum += square % 10;
-            square/= 10;
+            square /= 10;
         }
         return digitSum == number;
     }
-
-
 }
