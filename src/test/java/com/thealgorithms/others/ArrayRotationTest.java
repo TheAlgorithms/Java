2eb72d9d8c4143e1f54b2174e6a6ef8fb9ec4d43
bhanu1012 <bhanubasyan@gmail.com>
2026-05-17 20:33:52 +0530

    Fix formatting in ArrayRotation tests

--- src/test/java/com/thealgorithms/others/ArrayRotationTest.java
+++ src/test/java/com/thealgorithms/others/ArrayRotationTest.java
@@ -8,38 +8,40 @@ public class ArrayRotationTest {
 
     @Test
     void shouldRotateArrayRightByTwoPositions() {
-        int[] values = {1, 2, 3, 4, 5};
+        int[] values = { 1, 2, 3, 4, 5 };
 
         ArrayRotation.rotateRight(values, 2);
 
-        assertArrayEquals(new int[] {4, 5, 1, 2, 3}, values);
+        assertArrayEquals(new int[] { 4, 5, 1, 2, 3 }, values);
+
     }
 
     @Test
     void shouldRotateArrayLeftByTwoPositions() {
-        int[] values = {1, 2, 3, 4, 5};
+        int[] values = { 1, 2, 3, 4, 5 };
 
         ArrayRotation.rotateLeft(values, 2);
 
-        assertArrayEquals(new int[] {3, 4, 5, 1, 2}, values);
+        assertArrayEquals(new int[] { 3, 4, 5, 1, 2 }, values);
+
     }
 
     @Test
     void shouldHandleRotationGreaterThanArrayLength() {
-        int[] values = {10, 20, 30, 40};
+        int[] values = { 10, 20, 30, 40 };
 
-        ArrayRotation.rotateRight(values, 6);
+        ArrayRotation.rotateRight(values, 2);
 
-        assertArrayEquals(new int[] {30, 40, 10, 20}, values);
+        assertArrayEquals(new int[] { 30, 40, 10, 20 }, values);
     }
 
     @Test
     void shouldKeepSingleElementArrayUnchanged() {
-        int[] values = {99};
+        int[] values = { 99 };
 
         ArrayRotation.rotateLeft(values, 5);
 
-        assertArrayEquals(new int[] {99}, values);
+        assertArrayEquals(new int[] { 99 }, values);
     }
 
     @Test
@@ -53,11 +55,10 @@ public class ArrayRotationTest {
 
     @Test
     void shouldReturnOriginalArrayWhenRotationIsZero() {
-        int[] values = {7, 8, 9};
+        int[] values = { 7, 8, 9 };
 
         ArrayRotation.rotateLeft(values, 0);
 
-        assertArrayEquals(new int[] {7, 8, 9}, values);
+        assertArrayEquals(new int[] { 7, 8, 9 }, values);
     }
- }
-
+}
