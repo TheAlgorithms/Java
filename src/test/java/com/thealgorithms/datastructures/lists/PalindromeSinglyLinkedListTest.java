1c68b098400d05d47c1f61d277e580e78ef80baf
Priyanshu <pv254424@gmail.com>
2026-06-21 17:32:52 +0530

    style: add blank line between static and non-static imports

--- src/test/java/com/thealgorithms/datastructures/lists/PalindromeSinglyLinkedListTest.java
+++ src/test/java/com/thealgorithms/datastructures/lists/PalindromeSinglyLinkedListTest.java
@@ -2,6 +2,7 @@ package com.thealgorithms.datastructures.lists;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class PalindromeSinglyLinkedListTest {
