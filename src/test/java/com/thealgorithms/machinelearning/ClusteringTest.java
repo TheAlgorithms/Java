929c92dc7c49fedd76d471038c6031bb780982a8
Ashish2343 <a98126959@gmail.com>
2026-08-09 10:35:25 +0530

    Fixed static imports

--- src/test/java/com/thealgorithms/machinelearning/ClusteringTest.java
+++ src/test/java/com/thealgorithms/machinelearning/ClusteringTest.java
@@ -1,13 +1,9 @@
 package com.thealgorithms.machinelearning;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.thealgorithms.machinelearning.Clustering.ClusteringResult;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ClusteringTest {
 
