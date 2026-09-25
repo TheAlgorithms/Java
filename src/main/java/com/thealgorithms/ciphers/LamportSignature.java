e335f952272634647742a3bda26b3ccf5402e51f
nitin1053 <nitinjha1053@gmail.com>
2026-09-25 13:39:39 +0530

    Fix for the check

--- src/main/java/com/thealgorithms/ciphers/LamportSignature.java
+++ src/main/java/com/thealgorithms/ciphers/LamportSignature.java
@@ -1,6 +1,5 @@
 package com.thealgorithms.ciphers;
 
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
